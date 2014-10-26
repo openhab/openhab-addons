a6a9c5eb49c379a51047e0791741276b1c6b251f
Kai Kreuzer <kai@openhab.org>
2014-10-26 21:47:32 +0100

    renamed core.logging to core.init
    Signed-off-by: Kai Kreuzer <kai@openhab.org> (github: @kaikreuzer)

--- bundles/core/org.openhab.core/src/main/java/org/openhab/core/internal/CoreActivator.java
+++ bundles/core/org.openhab.core/src/main/java/org/openhab/core/internal/CoreActivator.java
@@ -8,19 +8,10 @@
  */
 package org.openhab.core.internal;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
-import java.util.List;
-import java.util.UUID;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
-import org.eclipse.smarthome.config.core.ConfigConstants;
 import org.eclipse.smarthome.model.rule.runtime.RuleEngine;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -38,12 +29,6 @@ public class CoreActivator implements BundleActivator {
 
 	private static Logger logger = LoggerFactory.getLogger(CoreActivator.class);
 
-	private static final String STATIC_CONTENT_DIR = "id";
-
-	private static final String UUID_FILE_NAME = "uuid";
-
-	private static final String VERSION_FILE_NAME = "version";
-
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -52,8 +37,6 @@ public class CoreActivator implements BundleActivator {
 	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
-		createUUIDFile();
-
 		String versionString = context.getBundle().getVersion().toString();
 		String buildString = "";
 		// if the version string contains a qualifier, remove it!
@@ -64,7 +47,6 @@ public class CoreActivator implements BundleActivator {
 			}
 			versionString = StringUtils.substringBeforeLast(versionString, ".");
 		}
-		createVersionFile(versionString);
 		
 		if (buildString.equals("")) {
 			logger.info("openHAB runtime has been started (v{}).",
@@ -105,72 +87,4 @@ public class CoreActivator implements BundleActivator {
 		logger.info("openHAB runtime has been terminated.");
 	}
 
-	/**
-	 * Creates a unified unique id and writes it to the
-	 * <code>webapps/static</code> directory. An existing <code>uuid</code> file
-	 * won't be overwritten.
-	 */
-	private String createUUIDFile() {
-		File file = new File(getUserDataDir() + File.separator + STATIC_CONTENT_DIR + File.separator
-				+ UUID_FILE_NAME);
-		String uuidString = "";
-
-		if (!file.exists()) {
-			uuidString = UUID.randomUUID().toString();
-			writeFile(file, uuidString);
-		} else {
-			uuidString = readFirstLine(file);
-			logger.debug("UUID file already exists at '{}' with content '{}'",
-					file.getAbsolutePath(), uuidString);
-		}
-
-		return uuidString;
-	}
-
-	/**
-	 * Creates a file with given <code>version</code>. The file will be
-	 * overwritten each time openHAB has been started.
-	 * 
-	 * @param version
-	 *            the version number to write to the version file
-	 */
-	private void createVersionFile(String version) {
-		File file = new File(getUserDataDir() + File.separator + STATIC_CONTENT_DIR + File.separator
-				+ VERSION_FILE_NAME);
-		writeFile(file, version);
-	}
-
-	private void writeFile(File file, String content) {
-		// create intermediary directories
-		file.getParentFile().mkdirs();
-		try {
-			IOUtils.write(content, new FileOutputStream(file));
-			logger.debug("Created file '{}' with content '{}'",
-					file.getAbsolutePath(), content);
-		} catch (FileNotFoundException e) {
-			logger.error("Couldn't create file '" + file.getPath() + "'.", e);
-		} catch (IOException e) {
-			logger.error("Couldn't write to file '" + file.getPath() + "'.", e);
-		}
-	}
-
-	private String readFirstLine(File file) {
-		List<String> lines = null;
-		try {
-			lines = IOUtils.readLines(new FileInputStream(file));
-		} catch (IOException ioe) {
-			// no exception handling - we just return the empty String
-		}
-		return lines != null && lines.size() > 0 ? lines.get(0) : "";
-	}
-
-	private String getUserDataDir() {
-		String progArg = System.getProperty(ConfigConstants.USERDATA_DIR_PROG_ARGUMENT);
-		if (progArg != null) {
-			return progArg;
-		} else {
-			return "userdata";
-		}
-		
-	}
 }
