0178bfcdec0fac51bc93a5ab955b8a717351ea1c
pamapa <wsx,456>
2014-12-17 16:47:10 +0100

    fix copyrights

--- addons/binding/org.openhab.binding.BluetoothLE/src/main/java/org/openhab/binding/BluetoothLE/discovery/BluetoothLEDiscoveryServiceImpl.java
+++ addons/binding/org.openhab.binding.BluetoothLE/src/main/java/org/openhab/binding/BluetoothLE/discovery/BluetoothLEDiscoveryServiceImpl.java
@@ -5,6 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.openhab.binding.BluetoothLE.discovery;
 
 import static org.openhab.binding.BluetoothLE.BluetoothLEBindingConstants.UUID_BATTERY;
