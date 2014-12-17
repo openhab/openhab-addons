8a37a1e82f23a55087ee23331d699c269999c085
pamapa <wsx,456>
2014-12-17 16:46:33 +0100

    fix copyrights

--- addons/binding/org.openhab.binding.BluetoothLE/src/main/java/org/openhab/binding/BluetoothLE/protocol/BluetoothLEUDPConnector.java
+++ addons/binding/org.openhab.binding.BluetoothLE/src/main/java/org/openhab/binding/BluetoothLE/protocol/BluetoothLEUDPConnector.java
@@ -6,6 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.openhab.binding.BluetoothLE.protocol;
 
 import java.net.DatagramPacket;
