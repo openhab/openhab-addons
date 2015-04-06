faa290f4225e5a3a2315305d2dde30a64b052db9
Tobias Bräutigam <tbraeutigam@gmail.com>
2015-04-06 10:33:37 +0200

    Merge pull request #1 from openhab/master
    Merge with master
    
    Merge pull request #2 from openhab/master
    
    Merge with master
    
    initial contribution of the pulseaudio binding, including discovery
    service
    
    Signed-off-by: Tobias Bräutigam <tbraeutigam@gmail.com>
    
    - check if the server is connectable before creating a bridge for it
    - add binding to pom.xml
    
    Signed-off-by: Tobias Bräutigam <tbraeutigam@gmail.com>
    
    use real pulseaudio item names
    
    Signed-off-by: Tobias Bräutigam <tbraeutigam@gmail.com>
    
    - finer grained thing definitions
    - sink-inputs can be rerouted
    
    Signed-off-by: Tobias Bräutigam <tbraeutigam@gmail.com>
    
    - fixed misdefined channel
    - some code cleanup
    
    Signed-off-by: Tobias Bräutigam <tbraeutigam@gmail.com>
    
    the mdns bundles name has changed
    
    Signed-off-by: Tobias Bräutigam <tbraeutigam@gmail.com>
    
    missing part of the last commit
    
    Signed-off-by: Tobias Bräutigam <tbraeutigam@gmail.com>
    
    changes due to Kai´s code review
    
    Signed-off-by: Tobias Bräutigam <tbraeutigam@gmail.com>
    
    log message removed
    
    Signed-off-by: Tobias Bräutigam <tbraeutigam@gmail.com>
    
    moved client connection into a scheduled runnable
    
    Signed-off-by: Tobias Bräutigam <tbraeutigam@gmail.com>

---
+++ addons/binding/org.openhab.binding.pulseaudio/src/main/java/org/openhab/binding/pulseaudio/internal/items/AbstractDeviceConfig.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2010-2014, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.pulseaudio.internal.items;
+
+/**
+ * Abstract root class for all items in an pulseaudio server. Every item in a
+ * pulseaudio server has a name and a unique id which can be inherited by this
+ * class.
+ * 
+ * @author Tobias Bräutigam
+ * @since 1.2.0
+ */
+public abstract class AbstractDeviceConfig {
+
+	protected int id;
+	protected String name;
+	
+	public AbstractDeviceConfig(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	/**
+	 * returns the internal id of this device
+	 * @return
+	 */
+	public int getId() {
+		return id;
+	}
+
+	public String getUIDName() {
+		return name.replaceAll("[^A-Za-z0-9_]", "_");
+	}
+	public String getPaName() {
+		return name;
+	}
+}
