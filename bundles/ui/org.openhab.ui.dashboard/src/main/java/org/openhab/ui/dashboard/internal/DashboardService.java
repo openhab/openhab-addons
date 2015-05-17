67d2de7bbced7458af53708e0eb51767fcdd1605
Kai Kreuzer <kai@openhab.org>
2015-05-17 22:46:16 +0200

    Introducing DashboardTiles that can be dynamically registered in order to be listed on the openHAB Dashboard (fixes #235).
    Signed-off-by: Kai Kreuzer <kai@openhab.org> (github: @kaikreuzer)

--- bundles/ui/org.openhab.ui.dashboard/src/main/java/org/openhab/ui/dashboard/internal/DashboardService.java
+++ bundles/ui/org.openhab.ui.dashboard/src/main/java/org/openhab/ui/dashboard/internal/DashboardService.java
@@ -8,6 +8,18 @@
  */
 package org.openhab.ui.dashboard.internal;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Hashtable;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.apache.commons.io.IOUtils;
+import org.openhab.ui.dashboard.DashboardTile;
+import org.osgi.framework.BundleContext;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.http.HttpService;
 import org.osgi.service.http.NamespaceException;
@@ -15,24 +27,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This component registers the dashboard.
+ * This component registers the dashboard resources.
  * 
  * @author Kai Kreuzer - Initial contribution
  */
 public class DashboardService {
 	
     public static final String DASHBOARD_ALIAS = "/start";
-    
+
+    /** the name of the servlet to be used in the URL */
+    public static final String SERVLET_NAME = "index";
+
     private static final Logger logger = LoggerFactory.getLogger(DashboardService.class);
 
     protected HttpService httpService;
+    
+    protected Set<DashboardTile> tiles = new CopyOnWriteArraySet<>();
+
+    private BundleContext bundleContext;
 
     protected void activate(ComponentContext componentContext) {
         try {
+            bundleContext = componentContext.getBundleContext();
+            Hashtable<String, String> props = new Hashtable<String, String>();
+            httpService.registerServlet(DASHBOARD_ALIAS + "/" + SERVLET_NAME, createServlet(), props, httpService.createDefaultHttpContext());
             httpService.registerResources(DASHBOARD_ALIAS, "web", null);
             logger.info("Started dashboard at " + DASHBOARD_ALIAS);
-        } catch (NamespaceException e) {
-            logger.error("Error during servlet startup", e);
+        } catch (NamespaceException | ServletException e) {
+            logger.error("Error during dashboard startup: {}", e.getMessage());
         }
     }
 
@@ -48,5 +70,41 @@ public class DashboardService {
     protected void unsetHttpService(HttpService httpService) {
         this.httpService = null;
     }
+    
+    protected void addDashboardTile(DashboardTile tile) {
+        tiles.add(tile);
+    }
 
+    protected void removeDashboardTile(DashboardTile tile) {
+        tiles.remove(tile);
+    }
+    
+    protected HttpServlet createServlet() {
+        String indexTemplate;
+        String entryTemplate;
+        
+        URL index = bundleContext.getBundle().getEntry("templates/index.html");
+        if (index != null) {
+            try {
+                indexTemplate = IOUtils.toString(index.openStream());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            throw new RuntimeException("Cannot find index.html - failed to initialize dashboard servlet");
+        }
+
+        URL entry = bundleContext.getBundle().getEntry("templates/entry.html");
+        if (entry != null) {
+            try {
+                entryTemplate = IOUtils.toString(entry.openStream());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            throw new RuntimeException("Cannot find entry.html - failed to initialize dashboard servlet");
+        }
+        
+        return new DashboardServlet(indexTemplate, entryTemplate, tiles);
+    }
 }
