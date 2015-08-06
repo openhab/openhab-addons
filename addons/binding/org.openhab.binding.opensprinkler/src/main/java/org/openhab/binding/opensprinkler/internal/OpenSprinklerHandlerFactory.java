121ba6e36ea7ca3f87d2595036f76159fb71eafc
Chris Graham <crackerstealth@gmail.com>
2015-08-06 00:13:16 -0600

    Initial contribution of the OpenSprinkler binding for OpenHAB 2 created.
    Also-by: Robert Savage (robert@pi4j.com) [http://pi4j.com]
    Also-by: Daniel Sendula (daniel@pi4j.com) [http://pi4j.com]

---
+++ addons/binding/org.openhab.binding.opensprinkler/src/main/java/org/openhab/binding/opensprinkler/internal/OpenSprinklerHandlerFactory.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2014 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.opensprinkler.internal;
+
+import static org.openhab.binding.opensprinkler.OpenSprinklerBindingConstants.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.openhab.binding.opensprinkler.handler.OpenSprinklerHTTPHandler;
+import org.openhab.binding.opensprinkler.handler.OpenSprinklerPiHandler;
+
+/**
+ * The {@link OpenSprinklerHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author Chris Graham - Initial contribution
+ */
+public class OpenSprinklerHandlerFactory extends BaseThingHandlerFactory {
+    private final static Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = new HashSet<ThingTypeUID>(
+            Arrays.asList(OPENSPRINKLER_THING, OPENSPRINKLERPI_THING));
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
+
+    @Override
+    protected ThingHandler createHandler(Thing thing) {
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (thingTypeUID.equals(OPENSPRINKLER_THING)) {
+            return new OpenSprinklerHTTPHandler(thing);
+        } else if (thingTypeUID.equals(OPENSPRINKLERPI_THING)) {
+            return new OpenSprinklerPiHandler(thing);
+        }
+
+        return null;
+    }
+}
