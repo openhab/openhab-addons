6dc13f5bcdd2a873546f270d56dfabeca23bb279
Marcel Verpaalen <marcel@verpaalen.com>
2015-10-21 15:25:22 +0200

    MAX! improve stability & actual updates * Fixes initialisation issues for MAX! devices during when loading the bridge * Improved communication module * Code simplification * Experimental: Implementation of actuals update (issue #203) * Foundation for #201 * Refactoring binding structure
    Signed-off-by: Marcel Verpaalen <marcel@verpaalen.com>

--- addons/binding/org.openhab.binding.max/src/main/java/org/openhab/binding/max/internal/device/RoomInformation.java
+++ addons/binding/org.openhab.binding.max/src/main/java/org/openhab/binding/max/internal/device/RoomInformation.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.max.internal.message;
+package org.openhab.binding.max.internal.device;
 
 /**
 * Room information provided b the M message meta information. 
--- addons/binding/org.openhab.binding.max/src/main/java/org/openhab/binding/max/internal/device/ShutterContact.java
+++ addons/binding/org.openhab.binding.max/src/main/java/org/openhab/binding/max/internal/device/ShutterContact.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.max.internal.message;
+package org.openhab.binding.max.internal.device;
 
 import org.eclipse.smarthome.core.library.types.OpenClosedType;
 
