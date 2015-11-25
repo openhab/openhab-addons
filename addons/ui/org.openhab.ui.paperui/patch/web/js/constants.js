b95374cf019e4e38bc7eb0c22b6b7191b6f75312
Kai Kreuzer <kai@openhab.org>
2015-11-25 15:37:04 +0100

    enabled advanced mode by default and hide control pane from menu
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

---
+++ addons/ui/org.openhab.ui.paperui/patch/web/js/constants.js
@@ -0,0 +1,16 @@
+/* This file can be be overridden by fragments for customization */
+angular.module('PaperUI.constants', [])
+.constant('globalConfig', {
+   'advancedDefault': true 
+}).constant('restConfig', {
+  'restPath': '/rest',
+  'eventPath': $('#authentication').data('access-token') != '{{ACCESS_TOKEN}}' ? '/rest/events?access_token=' + $('#authentication').data('access-token') : '/rest/events'
+}).constant('moduleConfig', {
+  'control': false,
+  'configuration': true,
+  'setup': true,
+  'configuration': true,
+  'extensions': true,
+  'rules': false,
+  'preferences': true
+});
