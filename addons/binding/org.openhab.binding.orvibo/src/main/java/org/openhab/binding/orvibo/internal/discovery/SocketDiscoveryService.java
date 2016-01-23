fbe003a7f923536b413b82b5a44851328196dc24
Daniel Walters <danielwalters1@hotmail.com>
2016-01-23 22:30:52 +0000

    Initial check in
    Signed-off-by: Daniel Walters <danielwalters1@hotmail.com>

---
+++ addons/binding/org.openhab.binding.orvibo/src/main/java/org/openhab/binding/orvibo/internal/discovery/SocketDiscoveryService.java
@@ -0,0 +1,94 @@
+package org.openhab.binding.orvibo.internal.discovery;
+
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.smarthome.config.discovery.AbstractDiscoveryService;
+import org.eclipse.smarthome.config.discovery.DiscoveryResult;
+import org.eclipse.smarthome.config.discovery.DiscoveryResultBuilder;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.ThingUID;
+import org.openhab.binding.orvibo.OrviboBindingConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.tavalin.s20.S20Client;
+import com.github.tavalin.s20.S20Client.SocketDiscoveryListener;
+import com.github.tavalin.s20.socket.Socket;
+
+public class SocketDiscoveryService extends AbstractDiscoveryService implements SocketDiscoveryListener {
+
+    private final Logger logger = LoggerFactory.getLogger(SocketDiscoveryService.class);
+    private final static int SEARCH_TIME = 60;
+    private S20Client s20Client;
+
+    public SocketDiscoveryService() throws SocketException {
+        super(getSupportedThingTypeUIDs(), SEARCH_TIME);
+    }
+
+    private static Set<ThingTypeUID> getSupportedThingTypeUIDs() {
+        return Collections.singleton(OrviboBindingConstants.THING_TYPE_S20);
+    }
+
+    @Override
+    protected void activate(Map<String, Object> configProperties) {
+        try {
+            s20Client = S20Client.getInstance();
+            super.activate(configProperties);
+        } catch (SocketException ex) {
+            logger.error("Error occured while activating S20 discovery service: " + ex.getMessage(), ex);
+        }
+    }
+
+    @Override
+    protected void startScan() {
+        logger.debug("starting manual scan");
+        s20Client.addSocketDiscoveryListener(this);
+        s20Client.globalDiscovery();
+        for (Socket socket : s20Client.getAllSocketsCollection().values()) {
+            doThingDiscovered(socket);
+        }
+    }
+
+    @Override
+    protected void startBackgroundDiscovery() {
+        logger.debug("starting automatic background scan");
+        s20Client.addSocketDiscoveryListener(this);
+        s20Client.globalDiscovery();
+
+    }
+
+    @Override
+    protected void stopBackgroundDiscovery() {
+        s20Client.removeSocketDiscoveryListener(this);
+    }
+
+    @Override
+    public void socketDiscovered(Socket socket) {
+        doThingDiscovered(socket);
+    }
+
+    private DiscoveryResult createDiscoveryResult(Socket socket) {
+        ThingUID thingUID = getUID(socket);
+        String label = socket.getLabel();
+        if (StringUtils.isBlank(label)) {
+            label = "S20";
+        }
+        return DiscoveryResultBuilder.create(thingUID).withLabel(label)
+                .withProperty(OrviboBindingConstants.CONFIG_PROPERTY_DEVICE_ID, socket.getDeviceId()).build();
+    }
+
+    private ThingUID getUID(Socket socket) {
+        ThingUID thingUID = new ThingUID(OrviboBindingConstants.THING_TYPE_S20, socket.getDeviceId());
+        return thingUID;
+    }
+
+    private void doThingDiscovered(Socket socket) {
+        DiscoveryResult discoveryResult = createDiscoveryResult(socket);
+        thingDiscovered(discoveryResult);
+    }
+
+}
