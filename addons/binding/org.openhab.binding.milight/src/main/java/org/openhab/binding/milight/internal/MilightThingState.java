6c34d930fa8665ffb183c5b59728b92726762af6
David Gräff <david.graeff@web.de>
2016-02-17 13:06:06 +0100

    The milight binding for OH2.  * Discovery added.  * Milight controller are modelled as bridges. Light bulbs are modelled as things.  * All protocol related stuff is in internal/protocol. MilightCommunication is a pure,    stateless protocol implementation for milight compatible bridges. MilightDiscover    is for the discovery part.
     Tested with easybulb plus (milight controller V4) and RGB bulbs.
    
    Signed-off-by: David Graeff <david.graeff@web.de>

---
+++ addons/binding/org.openhab.binding.milight/src/main/java/org/openhab/binding/milight/internal/MilightThingState.java
@@ -0,0 +1,175 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.milight.internal;
+
+import org.openhab.binding.milight.internal.protocol.MilightCommunication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This represents the configuration of a openHAB item that is binded to a Mi-Light
+ * bulb. It contains the following information:
+ *
+ * <ul>
+ * <li>The channel number the bulb has on the Milight bridge where 0 represents all white bulbs, 1-4 white channels an 5
+ * RGB bulbs.</li>
+ * <li>The binding type of the Mi-Light item</li>
+ * <ul>
+ * <li>Brightness</li>
+ * <li>Color temperature</li>
+ * <li>RGB</li>
+ * <li>Night Mode</li>
+ * <li>Disco Mode</li>
+ * <li>Disco Speed</li>
+ * </ul>
+ *
+ * @author Hans-Joerg Merk
+ * @author Kai Kreuzer
+ * @since 1.3.0
+ */
+public class MilightThingState {
+    static final Logger logger = LoggerFactory.getLogger(MilightThingState.class);
+
+    /**
+     * The channel number under which the bulb is filed in the Mi-Light bridge.
+     */
+    private final int bulb;
+    private final MilightCommunication com;
+
+    private int brightness, discoSpeed, discoMode;
+    private int colorTemperature; // only for white leds applicable
+    private float hue; // only for rgb(w) leds applicable
+
+    public MilightThingState(int bulb, MilightCommunication com) {
+        this.bulb = bulb;
+        this.com = com;
+    }
+
+    /**
+     * @return The deviceId that has been declared in the binding
+     *         configuration.
+     */
+    public String getDeviceId() {
+        return com.getBridgeId();
+    }
+
+    /**
+     * @return The channel number that has been declared in the binding
+     *         configuration.
+     */
+    public int getChannelNumber() {
+        return bulb;
+    }
+
+    /**
+     *
+     * @param value A value from 0..100
+     */
+    public void setBrightness(int value) {
+        brightness = com.setBrightness(bulb, value, brightness);
+    }
+
+    public void setDiscoSpeed(int value) {
+        discoSpeed = com.setDiscoSpeed(bulb, value, discoSpeed);
+    }
+
+    public void setDiscoMode(int value) {
+        discoMode = com.setDiscoMode(bulb, value, discoMode);
+    }
+
+    /**
+     * Set the color temperature (cold white ... warm white)
+     * Only for white leds applicable.
+     */
+    public void setColorTemperature(int value) {
+        colorTemperature = com.setColorTemperature(bulb, value, colorTemperature);
+    }
+
+    public void increaseBrightness() {
+        brightness = com.increaseBrightness(bulb, brightness);
+    }
+
+    public void decreaseBrightness() {
+        brightness = com.decreaseBrightness(bulb, brightness);
+    }
+
+    public void warmer() {
+        brightness = com.warmer(bulb, brightness);
+    }
+
+    public void cooler() {
+        brightness = com.cooler(bulb, brightness);
+    }
+
+    public void nextDiscoMode() {
+        discoMode = com.nextDiscoMode(bulb, discoMode);
+    }
+
+    public void previousDiscoMode() {
+        discoMode = com.previousDiscoMode(bulb, discoMode);
+    }
+
+    public void increaseSpeed() {
+        com.increaseSpeed(bulb);
+    }
+
+    public void decreaseSpeed() {
+        com.decreaseSpeed(bulb);
+    }
+
+    public void setNightMode() {
+        com.setNightMode(bulb);
+    }
+
+    public void setWhiteMode() {
+        com.setWhiteMode(bulb);
+    }
+
+    public void setFull() {
+        brightness = com.setFull(bulb);
+    }
+
+    public void setOn() {
+        com.setOn(bulb);
+    }
+
+    public void setOff() {
+        brightness = com.setOff(bulb);
+    }
+
+    /**
+     * Set color
+     *
+     * @param hue A value from 0 to 360
+     */
+    public void setColor(int hue) {
+        com.setColor(bulb, hue);
+        this.hue = (hue);
+    }
+
+    public int getBrightness() {
+        return brightness;
+    }
+
+    public int getDiscoSpeed() {
+        return discoSpeed;
+    }
+
+    public int getDiscoMode() {
+        return discoMode;
+    }
+
+    public int getColorTemperature() {
+        return colorTemperature;
+    }
+
+    public float getHue() {
+        return hue;
+    }
+}
---
+++ addons/binding/org.openhab.binding.milight/src/main/java/org/openhab/binding/milight/internal/discovery/MilightBridgeDiscovery.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.milight.internal.discovery;
+
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Map;
+import java.util.TimerTask;
+import java.util.TreeMap;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.smarthome.config.discovery.AbstractDiscoveryService;
+import org.eclipse.smarthome.config.discovery.DiscoveryResult;
+import org.eclipse.smarthome.config.discovery.DiscoveryResultBuilder;
+import org.eclipse.smarthome.core.thing.ThingUID;
+import org.openhab.binding.milight.MilightBindingConstants;
+import org.openhab.binding.milight.internal.MilightHandlerFactory;
+import org.openhab.binding.milight.internal.protocol.MilightDiscover;
+import org.openhab.binding.milight.internal.protocol.MilightDiscover.DiscoverResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link MilightHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author David Graeff - Initial contribution
+ */
+public class MilightBridgeDiscovery extends AbstractDiscoveryService implements DiscoverResult {
+    private ScheduledFuture<?> backgroundFuture;
+    private final InetAddress broadcast;
+    private Logger logger = LoggerFactory.getLogger(MilightBridgeDiscovery.class);
+    private MilightDiscover receiveThread;
+
+    class DetectTask extends TimerTask {
+        @Override
+        public void run() {
+            receiveThread.sendDiscover(scheduler);
+        }
+    }
+
+    private void startDiscoveryService() {
+        if (receiveThread == null) {
+            try {
+                receiveThread = new MilightDiscover(broadcast, this, 50, 2000 / 50);
+            } catch (SocketException e) {
+                logger.error("Opening a socket for the milight discovery service failed. " + e.getLocalizedMessage());
+                return;
+            }
+            receiveThread.start();
+        }
+    }
+
+    public MilightBridgeDiscovery() throws IllegalArgumentException, UnknownHostException {
+        super(MilightBindingConstants.BRIDGE_THING_TYPES_UIDS, 2, true);
+        byte[] addr = { (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff };
+        broadcast = InetAddress.getByAddress(addr);
+        startDiscoveryService();
+    }
+
+    @Override
+    protected void startBackgroundDiscovery() {
+        if (backgroundFuture != null) {
+            return;
+        }
+
+        startDiscoveryService();
+
+        backgroundFuture = scheduler.scheduleAtFixedRate(new DetectTask(), 50, 60000 * 30, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    protected void stopBackgroundDiscovery() {
+        stopScan();
+        if (backgroundFuture != null) {
+            backgroundFuture.cancel(false);
+            backgroundFuture = null;
+        }
+        if (receiveThread != null) {
+            receiveThread.stopReceiving();
+        }
+        receiveThread = null;
+    }
+
+    @Override
+    public void bridgeDetected(InetAddress addr, String id) {
+        logger.debug("Milight bridge found " + addr.getHostName() + " " + id);
+        ThingUID thingUID = new ThingUID(MilightBindingConstants.BINDING_ID, "bridge", id);
+        String label = "Milight Bridge " + id;
+        Map<String, Object> properties = new TreeMap<>();
+        properties.put((String) MilightBindingConstants.CONFIG_ID, id);
+        properties.put((String) MilightBindingConstants.CONFIG_HOST_NAME, addr.getHostAddress());
+        DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(thingUID).withLabel(label)
+                .withProperties(properties).build();
+        thingDiscovered(discoveryResult);
+    }
+
+    @Override
+    protected void startScan() {
+        startDiscoveryService();
+        receiveThread.sendDiscover(scheduler);
+    }
+
+    @Override
+    protected synchronized void stopScan() {
+        if (receiveThread != null) {
+            receiveThread.stopResend();
+        }
+        super.stopScan();
+    }
+
+    @Override
+    public void noBridgeDetected() {
+
+    }
+}
+
