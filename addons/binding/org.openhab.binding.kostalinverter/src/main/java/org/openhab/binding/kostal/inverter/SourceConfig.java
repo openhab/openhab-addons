c8fa53fef3976371b853cc75495189d4ea59db66
Christian Schneider <chris@die-schneider.net>
2016-02-20 17:23:54 +0100

    Fixing issues from review

---
+++ addons/binding/org.openhab.binding.kostalinverter/src/main/java/org/openhab/binding/kostal/inverter/SourceConfig.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.kostal.inverter;
+
+/**
+ * @author Christian Schneider
+ */
+public class SourceConfig {
+    public String url;
+    public String userName;
+    public String password;
+    public int refreshInterval;
+}
--- addons/binding/org.openhab.binding.kostalinverter/src/main/java/org/openhab/binding/kostal/inverter/WebscrapeHandlerFactory.java
+++ addons/binding/org.openhab.binding.kostalinverter/src/main/java/org/openhab/binding/kostal/inverter/WebscrapeHandlerFactory.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.kostal.inverter;
 
 import org.eclipse.smarthome.core.thing.Thing;
@@ -5,6 +13,9 @@ import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
 
+/**
+ * @author Christian Schneider
+ */
 public class WebscrapeHandlerFactory extends BaseThingHandlerFactory {
     public final static ThingTypeUID KOSTAL_INVERTER = new ThingTypeUID("kostal", "kostal-inverter");
 
