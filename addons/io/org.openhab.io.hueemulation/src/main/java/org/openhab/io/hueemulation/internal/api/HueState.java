f20b867f794f4ea215c005c1350ff05d5846152f
Dan Cunningham <dan@digitaldan.com>
2016-02-23 09:48:13 -0800

    Initial Commit of the Hue Emulator
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

---
+++ addons/io/org.openhab.io.hueemulation/src/main/java/org/openhab/io/hueemulation/internal/api/HueState.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.io.hueemulation.internal.api;
+
+/**
+ * Hue API state object
+ *
+ * @author Dan Cunningham
+ *
+ */
+public class HueState {
+    public boolean on;
+    public short bri = -1;
+    public int hue = 0;
+    public short sat = 0;
+    public double[] xy = { 0, 0 };
+    public int ct = 500;
+    public String alert = "none";
+    public String effect = "none";
+    public String colormode = "ct";
+    public boolean reachable = true;
+
+    public HueState() {
+        super();
+    }
+
+    public HueState(boolean on, short bri) {
+        super();
+        this.on = on;
+        this.bri = bri;
+    }
+
+    @Override
+    public String toString() {
+        String xyString = "{";
+        for (double d : xy) {
+            xyString += d + " ";
+        }
+        xyString += "}";
+        return "[on: " + on + " bri: " + bri + " hue: " + hue + " sat: " + sat + " xy: " + xyString + " ct: " + ct
+                + " alert: " + alert + " effect: " + effect + " colormode: " + colormode + " reachable: " + reachable;
+    }
+}
