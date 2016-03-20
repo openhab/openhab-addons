fe95fba6b39720eb8afa035827594801df18189f
Jorg de Jong <jorg@dejong.info>
2016-03-20 17:22:55 +0100

    Initial contribution of the Toon Binding
    Signed-off-by: Jorg de Jong (github: jongj)
    
    Signed-off-by: Jorg de Jong <jorg@dejong.info> (github: jongj)

---
+++ addons/binding/org.openhab.binding.toon/src/main/java/org/openhab/binding/toon/internal/api/ToonState.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.toon.internal.api;
+
+/**
+ * The {@link ToonState} class defines the json object as received by the api.
+ *
+ * @author Jorg de Jong - Initial contribution
+ */
+public class ToonState {
+    private ThermostatInfo thermostatInfo;
+    private PowerUsage powerUsage;
+    private GasUsage gasUsage;
+    private Boolean success;
+    private DeviceStatusInfo deviceStatusInfo;
+    private DeviceConfigInfo deviceConfigInfo;
+
+    public ThermostatInfo getThermostatInfo() {
+        return thermostatInfo;
+    }
+
+    public void setThermostatInfo(ThermostatInfo thermostatInfo) {
+        this.thermostatInfo = thermostatInfo;
+    }
+
+    public PowerUsage getPowerUsage() {
+        return powerUsage;
+    }
+
+    public void setPowerUsage(PowerUsage powerUsage) {
+        this.powerUsage = powerUsage;
+    }
+
+    public GasUsage getGasUsage() {
+        return gasUsage;
+    }
+
+    public void setGasUsage(GasUsage gasUsage) {
+        this.gasUsage = gasUsage;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public DeviceStatusInfo getDeviceStatusInfo() {
+        return deviceStatusInfo;
+    }
+
+    public void setDeviceStatusInfo(DeviceStatusInfo deviceStatusInfo) {
+        this.deviceStatusInfo = deviceStatusInfo;
+    }
+
+    public DeviceConfigInfo getDeviceConfigInfo() {
+        return deviceConfigInfo;
+    }
+
+    public void setDeviceConfigInfo(DeviceConfigInfo deviceConfigInfo) {
+        this.deviceConfigInfo = deviceConfigInfo;
+    }
+
+}
