4f38391538960da099bbd7320f2fb38381b07816
Ben Woodford <me@benwoodford.co.uk>
2016-04-10 17:15:03 +0100

    Commit to poke TravisCI

--- addons/binding/org.openhab.binding.tesla/src/main/java/org/openhab/binding/tesla/internal/protocol/GUIState.java
+++ addons/binding/org.openhab.binding.tesla/src/main/java/org/openhab/binding/tesla/internal/protocol/GUIState.java
@@ -11,18 +11,18 @@ package org.openhab.binding.tesla.internal.protocol;
 /**
  * The {@link GUIState} is a datastructure to capture
  * variables sent by the Tesla Vehicle
- * 
+ *
  * @author Karel Goderis - Initial contribution
  */
 public class GUIState {
 
-	public String gui_distance_units;
-	public String gui_temperature_units;
-	public String gui_charge_rate_units;
-	public String gui_24_hour_time;
-	public String gui_range_display;
-	
-	public GUIState() {
-	}
-	
+    public String gui_distance_units;
+    public String gui_temperature_units;
+    public String gui_charge_rate_units;
+    public String gui_24_hour_time;
+    public String gui_range_display;
+
+    public GUIState() {
+    }
+
 }
