82701377a3d48461d825891ced5015912fc8af3c
Oliver Libutzki <oliver@libutzki.de>
2016-04-22 21:32:53 +0200

    #711 - Migration of Somfy URTSI II binding to openHAB 2 (#713)
    * Migration of Somfy URTSI II binding to openHAB 2
    
    PR: https://github.com/openhab/openhab2-addons/pull/713
    Signed-off-by: Oliver Libutzki <oliver@libutzki.de> (github: OLibutzki)

---
+++ addons/binding/org.openhab.binding.urtsi/src/main/java/org/openhab/binding/urtsi/UrtsiBindingConstants.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.urtsi;
+
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+/**
+ * The {@link UrtsiBinding} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Oliver Libutzki - Initial contribution
+ */
+public class UrtsiBindingConstants {
+
+    public static final String BINDING_ID = "urtsi";
+
+    // List of all Thing Type UIDs
+    /**
+     * URTSI II box
+     */
+    public final static ThingTypeUID URTSI_DEVICE_THING_TYPE = new ThingTypeUID(BINDING_ID, "urtsidevice");
+
+    /**
+     * RTS Device (e.g. rollershutter)
+     */
+    public final static ThingTypeUID RTS_DEVICE_THING_TYPE = new ThingTypeUID(BINDING_ID, "rtsdevice");
+
+    /**
+     * Rollershutter's position
+     */
+    public final static String POSITION = "position";
+
+}
---
+++ addons/binding/org.openhab.binding.urtsi/src/main/java/org/openhab/binding/urtsi/internal/UrtsiHandlerFactory.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2014 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.urtsi.internal;
+
+import static org.openhab.binding.urtsi.UrtsiBindingConstants.*;
+
+import java.util.List;
+
+import org.eclipse.smarthome.core.thing.Bridge;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.openhab.binding.urtsi.handler.RtsDeviceHandler;
+import org.openhab.binding.urtsi.handler.UrtsiDeviceHandler;
+
+import com.google.common.collect.Lists;
+
+/**
+ * The {@link UrtsiHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author Oliver Libutzki - Initial contribution
+ */
+public class UrtsiHandlerFactory extends BaseThingHandlerFactory {
+
+    private final static List<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Lists.newArrayList(URTSI_DEVICE_THING_TYPE,
+            RTS_DEVICE_THING_TYPE);
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
+
+    @Override
+    protected ThingHandler createHandler(Thing thing) {
+
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (thingTypeUID.equals(URTSI_DEVICE_THING_TYPE) && thing instanceof Bridge) {
+            return new UrtsiDeviceHandler((Bridge) thing);
+        } else if (thingTypeUID.equals(RTS_DEVICE_THING_TYPE)) {
+            return new RtsDeviceHandler(thing);
+        }
+
+        return null;
+    }
+}
