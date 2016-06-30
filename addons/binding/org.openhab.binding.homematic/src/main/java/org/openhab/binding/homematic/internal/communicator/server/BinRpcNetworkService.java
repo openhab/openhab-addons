8f909397591f8ae2a094071b37c33387514e809c
Gerhard R <gerrieg.openhab@icloud.com>
2016-06-30 21:04:45 +0200

    Homematic: Fixed scheduled pool rejection on reconnect (#1078)
    * Fixed scheduled pool rejection on reconnect
    
    Signed-off-by: Gerhard Riegler <gerhard.riegler@gmail.com>

--- addons/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/communicator/server/BinRpcNetworkService.java
+++ addons/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/communicator/server/BinRpcNetworkService.java
@@ -11,9 +11,9 @@ package org.openhab.binding.homematic.internal.communicator.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
 
 import org.eclipse.smarthome.core.common.ThreadPoolManager;
+import org.openhab.binding.homematic.internal.common.HomematicConfig;
 
 /**
  * Waits for a message from the Homematic gateway and starts the RpcCallbackHandler to handle the message.
@@ -21,21 +21,20 @@ import org.eclipse.smarthome.core.common.ThreadPoolManager;
  * @author Gerhard Riegler - Initial contribution
  */
 public class BinRpcNetworkService implements Runnable {
-    // FIXME: dependency to openHab
-    private final ExecutorService pool = ThreadPoolManager.getPool("homematicRpc");
+    private static final String RPC_POOL_NAME = "homematicRpc";
     private ServerSocket serverSocket;
     private boolean accept = true;
     private RpcEventListener listener;
-    private String encoding;
+    private HomematicConfig config;
 
     /**
      * Creates the socket for listening to events from the Homematic gateway.
      */
-    public BinRpcNetworkService(RpcEventListener listener, int port, String encoding) throws IOException {
+    public BinRpcNetworkService(RpcEventListener listener, HomematicConfig config) throws IOException {
         this.listener = listener;
-        this.encoding = encoding;
+        this.config = config;
 
-        serverSocket = new ServerSocket(port);
+        serverSocket = new ServerSocket(config.getCallbackPort());
         serverSocket.setReuseAddress(true);
     }
 
@@ -47,8 +46,8 @@ public class BinRpcNetworkService implements Runnable {
         while (accept) {
             try {
                 Socket cs = serverSocket.accept();
-                BinRpcCallbackHandler rpcHandler = new BinRpcCallbackHandler(cs, listener, encoding);
-                pool.execute(rpcHandler);
+                BinRpcCallbackHandler rpcHandler = new BinRpcCallbackHandler(cs, listener, config.getEncoding());
+                ThreadPoolManager.getPool(RPC_POOL_NAME).execute(rpcHandler);
             } catch (IOException ex) {
                 // ignore
             }
@@ -65,7 +64,6 @@ public class BinRpcNetworkService implements Runnable {
         } catch (IOException ioe) {
             // ignore
         }
-        pool.shutdownNow();
     }
 
 }
