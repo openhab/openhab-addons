f831c1579045ba31b27bfb9bbbdf89fc0ca1d0ec
janKG <github@janKG.com>
2016-07-03 23:24:16 +0200

    BigDecimal.ZERO instead of new BigDecimal("0")
    Signed-off-by: janKG <github@janKG.com>

--- addons/binding/org.openhab.binding.avmfritz/src/main/java/org/openhab/binding/avmfritz/internal/ahamodel/TemperatureModel.java
+++ addons/binding/org.openhab.binding.avmfritz/src/main/java/org/openhab/binding/avmfritz/internal/ahamodel/TemperatureModel.java
@@ -31,7 +31,7 @@ public class TemperatureModel {
 	private BigDecimal offset;
 
 	public BigDecimal getCelsius() {
-		return celsius != null ? celsius.multiply(TEMP_FACTOR) : new BigDecimal("0");
+		return celsius != null ? celsius.multiply(TEMP_FACTOR) : BigDecimal.ZERO;
 	}
 
 	public void setCelsius(BigDecimal celsius) {
@@ -39,7 +39,7 @@ public class TemperatureModel {
 	}
 
 	public BigDecimal getOffset() {
-		return offset != null ? offset.multiply(TEMP_FACTOR) : new BigDecimal("0");
+		return offset != null ? offset.multiply(TEMP_FACTOR) : BigDecimal.ZERO;
 	}
 
 	public void setOffset(BigDecimal offset) {
