21fa3af80d484dd08ee2952dd71f367b38cc35e7
hazzeh <hazzeh@users.noreply.github.com>
2016-07-09 14:39:52 +0200

    Add invert percentage option for blinds channel (#1102)
    Add option to invert the percentage value of blinds position.
    
    Signed-off-by: Hans Hazelius <hans@hazelius.se>

--- addons/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/converter/ZWaveMultiLevelSwitchConverter.java
+++ addons/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/converter/ZWaveMultiLevelSwitchConverter.java
@@ -72,13 +72,14 @@ public class ZWaveMultiLevelSwitchConverter extends ZWaveCommandClassConverter {
      */
     @Override
     public State handleEvent(ZWaveThingChannel channel, ZWaveCommandClassValueEvent event) {
-        boolean configInvert = "true".equalsIgnoreCase(channel.getArguments().get("config_invert"));
+        boolean configInvertControl = "true".equalsIgnoreCase(channel.getArguments().get("config_invert_control"));
+        boolean configInvertPercent = "true".equalsIgnoreCase(channel.getArguments().get("config_invert_percent"));
 
         int value = (int) event.getValue();
         State state;
         switch (channel.getDataType()) {
             case PercentType:
-                if (configInvert) {
+                if (configInvertPercent) {
                     state = new PercentType(100 - value);
                 } else {
                     state = new PercentType(value);
@@ -97,7 +98,7 @@ public class ZWaveMultiLevelSwitchConverter extends ZWaveCommandClassConverter {
                     state = OnOffType.ON;
                 }
 
-                if (configInvert) {
+                if (configInvertControl) {
                     if (state == OnOffType.ON) {
                         state = OnOffType.OFF;
                     } else {
@@ -128,13 +129,14 @@ public class ZWaveMultiLevelSwitchConverter extends ZWaveCommandClassConverter {
         SerialMessage serialMessage = null;
         // boolean restoreLastValue = "true".equalsIgnoreCase(channel.getArguments().get("restoreLastValue"));
 
-        boolean configInvert = "true".equalsIgnoreCase(channel.getArguments().get("config_invert"));
+        boolean configInvertControl = "true".equalsIgnoreCase(channel.getArguments().get("config_invert_control"));
+        boolean configInvertPercent = "true".equalsIgnoreCase(channel.getArguments().get("config_invert_percent"));
 
         if (command instanceof StopMoveType && command == StopMoveType.STOP) {
             // Special handling for the STOP command
             serialMessage = commandClass.stopLevelChangeMessage();
         } else if (command instanceof UpDownType) {
-            if (configInvert == false) {
+            if (configInvertControl == false) {
                 if (command == UpDownType.UP) {
                     serialMessage = commandClass.startLevelChangeMessage(true, 0xff);
                 } else {
@@ -149,7 +151,7 @@ public class ZWaveMultiLevelSwitchConverter extends ZWaveCommandClassConverter {
             }
         } else if (command instanceof PercentType) {
             int value;
-            if (configInvert) {
+            if (configInvertPercent) {
                 value = 100 - ((PercentType) command).intValue();
             } else {
                 value = ((PercentType) command).intValue();
@@ -165,7 +167,7 @@ public class ZWaveMultiLevelSwitchConverter extends ZWaveCommandClassConverter {
             serialMessage = commandClass.setValueMessage(value);
         } else if (command instanceof OnOffType) {
             int value;
-            if (configInvert) {
+            if (configInvertControl) {
                 value = command == OnOffType.ON ? 0 : 99;
             } else {
                 value = command == OnOffType.ON ? 99 : 0;
