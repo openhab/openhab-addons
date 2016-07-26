ac01b0e5b1ddc5a2e66f8a478e158beaaf342114
Andy Lintner <github@beowulfe.com>
2016-07-26 04:30:36 -0400

    Initial contribution of HD PowerView binding (#660)
    Adds binding for Hunter Douglas PowerView shades.
    
    Signed-off-by: Andy Lintner dev@beowulfe.com (github: beowulfe)

---
+++ addons/binding/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/config/HDPowerViewHubConfiguration.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.hdpowerview.config;
+
+/**
+ * Basic configuration for the HD Power View HUB
+ *
+ * @author Andy Lintner
+ */
+public class HDPowerViewHubConfiguration {
+
+    public static String HOST = "host";
+
+    public String host;
+
+    public long refresh;
+}
---
+++ addons/binding/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/config/HDPowerViewShadeConfiguration.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.hdpowerview.config;
+
+/**
+ * Basic configuration for an HD Power View Shade
+ *
+ * @author Andy Lintner
+ */
+public class HDPowerViewShadeConfiguration {
+
+    public static String ID = "id";
+
+    public int id;
+
+}
