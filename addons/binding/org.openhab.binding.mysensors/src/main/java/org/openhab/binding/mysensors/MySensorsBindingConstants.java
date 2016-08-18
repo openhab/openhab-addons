c6b9bd033ed843b9e6e653b272a46e24c2770e8a
gnalbandian <gonzalonalbandian@hotmail.com>
2016-08-18 00:02:04 -0300

    Update constants to comply with OH2
    Added following things:
    
    THING_TYPE_LOCK
    THING_TYPE_LEVEL
    THING_TYPE_RGB_LIGHT 
    THING_TYPE_RGBW_LIGHT
    THING_TYPE_RGBW_LIGHT
    THING_TYPE_PH_METER

--- addons/binding/org.openhab.binding.mysensors/src/main/java/org/openhab/binding/mysensors/MySensorsBindingConstants.java
+++ addons/binding/org.openhab.binding.mysensors/src/main/java/org/openhab/binding/mysensors/MySensorsBindingConstants.java
@@ -188,6 +188,13 @@ public class MySensorsBindingConstants {
     public final static ThingTypeUID THING_TYPE_CUSTOM = new ThingTypeUID(BINDING_ID, "customSensor");
 
     public final static ThingTypeUID THING_TYPE_HVAC = new ThingTypeUID(BINDING_ID, "hvacThermostat");
+	
+    public final static ThingTypeUID THING_TYPE_LOCK = new ThingTypeUID(BINDING_ID, "lock");
+    public final static ThingTypeUID THING_TYPE_LEVEL = new ThingTypeUID(BINDING_ID, "level");
+    public final static ThingTypeUID THING_TYPE_RGB_LIGHT = new ThingTypeUID(BINDING_ID, "rgbLight");
+    public final static ThingTypeUID THING_TYPE_RGBW_LIGHT = new ThingTypeUID(BINDING_ID, "rgbwLight");
+    public final static ThingTypeUID THING_TYPE_RGBW_LIGHT = new ThingTypeUID(BINDING_ID, "rgbwLight");
+    public final static ThingTypeUID THING_TYPE_PH_METER = new ThingTypeUID(BINDING_ID, "phMeter");
 
     public final static ThingTypeUID THING_TYPE_BRIDGE_SER = new ThingTypeUID(BINDING_ID, "bridge-ser");
     public final static ThingTypeUID THING_TYPE_BRIDGE_ETH = new ThingTypeUID(BINDING_ID, "bridge-eth");
@@ -229,6 +236,22 @@ public class MySensorsBindingConstants {
     public final static String CHANNEL_VAR5 = "var5";
     public final static String CHANNEL_FLOW = "flow";
     public final static String CHANNEL_VOLUME = "volume";
+	public final static String CHANNEL_LOCK_STATUS = "lock-status";
+	public final static String CHANNEL_LEVEL = "level";
+	public final static String CHANNEL_RGB = "rgb";
+	public final static String CHANNEL_RGBW = "rgbw";
+	public final static String CHANNEL_ID = "id";
+	public final static String CHANNEL_UNIT_PREFIX = "unit-prefix";
+	public final static String CHANNEL_TEXT = "text";
+	public final static String CHANNEL_CUSTOM = "custom";
+	public final static String CHANNEL_POSITION = "position";
+	public final static String CHANNEL_IR_RECORD = "ir-record";
+	public final static String CHANNEL_PH = "ph";
+	public final static String CHANNEL_ORP = "orp";
+	public final static String CHANNEL_EC = "ec";
+	public final static String CHANNEL_VAR = "var";
+	public final static String CHANNEL_VA = "va";
+	public final static String CHANNEL_POWER_FACTOR = "power-factor";
 
     public final static String CHANNEL_LAST_UPDATE = "lastupdate";
 
@@ -281,6 +304,22 @@ public class MySensorsBindingConstants {
             put(MYSENSORS_SUBTYPE_V_VAR5, CHANNEL_VAR5);
             put(MYSENSORS_SUBTYPE_V_FLOW, CHANNEL_FLOW);
             put(MYSENSORS_SUBTYPE_V_VOLUME, CHANNEL_VOLUME);
+			put(MYSENSORS_SUBTYPE_V_LOCK_STATUS, CHANNEL_LOCK_STATUS);
+			put(MYSENSORS_SUBTYPE_V_LEVEL, CHANNEL_LEVEL);
+			put(MYSENSORS_SUBTYPE_V_RGB, CHANNEL_RGB);
+			put(MYSENSORS_SUBTYPE_V_RGBW, CHANNEL_RGBW);
+			put(MYSENSORS_SUBTYPE_V_ID, CHANNEL_ID);
+			put(MYSENSORS_SUBTYPE_V_UNIT_PREFIX, CHANNEL_UNIT_PREFIX);
+			put(MYSENSORS_SUBTYPE_V_TEXT, CHANNEL_TEXT);
+			put(MYSENSORS_SUBTYPE_V_CUSTOM, CHANNEL_CUSTOM);
+			put(MYSENSORS_SUBTYPE_V_POSITION, CHANNEL_POSITION);
+			put(MYSENSORS_SUBTYPE_V_IR_RECORD, CHANNEL_IR_RECORD);
+			put(MYSENSORS_SUBTYPE_V_PH, CHANNEL_PH);
+			put(MYSENSORS_SUBTYPE_V_ORP, CHANNEL_ORP);
+			put(MYSENSORS_SUBTYPE_V_EC, CHANNEL_EC);
+			put(MYSENSORS_SUBTYPE_V_VAR, CHANNEL_VAR);
+			put(MYSENSORS_SUBTYPE_V_VA, CHANNEL_VA);
+			put(MYSENSORS_SUBTYPE_V_POWER_FACTOR, CHANNEL_POWER_FACTOR);
 
         }
     };
@@ -302,7 +341,8 @@ public class MySensorsBindingConstants {
             THING_TYPE_TEMPERATURE, THING_TYPE_LIGHT, THING_TYPE_MULTIMETER, THING_TYPE_POWER, THING_TYPE_BARO,
             THING_TYPE_DOOR, THING_TYPE_MOTION, THING_TYPE_SMOKE, THING_TYPE_DIMMER, THING_TYPE_COVER, THING_TYPE_WIND,
             THING_TYPE_RAIN, THING_TYPE_UV, THING_TYPE_WEIGHT, THING_TYPE_DISTANCE, THING_TYPE_LIGHT_LEVEL,
-            THING_TYPE_HVAC, THING_TYPE_WATER, THING_TYPE_CUSTOM);
+            THING_TYPE_HVAC, THING_TYPE_WATER, THING_TYPE_CUSTOM, THING_TYPE_LOCK, THING_TYPE_LEVEL, THING_TYPE_RGB_LIGHT,
+			THING_TYPE_RGBW_LIGHT, THING_TYPE_RGBW_LIGHT, THING_TYPE_PH_METER);
     /** Supported bridges */
     public final static Set<ThingTypeUID> SUPPORTED_BRIDGE_THING_TYPES_UIDS = ImmutableSet.of(THING_TYPE_BRIDGE_SER,
             THING_TYPE_BRIDGE_ETH);
@@ -312,5 +352,6 @@ public class MySensorsBindingConstants {
             THING_TYPE_TEMPERATURE, THING_TYPE_LIGHT, THING_TYPE_MULTIMETER, THING_TYPE_POWER, THING_TYPE_BARO,
             THING_TYPE_DOOR, THING_TYPE_MOTION, THING_TYPE_SMOKE, THING_TYPE_DIMMER, THING_TYPE_COVER, THING_TYPE_WIND,
             THING_TYPE_RAIN, THING_TYPE_UV, THING_TYPE_WEIGHT, THING_TYPE_DISTANCE, THING_TYPE_LIGHT_LEVEL,
-            THING_TYPE_HVAC, THING_TYPE_WATER, THING_TYPE_CUSTOM, THING_TYPE_BRIDGE_SER, THING_TYPE_BRIDGE_ETH);
+            THING_TYPE_HVAC, THING_TYPE_WATER, THING_TYPE_CUSTOM, THING_TYPE_LOCK, THING_TYPE_LEVEL, THING_TYPE_RGB_LIGHT,
+			THING_TYPE_RGBW_LIGHT, THING_TYPE_RGBW_LIGHT, THING_TYPE_PH_METER THING_TYPE_BRIDGE_SER, THING_TYPE_BRIDGE_ETH);
 }
