98c40adfd428fee645d52de9a79d86623e7b788f
jongj <jongj@users.noreply.github.com>
2016-09-14 09:22:14 +0200

    Initial contribution of the Toon Binding (#777)
    * Initial contribution of the Toon Binding
    
    Signed-off-by: Jorg de Jong <jorg@dejong.info> (github: jongj)

---
+++ addons/binding/org.openhab.binding.toon/src/main/java/org/openhab/binding/toon/internal/api/GasUsage.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.toon.internal.api;
+
+/**
+ * The {@link GasUsage} class defines the json object as received by the api.
+ *
+ * @author Jorg de Jong - Initial contribution
+ */
+public class GasUsage {
+    private Long meterReading;
+
+    public Long getMeterReading() {
+        return meterReading;
+    }
+
+    public void setMeterReading(Long meterReading) {
+        this.meterReading = meterReading;
+    }
+}
