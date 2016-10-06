Bakkashan/openhab2

Initial contribution of the Miele binding that supports the Miele@Home appliances. It is based on the JSON-RPC protocol exposed by the Miele XGW3000 ZigBee gateway.
Signed-off-by: Karel Goderis <karel.goderis@me.com>

addons/binding/org.openhab.binding.miele/src/main/java/org/openhab/binding/miele/handler/FridgeChannelSelector.java
@@ -1,0 +1,153 @@
+/**
+ * Copyright (c) 2014 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.miele.handler;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map.Entry;
+
+import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.library.types.OpenClosedType;
+import org.eclipse.smarthome.core.library.types.StringType;
+import org.eclipse.smarthome.core.types.State;
+import org.eclipse.smarthome.core.types.Type;
+import org.eclipse.smarthome.core.types.UnDefType;
+import org.openhab.binding.miele.handler.MieleBridgeHandler.DeviceMetaData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonElement;
+
+/**
+ * The {@link ApplianceChannelSelector} for fridges
+ *
+ * @author Karel Goderis - Initial contribution
+ */
+public enum FridgeChannelSelector implements ApplianceChannelSelector {
+
+    PRODUCT_TYPE("productTypeId", "productType", StringType.class, true),
+    DEVICE_TYPE("mieleDeviceType", "deviceType", StringType.class, true),
+    BRAND_ID("brandId", "brandId", StringType.class, true),
+    COMPANY_ID("companyId", "companyId", StringType.class, true),
+    STATE("state", "state", StringType.class, false),
+    SUPERCOOL(null, "supercool", OnOffType.class, false),
+    FRIDGECURRENTTEMP("currentTemperature", "current", DecimalType.class, false) {
+        @Override
+        public State getState(String s, DeviceMetaData dmd) {
+            return getState(s);
+        }
+    },
+    FRIDGETARGETTEMP("targetTemperature", "target", DecimalType.class, false) {
+        @Override
+        public State getState(String s, DeviceMetaData dmd) {
+            return getState(s);
+        }
+    },
+    DOOR("signalDoor", "door", OpenClosedType.class, false) {
+        @Override
+
+        public State getState(String s, DeviceMetaData dmd) {
+            if (s.equals("true")) {
+                return getState("OPEN");
+            }
+
+            if (s.equals("false")) {
+                return getState("CLOSED");
+            }
+
+            return UnDefType.UNDEF;
+        }
+    },
+    START(null, "start", OnOffType.class, false);
+
+    protected Logger logger = LoggerFactory.getLogger(FridgeChannelSelector.class);
+
+    private final String mieleID;
+    private final String channelID;
+    private final Class<? extends Type> typeClass;
+    private final boolean isProperty;
+
+    private FridgeChannelSelector(String propertyID, String channelID, Class<? extends Type> typeClass,
+            boolean isProperty) {
+        this.mieleID = propertyID;
+        this.channelID = channelID;
+        this.typeClass = typeClass;
+        this.isProperty = isProperty;
+    }
+
+    @Override
+    public String toString() {
+        return mieleID;
+    }
+
+    @Override
+    public String getMieleID() {
+        return mieleID;
+    }
+
+    @Override
+    public String getChannelID() {
+        return channelID;
+    }
+
+    @Override
+    public Class<? extends Type> getTypeClass() {
+        return typeClass;
+    }
+
+    @Override
+    public boolean isProperty() {
+        return isProperty;
+    }
+
+    @Override
+    public State getState(String s, DeviceMetaData dmd) {
+        if (dmd != null) {
+            String localizedValue = getMieleEnum(s, dmd);
+            if (localizedValue == null) {
+                localizedValue = dmd.LocalizedValue;
+            }
+            if (localizedValue == null) {
+                localizedValue = s;
+            }
+
+            return getState(localizedValue);
+        } else {
+            return getState(s);
+        }
+    }
+
+    public State getState(String s) {
+        try {
+            Method valueOf = typeClass.getMethod("valueOf", String.class);
+            State state = (State) valueOf.invoke(typeClass, s);
+            if (state != null) {
+                return state;
+            }
+        } catch (NoSuchMethodException e) {
+        } catch (IllegalArgumentException e) {
+        } catch (IllegalAccessException e) {
+        } catch (InvocationTargetException e) {
+        }
+
+        return null;
+    }
+
+    public String getMieleEnum(String s, DeviceMetaData dmd) {
+        if (dmd.MieleEnum != null) {
+            for (Entry<String, JsonElement> enumEntry : dmd.MieleEnum.entrySet()) {
+                if (enumEntry.getValue().getAsString().trim().equals(s.trim())) {
+                    return enumEntry.getKey();
+                }
+            }
+        }
+
+        return null;
+    }
+}

openhab/openhab-addons

addons/binding/org.openhab.binding.miele/src/main/java/org/openhab/binding/miele/handler/FridgeChannelSelector.java
@@ -1,0 +1,150 @@
+/**
+ * Copyright (c) 2014 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.miele.handler;
+
+import java.lang.reflect.Method;
+import java.util.Map.Entry;
+
+import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.library.types.OpenClosedType;
+import org.eclipse.smarthome.core.library.types.StringType;
+import org.eclipse.smarthome.core.types.State;
+import org.eclipse.smarthome.core.types.Type;
+import org.eclipse.smarthome.core.types.UnDefType;
+import org.openhab.binding.miele.handler.MieleBridgeHandler.DeviceMetaData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonElement;
+
+/**
+ * The {@link ApplianceChannelSelector} for fridges
+ *
+ * @author Karel Goderis - Initial contribution
+ */
+public enum FridgeChannelSelector implements ApplianceChannelSelector {
+
+    PRODUCT_TYPE("productTypeId", "productType", StringType.class, true),
+    DEVICE_TYPE("mieleDeviceType", "deviceType", StringType.class, true),
+    BRAND_ID("brandId", "brandId", StringType.class, true),
+    COMPANY_ID("companyId", "companyId", StringType.class, true),
+    STATE("state", "state", StringType.class, false),
+    SUPERCOOL(null, "supercool", OnOffType.class, false),
+    FRIDGECURRENTTEMP("currentTemperature", "current", DecimalType.class, false) {
+        @Override
+        public State getState(String s, DeviceMetaData dmd) {
+            return getState(s);
+        }
+    },
+    FRIDGETARGETTEMP("targetTemperature", "target", DecimalType.class, false) {
+        @Override
+        public State getState(String s, DeviceMetaData dmd) {
+            return getState(s);
+        }
+    },
+    DOOR("signalDoor", "door", OpenClosedType.class, false) {
+        @Override
+
+        public State getState(String s, DeviceMetaData dmd) {
+            if (s.equals("true")) {
+                return getState("OPEN");
+            }
+
+            if (s.equals("false")) {
+                return getState("CLOSED");
+            }
+
+            return UnDefType.UNDEF;
+        }
+    },
+    START(null, "start", OnOffType.class, false);
+
+    protected Logger logger = LoggerFactory.getLogger(FridgeChannelSelector.class);
+
+    private final String mieleID;
+    private final String channelID;
+    private final Class<? extends Type> typeClass;
+    private final boolean isProperty;
+
+    private FridgeChannelSelector(String propertyID, String channelID, Class<? extends Type> typeClass,
+            boolean isProperty) {
+        this.mieleID = propertyID;
+        this.channelID = channelID;
+        this.typeClass = typeClass;
+        this.isProperty = isProperty;
+    }
+
+    @Override
+    public String toString() {
+        return mieleID;
+    }
+
+    @Override
+    public String getMieleID() {
+        return mieleID;
+    }
+
+    @Override
+    public String getChannelID() {
+        return channelID;
+    }
+
+    @Override
+    public Class<? extends Type> getTypeClass() {
+        return typeClass;
+    }
+
+    @Override
+    public boolean isProperty() {
+        return isProperty;
+    }
+
+    @Override
+    public State getState(String s, DeviceMetaData dmd) {
+        if (dmd != null) {
+            String localizedValue = getMieleEnum(s, dmd);
+            if (localizedValue == null) {
+                localizedValue = dmd.LocalizedValue;
+            }
+            if (localizedValue == null) {
+                localizedValue = s;
+            }
+
+            return getState(localizedValue);
+        } else {
+            return getState(s);
+        }
+    }
+
+    public State getState(String s) {
+        try {
+            Method valueOf = typeClass.getMethod("valueOf", String.class);
+            State state = (State) valueOf.invoke(typeClass, s);
+            if (state != null) {
+                return state;
+            }
+        } catch (Exception e) {
+            logger.error("An exception occurred while converting '{}' into a State", s);
+        }
+
+        return null;
+    }
+
+    public String getMieleEnum(String s, DeviceMetaData dmd) {
+        if (dmd.MieleEnum != null) {
+            for (Entry<String, JsonElement> enumEntry : dmd.MieleEnum.entrySet()) {
+                if (enumEntry.getValue().getAsString().trim().equals(s.trim())) {
+                    return enumEntry.getKey();
+                }
+            }
+        }
+
+        return null;
+    }
+}