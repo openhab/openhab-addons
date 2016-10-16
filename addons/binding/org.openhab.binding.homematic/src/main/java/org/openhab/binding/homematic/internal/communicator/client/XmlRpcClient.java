d9f07d95890d269d725f2decd5bdafa4254b8401
Martin Herbst <mail@mherbst.de>
2016-10-16 20:52:27 +0200

    Make sure that the correct encoding is used here

--- addons/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/communicator/client/XmlRpcClient.java
+++ addons/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/communicator/client/XmlRpcClient.java
@@ -92,8 +92,8 @@ public class XmlRpcClient extends RpcClient {
                 logger.trace("Client XmlRpcResponse (port {}):\n{}", port, result);
             }
 
-            Object[] data = new XmlRpcResponse(new ByteArrayInputStream(result.getBytes()), config.getEncoding())
-                    .getResponseData();
+            Object[] data = new XmlRpcResponse(new ByteArrayInputStream(result.getBytes(config.getEncoding())),
+                    config.getEncoding()).getResponseData();
             return new RpcResponseParser(request).parse(data);
         } catch (UnknownRpcFailureException ex) {
             throw ex;
