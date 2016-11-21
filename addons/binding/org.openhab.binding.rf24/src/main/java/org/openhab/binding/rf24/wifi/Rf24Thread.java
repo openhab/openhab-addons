e1a54eb8ef5064f6d6a9c62199b49c39c364d6f5
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-11-21 19:20:54 +0100

    Using thread pool

--- addons/binding/org.openhab.binding.rf24/src/main/java/org/openhab/binding/rf24/wifi/Rf24Thread.java
+++ addons/binding/org.openhab.binding.rf24/src/main/java/org/openhab/binding/rf24/wifi/Rf24Thread.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
@@ -17,12 +16,9 @@ import pl.grzeslowski.smarthome.common.io.id.HardwareId;
 import pl.grzeslowski.smarthome.common.io.id.IdUtils;
 import pl.grzeslowski.smarthome.common.io.id.TransmitterId;
 import pl.grzeslowski.smarthome.proto.sensor.Sensor.SensorResponse;
-import pl.grzeslowski.smarthome.rf24.Rf24Adapter;
 import pl.grzeslowski.smarthome.rf24.helpers.Pipe;
 
-public class Rf24Thread extends Thread implements AutoCloseable {
-    private static final long SLEEP_TIME = TimeUnit.SECONDS.toMillis(5);
-    private static final IdUtils ID_UTILS = new IdUtils(Rf24Adapter.MAX_NUMBER_OF_READING_PIPES);
+public class Rf24Thread implements AutoCloseable, Runnable {
     private Logger logger = LoggerFactory.getLogger(Rf24Thread.class);
 
     private final AtomicBoolean exit = new AtomicBoolean(false);
@@ -30,20 +26,18 @@ public class Rf24Thread extends Thread implements AutoCloseable {
     private final Collection<Pipe> pipes;
     private final Set<OnMessage> notify = new HashSet<>();
 
-    public Rf24Thread(WiFi wifi, TransmitterId transmitterId) {
-        super(Rf24Thread.class.getSimpleName());
+    public Rf24Thread(IdUtils idUtils, WiFi wifi, TransmitterId transmitterId) {
         this.wifi = Preconditions.checkNotNull(wifi);
         Preconditions.checkNotNull(transmitterId);
         // @formatter:off
-        this.pipes = ID_UTILS.findReceiversForTransmitter(transmitterId)
+        this.pipes = idUtils.findReceiversForTransmitter(transmitterId)
                 .stream()
-                .map(ID_UTILS::toCommonId)
+                .map(idUtils::toCommonId)
                 .map(HardwareId::fromCommonId)
                 .map(HardwareId::getId)
                 .map(id -> new Pipe(id))
                 .collect(Collectors.toList());
         // @formatter:on
-        setDaemon(true); // this thread should be close automatically on the end
     }
 
     @Override
@@ -54,19 +48,18 @@ public class Rf24Thread extends Thread implements AutoCloseable {
         // if (!wifi.isInit()) return; // this is not the best cause thread will end
         // or
         // while(!wifi.isInit()) sleep(xxx);
-        while (!exit.get()) {
-            Optional<SensorResponse> read = wifi.read(pipes);
-            if (read.isPresent()) {
-                SensorResponse response = read.get();
 
-                synchronized (notify) {
-                    notify.stream().forEach(message -> message.onMessage(response));
-                }
-            } else {
+        // if closed do not work anymore
+        if (exit.get()) {
+            return;
+        }
 
-                // sleep only when nothing in pipe is presented!
-                logger.debug("Sleeping ({} ms), because there was nothing in the pipes...", SLEEP_TIME);
-                sleep();
+        Optional<SensorResponse> read = wifi.read(pipes);
+        if (read.isPresent()) {
+            SensorResponse response = read.get();
+
+            synchronized (notify) {
+                notify.stream().forEach(message -> message.onMessage(response));
             }
         }
     }
@@ -87,14 +80,6 @@ public class Rf24Thread extends Thread implements AutoCloseable {
         Preconditions.checkNotNull(message);
         synchronized (notify) {
             notify.remove(Preconditions.checkNotNull(message));
-        }
-    }
-
-    private void sleep() {
-        try {
-            Thread.sleep(SLEEP_TIME);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
         }
     }
 
--- addons/binding/org.openhab.binding.rf24/src/main/java/org/openhab/binding/rf24/wifi/X.java
+++ addons/binding/org.openhab.binding.rf24/src/main/java/org/openhab/binding/rf24/wifi/X.java
@@ -1,23 +1,32 @@
 package org.openhab.binding.rf24.wifi;
 
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import com.google.common.base.Preconditions;
 
+import pl.grzeslowski.smarthome.common.io.id.IdUtils;
 import pl.grzeslowski.smarthome.common.io.id.TransmitterId;
 
 public class X implements AutoCloseable {
+    private static final long INITIAL_DELAY = TimeUnit.SECONDS.toMillis(5);
+    private static final long DELAY = TimeUnit.SECONDS.toMillis(5);
+
     private final WiFi wifi;
     private final TransmitterId transmitterId;
     private final Rf24Thread rf24Thread;
+    private ScheduledExecutorService executor;
 
-    public X(WiFi wifi, TransmitterId transmitterId) {
+    public X(IdUtils idUtils, WiFi wifi, TransmitterId transmitterId, ScheduledExecutorService executor) {
         this.wifi = Preconditions.checkNotNull(wifi);
         this.transmitterId = Preconditions.checkNotNull(transmitterId);
-        this.rf24Thread = new Rf24Thread(wifi, transmitterId);
+        this.executor = Preconditions.checkNotNull(executor);
+        this.rf24Thread = new Rf24Thread(idUtils, wifi, transmitterId);
     }
 
     public void init() {
         wifi.init();
-        rf24Thread.start();
+        executor.scheduleWithFixedDelay(rf24Thread, INITIAL_DELAY, DELAY, TimeUnit.MILLISECONDS);
     }
 
     @Override
