0a928375ff988264e084d34adeb4e632282943be
borisk <boris.krivonog@inova.si>
2016-12-05 15:37:11 +0100

    Updated docs.

--- addons/binding/org.openhab.binding.regoheatpump/src/main/java/org/openhab/binding/regoheatpump/internal/utils/QueueHashSet.java
+++ addons/binding/org.openhab.binding.regoheatpump/src/main/java/org/openhab/binding/regoheatpump/internal/utils/QueueHashSet.java
@@ -10,16 +10,31 @@ public class QueueHashSet<T> {
     private final HashSet<T> set = new HashSet<T>();
     private final LinkedList<T> list = new LinkedList<T>();
 
-    public synchronized void push(T value) {
+    /**
+     * Adds the specified element to this set if it is not already present.
+     *
+     * @param value element to be added to the end of the list
+     */
+    public synchronized void add(T value) {
         if (set.add(value)) {
             list.add(value);
         }
     }
 
+    /**
+     * Retrieves, but does not remove, the head (first element) of this set.
+     *
+     * @return the head of this list, or null if this list is empty
+     */
     public synchronized T peek() {
         return list.peek();
     }
 
+    /**
+     * Retrieves and removes the head (first element) of this list.
+     *
+     * @return the head of this list, or null if this list is empty
+     */
     public synchronized T poll() {
         final T value = list.poll();
         if (value != null) {
