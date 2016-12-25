c2a7e946312ba8b2a771192294fa7aaf6af708e7
Martin van Wingerden <martinvw@users.noreply.github.com>
2016-12-25 19:13:12 +0100

    Ported the support for IT message on Lighting5 (#1595)
    Signed-off-by: Martin van Wingerden <martinvw@mtin.nl>

--- addons/binding/org.openhab.binding.rfxcom.test/src/test/java/org/openhab/binding/rfxcom/internal/messages/RFXComLighting5MessageTest.java
+++ addons/binding/org.openhab.binding.rfxcom.test/src/test/java/org/openhab/binding/rfxcom/internal/messages/RFXComLighting5MessageTest.java
@@ -8,10 +8,18 @@
  */
 package org.openhab.binding.rfxcom.internal.messages;
 
+import org.eclipse.smarthome.core.library.types.OnOffType;
 import org.junit.Test;
+import org.openhab.binding.rfxcom.RFXComValueSelector;
 import org.openhab.binding.rfxcom.internal.exceptions.RFXComException;
 import org.openhab.binding.rfxcom.internal.exceptions.RFXComNotImpException;
-import org.openhab.binding.rfxcom.internal.messages.RFXComBaseMessage.PacketType;
+
+import javax.xml.bind.DatatypeConverter;
+
+import static org.junit.Assert.assertEquals;
+import static org.openhab.binding.rfxcom.internal.messages.RFXComBaseMessage.PacketType.LIGHTING5;
+import static org.openhab.binding.rfxcom.internal.messages.RFXComLighting5Message.Commands.ON;
+import static org.openhab.binding.rfxcom.internal.messages.RFXComLighting5Message.SubType.IT;
 
 /**
  * Test for RFXCom-binding
@@ -21,14 +29,24 @@ import org.openhab.binding.rfxcom.internal.messages.RFXComBaseMessage.PacketType
  */
 public class RFXComLighting5MessageTest {
     @Test
-    public void checkForSupportTest() throws RFXComException, RFXComNotImpException {
-        RFXComMessageFactory.createMessage(PacketType.LIGHTING5);
+    public void basicBoundaryCheck() throws RFXComException, RFXComNotImpException {
+        RFXComTestHelper.basicBoundaryCheck(LIGHTING5);
     }
 
     @Test
-    public void basicBoundaryCheck() throws RFXComException, RFXComNotImpException {
-        RFXComTestHelper.basicBoundaryCheck(PacketType.LIGHTING5);
+    public void convertFromStateItMessage() throws RFXComException, RFXComNotImpException {
+        RFXComMessage itMessageObject = RFXComMessageFactory.createMessage(LIGHTING5);
+        itMessageObject.setDeviceId("2061.1");
+        itMessageObject.setSubType(IT);
+        itMessageObject.convertFromState(RFXComValueSelector.COMMAND, OnOffType.ON);
+        byte[] message = itMessageObject.decodeMessage();
+        String hexMessage = DatatypeConverter.printHexBinary(message);
+        assertEquals("Message is not as expected", "0A140F0000080D01010000", hexMessage);
+        RFXComLighting5Message msg = (RFXComLighting5Message) RFXComMessageFactory.createMessage(message);
+        assertEquals("SubType", IT, msg.subType);
+        assertEquals("Sensor Id", "2061.1", msg.getDeviceId());
+        assertEquals("Command", ON, msg.command);
     }
 
-    // TODO please add tests for real messages
+    // TODO please add more tests for different messages
 }
