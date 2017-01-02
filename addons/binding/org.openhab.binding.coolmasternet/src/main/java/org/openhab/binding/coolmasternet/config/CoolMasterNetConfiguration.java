d275fc73c9b11a71ce4978a9deb7025673325d52
Angus Gratton <gus@projectgus.com>
2017-01-02 22:00:54 +1100

    Binding for CoolAutomation CoolMasterNet HVAC controller (#1122)
    * Initial coolmasternet driver
    
    Signed-off-by: Angus Gratton <gus@projectgus.com> (github: projectgus)

---
+++ addons/binding/org.openhab.binding.coolmasternet/src/main/java/org/openhab/binding/coolmasternet/config/CoolMasterNetConfiguration.java
@@ -0,0 +1,15 @@
+package org.openhab.binding.coolmasternet.config;
+
+/**
+ * The {@link CoolMasterNetConfiguration} is responsible for holding
+ * configuration information needed to access/poll the coolmasternet unit
+ *
+ * @author Angus Gratton
+ */
+
+public class CoolMasterNetConfiguration {
+    public static final String HOST = "host";
+    public static final String PORT = "port";
+    public static final String UID = "uid";
+    public static final String REFRESH = "refresh";
+}
