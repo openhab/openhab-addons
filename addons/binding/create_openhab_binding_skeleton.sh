5d2b6dc6f73854867f3203d0381837e5d68b4b3c
frol2103 <github@frol.be>
2017-01-09 09:00:30 +0100

    Refactor binding skeleton script to make it more human readable (#1674)
    Signed-off-by: François Pelsser <github@frol.be>

--- addons/binding/create_openhab_binding_skeleton.sh
+++ addons/binding/create_openhab_binding_skeleton.sh
@@ -5,4 +5,16 @@ camelcaseId=$1
 
 id=`echo $camelcaseId | tr '[:upper:]' '[:lower:]'` 
 
-mvn archetype:generate -N -DarchetypeGroupId=org.eclipse.smarthome.archetype -DarchetypeArtifactId=org.eclipse.smarthome.archetype.binding -DarchetypeVersion=0.9.0-SNAPSHOT -DgroupId=org.openhab.binding -DartifactId=org.openhab.binding.$id -Dpackage=org.openhab.binding.$id -DarchetypeCatalog='file://../archetype-catalog.xml' -Dversion=2.0.0-SNAPSHOT -DbindingId=$id -DbindingIdCamelCase=$camelcaseId -DvendorName=openHAB -Dnamespace=org.openhab
+mvn archetype:generate -N \
+  -DarchetypeGroupId=org.eclipse.smarthome.archetype \
+  -DarchetypeArtifactId=org.eclipse.smarthome.archetype.binding \
+  -DarchetypeVersion=0.9.0-SNAPSHOT \
+  -DgroupId=org.openhab.binding \
+  -DartifactId=org.openhab.binding.$id \
+  -Dpackage=org.openhab.binding.$id \
+  -DarchetypeCatalog='file://../archetype-catalog.xml' \
+  -Dversion=2.0.0-SNAPSHOT \
+  -DbindingId=$id \
+  -DbindingIdCamelCase=$camelcaseId \
+  -DvendorName=openHAB \
+  -Dnamespace=org.openhab
