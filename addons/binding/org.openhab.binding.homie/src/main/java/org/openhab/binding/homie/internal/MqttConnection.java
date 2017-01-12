a34d1b0fe3557360c61152896eb391f2642b3309
Michael Kolb <dev@db1smk.com>
2017-01-12 16:30:33 +0100

    Added reconnect capability and settings to the thing

--- addons/binding/org.openhab.binding.homie/src/main/java/org/openhab/binding/homie/internal/MqttConnection.java
+++ addons/binding/org.openhab.binding.homie/src/main/java/org/openhab/binding/homie/internal/MqttConnection.java
@@ -2,10 +2,13 @@ package org.openhab.binding.homie.internal;
 
 import static org.openhab.binding.homie.HomieBindingConstants.*;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClientPersistence;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.slf4j.Logger;
@@ -23,6 +26,31 @@ public class MqttConnection {
         logger.debug("Homie MQTT Connection start");
         client = new MqttClient(BROKER_URL, MQTT_CLIENTID, new MemoryPersistence());
         client.connect();
+        client.setCallback(new MqttCallback() {
+
+            @Override
+            public void messageArrived(String arg0, MqttMessage arg1) throws Exception {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void connectionLost(Throwable arg0) {
+                logger.error("MQTT Connection lost", arg0);
+                try {
+                    client.connect();
+                } catch (MqttException e) {
+                    logger.error("MQTT Reconnect failed", e);
+                }
+
+            }
+        });
         logger.debug("Homie MQTT Connection connected");
     }
 
