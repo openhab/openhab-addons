1f9637d800f72f4ca5e609ac9519e52a51d4ecf9
Sebastien Marchand <sebastien.marchand.polymtl@gmail.com>
2017-01-13 21:35:24 -0500

    Add missing file.

---
+++ addons/binding/org.openhab.binding.wink/src/main/java/org/openhab/binding/wink/handler/LightBulbHandler.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.wink.handler;
+
+import static org.openhab.binding.wink.WinkBindingConstants.*;
+
+import org.openhab.binding.wink.handler.WinkHandler;
+import org.eclipse.smarthome.core.thing.ChannelUID;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingStatus;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
+import org.eclipse.smarthome.core.types.Command;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO: The {@link LightBulbHandler} is responsible for handling commands, which are
+ * sent to one of the channels.
+ * 
+ * @author Sebastien Marchand - Initial contribution
+ */
+public class LightBulbHandler extends WinkHandler {
+
+  private Logger logger = LoggerFactory.getLogger(LightBulbHandler.class);
+
+	public LightBulbHandler(Thing thing) {
+    logger.info("Here's a new light bulb handler!");
+		super(thing);
+	}
+
+	@Override
+	public void handleCommand(ChannelUID channelUID, Command command) {
+        //if(channelUID.getId().equals(CHANNEL_1)) {
+            // TODO: handle command
+
+            // Note: if communication with thing fails for some reason,
+            // indicate that by setting the status with detail information
+            // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
+            // "Could not control device at IP address x.x.x.x");
+        //}
+        logger.info("Received a command!");
+	}
+
+    @Override
+    public void initialize() {
+        // TODO: Initialize the thing. If done set status to ONLINE to indicate proper working.
+        // Long running initialization should be done asynchronously in background.
+        updateStatus(ThingStatus.ONLINE);
+
+        // Note: When initialization can NOT be done set the status with more details for further
+        // analysis. See also class ThingStatusDetail for all available status details.
+        // Add a description to give user information to understand why thing does not work
+        // as expected. E.g.
+        // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
+        // "Can not access device as username and/or password are invalid");
+    }
+}
