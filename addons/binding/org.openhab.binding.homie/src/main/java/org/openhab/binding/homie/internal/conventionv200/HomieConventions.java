df711f84c05c3cdecb0816cceb1c881b2e40a990
Michael Kolb <dev@db1smk.com>
2017-01-16 00:03:15 +0100

    Removed unused code

--- addons/binding/org.openhab.binding.homie/src/main/java/org/openhab/binding/homie/internal/conventionv200/HomieConventions.java
+++ addons/binding/org.openhab.binding.homie/src/main/java/org/openhab/binding/homie/internal/conventionv200/HomieConventions.java
@@ -12,19 +12,10 @@ public class HomieConventions {
      * Pattern to match the ID Format of homie
      */
     protected final static String ID_PATTERN = "(?!-)([a-z0-9\\-]+)(?<!-)";
-
-    public final static String STATS_UPTIME_TOPIC_SUFFIX = "$stats/uptime";
-    public final static String ONLINE_TOPIC_SUFFIX = "$online";
+    /**
+     * Topic suffix which contents will be used to name a discovered device
+     */
     public final static String NAME_TOPIC_SUFFIX = "$name";
-    public final static String LOCALIP_TOPIC_SUFFIX = "$localip";
-    public static final String IMPLEMENTATION_TOPIC_SUFFIX = "$implementation";
-    public static final String FIRMWARE_CHECKSUM_TOPIC_SUFFIX = "$fw/checksum";
-    public static final String FIRMWARE_VERSION_TOPIC_SUFFIX = "$fw/version";
-    public static final String FIRMWARE_NAME_TOPIC_SUFFIX = "$fw/name";
-    public static final String STATS_INTERVAL_TOPIC_SUFFIX = "$stats/interval";
-    public static final String STATS_SIGNAL_TOPIC_SUFFIX = "$stats/signal";
-    public static final String HOMIE_TOPIC_SUFFIX = "$homie";
-    public static final String MAC_TOPIC_SUFFIX = "$mac";
 
     public static final String HOMIE_NODE_TYPE_ANNOUNCEMENT_TOPIC_SUFFIX = "$type";
     public static final String HOMIE_NODE_PROPERTYLIST_ANNOUNCEMENT_TOPIC_SUFFIX = "$properties";
