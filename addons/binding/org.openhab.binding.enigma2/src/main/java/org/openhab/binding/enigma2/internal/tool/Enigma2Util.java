db2de21ed65e41e8cbef117b9e9998d7974990a6
tratho <thomas.traunbauer@gmail.com>
2017-01-19 14:54:01 +0100

    [Enigma2] renamed XmlUtil to Enigma2Util, added functionality
    renamed XmlUtil to Enigma2XMLUtil
    rename *.internal.xml to *.intneral.tool
    added javadoc, changed init
    
    Signed-off-by: Thomas Traunbauer <thomas.traunbauer@gmail.com>(GitHub:tratho)

---
+++ addons/binding/org.openhab.binding.enigma2/src/main/java/org/openhab/binding/enigma2/internal/tool/Enigma2Util.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2014-2017 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.enigma2.internal.tool;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.eclipse.smarthome.io.net.http.HttpUtil;
+import org.openhab.binding.enigma2.internal.Enigma2ServiceContainer;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * The {@link Enigma2Util} is a static helper class, to get infos from a XML
+ *
+ * @author Thomas Traunbauer - Initial contribution
+ */
+public class Enigma2Util {
+
+    /**
+     * Finds the content in an element
+     */
+    public static String getContentOfElement(String content, String element) {
+        final String beginTag = "<" + element + ">";
+        final String endTag = "</" + element + ">";
+
+        final int startIndex = content.indexOf(beginTag) + beginTag.length();
+        final int endIndex = content.indexOf(endTag);
+
+        if (startIndex != -1 && endIndex != -1) {
+            return content.substring(startIndex, endIndex);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Executes an URL and returns to answer
+     */
+    public static String executeUrl(String url) throws IOException {
+        return HttpUtil.executeUrl("GET", url, 5000);
+    }
+
+    /**
+     * Scans "http://enigma2/web/getallservices" and generates map
+     *
+     * @throws IOException
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     */
+    public static Enigma2ServiceContainer generateServiceMaps()
+            throws IOException, ParserConfigurationException, SAXException {
+        Enigma2ServiceContainer serviceContainer = new Enigma2ServiceContainer();
+
+        File inputFile = new File("services.xml");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(inputFile));
+
+        String content = executeUrl("http://192.168.0.166/web/getallservices");
+        writer.write(content);
+        writer.close();
+
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(inputFile);
+        doc.getDocumentElement().normalize();
+        NodeList listOfBouquets = doc.getElementsByTagName("e2bouquet");
+        for (int bouquetIndex = 0; bouquetIndex < listOfBouquets.getLength(); bouquetIndex++) {
+            NodeList listOfServices = doc.getElementsByTagName("e2servicelist");
+            for (int serviceIndex = 0; serviceIndex < listOfServices.getLength(); serviceIndex++) {
+                NodeList serviceList = doc.getElementsByTagName("e2service");
+                for (int i = 0; i < serviceList.getLength(); i++) {
+                    Node service = serviceList.item(i);
+                    if (service.getNodeType() == Node.ELEMENT_NODE) {
+                        Element eElement = (Element) service;
+                        String e2servicereference = eElement.getElementsByTagName("e2servicereference").item(0)
+                                .getTextContent();
+                        String e2servicename = eElement.getElementsByTagName("e2servicename").item(0).getTextContent();
+
+                        serviceContainer.add(e2servicename, e2servicereference);
+                    }
+                }
+            }
+        }
+        inputFile.delete();
+        return serviceContainer;
+    }
+}
