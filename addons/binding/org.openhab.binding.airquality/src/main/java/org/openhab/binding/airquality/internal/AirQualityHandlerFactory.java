ce70d688406b3995f411ee26fbb1fbed921d5137
kubawolanin <hi@kubawolanin.com>
2017-01-20 19:25:58 +0100

    AirQuality binding - initial commit
    Signed-off-by: Kuba Wolanin <hi@kubawolanin.com>
    Also-by: Łukasz Dywicki <luke@code-house.org>
    
    Air Quality: code review follow-up
    AirQuality bugfixes
    AirQuality code review followup
    AirQuality - sitemap in README touchup and units added
    AirQuality enhancements
    Signed-off-by: Kuba Wolanin <hi@kubawolanin.com>
    
    AirQuality binding - code review follow-up
    
    Signed-off-by: kubawolanin <hi@kubawolanin.com>

---
+++ addons/binding/org.openhab.binding.airquality/src/main/java/org/openhab/binding/airquality/internal/AirQualityHandlerFactory.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.airquality.internal;
+
+import static org.openhab.binding.airquality.AirQualityBindingConstants.THING_TYPE_AQI;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.openhab.binding.airquality.handler.AirQualityHandler;
+
+/**
+ * The {@link AirQualityHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author Kuba Wolanin - Initial contribution
+ */
+public class AirQualityHandlerFactory extends BaseThingHandlerFactory {
+
+    private final static Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_AQI);
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
+
+    @Override
+    protected ThingHandler createHandler(Thing thing) {
+
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (thingTypeUID.equals(THING_TYPE_AQI)) {
+            return new AirQualityHandler(thing);
+        }
+
+        return null;
+    }
+
+}
