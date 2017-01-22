9980caf138d5d4f0aa83fa0eb6c91fbf923ec3e8
craigh <craigh@quailholdings.com>
2017-01-22 08:32:19 -1000

    removed package export that didn't fix problem tried adding empty constructor variable

--- addons/binding/org.openhab.binding.isy/src/main/java/org/openhab/binding/isy/internal/Variable.java
+++ addons/binding/org.openhab.binding.isy/src/main/java/org/openhab/binding/isy/internal/Variable.java
@@ -19,6 +19,10 @@ public class Variable {
     public Integer val;
     // protected String ts;
 
+    public Variable() {
+
+    }
+
     @Override
     public String toString() {
         return new StringBuilder("Isy Variable: ts=").append(id).append(", id=").append(id).append(", enabled=")
