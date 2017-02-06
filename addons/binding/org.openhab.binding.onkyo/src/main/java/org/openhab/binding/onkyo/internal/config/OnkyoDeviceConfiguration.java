0146584192e8ef65a0ef90daf37ef24022182740
Pauli Anttila <pauli.anttila@gmail.com>
2017-02-06 20:51:47 +0200

    Fix Onkyo binding package naming
    Signed-off-by: Pauli Anttila <pauli.anttila@gmail.com>

--- addons/binding/org.openhab.binding.onkyo/src/main/java/org/openhab/binding/onkyo/internal/config/OnkyoDeviceConfiguration.java
+++ addons/binding/org.openhab.binding.onkyo/src/main/java/org/openhab/binding/onkyo/internal/config/OnkyoDeviceConfiguration.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.rfxcom.internal.config;
+package org.openhab.binding.onkyo.internal.config;
 
 /**
  * Configuration class for {@link OnkyoBinding} device.
