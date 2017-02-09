9433d994710c568ffe2d171acc518475bc42595b
David Bennett <ddt@google.com>
2017-02-09 13:01:07 -0800

    Update some fixes to tests.

---
+++ addons/binding/org.openhab.binding.nest.test/src/test/java/org/openhab/binding/nest/discovery/NestDiscoveryServiceTest.java
@@ -0,0 +1,19 @@
+package org.openhab.binding.nest.discovery;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NestDiscoveryServiceTest {
+    // Bridge bridge;
+
+    @Before
+    public void setup() {
+        // Create the mocks and other needed things.
+        // bridge = Mockito.mock(Bridge.class);
+    }
+
+    @Test
+    public void testDiscoveryService() {
+
+    }
+}
