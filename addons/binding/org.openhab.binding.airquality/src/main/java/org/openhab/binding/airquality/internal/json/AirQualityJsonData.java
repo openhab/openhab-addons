9120828e5bc2751e197737c86a1592b0f7b0ec20
Kuba Wolanin <hi@kubawolanin.com>
2017-02-16 12:20:23 +0100

    AirQuality binding (#1738)
    * AirQuality binding - initial commit
    
    Also-by: Łukasz Dywicki <luke@code-house.org>
    Signed-off-by: Kuba Wolanin <hi@kubawolanin.com>

---
+++ addons/binding/org.openhab.binding.airquality/src/main/java/org/openhab/binding/airquality/internal/json/AirQualityJsonData.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.airquality.internal.json;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The {@link AirQualityJsonData} is responsible for storing
+ * the "data" node from the waqi.org JSON response
+ *
+ * @author Kuba Wolanin - Initial contribution
+ */
+public class AirQualityJsonData {
+
+    private int aqi;
+    private int idx;
+
+    private final static String GOOD = "GOOD";
+    private final static String MODERATE = "MODERATE";
+    private final static String UNHEALTHY_FOR_SENSITIVE = "UNHEALTHY_FOR_SENSITIVE";
+    private final static String UNHEALTHY = "UNHEALTHY";
+    private final static String VERY_UNHEALTHY = "VERY_UNHEALTHY";
+    private final static String HAZARDOUS = "HAZARDOUS";
+    private final static String NO_DATA = "NO_DATA";
+
+    private AirQualityJsonTime time;
+    private AirQualityJsonCity city;
+    private List<Attribute> attributions;
+    private AirQualityJsonIaqi iaqi;
+
+    public AirQualityJsonData() {
+    }
+
+    /**
+     * Air Quality Index
+     *
+     * @return {Integer}
+     */
+    public int getAqi() {
+        return aqi;
+    }
+
+    /**
+     * Measuring Station ID
+     *
+     * @return {Integer}
+     */
+    public int getStationId() {
+        return idx;
+    }
+
+    /**
+     * Receives "time" node from the "data" object in JSON response
+     *
+     * @return {AirQualityJsonTime}
+     */
+    public AirQualityJsonTime getTime() {
+        return time;
+    }
+
+    /**
+     * Receives "city" node from the "data" object in JSON response
+     *
+     * @return {AirQualityJsonCity}
+     */
+    public AirQualityJsonCity getCity() {
+        return city;
+    }
+
+    /**
+     * Collects a list of attributions (vendors making data available)
+     * and transforms it into readable string.
+     * Currently displayed in Thing Status description when ONLINE
+     *
+     * @return {String}
+     */
+    public String getAttributions() {
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < attributions.size(); i++) {
+            list.add(attributions.get(i).getName());
+        }
+        return "Attributions: " + String.join(", ", list);
+    }
+
+    /**
+     * Receives "iaqi" node from the "data" object in JSON response
+     *
+     * @return {AirQualityJsonIaqi}
+     */
+    public AirQualityJsonIaqi getIaqi() {
+        return iaqi;
+    }
+
+    /**
+     * Interprets the current aqi value within the ranges;
+     * Returns AQI in a human readable format
+     *
+     * @return
+     */
+    public String getAqiDescription() {
+        if (aqi > 0 && aqi <= 50) {
+            return GOOD;
+        } else if (aqi >= 51 && aqi <= 100) {
+            return MODERATE;
+        } else if (aqi >= 101 && aqi <= 150) {
+            return UNHEALTHY_FOR_SENSITIVE;
+        } else if (aqi >= 151 && aqi <= 200) {
+            return UNHEALTHY;
+        } else if (aqi >= 201 && aqi < 300) {
+            return VERY_UNHEALTHY;
+        } else if (aqi >= 300) {
+            return HAZARDOUS;
+        }
+
+        return NO_DATA;
+    }
+
+}
