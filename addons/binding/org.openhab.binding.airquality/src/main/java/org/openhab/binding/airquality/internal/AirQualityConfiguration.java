5a0c89d5497a00b32f33172df2ab359a9a359b89
Kuba Wolanin <hi@kubawolanin.com>
2017-02-16 12:20:23 +0100

    AirQuality binding (#1738)
    * AirQuality binding - initial commit
    
    Also-by: Łukasz Dywicki <luke@code-house.org>
    Signed-off-by: Kuba Wolanin <hi@kubawolanin.com>

---
+++ addons/binding/org.openhab.binding.airquality/src/main/java/org/openhab/binding/airquality/internal/AirQualityConfiguration.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.airquality.internal;
+
+/**
+ * The {@link AirQualityConfiguration} is the class used to match the
+ * thing configuration.
+ *
+ * @author Kuba Wolanin - Initial contribution
+ */
+public class AirQualityConfiguration {
+
+    public String apikey;
+
+    public String location;
+
+    public Integer stationId;
+
+    public Integer refresh;
+
+}
