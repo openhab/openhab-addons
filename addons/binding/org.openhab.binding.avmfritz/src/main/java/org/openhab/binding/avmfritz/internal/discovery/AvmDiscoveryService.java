ad1542575694f38a890b15f8b23207b58cf8a67f
Christoph Weitkamp <mail@christophweitkamp.de>
2017-02-26 15:01:00 +0100

    Revert "Added some more debug loggings"
    This reverts commit 0c1a308b604b505a1d8de08bddb362e707139d70.
    
    Signed-off-by: Christoph Weitkamp github@christophweitkamp.de

--- addons/binding/org.openhab.binding.avmfritz/src/main/java/org/openhab/binding/avmfritz/internal/discovery/AvmDiscoveryService.java
+++ addons/binding/org.openhab.binding.avmfritz/src/main/java/org/openhab/binding/avmfritz/internal/discovery/AvmDiscoveryService.java
@@ -32,6 +32,7 @@ import org.slf4j.LoggerFactory;
  *
  * @author Robert Bausdorf
 <<<<<<< HEAD
+<<<<<<< HEAD
  *
  */
 public class AvmDiscoveryService extends AbstractDiscoveryService {
@@ -177,6 +178,8 @@ public class AvmDiscoveryService extends AbstractDiscoveryService {
     }
 =======
  * @author Christoph Weitkamp
+=======
+>>>>>>> Revert "added some more debug loggings"
  * 
  */
 public class AvmDiscoveryService extends AbstractDiscoveryService {
@@ -195,7 +198,7 @@ public class AvmDiscoveryService extends AbstractDiscoveryService {
 	 * Initial delay in s for scanning job.
 	 */
 	private final static int INITIAL_DELAY = 5;
-
+	
 	/**
 	 * Scan interval in s for scanning job.
 	 */
@@ -229,8 +232,10 @@ public class AvmDiscoveryService extends AbstractDiscoveryService {
 	 */
 	@Override
 	public void startScan() {
-		logger.debug("starting scan on bridge {}", bridgeHandler.getThing().getUID());
-		FritzAhaDiscoveryCallback callback = new FritzAhaDiscoveryCallback(bridgeHandler.getWebInterface(), this);
+		logger.debug("starting scan on bridge "
+				+ bridgeHandler.getThing().getUID());
+		FritzAhaDiscoveryCallback callback = new FritzAhaDiscoveryCallback(
+				bridgeHandler.getWebInterface(), this);
 		bridgeHandler.getWebInterface().asyncGet(callback);
 	}
 
@@ -239,16 +244,13 @@ public class AvmDiscoveryService extends AbstractDiscoveryService {
 	 */
 	@Override
 	protected synchronized void stopScan() {
-		logger.debug("stop scan on bridge {}", bridgeHandler.getThing().getUID());
 		super.stopScan();
 		removeOlderResults(getTimestampOfLastScan());
 	}
 
 	/**
 	 * Add one discovered AHA device to inbox.
-	 * 
-	 * @param device
-	 *            Device model received from a FRITZ!Box
+	 * @param device Device model received from a FRITZ!Box
 	 */
 	public void onDeviceAddedInternal(DeviceModel device) {
 		ThingUID thingUID = this.bridgeHandler.getThingUID(device);
@@ -257,14 +259,16 @@ public class AvmDiscoveryService extends AbstractDiscoveryService {
 			Map<String, Object> properties = new HashMap<>(1);
 			properties.put(THING_AIN, device.getIdentifier());
 			properties.put(SERIAL_NUMBER, device.getIdentifier());
-
-			DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(thingUID).withProperties(properties)
-					.withRepresentationProperty(device.getIdentifier()).withBridge(bridgeUID)
-					.withLabel(device.getName()).build();
+			
+			DiscoveryResult discoveryResult = DiscoveryResultBuilder
+					.create(thingUID).withProperties(properties)
+					.withRepresentationProperty(device.getIdentifier())
+					.withBridge(bridgeUID).withLabel(device.getName()).build();
 
 			thingDiscovered(discoveryResult);
 		} else {
-			logger.debug("discovered unsupported device with id {}", device.getIdentifier());
+			logger.debug("discovered unsupported device with id {}",
+					device.getIdentifier());
 		}
 	}
 
@@ -275,8 +279,9 @@ public class AvmDiscoveryService extends AbstractDiscoveryService {
 	protected void startBackgroundDiscovery() {
 		if (scanningJob == null || scanningJob.isCancelled()) {
 			logger.debug("start background scanning job");
-			this.scanningJob = scheduler.scheduleWithFixedDelay(this.scanningRunnable, INITIAL_DELAY, SCAN_INTERVAL,
-					TimeUnit.SECONDS);
+			this.scanningJob = AvmDiscoveryService.scheduler
+					.scheduleWithFixedDelay(this.scanningRunnable, 
+							INITIAL_DELAY, SCAN_INTERVAL, TimeUnit.SECONDS);
 		} else {
 			logger.debug("scanningJob active");
 		}
@@ -288,8 +293,7 @@ public class AvmDiscoveryService extends AbstractDiscoveryService {
 	@Override
 	protected void stopBackgroundDiscovery() {
 		if (scanningJob != null && !scanningJob.isCancelled()) {
-			logger.debug("stop background scanning job");
-			scanningJob.cancel(true);
+			scanningJob.cancel(false);
 			scanningJob = null;
 		}
 	}
@@ -302,24 +306,22 @@ public class AvmDiscoveryService extends AbstractDiscoveryService {
 		 * Handler for delegation to callbacks.
 		 */
 		private AvmDiscoveryService service;
-
 		/**
 		 * Constructor.
-		 * 
 		 * @param handler
 		 */
 		public FritzScan(AvmDiscoveryService service) {
 			this.service = service;
 		}
-
 		/**
-		 * Poll the FRITZ!Box websevice one time.
+		 * Poll the FRITZ!Box websevice one time. 
 		 */
 		@Override
 		public void run() {
-			logger.debug("starting scan on bridge {}", bridgeHandler.getThing().getUID());
-			FritzAhaDiscoveryCallback callback = new FritzAhaDiscoveryCallback(bridgeHandler.getWebInterface(),
-					service);
+			logger.debug("starting scan on bridge "
+					+ bridgeHandler.getThing().getUID());
+			FritzAhaDiscoveryCallback callback = new FritzAhaDiscoveryCallback(
+					bridgeHandler.getWebInterface(), service);
 			bridgeHandler.getWebInterface().asyncGet(callback);
 		}
 	}
