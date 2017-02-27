3ef6177e50c06d608e5cbaeed5fb6aed1dc8fe14
Oliver Libutzki <OLibutzki@users.noreply.github.com>
2017-02-27 22:32:48 +0100

    [#1967] Initialization is delegated to a scheduler (#1968)
    Signed-off-by: Oliver Libutzki <oliver@libutzki.de>

--- addons/binding/org.openhab.binding.amazondashbutton/src/main/java/org/openhab/binding/amazondashbutton/handler/AmazonDashButtonHandler.java
+++ addons/binding/org.openhab.binding.amazondashbutton/src/main/java/org/openhab/binding/amazondashbutton/handler/AmazonDashButtonHandler.java
@@ -55,35 +55,43 @@ public class AmazonDashButtonHandler extends BaseThingHandler implements PcapNet
     public void initialize() {
         PcapNetworkInterfaceService.instance().registerListener(this);
         AmazonDashButtonConfig dashButtonConfig = getConfigAs(AmazonDashButtonConfig.class);
-        String pcapNetworkInterfaceName = dashButtonConfig.pcapNetworkInterfaceName;
-        String macAddress = dashButtonConfig.macAddress;
+        final String pcapNetworkInterfaceName = dashButtonConfig.pcapNetworkInterfaceName;
+        final String macAddress = dashButtonConfig.macAddress;
         final Integer packetInterval = dashButtonConfig.packetInterval;
-        PcapNetworkInterfaceWrapper pcapNetworkInterface = PcapUtil.getNetworkInterfaceByName(pcapNetworkInterfaceName);
-        if (pcapNetworkInterface == null) {
-            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.HANDLER_INITIALIZING_ERROR,
-                    "The networkinterface " + pcapNetworkInterfaceName + " is not present.");
-            return;
-        }
-
-        packetCapturingService = new PacketCapturingService(pcapNetworkInterface);
-        boolean capturingStarted = packetCapturingService.startCapturing(new PacketCapturingHandler() {
+        scheduler.submit(new Runnable() {
 
             @Override
-            public void packetCaptured(MacAddress macAddress) {
-                long now = System.currentTimeMillis();
-                if (lastCommandHandled + packetInterval < now) {
-                    ChannelUID pressChannel = new ChannelUID(getThing().getUID(), PRESS);
-                    triggerChannel(pressChannel);
-                    lastCommandHandled = now;
+            public void run() {
+                PcapNetworkInterfaceWrapper pcapNetworkInterface = PcapUtil
+                        .getNetworkInterfaceByName(pcapNetworkInterfaceName);
+                if (pcapNetworkInterface == null) {
+                    updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.HANDLER_INITIALIZING_ERROR,
+                            "The networkinterface " + pcapNetworkInterfaceName + " is not present.");
+                    return;
+                }
+
+                packetCapturingService = new PacketCapturingService(pcapNetworkInterface);
+                boolean capturingStarted = packetCapturingService.startCapturing(new PacketCapturingHandler() {
+
+                    @Override
+                    public void packetCaptured(MacAddress macAddress) {
+                        long now = System.currentTimeMillis();
+                        if (lastCommandHandled + packetInterval < now) {
+                            ChannelUID pressChannel = new ChannelUID(getThing().getUID(), PRESS);
+                            triggerChannel(pressChannel);
+                            lastCommandHandled = now;
+                        }
+                    }
+                }, macAddress);
+                if (capturingStarted) {
+                    updateStatus(ThingStatus.ONLINE);
+                } else {
+                    updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.NONE,
+                            "The capturing for " + pcapNetworkInterfaceName + " cannot be started.");
                 }
             }
-        }, macAddress);
-        if (capturingStarted) {
-            updateStatus(ThingStatus.ONLINE);
-        } else {
-            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.NONE,
-                    "The capturing for " + pcapNetworkInterfaceName + " cannot be started.");
-        }
+        });
+
     }
 
     @Override
