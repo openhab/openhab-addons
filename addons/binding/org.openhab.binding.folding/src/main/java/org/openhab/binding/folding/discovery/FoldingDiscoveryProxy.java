33b230e009d3445618de30e4f28233a7cf3061a9
fa2k <pmb@fa2k.net>
2017-03-01 12:09:40 +0100

    Folding@home binding initial commit (#1329)
    This commit contains a new binding for Folding@home (for the development
    history, see the fork by the famake3 organisation on Github).
    
    Signed-off-by: Marius Bjoernstad <pmb@fa2k.net> (github: fa2k)

---
+++ addons/binding/org.openhab.binding.folding/src/main/java/org/openhab/binding/folding/discovery/FoldingDiscoveryProxy.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.folding.discovery;
+
+import org.eclipse.smarthome.core.thing.ThingUID;
+
+/**
+ * Singleton instance to connect the Client handler and the discovery
+ * service.
+ *
+ * @author Marius Bjoernstad - Initial contribution
+ */
+public class FoldingDiscoveryProxy {
+
+    private static FoldingDiscoveryProxy instance;
+    private FoldingSlotDiscoveryService discoveryService = null;
+
+    private FoldingDiscoveryProxy() {
+    }
+
+    public static FoldingDiscoveryProxy getInstance() {
+        if (instance == null) {
+            instance = new FoldingDiscoveryProxy();
+        }
+        return instance;
+    }
+
+    public void setService(FoldingSlotDiscoveryService service) {
+        this.discoveryService = service;
+    }
+
+    public void newSlot(ThingUID bridgeUID, String host, String id, String description) {
+        if (instance != null) {
+            discoveryService.newSlot(bridgeUID, host, id, description);
+        }
+    }
+
+}
