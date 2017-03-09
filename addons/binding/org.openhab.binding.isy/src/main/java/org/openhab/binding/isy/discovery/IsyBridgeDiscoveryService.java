bc322abde86bea4637f6c9731980b63a4e166467
craigh <craigh@quailholdings.com>
2017-03-08 15:03:56 -1000

    remove unused dependency on upnp

--- addons/binding/org.openhab.binding.isy/src/main/java/org/openhab/binding/isy/discovery/IsyBridgeDiscoveryService.java
+++ addons/binding/org.openhab.binding.isy/src/main/java/org/openhab/binding/isy/discovery/IsyBridgeDiscoveryService.java
@@ -1,46 +1,25 @@
 package org.openhab.binding.isy.discovery;
 
-import java.util.Collections;
-import java.util.Set;
-
-import org.eclipse.smarthome.config.discovery.DiscoveryResult;
-import org.eclipse.smarthome.config.discovery.UpnpDiscoveryParticipant;
+import org.eclipse.smarthome.config.discovery.AbstractDiscoveryService;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
-import org.eclipse.smarthome.core.thing.ThingUID;
-import org.jupnp.model.meta.RemoteDevice;
 import org.openhab.binding.isy.IsyBindingConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class IsyBridgeDiscoveryService implements UpnpDiscoveryParticipant {
+import com.google.common.collect.ImmutableSet;
+
+public class IsyBridgeDiscoveryService extends AbstractDiscoveryService {
     private static final Logger logger = LoggerFactory.getLogger(IsyBridgeDiscoveryService.class);
     private static final int DISCOVER_TIMEOUT_SECONDS = 30;
 
-    // public IsyBridgeDiscoveryService() {
-    // super(ImmutableSet.of(new ThingTypeUID(IsyBindingConstants.BINDING_ID, "-")), DISCOVER_TIMEOUT_SECONDS, false);
-    // }
-    //
-    // @Override
-    // protected void startScan() {
-    // logger.debug("start scan called for isy bridge");
-    //
-    // }
-
-    @Override
-    public Set<ThingTypeUID> getSupportedThingTypeUIDs() {
-        return Collections.singleton(IsyBindingConstants.THING_TYPE_ISYBRIDGE);
+    public IsyBridgeDiscoveryService() {
+        super(ImmutableSet.of(new ThingTypeUID(IsyBindingConstants.BINDING_ID, "-")), DISCOVER_TIMEOUT_SECONDS, false);
     }
 
     @Override
-    public DiscoveryResult createResult(RemoteDevice device) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    protected void startScan() {
+        logger.debug("start scan called for isy bridge");
 
-    @Override
-    public ThingUID getThingUID(RemoteDevice device) {
-        // TODO Auto-generated method stub
-        return null;
     }
 
 }
