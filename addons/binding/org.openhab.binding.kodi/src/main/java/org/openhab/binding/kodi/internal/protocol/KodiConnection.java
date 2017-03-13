93d8b972d63ee10a9b06f407485f83795d0d13fb
Christoph Weitkamp <c.weitkamp@cx9.de>
2017-03-13 15:58:10 +0100

    Applied the code formatter

--- addons/binding/org.openhab.binding.kodi/src/main/java/org/openhab/binding/kodi/internal/protocol/KodiConnection.java
+++ addons/binding/org.openhab.binding.kodi/src/main/java/org/openhab/binding/kodi/internal/protocol/KodiConnection.java
@@ -252,12 +252,12 @@ public class KodiConnection implements KodiClientSocketEventListener {
         }
 
         String mediaType = item.get("type").getAsString();
-		if (mediaType.equals("channel") && item.has("channeltype")) {
-			String channelType = item.get("channeltype").getAsString();
-			if (channelType.equals("radio")) {
-				mediaType = "radio";
-			}
-		}
+        if (mediaType.equals("channel") && item.has("channeltype")) {
+            String channelType = item.get("channeltype").getAsString();
+            if (channelType.equals("radio")) {
+                mediaType = "radio";
+            }
+        }
 
         String artist = "";
         if (mediaType.equals("movie")) {
@@ -371,13 +371,13 @@ public class KodiConnection implements KodiClientSocketEventListener {
             if (data.has("item")) {
                 JsonObject item = data.get("item").getAsJsonObject();
                 String mediaType = item.get("type").getAsString();
-				if (mediaType.equals("channel") && item.has("channeltype")) {
-					String channelType = item.get("channeltype").getAsString();
-					if (channelType.equals("radio")) {
-						mediaType = "radio";
-					}
-				}
-				listener.updateMediaType(mediaType);
+                if (mediaType.equals("channel") && item.has("channeltype")) {
+                    String channelType = item.get("channeltype").getAsString();
+                    if (channelType.equals("radio")) {
+                        mediaType = "radio";
+                    }
+                }
+                listener.updateMediaType(mediaType);
             }
             requestPlayerUpdate(playerId, false);
         } else if ("Player.OnPause".equals(method)) {
