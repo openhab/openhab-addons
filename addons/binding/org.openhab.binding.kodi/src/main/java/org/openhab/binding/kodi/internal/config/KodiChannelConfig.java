f42f351d2ff61c65014e41e0147b00a0104617ce
Christoph Weitkamp <mail@christophweitkamp.de>
2017-03-14 22:38:12 +0100

    Added channels for opening PVR TV or Radio stream (step2: config for tv and radio channels)

---
+++ addons/binding/org.openhab.binding.kodi/src/main/java/org/openhab/binding/kodi/internal/config/KodiChannelConfig.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.kodi.internal.config;
+
+/**
+ * Channel configuration from openHab.
+ *
+ * @author Christoph Weitkamp
+ */
+
+public class KodiChannelConfig {
+    private String group;
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+}
