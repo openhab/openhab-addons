04f6c9b656da09be303814bd283b969f541f297e
Christoph Weitkamp <github@christophweitkamp.de>
2017-04-25 08:12:15 +0200

    Removed maven flag -DarchetypeCatalog (#2173)
    * Converted archetype-catalog.xml to valid archetype-settings.xml 
    * Replaced maven flag `archetypeCatalog` by a `settings` in scripts for new bindings
    closes #2169

--- addons/binding/create_openhab_binding_skeleton.sh
+++ addons/binding/create_openhab_binding_skeleton.sh
@@ -5,14 +5,13 @@ camelcaseId=$1
 
 id=`echo $camelcaseId | tr '[:upper:]' '[:lower:]'` 
 
-mvn archetype:generate -N \
+mvn -s ../archetype-settings.xml archetype:generate -N \
   -DarchetypeGroupId=org.eclipse.smarthome.archetype \
   -DarchetypeArtifactId=org.eclipse.smarthome.archetype.binding \
   -DarchetypeVersion=0.9.0-SNAPSHOT \
   -DgroupId=org.openhab.binding \
   -DartifactId=org.openhab.binding.$id \
   -Dpackage=org.openhab.binding.$id \
-  -DarchetypeCatalog='file://../archetype-catalog.xml' \
   -Dversion=2.1.0-SNAPSHOT \
   -DbindingId=$id \
   -DbindingIdCamelCase=$camelcaseId \
