3705947c8d14c7ab455fbbfd5a1e32a22fa7fa51
Christian Fischer <sw-dev@computerlyrik.de>
2017-05-04 09:49:44 +0200

    re-implement Discovery service with polling task

--- src/main/java/org/openhab/binding/hs110/discovery/HS110DiscoveryService.java
+++ src/main/java/org/openhab/binding/hs110/discovery/HS110DiscoveryService.java
@@ -12,11 +12,15 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import org.eclipse.smarthome.config.discovery.AbstractDiscoveryService;
 import org.eclipse.smarthome.config.discovery.DiscoveryResult;
@@ -30,7 +34,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  *
- * The {@link HS110DiscoveryService} detects new Power Plugs by sending a UDP network broadcast
+ * The {@link HS110DiscoveryService2} detects new Power Plugs by sending a UDP network broadcast
  * and parsing the answer into a thing.
  *
  * @author Christian Fischer - Initial contribution
@@ -42,16 +46,19 @@ public class HS110DiscoveryService extends AbstractDiscoveryService {
     ///// Network
     private byte[] discoverbuffer = Util.encryptBytes(HS110.Command.SYSINFO.value);
     private final DatagramPacket discoverPacket;
-    private boolean willbeclosed = false;
-    private DatagramSocket datagramSocket;
     private byte[] buffer = new byte[1024];
     private DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
     private final InetAddress broadcast;
 
+    private Future<?> scanFuture;
+    private static final int DISCOVER_TIMEOUT_SECONDS = 5;
+    private static final int RECEIVE_JOB_TIMEOUT = 200;
+    private static final int UDP_PACKET_TIMEOUT = RECEIVE_JOB_TIMEOUT - 50;
+
     public HS110DiscoveryService() throws UnknownHostException {
 
-        super(HS110BindingConstants.SUPPORTED_THING_TYPES_UIDS, 2, false);
+        super(HS110BindingConstants.SUPPORTED_THING_TYPES_UIDS, DISCOVER_TIMEOUT_SECONDS, false);
         byte[] addr = { (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff };
         broadcast = InetAddress.getByAddress(addr);
         discoverPacket = new DatagramPacket(discoverbuffer, discoverbuffer.length, broadcast, 9999);
@@ -60,27 +67,70 @@ public class HS110DiscoveryService extends AbstractDiscoveryService {
 
     @Override
     protected void startScan() {
+        logger.debug("Start Scan for {}",
+                this.getSupportedThingTypes().stream().map(uid -> uid.toString()).collect(Collectors.joining(" ")));
+        discoverThings();
 
-        try {
-            datagramSocket = new DatagramSocket(null);
-            datagramSocket.setBroadcast(true);
-            datagramSocket.send(discoverPacket);
+    }
 
-            if (logger.isTraceEnabled()) {
-                String packetContent = new String(discoverPacket.getData(), StandardCharsets.UTF_8);
-                logger.debug("Discovery package sent: {}", packetContent);
-            }
+    @Override
+    protected void stopScan() {
+        logger.debug("Stop Scan for {}",
+                this.getSupportedThingTypes().stream().map(uid -> uid.toString()).collect(Collectors.joining(" ")));
 
-            while (true) {
-                datagramSocket.receive(packet);
-                logger.debug("Discovery returned package with length {}", packet.getLength());
-                detectThing(packet);
-                packet.setLength(buffer.length);
-            }
-        } catch (IOException e) {
-            logger.error("Error starting discovery", e);
+        if (scanFuture != null) {
+            scanFuture.cancel(true);
         }
+        super.stopScan();
+    }
 
+    private void discoverThings() {
+        if (scanFuture == null) {
+            try (DatagramSocket datagramSocket = new DatagramSocket(null)) {
+                datagramSocket.setBroadcast(true);
+                datagramSocket.send(discoverPacket);
+
+                if (logger.isTraceEnabled()) {
+                    String packetContent = new String(discoverPacket.getData(), StandardCharsets.UTF_8);
+                    logger.debug("Discovery package sent: {}", packetContent);
+                }
+
+                datagramSocket.setSoTimeout(UDP_PACKET_TIMEOUT);
+
+                scanFuture = scheduler.scheduleAtFixedRate(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                            datagramSocket.receive(packet);
+
+                            logger.debug("Discovery returned package with length {}", packet.getLength());
+                            if (packet.getLength() > 0) {
+                                detectThing(packet);
+                            }
+
+                            packet.setLength(buffer.length);
+
+                        } catch (SocketTimeoutException e) {
+                            logger.info("No response received in time");
+
+                        } catch (IOException e) {
+                            logger.error("Error during discovery", e);
+                        }
+                    }
+                }, 0, RECEIVE_JOB_TIMEOUT, TimeUnit.MICROSECONDS);
+
+            } catch (Throwable ex) {
+                logger.error("Error starting discovery", ex);
+            } finally {
+                scanFuture = null;
+                removeOlderResults(getTimestampOfLastScan());
+            }
+        } else
+
+        {
+            logger.debug("HS110 device discovery scan in progress");
+        }
     }
 
     private void detectThing(DatagramPacket packet) throws IOException {
@@ -100,5 +150,4 @@ public class HS110DiscoveryService extends AbstractDiscoveryService {
                 .withProperties(properties).build();
         thingDiscovered(discoveryResult);
     }
-
 }
