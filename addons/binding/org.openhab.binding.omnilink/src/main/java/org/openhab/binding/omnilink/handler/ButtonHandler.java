60f328c21b3407e5daa38098ee7583b24167773d
craigh <craigh@quailholdings.com>
2017-05-08 16:12:08 -0700

    add some logging for buttons

--- addons/binding/org.openhab.binding.omnilink/src/main/java/org/openhab/binding/omnilink/handler/ButtonHandler.java
+++ addons/binding/org.openhab.binding.omnilink/src/main/java/org/openhab/binding/omnilink/handler/ButtonHandler.java
@@ -5,6 +5,7 @@ import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.UID;
 import org.eclipse.smarthome.core.types.Command;
+import org.eclipse.smarthome.core.types.RefreshType;
 import org.openhab.binding.omnilink.OmnilinkBindingConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,10 +22,15 @@ public class ButtonHandler extends AbstractOmnilinkHandler {
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
         String[] channelParts = channelUID.getAsString().split(UID.SEPARATOR);
-        getOmnilinkBridgeHander().sendOmnilinkCommand(CommandMessage.CMD_BUTTON, 0, Integer.parseInt(channelParts[2]));
+        logger.debug("must handle command for button.  channel: {}, command: {}", channelUID, command);
+        if (!(command instanceof RefreshType)) {
+            getOmnilinkBridgeHander().sendOmnilinkCommand(CommandMessage.CMD_BUTTON, 0,
+                    Integer.parseInt(channelParts[2]));
+        }
     }
 
     public void buttonPressed() {
+        logger.debug("buttonPressed");
         updateState(OmnilinkBindingConstants.CHANNEL_BUTTON_PRESS, OnOffType.ON);
     }
 }
