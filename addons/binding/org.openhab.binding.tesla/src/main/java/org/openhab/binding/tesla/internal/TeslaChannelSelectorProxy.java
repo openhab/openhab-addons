d8de642abb335a79bc78112bab811e33705cd12d
Karel Goderis <karel.goderis@me.com>
2017-05-08 10:53:11 +0200

    Tesla : improve vehicle_drive data handling by using the in-sequence-order of the timestamps of the results returned by the various Tesla APIs (#2199)
    Signed-off-by: Karel Goderis <karel.goderis@me.com>

--- addons/binding/org.openhab.binding.tesla/src/main/java/org/openhab/binding/tesla/internal/TeslaChannelSelectorProxy.java
+++ addons/binding/org.openhab.binding.tesla/src/main/java/org/openhab/binding/tesla/internal/TeslaChannelSelectorProxy.java
@@ -869,11 +869,9 @@ public class TeslaChannelSelectorProxy {
         TIMESTAMP("timestamp", "eventstamp", DateTimeType.class, false) {
             @Override
             public State getState(String s, TeslaChannelSelectorProxy proxy, Map<String, String> properties) {
-                Date date = new Date();
-                SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-
-                date.setTime(Long.valueOf(s));
-                return super.getState(DATE_FORMATTER.format(date));
+                Date date = new Date(Long.valueOf(s));
+                SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+                return super.getState(dateFormatter.format(date));
             }
         },
         TRIP_CARGING("trip_charging", "tripcharging", OnOffType.class, false) {
