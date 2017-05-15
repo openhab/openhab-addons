7bf59ee3d3f8ecdc900e120f1b82f7003f2a6d22
Karel Goderis <karel.goderis@me.com>
2017-05-15 21:45:07 +0200

    Keba : Fixes #2237 #2236 #2234 #2038
    Signed-off-by: Karel Goderis <karel.goderis@me.com>

--- addons/binding/org.openhab.binding.keba/src/main/java/org/openhab/binding/keba/KebaBindingConstants.java
+++ addons/binding/org.openhab.binding.keba/src/main/java/org/openhab/binding/keba/KebaBindingConstants.java
@@ -8,6 +8,10 @@
  */
 package org.openhab.binding.keba;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.ArrayUtils;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 
 /**
@@ -50,5 +54,74 @@ public class KebaBindingConstants {
     public static final String CHANNEL_POWER_FACTOR = "powerfactor";
     public static final String CHANNEL_SESSION_CONSUMPTION = "sessionconsumption";
     public static final String CHANNEL_TOTAL_CONSUMPTION = "totalconsumption";
+    public static final String CHANNEL_DISPLAY = "display";
 
+    public enum KebaType {
+        P20,
+        P30
+    };
+
+    public enum KebaSeries {
+
+        E('0'),
+        B('1'),
+        C('2', '3'),
+        X('A', 'B', 'C', 'D');
+
+        private final List<Character> things = new ArrayList<Character>();
+
+        KebaSeries(char... e) {
+            Character[] cArray = ArrayUtils.toObject(e);
+            for (char c : cArray) {
+                things.add(c);
+            }
+        }
+
+        public boolean matchesSeries(char c) {
+            return things.contains(c);
+        }
+
+        public static KebaSeries getSeries(char text) throws IllegalArgumentException {
+            for (KebaSeries c : KebaSeries.values()) {
+                if (c.matchesSeries(text)) {
+                    return c;
+                }
+            }
+
+            throw new IllegalArgumentException("Not a valid series");
+        }
+    };
+
+    public enum KebaFirmware {
+        V201M21("2.01m21"),
+        V22A1("2.2a1"),
+        V23A2("2.3a2"),
+        V23A3("2.3a3"),
+        V25A3("2.5a3"),
+        V3042A1("3.04.2a1"),
+        V3062A5("3.06.2a5"),
+        V3071A1("3.07.1a1");
+
+        private final String id;
+
+        private KebaFirmware(final String id) {
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return id;
+        }
+
+        public static KebaFirmware getFirmware(String text) throws IllegalArgumentException {
+            for (KebaFirmware c : KebaFirmware.values()) {
+                if (text.contains(c.id)) {
+                    return c;
+                }
+            }
+
+            throw new IllegalArgumentException("Not a valid firmware");
+        }
+
+    };
 }
