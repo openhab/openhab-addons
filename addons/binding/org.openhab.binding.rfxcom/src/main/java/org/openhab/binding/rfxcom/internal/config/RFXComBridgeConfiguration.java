67df4bc7169ebffd7974df08591488de1fe83298
Mike Jagdis <mjagdis@eris-associates.co.uk>
2017-05-22 17:54:07 +0100

    [RFXCOM] Add a bridge option to disable discovery
    These RF protocols are just inherently noisy. Having the inbox
    continuously fill up with junk is annoying.
    
    Closes #2186
    
    Signed-off-by: Mike Jagdis <mjagdis@eris-associates.co.uk> (github: mjagdis)

--- addons/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/internal/config/RFXComBridgeConfiguration.java
+++ addons/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/internal/config/RFXComBridgeConfiguration.java
@@ -31,6 +31,9 @@ public class RFXComBridgeConfiguration {
 
     public String transceiverType;
 
+    // Prevent unknown devices from being added to the inbox
+    public boolean disableDiscovery;
+
     // Won't configure protocols to RFXCOM transceiver
     public boolean ignoreConfig;
 
