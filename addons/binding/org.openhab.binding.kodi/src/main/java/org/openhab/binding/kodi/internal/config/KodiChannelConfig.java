a9ad7e821fca7e03f8d3265a37f790dea98f5aeb
Christoph Weitkamp <github@christophweitkamp.de>
2017-05-26 15:53:35 +0200

    [kodi] Added channels for opening PVR TV or Radio streams (#2084)
    * Added channels for opening PVR TV or Radio streams
    * Added polling job to fix refreshing channels while Kodi is playing
    * Use default channelgroupids (TV=1,Radio=2) if configured channelgroup is not valid
    * Removed duplicate implementation of refreshing 'mediatype' channel
    
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

---
+++ addons/binding/org.openhab.binding.kodi/src/main/java/org/openhab/binding/kodi/internal/config/KodiChannelConfig.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.kodi.internal.config;
+
+/**
+ * Channel configuration from openHab.
+ *
+ * @author Christoph Weitkamp - Added channels for opening PVR TV or Radio streams
+ * 
+ */
+
+public class KodiChannelConfig {
+    private String group;
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(final String group) {
+        this.group = group;
+    }
+}
