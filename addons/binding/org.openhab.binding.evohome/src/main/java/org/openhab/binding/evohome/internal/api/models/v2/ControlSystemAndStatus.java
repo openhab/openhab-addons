07a033013137977bd84ac4f4716a8414e4dcec2f
foxy82 <neil.renaud@gmail.com>
2017-05-30 16:12:46 +0100

    Revert "Updating to Nebula83 repo"

--- addons/binding/org.openhab.binding.evohome/src/main/java/org/openhab/binding/evohome/internal/api/models/v2/ControlSystemAndStatus.java
+++
@@ -1,9 +0,0 @@
-package org.openhab.binding.evohome.internal.api.models.v2;
-
-import org.openhab.binding.evohome.internal.api.models.v2.response.TemperatureControlSystem;
-import org.openhab.binding.evohome.internal.api.models.v2.response.TemperatureControlSystemStatus;
-
-public class ControlSystemAndStatus {
-    public TemperatureControlSystemStatus controlSystemStatus;
-    public TemperatureControlSystem       controlSystem;
-}
--- addons/binding/org.openhab.binding.evohome/src/main/java/org/openhab/binding/evohome/internal/api/models/v2/response/Locations.java
+++
@@ -1,9 +0,0 @@
-package org.openhab.binding.evohome.internal.api.models.v2.response;
-
-import java.util.ArrayList;
-
-import org.openhab.binding.evohome.internal.api.models.v2.response.Location;
-
-public class Locations extends ArrayList<Location> {
-
-}
