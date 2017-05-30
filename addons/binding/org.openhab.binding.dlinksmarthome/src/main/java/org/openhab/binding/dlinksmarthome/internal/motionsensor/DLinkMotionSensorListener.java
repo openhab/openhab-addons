9b9bd8a0c069541f07589bb80fadfe95d11d0009
Mike Major <mike_j_major@hotmail.com>
2017-05-30 09:17:41 +0100

    D-Link Smart Home device binding (#2035)
    * D-Link Smart Home binding
    
    Signed-off-by: Mike Major <mike_j_major@hotmail.com>

---
+++ addons/binding/org.openhab.binding.dlinksmarthome/src/main/java/org/openhab/binding/dlinksmarthome/internal/motionsensor/DLinkMotionSensorListener.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.dlinksmarthome.internal.motionsensor;
+
+import org.openhab.binding.dlinksmarthome.internal.motionsensor.DLinkMotionSensorCommunication.DeviceStatus;
+
+/**
+ * The {@link DLinkMotionSensorListener} provides callbacks for motion detection
+ * and device status changes.
+ *
+ * @author Mike Major - Initial contribution
+ */
+public interface DLinkMotionSensorListener {
+
+    /**
+     * Callback to indicate motion has been detected
+     */
+    void motionDetected();
+
+    /**
+     * Callback to indicate a change in the device status
+     */
+    void sensorStatus(final DeviceStatus status);
+}
