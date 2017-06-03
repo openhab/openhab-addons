diamond5170/openhab2-addons

Vera controller binding for new SmartHome API version.

addons/binding/org.openhab.binding.vera2/src/main/java/org/openhab/binding/vera2/VeraBindingConstants.java
@@ -1,0 +1,99 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.vera2;
+
+import java.util.Set;
+
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * The {@link VeraBinding} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Dmitriy Ponomarev
+ */
+public class VeraBindingConstants {
+
+    public static final String BINDING_ID = "vera2";
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID THING_TYPE_BRIDGE = new ThingTypeUID(BINDING_ID, "veraController");
+    public static final ThingTypeUID THING_TYPE_DEVICE = new ThingTypeUID(BINDING_ID, "veraDevice");
+
+    public static final Set<ThingTypeUID> SUPPORTED_DEVICE_THING_TYPES_UIDS = ImmutableSet.of(THING_TYPE_BRIDGE,
+            THING_TYPE_DEVICE);
+
+    // List of ignored devices for Discovery
+    public static final Set<String> DISCOVERY_IGNORED_DEVICES = ImmutableSet.of("BatteryPolling");
+
+    // List of all Channel IDs
+    public static final String BATTERY_CHANNEL = "battery";
+    public static final String DOORLOCK_CHANNEL = "doorlock";
+    public static final String SENSOR_BINARY_CHANNEL = "sensorBinary";
+    public static final String SENSOR_MULTILEVEL_CHANNEL = "sensorMultilevel";
+    public static final String SENSOR_DISCRETE_CHANNEL = "sensorDiscrete";
+    public static final String SWITCH_BINARY_CHANNEL = "switchBinary";
+    public static final String SWITCH_CONTROL_CHANNEL = "switchControl";
+    public static final String SWITCH_MULTILEVEL_CHANNEL = "switchMultilevel";
+    // switch multilevel (color)
+    public static final String SWITCH_COLOR_CHANNEL = "switchColor";
+    public static final String SWITCH_COLOR_TEMPERATURE_CHANNEL = "switchColorTemperature";
+    // thermostat
+    public static final String THERMOSTAT_MODE_CHANNEL = "thermostatMode";
+    public static final String THERMOSTAT_SET_POINT_CHANNEL = "thermostatSetPoint";
+
+    public static final String THERMOSTAT_MODE_CC_CHANNEL = "thermostatModeCC";
+
+    // sensor multilevel
+    public static final String SENSOR_TEMPERATURE_CHANNEL = "sensorTemperature";
+    public static final String SENSOR_LUMINOSITY_CHANNEL = "sensorLuminosity";
+    public static final String SENSOR_HUMIDITY_CHANNEL = "sensorHumidity";
+    public static final String SENSOR_BAROMETER_CHANNEL = "sensorBarometer";
+    public static final String SENSOR_ULTRAVIOLET_CHANNEL = "sensorUltraviolet";
+    public static final String SENSOR_CO2_CHANNEL = "sensorCO2";
+    public static final String SENSOR_ENERGY_CHANNEL = "sensorEnergy";
+    // sensor multilevel (meter)
+    public static final String SENSOR_METER_KWH_CHANNEL = "sensorMeterKWh";
+    public static final String SENSOR_METER_W_CHANNEL = "sensorMeterW";
+    // sensor binary
+    public static final String SENSOR_SMOKE_CHANNEL = "sensorSmoke";
+    public static final String SENSOR_CO_CHANNEL = "sensorCo";
+    public static final String SENSOR_FLOOD_CHANNEL = "sensorFlood";
+    public static final String SENSOR_TAMPER_CHANNEL = "sensorTamper";
+    public static final String SENSOR_DOOR_WINDOW_CHANNEL = "sensorDoorWindow";
+    public static final String SENSOR_MOTION_CHANNEL = "sensorMotion";
+    // switch binary
+    public static final String SWITCH_POWER_OUTLET_CHANNEL = "switchPowerOutlet";
+    // switch multilevel
+    public static final String SWITCH_ROLLERSHUTTER_CHANNEL = "switchBlinds";
+    // special channels
+    public static final String ACTIONS_CHANNEL = "actions";
+    public static final String ACTIONS_CHANNEL_OPTION_REFRESH = "REFRESH";
+
+    /* Bridge config properties */
+    public static final String BRIDGE_CONFIG_vera_SERVER_IP_ADDRESS = "veraControllerIpAddress";
+    public static final String BRIDGE_CONFIG_vera_SERVER_PORT = "veraControllerPort";
+    public static final String BRIDGE_CONFIG_POLLING_INTERVAL = "pollingInterval";
+
+    public static final String DEVICE_CONFIG_DEVICE_ID = "deviceId";
+    public static final String DEVICE_PROP_ROOM = "room";
+    public static final String DEVICE_PROP_CATEGORY = "category";
+    public static final String DEVICE_PROP_SUBCATEGORY = "subcategory";
+    public static final String DEVICE_PROP_LAST_UPDATE = "lastUpdate";
+
+    /* Bridge properties */
+    public static final String BRIDGE_PROP_SOFTWARE_REVISION_VERSION = "softwareRevisionVersion";
+    public static final String BRIDGE_PROP_SOFTWARE_REVISION_DATE = "softwareRevisionDate";
+    public static final String BRIDGE_PROP_SDK = "SDK";
+    public static final String BRIDGE_PROP_MANUFACTURER_ID = "manufacturerId";
+    public static final String BRIDGE_PROP_SECURE_INCLUSION = "secureInclusion";
+    public static final String BRIDGE_PROP_FREQUENCY = "frequency";
+}

addons/binding/org.openhab.binding.vera2/src/main/java/org/openhab/binding/vera2/config/VeraDeviceConfiguration.java
@@ -1,0 +1,35 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.vera2.config;
+
+import static org.openhab.binding.vera2.VeraBindingConstants.DEVICE_CONFIG_DEVICE_ID;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * The {@link VeraDeviceConfiguration} class defines the model for a device configuration.
+ *
+ * @author Dmitriy Ponomarev
+ */
+public class VeraDeviceConfiguration {
+    private String deviceId;
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append(DEVICE_CONFIG_DEVICE_ID, this.getDeviceId()).toString();
+    }
+}

addons/binding/org.openhab.binding.vera2/src/main/java/org/openhab/binding/vera2/internal/converter/VeraDeviceStateConverter.java
@@ -1,0 +1,189 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.vera2.internal.converter;
+
+import static org.openhab.binding.vera2.VeraBindingConstants.BATTERY_CHANNEL;
+
+import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.library.types.OpenClosedType;
+import org.eclipse.smarthome.core.library.types.PercentType;
+import org.eclipse.smarthome.core.thing.Channel;
+import org.eclipse.smarthome.core.types.State;
+import org.eclipse.smarthome.core.types.UnDefType;
+import org.openhab.binding.vera2.controller.Vera.json.Device;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link VeraDeviceStateConverter} is responsible for converting Vera device level to openHAB states
+ *
+ * @author Dmitriy Ponomarev
+ */
+public class VeraDeviceStateConverter {
+    private static Logger logger = LoggerFactory.getLogger(VeraDeviceStateConverter.class);
+
+    public static State toState(Device device, Channel channel) {
+        if (channel.getUID().getId().split("-")[0].equals(BATTERY_CHANNEL)) {
+            return getMultilevelState(device.batterylevel);
+        }
+
+        int category = Integer.parseInt(device.category);
+        int subcategory = Integer.parseInt(device.subcategory);
+        switch (category) {
+            case 0:
+            case 1: // Interface
+                break;
+            case 2: // Dimmable Light
+                switch (subcategory) {
+                    case 1:
+                    case 2:
+                    case 3:
+                        return getPercentState(device.level);
+                    case 4:
+                        return getPercentState(device.level);// TODO getColorState(device.light);
+                }
+                break;
+            case 3: // Switch
+                return getBinaryState(device.status);
+            case 4: // Security Sensor
+                switch (subcategory) {
+                    case 1:
+                        return getDoorlockState(device.tripped.equals("1") ? "0" : "1");
+                    case 2:
+                        return getBinaryState(device.tripped);
+                    case 3:
+                        return getBinaryState(device.tripped);
+                    case 4:
+                        return getBinaryState(device.tripped);
+                    case 5:
+                        return getBinaryState(device.tripped);
+                    case 6:
+                        return getBinaryState(device.tripped);
+                }
+                break;
+            case 5: // TODO HVAC
+                logger.warn("TODO: HVAC: {}, {}, {}, {}", device.id, device.name, device.category, device.categoryName);
+                break;
+            case 6: // TODO Camera
+                logger.warn("TODO: Camera: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 7: // Door Lock
+                return getDoorlockState(device.locked);
+            case 8: // Window Covering
+                return getPercentState(device.level);
+            case 9: // TODO Remote Control
+                logger.warn("TODO: Remote Control: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 10: // TODO IR Transmitter
+                logger.warn("TODO: IR Transmitter: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 11: // TODO Generic I/O
+                logger.warn("TODO: Generic I/O: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 12: // Generic Sensor
+                return getMultilevelState(device.level);
+            case 13: // TODO Serial Port
+                logger.warn("TODO: Serial Port I/O: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 14: // Scene Controller
+                return getBinaryState(device.status);
+            case 15: // TODO A/V
+                logger.warn("TODO: A/V: {}, {}, {}, {}", device.id, device.name, device.category, device.categoryName);
+                break;
+            case 16: // Humidity Sensor
+                return getMultilevelState(device.humidity);
+            case 17: // Temperature Sensor
+                return getMultilevelState(device.temperature);
+            case 18: // Light Sensor
+                return getMultilevelState(device.light);
+            case 19: // TODO Z-Wave Interface
+                logger.warn("TODO: Z-Wave Interface: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 20: // TODO Insteon Interface
+                logger.warn("TODO: Insteon Interface: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 21: // Power Meter
+                return getMultilevelState(device.level);
+            case 22: // TODO Alarm Panel
+                logger.warn("TODO: Alarm Panel: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 23: // TODO Alarm Partition
+                logger.warn("TODO: Alarm Partition: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 24: // TODO Siren
+                logger.warn("TODO: Siren: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 25: // TODO Weather
+                logger.warn("TODO: Weather: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 26: // TODO Philips Controller
+                logger.warn("TODO: Philips Controller: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 27: // TODO Appliance
+                logger.warn("TODO: Appliance: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 28: // UV Sensor
+                return getMultilevelState(device.level);
+            default:
+                logger.warn("Unknown device type: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+        }
+        return UnDefType.UNDEF;
+    }
+
+    private static State getMultilevelState(String multilevelValue) {
+        if (multilevelValue != null) {
+            return new DecimalType(multilevelValue);
+        }
+        return UnDefType.UNDEF;
+    }
+
+    private static State getPercentState(String multilevelValue) {
+        if (multilevelValue != null) {
+            return new PercentType(multilevelValue);
+        }
+        return UnDefType.UNDEF;
+    }
+
+    private static State getBinaryState(String status) {
+        if (status != null) {
+            if (status.equals("1")) {
+                return OnOffType.ON;
+            } else if (status.equals("0")) {
+                return OnOffType.OFF;
+            }
+        }
+        return UnDefType.UNDEF;
+    }
+
+    private static State getDoorlockState(String status) {
+        if (status != null) {
+            if (status.equals("1")) {
+                return OpenClosedType.CLOSED;
+            } else if (status.equals("0")) {
+                return OpenClosedType.OPEN;
+            }
+        }
+        return UnDefType.UNDEF;
+    }
+}

openhab/openhab-addons

Signed-off-by: Dmitriy Ponomarev <diamond5170@gmail.com>

addons/binding/org.openhab.binding.vera2/src/main/java/org/openhab/binding/vera2/VeraBindingConstants.java
@@ -1,0 +1,103 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.vera2;
+
+import java.util.Set;
+
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * The {@link VeraBinding} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Dmitriy Ponomarev
+ */
+public class VeraBindingConstants {
+
+    public static final String BINDING_ID = "vera2";
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID THING_TYPE_BRIDGE = new ThingTypeUID(BINDING_ID, "veraController");
+    public static final ThingTypeUID THING_TYPE_DEVICE = new ThingTypeUID(BINDING_ID, "veraDevice");
+    public static final ThingTypeUID THING_TYPE_SCENE = new ThingTypeUID(BINDING_ID, "veraScene");
+
+    public static final Set<ThingTypeUID> SUPPORTED_DEVICE_THING_TYPES_UIDS = ImmutableSet.of(THING_TYPE_BRIDGE,
+            THING_TYPE_DEVICE, THING_TYPE_SCENE);
+
+    // List of ignored devices for Discovery
+    public static final Set<String> DISCOVERY_IGNORED_DEVICES = ImmutableSet.of("BatteryPolling");
+
+    // List of all Channel IDs
+    public static final String BATTERY_CHANNEL = "battery";
+    public static final String DOORLOCK_CHANNEL = "doorlock";
+    public static final String SENSOR_BINARY_CHANNEL = "sensorBinary";
+    public static final String SENSOR_MULTILEVEL_CHANNEL = "sensorMultilevel";
+    public static final String SENSOR_DISCRETE_CHANNEL = "sensorDiscrete";
+    public static final String SWITCH_BINARY_CHANNEL = "switchBinary";
+    public static final String SWITCH_CONTROL_CHANNEL = "switchControl";
+    public static final String SWITCH_MULTILEVEL_CHANNEL = "switchMultilevel";
+    // switch multilevel (color)
+    public static final String SWITCH_COLOR_CHANNEL = "switchColor";
+    public static final String SWITCH_COLOR_TEMPERATURE_CHANNEL = "switchColorTemperature";
+    // thermostat
+    public static final String THERMOSTAT_MODE_CHANNEL = "thermostatMode";
+    public static final String THERMOSTAT_SET_POINT_CHANNEL = "thermostatSetPoint";
+
+    public static final String THERMOSTAT_MODE_CC_CHANNEL = "thermostatModeCC";
+
+    // sensor multilevel
+    public static final String SENSOR_TEMPERATURE_CHANNEL = "sensorTemperature";
+    public static final String SENSOR_LUMINOSITY_CHANNEL = "sensorLuminosity";
+    public static final String SENSOR_HUMIDITY_CHANNEL = "sensorHumidity";
+    public static final String SENSOR_BAROMETER_CHANNEL = "sensorBarometer";
+    public static final String SENSOR_ULTRAVIOLET_CHANNEL = "sensorUltraviolet";
+    public static final String SENSOR_CO2_CHANNEL = "sensorCO2";
+    public static final String SENSOR_ENERGY_CHANNEL = "sensorEnergy";
+    // sensor multilevel (meter)
+    public static final String SENSOR_METER_KWH_CHANNEL = "sensorMeterKWh";
+    public static final String SENSOR_METER_W_CHANNEL = "sensorMeterW";
+    // sensor binary
+    public static final String SENSOR_SMOKE_CHANNEL = "sensorSmoke";
+    public static final String SENSOR_CO_CHANNEL = "sensorCo";
+    public static final String SENSOR_FLOOD_CHANNEL = "sensorFlood";
+    public static final String SENSOR_TAMPER_CHANNEL = "sensorTamper";
+    public static final String SENSOR_DOOR_WINDOW_CHANNEL = "sensorDoorWindow";
+    public static final String SENSOR_MOTION_CHANNEL = "sensorMotion";
+    // switch binary
+    public static final String SWITCH_POWER_OUTLET_CHANNEL = "switchPowerOutlet";
+    // switch multilevel
+    public static final String SWITCH_ROLLERSHUTTER_CHANNEL = "switchBlinds";
+    // special channels
+    public static final String ACTIONS_CHANNEL = "actions";
+    public static final String ACTIONS_CHANNEL_OPTION_REFRESH = "REFRESH";
+
+    /* Bridge config properties */
+    public static final String BRIDGE_CONFIG_vera_SERVER_IP_ADDRESS = "veraControllerIpAddress";
+    public static final String BRIDGE_CONFIG_vera_SERVER_PORT = "veraControllerPort";
+    public static final String BRIDGE_CONFIG_POLLING_INTERVAL = "pollingInterval";
+
+    public static final String PROP_ROOM = "room";
+    public static final String PROP_LAST_UPDATE = "lastUpdate";
+
+    public static final String DEVICE_CONFIG_ID = "deviceId";
+    public static final String DEVICE_PROP_CATEGORY = "category";
+    public static final String DEVICE_PROP_SUBCATEGORY = "subcategory";
+
+    public static final String SCENE_CONFIG_ID = "sceneId";
+
+    /* Bridge properties */
+    public static final String BRIDGE_PROP_SOFTWARE_REVISION_VERSION = "softwareRevisionVersion";
+    public static final String BRIDGE_PROP_SOFTWARE_REVISION_DATE = "softwareRevisionDate";
+    public static final String BRIDGE_PROP_SDK = "SDK";
+    public static final String BRIDGE_PROP_MANUFACTURER_ID = "manufacturerId";
+    public static final String BRIDGE_PROP_SECURE_INCLUSION = "secureInclusion";
+    public static final String BRIDGE_PROP_FREQUENCY = "frequency";
+}

addons/binding/org.openhab.binding.vera2/src/main/java/org/openhab/binding/vera2/config/VeraDeviceConfiguration.java
@@ -1,0 +1,35 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.vera2.config;
+
+import static org.openhab.binding.vera2.VeraBindingConstants.DEVICE_CONFIG_ID;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * The {@link VeraDeviceConfiguration} class defines the model for a device configuration.
+ *
+ * @author Dmitriy Ponomarev
+ */
+public class VeraDeviceConfiguration {
+    private String deviceId;
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append(DEVICE_CONFIG_ID, this.getDeviceId()).toString();
+    }
+}

addons/binding/org.openhab.binding.vera2/src/main/java/org/openhab/binding/vera2/internal/converter/VeraDeviceStateConverter.java
@@ -1,0 +1,180 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.vera2.internal.converter;
+
+import static org.openhab.binding.vera2.VeraBindingConstants.BATTERY_CHANNEL;
+
+import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.library.types.OpenClosedType;
+import org.eclipse.smarthome.core.library.types.PercentType;
+import org.eclipse.smarthome.core.thing.Channel;
+import org.eclipse.smarthome.core.types.State;
+import org.eclipse.smarthome.core.types.UnDefType;
+import org.openhab.binding.vera2.controller.Vera.json.Device;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link VeraDeviceStateConverter} is responsible for converting Vera device level to openHAB states
+ *
+ * @author Dmitriy Ponomarev
+ */
+public class VeraDeviceStateConverter {
+    private static Logger logger = LoggerFactory.getLogger(VeraDeviceStateConverter.class);
+
+    public static State toState(Device device, Channel channel) {
+        if (channel.getUID().getId().split("-")[0].equals(BATTERY_CHANNEL)) {
+            return getMultilevelState(device.batterylevel);
+        }
+
+        int category = Integer.parseInt(device.category);
+        int subcategory = Integer.parseInt(device.subcategory);
+        switch (category) {
+            case 0:
+            case 1: // Interface
+                break;
+            case 2: // Dimmable Light
+                switch (subcategory) {
+                    case 1:
+                    case 2:
+                    case 3:
+                        return getPercentState(device.level);
+                    case 4:
+                        return getPercentState(device.level);// TODO getColorState(device.light);
+                }
+                break;
+            case 3: // Switch
+                return getBinaryState(device.status);
+            case 4: // Security Sensor
+                switch (subcategory) {
+                    case 1:
+                        return getDoorlockState(device.tripped.equals("1") ? "0" : "1");
+                    default:
+                        return getBinaryState(device.tripped);
+                }
+            case 5: // TODO HVAC
+                logger.warn("TODO: HVAC: {}, {}, {}, {}", device.id, device.name, device.category, device.categoryName);
+                break;
+            case 6: // TODO Camera
+                logger.warn("TODO: Camera: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 7: // Door Lock
+                return getBinaryState(device.locked);
+            case 8: // Window Covering
+                return getPercentState(device.level);
+            case 9: // TODO Remote Control
+                logger.warn("TODO: Remote Control: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 10: // TODO IR Transmitter
+                logger.warn("TODO: IR Transmitter: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 11: // TODO Generic I/O
+                logger.warn("TODO: Generic I/O: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 12: // Generic Sensor
+                return getMultilevelState(device.level);
+            case 13: // TODO Serial Port
+                logger.warn("TODO: Serial Port I/O: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 14: // Scene Controller
+                return getBinaryState(device.status);
+            case 15: // TODO A/V
+                logger.warn("TODO: A/V: {}, {}, {}, {}", device.id, device.name, device.category, device.categoryName);
+                break;
+            case 16: // Humidity Sensor
+                return getMultilevelState(device.humidity);
+            case 17: // Temperature Sensor
+                return getMultilevelState(device.temperature);
+            case 18: // Light Sensor
+                return getMultilevelState(device.light);
+            case 19: // TODO Z-Wave Interface
+                logger.warn("TODO: Z-Wave Interface: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 20: // TODO Insteon Interface
+                logger.warn("TODO: Insteon Interface: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 21: // Power Meter
+                return getMultilevelState(device.level);
+            case 22: // TODO Alarm Panel
+                logger.warn("TODO: Alarm Panel: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 23: // TODO Alarm Partition
+                logger.warn("TODO: Alarm Partition: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 24: // TODO Siren
+                logger.warn("TODO: Siren: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 25: // TODO Weather
+                logger.warn("TODO: Weather: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 26: // TODO Philips Controller
+                logger.warn("TODO: Philips Controller: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 27: // TODO Appliance
+                logger.warn("TODO: Appliance: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+                break;
+            case 28: // UV Sensor
+                return getMultilevelState(device.level);
+            default:
+                logger.warn("Unknown device type: {}, {}, {}, {}", device.id, device.name, device.category,
+                        device.categoryName);
+        }
+        return UnDefType.UNDEF;
+    }
+
+    private static State getMultilevelState(String multilevelValue) {
+        if (multilevelValue != null) {
+            return new DecimalType(multilevelValue);
+        }
+        return UnDefType.UNDEF;
+    }
+
+    private static State getPercentState(String multilevelValue) {
+        if (multilevelValue != null) {
+            return new PercentType(multilevelValue);
+        }
+        return UnDefType.UNDEF;
+    }
+
+    private static State getBinaryState(String status) {
+        if (status != null) {
+            if (status.equals("1")) {
+                return OnOffType.ON;
+            } else if (status.equals("0")) {
+                return OnOffType.OFF;
+            }
+        }
+        return UnDefType.UNDEF;
+    }
+
+    private static State getDoorlockState(String status) {
+        if (status != null) {
+            if (status.equals("1")) {
+                return OpenClosedType.CLOSED;
+            } else if (status.equals("0")) {
+                return OpenClosedType.OPEN;
+            }
+        }
+        return UnDefType.UNDEF;
+    }
+}