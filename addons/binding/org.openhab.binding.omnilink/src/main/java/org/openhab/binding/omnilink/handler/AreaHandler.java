87c6ba1769f8d7729e65fcd0c9dd5e63e2f2d1ab
Dan Cunningham <dan@digitaldan.com>
2017-06-12 13:44:11 -0700

    removed refresh type and added channel linked
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

--- addons/binding/org.openhab.binding.omnilink/src/main/java/org/openhab/binding/omnilink/handler/AreaHandler.java
+++ addons/binding/org.openhab.binding.omnilink/src/main/java/org/openhab/binding/omnilink/handler/AreaHandler.java
@@ -9,18 +9,17 @@ import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.UID;
 import org.eclipse.smarthome.core.types.Command;
-import org.eclipse.smarthome.core.types.RefreshType;
 import org.eclipse.smarthome.core.types.UnDefType;
 import org.openhab.binding.omnilink.OmnilinkBindingConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.digitaldan.jomnilinkII.Message;
 import com.digitaldan.jomnilinkII.OmniInvalidResponseException;
 import com.digitaldan.jomnilinkII.OmniUnknownMessageTypeException;
+import com.digitaldan.jomnilinkII.MessageTypes.ObjectStatus;
 import com.digitaldan.jomnilinkII.MessageTypes.SecurityCodeValidation;
 import com.digitaldan.jomnilinkII.MessageTypes.statuses.AreaStatus;
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
 
 public class AreaHandler extends AbstractOmnilinkHandler {
     private Logger logger = LoggerFactory.getLogger(AreaHandler.class);
@@ -32,23 +31,7 @@ public class AreaHandler extends AbstractOmnilinkHandler {
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
         logger.debug("handleCommand: {}, command: {}", channelUID, command);
-        String[] channelParts = channelUID.getAsString().split(UID.SEPARATOR);
-        if (command instanceof RefreshType) {
-            Futures.addCallback(getOmnilinkBridgeHander().getAreaStatus(Integer.parseInt(channelParts[2])),
-                    new FutureCallback<AreaStatus>() {
-
-                        @Override
-                        public void onFailure(Throwable arg0) {
-                            logger.error("Failure getting status", arg0);
-                        }
-
-                        @Override
-                        public void onSuccess(AreaStatus status) {
-                            logger.debug("handle area status: {}", status);
-                            handleAreaEvent(status);
-                        }
-                    });
-        } else if (command instanceof StringType) {
+        if (command instanceof StringType) {
             int mode;
             switch (channelUID.getId()) {
                 case "disarm":
@@ -75,7 +58,6 @@ public class AreaHandler extends AbstractOmnilinkHandler {
                 default:
                     mode = -1;
             }
-
             int areaNumber;
             if (getThing().getConfiguration()
                     .get(OmnilinkBindingConstants.THING_PROPERTIES_NUMBER) instanceof BigDecimal) {
@@ -145,6 +127,20 @@ public class AreaHandler extends AbstractOmnilinkHandler {
                 updateState(new ChannelUID(thing.getUID(), OmnilinkBindingConstants.CHANNEL_AREA_ALARMS[i]),
                         OnOffType.OFF);
             }
+        }
+    }
+
+    @Override
+    public void channelLinked(ChannelUID channelUID) {
+        logger.debug("channel linked: {}", channelUID);
+        String[] channelParts = channelUID.getAsString().split(UID.SEPARATOR);
+        int areaId = Integer.parseInt(channelParts[2]);
+        ObjectStatus objStatus;
+        try {
+            objStatus = getOmnilinkBridgeHander().requestObjectStatusNew(Message.OBJ_TYPE_AREA, areaId, areaId, false);
+            handleAreaEvent((AreaStatus) objStatus.getStatuses()[0]);
+        } catch (OmniInvalidResponseException | OmniUnknownMessageTypeException | BridgeOfflineException e) {
+            logger.debug("Unexpected exception refreshing area:", e);
         }
     }
 }
