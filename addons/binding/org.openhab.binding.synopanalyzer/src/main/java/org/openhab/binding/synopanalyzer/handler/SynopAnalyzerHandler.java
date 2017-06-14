1805af8ef1e2aca8937526c460e50e3b77209984
Gaël L'hopital <glhopital@gmail.com>
2017-06-14 10:28:55 +0200

    Removed utilisation of org.apache.http.client
    Signed-off-by: Gaël L'hopital <glhopital@gmail.com>
    
    Correcting lost typo
    
    Signed-off-by: Gaël L'hopital <glhopital@gmail.com>

--- addons/binding/org.openhab.binding.synopanalyzer/src/main/java/org/openhab/binding/synopanalyzer/handler/SynopAnalyzerHandler.java
+++ addons/binding/org.openhab.binding.synopanalyzer/src/main/java/org/openhab/binding/synopanalyzer/handler/SynopAnalyzerHandler.java
@@ -12,24 +12,17 @@ import static org.openhab.binding.synopanalyzer.SynopAnalyzerBindingConstants.*;
 import static org.openhab.binding.synopanalyzer.internal.UnitUtils.*;
 
 import java.io.IOException;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.List;
 import java.util.TimeZone;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.message.BasicNameValuePair;
 import org.eclipse.smarthome.core.library.types.DateTimeType;
 import org.eclipse.smarthome.core.library.types.DecimalType;
 import org.eclipse.smarthome.core.library.types.StringType;
-import org.eclipse.smarthome.core.thing.Channel;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingStatus;
@@ -37,6 +30,8 @@ import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.RefreshType;
 import org.eclipse.smarthome.core.types.State;
+import org.eclipse.smarthome.core.types.UnDefType;
+import org.eclipse.smarthome.io.net.http.HttpUtil;
 import org.openhab.binding.synopanalyzer.config.SynopAnalyzerConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -55,8 +50,9 @@ import com.nwpi.synop.SynopShip;
  */
 public class SynopAnalyzerHandler extends BaseThingHandler {
 
-    private static final String OGIMET_SYNOP_PATH = "/cgi-bin/getsynop";
-    private static final String OGIMET_URL = "www.ogimet.com";
+    private static final String OGIMET_SYNOP_PATH = "http://www.ogimet.com/cgi-bin/getsynop?";
+    private static final int REQUEST_TIMEOUT = 5000;
+    private static final SimpleDateFormat SYNOP_DATE_FORMAT = new SimpleDateFormat("yyyyMMddHH00");
 
     private Logger logger = LoggerFactory.getLogger(SynopAnalyzerHandler.class);
 
@@ -75,7 +71,7 @@ public class SynopAnalyzerHandler extends BaseThingHandler {
                 configuration.refreshInterval, configuration.stationId);
 
         executionJob = scheduler.scheduleWithFixedDelay(() -> {
-            updateSynop();
+            updateSynopChannels();
         }, 1, configuration.refreshInterval, TimeUnit.MINUTES);
         super.initialize();
 
@@ -88,20 +84,22 @@ public class SynopAnalyzerHandler extends BaseThingHandler {
 
         for (int backInTime = 0; backInTime < 24 && !message.startsWith(configuration.stationId); backInTime++) {
             observationTime.roll(Calendar.HOUR, false);
-            // Calendar observationTime = currentTime;
-            // observationTime.ro.add(Calendar.HOUR, backInTime * -1);
+
             String url = forgeURL(observationTime);
+
             try {
-                message = IOUtils.toString(new URL(url));
+                message = HttpUtil.executeUrl("GET", url, REQUEST_TIMEOUT);
+                logger.debug(message);
             } catch (IOException e) {
-                logger.warn("Erroneous URL while preparing Synop request");
+                logger.warn("Synop request timedout : {}", e.getMessage());
                 updateStatus(ThingStatus.OFFLINE);
                 return null;
             }
+
         }
 
         if (message.startsWith(configuration.stationId)) {
-            logger.debug("Received a valid synop message");
+            logger.debug("Valid Synop message received");
             updateStatus(ThingStatus.ONLINE);
 
             String[] messageParts = message.split(",");
@@ -116,83 +114,73 @@ public class SynopAnalyzerHandler extends BaseThingHandler {
         }
     }
 
-    private void updateSynop() {
+    private void updateSynopChannels() {
         logger.debug("Updating device channels");
 
         Synop synop = getLastAvailableSynop();
         if (synop != null) {
-            for (Channel channel : getThing().getChannels()) {
+            getThing().getChannels().forEach(channel -> {
                 String channelId = channel.getUID().getId();
-                State state = null;
-
-                switch (channelId) {
-                    case HORIZONTAL_VISIBILITY:
-                        state = new StringType(synop.getHorizontalVisibility());
-                        break;
-                    case OCTA:
-                        state = new DecimalType(synop.getOcta());
-                        break;
-                    case OVERCAST:
-                        state = new StringType(synop.getOvercast());
-                        break;
-                    case PRESSURE:
-                        state = new DecimalType(synop.getPressure());
-                        break;
-                    case TEMPERATURE:
-                        state = new DecimalType(synop.getTemperature());
-                        break;
-                    case WIND_ANGLE:
-                        state = new DecimalType(synop.getWindDirection());
-                        break;
-                    case WIND_DIRECTION:
-                        int angle = synop.getWindDirection();
-                        String direction = getWindDirection(angle);
-                        state = new StringType(direction);
-                        break;
-                    case WIND_SPEED_MS:
-                        if (synop.getWindUnit().equalsIgnoreCase("m/s")) {
-                            state = new DecimalType(synop.getWindSpeed());
-                        } else {
-                            Double kmhSpeed = knotsToKmh(new Double(synop.getWindSpeed()));
-                            state = new DecimalType(kmhToMps(kmhSpeed));
-                        }
-                        break;
-                    case WIND_SPEED_KNOTS:
-                        if (synop.getWindUnit().equalsIgnoreCase("knots")) {
-                            state = new DecimalType(synop.getWindSpeed());
-                        } else {
-                            Double kmhSpeed = mpsToKmh(new Double(synop.getWindSpeed()));
-                            Double knotSpeed = kmhToKnots(kmhSpeed);
-                            state = new DecimalType(knotSpeed);
-                        }
-                        break;
-                    case WIND_SPEED_BEAUFORT:
-                        Double kmhSpeed;
-                        if (synop.getWindUnit().equalsIgnoreCase("m/s")) {
-                            kmhSpeed = mpsToKmh(new Double(synop.getWindSpeed()));
-                        } else {
-                            kmhSpeed = knotsToKmh(new Double(synop.getWindSpeed()));
-                        }
-                        Double beaufort = kmhToBeaufort(kmhSpeed);
-                        state = new DecimalType(beaufort);
-                        break;
-                    case TIME_UTC:
-                        Calendar observationTime = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-                        observationTime.set(Calendar.DAY_OF_MONTH, synop.getDay());
-                        observationTime.set(Calendar.HOUR_OF_DAY, synop.getHour());
-                        observationTime.set(Calendar.MINUTE, 0);
-                        observationTime.set(Calendar.SECOND, 0);
-                        observationTime.set(Calendar.MILLISECOND, 0);
-                        state = new DateTimeType(observationTime);
-                        break;
-                    default:
-                        logger.debug("Unsupported channel Id '{}'", channelId);
-                        break;
-                }
-                updateState(channel.getUID(), state);
-            }
+                updateState(channelId, getChannelState(channelId, synop));
+            });
         }
+    }
 
+    private State getChannelState(String channelId, Synop synop) {
+
+        switch (channelId) {
+            case HORIZONTAL_VISIBILITY:
+                return new StringType(synop.getHorizontalVisibility());
+            case OCTA:
+                return new DecimalType(synop.getOcta());
+            case OVERCAST:
+                return new StringType(synop.getOvercast());
+            case PRESSURE:
+                return new DecimalType(synop.getPressure());
+            case TEMPERATURE:
+                return new DecimalType(synop.getTemperature());
+            case WIND_ANGLE:
+                return new DecimalType(synop.getWindDirection());
+            case WIND_DIRECTION:
+                int angle = synop.getWindDirection();
+                String direction = getWindDirection(angle);
+                return new StringType(direction);
+            case WIND_SPEED_MS:
+                if (synop.getWindUnit().equalsIgnoreCase("m/s")) {
+                    return new DecimalType(synop.getWindSpeed());
+                } else {
+                    Double kmhSpeed = knotsToKmh(new Double(synop.getWindSpeed()));
+                    return new DecimalType(kmhToMps(kmhSpeed));
+                }
+            case WIND_SPEED_KNOTS:
+                if (synop.getWindUnit().equalsIgnoreCase("knots")) {
+                    return new DecimalType(synop.getWindSpeed());
+                } else {
+                    Double kmhSpeed = mpsToKmh(new Double(synop.getWindSpeed()));
+                    Double knotSpeed = kmhToKnots(kmhSpeed);
+                    return new DecimalType(knotSpeed);
+                }
+            case WIND_SPEED_BEAUFORT:
+                Double kmhSpeed;
+                if (synop.getWindUnit().equalsIgnoreCase("m/s")) {
+                    kmhSpeed = mpsToKmh(new Double(synop.getWindSpeed()));
+                } else {
+                    kmhSpeed = knotsToKmh(new Double(synop.getWindSpeed()));
+                }
+                Double beaufort = kmhToBeaufort(kmhSpeed);
+                return new DecimalType(beaufort);
+            case TIME_UTC:
+                Calendar observationTime = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+                observationTime.set(Calendar.DAY_OF_MONTH, synop.getDay());
+                observationTime.set(Calendar.HOUR_OF_DAY, synop.getHour());
+                observationTime.set(Calendar.MINUTE, 0);
+                observationTime.set(Calendar.SECOND, 0);
+                observationTime.set(Calendar.MILLISECOND, 0);
+                return new DateTimeType(observationTime);
+            default:
+                logger.error("Unsupported channel Id '{}'", channelId);
+                return UnDefType.UNDEF;
+        }
     }
 
     private Synop createSynopObject(String synopMessage) {
@@ -207,17 +195,12 @@ public class SynopAnalyzerHandler extends BaseThingHandler {
     }
 
     private String forgeURL(Calendar currentTime) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHH00");
-        String beginDate = simpleDateFormat.format(currentTime.getTime());
+        String beginDate = SYNOP_DATE_FORMAT.format(currentTime.getTime());
 
-        List<NameValuePair> qparams = new ArrayList<>();
-        qparams.add(new BasicNameValuePair("block", configuration.stationId));
-        qparams.add(new BasicNameValuePair("begin", beginDate));
+        StringBuilder url = new StringBuilder().append(OGIMET_SYNOP_PATH).append("block=")
+                .append(configuration.stationId).append("&begin=").append(beginDate);
 
-        URIBuilder builder = new URIBuilder().setScheme("http").setHost(OGIMET_URL).setPath(OGIMET_SYNOP_PATH)
-                .setParameters(qparams);
-
-        return builder.toString();
+        return url.toString();
     }
 
     @Override
@@ -231,7 +214,7 @@ public class SynopAnalyzerHandler extends BaseThingHandler {
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
         if (command == RefreshType.REFRESH) {
-            updateSynop();
+            updateSynopChannels();
         }
     }
 
