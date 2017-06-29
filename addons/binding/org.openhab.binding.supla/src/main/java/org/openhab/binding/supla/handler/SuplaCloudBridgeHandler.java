d1cb31459a1e4a214d90064ad95ffab8ede8a5a3
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-06-29 22:36:47 +0200

    Making discovery service looks pretty finally...

--- addons/binding/org.openhab.binding.supla/src/main/java/org/openhab/binding/supla/handler/SuplaCloudBridgeHandler.java
+++ addons/binding/org.openhab.binding.supla/src/main/java/org/openhab/binding/supla/handler/SuplaCloudBridgeHandler.java
@@ -6,36 +6,17 @@ import org.eclipse.smarthome.core.thing.ThingStatus;
 import org.eclipse.smarthome.core.thing.binding.BaseBridgeHandler;
 import org.eclipse.smarthome.core.types.Command;
 import org.openhab.binding.supla.SuplaCloudConfiguration;
-import org.openhab.binding.supla.internal.api.IoDevicesManager;
 import org.openhab.binding.supla.internal.di.ApplicationContext;
-import org.openhab.binding.supla.internal.discovery.SuplaDiscoveryService;
-import org.openhab.binding.supla.internal.supla.entities.SuplaChannel;
-import org.openhab.binding.supla.internal.supla.entities.SuplaIoDevice;
-import org.openhab.binding.supla.internal.supla.entities.SuplaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ScheduledFuture;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-import static java.lang.String.format;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.openhab.binding.supla.SuplaBindingConstants.RELAY_CHANNEL_TYPE;
-
-@SuppressWarnings("unused")
 public final class SuplaCloudBridgeHandler extends BaseBridgeHandler {
     private final Logger logger = LoggerFactory.getLogger(SuplaCloudBridgeHandler.class);
 
     private SuplaCloudConfiguration configuration;
     private ApplicationContext applicationContext;
-
-    private ScheduledFuture<?> pollingJob;
-    private PoolingRunnable poolingRunnable = new PoolingRunnable();
-    private SuplaDiscoveryService discoveryService;
 
     public SuplaCloudBridgeHandler(Bridge bridge) {
         super(bridge);
@@ -52,7 +33,6 @@ public final class SuplaCloudBridgeHandler extends BaseBridgeHandler {
         logger.debug("Initializing SuplaCloudBridgeHandler");
         this.configuration = getConfigAs(SuplaCloudConfiguration.class);
         this.applicationContext = new ApplicationContext(configuration.toSuplaCloudServer());
-        startAutomaticRefresh();
         updateStatus(ThingStatus.ONLINE);
     }
 
@@ -60,103 +40,4 @@ public final class SuplaCloudBridgeHandler extends BaseBridgeHandler {
         return Optional.ofNullable(applicationContext);
     }
 
-    private void startAutomaticRefresh() {
-        if (pollingJob == null || pollingJob.isCancelled()) {
-            pollingJob = scheduler.scheduleAtFixedRate(poolingRunnable, 0, configuration.refreshInterval, SECONDS);
-        }
-    }
-
-    //
-    // Service Discovery
-    //
-
-    public void registerDiscoveryService(SuplaDiscoveryService discoveryService) {
-        logger.trace("Register Discovery Service {}", discoveryService);
-        this.discoveryService = checkNotNull(discoveryService);
-    }
-
-    public void unregisterDiscoveryService() {
-        logger.trace("Unregister Discovery Service");
-        discoveryService = null;
-    }
-
-    private final class PoolingRunnable implements Runnable {
-        private final Logger logger = LoggerFactory.getLogger(PoolingRunnable.class);
-
-        @Override
-        public void run() {
-            logger.debug("Starting PoolingRunnable job...");
-            final IoDevicesManager ioDevicesManager = applicationContext.getIoDevicesManager();
-            final List<SuplaIoDevice> list = ioDevicesManager.obtainIoDevices();
-            if (list == null || list.size() == 0) {
-                logger.trace("There are no SuplaIoDevices on {}", applicationContext.getSuplaCloudServer());
-                return;
-            }
-            if (SuplaCloudBridgeHandler.this.discoveryService != null) {
-                logger.debug("Has discovery service. Send all ({}) SuplaIoDevices to it", list.size());
-                list.forEach(device -> discoveryService.addSuplaThing(findThingType(device), device.getId(),
-                        findThingLabel(device), buildThingProperties(device)));
-            }
-        }
-    }
-
-    private String findThingType(SuplaIoDevice device) {
-        final long relayChannelsCount = findRelayChannelsCount(device);
-        if (relayChannelsCount == 2) {
-//            return TWO_CHANNEL_RELAY_THING_ID;
-        } else if (relayChannelsCount == 1) {
-//            return ONE_CHANNEL_RELAY_THING_ID;
-        } else {
-            throw new RuntimeException(format("relayChannelsCount = %s", relayChannelsCount));
-        }
-        throw new UnsupportedOperationException();
-    }
-
-    private long findRelayChannelsCount(SuplaIoDevice device) {
-        return device.getChannels()
-                .stream()
-                .map(SuplaChannel::getType)
-                .map(SuplaType::getName)
-                .filter(RELAY_CHANNEL_TYPE::equals)
-                .count();
-    }
-
-    private String findThingLabel(SuplaIoDevice device) {
-        final StringBuilder sb = new StringBuilder();
-
-        final String name = device.getName();
-        if (isValidString(name)) {
-            logger.trace("Using name ad ID for {}", device);
-            sb.append(name);
-        }
-        final String comment = device.getComment();
-        if (isValidString(comment)) {
-            logger.trace("Using comment ad ID for {}", device);
-            sb.append("(").append(comment).append(")");
-        }
-        final String primaryLabel = sb.toString();
-        if (isValidString(primaryLabel)) {
-            return primaryLabel;
-        } else {
-            logger.trace("Using gUID ad ID for {}", device);
-            return device.getGuid();
-        }
-    }
-
-    private boolean isValidString(String string) {
-        return string != null && !string.trim().isEmpty();
-    }
-
-    private Map<String, Object> buildThingProperties(SuplaIoDevice device) {
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("supla-cloud-id", (int) device.getId());
-
-        // TODO support multiple channels
-//        final SuplaChannel firstChannel = device.getChannels().iterator().next();
-//        checkArgument(RELAY_CHANNEL_TYPE.equals(firstChannel.getType().getName()),
-//                "Wrong channel name! Expected %s got %s.", RELAY_CHANNEL_TYPE, firstChannel.getType().getName());
-//        properties.put(SWITCH_1_CHANNEL, String.valueOf(firstChannel.getId()));
-
-        return properties;
-    }
 }
--- addons/binding/org.openhab.binding.supla/src/main/java/org/openhab/binding/supla/internal/discovery/SuplaDiscoveryService.java
+++ addons/binding/org.openhab.binding.supla/src/main/java/org/openhab/binding/supla/internal/discovery/SuplaDiscoveryService.java
@@ -1,28 +1,38 @@
 package org.openhab.binding.supla.internal.discovery;
 
+import com.google.common.collect.ImmutableMap;
 import org.eclipse.smarthome.config.discovery.AbstractDiscoveryService;
 import org.eclipse.smarthome.config.discovery.DiscoveryResult;
 import org.eclipse.smarthome.config.discovery.DiscoveryResultBuilder;
 import org.eclipse.smarthome.core.thing.ThingUID;
 import org.openhab.binding.supla.handler.SuplaCloudBridgeHandler;
+import org.openhab.binding.supla.internal.di.ApplicationContext;
+import org.openhab.binding.supla.internal.supla.entities.SuplaIoDevice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
-import static org.openhab.binding.supla.SuplaBindingConstants.SUPPORTED_THING_TYPES_UIDS;
+import static org.openhab.binding.supla.SuplaBindingConstants.*;
 
 public class SuplaDiscoveryService extends AbstractDiscoveryService {
     private final Logger logger = LoggerFactory.getLogger(SuplaDiscoveryService.class);
     private final SuplaCloudBridgeHandler suplaCloudBridgeHandler;
+    private final ApplicationContext applicationContext;
 
+    @SuppressWarnings("ConstantConditions")
     public SuplaDiscoveryService(SuplaCloudBridgeHandler suplaCloudBridgeHandler) {
         super(SUPPORTED_THING_TYPES_UIDS, 10, false);
         this.suplaCloudBridgeHandler = suplaCloudBridgeHandler;
+        this.applicationContext = suplaCloudBridgeHandler.getApplicationContext().get();
     }
 
-    public void addSuplaThing(String thingType, long thingID, String label, Map<String, Object> properties) {
-        addThing(suplaCloudBridgeHandler.getThing().getUID(), thingType, thingID, label, properties);
+    private void addSuplaThing(SuplaIoDevice device) {
+        addThing(suplaCloudBridgeHandler.getThing().getUID(),
+                SUPLA_DEVICE_THING_ID,
+                device.getId(),
+                buildThingLabel(device),
+                buildThingProperties(device));
     }
 
     private void addThing(ThingUID bridgeUID, String thingType, long thingID, String label, Map<String, Object> properties) {
@@ -51,17 +61,42 @@ public class SuplaDiscoveryService extends AbstractDiscoveryService {
         }
     }
 
-    public void activate() {
-        suplaCloudBridgeHandler.registerDiscoveryService(this);
-    }
-
-    @Override
-    public void deactivate() {
-        suplaCloudBridgeHandler.unregisterDiscoveryService();
-    }
-
     @Override
     protected void startScan() {
-        // Scan will be done by bridge
+        applicationContext.getIoDevicesManager()
+                .obtainIoDevices()
+                .forEach(this::addSuplaThing);
+    }
+
+    private String buildThingLabel(SuplaIoDevice device) {
+        final StringBuilder sb = new StringBuilder();
+
+        final String name = device.getName();
+        if (isValidString(name)) {
+            logger.trace("Using name ad ID for {}", device);
+            sb.append(name);
+        }
+        final String comment = device.getComment();
+        if (isValidString(comment)) {
+            logger.trace("Using comment ad ID for {}", device);
+            sb.append("(").append(comment).append(")");
+        }
+        final String primaryLabel = sb.toString();
+        if (isValidString(primaryLabel)) {
+            return primaryLabel;
+        } else {
+            logger.trace("Using gUID ad ID for {}", device);
+            return device.getGuid();
+        }
+    }
+
+    private boolean isValidString(String string) {
+        return string != null && !string.trim().isEmpty();
+    }
+
+    private Map<String, Object> buildThingProperties(SuplaIoDevice device) {
+        return ImmutableMap.<String, Object>builder()
+                .put(SUPLA_IO_DEVICE_ID, (int) device.getId())
+                .build();
     }
 }
