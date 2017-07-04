2cb83a1cf7cbe3ba9aab1d4a0428287b7557a3bc
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-04 23:43:26 +0200

    Removing time from SuplaServerInfo.java

--- addons/binding/org.openhab.binding.supla/src/main/java/org/openhab/binding/supla/internal/supla/entities/SuplaServerInfo.java
+++ addons/binding/org.openhab.binding.supla/src/main/java/org/openhab/binding/supla/internal/supla/entities/SuplaServerInfo.java
@@ -1,10 +1,7 @@
 package org.openhab.binding.supla.internal.supla.entities;
 
-import java.time.LocalDateTime;
-
 public final class SuplaServerInfo {
     private String address;
-    private LocalDateTime time;
 
     public String getAddress() {
         return address;
@@ -14,14 +11,6 @@ public final class SuplaServerInfo {
         this.address = address;
     }
 
-    public LocalDateTime getTime() {
-        return time;
-    }
-
-    public void setTime(LocalDateTime time) {
-        this.time = time;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -29,8 +18,7 @@ public final class SuplaServerInfo {
 
         SuplaServerInfo that = (SuplaServerInfo) o;
 
-        if (address != null ? !address.equals(that.address) : that.address != null) return false;
-        return time != null ? time.equals(that.time) : that.time == null;
+        return address != null ? address.equals(that.address) : that.address == null;
     }
 
     @Override
@@ -42,7 +30,6 @@ public final class SuplaServerInfo {
     public String toString() {
         return "SuplaServerInfo{" +
                 "address='" + address + '\'' +
-                ", time=" + time +
                 '}';
     }
 }
