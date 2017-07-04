559a3bb17df6cd381c7c840d9bf7ffecdb75e360
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-04 23:47:33 +0200

    JettyHttpExecutor.java TODO

---
+++ addons/binding/org.openhab.binding.supla/src/main/java/org/openhab/binding/supla/internal/server/http/JettyHttpExecutor.java
@@ -0,0 +1,19 @@
+package org.openhab.binding.supla.internal.server.http;
+
+// TODO
+public final class JettyHttpExecutor implements HttpExecutor {
+    @Override
+    public Response get(Request request) {
+        return null;
+    }
+
+    @Override
+    public Response post(Request request, Body body) {
+        return null;
+    }
+
+    @Override
+    public Response patch(Request request, Body body) {
+        return null;
+    }
+}
