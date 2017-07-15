40f1c103b44a6a0ed9bc8a692dcc2e387834a14e
mjagdis <mjagdis@eris-associates.co.uk>
2017-07-15 20:06:14 +0100

    [RFXCOM] Handle Somfy/Rfy extended responses (#2400)
    * [RFXCOM] Handle Somfy/Rfy extended responses
    
    Somfy/Rfy commands return extended responses using interface messages
    rather than simple transmitter message responses. We don't support them
    (and don't need to?) but we do need to _survive_ them at least.
    
    The response to a START_RECEIVER causes (re)send of the first message in
    the queue after going online. Any other interface message response means
    the queue head message has been dealt with and we can move on to the
    next.
    
    Signed-off-by: Mike Jagdis <mjagdis@eris-associates.co.uk> (github: mjagdis)

--- addons/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/handler/RFXComBridgeHandler.java
+++ addons/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/handler/RFXComBridgeHandler.java
@@ -275,10 +275,12 @@ public class RFXComBridgeHandler extends BaseBridgeHandler {
                             connector.sendMessage(RFXComMessageFactory.CMD_START_RECEIVER);
                         }
                     } else if (msg.subType == SubType.START_RECEIVER) {
+                        updateStatus(ThingStatus.ONLINE);
                         logger.debug("Start TX of any queued messages");
                         transmitQueue.send();
-
-                        updateStatus(ThingStatus.ONLINE);
+                    } else {
+                        logger.debug("Interface response received: {}", msg);
+                        transmitQueue.sendNext();
                     }
                 } else if (message instanceof RFXComTransmitterMessage) {
                     RFXComTransmitterMessage resp = (RFXComTransmitterMessage) message;
