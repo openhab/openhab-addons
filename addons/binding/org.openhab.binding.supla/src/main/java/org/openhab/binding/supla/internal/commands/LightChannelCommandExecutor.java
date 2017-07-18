cfa3bbc342c64d5428f8da8ff285c4e77e442657
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-18 19:32:32 +0200

    LightChannelCommandExecutor extends SwitchChannelCommandExecutor, because it does the same shit as SwitchCommand

--- addons/binding/org.openhab.binding.supla/src/main/java/org/openhab/binding/supla/internal/commands/LightChannelCommandExecutor.java
+++ addons/binding/org.openhab.binding.supla/src/main/java/org/openhab/binding/supla/internal/commands/LightChannelCommandExecutor.java
@@ -1,49 +1,10 @@
 package org.openhab.binding.supla.internal.commands;
 
-import org.eclipse.smarthome.core.library.types.OnOffType;
-import org.eclipse.smarthome.core.types.Command;
-import org.eclipse.smarthome.core.types.RefreshType;
-import org.eclipse.smarthome.core.types.State;
 import org.openhab.binding.supla.internal.api.ChannelManager;
 import org.openhab.binding.supla.internal.supla.entities.SuplaChannel;
-import org.openhab.binding.supla.internal.supla.entities.SuplaChannelStatus;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.function.Consumer;
-
-import static com.google.common.base.Preconditions.checkNotNull;
-import static org.eclipse.smarthome.core.library.types.OnOffType.ON;
-
-final class LightChannelCommandExecutor implements CommandExecutor {
-    private final Logger logger = LoggerFactory.getLogger(LightChannelCommandExecutor.class);
-    private final ChannelManager channelManager;
-    private final SuplaChannel suplaChannel;
-
+final class LightChannelCommandExecutor extends SwitchChannelCommandExecutor implements CommandExecutor {
     LightChannelCommandExecutor(ChannelManager channelManager, SuplaChannel suplaChannel) {
-        this.channelManager = checkNotNull(channelManager);
-        this.suplaChannel = suplaChannel;
-    }
-
-    @Override
-    public void execute(Consumer<? super State> updateState, Command command) {
-        if (command instanceof OnOffType) {
-            final OnOffType switchCommand = (OnOffType) command;
-            if(switchCommand == ON) {
-                channelManager.turnOn(suplaChannel);
-            } else {
-                channelManager.turnOff(suplaChannel);
-            }
-        } else if (command instanceof RefreshType) {
-            channelManager.obtainChannelStatus(suplaChannel)
-                    .map(this::getState)
-                    .ifPresent(updateState);
-        } else {
-            logger.debug("Don't know how to handle {} for {}", command.getClass().getSimpleName(), this.getClass().getSimpleName());
-        }
-    }
-
-    private OnOffType getState(SuplaChannelStatus status) {
-        return status.isEnabled() ? OnOffType.ON : OnOffType.OFF;
+        super(channelManager, suplaChannel);
     }
 }
