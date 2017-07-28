c84649ef0d75e075737d67f2c1e4ccd1d97a57eb
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-28 23:54:57 +0200

    Tests for obtaining channel status

--- addons/binding/org.openhab.binding.supla/src/test/java/org/openhab/binding/supla/internal/supla/api/SuplaChannelManagerTest.java
+++ addons/binding/org.openhab.binding.supla/src/test/java/org/openhab/binding/supla/internal/supla/api/SuplaChannelManagerTest.java
@@ -13,9 +13,13 @@ import org.openhab.binding.supla.internal.http.Request;
 import org.openhab.binding.supla.internal.http.Response;
 import org.openhab.binding.supla.internal.mappers.JsonMapper;
 import org.openhab.binding.supla.internal.supla.entities.SuplaChannel;
+import org.openhab.binding.supla.internal.supla.entities.SuplaChannelStatus;
 import org.openhab.binding.supla.internal.supla.entities.SuplaFunction;
 import org.openhab.binding.supla.internal.supla.entities.SuplaType;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -28,6 +32,9 @@ public class SuplaChannelManagerTest extends SuplaTest {
     @Mock
     private JsonMapper jsonMapper;
 
+    private final SuplaChannel channel = new SuplaChannel(10, 11, "cpa", new SuplaType(1, "xxx"), new SuplaFunction(3, "yyy"));
+    private final Request request = new Request("/channels/10");
+
     @Before
     public void init() {
         manager = new SuplaChannelManager(httpExecutor, jsonMapper);
@@ -37,8 +44,6 @@ public class SuplaChannelManagerTest extends SuplaTest {
     public void shouldRunPatchMethodToTurnOnChannel() {
 
         // given
-        final SuplaChannel channel = new SuplaChannel(10, 11, "cpa", new SuplaType(1, "xxx"), new SuplaFunction(3, "yyy"));
-        final Request request = new Request("/channels/10");
         final JsonBody body = new JsonBody(ImmutableMap.<String, String>builder().put("action", "turn-on").build(), jsonMapper);
 
         when(httpExecutor.patch(request, body)).thenReturn(new Response(200, "resp"));
@@ -54,8 +59,6 @@ public class SuplaChannelManagerTest extends SuplaTest {
     public void shouldRunPatchMethodToTurnOffChannel() {
 
         // given
-        final SuplaChannel channel = new SuplaChannel(10, 11, "cpa", new SuplaType(1, "xxx"), new SuplaFunction(3, "yyy"));
-        final Request request = new Request("/channels/10");
         final JsonBody body = new JsonBody(ImmutableMap.<String, String>builder().put("action", "turn-off").build(), jsonMapper);
 
         when(httpExecutor.patch(request, body)).thenReturn(new Response(200, "resp"));
@@ -66,4 +69,35 @@ public class SuplaChannelManagerTest extends SuplaTest {
         // then
         verify(httpExecutor).patch(request, body);
     }
+
+    @Test
+    public void shouldObtainChannelStatus() {
+
+        // given
+        final String response = "resp";
+        when(httpExecutor.get(request)).thenReturn(new Response(200, response));
+        when(jsonMapper.to(SuplaChannelStatus.class, response)).thenReturn(new SuplaChannelStatus(true, true));
+
+        // when
+        manager.obtainChannelStatus(channel);
+
+        // then
+        verify(httpExecutor).get(request);
+        verify(jsonMapper).to(SuplaChannelStatus.class, response);
+    }
+
+    @Test
+    public void shouldReturnEmptyIfThereIsNoChannelStatus() {
+
+        // given
+        when(httpExecutor.get(request)).thenReturn(new Response(503, "error"));
+
+        // when
+        final Optional<SuplaChannelStatus> status = manager.obtainChannelStatus(channel);
+
+        // then
+        verify(httpExecutor).get(request);
+        assertThat(status).isEmpty();
+    }
+
 }
