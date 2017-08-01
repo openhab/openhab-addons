f83f210d9bc6ee19e9803e0cf00fb976c7d9b6f0
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-01 23:47:18 +0200

    Not mocking jsonMapper

--- addons/binding/org.openhab.binding.supla/src/test/java/org/openhab/binding/supla/internal/supla/api/SuplaIoDevicesManagerTest.java
+++ addons/binding/org.openhab.binding.supla/src/test/java/org/openhab/binding/supla/internal/supla/api/SuplaIoDevicesManagerTest.java
@@ -1,9 +1,8 @@
 package org.openhab.binding.supla.internal.supla.api;
 
-import com.google.common.collect.ImmutableMap;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.openhab.binding.supla.SuplaTest;
@@ -14,48 +13,46 @@ import org.openhab.binding.supla.internal.mappers.GsonMapper;
 import org.openhab.binding.supla.internal.mappers.JsonMapper;
 import org.openhab.binding.supla.internal.supla.entities.SuplaIoDevice;
 
-import java.lang.reflect.Type;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SuplaIoDevicesManagerTest extends SuplaTest {
 
-    @InjectMocks private SuplaIoDevicesManager manager;
-
-    @Mock private JsonMapper jsonMapper;
+    private SuplaIoDevicesManager manager;
     @Mock private HttpExecutor httpExecutor;
+    private final JsonMapper jsonMapper = new GsonMapper();
+
+    @Before
+    public void init() {
+        manager = new SuplaIoDevicesManager(httpExecutor, jsonMapper);
+    }
 
     @Test
     public void shouldObtainAllDevices() {
 
         // given
-        List<SuplaIoDevice> devices = new ArrayList<>();
-        devices.add(randomSuplaIoDevice());
-        devices.add(randomSuplaIoDevice());
+        final SuplaIoDevice device1 = randomSuplaIoDevice();
+        final SuplaIoDevice device2 = randomSuplaIoDevice();
 
-        given(httpExecutor.get(new Request("/iodevices"))).willReturn(new Response(200, "resp"));
-
-        Map<String, List<SuplaIoDevice>> map = ImmutableMap.<String, List<SuplaIoDevice>>builder().put("iodevices", devices).build();
-        given(jsonMapper.to(any(Type.class), eq("resp"))).willReturn(map);
+        final String json = "{\"iodevices\": ["+jsonMapper.map(device1)+", "+jsonMapper.map(device2)+"]}";
+        given(httpExecutor.get(new Request("/iodevices"))).willReturn(new Response(200, json));
 
         // when
         final List<SuplaIoDevice> suplaIoDevices = manager.obtainIoDevices();
 
         // then
-        assertThat(suplaIoDevices).isEqualTo(devices);
+        assertThat(suplaIoDevices).containsExactly(device1, device2);
     }
 
     @Test
     public void shouldReturnIoDeviceWhenHttpExecutorReturnsOneInstance() {
 
         // given
-        SuplaIoDevicesManager manager = new SuplaIoDevicesManager(httpExecutor, new GsonMapper());
-
         final int id = 100;
         final String json = "[{\"id\":" + id + ", \"channels\": []}]";
         given(httpExecutor.get(new Request("/iodevices/" + id))).willReturn(new Response(200, json));
