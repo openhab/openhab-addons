fe899cedf175e2e536ef3d4760f554ba41568729
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-02 23:37:19 +0200

    SupplaHandlerFacotry need to register service discovery

--- addons/binding/org.openhab.binding.supla/src/test/java/org/openhab/binding/supla/internal/SuplaHandlerFactoryTest.java
+++ addons/binding/org.openhab.binding.supla/src/test/java/org/openhab/binding/supla/internal/SuplaHandlerFactoryTest.java
@@ -1,6 +1,7 @@
 package org.openhab.binding.supla.internal;
 
 
+import org.eclipse.smarthome.config.discovery.DiscoveryService;
 import org.eclipse.smarthome.core.thing.Bridge;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
@@ -10,11 +11,17 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.openhab.binding.supla.handler.SuplaCloudBridgeHandler;
 import org.openhab.binding.supla.handler.SuplaIoDeviceHandler;
+import org.openhab.binding.supla.internal.discovery.SuplaDiscoveryService;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
+import java.util.Hashtable;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.openhab.binding.supla.SuplaBindingConstants.BRIDGE_THING_TYPE;
 import static org.openhab.binding.supla.SuplaBindingConstants.SUPLA_IO_DEVICE_THING_TYPE;
 
@@ -55,5 +62,24 @@ public class SuplaHandlerFactoryTest {
 
         // then
         assertThat(handler).isOfAnyClassIn(SuplaCloudBridgeHandler.class);
+    }
+
+    @Test
+    public void shouldRegisterServiceDiscoveryWhenCreatingABridge() {
+
+        // given
+        given(bridge.getThingTypeUID()).willReturn(BRIDGE_THING_TYPE);
+        factory.setBundleContext(bundleContext);
+        given(bundleContext.getBundle()).willReturn(bundle);
+        given(bundle.getBundleId()).willReturn(1L);
+
+        // when
+        factory.createHandler(bridge);
+
+        // then
+        verify(bundleContext).registerService(
+                eq(DiscoveryService.class.getName()),
+                any(SuplaDiscoveryService.class),
+                eq(new Hashtable<>()));
     }
 }
