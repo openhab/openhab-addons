e93093612e348953a84b89e1b5f52bcca8aeecb2
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-02 23:14:46 +0200

    Testing post methods

--- addons/binding/org.openhab.binding.supla/src/test/java/org/openhab/binding/supla/internal/http/OAuthApiHttpExecutorTest.java
+++ addons/binding/org.openhab.binding.supla/src/test/java/org/openhab/binding/supla/internal/http/OAuthApiHttpExecutorTest.java
@@ -8,9 +8,13 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.openhab.binding.supla.SuplaTest;
 import org.openhab.binding.supla.internal.api.TokenManager;
+import org.openhab.binding.supla.internal.mappers.JsonMapper;
 import org.openhab.binding.supla.internal.supla.entities.SuplaToken;
 
+import java.util.HashMap;
+
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.openhab.binding.supla.internal.http.CommonHeaders.authorizationHeader;
 
@@ -52,7 +56,47 @@ public class OAuthApiHttpExecutorTest extends SuplaTest {
         verify(httpExecutor).get(new Request("/api/path", ImmutableList.of(authorizationHeader(token))));
     }
 
+    @Test
+    public void shouldPassPostRequestToHttpExecutor() {
+
+        // given
+        final Header h1 = new Header("k", "v");
+        final Header h2 = new Header("k1", "v1");
+        final Request request = new Request("/path", ImmutableList.of(h1, h2));
+
+        final Body body = randomBody();
+
+        final SuplaToken token = randomToken();
+        given(tokenManager.obtainToken()).willReturn(token);
+
+        // when
+        oAuthApiHttpExecutor.post(request, body);
+
+        // then
+        verify(httpExecutor).post(new Request("/api/path", ImmutableList.of(h1, h2, authorizationHeader(token))), body);
+    }
+
+    @Test
+    public void shouldPassPostRequestToHttpExecutorWithoutHeaders() {
+
+        // given
+        final Request request = new Request("/path");
+        final Body body = randomBody();
+        final SuplaToken token = randomToken();
+        given(tokenManager.obtainToken()).willReturn(token);
+
+        // when
+        oAuthApiHttpExecutor.post(request, body);
+
+        // then
+        verify(httpExecutor).post(new Request("/api/path", ImmutableList.of(authorizationHeader(token))), body);
+    }
+
     private SuplaToken randomToken() {
         return new SuplaToken("acc", 100, "token type", "sc", "re");
+    }
+
+    private Body randomBody() {
+        return new JsonBody(new HashMap<>(), mock(JsonMapper.class));
     }
 }
