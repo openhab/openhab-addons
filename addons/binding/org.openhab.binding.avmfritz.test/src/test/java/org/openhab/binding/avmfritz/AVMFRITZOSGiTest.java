3d294411eb7afcd7475911dab37bb99368eb44b8
Christoph Weitkamp <github@christophweitkamp.de>
2017-08-07 15:26:00 +0200

    Added pure Java test classes for avmfritz binding
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

---
+++ addons/binding/org.openhab.binding.avmfritz.test/src/test/java/org/openhab/binding/avmfritz/AVMFRITZOSGiTest.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.avmfritz;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+import org.openhab.binding.avmfritz.handler.AVMFRITZHandler;
+import org.eclipse.smarthome.core.thing.Bridge;
+import org.eclipse.smarthome.core.thing.ManagedThingProvider;
+import org.eclipse.smarthome.core.thing.ThingProvider;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.binding.builder.BridgeBuilder;
+import org.eclipse.smarthome.test.java.JavaOSGiTest;
+import org.eclipse.smarthome.test.storage.VolatileStorageService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+* Tests cases for {@link AVMFRITZHandler}.
+*
+* @author Christoph Weitkamp - Initial contribution
+*/
+public class AVMFRITZOSGiTest extends JavaOSGiTest {
+
+    private static final ThingTypeUID BRIDGE_THING_TYPE_UID = new ThingTypeUID("avmfritz", "bridge");
+
+    private ManagedThingProvider managedThingProvider;
+    private final VolatileStorageService volatileStorageService = new VolatileStorageService();
+    private Bridge bridge;
+
+    @Before
+    public void setUp() {
+        registerService(volatileStorageService);
+        managedThingProvider = getService(ThingProvider.class, ManagedThingProvider.class);
+        bridge = BridgeBuilder.create(BRIDGE_THING_TYPE_UID, "1").withLabel("My Bridge").build();
+    }
+
+    @After
+    public void tearDown() {
+        managedThingProvider.remove(bridge.getUID());
+        unregisterService(volatileStorageService);
+    }
+
+    @Test
+    public void creationOfAVMFRITZHandler() {
+        assertThat(bridge.getHandler(), is(nullValue()));
+        managedThingProvider.add(bridge);
+        waitForAssert(() -> assertThat(bridge.getHandler(), is(notNullValue())));
+    }
+
+}
