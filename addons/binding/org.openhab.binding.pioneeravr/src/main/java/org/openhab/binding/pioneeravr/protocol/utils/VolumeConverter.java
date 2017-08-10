6282084d53e5957bb550169ed8b68a8533597943
Michel Mohrmann <mailme@thallos.de>
2017-08-10 21:23:28 +0200

    [PioneerAvr] Zone2 / Zone3 / HDZone Volume Control
    Signed-off-by: Michel Mohrmann <mailme@thallos.de> (github: mimo74)

--- addons/binding/org.openhab.binding.pioneeravr/src/main/java/org/openhab/binding/pioneeravr/protocol/utils/VolumeConverter.java
+++ addons/binding/org.openhab.binding.pioneeravr/src/main/java/org/openhab/binding/pioneeravr/protocol/utils/VolumeConverter.java
@@ -15,72 +15,71 @@ import java.text.DecimalFormat;
  * @author Antoine Besnard - Initial contribution
  */
 public final class VolumeConverter {
-
-    private static final String IP_CONTROL_VOLUME_FORMAT = "000";
-    private static final String IP_CONTROL_VOLUME_DEFAULT_VALUE = "000";
-
-    private static final double MAX_IP_CONTROL_VOLUME = 184;
-    private static final double MIN_DB_VOLUME = 80;
-
-    private static final DecimalFormat FORMATTER = new DecimalFormat(IP_CONTROL_VOLUME_FORMAT);
-
+    
+    private static final String[] IP_CONTROL_VOLUME_FORMAT = { "000", "00", "00", "00" };
+    private static final String[] IP_CONTROL_VOLUME_DEFAULT_VALUE = { "000", "00", "00", "00" };
+    
+    private static final double[] MAX_IP_CONTROL_VOLUME = { 184, 80, 80, 80 };
+    private static final double[] MIN_DB_VOLUME = { 80, 80, 80, 80 };
+    
     /**
      * Return the double value of the volume from the value received in the IpControl response.
      *
      * @param ipControlVolume
      * @return the volume in Db
      */
-    public static double convertFromIpControlVolumeToDb(String ipControlVolume) {
+    public static double convertFromIpControlVolumeToDb(String ipControlVolume, int zone) {
         double ipControlVolumeInt = Double.parseDouble(ipControlVolume);
-        return ((ipControlVolumeInt - 1d) / 2d) - MIN_DB_VOLUME;
+        return ((ipControlVolumeInt - 1d) / 2d) - MIN_DB_VOLUME[zone - 1];
     }
-
+    
     /**
      * Return the string parameter to send to the AVR based on the given volume.
      *
      * @param volumeDb
      * @return the volume for IpControlRequest
      */
-    public static String convertFromDbToIpControlVolume(double volumeDb) {
-        double ipControlVolume = ((MIN_DB_VOLUME + volumeDb) * 2d) + 1d;
-        return formatIpControlVolume(ipControlVolume);
+    public static String convertFromDbToIpControlVolume(double volumeDb, int zone) {
+        double ipControlVolume = ((MIN_DB_VOLUME[zone - 1] + volumeDb) * 2d) + 1d;
+        return formatIpControlVolume(ipControlVolume, zone);
     }
-
+    
     /**
      * Return the String parameter to send to the AVR based on the given persentage of the max volume level.
      *
      * @param volumePercent
      * @return the volume for IpControlRequest
      */
-    public static String convertFromPercentToIpControlVolume(double volumePercent) {
-        double ipControlVolume = 1 + (volumePercent * MAX_IP_CONTROL_VOLUME / 100);
-        return formatIpControlVolume(ipControlVolume);
+    public static String convertFromPercentToIpControlVolume(double volumePercent, int zone) {
+        double ipControlVolume = 1 + (volumePercent * MAX_IP_CONTROL_VOLUME[zone - 1] / 100);
+        return formatIpControlVolume(ipControlVolume, zone);
     }
-
+    
     /**
      * Return the percentage of the max volume levelfrom the value received in the IpControl response.
      *
      * @param ipControlVolume
      * @return the volume percentage
      */
-    public static double convertFromIpControlVolumeToPercent(String ipControlVolume) {
+    public static double convertFromIpControlVolumeToPercent(String ipControlVolume, int zone) {
         double ipControlVolumeInt = Double.parseDouble(ipControlVolume);
-        return ((ipControlVolumeInt - 1d) * 100d) / MAX_IP_CONTROL_VOLUME;
+        return ((ipControlVolumeInt - 1d) * 100d) / MAX_IP_CONTROL_VOLUME[zone - 1];
     }
-
+    
     /**
      * Format the given double value to an IpControl volume.
      *
      * @param ipControlVolume
      * @return
      */
-    private static String formatIpControlVolume(double ipControlVolume) {
-        String result = IP_CONTROL_VOLUME_DEFAULT_VALUE;
+    private static String formatIpControlVolume(double ipControlVolume, int zone) {
+        DecimalFormat FORMATTER = new DecimalFormat(IP_CONTROL_VOLUME_FORMAT[zone - 1]);
+        String result = IP_CONTROL_VOLUME_DEFAULT_VALUE[zone - 1];
         // DecimalFormat is not ThreadSafe
         synchronized (FORMATTER) {
             result = FORMATTER.format(Math.round(ipControlVolume));
         }
         return result;
     }
-
+    
 }
