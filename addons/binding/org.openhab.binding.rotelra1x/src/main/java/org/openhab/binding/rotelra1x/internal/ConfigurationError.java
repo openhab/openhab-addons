655a6ba29d91544687c63bc57cd552667df632e7
fa2k <pmb@fa2k.net>
2017-08-27 13:41:15 +0200

    Add copyright header; move and update class comment block
    Sign off by: Marius Bjørnstad <pmb@fa2k.net>

--- addons/binding/org.openhab.binding.rotelra1x/src/main/java/org/openhab/binding/rotelra1x/internal/ConfigurationError.java
+++ addons/binding/org.openhab.binding.rotelra1x/src/main/java/org/openhab/binding/rotelra1x/internal/ConfigurationError.java
@@ -1,10 +1,21 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.rotelra1x.internal;
+
+/**
+ * Exception to indicate a failure due to incorrect or missing Thing configuration.
+ *
+ * @author Marius Bjørnstad
+ */
 
 public class ConfigurationError extends Exception {
 
-    /**
-     * Exception to indicate a failure due to incorrect configuration.
-     */
     private static final long serialVersionUID = 1L;
 
     public ConfigurationError(String message) {
