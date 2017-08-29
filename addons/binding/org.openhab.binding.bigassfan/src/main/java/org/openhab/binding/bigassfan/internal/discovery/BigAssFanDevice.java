889e7fd18ab16a249b165c3aff7a42b98fa9a5e1
VelinYordanov <velin.iordanov@gmail.com>
2017-08-29 11:40:51 +0300

    Renamed bigassfan package to internal. See openhab/static-code-analysis#100
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.bigassfan/src/main/java/org/openhab/binding/bigassfan/internal/discovery/BigAssFanDevice.java
+++ addons/binding/org.openhab.binding.bigassfan/src/main/java/org/openhab/binding/bigassfan/internal/discovery/BigAssFanDevice.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.bigassfan.discovery;
+package org.openhab.binding.bigassfan.internal.discovery;
 
 /**
  * The {@link BigAssFanDevice} is responsible for storing information about a fan.
