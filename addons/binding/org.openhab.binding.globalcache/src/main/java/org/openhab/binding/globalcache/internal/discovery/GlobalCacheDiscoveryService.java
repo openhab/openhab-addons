3c96bf5e8349dd2ebfbf79903e833efee09765ef
VelinYordanov <velin.iordanov@gmail.com>
2017-08-29 14:50:20 +0300

    Renamed discovery package to internal in the globalcache binding. See openhab/static-code-analysis#100
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.globalcache/src/main/java/org/openhab/binding/globalcache/internal/discovery/GlobalCacheDiscoveryService.java
+++ addons/binding/org.openhab.binding.globalcache/src/main/java/org/openhab/binding/globalcache/internal/discovery/GlobalCacheDiscoveryService.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.globalcache.discovery;
+package org.openhab.binding.globalcache.internal.discovery;
 
 import static org.openhab.binding.globalcache.GlobalCacheBindingConstants.*;
 
