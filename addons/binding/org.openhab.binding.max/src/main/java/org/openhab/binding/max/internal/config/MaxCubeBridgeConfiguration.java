1e54fa4af9c9160ca24aa913d11648287f05e006
VelinYordanov <velin.iordanov@gmail.com>
2017-08-29 23:06:57 +0300

    Renamed the config package to internal in the max binding. (#2624)
    See openhab/static-code-analysis#100
    
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.max/src/main/java/org/openhab/binding/max/internal/config/MaxCubeBridgeConfiguration.java
+++ addons/binding/org.openhab.binding.max/src/main/java/org/openhab/binding/max/internal/config/MaxCubeBridgeConfiguration.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.max.config;
+package org.openhab.binding.max.internal.config;
 
 import org.openhab.binding.max.MaxBinding;
 
