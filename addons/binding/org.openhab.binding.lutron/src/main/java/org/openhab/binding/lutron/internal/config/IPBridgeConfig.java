2ecb63b960d10b1994cb6834539b69ec7af7ef8d
VelinYordanov <velin.iordanov@gmail.com>
2017-08-30 10:18:54 +0300

    Renamed the config package to internal in the lutron binding. (#2623)
    See openhab/static-code-analysis#100
    
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.lutron/src/main/java/org/openhab/binding/lutron/internal/config/IPBridgeConfig.java
+++ addons/binding/org.openhab.binding.lutron/src/main/java/org/openhab/binding/lutron/internal/config/IPBridgeConfig.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.lutron.config;
+package org.openhab.binding.lutron.internal.config;
 
 import org.apache.commons.lang.StringUtils;
 
