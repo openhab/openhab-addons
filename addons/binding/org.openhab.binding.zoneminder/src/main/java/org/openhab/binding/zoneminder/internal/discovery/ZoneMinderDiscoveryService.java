95d7bde890abe0b106c0f3dcf017f276621339ca
VelinYordanov <velin.iordanov@gmail.com>
2017-08-30 13:59:03 +0300

    Renamed discovery package to internal in the ZoneMinder binding. See openhab/static-code-analysis#100
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.zoneminder/src/main/java/org/openhab/binding/zoneminder/internal/discovery/ZoneMinderDiscoveryService.java
+++ addons/binding/org.openhab.binding.zoneminder/src/main/java/org/openhab/binding/zoneminder/internal/discovery/ZoneMinderDiscoveryService.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.zoneminder.discovery;
+package org.openhab.binding.zoneminder.internal.discovery;
 
 import java.util.HashMap;
 import java.util.Map;
