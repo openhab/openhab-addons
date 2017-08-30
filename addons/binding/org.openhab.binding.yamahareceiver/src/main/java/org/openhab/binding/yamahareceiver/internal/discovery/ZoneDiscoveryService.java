63b58bd809594804c6adaa6ec73e126b2ba00d0b
VelinYordanov <velin.iordanov@gmail.com>
2017-08-30 13:46:13 +0300

    Renamed discovery package to internal in the yamahareceiver binding. See openhab/static-code-analysis#100
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.yamahareceiver/src/main/java/org/openhab/binding/yamahareceiver/internal/discovery/ZoneDiscoveryService.java
+++ addons/binding/org.openhab.binding.yamahareceiver/src/main/java/org/openhab/binding/yamahareceiver/internal/discovery/ZoneDiscoveryService.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.yamahareceiver.discovery;
+package org.openhab.binding.yamahareceiver.internal.discovery;
 
 import java.util.ArrayList;
 import java.util.HashMap;
