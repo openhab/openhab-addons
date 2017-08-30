da4dc14dd323377889f94746a8b3c8dd2770a8bb
VelinYordanov <velin.iordanov@gmail.com>
2017-08-30 11:45:05 +0300

    Renamed command package to internal in the tesla binding. See openhab/static-code-analysis#100
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.tesla/src/main/java/org/openhab/binding/tesla/internal/command/TeslaCommandExtension.java
+++ addons/binding/org.openhab.binding.tesla/src/main/java/org/openhab/binding/tesla/internal/command/TeslaCommandExtension.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.tesla.command;
+package org.openhab.binding.tesla.internal.command;
 
 import static org.openhab.binding.tesla.TeslaBindingConstants.*;
 
