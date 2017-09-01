5ebdc35218991072aa81cad6937ff24906d35062
David Gräff <david.graeff@web.de>
2017-09-01 19:54:33 +1000

    Fix DHCP property displays wrong state (#2661)
    Signed-off-by: David Gräff <david.graeff@web.de>

--- addons/binding/org.openhab.binding.network/src/main/java/org/openhab/binding/network/handler/NetworkHandler.java
+++ addons/binding/org.openhab.binding.network/src/main/java/org/openhab/binding/network/handler/NetworkHandler.java
@@ -183,17 +183,17 @@ public class NetworkHandler extends BaseThingHandler implements PresenceDetectio
         presenceDetection.setRefreshInterval(handlerConfiguration.refreshInterval.longValue());
         presenceDetection.setTimeout(handlerConfiguration.timeout.intValue());
 
-        // Update properties
+        updateStatus(ThingStatus.ONLINE);
+        presenceDetection.startAutomaticRefresh(scheduler);
+
+        // Update properties (after startAutomaticRefresh, to get the correct dhcp state)
         updateProperty(NetworkBindingConstants.PROPERTY_ARP_STATE,
                 presenceDetection.arpPingMethod() != null ? presenceDetection.arpPingMethod().name() : "Disabled");
         updateProperty(NetworkBindingConstants.PROPERTY_ICMP_STATE,
                 presenceDetection.getPingMethod() != null ? presenceDetection.getPingMethod().name() : "Disabled");
-        updateProperty(NetworkBindingConstants.PROPERTY_DHCP_STATE, presenceDetection.getDhcpState());
         updateProperty(NetworkBindingConstants.PROPERTY_PRESENCE_DETECTION_TYPE, "");
         updateProperty(NetworkBindingConstants.PROPERTY_IOS_WAKEUP, presenceDetection.isIOSdevice() ? "On" : "Off");
-
-        updateStatus(ThingStatus.ONLINE);
-        presenceDetection.startAutomaticRefresh(scheduler);
+        updateProperty(NetworkBindingConstants.PROPERTY_DHCP_STATE, presenceDetection.getDhcpState());
     }
 
     // Create a new network service and apply all configurations.
