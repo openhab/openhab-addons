737e9a35a202e06b68ff08f124b504efd41b49e2
VelinYordanov <velin.iordanov@gmail.com>
2017-09-26 10:43:45 +0300

    Renamed a few packages in the loxone binding. See openhab/static-code-analysis#100
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.loxone/src/main/java/org/openhab/binding/loxone/internal/discovery/LoxoneMiniserverDiscoveryParticipant.java
+++ addons/binding/org.openhab.binding.loxone/src/main/java/org/openhab/binding/loxone/internal/discovery/LoxoneMiniserverDiscoveryParticipant.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.loxone.discovery;
+package org.openhab.binding.loxone.internal.discovery;
 
 import java.util.HashMap;
 import java.util.Map;
