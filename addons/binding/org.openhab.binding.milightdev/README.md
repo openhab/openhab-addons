a1554a32f36e09f5b13bb8e50f1bc45ea6588821
David Gräff <david.graeff@web.de>
2017-09-27 16:26:15 +0800

    Milight: Move simulated V6 bridge to own bundle. Use OSGi DS annotations
    Signed-off-by: David Gräff <david.graeff@web.de>

---
+++ addons/binding/org.openhab.binding.milightdev/README.md
@@ -0,0 +1,4 @@
+# Milight/Easybulb/Limitless development helper binding
+
+Install this binding to help if a developer asked you to install it. It provides
+a simulated Milight bridge and will log received packets.
--- addons/binding/org.openhab.binding.milightdev/src/main/java/org/openhab/binding/milight/internal/simulatedbridge/EmulatedV6Bridge.java
+++ addons/binding/org.openhab.binding.milightdev/src/main/java/org/openhab/binding/milight/internal/simulatedbridge/EmulatedV6Bridge.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.milight.internal.test;
+package org.openhab.binding.milight.internal.simulatedbridge;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
