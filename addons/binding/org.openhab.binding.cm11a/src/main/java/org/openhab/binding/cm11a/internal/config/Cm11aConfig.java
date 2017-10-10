9069e5002f5574b45acb410778ddbad77aae4781
VelinYordanov <velin.iordanov@gmail.com>
2017-10-10 16:18:11 +0300

    Renamed the config package to internal in the org.openhab.binding.cm11a binding. (#2775)
    See openhab/static-code-analysis#100
    
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.cm11a/src/main/java/org/openhab/binding/cm11a/internal/config/Cm11aConfig.java
+++ addons/binding/org.openhab.binding.cm11a/src/main/java/org/openhab/binding/cm11a/internal/config/Cm11aConfig.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.cm11a.config;
+package org.openhab.binding.cm11a.internal.config;
 
 /**
  * Configuration constants for Cm11A
