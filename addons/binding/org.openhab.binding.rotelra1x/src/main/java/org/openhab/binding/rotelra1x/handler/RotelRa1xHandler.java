c3092c24b244d8bc3caf849c4894bbb7811d0ea4
fa2k <pmb@fa2k.net>
2017-10-15 15:18:29 +0200

    Catch only unexpected, unchecked exceptions
    Sign off by: Marius Bjørnstad<pmb@fa2k.net>

--- addons/binding/org.openhab.binding.rotelra1x/src/main/java/org/openhab/binding/rotelra1x/handler/RotelRa1xHandler.java
+++ addons/binding/org.openhab.binding.rotelra1x/src/main/java/org/openhab/binding/rotelra1x/handler/RotelRa1xHandler.java
@@ -254,7 +254,7 @@ public class RotelRa1xHandler extends BaseThingHandler {
                         return;
                     }
                 }
-            } catch (Exception e) {
+            } catch (RuntimeException e) {
                 if (serialPort != null) { // If serial port is closed, it's set to null,
                                           // there is no message here,
                     logger.warn("Unexpected error", e);
