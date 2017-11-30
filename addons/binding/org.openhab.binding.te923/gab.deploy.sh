6f2f9b8160f26bd4456711b60408e1877b13f636
Gabriel Klein <gabriel.klein@nuage.ch>
2017-11-30 01:14:54 +0100

    Added TE923 binding using command line provided by http://te923.fukz.org/

--- addons/binding/org.openhab.binding.te923/gab.deploy.sh
+++
@@ -1,5 +0,0 @@
-scp target/org.openhab.binding.te923-2.2.0-SNAPSHOT.jar root@gabrielklein.internet-box.ch:/usr/share/openhab2/addons/
-
-scp target/org.openhab.binding.te923-2.2.0-SNAPSHOT.jar root@gabrielklein.internet-box.ch:/tmp/
-
-
