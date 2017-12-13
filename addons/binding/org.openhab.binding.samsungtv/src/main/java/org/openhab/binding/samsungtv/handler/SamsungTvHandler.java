be7a118a38045d94d0dbab01dd3f3f931575394e
Kai Kreuzer <kai@openhab.org>
2017-12-13 23:26:36 +0100

    adapted method signatures to breaking change from ESH
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- addons/binding/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/handler/SamsungTvHandler.java
+++ addons/binding/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/handler/SamsungTvHandler.java
@@ -210,7 +210,7 @@ public class SamsungTvHandler extends BaseThingHandler implements DiscoveryListe
 
     @Override
     public Collection<ThingUID> removeOlderResults(DiscoveryService source, long timestamp,
-            Collection<ThingTypeUID> thingTypeUIDs) {
+            Collection<ThingTypeUID> thingTypeUIDs, ThingUID bridgeUID) {
         return Collections.emptyList();
     }
 
