c5d6382c077e65228d64a2910fd61e18f59280cf
Martin van Wingerden <martinvw@mtin.nl>
2017-12-17 21:11:32 +0100

    Adding support for CURRENT messages
    Follow-up of #1271
    
    Also-by:  Jordan Cook <jordan@jc.me.uk> (github: netjordan)
    Signed-off-by: Martin van Wingerden <martinvw@mtin.nl>

--- addons/binding/org.openhab.binding.rfxcom.test/src/test/java/org/openhab/binding/rfxcom/internal/messages/RFXComCurrentMessageTest.java
+++ addons/binding/org.openhab.binding.rfxcom.test/src/test/java/org/openhab/binding/rfxcom/internal/messages/RFXComCurrentMessageTest.java
@@ -9,17 +9,39 @@
 package org.openhab.binding.rfxcom.internal.messages;
 
 import org.junit.Test;
-import org.openhab.binding.rfxcom.internal.exceptions.RFXComMessageNotImplementedException;
+import org.openhab.binding.rfxcom.internal.exceptions.RFXComException;
+
+import javax.xml.bind.DatatypeConverter;
+
+import static org.junit.Assert.assertEquals;
+import static org.openhab.binding.rfxcom.internal.messages.RFXComCurrentMessage.*;
 
 /**
  * Test for RFXCom-binding
  *
- * @author Martin van Wingerden
- * @since 1.9.0
+ * @author Martin van Wingerden - Initial Contribution
  */
 public class RFXComCurrentMessageTest {
-    @Test(expected = RFXComMessageNotImplementedException.class)
-    public void checkNotImplemented() throws Exception {
-        RFXComMessageFactory.createMessage(RFXComBaseMessage.PacketType.CURRENT);
+
+    @Test
+    public void testSomeMessages() throws RFXComException {
+        String message = "0D59010F860004001D0000000049";
+
+        final RFXComCurrentMessage msg = (RFXComCurrentMessage) RFXComMessageFactory
+                .createMessage(DatatypeConverter.parseHexBinary(message));
+        assertEquals("SubType", SubType.ELEC1, msg.subType);
+        assertEquals("Seq Number", 15, (short) (msg.seqNbr & 0xFF));
+        assertEquals("Sensor Id", "34304", msg.getDeviceId());
+        assertEquals("Count", 4, msg.count);
+        assertEquals("Channel 1", 2.9d, msg.channel1Amps, 0.01);
+        assertEquals("Channel 2", 0d, msg.channel2Amps, 0.01);
+        assertEquals("Channel 3", 0d, msg.channel3Amps, 0.01);
+        assertEquals("Signal Level", 4, msg.signalLevel);
+        assertEquals("Battery Level", 9, msg.batteryLevel);
+
+        byte[] decoded = msg.decodeMessage();
+
+        assertEquals("Message converted back", message, DatatypeConverter.printHexBinary(decoded));
+
     }
 }
