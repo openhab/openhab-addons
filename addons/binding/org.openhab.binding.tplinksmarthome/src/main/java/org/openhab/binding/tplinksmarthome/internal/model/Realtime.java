b30f381c15f166a8533ed73619b9c0df7da4cbd1
root <source@stonacek.nz>
2018-01-20 19:37:16 +1300

    Added support for HS110v2 devices
    Added basic support for HS110v2 devices returning different get_realtime json
    
    Signed-off-by: KC Stonacek <source@stonacek.nz> (github: kcstonacek)

--- addons/binding/org.openhab.binding.tplinksmarthome/src/main/java/org/openhab/binding/tplinksmarthome/internal/model/Realtime.java
+++ addons/binding/org.openhab.binding.tplinksmarthome/src/main/java/org/openhab/binding/tplinksmarthome/internal/model/Realtime.java
@@ -8,6 +8,8 @@
  */
 package org.openhab.binding.tplinksmarthome.internal.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Data class for reading tp-Link Smart Plug energy monitoring.
  * Only getter methods as the values are set by gson based on the retrieved json.
@@ -16,9 +18,22 @@ package org.openhab.binding.tplinksmarthome.internal.model;
  */
 public class Realtime extends ErrorResponse {
 
+    /* 
+     * for devices like:
+     *   {"system":{"get_sysinfo":{"sw_ver":"1.0.10 Build 161012 Rel.165510","hw_ver":"2.0","type":"IOT.SMARTPLUGSWITCH","model":"HS110(AU)"
+     * The get_realtime json response looks like:
+     *   "emeter":{"get_realtime":{"voltage_mv":239425,"current_ma":1234,"power_mw":123456,"total_wh":12345,"err_code":0}}}
+     *
+     * use gson's alternate {@link com.google.gson.annotations.SerializedName} so both are valid
+     */
+
+    @SerializedName(value="current", alternate={"current_ma"})
     private double current;
+    @SerializedName(value="power", alternate={"power_mw"})
     private double power;
+    @SerializedName(value="total", alternate={"total_wh"})
     private double total;
+    @SerializedName(value="voltage", alternate={"voltage_mv"})
     private double voltage;
 
     public double getCurrent() {
