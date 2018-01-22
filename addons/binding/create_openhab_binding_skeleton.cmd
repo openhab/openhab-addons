3bbec7b1c02b206282ae72dff7965588064879b3
Christoph Weitkamp <github@christophweitkamp.de>
2018-01-22 20:13:52 +0100

    Adjusted skeleton scripts to be compliant with ESH versions
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

--- addons/binding/create_openhab_binding_skeleton.cmd
+++ addons/binding/create_openhab_binding_skeleton.cmd
@@ -6,13 +6,34 @@ SET ARGC=0
 FOR %%x IN (%*) DO SET /A ARGC+=1
 
 IF %ARGC% NEQ 2 (
-	echo Usage: %0 BindingIdInCamelCase BindingIdInLowerCase
+	echo Usage: %0 BindingIdInCamelCase Author
 	exit /B 1
 )
 
-call mvn -s ../archetype-settings.xml archetype:generate -N -DarchetypeGroupId=org.eclipse.smarthome.archetype -DarchetypeArtifactId=org.eclipse.smarthome.archetype.binding -DarchetypeVersion=0.10.0-SNAPSHOT -DgroupId=org.openhab.binding -DartifactId=org.openhab.binding.%2 -Dpackage=org.openhab.binding.%2 -Dversion=2.3.0-SNAPSHOT -DbindingId=%2 -DbindingIdCamelCase=%1 -DvendorName=openHAB -Dnamespace=org.openhab
+SET BindingVersion="2.3.0-SNAPSHOT"
+SET ArchetypeVersion="0.10.0-SNAPSHOT"
 
-COPY ..\..\src\etc\about.html org.openhab.binding.%2%\
+SET BindingIdInCamelCase=%1
+SET BindingIdInLowerCase=%BindingIdInCamelCase%
+SET Author=%2
+
+call :LoCase BindingIdInLowerCase
+
+call mvn -s ../archetype-settings.xml archetype:generate -N -DarchetypeGroupId=org.eclipse.smarthome.archetype -DarchetypeArtifactId=org.eclipse.smarthome.archetype.binding -DarchetypeVersion=%ArchetypeVersion% -DgroupId=org.openhab.binding -DartifactId=org.openhab.binding.%BindingIdInLowerCase% -Dpackage=org.openhab.binding.%BindingIdInLowerCase% -Dversion=%BindingVersion% -DbindingId=%BindingIdInLowerCase% -DbindingIdCamelCase=%BindingIdInCamelCase% -DvendorName=openHAB -Dnamespace=org.openhab -Dauthor="%Author%"
+
+COPY ..\..\src\etc\about.html org.openhab.binding.%BindingIdInLowerCase%\
+
+(SET BindingIdInLowerCase=)
+(SET BindingIdInCamelCase=)
+(SET Author=)
+
+GOTO:EOF
+
+
+:LoCase
+:: Subroutine to convert a variable VALUE to all lower case.
+:: The argument for this subroutine is the variable NAME.
+FOR %%i IN ("A=a" "B=b" "C=c" "D=d" "E=e" "F=f" "G=g" "H=h" "I=i" "J=j" "K=k" "L=l" "M=m" "N=n" "O=o" "P=p" "Q=q" "R=r" "S=s" "T=t" "U=u" "V=v" "W=w" "X=x" "Y=y" "Z=z") DO CALL SET "%1=%%%1:%%~i%%"
+GOTO:EOF
 
 ENDLOCAL
-
