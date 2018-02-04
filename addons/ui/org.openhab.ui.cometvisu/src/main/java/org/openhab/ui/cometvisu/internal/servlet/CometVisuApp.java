fa20e1ee3662dd7ea28da2908a4a1168435ee4bb
Tobias Bräutigam <peuter@users.noreply.github.com>
2018-02-04 09:01:43 +0100

    [cometvisu] add type check to handle string and boolean values (#3204)
    refs #3197
    
    Signed-off-by: Tobias Bräutigam <tbraeutigam@gmail.com>

--- addons/ui/org.openhab.ui.cometvisu/src/main/java/org/openhab/ui/cometvisu/internal/servlet/CometVisuApp.java
+++ addons/ui/org.openhab.ui.cometvisu/src/main/java/org/openhab/ui/cometvisu/internal/servlet/CometVisuApp.java
@@ -190,7 +190,16 @@ public class CometVisuApp {
                 Config.COMETVISU_WEBAPP_ALIAS = (String) properties.get(Config.COMETVISU_WEBAPP_ALIAS_PROPERTY);
             }
             if (properties.get(Config.COMETVISU_AUTODOWNLOAD_PROPERTY) != null) {
-                Boolean newValue = Boolean.valueOf((String) properties.get(Config.COMETVISU_AUTODOWNLOAD_PROPERTY));
+                Object propertyValue = properties.get(Config.COMETVISU_AUTODOWNLOAD_PROPERTY);
+
+                // Value might be a string or a boolean
+                Boolean newValue = false;
+                if (propertyValue instanceof String) {
+                    newValue = Boolean.valueOf((String) propertyValue);
+                } else if (propertyValue instanceof Boolean) {
+                    newValue = (Boolean) propertyValue;
+                }
+
                 boolean changed = Config.COMETVISU_AUTO_DOWNLOAD != newValue;
                 Config.COMETVISU_AUTO_DOWNLOAD = newValue;
                 if (Config.COMETVISU_AUTO_DOWNLOAD && changed) {
