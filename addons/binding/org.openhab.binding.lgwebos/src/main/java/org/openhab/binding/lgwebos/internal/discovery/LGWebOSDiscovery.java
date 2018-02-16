7f2e0c9cecc5e71fba9162ff604595cbd6f29a0c
Sebastian Prehn <sebastian.prehn@gmx.de>
2018-02-16 22:25:25 +0100

    [LGWebOS] LocalIP Binding Property (#3232)
    * Removed unused import in Manifest.
    * Reintroducing the ability to set the IP address to bind to.
    
    Signed-off-by: Sebastian Prehn <sebastian.prehn@gmx.de> (github: sprehn)
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.lgwebos/src/main/java/org/openhab/binding/lgwebos/internal/discovery/LGWebOSDiscovery.java
+++ addons/binding/org.openhab.binding.lgwebos/src/main/java/org/openhab/binding/lgwebos/internal/discovery/LGWebOSDiscovery.java
@@ -14,7 +14,9 @@ import java.io.File;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Map;
+import java.util.Optional;
 
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.smarthome.config.core.ConfigConstants;
 import org.eclipse.smarthome.config.discovery.AbstractDiscoveryService;
 import org.eclipse.smarthome.config.discovery.DiscoveryResult;
@@ -50,6 +52,7 @@ public class LGWebOSDiscovery extends AbstractDiscoveryService implements Discov
     private DiscoveryManager discoveryManager;
 
     private NetworkAddressService networkAddressService;
+    private Optional<InetAddress> localInetAddressesOverride;
 
     public LGWebOSDiscovery() {
         super(LGWebOSBindingConstants.SUPPORTED_THING_TYPES_UIDS, DISCOVERY_TIMEOUT_SECONDS, true);
@@ -68,6 +71,7 @@ public class LGWebOSDiscovery extends AbstractDiscoveryService implements Discov
     @Override
     protected void activate(Map<String, Object> configProperties) {
         logger.debug("Config Parameters: {}", configProperties);
+        localInetAddressesOverride = evaluateConfigPropertyLocalIP((String) configProperties.get("localIP"));
         Util.init(AbstractDiscoveryService.scheduler);
         discoveryManager = DiscoveryManager.getInstance();
         discoveryManager.setPairingLevel(DiscoveryManager.PairingLevel.ON);
@@ -161,16 +165,46 @@ public class LGWebOSDiscovery extends AbstractDiscoveryService implements Discov
 
     @Override
     public InetAddress getIpAddress() {
+        return localInetAddressesOverride.orElseGet(() -> getIpFromNetworkAddressService().orElse(null));
+    }
+
+    /**
+     * Evaluate local IP optional configuration property.
+     *
+     * @param localIP optional configuration string
+     * @return local ip or <code>empty</code> if property is not set or unparseable.
+     */
+    private Optional<InetAddress> evaluateConfigPropertyLocalIP(String localIP) {
+        if (StringUtils.isNotBlank(localIP)) {
+            try {
+                logger.debug("localIP property was explicitly set to: {}", localIP);
+                return Optional.ofNullable(InetAddress.getByName(localIP.trim()));
+            } catch (UnknownHostException e) {
+                logger.warn("localIP property could not be parsed: {} Details: {}", localIP, e.getMessage());
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    /**
+     * Uses OpenHAB's NetworkAddressService to determine the local primary network interface.
+     *
+     * @return local ip or <code>empty</code> if configured primary IP is not set or could not be parsed.
+     */
+    private Optional<InetAddress> getIpFromNetworkAddressService() {
         String ipAddress = networkAddressService.getPrimaryIpv4HostAddress();
         if (ipAddress == null) {
             logger.warn("No network interface could be found.");
-            return null;
+            return Optional.empty();
         }
         try {
-            return InetAddress.getByName(ipAddress);
+            return Optional.of(InetAddress.getByName(ipAddress));
         } catch (UnknownHostException e) {
             logger.warn("Configured primary IP cannot be parsed: {} Details: {}", ipAddress, e.getMessage());
-            return null;
+            return Optional.empty();
         }
+
     }
+
 }
