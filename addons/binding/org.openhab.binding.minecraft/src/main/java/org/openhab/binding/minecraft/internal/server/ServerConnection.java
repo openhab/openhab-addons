a4f6ed271aafa373d5d5800fe38fa8c8075b6b1d
Wouter Born <eclipse@maindrain.net>
2018-03-11 01:26:07 +0100

    Make logger non-static
    Signed-off-by: Wouter Born <eclipse@maindrain.net>

--- addons/binding/org.openhab.binding.minecraft/src/main/java/org/openhab/binding/minecraft/internal/server/ServerConnection.java
+++ addons/binding/org.openhab.binding.minecraft/src/main/java/org/openhab/binding/minecraft/internal/server/ServerConnection.java
@@ -34,8 +34,6 @@ import rx.subscriptions.Subscriptions;
  */
 public class ServerConnection {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ServerConnection.class);
-
     private String host;
     private int port;
     private ThingUID thingUID;
@@ -125,9 +123,12 @@ public class ServerConnection {
         final String serverUrl = String.format("ws://%s:%d/stream", host, port);
 
         return Observable.<ServerConnection> create(new OnSubscribe<ServerConnection>() {
+
+            private final Logger logger = LoggerFactory.getLogger(ServerConnection.class);
+
             @Override
             public void call(final Subscriber<? super ServerConnection> subscriber) {
-                LOGGER.info("Start connecting to Minecraft server at: {}", serverUrl);
+                logger.info("Start connecting to Minecraft server at: {}", serverUrl);
                 if (!subscriber.isUnsubscribed()) {
                     ServerConnection serverConnection = new ServerConnection(thingUID, host, port);
                     MinecraftSocketHandler socketHandler = new MinecraftSocketHandler() {
@@ -138,7 +139,7 @@ public class ServerConnection {
 
                         @Override
                         public void onClose() {
-                            LOGGER.info("Connection to Minecraft server stopped");
+                            logger.info("Connection to Minecraft server stopped");
                             subscriber.onCompleted();
                         };
                     };
