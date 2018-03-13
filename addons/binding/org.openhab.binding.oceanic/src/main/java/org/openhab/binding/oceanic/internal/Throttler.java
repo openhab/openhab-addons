d120eaf360ade2ab8a07052e46cfff9012db6886
Karel Goderis <karel.goderis@me.com>
2018-03-13 07:15:13 +0100

    [Oceanic] Provide a work-around for serial port access in the JVM by implementing a TCP “proxy”  (#3179)
    Additionally some documentation updates and formatting
    
    Signed-off-by: Karel Goderis <karel.goderis@me.com>

--- addons/binding/org.openhab.binding.oceanic/src/main/java/org/openhab/binding/oceanic/internal/Throttler.java
+++ addons/binding/org.openhab.binding.oceanic/src/main/java/org/openhab/binding/oceanic/internal/Throttler.java
@@ -15,14 +15,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The {@link SerialPortThrottler} is helper class that regulates the frequency at which messages/packets are sent to
+ * The {@link Throttler} is helper class that regulates the frequency at which messages/packets are sent to
  * the serial port.
  *
  * @author Karel Goderis - Initial Contribution
  */
-public class SerialPortThrottler {
+public class Throttler {
 
-    private static Logger logger = LoggerFactory.getLogger(SerialPortThrottler.class);
+    private static Logger logger = LoggerFactory.getLogger(Throttler.class);
 
     public static final long INTERVAL = 1000;
 
@@ -37,7 +37,6 @@ public class SerialPortThrottler {
         locks.get(key).lock();
 
         if (timestamps.get(key) != null) {
-
             long lastStamp = timestamps.get(key);
             long timeToWait = Math.max(INTERVAL - (System.currentTimeMillis() - lastStamp), 0);
             if (timeToWait > 0) {
@@ -48,11 +47,9 @@ public class SerialPortThrottler {
                 }
             }
         }
-
     }
 
     public static void unlock(String key) {
-
         if (locks.containsKey(key)) {
             timestamps.put(key, System.currentTimeMillis());
             locks.get(key).unlock();
@@ -60,7 +57,6 @@ public class SerialPortThrottler {
     }
 
     public static void lock() {
-
         for (ReentrantLock aLock : locks.values()) {
             aLock.lock();
         }
@@ -84,14 +80,11 @@ public class SerialPortThrottler {
     }
 
     public static void unlock() {
-
         for (String key : locks.keySet()) {
             if (locks.get(key).isHeldByCurrentThread()) {
                 timestamps.put(key, System.currentTimeMillis());
                 locks.get(key).unlock();
             }
         }
-
     }
-
 }
