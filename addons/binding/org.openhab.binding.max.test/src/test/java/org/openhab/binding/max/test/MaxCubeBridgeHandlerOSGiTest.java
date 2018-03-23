978d930e1bc0e512822508af9852b9d77813c48d
Wouter Born <github@maindrain.net>
2018-03-23 07:47:09 +0100

    [Max] Migrate Groovy to Java tests (#3318)
    Signed-off-by: Wouter Born <eclipse@maindrain.net>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

---
+++ addons/binding/org.openhab.binding.max.test/src/test/java/org/openhab/binding/max/test/MaxCubeBridgeHandlerOSGiTest.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.max.test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+import static org.openhab.binding.max.MaxBinding.*;
+
+import org.eclipse.smarthome.config.core.Configuration;
+import org.eclipse.smarthome.core.thing.Bridge;
+import org.eclipse.smarthome.core.thing.ThingRegistry;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.ThingUID;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.eclipse.smarthome.core.thing.binding.builder.BridgeBuilder;
+import org.eclipse.smarthome.test.java.JavaOSGiTest;
+import org.eclipse.smarthome.test.storage.VolatileStorageService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openhab.binding.max.MaxBinding;
+import org.openhab.binding.max.internal.handler.MaxCubeBridgeHandler;
+
+/**
+ * Tests for {@link MaxCubeBridgeHandler}.
+ *
+ * @author Marcel Verpaalen - Initial version
+ * @author Wouter Born - Migrate Groovy to Java tests
+ * @since 2.0
+ */
+public class MaxCubeBridgeHandlerOSGiTest extends JavaOSGiTest {
+
+    private static final ThingTypeUID BRIDGE_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, BRIDGE_MAXCUBE);
+
+    private ThingRegistry thingRegistry;
+    private VolatileStorageService volatileStorageService = new VolatileStorageService();
+
+    private Bridge maxBridge;
+
+    @Before
+    public void setUp() {
+        registerService(volatileStorageService);
+
+        thingRegistry = getService(ThingRegistry.class);
+        assertThat(thingRegistry, is(notNullValue()));
+    }
+
+    @After
+    public void tearDown() {
+        if (maxBridge != null) {
+            thingRegistry.remove(maxBridge.getUID());
+
+            // TODO: Due to synchronization issues the handler is currently not unset. To be fixed by PR #1789.
+            // waitForAssert(() -> assertThat(maxBridge.getHandler(), is(nullValue())));
+        }
+
+        unregisterService(volatileStorageService);
+    }
+
+    @Test
+    public void maxCubeBridgeHandlerIsCreated() {
+        MaxCubeBridgeHandler maxBridgeHandler = getService(ThingHandler.class, MaxCubeBridgeHandler.class);
+        assertThat(maxBridgeHandler, is(nullValue()));
+
+        Configuration configuration = new Configuration();
+        configuration.put(MaxBinding.PROPERTY_SERIAL_NUMBER, "KEQ0565026");
+        configuration.put(MaxBinding.PROPERTY_IP_ADDRESS, "192.168.3.100");
+
+        ThingUID cubeUid = new ThingUID(BRIDGE_THING_TYPE_UID, "testCube");
+
+        maxBridge = BridgeBuilder.create(BRIDGE_THING_TYPE_UID, cubeUid).withConfiguration(configuration).build();
+        thingRegistry.add(maxBridge);
+
+        // wait for MaxCubeBridgeHandler to be registered
+        waitForAssert(() -> assertThat(maxBridge.getHandler(), is(notNullValue())));
+    }
+
+}
