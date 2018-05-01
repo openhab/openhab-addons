94fdc2ae6c3ddd3680f97e7ed33d5ef1d39e4ad7
Marco Meyer <marco.g.meyer@gmail.com>
2018-05-01 23:07:14 +0200

    * removed channel groups and thus renamed all channels. So existing item files have to be updated!
    Signed-off-by: Marco Meyer <marco.g.meyer@gmail.com> (github: reyem)

--- addons/binding/org.openhab.binding.robonect/src/main/java/org/openhab/binding/robonect/RobonectBindingConstants.java
+++ addons/binding/org.openhab.binding.robonect/src/main/java/org/openhab/binding/robonect/RobonectBindingConstants.java
@@ -24,33 +24,33 @@ public class RobonectBindingConstants {
     public static final ThingTypeUID THING_TYPE_AUTOMOWER = new ThingTypeUID(BINDING_ID, "mower");
 
     // List of all Channel ids
-    public static final String CHANNEL_MOWER_NAME = "mowerInfo#name";
-    public static final String CHANNEL_STATUS_BATTERY = "mowerStatus#battery";
-    public static final String CHANNEL_STATUS_DURATION = "mowerStatus#duration";
-    public static final String CHANNEL_STATUS_HOURS = "mowerStatus#hours";
-    public static final String CHANNEL_STATUS_MODE = "mowerStatus#mode";
-    public static final String CHANNEL_STATUS = "mowerStatus#status";
-    public static final String CHANNEL_MOWER_STATUS_STARTED = "mowerStatus#started";
-    public static final String CHANNEL_MOWER_STATUS_OFFLINE_TRIGGER = "mowerStatus#offlineTrigger";
-    public static final String CHANNEL_TIMER_STATUS = "timer#status";
-    public static final String CHANNEL_TIMER_NEXT_TIMER = "timer#nextTimer";
-    public static final String CHANNEL_WLAN_SIGNAL = "wlan#signal";
+    public static final String CHANNEL_MOWER_NAME = "name";
+    public static final String CHANNEL_STATUS_BATTERY = "battery";
+    public static final String CHANNEL_STATUS_DURATION = "status-duration";
+    public static final String CHANNEL_STATUS_HOURS = "mowing-hours";
+    public static final String CHANNEL_STATUS_MODE = "mode";
+    public static final String CHANNEL_STATUS = "status";
+    public static final String CHANNEL_MOWER_STATUS_STARTED = "started";
+    public static final String CHANNEL_MOWER_STATUS_OFFLINE_TRIGGER = "offlineTrigger";
+    public static final String CHANNEL_TIMER_STATUS = "timer-status";
+    public static final String CHANNEL_TIMER_NEXT_TIMER = "timer-next";
+    public static final String CHANNEL_WLAN_SIGNAL = "wlan-signal";
     
-    public static final String CHANNEL_JOB_REMOTE_START = "job#remoteStart";
-    public static final String CHANNEL_JOB_AFTER_MODE = "job#afterMode";
-    public static final String CHANNEL_JOB_START = "job#start";
-    public static final String CHANNEL_JOB_END = "job#end";
+    public static final String CHANNEL_JOB_REMOTE_START = "job-remote-start";
+    public static final String CHANNEL_JOB_AFTER_MODE = "job-after-mode";
+    public static final String CHANNEL_JOB_START = "job-start";
+    public static final String CHANNEL_JOB_END = "job-end";
     
-    public static final String CHANNEL_ERROR_CODE = "error#code";
-    public static final String CHANNEL_ERROR_MESSAGE = "error#message";
-    public static final String CHANNEL_ERROR_DATE = "error#date";
+    public static final String CHANNEL_ERROR_CODE = "error-code";
+    public static final String CHANNEL_ERROR_MESSAGE = "error-message";
+    public static final String CHANNEL_ERROR_DATE = "error-date";
     
-    public static final String CHANNEL_LAST_ERROR_CODE = "lastError#code";
-    public static final String CHANNEL_LAST_ERROR_MESSAGE = "lastError#message";
-    public static final String CHANNEL_LAST_ERROR_DATE = "lastError#date";
+    public static final String CHANNEL_LAST_ERROR_CODE = "last-error-code";
+    public static final String CHANNEL_LAST_ERROR_MESSAGE = "last-error-message";
+    public static final String CHANNEL_LAST_ERROR_DATE = "last-error-date";
     
-    public static final String CHANNEL_HEALTH_TEMP = "health#temperature";
-    public static final String CHANNEL_HEALTH_HUM = "health#humidity";
+    public static final String CHANNEL_HEALTH_TEMP = "health-temperature";
+    public static final String CHANNEL_HEALTH_HUM = "health-humidity";
     
     public static final String PROPERTY_COMPILED = "compiled";
     public static final String PROPERTY_COMMENT = "comment";
