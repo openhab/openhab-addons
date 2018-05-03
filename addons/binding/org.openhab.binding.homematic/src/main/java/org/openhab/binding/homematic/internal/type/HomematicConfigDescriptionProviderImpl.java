d1eca41df695afdf058ccefbb1f10cf802ea062f
Michael Reitler <github@madpage.de>
2018-05-03 13:34:21 +0200

    [homematic] Support external definition of (xml) thing types
    Introduced interface org.openhab.binding.homematic.type.HomematicThingTypeExcluder to allow external definition of ThingTypes for this binding by suppressing those ThingTypes which are generated by
    HomematicThingTypeProvider.
    Same applies for ChannelTypes, ChannelGroupTypes and ConfigDescriptions. Applications may use this to provide the whole set of supported homematic ThingTypes immediately. By implementing
    HomematicThingTypeExcluder, the generated types are excluded from their specific provider to prevent duplicates.
    The footprint of this injection is minimal and for types that are not excluded (e.g. in plain openhab installation you don't have a single HomematicThingTypeExcluder), the behavoiur stays unchanged.
    
    Further refactorings which belong to this contribution:
    * refactor config-description URIs (before this change they consited of 5 segments but only 3 segments are allowed / relevant)
    * specified "serialNumber" as representation-property which uniquely identifies a Thing / DiscoveryResult
    
    Signed-off-by: Michael Reitler <github@madpage.de>

--- addons/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/type/HomematicConfigDescriptionProviderImpl.java
+++ addons/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/type/HomematicConfigDescriptionProviderImpl.java
@@ -9,31 +9,74 @@
 package org.openhab.binding.homematic.internal.type;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.config.core.ConfigDescription;
 import org.eclipse.smarthome.config.core.ConfigDescriptionProvider;
+import org.openhab.binding.homematic.type.HomematicThingTypeExcluder;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 
 /**
  *
  * @author Gerhard Riegler - Initial contribution
+ * @author Michael Reitler - Added HomematicThingTypeExcluder
  */
 @Component(service = { HomematicConfigDescriptionProvider.class, ConfigDescriptionProvider.class }, immediate = true)
 public class HomematicConfigDescriptionProviderImpl implements HomematicConfigDescriptionProvider {
     private Map<URI, ConfigDescription> configDescriptionsByURI = new HashMap<URI, ConfigDescription>();
+    protected List<HomematicThingTypeExcluder> homematicThingTypeExcluders = new CopyOnWriteArrayList<>();
+    
+    @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
+    protected void addHomematicThingTypeExcluder(HomematicThingTypeExcluder homematicThingTypeExcluder){
+        if(homematicThingTypeExcluders != null){
+            homematicThingTypeExcluders.add(homematicThingTypeExcluder);
+        }
+    }
+     
+    protected void removeHomematicThingTypeExcluder(HomematicThingTypeExcluder homematicThingTypeExcluder){
+        if(homematicThingTypeExcluders != null){
+            homematicThingTypeExcluders.remove(homematicThingTypeExcluder);
+        }
+    }
 
+    private boolean isConfigDescriptionExcluded(URI configDescriptionURI) {
+        // delegate to excluders
+        for (HomematicThingTypeExcluder excluder : homematicThingTypeExcluders) {
+            if (excluder.isConfigDescriptionExcluded(configDescriptionURI)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
     @Override
     public Collection<ConfigDescription> getConfigDescriptions(Locale locale) {
-        return configDescriptionsByURI.values();
+        Collection<ConfigDescription> result = new ArrayList<>();
+        for (URI configDescriptionURI : configDescriptionsByURI.keySet()) {
+            if (!isConfigDescriptionExcluded(configDescriptionURI)) {
+                result.add(configDescriptionsByURI.get(configDescriptionURI));
+            }
+        }
+        return result;
     }
 
     @Override
     public ConfigDescription getConfigDescription(URI uri, @Nullable Locale locale) {
+        return isConfigDescriptionExcluded(uri) ? null : configDescriptionsByURI.get(uri);
+    }
+    
+    @Override
+    public ConfigDescription getInternalConfigDescription(URI uri) {
         return configDescriptionsByURI.get(uri);
     }
 
