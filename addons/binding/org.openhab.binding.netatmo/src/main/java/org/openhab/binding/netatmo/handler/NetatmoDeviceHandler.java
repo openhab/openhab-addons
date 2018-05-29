809e50532e0499ce85b85fd5c8fe4db309e57d79
Gaël L'hopital <gael@lhopital.org>
2018-05-29 10:45:06 +0200

    Introducing Planning capability for Thermostat module. (#3336)
    Signed-off-by: Gaël L'hopital <glhopital@gmail.com>

--- addons/binding/org.openhab.binding.netatmo/src/main/java/org/openhab/binding/netatmo/handler/NetatmoDeviceHandler.java
+++ addons/binding/org.openhab.binding.netatmo/src/main/java/org/openhab/binding/netatmo/handler/NetatmoDeviceHandler.java
@@ -222,4 +222,8 @@ public abstract class NetatmoDeviceHandler<DEVICE> extends AbstractNetatmoThingH
 
     protected abstract @Nullable Integer getDataTimestamp();
 
+    public void expireData() {
+        refreshStrategy.expireData();
+    }
+
 }
