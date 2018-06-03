555d9ebf098b8553f9fe4a26b506b8c099f39010
Martin van Wingerden <martinvw@users.noreply.github.com>
2018-06-03 23:58:56 +0200

    Updated / edit some missing or otherwise distinct copyright headers (#3617)
    * Updated / edit some missing or otherwise distinct copyright headers
    * Remove files generated by DS annotations
    * Remove DS annotation from NestTestHandlerFactory so commited XML is not regenerated
    * Add missing .gitignore files
    
    Signed-off-by: Wouter Born <eclipse@maindrain.net>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.nest.test/src/test/java/org/openhab/binding/nest/test/NestTestHandlerFactory.java
+++ addons/binding/org.openhab.binding.nest.test/src/test/java/org/openhab/binding/nest/test/NestTestHandlerFactory.java
@@ -19,18 +19,15 @@ import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.ThingUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
-import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
 import org.openhab.binding.nest.handler.NestBridgeHandler;
 import org.openhab.binding.nest.internal.discovery.NestDiscoveryService;
 import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.component.annotations.Component;
 
 /**
  * The {@link NestTestHandlerFactory} is responsible for creating test things and thing handlers.
  *
  * @author Wouter Born - Increase test coverage
  */
-@Component(service = ThingHandlerFactory.class, immediate = true, configurationPid = "binding.nest.test")
 public class NestTestHandlerFactory extends BaseThingHandlerFactory {
 
     private String redirectUrl = "http://localhost";
