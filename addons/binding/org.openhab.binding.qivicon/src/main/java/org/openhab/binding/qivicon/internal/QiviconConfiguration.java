5fa1ab5c25c59b9baded6d7b93b604303b9d01ff
Claudius Ellsel <claudius.ellsel@live.de>
2018-06-09 17:15:19 +0200

    Init for Qivicon binding
    Signed-off-by: Claudius Ellsel claudius.ellsel@live.de (github: claell)

---
+++ addons/binding/org.openhab.binding.qivicon/src/main/java/org/openhab/binding/qivicon/internal/QiviconConfiguration.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2014,2018 by the respective copyright holders.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.qivicon.internal;
+
+/**
+ * The {@link QiviconConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Claudius Ellsel - Initial contribution
+ */
+public class QiviconConfiguration {
+
+    /**
+     * Sample configuration parameter. Replace with your own.
+     */
+    public String config1;
+}
---
+++ addons/binding/org.openhab.binding.qivicon/src/main/java/org/openhab/binding/qivicon/internal/QiviconHandlerFactory.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2014,2018 by the respective copyright holders.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.qivicon.internal;
+
+import static org.openhab.binding.qivicon.internal.QiviconBindingConstants.*;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.qivicon.internal.QiviconHandler;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * The {@link QiviconHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author Claudius Ellsel - Initial contribution
+ */
+@NonNullByDefault
+@Component(configurationPid = "binding.qivicon", service = ThingHandlerFactory.class)
+public class QiviconHandlerFactory extends BaseThingHandlerFactory {
+
+    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_SAMPLE);
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
+
+    @Override
+    protected @Nullable ThingHandler createHandler(Thing thing) {
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (THING_TYPE_SAMPLE.equals(thingTypeUID)) {
+            return new QiviconHandler(thing);
+        }
+
+        return null;
+    }
+}
