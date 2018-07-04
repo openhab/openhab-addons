83780a1ea336f7d82d3ad5fa4b333f04bd1162f1
Sebastian Prehn <sebastian.prehn@gmx.de>
2018-07-04 21:46:49 +0200

    Addressed code review comments and bumped to version 2.4.
    Signed-off-by: Sebastian Prehn <sebastian.prehn@gmx.de> (github: sprehn)

--- addons/action/org.openhab.action.lgwebos/src/main/java/org/openhab/action/lgwebos/internal/LGWebOSModuleHandlerFactory.java
+++ addons/action/org.openhab.action.lgwebos/src/main/java/org/openhab/action/lgwebos/internal/LGWebOSModuleHandlerFactory.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2017 by the respective copyright holders.
+ * Copyright (c) 2010-2018 by the respective copyright holders.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -19,12 +19,12 @@ import org.eclipse.smarthome.automation.Module;
 import org.eclipse.smarthome.automation.handler.BaseModuleHandlerFactory;
 import org.eclipse.smarthome.automation.handler.ModuleHandler;
 import org.eclipse.smarthome.automation.handler.ModuleHandlerFactory;
-import org.openhab.binding.lgwebos.LGWebOS;
 import org.openhab.action.lgwebos.handler.AppActionHandler;
 import org.openhab.action.lgwebos.handler.BrowserActionHandler;
 import org.openhab.action.lgwebos.handler.ButtonActionHandler;
 import org.openhab.action.lgwebos.handler.TextActionHandler;
 import org.openhab.action.lgwebos.handler.ToastActionHandler;
+import org.openhab.binding.lgwebos.LGWebOS;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
