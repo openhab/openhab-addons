5e9202e632a61c984a52e217fe89485a01c7daa6
Wouter Born <github@maindrain.net>
2018-08-15 16:01:28 +0200

    Remove outdated license header duplicates (#3830)
    * Remove outdated license header duplicates
    
    Signed-off-by: Wouter Born <eclipse@maindrain.net>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/handler/HDPowerViewHubHandler.java
+++ addons/binding/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/handler/HDPowerViewHubHandler.java
@@ -6,13 +6,6 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-/**
-e * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- */
 package org.openhab.binding.hdpowerview.handler;
 
 import java.io.IOException;
--- addons/voice/org.openhab.voice.marytts/src/main/java/org/openhab/voice/marytts/internal/MaryTTSService.java
+++ addons/voice/org.openhab.voice.marytts/src/main/java/org/openhab/voice/marytts/internal/MaryTTSService.java
@@ -7,13 +7,6 @@
  * http://www.eclipse.org/legal/epl-v10.html
  */
 package org.openhab.voice.marytts.internal;
-/**
- * Copyright (c) 2014-2016 openHAB UG (haftungsbeschraenkt) and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- */
 
 import java.io.IOException;
 import java.util.HashSet;
