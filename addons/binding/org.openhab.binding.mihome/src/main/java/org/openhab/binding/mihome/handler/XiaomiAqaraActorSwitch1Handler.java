023cc9d2f70eed60e2a39b5e2f4f693d50a645f9
dimalo <dima.loschkin@gmail.com>
2018-08-29 08:44:40 +0200

    [MiHome] add Aqara Cube & fix #3100 (#3865)
    Signed-off-by: Dieter Schmidt <dima.loschkin@gmail.com>

--- addons/binding/org.openhab.binding.mihome/src/main/java/org/openhab/binding/mihome/handler/XiaomiAqaraActorSwitch1Handler.java
+++ addons/binding/org.openhab.binding.mihome/src/main/java/org/openhab/binding/mihome/handler/XiaomiAqaraActorSwitch1Handler.java
@@ -63,7 +63,7 @@ public class XiaomiAqaraActorSwitch1Handler extends XiaomiActorBaseHandler {
 
     @Override
     void parseWriteAck(JsonObject data) {
-        parseDefault(data);
+        logger.debug("Got write ack message but ignoring it to prevent item state toggling");
     }
 
     @Override
