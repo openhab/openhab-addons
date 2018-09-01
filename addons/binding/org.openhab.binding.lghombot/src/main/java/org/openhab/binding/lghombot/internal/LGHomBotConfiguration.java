1afa9c5a795262238c1ac744a56cc2966cb673f8
Fredrik Ahlström <fredrikahlstrom75@gmail.com>
2018-09-01 22:42:13 +0200

    [lghombot] Added preliminary Discovery.
    Signed-off-by: Fredrik Ahlström <fredrikahlstrom75@gmail.com>

--- addons/binding/org.openhab.binding.lghombot/src/main/java/org/openhab/binding/lghombot/internal/LGHomBotConfiguration.java
+++ addons/binding/org.openhab.binding.lghombot/src/main/java/org/openhab/binding/lghombot/internal/LGHomBotConfiguration.java
@@ -12,6 +12,8 @@
  */
 package org.openhab.binding.lghombot.internal;
 
+import org.openhab.binding.lghombot.internal.discovery.LGHomBotDiscovery;
+
 /**
  * The {@link LGHomBotConfiguration} class contains fields mapping thing configuration parameters.
  *
@@ -19,34 +21,76 @@ package org.openhab.binding.lghombot.internal;
  */
 public class LGHomBotConfiguration {
 
+    /**
+     * Constant field used in {@link LGHomBotDiscovery} to set the configuration property during discovery. Value of
+     * this field needs to match {@link #ipAddress}
+     */
+    public static final String IP_ADDRESS = "ipAddress";
+
+    /**
+     * IP Address (or host name) of HomBot
+     */
     private String ipAddress;
+
+    /**
+     * Port used by the HomBot
+     */
     private Integer port;
+
+    /**
+     * Polling time (in seconds) to refresh state from the HomBot itself.
+     */
     private Integer pollingPeriod;
 
+    /**
+     * Gets the IP address or host name of the HomBot
+     *
+     * @return the IP address or host name of the HomBot
+     */
     public String getIpAddress() {
         return ipAddress;
     }
 
+    /**
+     * Sets the IP address or host name of the HomBot
+     *
+     * @param ipAddress the IP Address or host name of the HomBot
+     */
     public void setIpAddress(String ipAddress) {
         this.ipAddress = ipAddress;
     }
 
+    /**
+     * Gets the port used by the HomBot
+     *
+     * @return the port used by the HomBot
+     */
     public Integer getPort() {
         return port;
     }
 
+    /**
+     * Sets the port used by the HomBot
+     *
+     * @param port the port used by the HomBot
+     */
     public void setPort(Integer port) {
         this.port = port;
     }
 
+    /**
+     * Gets the polling period (in seconds) to refresh state
+     *
+     * @return the polling period (in seconds) to refresh state
+     */
     public Integer getPollingPeriod() {
         return pollingPeriod;
     }
 
     /**
-     * Set the polling period in seconds.
+     * Sets the polling period (in seconds) to refresh state
      *
-     * @param pollingPeriod Time between polls in seconds.
+     * @param pollingPeriod the polling period (in seconds) to refresh state
      */
     public void setPollingPeriod(Integer pollingPeriod) {
         this.pollingPeriod = pollingPeriod;
