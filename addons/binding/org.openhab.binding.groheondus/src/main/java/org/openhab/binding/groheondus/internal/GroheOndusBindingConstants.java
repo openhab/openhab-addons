29080849ef5d75e312d674f813ca19ac5e4ffcc3
Florian <florian.schmidt.welzow@t-online.de>
2018-09-03 23:51:02 +0200

    [groheondus] Initial contribution (#3696)
    * [groheondus] Initial contribution
    
    This commit adds a new binding (GROHE ONDUS), which allows users of a
    GROHE smart device (such as a GROHE Sense Guard) to integrate these
    devices into openHAB. The binding allows to get some basic information
    out of the device and allows to control the valve (Sense Guard only)
    with an openHAB switch.
    
    The binding is build on top of a java API, which communicates with the
    GROHE REST api using the provided user credentials.
    
    This commit contains contribution from Arne Wohlert, who provided it
    directly to me and certified (a) of the DCO.
    
    Signed-off-by: Florian <florian.schmidt.welzow@t-online.de>

---
+++ addons/binding/org.openhab.binding.groheondus/src/main/java/org/openhab/binding/groheondus/internal/GroheOndusBindingConstants.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.groheondus.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+/**
+ * @author Florian Schmidt and Arne Wohlert - Initial contribution
+ */
+@NonNullByDefault
+public class GroheOndusBindingConstants {
+
+    private static final String BINDING_ID = "groheondus";
+
+    public static final ThingTypeUID THING_TYPE_BRIDGE_ACCOUNT = new ThingTypeUID(BINDING_ID, "account");
+    public static final ThingTypeUID THING_TYPE_APPLIANCE = new ThingTypeUID(BINDING_ID, "appliance");
+
+    public static final String CHANNEL_NAME = "name";
+    public static final String CHANNEL_PRESSURE = "pressure";
+    public static final String CHANNEL_TEMPERATURE = "temperature";
+    public static final String CHANNEL_VALVE_OPEN = "valve_open";
+}
