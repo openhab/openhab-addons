a1574741edd66e4eec92639a78b78486a7fa0160
VelinYordanov <velin.iordanov@gmail.com>
2018-09-14 14:24:11 +0300

    Made constants and handler packages internal in the feican binding
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.feican/src/main/java/org/openhab/binding/feican/internal/FeicanBindingConstants.java
+++ addons/binding/org.openhab.binding.feican/src/main/java/org/openhab/binding/feican/internal/FeicanBindingConstants.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.feican;
+package org.openhab.binding.feican.internal;
 
 import java.util.Collections;
 import java.util.Set;
