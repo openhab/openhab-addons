65a2199776dd8d0c6c97b02259dd95a843385543
VelinYordanov <velin.iordanov@gmail.com>
2018-09-14 19:10:57 +0300

    [dsmr] Made constants and handler packages internal in the dsmr binding. (#3932)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.dsmr/src/main/java/org/openhab/binding/dsmr/internal/handler/DSMRBridgeHandler.java
+++ addons/binding/org.openhab.binding.dsmr/src/main/java/org/openhab/binding/dsmr/internal/handler/DSMRBridgeHandler.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.dsmr.handler;
+package org.openhab.binding.dsmr.internal.handler;
 
 import java.util.ArrayList;
 import java.util.List;
