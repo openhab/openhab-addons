20d2c400ec688e469fd4cb675bf1df5846195efa
VelinYordanov <velin.iordanov@gmail.com>
2018-09-15 15:48:17 +0300

    [coolmasternet] Made constants and handler packages internal in the coolmasternet binding (#3927)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.coolmasternet/src/main/java/org/openhab/binding/coolmasternet/internal/CoolMasterNetBindingConstants.java
+++ addons/binding/org.openhab.binding.coolmasternet/src/main/java/org/openhab/binding/coolmasternet/internal/CoolMasterNetBindingConstants.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.coolmasternet;
+package org.openhab.binding.coolmasternet.internal;
 
 import java.util.Set;
 import java.util.stream.Collectors;
