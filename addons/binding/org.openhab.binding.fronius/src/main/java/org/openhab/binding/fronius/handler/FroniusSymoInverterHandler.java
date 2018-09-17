70ade44fc489acc035500e3dfee5630594d66864
Thomas Rokohl <thomas.rokohl@codemacher.de>
2018-09-17 11:59:27 +0200

    [Fronius] Fixed the NullPointerException if timeout by evaluate the status  (#3828)
    * Fixed the NullPointerException if timeout by evaluate the status
    Robust json/object handling
    
    Signed-off-by: Thomas Rokohl <thomas.rokohl@codemacher.de>

--- addons/binding/org.openhab.binding.fronius/src/main/java/org/openhab/binding/fronius/handler/FroniusSymoInverterHandler.java
+++ addons/binding/org.openhab.binding.fronius/src/main/java/org/openhab/binding/fronius/handler/FroniusSymoInverterHandler.java
@@ -18,6 +18,7 @@ import org.eclipse.smarthome.io.net.http.HttpUtil;
 import org.openhab.binding.fronius.FroniusBaseDeviceConfiguration;
 import org.openhab.binding.fronius.FroniusBindingConstants;
 import org.openhab.binding.fronius.FroniusBridgeConfiguration;
+import org.openhab.binding.fronius.internal.api.BaseFroniusResponse;
 import org.openhab.binding.fronius.internal.api.InverterRealtimeResponse;
 import org.openhab.binding.fronius.internal.api.PowerFlowRealtimeResponse;
 import org.openhab.binding.fronius.internal.api.ValueUnit;
@@ -35,12 +36,12 @@ import com.google.gson.JsonSyntaxException;
  */
 public class FroniusSymoInverterHandler extends FroniusBaseThingHandler {
 
+    private static final int API_TIMEOUT = 5000;
     private final Logger logger = LoggerFactory.getLogger(FroniusSymoInverterHandler.class);
     private InverterRealtimeResponse inverterRealtimeResponse;
     private PowerFlowRealtimeResponse powerFlowResponse;
     private FroniusBaseDeviceConfiguration config;
-    private static final int API_TIMEOUT = 5000;
-    private Gson gson;
+    private final Gson gson;
 
     public FroniusSymoInverterHandler(Thing thing) {
         super(thing);
@@ -74,27 +75,31 @@ public class FroniusSymoInverterHandler extends FroniusBaseThingHandler {
     protected Object getValue(String channelId) {
         String[] fields = StringUtils.split(channelId, "#");
 
-        if (inverterRealtimeResponse == null || inverterRealtimeResponse.getBody() == null
-                || inverterRealtimeResponse.getBody().getData() == null) {
-            return null;
-        }
-
         String fieldName = fields[0];
 
+        if (inverterRealtimeResponse == null) {
+            return null;
+        }
         switch (fieldName) {
             case FroniusBindingConstants.InverterDataChannelDayEnergy:
                 ValueUnit day = inverterRealtimeResponse.getBody().getData().getDayEnergy();
-                day.setUnit("kWh");
+                if (day != null) {
+                    day.setUnit("kWh");
+                }
                 return day;
             case FroniusBindingConstants.InverterDataChannelPac:
                 return inverterRealtimeResponse.getBody().getData().getPac();
             case FroniusBindingConstants.InverterDataChannelTotal:
                 ValueUnit total = inverterRealtimeResponse.getBody().getData().getTotalEnergy();
-                total.setUnit("MWh");
+                if (total != null) {
+                    total.setUnit("MWh");
+                }
                 return total;
             case FroniusBindingConstants.InverterDataChannelYear:
                 ValueUnit year = inverterRealtimeResponse.getBody().getData().getYearEnergy();
-                year.setUnit("MWh");
+                if (year != null) {
+                    year.setUnit("MWh");
+                }
                 return year;
             case FroniusBindingConstants.InverterDataChannelFac:
                 return inverterRealtimeResponse.getBody().getData().getFac();
@@ -107,10 +112,7 @@ public class FroniusSymoInverterHandler extends FroniusBaseThingHandler {
             case FroniusBindingConstants.InverterDataChannelUdc:
                 return inverterRealtimeResponse.getBody().getData().getUdc();
         }
-
-        if (powerFlowResponse == null || powerFlowResponse.getBody() == null
-                || powerFlowResponse.getBody().getData() == null
-                || powerFlowResponse.getBody().getData().getSite() == null) {
+        if (powerFlowResponse == null) {
             return null;
         }
         switch (fieldName) {
@@ -134,34 +136,34 @@ public class FroniusSymoInverterHandler extends FroniusBaseThingHandler {
     }
 
     /**
-     * Make the PowerFlowRealtimeDataRequest
      *
-     * @param ip address of the device
-     * @return {PowerFlowRealtimeResponse} the object representation of the json response
+     * @param type response class type
+     * @param url to request
+     * @return the object representation of the json response
      */
-    private PowerFlowRealtimeResponse getPowerFlowRealtime(String ip) {
-        PowerFlowRealtimeResponse result = null;
+    private <T extends BaseFroniusResponse> T collectDataFormUrl(Class<T> type, String url) {
+        T result = null;
         boolean resultOk = false;
         String errorMsg = null;
 
         try {
-            String location = FroniusBindingConstants.POWERFLOW_REALTIME_DATA.replace("%IP%",
-                    StringUtils.trimToEmpty(ip));
-            logger.debug("URL = {}", location);
-            String response = HttpUtil.executeUrl("GET", location, API_TIMEOUT);
+            logger.debug("URL = {}", url);
+            String response = HttpUtil.executeUrl("GET", url, API_TIMEOUT);
+
             if (response != null) {
                 logger.debug("aqiResponse = {}", response);
-                result = gson.fromJson(response, PowerFlowRealtimeResponse.class);
+                result = gson.fromJson(response, type);
             }
 
             if (result == null) {
                 errorMsg = "no data returned";
-            } else if (result.getBody() != null) {
-                resultOk = true;
             } else {
-                errorMsg = "missing data sub-object";
+                if (result.getHead().getStatus().getCode() == 0) {
+                    resultOk = true;
+                } else {
+                    errorMsg = result.getHead().getStatus().getReason();
+                }
             }
-
             if (!resultOk) {
                 logger.debug("Error in fronius response: {}", errorMsg);
             }
@@ -172,59 +174,38 @@ public class FroniusSymoInverterHandler extends FroniusBaseThingHandler {
             logger.debug("Error running fronius request: {}", e.getMessage());
         }
 
-        return resultOk ? result : null;
-    }
-
-    /**
-     * Make the InverterRealtimeDataRequest
-     *
-     * @param ip address of the device
-     * @return {InverterRealtimeResponse} the object representation of the json response
-     */
-    private InverterRealtimeResponse getRealtimeData(String ip, int deviceId) {
-        InverterRealtimeResponse result = null;
-        boolean resultOk = false;
-        String errorMsg = null;
-
-        try {
-            String location = FroniusBindingConstants.INVERTER_REALTIME_DATA_URL.replace("%IP%",
-                    StringUtils.trimToEmpty(ip));
-            location = location.replace("%DEVICEID%", Integer.toString(deviceId));
-            logger.debug("URL = {}", location);
-
-            String response = HttpUtil.executeUrl("GET", location, API_TIMEOUT);
-            if (response != null) {
-                logger.debug("aqiResponse = {}", response);
-                result = gson.fromJson(response, InverterRealtimeResponse.class);
-            }
-
-            if (result == null) {
-                errorMsg = "no data returned";
-            } else if (result.getBody() != null) {
-                resultOk = true;
-            } else {
-                errorMsg = "missing data sub-object";
-            }
-
-            if (!resultOk) {
-                logger.debug("Error in fronius response: {}", errorMsg);
-            }
-        } catch (JsonSyntaxException e) {
-            errorMsg = "Configuration is incorrect";
-            logger.debug("Error running fronius request: {}", e.getMessage());
-        } catch (IOException | IllegalStateException e) {
-            errorMsg = "Connection failed";
-            logger.debug("Error running fronius request: {}", e.getMessage());
-        }
-
         // Update the thing status
         if (resultOk) {
             updateStatus(ThingStatus.ONLINE);
         } else {
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.OFFLINE.COMMUNICATION_ERROR, errorMsg);
         }
-
         return resultOk ? result : null;
+    }
+
+    /**
+     * Make the PowerFlowRealtimeDataRequest
+     *
+     * @param ip address of the device
+     * @return {PowerFlowRealtimeResponse} the object representation of the json response
+     */
+    private PowerFlowRealtimeResponse getPowerFlowRealtime(String ip) {
+        String location = FroniusBindingConstants.POWERFLOW_REALTIME_DATA.replace("%IP%", StringUtils.trimToEmpty(ip));
+        return collectDataFormUrl(PowerFlowRealtimeResponse.class, location);
+    }
+
+    /**
+     * Make the InverterRealtimeDataRequest
+     *
+     * @param ip address of the device
+     * @param deviceId of the device
+     * @return {InverterRealtimeResponse} the object representation of the json response
+     */
+    private InverterRealtimeResponse getRealtimeData(String ip, int deviceId) {
+        String location = FroniusBindingConstants.INVERTER_REALTIME_DATA_URL.replace("%IP%",
+                StringUtils.trimToEmpty(ip));
+        location = location.replace("%DEVICEID%", Integer.toString(deviceId));
+        return collectDataFormUrl(InverterRealtimeResponse.class, location);
     }
 
 }
