c770d9e91bdbe4c2081214260b5f2874ca57167f
VelinYordanov <velin.iordanov@gmail.com>
2018-09-18 14:05:23 +0300

    Made constants and handler packages internal in the icloud binding
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.icloud/src/main/java/org/openhab/binding/icloud/internal/ICloudBindingConstants.java
+++ addons/binding/org.openhab.binding.icloud/src/main/java/org/openhab/binding/icloud/internal/ICloudBindingConstants.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.icloud;
+package org.openhab.binding.icloud.internal;
 
 import java.util.Arrays;
 import java.util.HashSet;
