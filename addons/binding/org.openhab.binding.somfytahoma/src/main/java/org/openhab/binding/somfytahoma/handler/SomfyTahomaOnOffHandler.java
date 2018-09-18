a14ffd6762580fb6bb7392308a6bd131688ce0e8
Ondrej Pecta <opecta@gmail.com>
2018-09-18 21:51:36 +0200

    [somfytahoma] Initial contribution of Somfy Tahoma binding (#2503)
    * Somfy Tahoma binding initial contribution
    
    Signed-off-by: Ondrej Pecta <opecta@gmail.com>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

---
+++ addons/binding/org.openhab.binding.somfytahoma/src/main/java/org/openhab/binding/somfytahoma/handler/SomfyTahomaOnOffHandler.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.somfytahoma.handler;
+
+import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.thing.ChannelUID;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.types.Command;
+import org.eclipse.smarthome.core.types.RefreshType;
+import org.openhab.binding.somfytahoma.SomfyTahomaBindingConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Hashtable;
+
+import static org.openhab.binding.somfytahoma.SomfyTahomaBindingConstants.SWITCH;
+
+/**
+ * The {@link SomfyTahomaOnOffHandler} is responsible for handling commands,
+ * which are sent to one of the channels of the on/off thing.
+ *
+ * @author Ondrej Pecta - Initial contribution
+ */
+public class SomfyTahomaOnOffHandler extends SomfyTahomaBaseThingHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(SomfyTahomaOnOffHandler.class);
+
+    public SomfyTahomaOnOffHandler(Thing thing) {
+        super(thing);
+    }
+
+    @Override
+    public Hashtable<String, String> getStateNames() {
+        return new Hashtable<String, String>() {{
+            put(SWITCH, "core:OnOffState");
+        }};
+    }
+
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        logger.debug("Received command {} for channel {}", command, channelUID);
+        if (channelUID.getId().equals(SomfyTahomaBindingConstants.SWITCH) && command instanceof OnOffType) {
+            sendCommand(command.toString().toLowerCase(), "[]");
+        }
+        if (command.equals(RefreshType.REFRESH)) {
+            updateChannelState(channelUID);
+        }
+    }
+}
