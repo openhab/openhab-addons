107b32a60d1adc5e2aae43d2fe3b2a591ebfb7a7
VelinYordanov <velin.iordanov@gmail.com>
2018-09-18 23:37:00 +0300

    [Hyperion] Made constants and handler packages internal in the hyperion binding (#3955)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.hyperion/src/main/java/org/openhab/binding/hyperion/internal/HyperionBindingConstants.java
+++ addons/binding/org.openhab.binding.hyperion/src/main/java/org/openhab/binding/hyperion/internal/HyperionBindingConstants.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.hyperion;
+package org.openhab.binding.hyperion.internal;
 
 import java.util.HashSet;
 import java.util.Set;
