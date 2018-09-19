533c5290f9bbb3e7b096530a669dcc5f7e5452d7
VelinYordanov <velin.iordanov@gmail.com>
2018-09-19 15:51:19 +0300

    Made constants and handler packages internal in the lametrictime binding
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.lametrictime/src/main/java/org/openhab/binding/lametrictime/internal/handler/LaMetricTimeAppHandler.java
+++ addons/binding/org.openhab.binding.lametrictime/src/main/java/org/openhab/binding/lametrictime/internal/handler/LaMetricTimeAppHandler.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.lametrictime.handler;
+package org.openhab.binding.lametrictime.internal.handler;
 
 import org.syphr.lametrictime.api.local.model.Widget;
 
