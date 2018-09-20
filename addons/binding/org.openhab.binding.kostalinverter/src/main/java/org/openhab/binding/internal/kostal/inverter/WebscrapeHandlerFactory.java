317eaf87bbe5c0f44365870b4117f4c462cdf7dd
VelinYordanov <velin.iordanov@gmail.com>
2018-09-20 12:07:33 +0300

    [kostal.inverter] Made kostal.inverter package internal (#3967)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.kostalinverter/src/main/java/org/openhab/binding/internal/kostal/inverter/WebscrapeHandlerFactory.java
+++ addons/binding/org.openhab.binding.kostalinverter/src/main/java/org/openhab/binding/internal/kostal/inverter/WebscrapeHandlerFactory.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.kostal.inverter;
+package org.openhab.binding.internal.kostal.inverter;
 
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
