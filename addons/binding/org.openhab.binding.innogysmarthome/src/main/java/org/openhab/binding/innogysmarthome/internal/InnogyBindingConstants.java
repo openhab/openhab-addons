12558ff48b38677b906e227b5d2201cb13a203cf
VelinYordanov <velin.iordanov@gmail.com>
2018-09-21 11:48:07 +0300

    [innogysmarthome] Made constants and handler packages internal in the innogysmarthome binding (#3957)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.innogysmarthome/src/main/java/org/openhab/binding/innogysmarthome/internal/InnogyBindingConstants.java
+++ addons/binding/org.openhab.binding.innogysmarthome/src/main/java/org/openhab/binding/innogysmarthome/internal/InnogyBindingConstants.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.innogysmarthome;
+package org.openhab.binding.innogysmarthome.internal;
 
 import java.util.Set;
 
