d51d1cb846729c107a9c5e3008760263ee8ed05d
VelinYordanov <velin.iordanov@gmail.com>
2018-09-21 23:24:27 +0300

    [rme] Made constants and handler packages internal (#4007)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.rme/src/main/java/org/openhab/binding/rme/internal/RMEBindingConstants.java
+++ addons/binding/org.openhab.binding.rme/src/main/java/org/openhab/binding/rme/internal/RMEBindingConstants.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.rme;
+package org.openhab.binding.rme.internal;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
--- addons/binding/org.openhab.binding.rme/src/main/java/org/openhab/binding/rme/internal/handler/RMEThingHandler.java
+++ addons/binding/org.openhab.binding.rme/src/main/java/org/openhab/binding/rme/internal/handler/RMEThingHandler.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.rme.handler;
+package org.openhab.binding.rme.internal.handler;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,7 +18,7 @@ import org.eclipse.smarthome.core.library.types.StringType;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.types.Command;
-import org.openhab.binding.rme.RMEBindingConstants.DataField;
+import org.openhab.binding.rme.internal.RMEBindingConstants.DataField;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
