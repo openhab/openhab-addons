bef85b2717cdc05b898c04261d855c6e857eb339
VelinYordanov <velin.iordanov@gmail.com>
2018-09-21 17:15:18 +0300

    [powermax] Made constants and handler packages internal
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.powermax/src/main/java/org/openhab/binding/powermax/internal/PowermaxBindingConstants.java
+++ addons/binding/org.openhab.binding.powermax/src/main/java/org/openhab/binding/powermax/internal/PowermaxBindingConstants.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.powermax;
+package org.openhab.binding.powermax.internal;
 
 import java.util.Set;
 
