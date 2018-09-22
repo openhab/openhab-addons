4d0bfdc906e6537ad7a5e21acdace5c98206e2d8
VelinYordanov <velin.iordanov@gmail.com>
2018-09-22 12:23:24 +0300

    [onewiregpio] Made constants and handler packages internal (#3996)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.onewiregpio/src/main/java/org/openhab/binding/onewiregpio/internal/OneWireGPIOHandlerFactory.java
+++ addons/binding/org.openhab.binding.onewiregpio/src/main/java/org/openhab/binding/onewiregpio/internal/OneWireGPIOHandlerFactory.java
@@ -8,7 +8,7 @@
  */
 package org.openhab.binding.onewiregpio.internal;
 
-import static org.openhab.binding.onewiregpio.OneWireGPIOBindingConstants.THING_TYPE;
+import static org.openhab.binding.onewiregpio.internal.OneWireGPIOBindingConstants.THING_TYPE;
 
 import java.util.Collections;
 import java.util.Set;
@@ -18,7 +18,7 @@ import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
 import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
-import org.openhab.binding.onewiregpio.handler.OneWireGPIOHandler;
+import org.openhab.binding.onewiregpio.internal.handler.OneWireGPIOHandler;
 import org.osgi.service.component.annotations.Component;
 
 /**
