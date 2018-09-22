01e8f4e05c92b187c6f9dc1a12e042f5885da308
VelinYordanov <velin.iordanov@gmail.com>
2018-09-22 11:37:03 +0300

    [russound] Made constants package internal (#4008)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.russound/src/main/java/org/openhab/binding/russound/internal/RussoundBindingConstants.java
+++ addons/binding/org.openhab.binding.russound/src/main/java/org/openhab/binding/russound/internal/RussoundBindingConstants.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.russound;
+package org.openhab.binding.russound.internal;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
