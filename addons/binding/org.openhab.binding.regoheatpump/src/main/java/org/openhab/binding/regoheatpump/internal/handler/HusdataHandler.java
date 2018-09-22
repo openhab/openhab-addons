8728624200bfbde4a1b9741942fb7dec6c273218
VelinYordanov <velin.iordanov@gmail.com>
2018-09-22 12:17:37 +0300

    [regoheatpump] Made constants and handler packages internal (#4006)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.regoheatpump/src/main/java/org/openhab/binding/regoheatpump/internal/handler/HusdataHandler.java
+++ addons/binding/org.openhab.binding.regoheatpump/src/main/java/org/openhab/binding/regoheatpump/internal/handler/HusdataHandler.java
@@ -6,9 +6,9 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.regoheatpump.handler;
+package org.openhab.binding.regoheatpump.internal.handler;
 
-import static org.openhab.binding.regoheatpump.RegoHeatPumpBindingConstants.*;
+import static org.openhab.binding.regoheatpump.internal.RegoHeatPumpBindingConstants.*;
 
 import java.io.BufferedReader;
 import java.io.EOFException;
