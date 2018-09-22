15eb05985b9f492bdd5e197d509ccbe05882c111
VelinYordanov <velin.iordanov@gmail.com>
2018-09-22 12:18:44 +0300

    [phc] Made constants and handler packages internal (#4002)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.phc/src/main/java/org/openhab/binding/phc/internal/handler/QueueObject.java
+++ addons/binding/org.openhab.binding.phc/src/main/java/org/openhab/binding/phc/internal/handler/QueueObject.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.phc.handler;
+package org.openhab.binding.phc.internal.handler;
 
 import org.eclipse.smarthome.core.types.Command;
 
