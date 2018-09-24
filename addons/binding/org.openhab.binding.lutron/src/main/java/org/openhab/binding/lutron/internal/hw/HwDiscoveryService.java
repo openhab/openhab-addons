e10c06356dcb0f7ac09a21b4943a0839a453ccd7
Andrew Shilliday <andrewshilliday@gmail.com>
2018-09-24 19:45:58 -0400

    Changed log level

--- addons/binding/org.openhab.binding.lutron/src/main/java/org/openhab/binding/lutron/internal/hw/HwDiscoveryService.java
+++ addons/binding/org.openhab.binding.lutron/src/main/java/org/openhab/binding/lutron/internal/hw/HwDiscoveryService.java
@@ -56,7 +56,7 @@ public class HwDiscoveryService extends AbstractDiscoveryService {
 			}
 		    }
 		} catch (InterruptedException e) {
-		    logger.warn("Scan interrupted");
+		    logger.debug("Scan interrupted");
 		} catch (Exception e) {
 		    logger.error("Error occurred running discovery scanner", e);
 		} finally {
