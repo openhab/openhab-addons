c4855115acdc70de1aa821578eba65bcc6709bc2
VelinYordanov <velin.iordanov@gmail.com>
2018-09-25 14:57:27 +0300

    [silvercrestwifisocket] Made constants and handler packages internal
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.silvercrestwifisocket/src/main/java/org/openhab/binding/silvercrestwifisocket/internal/handler/SilvercrestWifiSocketMediator.java
+++ addons/binding/org.openhab.binding.silvercrestwifisocket/src/main/java/org/openhab/binding/silvercrestwifisocket/internal/handler/SilvercrestWifiSocketMediator.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.silvercrestwifisocket.handler;
+package org.openhab.binding.silvercrestwifisocket.internal.handler;
 
 import java.util.Set;
 
