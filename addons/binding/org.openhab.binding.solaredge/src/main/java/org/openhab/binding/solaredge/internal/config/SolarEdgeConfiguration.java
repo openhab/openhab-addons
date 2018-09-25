9f9ca86cb07d9b03035ff9c1b0a5069c7c68d4b2
VelinYordanov <velin.iordanov@gmail.com>
2018-09-26 01:23:21 +0300

    [solaredge] Made constants and handler packages internal (#4022)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.solaredge/src/main/java/org/openhab/binding/solaredge/internal/config/SolarEdgeConfiguration.java
+++ addons/binding/org.openhab.binding.solaredge/src/main/java/org/openhab/binding/solaredge/internal/config/SolarEdgeConfiguration.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.solaredge.config;
+package org.openhab.binding.solaredge.internal.config;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
 
