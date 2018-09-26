c39d9ecc80c7a699b7425cdf440f49845f920416
VelinYordanov <velin.iordanov@gmail.com>
2018-09-26 15:35:13 +0300

    [rfxcom] Made constants and handler packages internal
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- addons/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/internal/handler/RFXComBridgeHandler.java
+++ addons/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/internal/handler/RFXComBridgeHandler.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.rfxcom.handler;
+package org.openhab.binding.rfxcom.internal.handler;
 
 import java.io.IOException;
 import java.util.List;
