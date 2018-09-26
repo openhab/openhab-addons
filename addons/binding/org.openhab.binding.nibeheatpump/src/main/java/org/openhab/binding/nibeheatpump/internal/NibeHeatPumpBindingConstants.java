7907ac9d241f8db3e94ada0c30ecfe52ac047dc5
VelinYordanov <velin.iordanov@gmail.com>
2018-09-26 20:35:31 +0300

    [nibeheatpump] Made constants and handler packages internal (#4046)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.nibeheatpump/src/main/java/org/openhab/binding/nibeheatpump/internal/NibeHeatPumpBindingConstants.java
+++ addons/binding/org.openhab.binding.nibeheatpump/src/main/java/org/openhab/binding/nibeheatpump/internal/NibeHeatPumpBindingConstants.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.nibeheatpump;
+package org.openhab.binding.nibeheatpump.internal;
 
 import java.util.Set;
 import java.util.stream.Collectors;
