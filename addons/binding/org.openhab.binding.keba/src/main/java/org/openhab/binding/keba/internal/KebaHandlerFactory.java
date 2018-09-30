ee87a5435dd6669596512581bed4ebd5814cb40b
VelinYordanov <velin.iordanov@gmail.com>
2018-09-30 13:13:31 +0300

    Made constants and handler packages internal in the keba binding. (#3965)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.keba/src/main/java/org/openhab/binding/keba/internal/KebaHandlerFactory.java
+++ addons/binding/org.openhab.binding.keba/src/main/java/org/openhab/binding/keba/internal/KebaHandlerFactory.java
@@ -8,7 +8,7 @@
  */
 package org.openhab.binding.keba.internal;
 
-import static org.openhab.binding.keba.KebaBindingConstants.THING_TYPE_KECONTACTP20;
+import static org.openhab.binding.keba.internal.KebaBindingConstants.THING_TYPE_KECONTACTP20;
 
 import java.util.Collections;
 import java.util.Set;
@@ -18,7 +18,7 @@ import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
 import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
-import org.openhab.binding.keba.handler.KeContactHandler;
+import org.openhab.binding.keba.internal.handler.KeContactHandler;
 import org.osgi.service.component.annotations.Component;
 
 /**
