897f87f8d7f87d5db7341ade20e276a3a75664cf
Mark Hilbush <mark@hilbush.com>
2018-10-25 15:47:09 -0400

    [squeezebox] Fix for ArrayIndexOutOfBounds exception (#4157)
    * Fix for ArrayIndexOutOfBounds exception
    * Add bounds checking
    
    Signed-off-by: Mark Hilbush <mark@hilbush.com>

--- addons/binding/org.openhab.binding.squeezebox/src/main/java/org/openhab/binding/squeezebox/internal/handler/SqueezeBoxServerHandler.java
+++ addons/binding/org.openhab.binding.squeezebox/src/main/java/org/openhab/binding/squeezebox/internal/handler/SqueezeBoxServerHandler.java
@@ -421,7 +421,8 @@ public class SqueezeBoxServerHandler extends BaseBridgeHandler {
                     // Message is very long and frequent; only show when running at trace level logging
                     logger.trace("Message received: {}", message);
 
-                    if (message.startsWith("listen 1")) {
+                    // Fix for some third-party apps that are sending "subscribe playlist"
+                    if (message.startsWith("listen 1") || message.startsWith("subscribe playlist")) {
                         continue;
                     }
 
@@ -577,6 +578,9 @@ public class SqueezeBoxServerHandler extends BaseBridgeHandler {
         }
 
         private void handleMixerMessage(String mac, String[] messageParts) {
+            if (messageParts.length < 4) {
+                return;
+            }
             String action = messageParts[2];
 
             switch (action) {
@@ -809,6 +813,9 @@ public class SqueezeBoxServerHandler extends BaseBridgeHandler {
         }
 
         private void handlePlaylistMessage(final String mac, String[] messageParts) {
+            if (messageParts.length < 3) {
+                return;
+            }
             String action = messageParts[2];
             String mode;
             if (action.equals("newsong")) {
@@ -821,6 +828,9 @@ public class SqueezeBoxServerHandler extends BaseBridgeHandler {
                     }
                 });
             } else if (action.equals("pause")) {
+                if (messageParts.length < 4) {
+                    return;
+                }
                 mode = messageParts[3].equals("0") ? "play" : "pause";
             } else if (action.equals("stop")) {
                 mode = "stop";
