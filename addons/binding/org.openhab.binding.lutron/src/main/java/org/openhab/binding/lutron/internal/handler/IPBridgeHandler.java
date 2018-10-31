09ecf3ea8e772c1a3940ea7c60bcad2fd104a55a
Bob A <bobadair@users.noreply.github.com>
2018-10-31 04:29:53 -0400

    [Lutron] Add support for Radio RA2 Timeclock and Green Mode control (#4152)
    * [Lutron] Add green mode and timeclock support for Radio RA2 systems
    * [Lutron] Fix LED definition for keypads T5-RL/T10-RL/T15-RL
    
    Signed-off-by: Bob Adair <bob.github@att.net> (github: bobadair)

--- addons/binding/org.openhab.binding.lutron/src/main/java/org/openhab/binding/lutron/internal/handler/IPBridgeHandler.java
+++ addons/binding/org.openhab.binding.lutron/src/main/java/org/openhab/binding/lutron/internal/handler/IPBridgeHandler.java
@@ -47,7 +47,7 @@ import org.slf4j.LoggerFactory;
  * @author Bob Adair - Added reconnect and heartbeat config parameters
  */
 public class IPBridgeHandler extends BaseBridgeHandler {
-    private static final Pattern STATUS_REGEX = Pattern.compile("~(OUTPUT|DEVICE|SYSTEM),([^,]+),(.*)");
+    private static final Pattern STATUS_REGEX = Pattern.compile("~(OUTPUT|DEVICE|SYSTEM|TIMECLOCK|MODE),([^,]+),(.*)");
 
     private static final String DB_UPDATE_DATE_FORMAT = "MM/dd/yyyy HH:mm:ss";
 
@@ -315,7 +315,7 @@ public class IPBridgeHandler extends BaseBridgeHandler {
             if (thing.getHandler() instanceof LutronHandler) {
                 LutronHandler handler = (LutronHandler) thing.getHandler();
 
-                if (handler.getIntegrationId() == integrationId) {
+                if (handler != null && handler.getIntegrationId() == integrationId) {
                     return handler;
                 }
             }
