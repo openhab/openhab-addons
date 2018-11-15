dc8d3577bb70a9e3e215add5444fc50f2302027c
george_foobot <george@foobot.io>
2018-11-15 14:23:35 +0100

    Foobot binding (#3247)
    Added headers
    
    Signed-off-by: George Katsis <george@foobot.io> (github: georgeFoobot)
    Also-by: Divya Chauhan <divyachauhan25@gmail.com> (github: divyachauhan25)

--- addons/binding/org.openhab.binding.foobot/src/main/java/org/openhab/binding/foobot/internal/handler/FoobotAccountHandler.java
+++ addons/binding/org.openhab.binding.foobot/src/main/java/org/openhab/binding/foobot/internal/handler/FoobotAccountHandler.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.foobot.internal.handler;
 
 import static org.openhab.binding.foobot.internal.FoobotBindingConstants.DEFAULT_REFRESH_PERIOD_MINUTES;
