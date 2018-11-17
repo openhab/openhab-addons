990bc6ddccc88dbadebbf06a4005121dd56a0e3b
Konstantin Polihronov <polychronov@gmail.com>
2018-11-17 19:04:17 +0200

    Started developing ParadoxAlarm direct binding - Skeleton only
    Signed-off-by: Konstantin Polihronov <polychronov@gmail.com> (github: theater)

---
+++ addons/binding/org.openhab.binding.paradoxalarm/src/main/java/org/openhab/binding/paradoxalarm/internal/ParadoxAlarmBindingConstants.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2014,2018 by the respective copyright holders.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.paradoxalarm.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+/**
+ * The {@link ParadoxAlarmBindingConstants} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Konstantin_Polihronov - Initial contribution
+ */
+@NonNullByDefault
+public class ParadoxAlarmBindingConstants {
+
+    private static final String BINDING_ID = "paradoxalarm";
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID THING_TYPE_SAMPLE = new ThingTypeUID(BINDING_ID, "sample");
+
+    // List of all Channel ids
+    public static final String CHANNEL_1 = "channel1";
+}
---
+++ addons/binding/org.openhab.binding.paradoxalarm/src/main/java/org/openhab/binding/paradoxalarm/internal/ParadoxAlarmConfiguration.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2014,2018 by the respective copyright holders.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.paradoxalarm.internal;
+
+/**
+ * The {@link ParadoxAlarmConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Konstantin_Polihronov - Initial contribution
+ */
+public class ParadoxAlarmConfiguration {
+
+    /**
+     * Sample configuration parameter. Replace with your own.
+     */
+    public String config1;
+}
