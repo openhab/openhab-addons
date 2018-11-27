0de0e93de9c8151223e73f7abe8be9cf11872e5c
Zachary Christiansen <volfan6415@gmail.com>
2018-11-27 08:10:51 -0800

    - add retry count and request timout config parameter to thing - add high/low onvalue config paramater to thing - updates to httpUtils to use request timeout and retry count paramaters with http requests - addition of custon expception for retry timeout exceed exception - updates to handler to utilize retry timeout config paramater and request timeout paramater or defaults of 30 secods and 2 if none configured
    Signed-off-by: Zachary Christiansen <volfan6415@gmail.com> (github: volfan6415)

---
+++ addons/binding/org.openhab.binding.konnected/src/main/java/org/openhab/binding/konnected/internal/KonnectedHttpRetryExceeded.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.konnected.internal;
+
+/**
+ * Custom exception class to be thrown when number of retries is exceeded.
+ *
+ * @author Zachary Christiansen - Initial contribution
+ */
+@SuppressWarnings("serial")
+public class KonnectedHttpRetryExceeded extends Exception {
+    public KonnectedHttpRetryExceeded(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
