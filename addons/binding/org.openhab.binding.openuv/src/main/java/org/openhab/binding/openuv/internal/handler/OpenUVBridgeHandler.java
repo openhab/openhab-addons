566c2533b1af9c3e6dfdee4745bcc6311c2f75a2
Gaël L'hopital <gael@lhopital.org>
2018-11-28 23:57:46 +0100

    [OpenUV] Give a little more time to API timeout. (#4290)
    Signed-off-by: Gaël L'hopital <gael@lhopital.org>
    Signed-off-by: Maximilian Hess <mail@ne0h.de>

--- addons/binding/org.openhab.binding.openuv/src/main/java/org/openhab/binding/openuv/internal/handler/OpenUVBridgeHandler.java
+++ addons/binding/org.openhab.binding.openuv/src/main/java/org/openhab/binding/openuv/internal/handler/OpenUVBridgeHandler.java
@@ -15,6 +15,7 @@ import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -50,6 +51,7 @@ import com.google.gson.JsonDeserializer;
 @NonNullByDefault
 public class OpenUVBridgeHandler extends BaseBridgeHandler {
     private final Logger logger = LoggerFactory.getLogger(OpenUVBridgeHandler.class);
+    private static final int REQUEST_TIMEOUT = (int) TimeUnit.SECONDS.toMillis(30);
 
     private final Gson gson = new GsonBuilder()
             .registerTypeAdapter(DecimalType.class,
@@ -83,7 +85,7 @@ public class OpenUVBridgeHandler extends BaseBridgeHandler {
             // Check if the provided api key is valid for use with the OpenUV service
             try {
                 // Run the HTTP request and get the JSON response
-                OpenUVJsonResponse response = getUVData("0", "0");
+                OpenUVJsonResponse response = getUVData("0", "0", null);
                 if ("Invalid API Key".equalsIgnoreCase(response.getError())) {
                     error = "API key has to be fixed";
                     errorDetail = response.getError();
@@ -127,18 +129,15 @@ public class OpenUVBridgeHandler extends BaseBridgeHandler {
         super.handleRemoval();
     }
 
-    public OpenUVJsonResponse getUVData(String latitude, String longitude) throws IOException {
-        return getUVData(latitude, longitude, "");
-    }
-
-    public OpenUVJsonResponse getUVData(String latitude, String longitude, String altitude) throws IOException {
+    public OpenUVJsonResponse getUVData(String latitude, String longitude, @Nullable String altitude)
+            throws IOException {
         StringBuilder urlBuilder = new StringBuilder(BASE_URL).append("?lat=").append(latitude).append("&lng=")
                 .append(longitude);
 
-        if (!"".equals(altitude)) {
+        if (altitude != null) {
             urlBuilder.append("&alt=").append(altitude);
         }
-        String jsonData = HttpUtil.executeUrl("GET", urlBuilder.toString(), header, null, null, 2000);
+        String jsonData = HttpUtil.executeUrl("GET", urlBuilder.toString(), header, null, null, REQUEST_TIMEOUT);
         logger.debug("URL = {}", jsonData);
 
         return gson.fromJson(jsonData, OpenUVJsonResponse.class);
