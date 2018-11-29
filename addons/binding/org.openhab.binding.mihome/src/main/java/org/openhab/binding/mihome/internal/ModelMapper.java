7126a906edec3155a93c087dde738c4241aecaf5
Nagy Attila Gábor <mrbig@sneaker.hu>
2018-11-29 22:27:58 +0100

    [MiHome] added another model type for new Aquara switch (#4279)
    Aquara single key switch
    
    Signed-off-by: Nagy Attila Gabor <mrbig@sneaker.hu>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.mihome/src/main/java/org/openhab/binding/mihome/internal/ModelMapper.java
+++ addons/binding/org.openhab.binding.mihome/src/main/java/org/openhab/binding/mihome/internal/ModelMapper.java
@@ -53,6 +53,7 @@ public class ModelMapper {
         THING_MAP.put("sensor_switch.aq2", THING_TYPE_SENSOR_AQARA_SWITCH);
         THING_MAP.put("sensor_switch.aq3", THING_TYPE_SENSOR_AQARA_SWITCH);
         THING_MAP.put("remote.b1acn01", THING_TYPE_SENSOR_AQARA_SWITCH);
+        THING_MAP.put("remote.b186acn01", THING_TYPE_SENSOR_AQARA1);
         THING_MAP.put("smoke", THING_TYPE_SENSOR_SMOKE);
         THING_MAP.put("switch", THING_TYPE_SENSOR_SWITCH);
         THING_MAP.put("vibration", THING_TYPE_SENSOR_AQARA_VIBRATION);
@@ -82,6 +83,7 @@ public class ModelMapper {
         LABEL_MAP.put("sensor_switch.aq2", "Xiaomi Aqara Wireless Switch");
         LABEL_MAP.put("sensor_switch.aq3", "Xiaomi Aqara Wireless Switch with Accelerometer");
         LABEL_MAP.put("remote.b1acn01", "Xiaomi Aqara Wireless Switch");
+        LABEL_MAP.put("remote.b186acn01", "Xiaomi Aqara Wireless Switch");
         LABEL_MAP.put("smoke", "Xiaomi Mijia Honeywell Fire Alarm Detector");
         LABEL_MAP.put("switch", "Xiaomi Mi Wireless Switch");
         LABEL_MAP.put("vibration", "Xiaomi Aqara Smart Motion Sensor");
