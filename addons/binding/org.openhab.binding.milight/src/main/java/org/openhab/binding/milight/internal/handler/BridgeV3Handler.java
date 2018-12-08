7d82b5640c219c764bca6bff9ac947c2cad741e4
Patrick Fink <mail@pfink.de>
2018-12-08 19:46:19 +0100

    Milight V3: Use any free port instead of fixed port on client-side for bridge communication (resolves #4232)
    Signed-off-by: Patrick Fink <mail@pfink.de>

--- addons/binding/org.openhab.binding.milight/src/main/java/org/openhab/binding/milight/internal/handler/BridgeV3Handler.java
+++ addons/binding/org.openhab.binding.milight/src/main/java/org/openhab/binding/milight/internal/handler/BridgeV3Handler.java
@@ -72,7 +72,7 @@ public class BridgeV3Handler extends AbstractBridgeHandler {
             socket = new DatagramSocket(null);
             socket.setReuseAddress(true);
             socket.setBroadcast(true);
-            socket.bind(new InetSocketAddress(MilightBindingConstants.PORT_DISCOVER));
+            socket.bind(new InetSocketAddress(0));
         } catch (SocketException e) {
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR, e.getMessage());
         }
