ef6b4e9b63a9804c2b3839c9d372d38d677c3856
David Graeff <david.graeff@web.de>
2018-12-08 19:42:21 +0100

    Hue emulation: User creation fix, storage service fix, turn white fix, group items
    Fixes:
    * Fix user creation without a proposed username in the request. Incl. test
    * Fix: The hue value was wrongly applied to the saturation field. Incl. test
    * Fix usage of StorageService: Set the classloader
    * Set the saturation to 0 if a ct (color temperature) value is set.
      This is because Alexa only sets "ct" if you command her to turn the light white.
    * Only call writeToFile in LightItems once, after all items have been
      loaded up from the registry.
    * Don't load items twice from the registry.
    * Reload items whenever the tags configuration has changed.
    * Allow group items
    
    Features:
    * Add troubleshoot section to readme.
      Allow a pretty printed output for /api/{username}/lights?debug=true.
    * Add REST API POST support on /api/{username}/groups.
    
    Tests:
    * Add LightItems class unit tests for adding/updating items and group items
      by category and tags.
    * Add tests for setting the hue and saturation and turn a light from color to white
    
    Refactor:
    * Move UserAuth class out of DataStore into own HueUserAuth class
    
    Fixes #4293
    Fixes #4307
    
    Signed-off-by: David Graeff <david.graeff@web.de>

--- addons/io/org.openhab.io.hueemulation.test/src/test/java/org/openhab/io/hueemulation/internal/HueEmulationServiceOSGiTest.java
+++ addons/io/org.openhab.io.hueemulation.test/src/test/java/org/openhab/io/hueemulation/internal/HueEmulationServiceOSGiTest.java
@@ -14,7 +14,8 @@ package org.openhab.io.hueemulation.internal;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.*;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -27,8 +28,13 @@ import java.net.URL;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
+import org.eclipse.smarthome.core.events.EventPublisher;
+import org.eclipse.smarthome.core.items.GroupItem;
 import org.eclipse.smarthome.core.items.Item;
 import org.eclipse.smarthome.core.items.ItemRegistry;
+import org.eclipse.smarthome.core.items.events.ItemCommandEvent;
+import org.eclipse.smarthome.core.library.types.HSBType;
+import org.eclipse.smarthome.core.library.types.OnOffType;
 import org.eclipse.smarthome.core.service.ReadyMarker;
 import org.eclipse.smarthome.core.service.ReadyService;
 import org.eclipse.smarthome.test.java.JavaOSGiTest;
@@ -38,9 +44,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.openhab.io.hueemulation.internal.dto.HueDataStore.UserAuth;
 import org.openhab.io.hueemulation.internal.dto.HueDevice;
+import org.openhab.io.hueemulation.internal.dto.HueStateColorBulb;
 import org.openhab.io.hueemulation.internal.dto.HueUnauthorizedConfig;
+import org.openhab.io.hueemulation.internal.dto.HueUserAuth;
 import org.osgi.service.cm.ConfigurationAdmin;
 
 import com.google.gson.Gson;
@@ -58,6 +65,12 @@ public class HueEmulationServiceOSGiTest extends JavaOSGiTest {
     @Mock
     ConfigurationAdmin configurationAdmin;
 
+    @Mock
+    EventPublisher eventPublisher;
+
+    @Mock
+    Item item;
+
     String host;
 
     @SuppressWarnings("null")
@@ -73,6 +86,10 @@ public class HueEmulationServiceOSGiTest extends JavaOSGiTest {
         assertThat(readyService, notNullValue());
         hueService = getService(HueEmulationService.class, HueEmulationService.class);
         assertThat(hueService, notNullValue());
+
+        when(item.getName()).thenReturn("itemname");
+
+        hueService.setEventPublisher(eventPublisher);
 
         readyService.markReady(new ReadyMarker("fake", "org.eclipse.smarthome.model.core"));
         waitFor(() -> hueService.discovery != null, 5000, 100);
@@ -154,6 +171,19 @@ public class HueEmulationServiceOSGiTest extends JavaOSGiTest {
         body = read(c);
         assertThat(body, containsString("success"));
         assertThat(hueService.ds.config.whitelist.get("testuser").name, is("label"));
+        hueService.ds.config.whitelist.clear();
+
+        // Add user name without proposing one (the bridge generates one)
+        body = "{'devicetype':'label'}";
+        c = (HttpURLConnection) new URL(host + "/api").openConnection();
+        c.setRequestProperty("Content-Type", "application/json");
+        c.setRequestMethod("POST");
+        c.setDoOutput(true);
+        c.getOutputStream().write(body.getBytes(), 0, body.getBytes().length);
+        assertThat(c.getResponseCode(), is(200));
+        body = read(c);
+        assertThat(body, containsString("success"));
+        assertThat(body, containsString(hueService.ds.config.whitelist.keySet().iterator().next()));
     }
 
     @Test
@@ -161,14 +191,13 @@ public class HueEmulationServiceOSGiTest extends JavaOSGiTest {
         HttpURLConnection c;
         String body;
 
-        hueService.ds.config.whitelist.put("testuser", new UserAuth("testUserLabel"));
+        hueService.ds.config.whitelist.put("testuser", new HueUserAuth("testUserLabel"));
 
         c = (HttpURLConnection) new URL(host + "/api/testuser/lights").openConnection();
         assertThat(c.getResponseCode(), is(200));
         body = read(c);
         assertThat(body, containsString("{}"));
 
-        Item item = mock(Item.class);
         hueService.ds.lights.put(1, new HueDevice(item, "switch", DeviceType.SwitchType));
         hueService.ds.lights.put(2, new HueDevice(item, "color", DeviceType.ColorType));
         hueService.ds.lights.put(3, new HueDevice(item, "white", DeviceType.WhiteTemperatureType));
@@ -186,5 +215,118 @@ public class HueEmulationServiceOSGiTest extends JavaOSGiTest {
         assertThat(c.getResponseCode(), is(200));
         body = read(c);
         assertThat(body, containsString("color"));
+    }
+
+    @Test
+    public void LightGroupItemSwitchTest()
+            throws InterruptedException, ExecutionException, TimeoutException, IOException {
+        HttpURLConnection c;
+        String body;
+
+        GroupItem gitem = new GroupItem("group", item);
+        hueService.ds.config.whitelist.put("testuser", new HueUserAuth("testUserLabel"));
+        hueService.ds.lights.put(7, new HueDevice(gitem, "switch", DeviceType.SwitchType));
+
+        body = "{'on':true}";
+        c = (HttpURLConnection) new URL(host + "/api/testuser/lights/7/state").openConnection();
+        c.setRequestProperty("Content-Type", "application/json");
+        c.setRequestMethod("PUT");
+        c.setDoOutput(true);
+        c.getOutputStream().write(body.getBytes(), 0, body.getBytes().length);
+        assertThat(c.getResponseCode(), is(200));
+        body = read(c);
+        assertThat(body, containsString("success"));
+        assertThat(body, containsString("on"));
+
+        verify(eventPublisher).post(argThat(ce -> assertOnValue((ItemCommandEvent) ce, true)));
+    }
+
+    @Test
+    public void LightHueTest() throws InterruptedException, ExecutionException, TimeoutException, IOException {
+        HttpURLConnection c;
+        String body;
+
+        hueService.ds.config.whitelist.put("testuser", new HueUserAuth("testUserLabel"));
+        hueService.ds.lights.put(2, new HueDevice(item, "color", DeviceType.ColorType));
+
+        body = "{'hue':1000}";
+        c = (HttpURLConnection) new URL(host + "/api/testuser/lights/2/state").openConnection();
+        c.setRequestProperty("Content-Type", "application/json");
+        c.setRequestMethod("PUT");
+        c.setDoOutput(true);
+        c.getOutputStream().write(body.getBytes(), 0, body.getBytes().length);
+        assertThat(c.getResponseCode(), is(200));
+        body = read(c);
+        assertThat(body, containsString("success"));
+        assertThat(body, containsString("hue"));
+
+        verify(eventPublisher).post(argThat(ce -> assertHueValue((ItemCommandEvent) ce, 1000)));
+    }
+
+    @Test
+    public void LightSaturationTest() throws InterruptedException, ExecutionException, TimeoutException, IOException {
+        HttpURLConnection c;
+        String body;
+
+        hueService.ds.config.whitelist.put("testuser", new HueUserAuth("testUserLabel"));
+        hueService.ds.lights.put(2, new HueDevice(item, "color", DeviceType.ColorType));
+
+        body = "{'sat':50}";
+        c = (HttpURLConnection) new URL(host + "/api/testuser/lights/2/state").openConnection();
+        c.setRequestProperty("Content-Type", "application/json");
+        c.setRequestMethod("PUT");
+        c.setDoOutput(true);
+        c.getOutputStream().write(body.getBytes(), 0, body.getBytes().length);
+        assertThat(c.getResponseCode(), is(200));
+        body = read(c);
+        assertThat(body, containsString("success"));
+        assertThat(body, containsString("sat"));
+
+        verify(eventPublisher).post(argThat(ce -> assertSatValue((ItemCommandEvent) ce, 50)));
+    }
+
+    /**
+     * Amazon echos are setting ct only, if commanded to turn a light white.
+     */
+    @Test
+    public void LightToWhiteTest() throws InterruptedException, ExecutionException, TimeoutException, IOException {
+        HttpURLConnection c;
+        String body;
+
+        // We start with a coloured state
+        when(item.getState()).thenReturn(new HSBType("100,100,100"));
+        hueService.ds.config.whitelist.put("testuser", new HueUserAuth("testUserLabel"));
+        hueService.ds.lights.put(2, new HueDevice(item, "color", DeviceType.ColorType));
+
+        body = "{'ct':500}";
+        c = (HttpURLConnection) new URL(host + "/api/testuser/lights/2/state").openConnection();
+        c.setRequestProperty("Content-Type", "application/json");
+        c.setRequestMethod("PUT");
+        c.setDoOutput(true);
+        c.getOutputStream().write(body.getBytes(), 0, body.getBytes().length);
+        assertThat(c.getResponseCode(), is(200));
+        body = read(c);
+        assertThat(body, containsString("success"));
+        assertThat(body, containsString("sat"));
+        assertThat(body, containsString("ct"));
+
+        // Saturation is expected to be 0 -> white light
+        verify(eventPublisher).post(argThat(ce -> assertSatValue((ItemCommandEvent) ce, 0)));
+    }
+
+    private boolean assertHueValue(ItemCommandEvent ce, int hueValue) {
+        assertThat(((HSBType) ce.getItemCommand()).getHue().intValue(), is(hueValue * 360 / HueStateColorBulb.MAX_HUE));
+        return true;
+    }
+
+    private boolean assertSatValue(ItemCommandEvent ce, int satValue) {
+        assertThat(((HSBType) ce.getItemCommand()).getSaturation().intValue(),
+                is(satValue * 100 / HueStateColorBulb.MAX_SAT));
+        return true;
+    }
+
+    private boolean assertOnValue(ItemCommandEvent ce, boolean value) {
+        assertThat(ce.getItemCommand(), is(OnOffType.from(value)));
+        return true;
     }
 }
