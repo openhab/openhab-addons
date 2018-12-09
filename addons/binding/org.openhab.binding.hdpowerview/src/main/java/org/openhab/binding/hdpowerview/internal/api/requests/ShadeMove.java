1c52dc93f3933adf742fc2face29af1d9e18b4b9
tomdc01 <tom@rooted.net>
2018-12-09 16:08:32 -0500

    [HD PowerView] Fix quoted ID issue (#3470)
    Fixes #3454
    
    Signed-off-by: Tom Parker (GitHub: tomdc01)

--- addons/binding/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/internal/api/requests/ShadeMove.java
+++ addons/binding/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/internal/api/requests/ShadeMove.java
@@ -19,7 +19,7 @@ public class ShadeMove {
 
     ShadeIdPosition shade;
 
-    public ShadeMove(String id, ShadePosition position) {
+    public ShadeMove(int id, ShadePosition position) {
         this.shade = new ShadeIdPosition(id, position);
     }
 }
