c73ba7212e5053bfe4d005ff85abad050f8518ec
Flole998 <Flole998@users.noreply.github.com>
2018-12-17 08:12:32 +0100

    [Hue emulation] Implement XY Listener (#4349)
    Implement XY Colormode
    Implement Testcases for XY Functionality
    Change Exception types
    
    Signed-off-by: Flole <flole@flole.de>
    Signed-off-by: Maximilian Hess <mail@ne0h.de>

--- addons/io/org.openhab.io.hueemulation/src/main/java/org/openhab/io/hueemulation/internal/dto/HueStateColorBulb.java
+++ addons/io/org.openhab.io.hueemulation/src/main/java/org/openhab/io/hueemulation/internal/dto/HueStateColorBulb.java
@@ -17,6 +17,7 @@ import org.eclipse.smarthome.core.library.types.PercentType;
  *
  * @author Dan Cunningham - Initial contribution
  * @author David Graeff - "extended color light bulbs" support
+ * @author Florian Lentz - added xy support
  *
  */
 public class HueStateColorBulb extends HueStateBulb {
@@ -78,17 +79,65 @@ public class HueStateColorBulb extends HueStateBulb {
      * Converts this HueState to a HSBType
      */
     public HSBType toHSBType() {
-        int bri = this.bri * 100 / MAX_BRI;
-        int sat = this.sat * 100 / MAX_SAT;
-        int hue = this.hue * 360 / MAX_HUE;
-
-        if (!this.on) {
-            bri = 0;
+        if (colormode == ColorMode.xy) {
+            int i;
+            double d = this.xy[0];
+            d = this.xy[1];
+            double y = ((double) this.bri) / 100.0d;
+            double x = (y / this.xy[1]) * this.xy[0];
+            double z = (y / this.xy[1]) * ((1.0d - this.xy[0]) - this.xy[1]);
+            int r = (int) (Math.abs(((1.4628067016601562d * x) - (0.18406230211257935d * y)) - (0.2743605971336365d * z)) * 255.0d);
+            int g = (int) (Math.abs((((-x) * 0.5217933058738708d) + (1.4472380876541138d * y)) + (0.06772270053625107d * z)) * 255.0d);
+            int b = (int) (Math.abs(((0.03493420034646988d * x) - (0.09689299762248993d * y)) + (1.288409948348999d * z)) * 255.0d);
+            if (r < g) {
+                i = r;
+            } else {
+                i = g;
+            }
+            double minValue = i;
+            if (minValue >= b) {
+                minValue = b;
+            }
+            double maxValue = (r > g ? r : g);
+            if (maxValue <= b) {
+                maxValue = b;
+            }
+            double delta = maxValue - minValue;
+            if (maxValue <= 0.0d) {
+                return new HSBType(new DecimalType(0), new PercentType(100), new PercentType((this.bri * 100) / MAX_BRI));
+            }
+            double h;
+            if (((double) r) >= maxValue) {
+                h = ((double) (g - b)) / delta;
+            } else if (((double) g) >= maxValue) {
+                h = 2.0d + (((double) (b - r)) / delta);
+            } else {
+                h = 4.0d + (((double) (r - g)) / delta);
+            }
+            h *= 60.0d;
+            if (h < 0.0d) {
+                h += 360.0d;
+            }
+            double hueSat = Math.floor((delta / maxValue) * 254.0d);
+            int percentSat = (int) ((100.0d * hueSat) / ((double) MAX_SAT));
+            if (!this.on) {
+                this.bri = 0;
+            }
+            return new HSBType(new DecimalType((Math.floor(182.04d * h) * 360.0d) / ((double) MAX_HUE)), new PercentType(percentSat), new PercentType((this.bri * 100) / MAX_BRI));
+       
+        } else {
+            int bri = this.bri * 100 / MAX_BRI;
+            int sat = this.sat * 100 / MAX_SAT;
+            int hue = this.hue * 360 / MAX_HUE;
+   
+            if (!this.on) {
+                bri = 0;
+            }
+            if (colormode == ColorMode.ct) {
+                sat = 0;
+            }
+            return new HSBType(new DecimalType(hue), new PercentType(sat), new PercentType(bri));
         }
-        if (colormode == ColorMode.ct) {
-            sat = 0;
-        }
-        return new HSBType(new DecimalType(hue), new PercentType(sat), new PercentType(bri));
     }
 
     @Override
