1aa10d8f3e05db6a0bd12a2127544a2de8e768e9
macrauder <macrauder@users.noreply.github.com>
2018-12-22 19:26:58 +0100

    [deconz] Support for window and door open/close sensors. (#4418)
    Signed-off-by: Thomas Herbst <thomas.herbsth@mac.com> (github: macrauder)

--- addons/binding/org.openhab.binding.deconz/src/main/java/org/openhab/binding/deconz/internal/handler/SensorThingHandler.java
+++ addons/binding/org.openhab.binding.deconz/src/main/java/org/openhab/binding/deconz/internal/handler/SensorThingHandler.java
@@ -19,6 +19,7 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.core.library.types.DecimalType;
 import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.library.types.OpenClosedType;
 import org.eclipse.smarthome.core.library.types.QuantityType;
 import org.eclipse.smarthome.core.library.types.StringType;
 import org.eclipse.smarthome.core.library.unit.SIUnits;
@@ -209,6 +210,7 @@ public class SensorThingHandler extends BaseThingHandler implements ValueUpdateL
         Integer buttonevent = state.buttonevent;
         Integer status = state.status;
         Boolean presence = state.presence;
+        Boolean open = state.open;
         Integer power = state.power;
         Integer lux = state.lux;
         Float temperature = state.temperature;
@@ -254,6 +256,11 @@ public class SensorThingHandler extends BaseThingHandler implements ValueUpdateL
             case BindingConstants.CHANNEL_VALUE:
                 if (status != null) {
                     updateState(channelID, new DecimalType(status));
+                }
+                break;
+            case BindingConstants.CHANNEL_OPENCLOSE:
+                if (open != null) {
+                    updateState(channelID, open ? OpenClosedType.OPEN : OpenClosedType.CLOSED);
                 }
                 break;
         }
