2a00f7259a7e2f551fb62377b9de093faed593ac
Philipp Arndt <2f.mail@gmx.de>
2019-01-05 15:31:42 +0100

    [homekit] Added support for contact sensors Signed-off-by: Philipp Arndt <2f.mail@gmx.de> (github: philipparndt)

--- addons/io/org.openhab.io.homekit/src/main/java/org/openhab/io/homekit/internal/accessories/HomekitAccessoryFactory.java
+++ addons/io/org.openhab.io.homekit/src/main/java/org/openhab/io/homekit/internal/accessories/HomekitAccessoryFactory.java
@@ -45,6 +45,9 @@ public class HomekitAccessoryFactory {
 
             case HUMIDITY_SENSOR:
                 return new HomekitHumiditySensorImpl(taggedItem, itemRegistry, updater);
+
+            case CONTACT_SENSOR:
+                return new HomekitContactSensorImpl(taggedItem, itemRegistry, updater);
         }
 
         throw new Exception("Unknown homekit type: " + taggedItem.getDeviceType());
---
+++ addons/io/org.openhab.io.homekit/src/main/java/org/openhab/io/homekit/internal/accessories/HomekitContactSensorImpl.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2010-2019 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.io.homekit.internal.accessories;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.eclipse.smarthome.core.items.ItemRegistry;
+import org.eclipse.smarthome.core.library.items.ContactItem;
+import org.eclipse.smarthome.core.library.types.OpenClosedType;
+import org.openhab.io.homekit.internal.HomekitAccessoryUpdater;
+import org.openhab.io.homekit.internal.HomekitTaggedItem;
+
+import com.beowulfe.hap.HomekitCharacteristicChangeCallback;
+import com.beowulfe.hap.accessories.ContactSensor;
+import com.beowulfe.hap.accessories.properties.ContactState;
+
+/**
+ * Implements Contact sensor using an Item that provides an On/Off state.
+ *
+ * @author Philipp Arndt - Initial contribution
+ */
+public class HomekitContactSensorImpl extends AbstractHomekitAccessoryImpl<ContactItem> implements ContactSensor {
+
+    public HomekitContactSensorImpl(HomekitTaggedItem taggedItem, ItemRegistry itemRegistry,
+            HomekitAccessoryUpdater updater) {
+        super(taggedItem, itemRegistry, updater, ContactItem.class);
+    }
+
+    @Override
+    public CompletableFuture<ContactState> getCurrentState() {
+        OpenClosedType state = getItem().getStateAs(OpenClosedType.class);
+        return CompletableFuture
+                .completedFuture(state == OpenClosedType.CLOSED ? ContactState.DETECTED : ContactState.NOT_DETECTED);
+    }
+
+    @Override
+    public void subscribeContactState(HomekitCharacteristicChangeCallback callback) {
+        getUpdater().subscribe(getItem(), callback);
+    }
+
+    @Override
+    public void unsubscribeContactState() {
+        getUpdater().unsubscribe(getItem());
+    }
+
+}
