e646a163afa1ffc1f61769edebb3e71c4fca8554
Daniel Weber <25605184+fruggy83@users.noreply.github.com>
2019-01-08 15:15:29 +0100

    * Implemented motion sensor EEP family A5_07, fixes #4543 (#4544)
    Signed-off-by: Daniel Weber <uni@fruggy.de>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

---
+++ addons/binding/org.openhab.binding.enocean/src/main/java/org/openhab/binding/enocean/internal/eep/A5_07/A5_07.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2010-2019 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.enocean.internal.eep.A5_07;
+
+import static org.openhab.binding.enocean.internal.EnOceanBindingConstants.*;
+
+import org.eclipse.smarthome.config.core.Configuration;
+import org.eclipse.smarthome.core.library.types.QuantityType;
+import org.eclipse.smarthome.core.library.unit.SmartHomeUnits;
+import org.eclipse.smarthome.core.types.State;
+import org.eclipse.smarthome.core.types.UnDefType;
+import org.openhab.binding.enocean.internal.eep.Base._4BSMessage;
+import org.openhab.binding.enocean.internal.messages.ERP1Message;
+
+/**
+ *
+ * @author Daniel Weber - Initial contribution
+ */
+public abstract class A5_07 extends _4BSMessage {
+
+    public A5_07(ERP1Message packet) {
+        super(packet);
+    }
+
+    protected abstract State getIllumination();
+
+    protected abstract State getMotion();
+
+    protected abstract State getSupplyVoltage();
+
+    protected State getSupplyVoltage(int value) {
+
+        if (value > 250) {
+            logger.warn("EEP A5-07 error code {}", value);
+            return UnDefType.UNDEF;
+        }
+
+        double voltage = value / 50.0; // 0..250 = 0.0..5.0V
+        return new QuantityType<>(voltage, SmartHomeUnits.VOLT);
+    }
+
+    @Override
+    protected State convertToStateImpl(String channelId, String channelTypeId, State currentState,
+            Configuration config) {
+        if (!isValid()) {
+            return UnDefType.UNDEF;
+        }
+
+        if (channelId.equals(CHANNEL_ILLUMINATION)) {
+            return getIllumination();
+        } else if (channelId.equals(CHANNEL_MOTIONDETECTION)) {
+            return getMotion();
+        } else if (channelId.equals(CHANNEL_BATTERY_VOLTAGE)) {
+            return getSupplyVoltage();
+        }
+
+        return UnDefType.UNDEF;
+    }
+}
