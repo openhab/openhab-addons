b70d9dd048fe763c9eecb26f59ccbee0f3111ded
Kai Kreuzer <kai@openhab.org>
2019-01-19 01:34:08 +0100

    refactored transform bundles
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

---
+++ addons/transform/org.openhab.transform.xpath.test/src/test/java/org/openhab/transform/xpath/internal/XPathTransformationServiceTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.transform.xpath.internal;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.smarthome.core.transform.TransformationException;
+import org.junit.Before;
+import org.junit.Test;
+import org.openhab.transform.xpath.internal.XPathTransformationService;
+
+/**
+ * @author Thomas.Eichstaedt-Engelen
+ */
+public class XPathTransformationServiceTest extends AbstractTransformationServiceTest {
+
+    private XPathTransformationService processor;
+
+    @Before
+    public void init() {
+        processor = new XPathTransformationService();
+    }
+
+    @Test
+    public void testTransformByXPath() throws TransformationException {
+
+        // method under test
+        String transformedResponse = processor.transform("//current_conditions/temp_c/@data", source);
+
+        // Asserts
+        assertEquals("8", transformedResponse);
+    }
+
+}
