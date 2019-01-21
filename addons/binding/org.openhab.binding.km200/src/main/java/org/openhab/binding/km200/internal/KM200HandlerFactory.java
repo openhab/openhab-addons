a7a76a4b292c1ebd0e995dbbf104526e6327dd22
Christoph Weitkamp <github@christophweitkamp.de>
2019-01-21 18:29:42 +0100

    Removed Guava dependency (#4642)
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>
    Signed-off-by: Maximilian Hess <mail@ne0h.de>

--- addons/binding/org.openhab.binding.km200/src/main/java/org/openhab/binding/km200/internal/KM200HandlerFactory.java
+++ addons/binding/org.openhab.binding.km200/src/main/java/org/openhab/binding/km200/internal/KM200HandlerFactory.java
@@ -12,10 +12,13 @@
  */
 package org.openhab.binding.km200.internal;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.smarthome.config.core.Configuration;
@@ -38,8 +41,6 @@ import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Sets;
-
 /**
  * The {@link KM200HandlerFactory} is responsible for creating things and thing
  * handlers.
@@ -49,8 +50,9 @@ import com.google.common.collect.Sets;
 @Component(service = ThingHandlerFactory.class, immediate = true, configurationPid = "binding.km200", configurationPolicy = ConfigurationPolicy.OPTIONAL)
 public class KM200HandlerFactory extends BaseThingHandlerFactory {
 
-    public final Set<ThingTypeUID> SUPPORTED_ALL_THING_TYPES_UIDS = Sets
-            .union(KM200GatewayHandler.SUPPORTED_THING_TYPES_UIDS, KM200ThingHandler.SUPPORTED_THING_TYPES_UIDS);
+    public final Set<ThingTypeUID> SUPPORTED_ALL_THING_TYPES_UIDS = Collections
+            .unmodifiableSet(Stream.concat(KM200GatewayHandler.SUPPORTED_THING_TYPES_UIDS.stream(),
+                    KM200ThingHandler.SUPPORTED_THING_TYPES_UIDS.stream()).collect(Collectors.toSet()));
 
     private Map<ThingUID, ServiceRegistration<?>> discoveryServiceRegs = new HashMap<>();
 
