dae4df23482be83a38dde9166701dcad60b04c56
Wouter Born <github@maindrain.net>
2019-01-22 09:57:11 +0100

    [weatherunderground] Changes ESH packages to openHAB (#4647)
    Signed-off-by: Wouter Born <eclipse@maindrain.net>
    Signed-off-by: Maximilian Hess <mail@ne0h.de>

--- addons/binding/org.openhab.binding.weatherunderground/src/main/java/org/openhab/binding/weatherunderground/internal/json/WeatherUndergroundJsonSimpleForecast.java
+++ addons/binding/org.openhab.binding.weatherunderground/src/main/java/org/openhab/binding/weatherunderground/internal/json/WeatherUndergroundJsonSimpleForecast.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.eclipse.smarthome.binding.weatherunderground.internal.json;
+package org.openhab.binding.weatherunderground.internal.json;
 
 import java.util.List;
 
