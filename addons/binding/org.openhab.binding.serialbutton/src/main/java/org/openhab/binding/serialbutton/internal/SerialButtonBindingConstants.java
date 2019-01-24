9b69cbd806be6a9421c82661a2ce3db6d11ae968
Wouter Born <eclipse@maindrain.net>
2019-01-24 22:22:34 +0100

    [serialbutton] Made constants and handler packages internal
    See eclipse/smarthome#4330
    
    Signed-off-by: Wouter Born <eclipse@maindrain.net>

--- addons/binding/org.openhab.binding.serialbutton/src/main/java/org/openhab/binding/serialbutton/internal/SerialButtonBindingConstants.java
+++ addons/binding/org.openhab.binding.serialbutton/src/main/java/org/openhab/binding/serialbutton/internal/SerialButtonBindingConstants.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.serialbutton;
+package org.openhab.binding.serialbutton.internal;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
