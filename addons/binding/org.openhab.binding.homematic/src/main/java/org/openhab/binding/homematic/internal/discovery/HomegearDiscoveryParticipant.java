57aa65c6e259a0487004d09a8b41ddcf54e4606a
Wouter Born <github@maindrain.net>
2019-01-24 21:44:36 +0100

    Fix all remaining UpnpDiscoveryParticipant deprecations (#4670)
    Signed-off-by: Wouter Born <eclipse@maindrain.net>
    Signed-off-by: Maximilian Hess <mail@ne0h.de>

--- addons/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/discovery/HomegearDiscoveryParticipant.java
+++ addons/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/discovery/HomegearDiscoveryParticipant.java
@@ -21,7 +21,7 @@ import java.util.Set;
 
 import org.eclipse.smarthome.config.discovery.DiscoveryResult;
 import org.eclipse.smarthome.config.discovery.DiscoveryResultBuilder;
-import org.eclipse.smarthome.config.discovery.UpnpDiscoveryParticipant;
+import org.eclipse.smarthome.config.discovery.upnp.UpnpDiscoveryParticipant;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.ThingUID;
 import org.jupnp.model.meta.DeviceDetails;
