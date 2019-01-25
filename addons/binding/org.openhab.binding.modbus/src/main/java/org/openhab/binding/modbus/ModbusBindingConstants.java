23e6cb2d5c6b8b18cf9f6ae0be94ba4f354135d2
Wouter Born <github@maindrain.net>
2019-01-25 12:16:51 +0100

    Revert 4685 modbus internal (#4704)
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.modbus/src/main/java/org/openhab/binding/modbus/ModbusBindingConstants.java
+++ addons/binding/org.openhab.binding.modbus/src/main/java/org/openhab/binding/modbus/ModbusBindingConstants.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.modbus.internal;
+package org.openhab.binding.modbus;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
