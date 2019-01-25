0e30aa27f6df82ff3fb9febb22d5e7bc3c1f86b1
Wouter Born <github@maindrain.net>
2019-01-25 12:13:06 +0100

    [sonyaudio] Made constants and handler packages internal (#4694)
    See eclipse/smarthome#4330
    
    Signed-off-by: Wouter Born <eclipse@maindrain.net>
    Signed-off-by: Pshatsillo <pshatsillo@gmail.com>

--- addons/binding/org.openhab.binding.sonyaudio/src/main/java/org/openhab/binding/sonyaudio/internal/handler/SrsZr5Handler.java
+++ addons/binding/org.openhab.binding.sonyaudio/src/main/java/org/openhab/binding/sonyaudio/internal/handler/SrsZr5Handler.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.sonyaudio.handler;
+package org.openhab.binding.sonyaudio.internal.handler;
 
 import java.io.IOException;
 import java.util.Map;
