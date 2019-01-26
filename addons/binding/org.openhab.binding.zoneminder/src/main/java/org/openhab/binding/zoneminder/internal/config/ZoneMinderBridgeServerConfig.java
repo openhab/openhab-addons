a0d87f0d63446fd6e7b96368fa0eb3febba006a8
VelinYordanov <velin.iordanov@gmail.com>
2019-01-26 14:12:01 +0200

    [zoneminder] Made constants and handler packages ineternal (#4037)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.zoneminder/src/main/java/org/openhab/binding/zoneminder/internal/config/ZoneMinderBridgeServerConfig.java
+++ addons/binding/org.openhab.binding.zoneminder/src/main/java/org/openhab/binding/zoneminder/internal/config/ZoneMinderBridgeServerConfig.java
@@ -12,7 +12,7 @@
  */
 package org.openhab.binding.zoneminder.internal.config;
 
-import org.openhab.binding.zoneminder.ZoneMinderConstants;
+import org.openhab.binding.zoneminder.internal.ZoneMinderConstants;
 
 /**
  * Configuration data according to zoneminderserver.xml
