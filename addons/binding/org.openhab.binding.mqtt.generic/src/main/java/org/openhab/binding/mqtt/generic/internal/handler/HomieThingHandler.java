c13c5811f50cf0e3898b30b880c50fd42323984b
Patrick Dobler <patrick.dobler@gmail.com>
2019-01-26 01:00:33 +0100

    revert changes

--- addons/binding/org.openhab.binding.mqtt.generic/src/main/java/org/openhab/binding/mqtt/generic/internal/handler/HomieThingHandler.java
+++ addons/binding/org.openhab.binding.mqtt.generic/src/main/java/org/openhab/binding/mqtt/generic/internal/handler/HomieThingHandler.java
@@ -186,7 +186,7 @@ public class HomieThingHandler extends AbstractMQTTThingHandler implements Devic
 
     /**
      * Callback of {@link DelayedBatchProcessing}.
-     * Add all newly discovered nodes and properties to the Thing.
+     * Add all newly discovered nodes and properties to the Thing and start subscribe to each channel state topic.
      */
     @Override
     public void accept(@Nullable List<Object> t) {
@@ -198,5 +198,11 @@ public class HomieThingHandler extends AbstractMQTTThingHandler implements Devic
                 .map(prop -> prop.getChannel()).collect(Collectors.toList());
         updateThing(editThing().withChannels(channels).build());
         updateProperty(MqttBindingConstants.HOMIE_PROPERTY_VERSION, device.attributes.homie);
+        final MqttBrokerConnection connection = this.connection;
+        if (connection != null) {
+            device.startChannels(connection, scheduler, attributeReceiveTimeout, this).thenRun(() -> {
+                logger.trace("Homie device {} fully attached", device.attributes.name);
+            });
+        }
     }
 }
