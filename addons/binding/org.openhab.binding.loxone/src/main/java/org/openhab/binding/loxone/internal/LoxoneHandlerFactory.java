e844240aaad54a1b944fee61bcf35d5027f26bcc
VelinYordanov <velin.iordanov@gmail.com>
2019-01-26 17:05:36 +0200

    [loxone] Made constants and handler packages internal in the loxone binding (#3973)
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>

--- addons/binding/org.openhab.binding.loxone/src/main/java/org/openhab/binding/loxone/internal/LoxoneHandlerFactory.java
+++ addons/binding/org.openhab.binding.loxone/src/main/java/org/openhab/binding/loxone/internal/LoxoneHandlerFactory.java
@@ -20,8 +20,7 @@ import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
 import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
-import org.openhab.binding.loxone.LoxoneBindingConstants;
-import org.openhab.binding.loxone.handler.LoxoneMiniserverHandler;
+import org.openhab.binding.loxone.internal.handler.LoxoneMiniserverHandler;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
