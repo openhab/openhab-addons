df3451d5ff0e74dab1b3f10ea31ecda6e5ad8a97
Wouter Born <github@maindrain.net>
2019-01-26 11:56:44 +0100

    [ZoneMinder] Use DS annotations, fix static code analysis findings (#3382)
    Signed-off-by: Wouter Born <eclipse@maindrain.net>
    Signed-off-by: Pshatsillo <pshatsillo@gmail.com>

--- addons/binding/org.openhab.binding.zoneminder/src/main/java/org/openhab/binding/zoneminder/handler/ZoneMinderThingType.java
+++ addons/binding/org.openhab.binding.zoneminder/src/main/java/org/openhab/binding/zoneminder/handler/ZoneMinderThingType.java
@@ -15,7 +15,7 @@ package org.openhab.binding.zoneminder.handler;
 /**
  * Enumerator for each Bridge and Thing
  *
- * @author Martin S. Eskildsen
+ * @author Martin S. Eskildsen - Initial contribution
  */
 public enum ZoneMinderThingType {
     ZoneMinderServerBridge,
