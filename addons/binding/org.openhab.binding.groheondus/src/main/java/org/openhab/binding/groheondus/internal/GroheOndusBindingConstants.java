6c50003de33f7f4085e1d93197c0137b0acfa228
Florian <florian.schmidt.welzow@t-online.de>
2019-01-27 23:03:30 +0100

    [groheondus] Export the waterconsumption value of the grohe sense guard (#4636)
    * Export the waterconsumption value of the grohe sense guard
    
    The waterconsumption is a combined metric of the total consumed water
    registered by the grohe sense guard. The channel allows you to get the
    value of a specific timeframe (in days) from 1 to up to 90 days (the 90
    days is a restriction of the api).
    
    This also fixes (by an update of the api library) the issue of
    deserializing an array to an actual appliance.
    
    Fixes #4554
    
    Signed-off-by: Florian <florian.schmidt.welzow@t-online.de>
    Signed-off-by: Pshatsillo <pshatsillo@gmail.com>

--- addons/binding/org.openhab.binding.groheondus/src/main/java/org/openhab/binding/groheondus/internal/GroheOndusBindingConstants.java
+++ addons/binding/org.openhab.binding.groheondus/src/main/java/org/openhab/binding/groheondus/internal/GroheOndusBindingConstants.java
@@ -31,7 +31,10 @@ public class GroheOndusBindingConstants {
     public static final String CHANNEL_PRESSURE = "pressure";
     public static final String CHANNEL_TEMPERATURE_GUARD = "temperature_guard";
     public static final String CHANNEL_VALVE_OPEN = "valve_open";
+    public static final String CHANNEL_WATERCONSUMPTION = "waterconsumption";
     public static final String CHANNEL_TEMPERATURE = "temperature";
     public static final String CHANNEL_HUMIDITY = "humidity";
     public static final String CHANNEL_BATTERY = "battery";
+
+    public static final String CHANNEL_CONFIG_TIMEFRAME = "timeframe";
 }
