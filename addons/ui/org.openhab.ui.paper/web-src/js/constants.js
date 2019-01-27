a4c88f90cc6d73827134ff30a8ebff11d00d7f34
Kai Kreuzer <kai@openhab.org>
2019-01-27 11:16:45 +0100

    Cleaning migrated ESH add-ons (#4724)
    * Cleaning migrated ESH add-ons
    - merged org.openhab.io.sound into org.openhab.io.javasound
    - added iconset feature to UIs
    - added openHAB customisation to Paper UI
    - removed commons-net dependency from Basic UI
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- addons/ui/org.openhab.ui.paper/web-src/js/constants.js
+++ addons/ui/org.openhab.ui.paper/web-src/js/constants.js
@@ -1,20 +1,23 @@
 /* This file can be be overridden by fragments for customization */
-angular.module('PaperUI.constants', []).constant('globalConfig', {
-    'advancedDefault' : false,
-    'defaultRoute' : '/control'
+angular.module('PaperUI.constants', [])
+.constant('globalConfig', {
+   'advancedDefault': true,
+   'defaultRoute': '/inbox/search'
 }).constant('restConfig', {
-    'restPath' : '/rest',
-    'eventPath' : $('#authentication').data('access-token') != '{{ACCESS_TOKEN}}' ? '/rest/events?access_token=' + $('#authentication').data('access-token') : '/rest/events'
+  'restPath': '/rest',
+  'eventPath': $('#authentication').data('access-token') != '{{ACCESS_TOKEN}}' ? '/rest/events?access_token=' + $('#authentication').data('access-token') : '/rest/events'
 }).constant('moduleConfig', {
-    'control' : true,
-    'configuration' : true,
-    'setup' : true,
-    'configuration' : true,
-    'extensions' : {
-        'label' : 'Extensions',
-        'visible' : true
-    },
-    'rules' : true,
-    'preferences' : true,
-    'groups' : true
+  'control': true,
+  'configuration': true,
+  'setup': true,
+  'configuration': true,
+  'extensions' : {
+      'label':'Add-ons',
+      'visible' : true
+  },
+  'rules': true,
+  'preferences': true,
+  'groups': false
+}).constant('moduleLabels', {
+    'extensions' : 'Add-ons'
 });
