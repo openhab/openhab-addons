9b50d63b54ec263faa0b0adad47d983f3ac8b72d
Wouter Born <github@maindrain.net>
2019-01-28 23:28:47 +0100

    [onewire] Fix license header (#4746)
    Signed-off-by: Wouter Born <eclipse@maindrain.net>
    Signed-off-by: Maximilian Hess <mail@ne0h.de>

--- addons/binding/org.openhab.binding.onewire.test/src/test/java/org/openhab/binding/onewire/internal/BasicThingHandlerTest.java
+++ addons/binding/org.openhab.binding.onewire.test/src/test/java/org/openhab/binding/onewire/internal/BasicThingHandlerTest.java
@@ -1,8 +1,8 @@
 /**
- * Copyright (c) 2014,2019 Contributors to the Eclipse Foundation
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
+ * information.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
