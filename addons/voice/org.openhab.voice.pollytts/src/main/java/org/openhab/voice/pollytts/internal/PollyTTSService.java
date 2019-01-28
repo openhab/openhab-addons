a108995399b0f2b4c52ee021b2be1805e26a873a
Wouter Born <eclipse@maindrain.net>
2019-01-28 19:32:41 +0100

    Use DS annotations
    Signed-off-by: Wouter Born <eclipse@maindrain.net>

--- addons/voice/org.openhab.voice.pollytts/src/main/java/org/openhab/voice/pollytts/internal/PollyTTSService.java
+++ addons/voice/org.openhab.voice.pollytts/src/main/java/org/openhab/voice/pollytts/internal/PollyTTSService.java
@@ -8,6 +8,8 @@
  */
 package org.openhab.voice.pollytts.internal;
 
+import static org.openhab.voice.pollytts.internal.PollyTTSService.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
@@ -16,6 +18,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.smarthome.config.core.ConfigConstants;
+import org.eclipse.smarthome.config.core.ConfigurableService;
 import org.eclipse.smarthome.core.audio.AudioException;
 import org.eclipse.smarthome.core.audio.AudioFormat;
 import org.eclipse.smarthome.core.audio.AudioStream;
@@ -24,16 +27,44 @@ import org.eclipse.smarthome.core.voice.TTSService;
 import org.eclipse.smarthome.core.voice.Voice;
 import org.openhab.voice.pollytts.internal.cloudapi.CachedPollyTTSCloudImplementation;
 import org.openhab.voice.pollytts.internal.cloudapi.PollyClientConfig;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * This is a TTS service implementation for using PollyTTS TTS service.
  *
- *  work derived for voicetts as sample
- *  * @author Robert Hillman - converted to Polly API, added polly voice interface, only MP3 audio currently
+ * work derived for voicetts as sample
+ * * @author Robert Hillman - converted to Polly API, added polly voice interface, only MP3 audio currently
  */
+@Component(configurationPid = SERVICE_PID, property = { Constants.SERVICE_PID + "=" + SERVICE_PID,
+        ConfigurableService.SERVICE_PROPERTY_LABEL + "=" + SERVICE_NAME + " Text-to-Speech",
+        ConfigurableService.SERVICE_PROPERTY_DESCRIPTION_URI + "=" + SERVICE_CATEGORY + ":" + SERVICE_ID,
+        ConfigurableService.SERVICE_PROPERTY_CATEGORY + "=" + SERVICE_CATEGORY })
 public class PollyTTSService implements TTSService {
+
+    /**
+     * Service name
+     */
+    static final String SERVICE_NAME = "Polly";
+
+    /**
+     * Service id
+     */
+    static final String SERVICE_ID = "pollytts";
+
+    /**
+     * Service category
+     */
+    static final String SERVICE_CATEGORY = "voice";
+
+    /**
+     * Service pid
+     */
+    static final String SERVICE_PID = "org.openhab." + SERVICE_CATEGORY + "." + SERVICE_ID;
 
     /** Cache folder name is below userdata/polly/cache. */
     private final String CACHE_FOLDER_NAME = "pollytts/cache";
@@ -58,6 +89,7 @@ public class PollyTTSService implements TTSService {
     /**
      * DS activate, with access to ConfigAdmin
      */
+    @Activate
     protected void activate(Map<String, Object> config) {
         try {
             modified(config);
@@ -70,6 +102,7 @@ public class PollyTTSService implements TTSService {
      * initialize voice service according to config values
      * Keys come from ConfigAdmin
      */
+    @Modified
     protected void modified(Map<String, Object> config) throws IOException {
         PollyClientConfig polly = new PollyClientConfig();
         polly.setAccessKey(config.get("accessKey").toString());
