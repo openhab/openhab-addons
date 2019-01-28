dc7da5564e0e0cc59684111235ea414dd09b37de
Richard Lavoie <lavoie.richard@gmail.com>
2019-01-27 21:02:51 -0500

    Define all the different thing type ids the lgtvserial binding support in java Signed-off-by: Richard Lavoie <lavoie.richard@gmail.com>

--- addons/binding/org.openhab.binding.lgtvserial/src/main/java/org/openhab/binding/lgtvserial/LgTvSerialBindingConstants.java
+++ addons/binding/org.openhab.binding.lgtvserial/src/main/java/org/openhab/binding/lgtvserial/LgTvSerialBindingConstants.java
@@ -28,6 +28,11 @@ public class LgTvSerialBindingConstants {
     public static final String BINDING_ID = "lgtvserial";
 
     // List of all Thing Type UIDs
-    public static final ThingTypeUID THING_TYPE_LGTV = new ThingTypeUID(BINDING_ID, "lgtv");
+    public static final ThingTypeUID THING_TYPE_LGTV_GENERIC = new ThingTypeUID(BINDING_ID, "lgtv");
+    public static final ThingTypeUID THING_TYPE_LGTV_LK_SERIES = new ThingTypeUID(BINDING_ID, "lgtv-LK-series");
+    public static final ThingTypeUID THING_TYPE_LGTV_LV_SERIES = new ThingTypeUID(BINDING_ID, "lgtv-LV-series");
+    public static final ThingTypeUID THING_TYPE_LGTV_LVx55_SERIES = new ThingTypeUID(BINDING_ID, "lgtv-LVx55-series");
+    public static final ThingTypeUID THING_TYPE_LGTV_M6503C = new ThingTypeUID(BINDING_ID, "lgtv-M6503C");
+    public static final ThingTypeUID THING_TYPE_LGTV_PW_SERIES = new ThingTypeUID(BINDING_ID, "lgtv-PW-series");
 
 }
--- addons/binding/org.openhab.binding.lgtvserial/src/main/java/org/openhab/binding/lgtvserial/internal/LgTvSerialHandlerFactory.java
+++ addons/binding/org.openhab.binding.lgtvserial/src/main/java/org/openhab/binding/lgtvserial/internal/LgTvSerialHandlerFactory.java
@@ -12,11 +12,15 @@
  */
 package org.openhab.binding.lgtvserial.internal;
 
+import static org.openhab.binding.lgtvserial.LgTvSerialBindingConstants.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
-import org.openhab.binding.lgtvserial.LgTvSerialBindingConstants;
 import org.openhab.binding.lgtvserial.handler.LgTvSerialHandler;
 import org.openhab.binding.lgtvserial.internal.protocol.serial.SerialCommunicatorFactory;
 
@@ -31,16 +35,26 @@ public class LgTvSerialHandlerFactory extends BaseThingHandlerFactory {
 
     private static final SerialCommunicatorFactory FACTORY = new SerialCommunicatorFactory();
 
+    private final static Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = new HashSet<>();
+    static {
+        SUPPORTED_THING_TYPES_UIDS.add(THING_TYPE_LGTV_GENERIC);
+        SUPPORTED_THING_TYPES_UIDS.add(THING_TYPE_LGTV_LV_SERIES);
+        SUPPORTED_THING_TYPES_UIDS.add(THING_TYPE_LGTV_LVx55_SERIES);
+        SUPPORTED_THING_TYPES_UIDS.add(THING_TYPE_LGTV_LK_SERIES);
+        SUPPORTED_THING_TYPES_UIDS.add(THING_TYPE_LGTV_M6503C);
+        SUPPORTED_THING_TYPES_UIDS.add(THING_TYPE_LGTV_PW_SERIES);
+    }
+
     @Override
     public boolean supportsThingType(ThingTypeUID thingTypeUID) {
-        return thingTypeUID.getBindingId().equals(LgTvSerialBindingConstants.BINDING_ID);
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
     }
 
     @Override
     protected ThingHandler createHandler(Thing thing) {
         ThingTypeUID thingTypeUID = thing.getThingTypeUID();
 
-        if (thingTypeUID.getBindingId().equals(LgTvSerialBindingConstants.BINDING_ID)) {
+        if (this.supportsThingType(thingTypeUID)) {
             return new LgTvSerialHandler(thing, FACTORY);
         }
 
