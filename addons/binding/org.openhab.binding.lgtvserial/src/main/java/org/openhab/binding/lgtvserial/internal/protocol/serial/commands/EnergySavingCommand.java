68fabbe01ca5ea829b196ca48bd0aa0b4cb89e0b
darkrift <lavoie.richard@gmail.com>
2019-01-28 04:39:49 -0500

    [lgtvserial] Added many more commands for serial LGTV and support daisy chaining (#2025)
    * Major refactoring to add many more commands and support daisy chaining
    * Passing null to update status of channel doesn't really work
    * Wrong commands for IR Key code
    * Fixed mismatch of channels and added a new command for color temperature
    * Mismatch types
    * Reduced logging level for non critical messages
    * Added interrupt flag so the caller can determine what to do with this thread.
    * Added warning logging
    * Added licence header and initial contribution javadoc
    * Moved responses to their own package
    * Changed the new Runnable to a lambda function
    * Converted some HashMap instantiation to it's "diamond" form
    * Using diamond operator.
    * Added reusable configuration in config-description ref
    * Added initial 3d commands support to string items
    * Added LV255C/LV355B/LV355C thing variant
    * Fixed many discrepancies, name mismatch and missing channel names
    * Fixed modifier order
    * Making loggers final
    * Adding debug statement when communicator fails to close the input and output streams
    * Added simple constructor level docblock
    * Unifying member access, removing "this." when further access didn't use it
    * Added 3d channels for the generic TV thing
    * Adding a safe guard in case a referenced channel in the xml files is not added to the command factory, this should never happen, but if it does the user will have a log about it and it won't propagate NPE when trying to update their state in the runnable task
    * Updated descriptions for 3d channels to make a mention that not all TV might support these channels
    * Reworked the README to describe all the added commands and known references on the protocol
    * Step 2 of the rename which was missing from the previous commit
    * Update header for EPL v2.0 license
    * Replaced space indentation by tab indentation that gave a warning while building on jenkins
    * Now using temperature dimension for the "temperature-value" channel
    * Extracted QuantityResponse
    * Set state readOnly to true for channels that can only read data
    * Define all the different thing type ids the lgtvserial binding support in java
    
    Signed-off-by: Richard Lavoie <lavoie.richard@gmail.com>
    Signed-off-by: Pshatsillo <pshatsillo@gmail.com>

---
+++ addons/binding/org.openhab.binding.lgtvserial/src/main/java/org/openhab/binding/lgtvserial/internal/protocol/serial/commands/EnergySavingCommand.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.lgtvserial.internal.protocol.serial.commands;
+
+import org.openhab.binding.lgtvserial.internal.protocol.serial.LGSerialCommand;
+
+/**
+ * This class handles the energy saving J/Q command.
+ *
+ * @author Richard Lavoie - Initial contribution
+ *
+ */
+public class EnergySavingCommand extends BaseStringCommand implements LGSerialCommand {
+
+    protected EnergySavingCommand(int setId) {
+        super('j', 'q', setId);
+    }
+
+}
---
+++ addons/binding/org.openhab.binding.lgtvserial/src/main/java/org/openhab/binding/lgtvserial/internal/protocol/serial/commands/VSizeCommand.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.lgtvserial.internal.protocol.serial.commands;
+
+/**
+ * This command handles the v-size F/T command.
+ *
+ * @author Richard Lavoie - Initial contribution
+ *
+ */
+public class VSizeCommand extends BasePercentCommand {
+
+    protected VSizeCommand(int setId) {
+        super('f', 't', setId);
+    }
+
+}
