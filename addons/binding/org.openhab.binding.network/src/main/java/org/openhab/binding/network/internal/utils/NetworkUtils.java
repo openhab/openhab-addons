c3a901ad4a4fa6aa3f5c2aee03cdfc90d932f240
Jan N. Klug <jan.n.klug@rub.de>
2019-02-10 12:49:01 +0100

    address review comments
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

--- addons/binding/org.openhab.binding.network/src/main/java/org/openhab/binding/network/internal/utils/NetworkUtils.java
+++ addons/binding/org.openhab.binding.network/src/main/java/org/openhab/binding/network/internal/utils/NetworkUtils.java
@@ -18,9 +18,9 @@ import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.net.NoRouteToHostException;
 import java.net.PortUnreachableException;
@@ -30,17 +30,18 @@ import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.util.Enumeration;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.SystemUtils;
 import org.apache.commons.net.util.SubnetUtils;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.net.CidrAddress;
+import org.eclipse.smarthome.core.net.NetUtil;
 import org.eclipse.smarthome.io.net.exec.ExecUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -62,39 +63,9 @@ public class NetworkUtils {
      *
      * @return The collected IPv4 Addresses
      */
-    public Set<String> getInterfaceIPs() {
-        Set<String> interfaceIPs = new HashSet<>();
-
-        Enumeration<NetworkInterface> interfaces;
-        try {
-            interfaces = NetworkInterface.getNetworkInterfaces();
-        } catch (SocketException ignored) {
-            // If we are not allowed to enumerate, we return an empty result set.
-            return interfaceIPs;
-        }
-
-        // For each interface ...
-        for (Enumeration<NetworkInterface> en = interfaces; en.hasMoreElements();) {
-            NetworkInterface networkInterface = en.nextElement();
-            boolean isLoopback = true;
-            try {
-                isLoopback = networkInterface.isLoopback();
-            } catch (SocketException ignored) {
-            }
-            if (!isLoopback) {
-                // .. and for each address ...
-                for (Iterator<InterfaceAddress> it = networkInterface.getInterfaceAddresses().iterator(); it
-                        .hasNext();) {
-
-                    // ... get IP and Subnet
-                    InterfaceAddress interfaceAddress = it.next();
-                    interfaceIPs.add(interfaceAddress.getAddress().getHostAddress() + "/"
-                            + interfaceAddress.getNetworkPrefixLength());
-                }
-            }
-        }
-
-        return interfaceIPs;
+    public Set<CidrAddress> getInterfaceIPs() {
+        return NetUtil.getAllInterfaceAddresses().stream().filter(a -> a.getAddress() instanceof Inet4Address)
+                .collect(Collectors.toSet());
     }
 
     /**
@@ -137,49 +108,42 @@ public class NetworkUtils {
      * @param maximumPerInterface The maximum of IP addresses per interface or 0 to get all.
      * @return Every single IP which can be assigned on the Networks the computer is connected to
      */
-    public Set<String> getNetworkIPs(Set<String> interfaceIPs, int maximumPerInterface) {
+    public Set<String> getNetworkIPs(Set<CidrAddress> interfaceIPs, int maximumPerInterface) {
         LinkedHashSet<String> networkIPs = new LinkedHashSet<>();
 
-        long minCIDRsuffix = 8; // historic Class A network, addresses = 16777214
+        short minCidrPrefixLength = 8; // historic Class A network, addresses = 16777214
         if (maximumPerInterface != 0) {
-            // calculate minimum CIDR suffix from maximumPerInterface (equals leading unset bits (Integer has 32 bits)
-            minCIDRsuffix = Integer.numberOfLeadingZeros(maximumPerInterface);
-            // if only the highest is set, increase suffix by 1 to cover all addresses
+            // calculate minimum CIDR prefix length from maximumPerInterface
+            // (equals leading unset bits (Integer has 32 bits)
+            minCidrPrefixLength = (short) Integer.numberOfLeadingZeros(maximumPerInterface);
             if (Integer.bitCount(maximumPerInterface) == 1) {
-                minCIDRsuffix++;
+                // if only the highest is set, decrease prefix by 1 to cover all addresses
+                minCidrPrefixLength--;
             }
         }
-        logger.trace("set minCIDRsuffix to {}, maximumPerInterFace is {}", minCIDRsuffix, maximumPerInterface);
+        logger.trace("set minCidrPrefixLength to {}, maximumPerInterface is {}", minCidrPrefixLength,
+                maximumPerInterface);
 
-        for (String string : interfaceIPs) {
-            Matcher addressMatcher = IP4_ADDRESS.matcher(string);
-            if (addressMatcher.matches()) {
-                if (Integer.valueOf(addressMatcher.group(2)) < minCIDRsuffix) {
-                    logger.info(
-                            "CIDR suffix is smaller than /{} on interface with address {}, truncating to /{}, some addresses might be lost",
-                            minCIDRsuffix, string, minCIDRsuffix);
-                    string = addressMatcher.group(1) + "/" + String.valueOf(minCIDRsuffix);
-                }
-                try {
-                    // gets every ip which can be assigned on the given network
-                    SubnetUtils utils = new SubnetUtils(string);
-                    String[] addresses = utils.getInfo().getAllAddresses();
-                    int len = addresses.length;
-                    if (maximumPerInterface != 0 && maximumPerInterface < len) {
-                        len = maximumPerInterface;
-                    }
-                    for (int i = 0; i < len; i++) {
-                        networkIPs.add(addresses[i]);
-                    }
-                } catch (Exception ex) {
-                }
-            } else {
-                logger.info("skipping interface with address {}: not an IPv4 address", string);
+        for (CidrAddress cidrNotation : interfaceIPs) {
+            if (cidrNotation.getPrefix() < minCidrPrefixLength) {
+                logger.info(
+                        "CIDR prefix is smaller than /{} on interface with address {}, truncating to /{}, some addresses might be lost",
+                        minCidrPrefixLength, cidrNotation, minCidrPrefixLength);
+                cidrNotation = new CidrAddress(cidrNotation.getAddress(), minCidrPrefixLength);
+            }
+
+            SubnetUtils utils = new SubnetUtils(cidrNotation.toString());
+            String[] addresses = utils.getInfo().getAllAddresses();
+            int len = addresses.length;
+            if (maximumPerInterface != 0 && maximumPerInterface < len) {
+                len = maximumPerInterface;
+            }
+            for (int i = 0; i < len; i++) {
+                networkIPs.add(addresses[i]);
             }
         }
 
         return networkIPs;
-
     }
 
     /**
