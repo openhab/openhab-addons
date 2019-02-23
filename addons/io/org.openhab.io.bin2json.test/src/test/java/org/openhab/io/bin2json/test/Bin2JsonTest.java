8ccc0ba7edc2662ed598d688180df87ec1efb6cd
Pauli Anttila <pauli.anttila@gmail.com>
2019-02-23 17:13:30 +0200

    Fixed headers
    Signed-off-by: Pauli Anttila <pauli.anttila@gmail.com>

--- addons/io/org.openhab.io.bin2json.test/src/test/java/org/openhab/io/bin2json/test/Bin2JsonTest.java
+++ addons/io/org.openhab.io.bin2json.test/src/test/java/org/openhab/io/bin2json/test/Bin2JsonTest.java
@@ -13,18 +13,6 @@
 package org.openhab.io.bin2json.test;
 
 import static org.junit.Assert.assertEquals;
-/**
- * Copyright (c) 2010-2019 Contributors to the openHAB project
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- *
- * SPDX-License-Identifier: EPL-2.0
- */
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -40,6 +28,11 @@ import org.slf4j.LoggerFactory;
 
 import com.google.gson.JsonObject;
 
+/**
+ * Integration tests for {@link Bin2Json}.
+ *
+ * @author Pauli Anttila - Initial contribution
+ */
 public class Bin2JsonTest {
     private final Logger logger = LoggerFactory.getLogger(Bin2JsonTest.class);
 
