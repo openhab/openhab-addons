1c9784c51e0e92f5b9c3ae6417b2c99990ced6ed
Kai Kreuzer <kai@openhab.org>
2019-02-24 18:57:12 +0100

    Removed check of firmware version
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- addons/binding/org.openhab.binding.keba/src/main/java/org/openhab/binding/keba/internal/handler/KeContactTransceiver.java
+++ addons/binding/org.openhab.binding.keba/src/main/java/org/openhab/binding/keba/internal/handler/KeContactTransceiver.java
@@ -74,7 +74,7 @@ class KeContactTransceiver {
                 selector = Selector.open();
 
                 if (transceiverThread == null) {
-                    transceiverThread = new Thread(transceiverRunnable, "ESH-Keba-Transceiver");
+                    transceiverThread = new Thread(transceiverRunnable, "openHAB-Keba-Transceiver");
                     transceiverThread.start();
                 }
 
