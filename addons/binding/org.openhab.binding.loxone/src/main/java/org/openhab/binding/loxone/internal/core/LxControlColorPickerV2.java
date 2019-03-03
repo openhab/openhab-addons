1fad038132b1c1ea05ef35b8cfa368b54653a437
Michael Mattan <michael.mattan@prosoccerdata.com>
2019-03-03 11:43:07 +0100

    [loxone] RGB integration (#3679)
    * [loxone] use more intelligent pattern matching
    * [loxone] create subclass of HSBType
    * [loxone]
    
    Signed-off-by: Michael Mattan <github@mattan.be>

--- addons/binding/org.openhab.binding.loxone/src/main/java/org/openhab/binding/loxone/internal/core/LxControlColorPickerV2.java
+++ addons/binding/org.openhab.binding.loxone/src/main/java/org/openhab/binding/loxone/internal/core/LxControlColorPickerV2.java
@@ -20,6 +20,7 @@ import org.eclipse.smarthome.core.library.types.DecimalType;
 import org.eclipse.smarthome.core.library.types.HSBType;
 import org.eclipse.smarthome.core.library.types.PercentType;
 import org.openhab.binding.loxone.internal.core.LxJsonApp3.LxJsonControl;
+import org.openhab.binding.loxone.internal.types.TemperatureHSBType;
 
 /**
  * A Color Picker V2 type of control on Loxone Miniserver.
@@ -45,9 +46,6 @@ public class LxControlColorPickerV2 extends LxControl {
             return TYPE_NAME;
         }
     }
-
-    private static final String COLOR_TYPE_TEMP = "temp";
-    private static final String COLOR_TYPE_HSV = "hsv";
 
     /**
      * Color state
@@ -153,142 +151,24 @@ public class LxControlColorPickerV2 extends LxControl {
     private HSBType mapLoxoneToOH(String color) {
         HSBType hsbColor = null;
 
-        Matcher m = Pattern.compile("\\((.*?)\\)").matcher(color);
-        if (m.find()) {
-            String[] colorSplit = m.group(1).split(",");
+        try {
+            Pattern hsvPattern = Pattern.compile("^hsv\\([0-9]\\d{0,2},[0-9]\\d{0,2},[0-9]\\d{0,2}\\)");
+            Pattern tempPattern = Pattern.compile("^temp\\([0-9]\\d{0,2},[0-9]\\d{0,4}\\)");
+            Matcher valueMatcher = Pattern.compile("\\((.*?)\\)").matcher(color);
 
-            if (isTemperatureColor(color) && colorSplit.length == 2) {
-                // convert temperature to a OH color
-                int brightness = constrain(Integer.valueOf(colorSplit[0]), 0, 100);
-                int temperature = constrain(Integer.valueOf(colorSplit[1]), 0, 65500);
-
-                int red = map(brightness, 0, 100, 0, calculateRed(temperature));
-                int green = map(brightness, 0, 100, 0, calculateGreen(temperature));
-                int blue = map(brightness, 0, 100, 0, calculateBlue(temperature));
-
-                hsbColor = HSBType.fromRGB(red, green, blue);
-            } else if (isHsvColor(color) && colorSplit.length == 3) {
-                hsbColor = new HSBType(m.group(1));
-            } else {
-                throw new IllegalArgumentException("Invalid color: " + color);
+            if (hsvPattern.matcher(color).matches() && valueMatcher.find()) {
+                // we have a hsv(hue,saturation,value) pattern
+                hsbColor = new HSBType(valueMatcher.group(1));
+            } else if (tempPattern.matcher(color).matches() && valueMatcher.find()) {
+                // we have a temp(brightness,kelvin) pattern
+                hsbColor = TemperatureHSBType.fromBrightnessTemperature(valueMatcher.group(1));
             }
+        } catch (Exception e) {
+            // an error happened during HSBType creation, we return null
+            hsbColor = null;
         }
 
         return hsbColor;
-    }
-
-    /**
-     * Re-maps a number from one range to another. That is, a value of fromLow would get mapped to toLow, a value of
-     * fromHigh to toHigh, values in-between to values in-between, etc.
-     *
-     * @param x        the number to map
-     * @param fromLow  the lower bound of the value's current range
-     * @param fromHigh the upper bound of the value's current range
-     * @param toLower  the lower bound of the value's target range
-     * @param toHigh   the upper bound of the value's target range
-     * @return the mapped value
-     */
-    private int map(int x, int fromLow, int fromHigh, int toLow, int toHigh) {
-        return (x - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
-    }
-
-    /**
-     * Constrains a number to be within a range.
-     *
-     * @param x   the number to constrain
-     * @param min the minimum value
-     * @param max the maximum value
-     * @return the constrained value
-     */
-    private int constrain(int x, int min, int max) {
-        if (x >= min && x <= max) {
-            return x;
-        } else if (x < min) {
-            return min;
-        } else {
-            return max;
-        }
-    }
-
-    /**
-     * calculates the red value based on the Kelvin temperature
-     *
-     * @param temp the Kelvin temperature
-     * @return the red value
-     */
-    private int calculateRed(int temp) {
-        int red = 255;
-        int temperature = temp / 100;
-
-        if (temperature > 66) {
-            red = temperature - 60;
-            red = ((Long) Math.round(329.698727466 * Math.pow(red, -0.1332047592))).intValue();
-        }
-
-        return constrain(red, 0, 255);
-    }
-
-    /**
-     * calculates the green value based on the Kelvin temperature
-     *
-     * @param temp green Kelvin temperature
-     * @return the red value
-     */
-    private int calculateGreen(int temp) {
-        int green;
-        int temperature = temp / 100;
-
-        if (temperature <= 66) {
-            green = temperature;
-            green = ((Long) Math.round((99.4708025861 * Math.log(green)) - 161.1195681661)).intValue();
-        } else {
-            green = temperature - 60;
-            green = ((Long) Math.round(288.1221695283 * Math.pow(green, -0.0755148492))).intValue();
-        }
-
-        return constrain(green, 0, 255);
-    }
-
-    /**
-     * calculates the blue value based on the Kelvin temperature
-     *
-     * @param temp the Kelvin temperature
-     * @return the blue value
-     */
-    private int calculateBlue(int temp) {
-        int blue = 255;
-        int temperature = temp / 100;
-
-        if (temperature < 65) {
-            if (temperature <= 19) {
-                blue = 0;
-            } else {
-                blue = temperature - 10;
-                blue = ((Long) Math.round((138.5177312231 * Math.log(blue)) - 305.0447927307)).intValue();
-            }
-        }
-
-        return constrain(blue, 0, 255);
-    }
-
-    /**
-     * checks whether the given Loxone color is a temperature
-     *
-     * @param color the loxone color
-     * @return boolean
-     */
-    private boolean isTemperatureColor(String color) {
-        return color.startsWith(COLOR_TYPE_TEMP);
-    }
-
-    /**
-     * checks whether the given Loxone color is a HSV-color
-     *
-     * @param color the loxone color
-     * @return boolean
-     */
-    private boolean isHsvColor(String color) {
-        return color.startsWith(COLOR_TYPE_HSV);
     }
 
 }
