b62e16e9c3d8f06f61d2f9b1d6d4adb0785f2713
pali <pauli.anttila@gmail.com>
2019-03-12 10:29:59 +0200

    [ihc] IHC / ELKO Binding initial contribution (#3708)
    Signed-off-by: Pauli Anttila <pauli.anttila@gmail.com>

---
+++ addons/binding/org.openhab.binding.ihc/src/main/java/org/openhab/binding/ihc/internal/converters/DecimalTypeWSEnumValueConverter.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.ihc.internal.converters;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.openhab.binding.ihc.internal.ws.exeptions.ConversionException;
+import org.openhab.binding.ihc.internal.ws.resourcevalues.WSEnumValue;
+
+/**
+ * DecimalType <-> WSEnumValue converter.
+ *
+ * @author Pauli Anttila - Initial contribution
+ */
+public class DecimalTypeWSEnumValueConverter implements Converter<WSEnumValue, DecimalType> {
+
+    @Override
+    public DecimalType convertFromResourceValue(@NonNull WSEnumValue from, @NonNull ConverterAdditionalInfo convertData)
+            throws ConversionException {
+        return new DecimalType(from.getEnumValueID());
+    }
+
+    @Override
+    public WSEnumValue convertFromOHType(@NonNull DecimalType from, @NonNull WSEnumValue value,
+            @NonNull ConverterAdditionalInfo convertData) throws ConversionException {
+        value.setEnumValueID(from.intValue());
+        return value;
+    }
+}
