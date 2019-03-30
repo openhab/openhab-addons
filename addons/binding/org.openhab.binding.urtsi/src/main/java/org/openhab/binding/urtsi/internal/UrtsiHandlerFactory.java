3ac7aabf562bb3d0b429dd7724c1109fda1cdaf0
Hilbrand Bouwkamp <hilbrand@h72.nl>
2019-03-30 20:54:42 +0100

    [urtsi] Remove dependency on google guava library. (#5329)
    Signed-off-by: Hilbrand Bouwkamp <hilbrand@h72.nl>
    Signed-off-by: Pshatsillo <pshatsillo@gmail.com>

--- addons/binding/org.openhab.binding.urtsi/src/main/java/org/openhab/binding/urtsi/internal/UrtsiHandlerFactory.java
+++ addons/binding/org.openhab.binding.urtsi/src/main/java/org/openhab/binding/urtsi/internal/UrtsiHandlerFactory.java
@@ -14,6 +14,7 @@ package org.openhab.binding.urtsi.internal;
 
 import static org.openhab.binding.urtsi.internal.UrtsiBindingConstants.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.smarthome.core.thing.Bridge;
@@ -26,8 +27,6 @@ import org.openhab.binding.urtsi.internal.handler.RtsDeviceHandler;
 import org.openhab.binding.urtsi.internal.handler.UrtsiDeviceHandler;
 import org.osgi.service.component.annotations.Component;
 
-import com.google.common.collect.Lists;
-
 /**
  * The {@link UrtsiHandlerFactory} is responsible for creating things and thing
  * handlers.
@@ -37,7 +36,7 @@ import com.google.common.collect.Lists;
 @Component(service = ThingHandlerFactory.class, configurationPid = "binding.urtsi")
 public class UrtsiHandlerFactory extends BaseThingHandlerFactory {
 
-    private static final List<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Lists.newArrayList(URTSI_DEVICE_THING_TYPE,
+    private static final List<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Arrays.asList(URTSI_DEVICE_THING_TYPE,
             RTS_DEVICE_THING_TYPE);
 
     @Override
