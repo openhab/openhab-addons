482bd8212abbb60acb44f6321656db0aaf902e50
Hilbrand Bouwkamp <hilbrand@h72.nl>
2019-04-01 13:43:40 +0200

    [mcp23017] Removed dependency on google guava library. (#5359)
    Also changed these lists to sets as they're only used to check if a value is part of the list. So makes more sense to use a set.
    
    Signed-off-by: Hilbrand Bouwkamp <hilbrand@h72.nl>
    Signed-off-by: Maximilian Hess <mail@ne0h.de>

--- addons/binding/org.openhab.binding.mcp23017/src/main/java/org/openhab/binding/mcp23017/internal/Mcp23017BindingConstants.java
+++ addons/binding/org.openhab.binding.mcp23017/src/main/java/org/openhab/binding/mcp23017/internal/Mcp23017BindingConstants.java
@@ -12,12 +12,12 @@
  */
 package org.openhab.binding.mcp23017.internal;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
-
-import com.google.common.collect.Lists;
 
 /**
  * The {@link Mcp23017BindingConstants} class defines common constants, which are
@@ -39,16 +39,16 @@ public class Mcp23017BindingConstants {
     public static final String PIN = "pin";
     public static final String PULL_MODE = "pull_mode";
     public static final String DEFAULT_PULL_MODE = "OFF";
-    
+
     public static final String ACTIVE_LOW = "active_low";
-    
+
     public static final String ACTIVE_LOW_ENABLED = "y";
 
     public static final String CHANNEL_GROUP_INPUT = "input";
     public static final String CHANNEL_GROUP_OUTPUT = "output";
 
-    public static final List<String> SUPPORTED_CHANNEL_GROUPS = Lists.newArrayList(CHANNEL_GROUP_INPUT,
-            CHANNEL_GROUP_OUTPUT);
+    public static final Set<String> SUPPORTED_CHANNEL_GROUPS = Arrays.asList(CHANNEL_GROUP_INPUT, CHANNEL_GROUP_OUTPUT)
+            .stream().collect(Collectors.toSet());
 
     public static final String CHANNEL_A0 = "A0";
     public static final String CHANNEL_A1 = "A1";
@@ -67,8 +67,9 @@ public class Mcp23017BindingConstants {
     public static final String CHANNEL_B6 = "B6";
     public static final String CHANNEL_B7 = "B7";
 
-    public static final List<String> SUPPORTED_CHANNELS = Lists.newArrayList(CHANNEL_A0, CHANNEL_A1, CHANNEL_A2,
-            CHANNEL_A3, CHANNEL_A4, CHANNEL_A5, CHANNEL_A6, CHANNEL_A7, CHANNEL_B0, CHANNEL_B1, CHANNEL_B2, CHANNEL_B3,
-            CHANNEL_B4, CHANNEL_B5, CHANNEL_B6, CHANNEL_B7);
+    public static final Set<String> SUPPORTED_CHANNELS = Arrays
+            .asList(CHANNEL_A0, CHANNEL_A1, CHANNEL_A2, CHANNEL_A3, CHANNEL_A4, CHANNEL_A5, CHANNEL_A6, CHANNEL_A7,
+                    CHANNEL_B0, CHANNEL_B1, CHANNEL_B2, CHANNEL_B3, CHANNEL_B4, CHANNEL_B5, CHANNEL_B6, CHANNEL_B7)
+            .stream().collect(Collectors.toSet());
 
 }
