e8d44bbd766cc36ce8fbaf1e10e5fd739d4fda9f
Ondrej Pecta <opecta@gmail.com>
2019-04-01 08:44:30 +0200

    [somfytahoma] Migration to the bnd based build system (#5351)
    Signed-off-by: Ondrej Pecta <opecta@gmail.com>
    Signed-off-by: Pshatsillo <pshatsillo@gmail.com>

---
+++ bundles/org.openhab.binding.somfytahoma/src/main/java/org/openhab/binding/somfytahoma/internal/model/SomfyTahomaLoginResponse.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.somfytahoma.internal.model;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link SomfyTahomaLoginResponse} holds information about login
+ * response to your TahomaLink account.
+ *
+ * @author Ondrej Pecta - Initial contribution
+ */
+@NonNullByDefault
+public class SomfyTahomaLoginResponse {
+
+    private boolean success;
+    private String version = "";
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+}
