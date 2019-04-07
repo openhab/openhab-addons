c3fa0b2e32852da23162ef662ddfe3e15cdeca95
Florian <florian.schmidt.welzow@t-online.de>
2019-04-07 13:16:14 +0200

    Move files to new location
    Signed-off-by: Florian <florian.schmidt.welzow@t-online.de>

--- bundles/org.openhab.binding.groheondus/src/main/java/org/openhab/binding/groheondus/internal/AccountServlet.java
+++ bundles/org.openhab.binding.groheondus/src/main/java/org/openhab/binding/groheondus/internal/AccountServlet.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.groheondus.internal;
 
 import java.io.IOException;
@@ -19,6 +31,9 @@ import org.osgi.service.http.HttpService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * @author Florian Schmidt - Initial contribution
+ */
 @NonNullByDefault
 public class AccountServlet extends HttpServlet {
 
