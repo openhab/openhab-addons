a965b831fa84ceb9e4d6016c7620c71a439d1431
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2019-04-11 22:50:18 +0200

    [freemobilesms] Initial skeleton for FreeMobile SMS service
    Signed-off-by: Guilhem Bonnefille <guilhem.bonnefille@gmail.com>

---
+++ addons/binding/org.openhab.binding.freemobilesms/src/main/java/org/openhab/binding/freemobilesms/internal/FreeMobileSmsConfiguration.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2014,2019 by the respective copyright holders.
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.freemobilesms.internal;
+
+/**
+ * The {@link FreeMobileSmsConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Guilhem Bonnefille <guilhem.bonnefille@gmail.com> - Initial contribution
+ */
+public class FreeMobileSmsConfiguration {
+
+    /**
+     * Sample configuration parameter. Replace with your own.
+     */
+    public String config1;
+}
