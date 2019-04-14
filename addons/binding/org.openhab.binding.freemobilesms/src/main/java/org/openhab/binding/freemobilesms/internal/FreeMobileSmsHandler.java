5d39a6e044111ffb6b285e22b6960d705dfada13
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2019-04-14 17:52:55 +0200

    [freemobilesms] Send message received on mesage channel
    Signed-off-by: Guilhem Bonnefille <guilhem.bonnefille@gmail.com>

--- addons/binding/org.openhab.binding.freemobilesms/src/main/java/org/openhab/binding/freemobilesms/internal/FreeMobileSmsHandler.java
+++ addons/binding/org.openhab.binding.freemobilesms/src/main/java/org/openhab/binding/freemobilesms/internal/FreeMobileSmsHandler.java
@@ -14,12 +14,15 @@ package org.openhab.binding.freemobilesms.internal;
 
 import static org.openhab.binding.freemobilesms.internal.FreeMobileSmsBindingConstants.*;
 
+import java.io.IOException;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.binding.freemobilesms.internal.FreeMobileSmsConfiguration;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingStatus;
+import org.eclipse.smarthome.core.thing.ThingStatusDetail;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.RefreshType;
@@ -28,6 +31,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import fr.free.smsapi.Account;
+import fr.free.smsapi.Sender;
 import fr.free.smsapi.RawAccount;
 
 /**
@@ -43,27 +47,49 @@ public class FreeMobileSmsHandler extends BaseThingHandler {
 
     private FreeMobileSmsConfiguration config;
 
+    private final Sender sender = new Sender();
+
     private Account account;
 
     public FreeMobileSmsHandler(Thing thing) {
         super(thing);
     }
 
+    protected void sendMessage(String message) {
+      logger.debug("Message to send: {}", message);
+      try {
+        sender.send(account, message);
+      } catch (IOException e) {
+        logger.error("Failed to send message: {}", e.getMessage());
+      }
+    }
+
+    private void handleMessage(Command command) {
+        logger.debug("Message command");
+        if (command instanceof StringType) {
+            sendMessage(command.toString());
+        } else {
+            logger.warn("Unsupported Command type");
+        }
+    }
+
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
         if (CHANNEL_MESSAGE.equals(channelUID.getId())) {
             if (command instanceof RefreshType) {
+                logger.debug("Refresh command");
                 // TODO: handle data refresh
+                return;
             }
 
-            if (command instanceof StringType) {
-                // TODO: handle data set
-            }
+            handleMessage(command);
 
             // Note: if communication with thing fails for some reason,
             // indicate that by setting the status with detail information:
             // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
             // "Could not control device at IP address x.x.x.x");
+        } else {
+          logger.warn("Received command {} for unknown channel: {}", command, channelUID);
         }
     }
 
