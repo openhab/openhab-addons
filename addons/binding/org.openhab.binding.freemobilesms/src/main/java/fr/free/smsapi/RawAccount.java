4834256362f0dfd2d69b125ec9770d96da70c82d
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2019-04-29 21:13:12 +0200

    [freemobilesms] Fix copyright
    Signed-off-by: Guilhem Bonnefille <guilhem.bonnefille@gmail.com>

--- addons/binding/org.openhab.binding.freemobilesms/src/main/java/fr/free/smsapi/RawAccount.java
+++ addons/binding/org.openhab.binding.freemobilesms/src/main/java/fr/free/smsapi/RawAccount.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014,2019 by the respective copyright holders.
+ * Copyright (c) 2010-2019 Contributors to the openHAB project.
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
