c08676fbba0a2f05534de5fa90e861db24023d54
Arjan Mels <arjan.mels@gmx.net>
2019-05-08 23:04:17 +0200

    Fixed config.xml file (missing protocol and token parameters)
    Signed-off-by: Arjan Mels <arjan.mels@gmx.net>

--- bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/service/RemoteControllerService.java
+++ bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/service/RemoteControllerService.java
@@ -209,17 +209,18 @@ public class RemoteControllerService implements SamsungTvService, RemoteControll
         String protocol = (String) getConfig(SamsungTvConfiguration.PROTOCOL);
         logger.info("Using {} interface", protocol);
 
-        if (SamsungTvConfiguration.PROTOCOL_NONE.equals(protocol)) {
-            remoteController = null;
-            return;
-        } else if (SamsungTvConfiguration.PROTOCOL_LEGACY.equals(protocol)) {
+        if (SamsungTvConfiguration.PROTOCOL_LEGACY.equals(protocol)) {
             remoteController = new RemoteControllerLegacy(host, port, "openHAB", "openHAB");
-        } else {
+        } else if (SamsungTvConfiguration.PROTOCOL_WEBSOCKET.equals(protocol)
+                || SamsungTvConfiguration.PROTOCOL_SECUREWEBSOCKET.equals(protocol)) {
             try {
                 remoteController = new RemoteControllerWebSocket(host, port, "openHAB", "openHAB", this);
             } catch (RemoteControllerException e) {
                 reportError("Cannot connect to remote control service", e);
             }
+        } else {
+            remoteController = null;
+            return;
         }
 
         if (remoteController != null) {
