b8f8eed063056e34b20a4ef806abb56257fbd3c5
Michael Geramb <mail@michael-geramb.at>
2019-05-11 17:36:17 +0200

    [amazonechocontrol] Remove beta tag Signed-off-by: Michael Geramb <mail@michael-geramb.at> (github: mgeramb)

--- bundles/org.openhab.binding.amazonechocontrol/src/main/java/org/openhab/binding/amazonechocontrol/internal/BindingServlet.java
+++ bundles/org.openhab.binding.amazonechocontrol/src/main/java/org/openhab/binding/amazonechocontrol/internal/BindingServlet.java
@@ -104,7 +104,7 @@ public class BindingServlet extends HttpServlet {
 
         StringBuilder html = new StringBuilder();
         html.append("<html><head><title>" + StringEscapeUtils.escapeHtml(BINDING_NAME) + "</title><head><body>");
-        html.append("<h1>" + StringEscapeUtils.escapeHtml(BINDING_NAME) + " Beta 8</h1>");
+        html.append("<h1>" + StringEscapeUtils.escapeHtml(BINDING_NAME) + "</h1>");
 
         synchronized (accountHandlers) {
             if (accountHandlers.isEmpty()) {
