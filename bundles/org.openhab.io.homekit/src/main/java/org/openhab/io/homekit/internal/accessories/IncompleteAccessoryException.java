8194a4c596cc933b8e6f9b33209ca3dda38d914a
J-N-K <J-N-K@users.noreply.github.com>
2019-05-19 18:20:21 +0200

    [homekit]  fix wrong group tag handling (#5610)
    * fix #5605
    
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>
    Signed-off-by: Maximilian Hess <mail@ne0h.de>

--- bundles/org.openhab.io.homekit/src/main/java/org/openhab/io/homekit/internal/accessories/IncompleteAccessoryException.java
+++ bundles/org.openhab.io.homekit/src/main/java/org/openhab/io/homekit/internal/accessories/IncompleteAccessoryException.java
@@ -20,10 +20,12 @@ import org.openhab.io.homekit.internal.HomekitCharacteristicType;
  */
 public class IncompleteAccessoryException extends Exception {
     private static final long serialVersionUID = 8595808359805444177L;
-    final HomekitCharacteristicType missingType;
 
     public IncompleteAccessoryException(HomekitCharacteristicType missingType) {
         super(String.format("Missing accessory type %s", missingType.getTag()));
-        this.missingType = missingType;
+    }
+
+    public IncompleteAccessoryException(String message) {
+        super(message);
     }
 }
