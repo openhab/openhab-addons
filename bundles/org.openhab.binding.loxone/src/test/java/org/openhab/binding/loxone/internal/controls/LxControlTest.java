f1871e6b536b18bc7df5b3e41cd880f748c330eb
Wouter Born <github@maindrain.net>
2019-06-02 16:28:06 +0200

    Fix double header
    Signed-off-by: Wouter Born <github@maindrain.net>

--- bundles/org.openhab.binding.loxone/src/test/java/org/openhab/binding/loxone/internal/controls/LxControlTest.java
+++ bundles/org.openhab.binding.loxone/src/test/java/org/openhab/binding/loxone/internal/controls/LxControlTest.java
@@ -10,17 +10,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-/**
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- *
- * SPDX-License-Identifier: EPL-2.0
- */
 package org.openhab.binding.loxone.internal.controls;
 
 import static org.junit.Assert.*;
