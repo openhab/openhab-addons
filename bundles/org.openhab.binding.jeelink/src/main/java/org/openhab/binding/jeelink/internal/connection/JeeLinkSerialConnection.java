openhab/openhab-addons

commit a1a7f39cc804c3b1288788771100fcac808ae3dc

Fixed potential resource leaks (#5497)
* Fixed imports
* Fixed spelling; Fixed potential resource leaks
* Changed exception handling

Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

bundles/org.openhab.binding.jeelink/src/main/java/org/openhab/binding/jeelink/internal/connection/JeeLinkSerialConnection.java
@@ -16,6 +16,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.util.TooManyListenersException;
 
 import org.slf4j.Logger;
@@ -78,25 +79,26 @@ public class JeeLinkSerialConnection extends AbstractJeeLinkConnection {
                 serialPort.setSerialPortParams(baudRate, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
                         SerialPort.PARITY_NONE);
 
-                final BufferedReader input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
-
-                serialPort.addEventListener(new SerialPortEventListener() {
-                    @Override
-                    public void serialEvent(SerialPortEvent event) {
-                        try {
-                            if (event.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
-                                propagateLine(input.readLine());
+                try (Reader isr = new InputStreamReader(serialPort.getInputStream());
+                        BufferedReader input = new BufferedReader(isr)) {
+                    serialPort.addEventListener(new SerialPortEventListener() {
+                        @Override
+                        public void serialEvent(SerialPortEvent event) {
+                            try {
+                                if (event.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+                                    propagateLine(input.readLine());
+                                }
+                            } catch (IOException ex) {
+                                logger.debug("Error reading from serial port!", ex);
+                                closeConnection();
+                                notifyAbort("propagate: " + ex.getMessage());
                             }
-                        } catch (IOException ex) {
-                            logger.debug("Error reading from serial port!", ex);
-                            closeConnection();
-                            notifyAbort("propagate: " + ex.getMessage());
                         }
-                    }
-                });
+                    });
 
-                serialPort.notifyOnDataAvailable(true);
-                notifyOpen();
+                    serialPort.notifyOnDataAvailable(true);
+                    notifyOpen();
+                }
             }
         } catch (UnsupportedCommOperationException | IOException | TooManyListenersException ex) {
             closeConnection();

addons/io/org.openhab.io.openhabcloud/src/main/java/org/openhab/io/openhabcloud/internal/CloudService.java
@@ -17,6 +17,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
@@ -99,9 +101,9 @@ public class CloudService implements ActionService, CloudClientListener, EventSu
     /**
      * This method sends notification message to mobile app through the openHAB Cloud service
      *
-     * @param userId   the {@link String} containing the openHAB Cloud user id to send message to
-     * @param message  the {@link String} containing a message to send to specified user id
-     * @param icon     the {@link String} containing a name of the icon to be used with this notification
+     * @param userId the {@link String} containing the openHAB Cloud user id to send message to
+     * @param message the {@link String} containing a message to send to specified user id
+     * @param icon the {@link String} containing a name of the icon to be used with this notification
      * @param severity the {@link String} containing severity (good, info, warning, error) of notification
      */
     public void sendNotification(String userId, String message, String icon, String severity) {
@@ -113,8 +115,8 @@ public class CloudService implements ActionService, CloudClientListener, EventSu
      * Sends an advanced notification to log. Log notifications are not pushed to user
      * devices but are shown to all account users in notifications log
      *
-     * @param message  the {@link String} containing a message to send to specified user id
-     * @param icon     the {@link String} containing a name of the icon to be used with this notification
+     * @param message the {@link String} containing a message to send to specified user id
+     * @param icon the {@link String} containing a name of the icon to be used with this notification
      * @param severity the {@link String} containing severity (good, info, warning, error) of notification
      */
     public void sendLogNotification(String message, String icon, String severity) {
@@ -126,8 +128,8 @@ public class CloudService implements ActionService, CloudClientListener, EventSu
      * Sends a broadcast notification. Broadcast notifications are pushed to all
      * mobile devices of all users of the account
      *
-     * @param message  the {@link String} containing a message to send to specified user id
-     * @param icon     the {@link String} containing a name of the icon to be used with this notification
+     * @param message the {@link String} containing a message to send to specified user id
+     * @param icon the {@link String} containing a name of the icon to be used with this notification
      * @param severity the {@link String} containing severity (good, info, warning, error) of notification
      */
     public void sendBroadcastNotification(String message, String icon, String severity) {
@@ -236,8 +238,8 @@ public class CloudService implements ActionService, CloudClientListener, EventSu
 
     private String readFirstLine(File file) {
         List<String> lines = null;
-        try {
-            lines = IOUtils.readLines(new FileInputStream(file));
+        try (InputStream fis = new FileInputStream(file)) {
+            lines = IOUtils.readLines(fis);
         } catch (IOException ioe) {
             // no exception handling - we just return the empty String
         }
@@ -251,8 +253,8 @@ public class CloudService implements ActionService, CloudClientListener, EventSu
     private void writeFile(File file, String content) {
         // create intermediary directories
         file.getParentFile().mkdirs();
-        try {
-            IOUtils.write(content, new FileOutputStream(file));
+        try (OutputStream fos = new FileOutputStream(file)) {
+            IOUtils.write(content, fos);
             logger.debug("Created file '{}' with content '{}'", file.getAbsolutePath(), content);
         } catch (FileNotFoundException e) {
             logger.error("Couldn't create file '{}'.", file.getPath(), e);

bundles/org.openhab.binding.coolmasternet/src/main/java/org/openhab/binding/coolmasternet/internal/ControllerHandler.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
@@ -147,20 +148,23 @@ public class ControllerHandler extends BaseBridgeHandler {
                 out.write(command.getBytes());
                 out.write("\r\n".getBytes());
 
-                BufferedReader in = new BufferedReader(new InputStreamReader(localSocket.getInputStream()));
-                while (true) {
-                    String line = in.readLine();
-                    logger.trace("Read result '{}'", line);
-                    if ("OK".equals(line)) {
-                        return response.toString();
-                    }
-                    response.append(line);
-                    if (response.length() > 100) {
-                        /*
-                         * Usually this loop times out on errors, but in the case that we just keep getting
-                         * data we should also fail with an error.
-                         */
-                        throw new CoolMasterClientError(String.format("Got gibberish response to command %s", command));
+                try (Reader isr = new InputStreamReader(localSocket.getInputStream());
+                        BufferedReader in = new BufferedReader(isr)) {
+                    while (true) {
+                        String line = in.readLine();
+                        logger.trace("Read result '{}'", line);
+                        if ("OK".equals(line)) {
+                            return response.toString();
+                        }
+                        response.append(line);
+                        if (response.length() > 100) {
+                            /*
+                             * Usually this loop times out on errors, but in the case that we just keep getting
+                             * data we should also fail with an error.
+                             */
+                            throw new CoolMasterClientError(
+                                    String.format("Got gibberish response to command %s", command));
+                        }
                     }
                 }
             } catch (SocketTimeoutException e) {

bundles/org.openhab.binding.harmonyhub/src/main/java/org/openhab/binding/harmonyhub/internal/discovery/HarmonyHubDiscoveryService.java
@@ -17,6 +17,7 @@ import static org.openhab.binding.harmonyhub.internal.HarmonyHubBindingConstants
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.io.StringReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -235,8 +236,9 @@ public class HarmonyHubDiscoveryService extends AbstractDiscoveryService {
 
         private void run() {
             while (running) {
-                try (Socket socket = serverSocket.accept()) {
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                try (Socket socket = serverSocket.accept();
+                        Reader isr = new InputStreamReader(socket.getInputStream());
+                        BufferedReader in = new BufferedReader(isr)) {
                     String input;
                     while ((input = in.readLine()) != null) {
                         if (!running) {

bundles/org.openhab.binding.hyperion/src/main/java/org/openhab/binding/hyperion/internal/connection/JsonTcpConnection.java
@@ -16,6 +16,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -59,9 +60,10 @@ public class JsonTcpConnection {
 
     public String send(String json) throws IOException {
         String response = null;
-        try (Socket hyperionServer = new Socket(address, port)) {
-            DataOutputStream outToServer = new DataOutputStream(hyperionServer.getOutputStream());
-            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(hyperionServer.getInputStream()));
+        try (Socket hyperionServer = new Socket(address, port);
+                DataOutputStream outToServer = new DataOutputStream(hyperionServer.getOutputStream());
+                Reader isr = new InputStreamReader(hyperionServer.getInputStream());
+                BufferedReader inFromServer = new BufferedReader(isr)) {
             logger.debug("Sending: {}", json);
             outToServer.writeBytes(json + System.lineSeparator());
             outToServer.flush();

bundles/org.openhab.binding.jeelink/src/main/java/org/openhab/binding/jeelink/internal/connection/AbstractJeeLinkConnection.java
@@ -77,15 +77,14 @@ public abstract class AbstractJeeLinkConnection implements JeeLinkConnection {
 
     @Override
     public void sendCommands(String commands) {
-        try {
-            if (commands != null && !commands.trim().isEmpty()) {
-                initCommands = commands.split(";");
+        if (commands != null && !commands.trim().isEmpty()) {
+            initCommands = commands.split(";");
 
-                if (logger.isDebugEnabled()) {
-                    logger.debug("Writing to device on port {}: {} ", port, Arrays.toString(initCommands));
-                }
+            if (logger.isDebugEnabled()) {
+                logger.debug("Writing to device on port {}: {} ", port, Arrays.toString(initCommands));
+            }
 
-                OutputStream initStream = getInitStream();
+            try (OutputStream initStream = getInitStream()) {
                 if (initStream == null) {
                     throw new IOException(
                             "Connection on port " + port + " did not provide an init stream for writing init commands");
@@ -98,11 +97,11 @@ public abstract class AbstractJeeLinkConnection implements JeeLinkConnection {
                     w.write(cmd);
                 }
                 w.flush();
+            } catch (IOException ex) {
+                logger.debug("Error writing to output stream!", ex);
+                closeConnection();
+                notifyAbort("propagate: " + ex.getMessage());
             }
-        } catch (IOException ex) {
-            logger.debug("Error writing to output stream!", ex);
-            closeConnection();
-            notifyAbort("propagate: " + ex.getMessage());
         }
     }
 

bundles/org.openhab.binding.squeezebox/src/main/java/org/openhab/binding/squeezebox/internal/handler/SqueezeBoxServerHandler.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.net.Socket;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
@@ -325,8 +326,8 @@ public class SqueezeBoxServerHandler extends BaseBridgeHandler {
         }
 
         logger.debug("Sending command: {}", sanitizeCommand(command));
-        try {
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        try (Writer osw = new OutputStreamWriter(clientSocket.getOutputStream());
+                BufferedWriter writer = new BufferedWriter(osw)) {
             writer.write(command + NEW_LINE);
             writer.flush();
         } catch (IOException e) {

bundles/org.openhab.binding.wifiled/src/main/java/org/openhab/binding/wifiled/internal/handler/AbstractWiFiLEDDriver.java
@@ -111,13 +111,12 @@ public abstract class AbstractWiFiLEDDriver {
     public abstract LEDStateDTO getLEDStateDTO() throws IOException;
 
     protected synchronized LEDState getLEDState() throws IOException {
-        try (Socket socket = new Socket(host, port)) {
+        try (Socket socket = new Socket(host, port);
+                DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+                DataInputStream inputStream = new DataInputStream(socket.getInputStream())) {
             logger.debug("Connected to '{}'", socket);
 
             socket.setSoTimeout(DEFAULT_SOCKET_TIMEOUT);
-
-            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
 
             byte[] data = { (byte) 0x81, (byte) 0x8A, (byte) 0x8B, (byte) 0x96 };
             outputStream.write(data);
@@ -135,8 +134,8 @@ public abstract class AbstractWiFiLEDDriver {
             int state = statusBytes[2] & 0xFF; // On/Off
             int program = statusBytes[3] & 0xFF;
             int programSpeed = statusBytes[5] & 0xFF;
-            
-            // On factory default the controller can be configured 
+
+            // On factory default the controller can be configured
             // with a value of 255 but max should be 31.
             if (programSpeed > 31) {
                 programSpeed = 31;
@@ -151,8 +150,6 @@ public abstract class AbstractWiFiLEDDriver {
             logger.debug("RGBW: {},{},{},{}, {}", red, green, blue, white, white2);
 
             return new LEDState(state, program, programSpeed, red, green, blue, white, white2);
-        } catch (Exception e) {
-            throw new IOException(e);
         }
     }
 
@@ -161,20 +158,19 @@ public abstract class AbstractWiFiLEDDriver {
     }
 
     protected synchronized void sendRaw(byte[] data, int delay) throws IOException {
-        try (Socket socket = new Socket(host, port)) {
+        try (Socket socket = new Socket(host, port);
+                DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
             logger.debug("Connected to '{}'", socket);
 
             socket.setSoTimeout(DEFAULT_SOCKET_TIMEOUT);
-
-            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
 
             sendRaw(data, outputStream);
 
             if (delay > 0) {
                 Thread.sleep(delay);
             }
-        } catch (Exception e) {
-            throw new IOException(e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 

commit 98cf5ddc398aeed212cab9c69b609c976408d845

Fix jeelink serial connection broken by #5497.
Signed-off-by: Volker Bier <volker.bier@web.de>

bundles/org.openhab.binding.jeelink/src/main/java/org/openhab/binding/jeelink/internal/connection/JeeLinkSerialConnection.java
@@ -16,7 +16,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.Reader;
 import java.util.TooManyListenersException;
 
 import org.slf4j.Logger;
@@ -79,26 +78,25 @@ public class JeeLinkSerialConnection extends AbstractJeeLinkConnection {
                 serialPort.setSerialPortParams(baudRate, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
                         SerialPort.PARITY_NONE);
 
-                try (Reader isr = new InputStreamReader(serialPort.getInputStream());
-                        BufferedReader input = new BufferedReader(isr)) {
-                    serialPort.addEventListener(new SerialPortEventListener() {
-                        @Override
-                        public void serialEvent(SerialPortEvent event) {
-                            try {
-                                if (event.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
-                                    propagateLine(input.readLine());
-                                }
-                            } catch (IOException ex) {
-                                logger.debug("Error reading from serial port!", ex);
-                                closeConnection();
-                                notifyAbort("propagate: " + ex.getMessage());
-                            }
-                        }
-                    });
+                final BufferedReader input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
 
-                    serialPort.notifyOnDataAvailable(true);
-                    notifyOpen();
-                }
+                serialPort.addEventListener(new SerialPortEventListener() {
+                    @Override
+                    public void serialEvent(SerialPortEvent event) {
+                        try {
+                            if (event.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+                                propagateLine(input.readLine());
+                            }
+                        } catch (IOException ex) {
+                            logger.debug("Error reading from serial port!", ex);
+                            closeConnection();
+                            notifyAbort("propagate: " + ex.getMessage());
+                        }
+                    }
+                });
+
+                serialPort.notifyOnDataAvailable(true);
+                notifyOpen();
             }
         } catch (UnsupportedCommOperationException | IOException | TooManyListenersException ex) {
             closeConnection();