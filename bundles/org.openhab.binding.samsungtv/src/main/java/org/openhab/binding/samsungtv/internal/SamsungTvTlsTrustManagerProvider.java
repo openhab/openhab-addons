openhab/openhab-addons

commit c141bb1a498cc2e9305a7512839ea3bee94232a2

[samsungtv] Added websocket support for remote controller (2016+ models) (#4169)
* Added websocket support for remote controller (2016+ models)
Changed component registration to annotations (OSGI-INF manual files
removed)
* Addressed feedback in pull request #4169:
- Refactored WOL functionality in separate class
- Removed TODO comments
- Various small fixes
* Added wwebsocketclient lifecycle event listener for more robust websocketclient open.
* Set SourceApp to empty string when no (recognized) app running
Changed from List to Set for listeners to prevent duplicate entries
* Prevent extraneous ON-OFF and OFF-ON transitions when TV supports Art
Mode
* Changed services list to set to prevent duplicate entries
* Changed json library to gson & refactored websocket handling
* Limited visibility of internal variables
* Various minor robustness and code clean changes
Changed try/catches to individual socket close calls in closeConnection
* Additional refactoring of code from RemoteControllerWebSocket to
WebSocket handlers
* Add support for passing web browser URL to TV via Websocket.
* Started implementing some improvements from PR #3109
Changed logger.error to logger.warn
* - Updated xsd schemas from eclipse smarthome to openhab
- Fixed minor logging bugs
- Removed accidentally comitted file
* Fixed static code check errors
(except MemberNameCheck which cannot be renamed which are json fields)
* Fixed immediate closing of connections after install of addon
* Fixed SSL connection by implementing TlsTrustManagerProvider
* Improved SourceApp updating for power on/off and artmode on/off
* Formatted all files with proper ESH codestyle and line endings

Signed-off-by: Arjan Mels <arjan.mels@gmx.net>

bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/SamsungTvTlsTrustManagerProvider.java
@@ -1,0 +1,82 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.samsungtv.internal;
+
+import java.net.Socket;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.X509ExtendedTrustManager;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.io.net.http.TlsTrustManagerProvider;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Provides a TrustManager to allow secure websocket connections to any TV (=server)
+ *
+ * @author Arjan Mels - Initial Contribution
+ */
+@Component
+@NonNullByDefault
+public class SamsungTvTlsTrustManagerProvider implements TlsTrustManagerProvider {
+    @Override
+    public String getHostName() {
+        return "SmartViewSDK";
+    }
+
+    private final X509ExtendedTrustManager trustAllCerts = new X509ExtendedTrustManager() {
+
+        @Override
+        public void checkClientTrusted(X509Certificate @Nullable [] x509Certificates, @Nullable String s)
+                throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate @Nullable [] x509Certificates, @Nullable String s)
+                throws CertificateException {
+        }
+
+        @Override
+        public X509Certificate @Nullable [] getAcceptedIssuers() {
+            return null;
+        }
+
+        @Override
+        public void checkClientTrusted(X509Certificate @Nullable [] x509Certificates, @Nullable String s,
+                @Nullable Socket socket) throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate @Nullable [] x509Certificates, @Nullable String s,
+                @Nullable Socket socket) throws CertificateException {
+        }
+
+        @Override
+        public void checkClientTrusted(X509Certificate @Nullable [] x509Certificates, @Nullable String s,
+                @Nullable SSLEngine sslEngine) throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate @Nullable [] x509Certificates, @Nullable String s,
+                @Nullable SSLEngine sslEngine) throws CertificateException {
+        }
+    };
+
+    @Override
+    public X509ExtendedTrustManager getTrustManager() {
+        return trustAllCerts;
+    }
+}

bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/WakeOnLanUtility.java
@@ -1,0 +1,138 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.samsungtv.internal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class with utility functions to support Wake On Lan (WOL)
+ *
+ * @author Arjan Mels - Initial contribution
+ *
+ */
+@NonNullByDefault
+public class WakeOnLanUtility {
+
+    private static Logger logger = LoggerFactory.getLogger(WakeOnLanUtility.class);
+
+    /**
+     * Get MAC address for host
+     * uses "arping" tool
+     *
+     * @param hostName Host Name (or IP address) of host to retrieve MAC address for
+     * @return MAC address
+     */
+    public static @Nullable String getMACAddress(String hostName) {
+        try {
+            Process proc = Runtime.getRuntime().exec("arping -r -c 1 -C 1 " + hostName);
+            proc.waitFor();
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String macAddress = stdInput.readLine();
+            if (macAddress != null) {
+                logger.info("MAC address of host {} is {}", hostName, macAddress);
+                return macAddress;
+            } else {
+                BufferedReader stdErr = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+                String error = stdErr.readLine();
+                logger.warn("Cannot get MAC addres of host {}: {}", hostName, error);
+            }
+        } catch (IOException | InterruptedException e) {
+            logger.debug("Problem getting MAC address: {}", e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * Send single WOL (Wake On Lan) package on all interfaces
+     *
+     * @macAddress MAC address to send WOL package to
+     */
+    public static void sendWOLPacket(String macAddress) {
+        byte[] bytes = getWOLPackage(macAddress);
+
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = interfaces.nextElement();
+                if (networkInterface.isLoopback()) {
+                    continue; // Do not want to use the loopback interface.
+                }
+                for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
+                    InetAddress broadcast = interfaceAddress.getBroadcast();
+                    if (broadcast == null) {
+                        continue;
+                    }
+
+                    try {
+                        DatagramPacket packet = new DatagramPacket(bytes, bytes.length, broadcast, 9);
+                        DatagramSocket socket = new DatagramSocket();
+                        socket.send(packet);
+                        socket.close();
+                        logger.trace("Sent WOL packet to {} {}", broadcast, macAddress);
+                    } catch (IOException e) {
+                        logger.warn("Problem sending WOL packet to {} {}", broadcast, macAddress);
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            logger.warn("Problem with interface while sending WOL packet to {}", macAddress);
+        }
+    }
+
+    /**
+     * Create WOL UDP package: 6 bytes 0xff and then 6 times the 6 byte mac address repeated
+     *
+     * @param macStr String representation of teh MAC address (either with : or -)
+     * @return byte array with the WOL package
+     * @throws IllegalArgumentException
+     */
+    private static byte[] getWOLPackage(String macStr) throws IllegalArgumentException {
+        byte[] macBytes = new byte[6];
+        String[] hex = macStr.split("(\\:|\\-)");
+        if (hex.length != 6) {
+            throw new IllegalArgumentException("Invalid MAC address.");
+        }
+        try {
+            for (int i = 0; i < 6; i++) {
+                macBytes[i] = (byte) Integer.parseInt(hex[i], 16);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid hex digit in MAC address.");
+        }
+
+        byte[] bytes = new byte[6 + 16 * macBytes.length];
+        for (int i = 0; i < 6; i++) {
+            bytes[i] = (byte) 0xff;
+        }
+        for (int i = 6; i < bytes.length; i += macBytes.length) {
+            System.arraycopy(macBytes, 0, bytes, i, macBytes.length);
+        }
+
+        return bytes;
+    }
+
+}

commit 850c0edb9e2ec7854afa22ac8672a8c6a6442f1d

[samsungtv] Fix minor bugs introduced with #4169 (#5738)
* Use TrustAllTrustMananger
* More extensive logging in onWebSocketClose & SamsungTvTlsTrustManagerProvider
* Changed default protocol to None, so autodiscover of port works
* Open additional sockets after token received

Signed-off-by: Arjan Mels <arjan.mels@gmx.net>

bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/SamsungTvTlsTrustManagerProvider.java
@@ -18,20 +18,26 @@ import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.X509ExtendedTrustManager;
+import javax.security.auth.x500.X500Principal;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.io.net.http.TlsTrustManagerProvider;
 import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Provides a TrustManager to allow secure websocket connections to any TV (=server)
+ * TODO: convert to using TrustAllTrustMananger (not supported in OH2.4 yet)
  *
  * @author Arjan Mels - Initial Contribution
  */
 @Component
 @NonNullByDefault
 public class SamsungTvTlsTrustManagerProvider implements TlsTrustManagerProvider {
+    private final Logger logger = LoggerFactory.getLogger(WakeOnLanUtility.class);
+
     @Override
     public String getHostName() {
         return "SmartViewSDK";
@@ -39,14 +45,25 @@ public class SamsungTvTlsTrustManagerProvider implements TlsTrustManagerProvider
 
     private final X509ExtendedTrustManager trustAllCerts = new X509ExtendedTrustManager() {
 
+        private void logCertificateCN(String function, X509Certificate @Nullable [] x509Certificates) {
+            if (x509Certificates != null && logger.isTraceEnabled()) {
+                for (X509Certificate certificate : x509Certificates) {
+                    logger.trace("TrustManager {} CN: {}", function,
+                            certificate.getSubjectX500Principal().getName(X500Principal.RFC2253));
+                }
+            }
+        }
+
         @Override
         public void checkClientTrusted(X509Certificate @Nullable [] x509Certificates, @Nullable String s)
                 throws CertificateException {
+            logCertificateCN("checkClientTrusted", x509Certificates);
         }
 
         @Override
         public void checkServerTrusted(X509Certificate @Nullable [] x509Certificates, @Nullable String s)
                 throws CertificateException {
+            logCertificateCN("checkServerTrusted", x509Certificates);
         }
 
         @Override
@@ -57,26 +74,31 @@ public class SamsungTvTlsTrustManagerProvider implements TlsTrustManagerProvider
         @Override
         public void checkClientTrusted(X509Certificate @Nullable [] x509Certificates, @Nullable String s,
                 @Nullable Socket socket) throws CertificateException {
+            logCertificateCN("checkClientTrustedSocket", x509Certificates);
         }
 
         @Override
         public void checkServerTrusted(X509Certificate @Nullable [] x509Certificates, @Nullable String s,
                 @Nullable Socket socket) throws CertificateException {
+            logCertificateCN("checkServerTrustedSocket", x509Certificates);
         }
 
         @Override
         public void checkClientTrusted(X509Certificate @Nullable [] x509Certificates, @Nullable String s,
                 @Nullable SSLEngine sslEngine) throws CertificateException {
+            logCertificateCN("checkClientTrustedEngine", x509Certificates);
         }
 
         @Override
         public void checkServerTrusted(X509Certificate @Nullable [] x509Certificates, @Nullable String s,
                 @Nullable SSLEngine sslEngine) throws CertificateException {
+            logCertificateCN("checkServerTrustedEngine", x509Certificates);
         }
     };
 
     @Override
     public X509ExtendedTrustManager getTrustManager() {
         return trustAllCerts;
+        // return TrustAllTrustMananger.getInstance();
     }
 }

bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/WakeOnLanUtility.java
@@ -36,7 +36,7 @@ import org.slf4j.LoggerFactory;
 @NonNullByDefault
 public class WakeOnLanUtility {
 
-    private static Logger logger = LoggerFactory.getLogger(WakeOnLanUtility.class);
+    private static final Logger logger = LoggerFactory.getLogger(WakeOnLanUtility.class);
 
     /**
      * Get MAC address for host

bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/discovery/SamsungTvDiscoveryParticipant.java
@@ -28,7 +28,6 @@ import org.eclipse.smarthome.config.discovery.upnp.UpnpDiscoveryParticipant;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.ThingUID;
 import org.jupnp.model.meta.RemoteDevice;
-import org.openhab.binding.samsungtv.internal.service.RemoteControllerService;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -56,8 +55,6 @@ public class SamsungTvDiscoveryParticipant implements UpnpDiscoveryParticipant {
         if (uid != null) {
             Map<String, Object> properties = new HashMap<>();
             properties.put(HOST_NAME, device.getIdentity().getDescriptorURL().getHost());
-
-            properties.putAll(RemoteControllerService.discover(device.getIdentity().getDescriptorURL().getHost()));
 
             DiscoveryResult result = DiscoveryResultBuilder.create(uid).withProperties(properties)
                     .withLabel(getLabel(device)).build();
@@ -98,8 +95,8 @@ public class SamsungTvDiscoveryParticipant implements UpnpDiscoveryParticipant {
                     if (logger.isDebugEnabled()) {
                         String modelName = device.getDetails().getModelDetails().getModelName();
                         String friendlyName = device.getDetails().getFriendlyName();
-                        logger.debug("Discovered a Samsung TV '{}' model '{}' thing with UDN '{}'", friendlyName,
-                                modelName, udn);
+                        logger.debug("Retrieved Thing UID for a Samsung TV '{}' model '{}' thing with UDN '{}'",
+                                friendlyName, modelName, udn);
                     }
 
                     return new ThingUID(SAMSUNG_TV_THING_TYPE, udn);

bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/handler/SamsungTvHandler.java
@@ -16,6 +16,7 @@ import static org.openhab.binding.samsungtv.internal.SamsungTvBindingConstants.*
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.TimeUnit;
@@ -67,7 +68,7 @@ public class SamsungTvHandler extends BaseThingHandler implements DiscoveryListe
     private static final int WOL_PACKET_RETRY_COUNT = 10;
     private static final int WOL_SERVICE_CHECK_COUNT = 30;
 
-    private Logger logger = LoggerFactory.getLogger(SamsungTvHandler.class);
+    private final Logger logger = LoggerFactory.getLogger(SamsungTvHandler.class);
 
     private UpnpIOService upnpIOService;
     private DiscoveryServiceRegistry discoveryServiceRegistry;
@@ -149,14 +150,6 @@ public class SamsungTvHandler extends BaseThingHandler implements DiscoveryListe
         discoveryServiceRegistry.addDiscoveryListener(this);
 
         checkAndCreateServices();
-
-        SamsungTvConfiguration configuration = getConfigAs(SamsungTvConfiguration.class);
-        if (StringUtils.isEmpty(configuration.macAddress) && configuration.hostName != null) {
-            String macAddress = WakeOnLanUtility.getMACAddress(configuration.hostName);
-            if (macAddress != null) {
-                getConfig().put(SamsungTvConfiguration.MAC_ADDRESS, macAddress);
-            }
-        }
     }
 
     @Override
@@ -277,14 +270,12 @@ public class SamsungTvHandler extends BaseThingHandler implements DiscoveryListe
             RemoteControllerService service = (RemoteControllerService) findServiceInstance(
                     RemoteControllerService.SERVICE_NAME);
             if (service == null) {
-                putOffline();
                 SamsungTvConfiguration configuration = getConfigAs(SamsungTvConfiguration.class);
                 service = RemoteControllerService.createNonUpnpService(configuration.hostName, configuration.port);
                 startService(service);
             } else {
                 // open connection again if needed
                 if (!service.checkConnection()) {
-                    putOffline();
                     service.start();
                 }
             }
@@ -309,9 +300,27 @@ public class SamsungTvHandler extends BaseThingHandler implements DiscoveryListe
     public void thingDiscovered(DiscoveryService source, DiscoveryResult result) {
         SamsungTvConfiguration configuration = getConfigAs(SamsungTvConfiguration.class);
 
-        if (configuration.hostName.equals(result.getProperties().get(SamsungTvConfiguration.HOST_NAME))) {
+        if (configuration.hostName != null
+                && configuration.hostName.equals(result.getProperties().get(SamsungTvConfiguration.HOST_NAME))) {
             logger.debug("thingDiscovered: {}, {}", result.getProperties().get(SamsungTvConfiguration.HOST_NAME),
                     result);
+
+            /* Check if configuration should be updated */
+            if (StringUtils.isEmpty(configuration.macAddress)) {
+                String macAddress = WakeOnLanUtility.getMACAddress(configuration.hostName);
+                if (macAddress != null) {
+                    getConfig().put(SamsungTvConfiguration.MAC_ADDRESS, macAddress);
+                    logger.debug("thingDiscovered, macAddress: {}", macAddress);
+                }
+            }
+            if (SamsungTvConfiguration.PROTOCOL_NONE.equals(configuration.protocol)) {
+                Map<String, Object> properties = RemoteControllerService.discover(configuration.hostName);
+                for (Map.Entry<String, Object> property : properties.entrySet()) {
+                    getConfig().put(property.getKey(), property.getValue());
+                    logger.debug("thingDiscovered, {}: {}", property.getKey(), property.getValue());
+                }
+            }
+
             /*
              * SamsungTV discovery services creates thing UID from UPnP UDN.
              * When thing is generated manually, thing UID may not match UPnP UDN, so store it for later use (e.g.

bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/protocol/RemoteControllerWebSocket.java
@@ -293,7 +293,7 @@ public class RemoteControllerWebSocket extends RemoteController implements Liste
      * @throws RemoteControllerException
      */
     public void sendKeys(List<KeyCode> keys, int sleepInMs) throws RemoteControllerException {
-        logger.debug("Try to send sequnce of commands: {}", keys);
+        logger.debug("Try to send sequence of commands: {}", keys);
 
         if (!isConnected()) {
             openConnection();

bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/protocol/WebSocketBase.java
@@ -53,8 +53,11 @@ class WebSocketBase extends WebSocketAdapter {
 
     @Override
     public void onWebSocketError(@Nullable Throwable error) {
-        logger.debug("{} connection error: {}", this.getClass().getSimpleName(),
-                error != null ? error.toString() : null);
+        if (logger.isTraceEnabled()) {
+            logger.trace("{} connection error", this.getClass().getSimpleName(), error);
+        } else {
+            logger.debug("{} connection error", this.getClass().getSimpleName());
+        }
         super.onWebSocketError(error);
         isConnecting = false;
     }

bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/protocol/WebSocketRemote.java
@@ -98,6 +98,12 @@ class WebSocketRemote extends WebSocketBase {
                     if (jsonMsg.data.token != null) {
                         this.remoteControllerWebSocket.callback.putConfig(SamsungTvConfiguration.WEBSOCKET_TOKEN,
                                 jsonMsg.data.token);
+                        // try opening additional websockets
+                        try {
+                            this.remoteControllerWebSocket.openConnection();
+                        } catch (RemoteControllerException e) {
+                            logger.warn("{}: Error ({})", this.getClass().getSimpleName(), e.getMessage());
+                        }
                     }
                     getApps();
                     break;

bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/service/MainTVServerService.java
@@ -55,7 +55,7 @@ public class MainTVServerService implements UpnpIOParticipant, SamsungTvService 
     public static final String SERVICE_NAME = "MainTVServer2";
     private static final List<String> SUPPORTEDCOMMANDS = Arrays.asList(SOURCE_NAME, BROWSER_URL, STOP_BROWSER);
 
-    private Logger logger = LoggerFactory.getLogger(MainTVServerService.class);
+    private final Logger logger = LoggerFactory.getLogger(MainTVServerService.class);
 
     private UpnpIOService service;
 

bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/service/MediaRendererService.java
@@ -55,7 +55,7 @@ public class MediaRendererService implements UpnpIOParticipant, SamsungTvService
     private final List<String> supportedCommands = Arrays.asList(VOLUME, MUTE, BRIGHTNESS, CONTRAST, SHARPNESS,
             COLOR_TEMPERATURE);
 
-    private Logger logger = LoggerFactory.getLogger(MediaRendererService.class);
+    private final Logger logger = LoggerFactory.getLogger(MediaRendererService.class);
 
     private @Nullable UpnpIOService service;
 

bundles/org.openhab.binding.samsungtv/src/main/java/org/openhab/binding/samsungtv/internal/service/RemoteControllerService.java
@@ -60,7 +60,7 @@ import com.google.gson.Gson;
 @NonNullByDefault
 public class RemoteControllerService implements SamsungTvService, RemoteControllerWebsocketCallback {
 
-    private Logger logger = LoggerFactory.getLogger(RemoteControllerService.class);
+    private final Logger logger = LoggerFactory.getLogger(RemoteControllerService.class);
 
     public static final String SERVICE_NAME = "RemoteControlReceiver";
 
@@ -209,17 +209,18 @@ public class RemoteControllerService implements SamsungTvService, RemoteControll
         String protocol = (String) getConfig(SamsungTvConfiguration.PROTOCOL);
         logger.info("Using {} interface", protocol);
 
-        if (SamsungTvConfiguration.PROTOCOL_NONE.equals(protocol)) {
-            remoteController = null;
-            return;
-        } else if (SamsungTvConfiguration.PROTOCOL_LEGACY.equals(protocol)) {
+        if (SamsungTvConfiguration.PROTOCOL_LEGACY.equals(protocol)) {
             remoteController = new RemoteControllerLegacy(host, port, "openHAB", "openHAB");
-        } else {
+        } else if (SamsungTvConfiguration.PROTOCOL_WEBSOCKET.equals(protocol)
+                || SamsungTvConfiguration.PROTOCOL_SECUREWEBSOCKET.equals(protocol)) {
             try {
                 remoteController = new RemoteControllerWebSocket(host, port, "openHAB", "openHAB", this);
             } catch (RemoteControllerException e) {
                 reportError("Cannot connect to remote control service", e);
             }
+        } else {
+            remoteController = null;
+            return;
         }
 
         if (remoteController != null) {

bundles/org.openhab.binding.samsungtv/src/main/resources/ESH-INF/config/config.xml
@@ -25,6 +25,22 @@
 			</description>
 			<default>1000</default>
 		</parameter>
+		<parameter name="protocol" type="text" required="true">
+			<label>Remote control protocol</label>
+			<description>The type of remote control protocol. This depends on the age of the TV.</description>
+			<options>
+				<option value="None">None</option>
+				<option value="Legacy">Legacy (Before 2014)</option>
+				<option value="WebSocket">Websocket (2016 and later TV's)</option>
+				<option value="SecureWebSocket">Secure websocket (2016 and later TV's)</option>
+			</options>
+			<default>None</default>
+		</parameter>
+		<parameter name="webSocketToken" type="text" readOnly="true">
+			<label>Websocket Token</label>
+			<description>Security token for secure websocket connection</description>
+			<advanced>true</advanced>
+		</parameter>
 	</config-description>
 
 </config-description:config-descriptions>