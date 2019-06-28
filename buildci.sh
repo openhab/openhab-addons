9934de109043271088a9d2e2b751a10b26d736b1
Martin van Wingerden <martinvw@mtin.nl>
2019-06-28 13:45:51 +0200

    Some finetuning to filter out download progress and include all warnings from the reporting utility
    Signed-of-by: Martin van Wingerden <martin@martinvw.nl>

--- buildci.sh
+++ buildci.sh
@@ -17,10 +17,11 @@ function mvnp() {
     set -o pipefail # exit build with error when pipes fail
     local command=(mvn $@)
     exec "${command[@]}" 2>&1 | # execute, redirect stderr to stdout
+	stdbuf -o0 grep -vE "Download(ed|ing) from [a-z.]+: https:" | # filter out downloads
         tee .build.log | # write output to log
         stdbuf -oL grep -E '^\[INFO\] Building .+ \[.+\]$' | # filter progress
-        sed -uE 's/^\[INFO\] Building (.*[^ ])[ ]+\[([0-9]+\/[0-9]+)\]$/\2| \1/' | # prefix project name with progress
-        sed -e :a -e 's/^.\{1,6\}|/ &/;ta' & # right align progress with padding
+        stdbuf -o0 sed -uE 's/^\[INFO\] Building (.*[^ ])[ ]+\[([0-9]+\/[0-9]+)\]$/\2| \1/' | # prefix project name with progress
+        stdbuf -o0 sed -e :a -e 's/^.\{1,6\}|/ &/;ta' & # right align progress with padding
     local pid=$!
     prevent_timeout $pid &
     wait $pid
@@ -32,13 +33,19 @@ CDIR=`pwd`
 
 if [ ! -z "$CHANGED_DIR" ] && [ -e "bundles/$CHANGED_DIR" ]; then
     echo "New addon pull request: Building $CHANGED_DIR"
-    echo "MAVEN_OPTS='-Xms1g -Xmx2g -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Dorg.slf4j.simpleLogger.defaultLogLevel=WARN'" > ~/.mavenrc
+    echo "MAVEN_OPTS='-Xms1g -Xmx2g -Dorg.slf4j.simpleLogger.log.org.openhab.tools.analysis.report.ReportUtility=DEBUG -Dorg.slf4j.simpleLogger.defaultLogLevel=WARN'" > ~/.mavenrc
     cd "bundles/$CHANGED_DIR"
-    mvn clean install 2>&1 | tee $CDIR/.build.log
+    mvn clean install -B 2>&1 | 
+	    stdbuf -o0 grep -vE "Download(ed|ing) from [a-z.]+: https:" | # Filter out Download(s)
+	    stdbuf -o0 grep -v "target/code-analysis" | # filter out some debug code from reporting utility
+	    tee $CDIR/.build.log
     if [ -e "../itests/$CHANGED_DIR" ]; then
       echo "New addon pull request: Building itest $CHANGED_DIR"
       cd "../itests/$CHANGED_DIR"
-      mvn clean install 2>&1 | tee -a $CDIR/.build.log
+      mvn clean install -B 2>&1 | 
+	      stdbuf -o0 grep -vE "Download(ed|ing) from [a-z.]+: https:" | # Filter out Download(s)
+	      stdbuf -o0 grep -v "target/code-analysis" | # filter out some debug code from reporting utility
+	      tee -a $CDIR/.build.log
     fi
 else
     echo "Build all"
