59851e1d9cedf989fb55f4b542c18add8bd75da9
Raphael Mack <ramack@raphael-mack.de>
2019-07-01 21:47:03 +0200

    Binding to access the Remote Control RS485 interface for Helios Domestic ventilation systems (KWL)
    fixes #5752
    
    Signed-off-by: Raphael Mack <ramack@raphael-mack.de>

---
+++ bundles/org.openhab.binding.heliosventiremote/src/main/java/org/openhab/binding/heliosventiremote/internal/HeliosVentiRemoteHandlerFactory.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.heliosventiremote.internal;
+
+import static org.openhab.binding.heliosventiremote.internal.HeliosVentiRemoteBindingConstants.THING_TYPE_HELIOS_VENTILATION;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
+import org.eclipse.smarthome.io.transport.serial.SerialPortManager;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link HeliosVentiRemoteHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author Raphael Mack - Initial contribution
+ */
+@NonNullByDefault
+@Component(configurationPid = "binding.heliosventiremote", service = ThingHandlerFactory.class)
+public class HeliosVentiRemoteHandlerFactory extends BaseThingHandlerFactory {
+
+    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections
+            .singleton(THING_TYPE_HELIOS_VENTILATION);
+
+    private @NonNullByDefault({}) SerialPortManager serialPortManager;
+    private final Logger logger = LoggerFactory.getLogger(HeliosVentiRemoteHandlerFactory.class);
+
+    @Reference
+    protected void setSerialPortManager(final SerialPortManager serialPortManager) {
+        this.serialPortManager = serialPortManager;
+    }
+
+    protected void unsetSerialPortManager(final SerialPortManager serialPortManager) {
+        this.serialPortManager = null;
+    }
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
+
+    @Override
+    protected @Nullable ThingHandler createHandler(Thing thing) {
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (THING_TYPE_HELIOS_VENTILATION.equals(thingTypeUID)) {
+            return new HeliosVentiRemoteHandler(thing, serialPortManager);
+        }
+
+        return null;
+    }
+}
