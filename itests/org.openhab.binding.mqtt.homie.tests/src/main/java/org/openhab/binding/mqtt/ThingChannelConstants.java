268f9149e1f573ee41069519215152f370cbabd3
J-N-K <J-N-K@users.noreply.github.com>
2019-07-07 13:51:43 +0200

    fix/improve tests for build (#5778)
    * add dependencies
    * nest
    * resolve mqtt itests
    * fix broker unit tests
    * make mqtt itests run (still failing)
    * make embedded broker tests more robust
    * hueemulation
    * fix poms
    
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

--- itests/org.openhab.binding.mqtt.homie.tests/src/main/java/org/openhab/binding/mqtt/ThingChannelConstants.java
+++ itests/org.openhab.binding.mqtt.homie.tests/src/main/java/org/openhab/binding/mqtt/ThingChannelConstants.java
@@ -10,20 +10,11 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.mqtt.generic;
+package org.openhab.binding.mqtt;
 
-import static org.openhab.binding.mqtt.homie.generic.internal.MqttBindingConstants.*;
+import static org.openhab.binding.mqtt.homie.generic.internal.MqttBindingConstants.HOMIE300_MQTT_THING;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.eclipse.smarthome.config.core.Configuration;
-import org.eclipse.smarthome.core.thing.Channel;
 import org.eclipse.smarthome.core.thing.ThingUID;
-import org.eclipse.smarthome.core.thing.type.ChannelTypeUID;
 
 /**
  * Static test definitions, like thing, bridge and channel definitions
