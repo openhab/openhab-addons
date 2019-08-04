49de2d7d95888855ebca95b6c2ac688e455462b6
Gaël L'hopital <gael@lhopital.org>
2019-08-04 09:00:09 +0200

    [VolvoOnCall] Correcting a potential NPE (#5856)
    * Correcting a potential NPE
    * Correcting precision losses
    
    Signed-off-by: Gaël L'hopital <gael@lhopital.org>
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.binding.volvooncall/src/main/java/org/openhab/binding/volvooncall/internal/handler/VehicleHandler.java
+++ bundles/org.openhab.binding.volvooncall/src/main/java/org/openhab/binding/volvooncall/internal/handler/VehicleHandler.java
@@ -238,7 +238,7 @@ public class VehicleHandler extends BaseThingHandler {
                     return UnDefType.UNDEF;
                 }
             case TRIP_DISTANCE:
-                return new QuantityType<Length>(tripDetails.distance / 1000, KILO(SIUnits.METRE));
+                return new QuantityType<Length>((double) tripDetails.distance / 1000, KILO(SIUnits.METRE));
             case TRIP_START_TIME:
                 return tripDetails.getStartTime();
             case TRIP_END_TIME:
@@ -246,9 +246,9 @@ public class VehicleHandler extends BaseThingHandler {
             case TRIP_DURATION:
                 return new QuantityType<Time>(tripDetails.getDurationInMinutes(), SmartHomeUnits.MINUTE);
             case TRIP_START_ODOMETER:
-                return new QuantityType<Length>(tripDetails.startOdometer / 1000, KILO(SIUnits.METRE));
+                return new QuantityType<Length>((double) tripDetails.startOdometer / 1000, KILO(SIUnits.METRE));
             case TRIP_STOP_ODOMETER:
-                return new QuantityType<Length>(tripDetails.endOdometer / 1000, KILO(SIUnits.METRE));
+                return new QuantityType<Length>((double) tripDetails.endOdometer / 1000, KILO(SIUnits.METRE));
             case TRIP_START_POSITION:
                 return tripDetails.getStartPosition();
             case TRIP_END_POSITION:
@@ -281,19 +281,33 @@ public class VehicleHandler extends BaseThingHandler {
             case FRONT_LEFT_WND:
                 return status.windows != null ? status.windows.frontLeftWindowOpen : UnDefType.NULL;
             case ODOMETER:
-                return new QuantityType<Length>(status.odometer.floatValue() / 1000, KILO(SIUnits.METRE));
+                return status.odometer != Status.UNDEFINED
+                        ? new QuantityType<Length>((double) status.odometer / 1000, KILO(SIUnits.METRE))
+                        : UnDefType.UNDEF;
             case TRIPMETER1:
-                return new QuantityType<Length>(status.tripMeter1.floatValue() / 1000, KILO(SIUnits.METRE));
+                return status.tripMeter1 != Status.UNDEFINED
+                        ? new QuantityType<Length>((double) status.tripMeter1 / 1000, KILO(SIUnits.METRE))
+                        : UnDefType.UNDEF;
             case TRIPMETER2:
-                return new QuantityType<Length>(status.tripMeter2.floatValue() / 1000, KILO(SIUnits.METRE));
+                return status.tripMeter2 != Status.UNDEFINED
+                        ? new QuantityType<Length>((double) status.tripMeter2 / 1000, KILO(SIUnits.METRE))
+                        : UnDefType.UNDEF;
             case DISTANCE_TO_EMPTY:
-                return new QuantityType<Length>(status.distanceToEmpty, KILO(SIUnits.METRE));
+                return status.distanceToEmpty != Status.UNDEFINED
+                        ? new QuantityType<Length>(status.distanceToEmpty, KILO(SIUnits.METRE))
+                        : UnDefType.UNDEF;
             case FUEL_AMOUNT:
-                return new QuantityType<Volume>(status.fuelAmount, SmartHomeUnits.LITRE);
+                return status.fuelAmount != Status.UNDEFINED
+                        ? new QuantityType<Volume>(status.fuelAmount, SmartHomeUnits.LITRE)
+                        : UnDefType.UNDEF;
             case FUEL_LEVEL:
-                return new QuantityType<>(status.fuelAmountLevel, SmartHomeUnits.PERCENT);
+                return status.fuelAmountLevel != Status.UNDEFINED
+                        ? new QuantityType<>(status.fuelAmountLevel, SmartHomeUnits.PERCENT)
+                        : UnDefType.UNDEF;
             case FUEL_CONSUMPTION:
-                return new DecimalType(status.averageFuelConsumption.floatValue() / 10);
+                return status.averageFuelConsumption != Status.UNDEFINED
+                        ? new DecimalType(status.averageFuelConsumption / 10)
+                        : UnDefType.UNDEF;
             case ACTUAL_LOCATION:
                 return position.getPosition();
             case CALCULATED_LOCATION:
