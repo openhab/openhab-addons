16b240d9d4cdc9fa65650a6a1062d82ceb90604d
J-N-K <J-N-K@users.noreply.github.com>
2019-08-05 22:03:18 +0200

    [mqtt] fix subscription issues (#5868)
    * fixes subscription issues
    * fix nullness
    * remove double unsubscription
    
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>
    Signed-off-by: Joe Inkenbrandt <joe@inkenbrandt.com>

--- bundles/org.openhab.binding.mqtt.generic/src/main/java/org/openhab/binding/mqtt/generic/AbstractMQTTThingHandler.java
+++ bundles/org.openhab.binding.mqtt.generic/src/main/java/org/openhab/binding/mqtt/generic/AbstractMQTTThingHandler.java
@@ -205,12 +205,22 @@ public abstract class AbstractMQTTThingHandler extends BaseThingHandler implemen
 
     @Override
     public void dispose() {
-        MqttBrokerConnection connection = this.connection;
-        if (connection != null) {
-            connection.unsubscribeAll();
+        stop();
+        try {
+            unsubscribeAll().get(500, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            logger.warn("unsubcription on disposal failed for {}: ", thing.getUID(), e);
         }
+        connection = null;
         super.dispose();
     }
+
+    /**
+     * this method must unsubscribe all topics used by this thing handler
+     *
+     * @return
+     */
+    public abstract CompletableFuture<Void> unsubscribeAll();
 
     @Override
     public void updateChannelState(ChannelUID channelUID, State value) {
