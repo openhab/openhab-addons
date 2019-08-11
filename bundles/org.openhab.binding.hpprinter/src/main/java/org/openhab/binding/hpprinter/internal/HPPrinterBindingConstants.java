7d0cb402ed638d1725a8e331397835e844199a80
Stewart Cossey <stewart.cossey@gmail.com>
2019-08-11 22:40:32 +1200

    Improvement to thing and channels.
    Simplify down to a single printer thing.
    Optimize channel groups and types.
    Change id and names to american english spelling.
    
    Signed-off-by: Stewart Cossey <stewart.cossey@gmail.com>

--- bundles/org.openhab.binding.hpprinter/src/main/java/org/openhab/binding/hpprinter/internal/HPPrinterBindingConstants.java
+++ bundles/org.openhab.binding.hpprinter/src/main/java/org/openhab/binding/hpprinter/internal/HPPrinterBindingConstants.java
@@ -18,6 +18,7 @@ import java.util.stream.Stream;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.type.ChannelGroupTypeUID;
 
 /**
  * The {@link HPPrinterBindingConstants} class defines common constants, which are
@@ -31,12 +32,10 @@ public class HPPrinterBindingConstants {
     private static final String BINDING_ID = "hpprinter";
 
     // ********** List of all Thing Type UIDs **********
-    public static final ThingTypeUID THING_MONOCHROME = new ThingTypeUID(BINDING_ID, "monochrome");
-    public static final ThingTypeUID THING_SINGLECOLOR = new ThingTypeUID(BINDING_ID, "singlecolor");
-    public static final ThingTypeUID THING_MULTICOLOR = new ThingTypeUID(BINDING_ID, "multicolor");
+    public static final ThingTypeUID THING_PRINTER = new ThingTypeUID(BINDING_ID, "printer");
 
     public static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Stream
-    .of(THING_MONOCHROME, THING_SINGLECOLOR, THING_MULTICOLOR)
+    .of(THING_PRINTER)
     .collect(Collectors.toSet());
 
 
@@ -44,7 +43,7 @@ public class HPPrinterBindingConstants {
     public static final String CHANNEL_STATUS = "status";
 
     // Colours
-    public static final String CHANNEL_COLOUR_LEVEL = "colourLevel";
+    public static final String CHANNEL_COLOR_LEVEL = "colorLevel";
     public static final String CHANNEL_CYAN_LEVEL = "cyanLevel";
     public static final String CHANNEL_MAGENTA_LEVEL = "magentaLevel";
     public static final String CHANNEL_YELLOW_LEVEL = "yellowLevel";
@@ -53,12 +52,13 @@ public class HPPrinterBindingConstants {
     // Page Counts
     public static final String CHANNEL_SUBSCRIPTION = "subsciptionCount";
     public static final String CHANNEL_TOTAL_PAGES = "totalCount";
-    public static final String CHANNEL_TOTAL_COLOURPAGES = "totalColorCount";
+    public static final String CHANNEL_TOTAL_COLORPAGES = "totalColorCount";
     public static final String CHANNEL_TOTAL_MONOPAGES = "totalMonochromeCount";
     public static final String CHANNEL_JAM_EVENTS = "jamEvents";
 
     public static final String CGROUP_INK = "ink";
     public static final String CGROUP_STATUS = "status";
-    public static final String CGROUP_CONTROL = "control";
     public static final String CGROUP_USAGE = "usage";
+
+    public static final ChannelGroupTypeUID GROUP_INK = new ChannelGroupTypeUID(BINDING_ID, CGROUP_INK);
 }
