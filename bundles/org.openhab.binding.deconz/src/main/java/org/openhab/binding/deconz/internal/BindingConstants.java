f288c1dfcde4e8a15a5bd19c476cd90b503943ba
Christoph Weitkamp <github@christophweitkamp.de>
2019-08-12 08:46:44 +0200

    [deconz] Added properties for sensors (#5899)
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

--- bundles/org.openhab.binding.deconz/src/main/java/org/openhab/binding/deconz/internal/BindingConstants.java
+++ bundles/org.openhab.binding.deconz/src/main/java/org/openhab/binding/deconz/internal/BindingConstants.java
@@ -74,6 +74,8 @@ public class BindingConstants {
     public static final String CONFIG_HOST = "host";
     public static final String CONFIG_APIKEY = "apikey";
 
+    public static final String UNIQUE_ID = "uid";
+
     public static String url(String host, @Nullable String apikey, @Nullable String endpointType,
             @Nullable String endpointID) {
         StringBuilder url = new StringBuilder();
