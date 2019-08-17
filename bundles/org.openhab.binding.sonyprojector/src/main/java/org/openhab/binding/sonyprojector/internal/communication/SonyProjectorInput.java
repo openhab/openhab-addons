134398c484994ebd0d79cde99a68c3e7f6ecc5fb
lolodomo <lg.hc@free.fr>
2019-08-17 11:52:55 +0200

    [sonyprojector] Remove code warnings (#5941)
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>
    Signed-off-by: Joe Inkenbrandt <joe@inkenbrandt.com>

--- bundles/org.openhab.binding.sonyprojector/src/main/java/org/openhab/binding/sonyprojector/internal/communication/SonyProjectorInput.java
+++ bundles/org.openhab.binding.sonyprojector/src/main/java/org/openhab/binding/sonyprojector/internal/communication/SonyProjectorInput.java
@@ -66,8 +66,7 @@ public enum SonyProjectorInput {
 
     private int category;
     private String name;
-    @Nullable
-    private String label;
+    private @Nullable String label;
     private byte[] dataCode;
 
     /**
