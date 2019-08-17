mbretzler/openhab2-addons

[RFXCOM] Map X10 BRIGHT/DIM to openhab INCREASE/DECREASE
The way an X10 SS13 (for instance) works is that the BRIGHT/DIM buttons
send an X10 DIM/BRIGHT command to the <X>0 (all units on house code X)
address and any dimmable device that last saw ON for itself acts on it.
That is to say if the last ON seen by A1 was addressed to A1 and an A0
DIM comes in then A1 will dim but A2 won't. Since this only works in a
pure X10 world we keep track of where the last command was going and map
incoming BRIGHT/DIM commands to INCREASE/DECREASE commands for the last
thing ON'd. This allows an X10 SS13 to operate as designed, both as
an on/off switch and as a dimmer controller simply by attaching the
switch's item to the switchable/dimmable thing's dimmer channel with no
rules necessary.

N.B. When sending INCREASE/DECREASE commands to an X10 device we change
the X10 address to be the all-units address. It would be better to
direct the command to the specific device required however I have not
tested whether this actually works or not (lack of working PLM).

Closes #2102

Signed-off-by: Mike Jagdis <mjagdis@eris-associates.co.uk> (github: mjagdis)

addons/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/handler/RFXComHandler.java
@@ -175,7 +175,7 @@ public class RFXComHandler extends BaseThingHandler implements DeviceMessageList
                                         updateState(CHANNEL_CHIME_SOUND, message.convertToState(valueSelector));
                                         break;
                                     case COMMAND:
-                                        postCommand(CHANNEL_COMMAND, (Command) message.convertToState(valueSelector));
+                                        postCommand(CHANNEL_COMMAND, message.convertToCommand(valueSelector));
                                         break;
                                     case COMMAND_ID:
                                         updateState(CHANNEL_COMMAND_ID, message.convertToState(valueSelector));

addons/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/internal/messages/RFXComLighting1Message.java
@@ -16,8 +16,10 @@ import org.eclipse.smarthome.core.library.items.ContactItem;
 import org.eclipse.smarthome.core.library.items.NumberItem;
 import org.eclipse.smarthome.core.library.items.SwitchItem;
 import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.library.types.IncreaseDecreaseType;
 import org.eclipse.smarthome.core.library.types.OnOffType;
 import org.eclipse.smarthome.core.library.types.OpenClosedType;
+import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.State;
 import org.eclipse.smarthome.core.types.Type;
 import org.openhab.binding.rfxcom.RFXComValueSelector;
@@ -110,6 +112,8 @@ public class RFXComLighting1Message extends RFXComBaseMessage {
     public byte signalLevel;
     public boolean group;
 
+    private static byte[] lastUnit = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
     public RFXComLighting1Message() {
         packetType = PacketType.LIGHTING1;
     }
@@ -143,8 +147,17 @@ public class RFXComLighting1Message extends RFXComBaseMessage {
 
         if ((command == Commands.GROUP_ON) || (command == Commands.GROUP_OFF)) {
             unitCode = 0;
+        } else if ((data[5] == 0) && ((command == Commands.DIM) || (command == Commands.BRIGHT))) {
+            // SS13 switches broadcast DIM/BRIGHT to X0 and the dimmers ignore
+            // the message unless the last X<n> ON they saw was for them. So we
+            // redirect an incoming broadcast DIM/BRIGHT to the correct item
+            // based on the last X<n> we saw or sent.
+            unitCode = lastUnit[(int)houseCode - (int)'A'];
         } else {
             unitCode = data[5];
+            if (command == Commands.ON) {
+                lastUnit[(int)houseCode - (int)'A'] = unitCode;
+            }
         }
 
         signalLevel = (byte) ((data[7] & 0xF0) >> 4);
@@ -162,7 +175,18 @@ public class RFXComLighting1Message extends RFXComBaseMessage {
         data[2] = subType.toByte();
         data[3] = seqNbr;
         data[4] = (byte) houseCode;
-        data[5] = unitCode;
+        // When an SS13 sends a DIM/BRIGHT it broadcasts to the all-units code (X0)
+        // and it is up to the dimmers to ignore the message if the last X<n> they
+        // saw wasn't for them. Sending to an actual X<n> may or may not work.
+        // It is untested against _any_ dimmer never mind all so we stick to doing
+        // the same as an SS13. At least for now. Using an explicit X<n> would be
+        // better (if it works) because X10 RF and PLM are lossy and different
+        // modules may have seen different traffic.
+        if ((command == Commands.DIM) || (command == Commands.BRIGHT)) {
+            data[5] = 0;
+        } else {
+            data[5] = unitCode;
+        }
         data[6] = command.toByte();
         data[7] = (byte) ((signalLevel & 0x0F) << 4);
 
@@ -172,6 +196,55 @@ public class RFXComLighting1Message extends RFXComBaseMessage {
     @Override
     public String getDeviceId() {
         return houseCode + ID_DELIMITER + unitCode;
+    }
+
+    @Override
+    public Command convertToCommand(RFXComValueSelector valueSelector) throws RFXComException {
+
+        Command OHcommand;
+
+        if (valueSelector.getItemClass() == SwitchItem.class) {
+
+            if (valueSelector == RFXComValueSelector.COMMAND) {
+
+                switch (command) {
+                    case OFF:
+                    case GROUP_OFF:
+                        OHcommand = OnOffType.OFF;
+                        break;
+
+                    case ON:
+                    case GROUP_ON:
+                        OHcommand = OnOffType.ON;
+                        break;
+
+                    case DIM:
+                        OHcommand = IncreaseDecreaseType.DECREASE;
+                        break;
+
+                    case BRIGHT:
+                        OHcommand = IncreaseDecreaseType.INCREASE;
+                        break;
+
+                    case CHIME:
+                        OHcommand = OnOffType.ON;
+                        break;
+
+                    default:
+                        throw new RFXComException("Can't convert " + command + " to SwitchItem");
+                }
+
+            } else {
+                throw new RFXComException("Can't convert " + valueSelector + " to SwitchItem");
+            }
+
+        } else {
+
+            throw new RFXComException("Can't convert " + valueSelector + " to " + valueSelector.getItemClass());
+
+        }
+
+        return OHcommand;
     }
 
     @Override

openhab/openhab-addons

Signed-off-by: Mike Jagdis <mjagdis@eris-associates.co.uk>

bundles/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/internal/handler/RFXComHandler.java
@@ -146,7 +146,7 @@ public class RFXComHandler extends BaseThingHandler implements DeviceMessageList
 
                         try {
                             if (channelId.equals(CHANNEL_COMMAND)) {
-                                postCommand(uid, (Command) message.convertToState(channelId));
+                                postCommand(uid, message.convertToCommand(channelId));
                             } else if (channelId.equals(CHANNEL_LOW_BATTERY)) {
                                 updateState(uid, isLowBattery(message.convertToState(CHANNEL_BATTERY_LEVEL)));
                             } else {

bundles/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/internal/messages/RFXComLighting1Message.java
@@ -14,9 +14,11 @@ package org.openhab.binding.rfxcom.internal.messages;
 
 import static org.openhab.binding.rfxcom.internal.RFXComBindingConstants.*;
 
+import org.eclipse.smarthome.core.library.types.IncreaseDecreaseType;
 import org.eclipse.smarthome.core.library.types.OnOffType;
 import org.eclipse.smarthome.core.library.types.OpenClosedType;
 import org.eclipse.smarthome.core.library.types.StringType;
+import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.State;
 import org.eclipse.smarthome.core.types.Type;
 import org.eclipse.smarthome.core.types.UnDefType;
@@ -85,6 +87,8 @@ public class RFXComLighting1Message extends RFXComDeviceMessageImpl<RFXComLighti
     public Commands command;
     public boolean group;
 
+    private static byte[] lastUnit = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
     public RFXComLighting1Message() {
         super(PacketType.LIGHTING1);
     }
@@ -116,8 +120,17 @@ public class RFXComLighting1Message extends RFXComDeviceMessageImpl<RFXComLighti
 
         if ((command == Commands.GROUP_ON) || (command == Commands.GROUP_OFF)) {
             unitCode = 0;
+        } else if ((data[5] == 0) && ((command == Commands.DIM) || (command == Commands.BRIGHT))) {
+            // SS13 switches broadcast DIM/BRIGHT to X0 and the dimmers ignore
+            // the message unless the last X<n> ON they saw was for them. So we
+            // redirect an incoming broadcast DIM/BRIGHT to the correct item
+            // based on the last X<n> we saw or sent.
+            unitCode = lastUnit[(int)houseCode - (int)'A'];
         } else {
             unitCode = data[5];
+            if (command == Commands.ON) {
+                lastUnit[(int)houseCode - (int)'A'] = unitCode;
+            }
         }
 
         signalLevel = (byte) ((data[7] & 0xF0) >> 4);
@@ -135,7 +148,18 @@ public class RFXComLighting1Message extends RFXComDeviceMessageImpl<RFXComLighti
         data[2] = subType.toByte();
         data[3] = seqNbr;
         data[4] = (byte) houseCode;
-        data[5] = unitCode;
+        // When an SS13 sends a DIM/BRIGHT it broadcasts to the all-units code (X0)
+        // and it is up to the dimmers to ignore the message if the last X<n> they
+        // saw wasn't for them. Sending to an actual X<n> may or may not work.
+        // It is untested against _any_ dimmer never mind all so we stick to doing
+        // the same as an SS13. At least for now. Using an explicit X<n> would be
+        // better (if it works) because X10 RF and PLM are lossy and different
+        // modules may have seen different traffic.
+        if ((command == Commands.DIM) || (command == Commands.BRIGHT)) {
+            data[5] = 0;
+        } else {
+            data[5] = unitCode;
+        }
         data[6] = command.toByte();
         data[7] = (byte) ((signalLevel & 0x0F) << 4);
 
@@ -148,6 +172,41 @@ public class RFXComLighting1Message extends RFXComDeviceMessageImpl<RFXComLighti
     }
 
     @Override
+    public Command convertToCommand(String channelId) throws RFXComUnsupportedChannelException {
+        switch (channelId) {
+            case CHANNEL_COMMAND:
+                switch (command) {
+                    case OFF:
+                    case GROUP_OFF:
+                        return OnOffType.OFF;
+                        break;
+
+                    case ON:
+                    case GROUP_ON:
+                        return OnOffType.ON;
+                        break;
+
+                    case DIM:
+                        return IncreaseDecreaseType.DECREASE;
+                        break;
+
+                    case BRIGHT:
+                        return IncreaseDecreaseType.INCREASE;
+                        break;
+
+                    case CHIME:
+                        return OnOffType.ON;
+                        break;
+
+                    default:
+                        throw new RFXComUnsupportedChannelException("Channel " + channelId + " does not accept " + command);
+                }
+
+            default:
+                return super.convertToCommand(channelId);
+        }
+    }
+
     public State convertToState(String channelId) throws RFXComUnsupportedChannelException {
         switch (channelId) {
             case CHANNEL_COMMAND:
@@ -164,7 +223,7 @@ public class RFXComLighting1Message extends RFXComDeviceMessageImpl<RFXComLighti
                         return OnOffType.ON;
 
                     default:
-                        throw new RFXComUnsupportedChannelException("Can't convert " + command + " for " + channelId);
+                        throw new RFXComUnsupportedChannelException("Channel " + channelId + " does not accept " + command);
                 }
 
             case CHANNEL_COMMAND_STRING:
@@ -184,11 +243,11 @@ public class RFXComLighting1Message extends RFXComDeviceMessageImpl<RFXComLighti
                         return OpenClosedType.OPEN;
 
                     default:
-                        throw new RFXComUnsupportedChannelException("Can't convert " + command + " for " + channelId);
+                        throw new RFXComUnsupportedChannelException("Channel " + channelId + " does not accept " + command);
                 }
 
             default:
-                return super.convertToState(channelId);
+                throw new RFXComUnsupportedChannelException("Channel " + channelId + " is not relevant here");
         }
     }
 