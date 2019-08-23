3fc8734485efa6d3b1564fd17f6eb91466fe64ab
psmedley <paul@smedley.id.au>
2019-08-23 21:32:15 +0930

    [opengarage] opengarage initial contribution by Paul Smedley (#5911)
    * opengarage initial contribution by Paul Smedley
    
    Signed-off-by: Paul Smedley <paul@smedley.id.au>

---
+++ bundles/org.openhab.binding.opengarage/src/main/java/org/openhab/binding/opengarage/internal/OpenGarageCommunicationException.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.opengarage.internal;
+
+import java.io.IOException;
+
+/**
+ * Exception for when an unexpected response is received from the OpenGarage controller.
+ *
+ * @author Paul Smedley - Initial contribution
+ *
+ */
+public class OpenGarageCommunicationException extends IOException {
+    private static final long serialVersionUID = 529232811860854017L;
+
+    public OpenGarageCommunicationException(String message) {
+        super(message);
+    }
+
+    public OpenGarageCommunicationException(Throwable ex) {
+        super(ex);
+    }
+
+    public OpenGarageCommunicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ bundles/org.openhab.binding.opengarage/src/main/java/org/openhab/binding/opengarage/internal/OpenGarageWebTargets.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.opengarage.internal;
+
+import java.io.IOException;
+
+import org.eclipse.smarthome.io.net.http.HttpUtil;
+import org.openhab.binding.opengarage.internal.api.ControllerVariables;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Handles performing the actual HTTP requests for communicating with Opengarage units.
+ *
+ * @author Paul Smedley - Initial Contribution
+ *
+ */
+public class OpenGarageWebTargets {
+    private static final int TIMEOUT_MS = 30000;
+
+    private String getControllerVariablesUri;
+    private String changeControllerVariablesUri;
+    private final Logger logger = LoggerFactory.getLogger(OpenGarageWebTargets.class);
+
+    public OpenGarageWebTargets(String ipAddress, long port, String password) {
+        String baseUri = "http://" + ipAddress + ":" + port + "/";
+        getControllerVariablesUri = baseUri + "jc";
+        changeControllerVariablesUri = baseUri + "cc?dkey=" + password;
+    }
+
+    public ControllerVariables getControllerVariables() throws OpenGarageCommunicationException {
+        String response = invoke(getControllerVariablesUri);
+        return ControllerVariables.parse(response);
+    }
+
+    public void setControllerVariables(boolean request) throws OpenGarageCommunicationException {
+        logger.debug("Received request: {}", request);
+          if (request) {
+              logger.debug("Received request to open door");
+              String queryParams = "&open=1";
+              invoke(changeControllerVariablesUri, queryParams);
+          } else {
+              logger.debug("Received request to close door");
+              String queryParams = "&close=1";
+              invoke(changeControllerVariablesUri, queryParams);
+          }
+    }
+
+    private String invoke(String uri) throws OpenGarageCommunicationException {
+        return invoke(uri, "");
+    }
+
+    private String invoke(String uri, String params) throws OpenGarageCommunicationException {
+        String uriWithParams = uri + params;
+        logger.debug("Calling url: {}", uriWithParams);
+        String response;
+        synchronized (this) {
+            try {
+                response = HttpUtil.executeUrl("GET", uriWithParams, TIMEOUT_MS);
+            } catch (IOException ex) {
+                logger.debug("{}", ex.getLocalizedMessage(), ex);
+                // Response will also be set to null if parsing in executeUrl fails so we use null here to make the
+                // error check below consistent.
+                response = null;
+            }
+        }
+
+        if (response == null) {
+            throw new OpenGarageCommunicationException(
+                    String.format("OpenGaragecontroller returned error while invoking %s", uriWithParams));
+        }
+        return response;
+    }
+}
