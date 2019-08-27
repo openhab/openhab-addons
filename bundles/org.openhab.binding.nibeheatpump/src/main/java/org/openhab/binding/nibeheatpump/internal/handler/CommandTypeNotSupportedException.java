8e3818dca755af075dae435a72d97c1d520736b8
Jevgeni Kiski <yozik04@gmail.com>
2019-08-27 17:08:47 +0300

    [nibeheatpump] #5012, #5604 Channel update logic, to and from modbus data conversion fixes (#5015)
    * Update channels even if value did not change
    * NibeGW to support ENC28J60
    * Less memory footprint
    * Read UDP packet flushing was missing
    * Do periodic updates to channels even if value did not change
    * Negative signed 8bit values are not parsed correctly and caching check fix
    * UDP Flush is only required for TRANSPORT_ETH_ENC28J60
    * Negative signed values convert fixes + tests
    * Proper number convert from Modbus
    * BigDecimal convert to Modbus fixes. Tests rewrite
    * Command type checking
    * MODBUS_READ_RESPONSE_MSG is now converted properly regardless of Item type
    * More command types support
    * Test improvements
    * READ_OUT message values are read twice in refreshInterval period if not changed
    * Some NibeDataType.S32 are actually NibeDataType.U32
    * TRANSPORT_ETH_ENC28J60 moved closer to PRODINO_BOARD
    * Credits to repo with int convert methods
    * Simpler method to convert. Optimize imports and reformat
    * Removed unnecessary cast, had to change values in tests to signed
    * More tests for edge cases of value converts
    * EthernetUdp import moved
    
    Fixes #5012
    Fixes #5604
    
    Signed-off-by: Jevgeni Kiski <yozik04@gmail.com> (github: yozik04)
    Signed-off-by: Daniel Dakhno <dakhnod@gmail.com>

---
+++ bundles/org.openhab.binding.nibeheatpump/src/main/java/org/openhab/binding/nibeheatpump/internal/handler/CommandTypeNotSupportedException.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.nibeheatpump.internal.handler;
+
+/**
+ * Thrown when a command type is not supported by the channel
+ *
+ * @author Jevgeni Kiski - Initial contribution
+ */
+public class CommandTypeNotSupportedException extends Exception {
+
+    private static final long serialVersionUID = -1238089408157624662L;
+}
