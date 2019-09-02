eb8469da58c97f214107aa783e9f7132d14f1615
lolodomo <lg.hc@free.fr>
2019-09-02 22:21:39 +0200

    [rotel] Rotel Binding initial contribution (#5805)
    * [rotel] Rotel Binding initial contribution
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>
    Signed-off-by: Maximilian Hess <mail@ne0h.de>

---
+++ bundles/org.openhab.binding.rotel/src/main/java/org/openhab/binding/rotel/internal/RotelPlayStatus.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.rotel.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Represents the different states of a source playback
+ *
+ * @author Laurent Garnier - Initial contribution
+ */
+@NonNullByDefault
+public enum RotelPlayStatus {
+    PLAYING,
+    PAUSED,
+    STOPPED;
+}
