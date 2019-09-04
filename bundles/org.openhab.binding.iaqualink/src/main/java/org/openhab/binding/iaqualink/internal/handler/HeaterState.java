72b8e36c9981619dedd6a6fab651b3b8a543cd85
Dan Cunningham <dan@digitaldan.com>
2019-09-04 11:14:10 -0700

    [iaqualink] iAquaLink Binding initial contribution (#5814)
    * Initial commit of the iAquaLink binding
    
    Signed-off-by: digitaldan <dan@digitaldan.com>

---
+++ bundles/org.openhab.binding.iaqualink/src/main/java/org/openhab/binding/iaqualink/internal/handler/HeaterState.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.iaqualink.internal.handler;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * Heater States returned by iAquaLink API
+ *
+ * @author Dan Cunningham - Initial contribution
+ *
+ */
+@NonNullByDefault
+public enum HeaterState {
+    OFF("0", "off"),
+    HEATING("1", "on"),
+    ENABLED("3", "enabled");
+
+    String value;
+    String label;
+
+    HeaterState(String value, String label) {
+        this.value = value;
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public static @Nullable HeaterState fromValue(String value) {
+        for (HeaterState state : HeaterState.values()) {
+            if (state.value.equals(value)) {
+                return state;
+            }
+        }
+        return null;
+    }
+}
