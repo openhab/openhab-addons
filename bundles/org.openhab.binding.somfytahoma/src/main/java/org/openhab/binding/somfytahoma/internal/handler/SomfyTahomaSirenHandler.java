88510b5c28b79875f55bd64ccf9f66aac4a2ada5
Ondrej Pecta <opecta@gmail.com>
2019-09-27 22:17:25 +0200

    [somfytahoma] Added support for ElectricitySensor, Dock & Siren (#5811)
    Signed-off-by: Ondrej Pecta <opecta@gmail.com>
    Signed-off-by: Rene Scherer <rene@scherer-online.com>

--- bundles/org.openhab.binding.somfytahoma/src/main/java/org/openhab/binding/somfytahoma/internal/handler/SomfyTahomaSirenHandler.java
+++ bundles/org.openhab.binding.somfytahoma/src/main/java/org/openhab/binding/somfytahoma/internal/handler/SomfyTahomaSirenHandler.java
@@ -17,6 +17,8 @@ import static org.openhab.binding.somfytahoma.internal.SomfyTahomaBindingConstan
 import java.util.HashMap;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.library.types.StringType;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.types.Command;
@@ -25,22 +27,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The {@link SomfyTahomaHeatingSystemHandler} is responsible for handling commands,
- * which are sent to one of the channels of the heating system thing.
+ * The {@link SomfyTahomaSirenHandler} is responsible for handling commands,
+ * which are sent to one of the channels of the siren thing.
  *
  * @author Ondrej Pecta - Initial contribution
  */
 @NonNullByDefault
-public class SomfyTahomaHeatingSystemHandler extends SomfyTahomaBaseThingHandler {
+public class SomfyTahomaSirenHandler extends SomfyTahomaBaseThingHandler {
 
-    private final Logger logger = LoggerFactory.getLogger(SomfyTahomaHeatingSystemHandler.class);
+    private final Logger logger = LoggerFactory.getLogger(SomfyTahomaSirenHandler.class);
 
-    public SomfyTahomaHeatingSystemHandler(Thing thing) {
+    public SomfyTahomaSirenHandler(Thing thing) {
         super(thing);
-        stateNames.put(TARGET_TEMPERATURE, "core:TargetTemperatureState");
-        stateNames.put(CURRENT_TEMPERATURE, "zwave:SetPointHeatingValueState");
-        stateNames.put(BATTERY_LEVEL, "core:BatteryLevelState");
-        stateNames.put(CURRENT_STATE, "zwave:SetPointTypeState");
+        stateNames.put(BATTERY, "core:BatteryState");
+        stateNames.put(ONOFF_STATE, "core:OnOffState");
+        stateNames.put(MEMORIZED_VOLUME, "io:MemorizedSimpleVolumeState");
     }
 
     @Override
@@ -48,11 +49,17 @@ public class SomfyTahomaHeatingSystemHandler extends SomfyTahomaBaseThingHandler
         logger.debug("Received command {} for channel {}", command, channelUID);
         if (RefreshType.REFRESH.equals(command)) {
             updateChannelState(channelUID);
-        } else {
-            if (TARGET_TEMPERATURE.equals(channelUID.getId())) {
-                String param = "[" + command.toString() + "]";
-                sendCommand("setTargetTemperature", param);
-            }
         }
+
+        //it is possible only to disable the siren
+        if (ONOFF_STATE.equals(channelUID.getId()) && command.equals(OnOffType.OFF)) {
+            sendCommand(COMMAND_OFF, "[]");
+        }
+
+        // highest or normal memorized volume
+        if (MEMORIZED_VOLUME.equals(channelUID.getId()) && command instanceof StringType) {
+            sendCommand("setMemorizedSimpleVolume", "[\"" + command.toString().toLowerCase() + "\"]");
+        }
+
     }
 }
