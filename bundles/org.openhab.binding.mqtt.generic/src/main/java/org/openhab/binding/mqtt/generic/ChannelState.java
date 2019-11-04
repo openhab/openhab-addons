bd51a8290edecbc4fb0974d62357befdcc62a9d4
J-N-K <J-N-K@users.noreply.github.com>
2019-11-04 13:41:58 +0100

    [mqtt] fix compilation error (#6254)
    * fix compilation error
    
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

--- bundles/org.openhab.binding.mqtt.generic/src/main/java/org/openhab/binding/mqtt/generic/ChannelState.java
+++ bundles/org.openhab.binding.mqtt.generic/src/main/java/org/openhab/binding/mqtt/generic/ChannelState.java
@@ -159,7 +159,7 @@ public class ChannelState implements MqttMessageSubscriber {
             if (transformedValue != null) {
                 strValue = transformedValue;
             } else {
-                logger.info("Transformation '{}' returned null on '{}', discarding message", strValue,
+                logger.debug("Transformation '{}' returned null on '{}', discarding message", strValue,
                         t.serviceName);
                 receivedOrTimeout();
                 return;
@@ -350,7 +350,14 @@ public class ChannelState implements MqttMessageSubscriber {
 
         // Outgoing transformations
         for (ChannelStateTransformation t : transformationsOut) {
-            mqttCommandValue = t.processValue(mqttCommandValue);
+            String transformedValue = t.processValue(mqttCommandValue);
+            if (transformedValue != null) {
+                mqttCommandValue = transformedValue;
+            } else {
+                logger.debug("Transformation '{}' returned null on '{}', discarding message", mqttCommandValue,
+                        t.serviceName);
+                return CompletableFuture.completedFuture(false);
+            }
         }
 
         // Formatter: Applied before the channel state value is published to the MQTT broker.
