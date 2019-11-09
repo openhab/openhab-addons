c82eded39195d273305afc84f72fa2c215dc2216
Arne Seime <arne.seime@gmail.com>
2019-11-09 13:13:07 +0100

    Added missing comment & license header
    Signed-off-by: Arne Seime <arne.seime@gmail.com>

--- bundles/org.openhab.binding.sensibo/src/test/java/org/openhab/binding/sensibo/internal/handler/SensiboSkyHandlerTest.java
+++ bundles/org.openhab.binding.sensibo/src/test/java/org/openhab/binding/sensibo/internal/handler/SensiboSkyHandlerTest.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.sensibo.internal.handler;
 
 import static org.junit.Assert.*;
@@ -31,6 +43,9 @@ import org.openhab.binding.sensibo.internal.handler.SensiboSkyHandler.StateChang
 import org.openhab.binding.sensibo.internal.model.SensiboModel;
 import org.openhab.binding.sensibo.internal.model.SensiboSky;
 
+/**
+ * @author Arne Seime - Initial contribution
+ */
 public class SensiboSkyHandlerTest {
 
     private WireHelper wireHelper = new WireHelper();
