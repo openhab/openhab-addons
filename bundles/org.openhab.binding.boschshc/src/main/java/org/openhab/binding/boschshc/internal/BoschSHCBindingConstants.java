31102cc336f0a58583523b40999527b4e0f85fd8
Stefan Kaestle <stefan@mad-kow.de>
2019-11-16 16:40:55 +0100

    Initial code from create_openhab_binding_skeleton.sh
    Signed-off-by: Stefan Kaestle <stefan@mad-kow.de>

---
+++ bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/BoschSHCBindingConstants.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.boschshc.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+/**
+ * The {@link BoschSHCBindingConstants} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Stefan Kästle - Initial contribution
+ */
+@NonNullByDefault
+public class BoschSHCBindingConstants {
+
+    private static final String BINDING_ID = "boschshc";
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID THING_TYPE_SAMPLE = new ThingTypeUID(BINDING_ID, "sample");
+
+    // List of all Channel ids
+    public static final String CHANNEL_1 = "channel1";
+}
