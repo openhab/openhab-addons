20205cc1fb004ab8b3563283fb682091a54986f8
Daniel Weber <25605184+fruggy83@users.noreply.github.com>
2019-11-25 22:29:13 +0100

    [enocean] Implement EEP A5-13 for environmental sensors (#5741)
    fixed formatting issues and added class descriptions
    
    Fixes #5740
    
    Signed-off-by: Daniel Weber <uni@fruggy.de>

---
+++ bundles/org.openhab.binding.enocean/src/main/java/org/openhab/binding/enocean/internal/eep/A5_13/A5_13.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.enocean.internal.eep.A5_13;
+
+import org.openhab.binding.enocean.internal.eep.Base._4BSMessage;
+import org.openhab.binding.enocean.internal.messages.ERP1Message;
+
+/**
+ * Base class for A5-13 EEP message handling
+ * 
+ * @author Daniel Weber - Initial contribution
+ */
+public abstract class A5_13 extends _4BSMessage {
+    public A5_13(ERP1Message packet) {
+        super(packet);
+    }
+
+    protected final int PARTONE = 0x10;
+    protected final int PARTTWO = 0x20;
+
+    protected int getMessageIdentifier() {
+        return getDB_0Value() & 0xF0;
+    }
+
+    protected boolean isPartOne() {
+        return getMessageIdentifier() == PARTONE;
+    }
+
+    protected boolean isPartTwo() {
+        return getMessageIdentifier() == PARTTWO;
+    }
+}
