7d7739e9b942c79ddd7818a196744d0bfae75d32
Hilbrand Bouwkamp <hilbrand@h72.nl>
2019-11-28 12:28:51 +0100

    Use enum of PMWState
    Signed-off-by: Hilbrand Bouwkamp <hilbrand@h72.nl>

--- bundles/org.openhab.binding.energenie/src/main/java/org/openhab/binding/energenie/internal/handler/EnergeniePWMHandler.java
+++ bundles/org.openhab.binding.energenie/src/main/java/org/openhab/binding/energenie/internal/handler/EnergeniePWMHandler.java
@@ -12,8 +12,6 @@
  */
 package org.openhab.binding.energenie.internal.handler;
 
-import static org.openhab.binding.energenie.internal.EnergenieBindingConstants.*;
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,9 +19,12 @@ import java.nio.charset.Charset;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
+import javax.measure.Unit;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.library.types.QuantityType;
+import org.eclipse.smarthome.core.library.unit.SmartHomeUnits;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingStatus;
@@ -31,6 +32,7 @@ import org.eclipse.smarthome.core.thing.ThingStatusDetail;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.State;
+import org.eclipse.smarthome.core.types.UnDefType;
 import org.eclipse.smarthome.io.net.http.HttpUtil;
 import org.openhab.binding.energenie.internal.config.EnergenieConfiguration;
 import org.slf4j.Logger;
@@ -44,6 +46,69 @@ import org.slf4j.LoggerFactory;
 
 @NonNullByDefault
 public class EnergeniePWMHandler extends BaseThingHandler {
+
+    public enum PWMState {
+        VOLTAGE("var V  = ", 9, 20, 10, SmartHomeUnits.VOLT),
+        CURRENT("var V  = ", 9, 20, 100, SmartHomeUnits.AMPERE),
+        POWER("var P=", 6, 20, 466, SmartHomeUnits.WATT),
+        ENERGY("var E=", 6, 20, 25600, SmartHomeUnits.KILOWATT_HOUR);
+
+        private final Logger logger = LoggerFactory.getLogger(PWMState.class);
+
+        private final String stateResponseSearch;
+        private final int start;
+        private final int stop;
+        private final int divisor;
+        private final Unit<?> unit;
+
+        private PWMState(final String stateResponseSearch, final int start, final int stop, final int divisor,
+                final Unit<?> unit) {
+            this.stateResponseSearch = stateResponseSearch;
+            this.start = start;
+            this.stop = stop;
+            this.divisor = divisor;
+            this.unit = unit;
+        }
+
+        public String getStateResponseSearch() {
+            return stateResponseSearch;
+        }
+
+        public int getStart() {
+            return start;
+        }
+
+        public int getStop() {
+            return stop;
+        }
+
+        public int getDivisor() {
+            return divisor;
+        }
+
+        public State readState(final String loginResponseString) {
+            final int findState = loginResponseString.lastIndexOf(stateResponseSearch);
+
+            if (findState > 0) {
+                logger.trace("searchstring {} found at position {}", stateResponseSearch, findState);
+                final String slicedResponseTmp = loginResponseString.substring(findState + start, findState + stop);
+                logger.trace("transformed state response = {}", slicedResponseTmp);
+                final String[] slicedResponse = slicedResponseTmp.split(";");
+                logger.trace("transformed state response = {} - {}", slicedResponse[0], slicedResponse[1]);
+                final double value;
+
+                if (Double.parseDouble(slicedResponse[0]) / 1 == Double.parseDouble(slicedResponse[0])) {
+                    value = Double.parseDouble(slicedResponse[0]) / divisor;
+                } else {
+                    value = -1.0;
+                }
+                return QuantityType.valueOf(value, unit);
+            } else {
+                logger.trace("searchstring '{} not found", stateResponseSearch);
+                return UnDefType.UNDEF;
+            }
+        }
+    }
 
     private final Logger logger = LoggerFactory.getLogger(EnergeniePWMHandler.class);
 
@@ -107,10 +172,10 @@ public class EnergeniePWMHandler extends BaseThingHandler {
             loginResponseString = HttpUtil.executeUrl("POST", url, urlContent, "TEXT/PLAIN", HTTP_TIMEOUT_MILLISECONDS);
 
             if (loginResponseString != null) {
-                readState(loginResponseString, "voltage");
-                readState(loginResponseString, "current");
-                readState(loginResponseString, "power");
-                readState(loginResponseString, "energy");
+                updateState("voltage", PWMState.VOLTAGE.readState(loginResponseString));
+                updateState("current", PWMState.CURRENT.readState(loginResponseString));
+                updateState("power", PWMState.POWER.readState(loginResponseString));
+                updateState("energy", PWMState.ENERGY.readState(loginResponseString));
                 try {
                     HttpUtil.executeUrl("POST", url, HTTP_TIMEOUT_MILLISECONDS);
                     logger.trace("logout from ip {}", host);
@@ -124,60 +189,6 @@ public class EnergeniePWMHandler extends BaseThingHandler {
             logger.debug("energenie: failed to login to {} with ip {}", thing.getUID(), host, e);
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR, e.getMessage());
         }
-    }
-
-    public void readState(String loginResponseString, String channel) {
-        String stateResponseSearch = "";
-        int start = 0, stop = 0, divisor = 1;
-        double value = 0.0;
-
-        switch (channel) {
-            case VOLTAGE:
-                stateResponseSearch = "var V  = ";
-                start = 9;
-                stop = 20;
-                divisor = 10;
-
-                break;
-            case CURRENT:
-                stateResponseSearch = "var I  = ";
-                start = 9;
-                stop = 20;
-                divisor = 100;
-                break;
-            case POWER:
-                stateResponseSearch = "var P=";
-                start = 6;
-                stop = 20;
-                divisor = 466;
-                break;
-            case ENERGY:
-                stateResponseSearch = "var E=";
-                start = 6;
-                stop = 20;
-                divisor = 25600;
-                break;
-        }
-
-        int findState = loginResponseString.lastIndexOf(stateResponseSearch);
-        if (findState > 0) {
-            logger.trace("searchstring {} found at position {}", stateResponseSearch, findState);
-            String slicedResponseTmp = loginResponseString.substring(findState + start, findState + stop);
-            logger.trace("transformed state response = {}", slicedResponseTmp);
-            String[] slicedResponse = slicedResponseTmp.split(";");
-            logger.trace("transformed state response = {} - {}", slicedResponse[0], slicedResponse[1]);
-            if (Double.parseDouble(slicedResponse[0]) / 1 == Double.parseDouble(slicedResponse[0])) {
-                value = Double.parseDouble(slicedResponse[0]) / divisor;
-            } else {
-                value = -1.0;
-            }
-            State valueState = new DecimalType(value);
-            updateState(channel, valueState);
-
-        } else {
-            logger.trace("searchstring '{} not found", stateResponseSearch);
-        }
-
     }
 
     private synchronized void onUpdate() {
