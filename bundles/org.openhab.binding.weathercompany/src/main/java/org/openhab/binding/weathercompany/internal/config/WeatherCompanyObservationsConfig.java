ce726c8d746cc41c7ba42e45fb560099bac7d95d
Mark Hilbush <mark@hilbush.com>
2019-12-05 08:08:05 -0500

    [weathercompany] Weather Company Binding initial contribution (#5780)
    Signed-off-by: Mark Hilbush <mark@hilbush.com>

---
+++ bundles/org.openhab.binding.weathercompany/src/main/java/org/openhab/binding/weathercompany/internal/config/WeatherCompanyObservationsConfig.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.weathercompany.internal.config;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * The {@link WeatherCompanyObservationsConfig} class contains fields mapping thing configuration parameters.
+ *
+ * @author Mark Hilbush - Initial contribution
+ */
+@NonNullByDefault
+public class WeatherCompanyObservationsConfig {
+    /**
+     * Personal Weather Station Id
+     */
+    public @Nullable String pwsStationId;
+
+    /**
+     * Interval with which forecast will be updated.
+     */
+    public int refreshInterval;
+
+    @Override
+    public String toString() {
+        return "WeatherCompanyConfig: { pwsStationId=" + pwsStationId + ", refreshInterval=" + refreshInterval + " }";
+    }
+}
