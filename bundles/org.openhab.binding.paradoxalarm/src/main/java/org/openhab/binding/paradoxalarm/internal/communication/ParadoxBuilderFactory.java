a44af2a6b1d0c6397bb5cfb5562c37b15bdd6f20
Konstantin Polihronov <polychronov@gmail.com>
2019-12-05 17:39:19 +0200

    Implementation of maxZones and maxPartitions parameters
    * Reworked creation of EvoCommunicator to use builder pattern
    * Added support for additional, non-mandatory parameters maxZones and
    maxPartitions which limit the maximum amount of zones and partitions
    that are used during refresh.
    * Changed Factory to create builder instead of communicator
    * Renamed classes to represent properly the new object creation design
    
    Signed-off-by: Konstantin Polihronov <polychronov@gmail.com>

---
+++ bundles/org.openhab.binding.paradoxalarm/src/main/java/org/openhab/binding/paradoxalarm/internal/communication/ParadoxBuilderFactory.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.paradoxalarm.internal.communication;
+
+import org.openhab.binding.paradoxalarm.internal.exceptions.ParadoxRuntimeException;
+import org.openhab.binding.paradoxalarm.internal.model.PanelType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link ParadoxBuilderFactory} used to create the proper communicator builder objects for different panel
+ * types.
+ *
+ * @author Konstantin Polihronov - Initial contribution
+ */
+public class ParadoxBuilderFactory {
+
+    private final Logger logger = LoggerFactory.getLogger(ParadoxBuilderFactory.class);
+
+    public ICommunicatorBuilder createBuilder(PanelType panelType) {
+        switch (panelType) {
+            case EVO48:
+            case EVO96:
+            case EVO192:
+                logger.debug("Creating new builder for Paradox {} system", panelType);
+                return new EvoCommunicator.EvoCommunicatorBuilder(panelType);
+            default:
+                throw new ParadoxRuntimeException("Unsupported panel type: " + panelType);
+        }
+    }
+}
