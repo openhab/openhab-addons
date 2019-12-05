760d73f75c4e3ca99658f7f193080b1d7b06e8a9
Konstantin Polihronov <polychronov@gmail.com>
2019-12-05 17:39:19 +0200

    Implementation of maxZones and maxPartitions parameters
    * Reworked creation of EvoCommunicator to use builder pattern
    * Added support for additional, non-mandatory parameters maxZones and
    maxPartitions which limit the maximum amount of zones and partitions
    that are used during refresh.
    * Changed Factory to create builder instead of communicator
    * Renamed classes to represent properly the new object creation design
    
    Signed-off-by: Konstantin Polihronov <polychronov@gmail.com>

---
+++ bundles/org.openhab.binding.paradoxalarm/src/main/java/org/openhab/binding/paradoxalarm/internal/communication/ICommunicatorBuilder.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.paradoxalarm.internal.communication;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+/**
+ * The {@link ICommunicatorBuilder} is representing the functionality of communicator builders.
+ * The idea is to ease initialization of communicators which can have lots of parameters.
+ *
+ * @author Konstantin Polihronov - Initial contribution
+ */
+public interface ICommunicatorBuilder {
+    ICommunicatorBuilder withMaxZones(Integer zones);
+
+    ICommunicatorBuilder withMaxPartitions(Integer partitions);
+
+    ICommunicatorBuilder withIp150Password(String ip150Password);
+
+    ICommunicatorBuilder withPcPassword(String pcPassword);
+
+    ICommunicatorBuilder withIpAddress(String ipAddress);
+
+    ICommunicatorBuilder withTcpPort(Integer tcpPort);
+
+    ICommunicatorBuilder withScheduler(ScheduledExecutorService scheduler);
+
+    IParadoxCommunicator build();
+}
