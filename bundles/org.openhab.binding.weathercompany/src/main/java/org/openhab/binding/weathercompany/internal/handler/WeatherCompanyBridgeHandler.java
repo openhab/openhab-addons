25af81063b87633484e209663467502898e2a611
Mark Hilbush <mark@hilbush.com>
2019-12-06 08:48:17 -0500

    Fix handling of thing status
    Signed-off-by: Mark Hilbush <mark@hilbush.com>

--- bundles/org.openhab.binding.weathercompany/src/main/java/org/openhab/binding/weathercompany/internal/handler/WeatherCompanyBridgeHandler.java
+++ bundles/org.openhab.binding.weathercompany/src/main/java/org/openhab/binding/weathercompany/internal/handler/WeatherCompanyBridgeHandler.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.weathercompany.internal;
+package org.openhab.binding.weathercompany.internal.handler;
 
 import java.io.IOException;
 import java.util.concurrent.Future;
