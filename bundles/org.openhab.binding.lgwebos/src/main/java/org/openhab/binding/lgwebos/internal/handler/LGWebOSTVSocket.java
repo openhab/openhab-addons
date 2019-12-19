6fe30e526a1ea7a55b7b529671f8526d9a0e2c9b
Bob Brodt <bbrodt@redhat.com>
2019-12-19 13:41:09 -0700

    Added rcButton channel
    Signed-off-by: Bob Brodt <bbrodt@redhat.com>

--- bundles/org.openhab.binding.lgwebos/src/main/java/org/openhab/binding/lgwebos/internal/handler/LGWebOSTVSocket.java
+++ bundles/org.openhab.binding.lgwebos/src/main/java/org/openhab/binding/lgwebos/internal/handler/LGWebOSTVSocket.java
@@ -812,6 +812,14 @@ public class LGWebOSTVSocket {
 
     }
 
+    // Simulate Remote Control Button press
+
+    public void sendRCButton(String rcButton, ResponseListener<CommandConfirmation> listener) {
+        executeMouse(s -> s.button(rcButton));
+    }
+
+    //
+
     public interface ConfigProvider {
         void storeKey(String key);
 
