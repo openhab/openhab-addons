d60491b360f37eac7813cb3ebc34cc566d7b191c
Sara Damiano <sdamiano@stroudcenter.org>
2020-01-12 15:21:32 -0500

    Try again to get color right.
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- bundles/org.openhab.binding.wizlighting/src/main/java/org/openhab/binding/wizlighting/internal/utils/WizColorConverter.java
+++ bundles/org.openhab.binding.wizlighting/src/main/java/org/openhab/binding/wizlighting/internal/utils/WizColorConverter.java
@@ -46,45 +46,32 @@ public class WizColorConverter {
      * the color channels. When creating colors, the WiZ bulbs only use the warm
      * white channel, the cool white channel is ignored.
      *
-     * Totally made this up. It's based on the commonly referenced SaikoLED blog, but that doesn't include a separate
-     * dimming channel. I couldn't find any satisfactory conversions that actually
-     * included the channels the way that the WiZ bulbs seem to.
+     * Totally made this up. It's based on the commonly referenced SaikoLED blog,
+     * but that doesn't include a separate but that doesn't include a
+     * separate dimming channel. I couldn't find any satisfactory conversions that
+     * actually included the channels the way that the WiZ bulbs seem to.
      *
      * @param hsbColor the {@link HSBType}.
      *
      * @return an interger array of the color components
      */
     public int[] hsbToRgbw(HSBType hsb) {
-        logger.trace("Converting hue and saturation to RGBW.  Incoming: {}", hsb);
+        logger.debug("Converting hue and saturation to RGBW.  Incoming: {}", hsb);
         double redD, greenD, blueD;
         int red, green, blue, white;
 
-        double hue = hsb.getHue().doubleValue() / 100;
-        double cosHue;
-        double cos60MinusHue;
+        // Since we're going to use the white lights to control saturation, recalculate what
+        // the HSBvalue would be if the color was fully saturated.
+        DecimalType hue = hsb.getHue();
+        PercentType valueBrightness = hsb.getBrightness();
+        // Create the new hue
+        HSBType fullySaturatedHue = new HSBType(hue, new PercentType(100), valueBrightness);
+        // get the RGB from the saturated colors
+        redD = fullySaturatedHue.getRed().doubleValue();
+        greenD = fullySaturatedHue.getGreen().doubleValue();
+        blueD = fullySaturatedHue.getBlue().doubleValue();
 
-        if (hue < 120) { // red to green
-            cosHue = Math.cos(Math.toRadians(hue));
-            cos60MinusHue = Math.cos(Math.toRadians(60 - hue));
-            redD = (255 * (1 + cosHue / cos60MinusHue));
-            greenD = (255 * (1 + (1 - cosHue / cos60MinusHue)));
-            blueD = 0;
-        } else if (hue < 240) { // green to blue
-            hue = hue - 120;
-            cosHue = Math.cos(Math.toRadians(hue));
-            cos60MinusHue = Math.cos(Math.toRadians(60 - hue));
-            greenD = (255 * (1 + cosHue / cos60MinusHue));
-            blueD = (255 * (1 + (1 - cosHue / cos60MinusHue)));
-            redD = 0;
-        } else { // blue to red
-            hue = hue - 240;
-            cosHue = Math.cos(Math.toRadians(hue));
-            cos60MinusHue = Math.cos(Math.toRadians(60 - hue));
-            blueD = (255 * (1 + cosHue / cos60MinusHue));
-            redD = (255 * (1 + (1 - cosHue / cos60MinusHue)));
-            greenD = 0;
-        }
-        logger.trace("Colors from hue R: {} G: {} B: {}", redD, greenD, blueD);
+        logger.debug("Colors from hue, assuming full saturation R: {} G: {} B: {}", redD, greenD, blueD);
 
         // Convert the "PercentType" to a percent
         double saturationPercent = (hsb.getSaturation().doubleValue() / 100);
@@ -106,7 +93,7 @@ public class WizColorConverter {
         }
 
         // Note: We're keeping the brightness in a totally separate channel
-        logger.trace("Outgoing colors, adjusted for saturation R: {} G: {} B: {} W: {}", red, green, blue, white);
+        logger.debug("Outgoing colors, adjusted for saturation R: {} G: {} B: {} W: {}", red, green, blue, white);
         return new int[] { red, green, blue, white };
     }
 
@@ -123,7 +110,7 @@ public class WizColorConverter {
      * @return a {@link ColorRequestParam} with the color components
      */
     public HSBType rgbwDimmingToHSB(int red, int green, int blue, int white, int dimming) {
-        logger.trace("Converting RGB to HS. Incoming R: {} G: {} B: {} W: {}", red, green, blue, white);
+        logger.debug("Converting RGB to HS. Incoming R: {} G: {} B: {} W: {}", red, green, blue, white);
         // Can get hue from the ratios of the colors.
         // The calculated *hue* component of the HSB should be correct regardless of the
         // state of the white lights because it's strictly based on the ratio of the colors
@@ -134,6 +121,8 @@ public class WizColorConverter {
         } else {
             saturationPercent = Math.max(red, Math.max(green, blue)) / (255 * 2);
         }
-        return new HSBType(hue, new PercentType((int) saturationPercent * 100), new PercentType(dimming));
+        HSBType out = new HSBType(hue, new PercentType((int) saturationPercent * 100), new PercentType(dimming));
+        logger.debug("Outgoing HSB: {}", out);
+        return out;
     }
 }
