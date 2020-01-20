512ff432f8d26390a641e5af93ce46dc81d65493
Gaël L'hopital <gael@lhopital.org>
2020-01-20 17:02:56 +0100

    Revert incorrect correction
    Signed-off-by: Gaël L'hopital <gael@lhopital.org>

--- bundles/org.openhab.binding.network/src/main/java/org/openhab/binding/network/internal/dhcp/DHCPPacketListenerServer.java
+++ bundles/org.openhab.binding.network/src/main/java/org/openhab/binding/network/internal/dhcp/DHCPPacketListenerServer.java
@@ -70,7 +70,7 @@ public class DHCPPacketListenerServer extends Thread {
 
         Byte dhcpMessageType = request.getDHCPMessageType();
 
-        if (dhcpMessageType != null && dhcpMessageType != DHCPPacket.DHCPREQUEST) {
+        if (dhcpMessageType != DHCPPacket.DHCPREQUEST) {
             return; // skipping non DHCPREQUEST message types
         }
 
