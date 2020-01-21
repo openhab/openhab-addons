9fe5cab0c7be49aece59df1d2753348aada08779
jossuar <georgios.moutsos@gmail.com>
2020-01-21 04:03:23 +0200

    Corrected build errors
    Signed-off-by: Georgios Moutsos <georgios.moutsos@gmail.com>

--- bundles/org.openhab.binding.caddx/src/main/java/org/openhab/binding/caddx/internal/discovery/CaddxDiscoveryService.java
+++ bundles/org.openhab.binding.caddx/src/main/java/org/openhab/binding/caddx/internal/discovery/CaddxDiscoveryService.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2019 Contributors to the openHAB project
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -115,7 +115,7 @@ public class CaddxDiscoveryService extends AbstractDiscoveryService {
             thingDiscovered(DiscoveryResultBuilder.create(thingUID).withProperties(properties)
                     .withLabel("Caddx Bridge - " + port).build());
         } catch (Exception e) {
-            logger.warn("addBridge(): Error: {}", e);
+            logger.warn("addBridge(): ", e);
         }
     }
 
