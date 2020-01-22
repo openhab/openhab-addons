04bee821c818539c9881202c4ab30b7e0e00853c
Dan Cunningham <dan@digitaldan.com>
2020-01-22 01:28:11 -0800

    [hydrawise] Some sensors do not have values for all fields.  Fixes #6868 . (#6882)
    Signed-off-by: digitaldan <dan@digitaldan.com>

--- bundles/org.openhab.binding.hydrawise/src/main/java/org/openhab/binding/hydrawise/internal/HydrawiseCloudHandler.java
+++ bundles/org.openhab.binding.hydrawise/src/main/java/org/openhab/binding/hydrawise/internal/HydrawiseCloudHandler.java
@@ -182,12 +182,17 @@ public class HydrawiseCloudHandler extends HydrawiseHandler {
     private void updateSensors(StatusScheduleResponse status) {
         status.sensors.forEach(sensor -> {
             String group = "sensor" + sensor.input;
-            updateGroupState(group, CHANNEL_SENSOR_ACTIVE, sensor.active > 0 ? OnOffType.ON : OnOffType.OFF);
             updateGroupState(group, CHANNEL_SENSOR_MODE, new DecimalType(sensor.type));
             updateGroupState(group, CHANNEL_SENSOR_NAME, new StringType(sensor.name));
-            updateGroupState(group, CHANNEL_SENSOR_OFFLEVEL, new DecimalType(sensor.offlevel));
             updateGroupState(group, CHANNEL_SENSOR_OFFTIMER, new DecimalType(sensor.offtimer));
             updateGroupState(group, CHANNEL_SENSOR_TIMER, new DecimalType(sensor.timer));
+            // Some fields are missing depending on sensor type.
+            if (sensor.offlevel != null) {
+                updateGroupState(group, CHANNEL_SENSOR_OFFLEVEL, new DecimalType(sensor.offlevel));
+            }
+            if (sensor.active != null) {
+                updateGroupState(group, CHANNEL_SENSOR_ACTIVE, sensor.active > 0 ? OnOffType.ON : OnOffType.OFF);
+            }
         });
     }
 
