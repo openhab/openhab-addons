f1dad1337fa0c9cbdc7be159689f55a19b520e56
Constantin Piber <cp.piber@gmail.com>
2020-01-25 18:26:49 +0100

    Cosmetic changes, cleanup, spotless
    Signed-off-by: Constantin Piber <cp.piber@gmail.com>

--- bundles/org.openhab.binding.exec/src/main/java/org/openhab/binding/exec/internal/handler/ExecHandler.java
+++ bundles/org.openhab.binding.exec/src/main/java/org/openhab/binding/exec/internal/handler/ExecHandler.java
@@ -24,9 +24,9 @@ import java.util.Calendar;
 import java.util.IllegalFormatException;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.PatternSyntaxException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -66,8 +66,8 @@ public class ExecHandler extends BaseThingHandler {
     /**
      * Shell executables
      */
-    public static final String[] SHELL_WINDOWS = new String[] {"cmd"};
-    public static final String[] SHELL_NIX = new String[] {"sh", "bash", "zsh", "csh"};
+    public static final String[] SHELL_WINDOWS = new String[] { "cmd" };
+    public static final String[] SHELL_NIX = new String[] { "sh", "bash", "zsh", "csh" };
 
     private Logger logger = LoggerFactory.getLogger(ExecHandler.class);
 
@@ -186,8 +186,7 @@ public class ExecHandler extends BaseThingHandler {
                     try {
                         cmdArray = commandLine.split(CMD_LINE_DELIMITER);
                     } catch (PatternSyntaxException e) {
-                        logger.warn("An exception occurred while splitting '{}' : '{}'",
-                                commandLine, e.getMessage());
+                        logger.warn("An exception occurred while splitting '{}' : '{}'", commandLine, e.getMessage());
                         updateState(RUN, OnOffType.OFF);
                         updateState(OUTPUT, new StringType(e.getMessage()));
                         return;
@@ -195,22 +194,28 @@ public class ExecHandler extends BaseThingHandler {
                 } else {
                     // Invoke shell with 'c' option and pass string
                     logger.debug("Passing to shell for parsing command.");
-                    if (getOperatingSystemType() == OS.WINDOWS) {
-                        shell = SHELL_WINDOWS;
-                        logger.debug("OS: WINDOWS ({})", getOperatingSystemName());
-                        cmdArray = createCmdArray(shell, "/c", commandLine);
-                    } else if (getOperatingSystemType() != OS.UNKNOWN) {
-                        // assume sh is present, should all be POSIX-compliant
-                        shell = SHELL_NIX;
-                        logger.debug("OS: *NIX ({})", getOperatingSystemName());
-                        cmdArray = createCmdArray(shell, "-c", commandLine);
-                    } else {
-                        String err = "OS not supported, please manually split commands!";
-                        logger.debug("OS: Unknown ({})", getOperatingSystemName());
-                        logger.warn(err);
-                        updateState(RUN, OnOffType.OFF);
-                        updateState(OUTPUT, new StringType(err));
-                        return;
+                    switch (getOperatingSystemType()) {
+                        case WINDOWS:
+                            shell = SHELL_WINDOWS;
+                            logger.debug("OS: WINDOWS ({})", getOperatingSystemName());
+                            cmdArray = createCmdArray(shell, "/c", commandLine);
+                            break;
+
+                        case LINUX:
+                        case MAC:
+                        case SOLARIS:
+                            // assume sh is present, should all be POSIX-compliant
+                            shell = SHELL_NIX;
+                            logger.debug("OS: *NIX ({})", getOperatingSystemName());
+                            cmdArray = createCmdArray(shell, "-c", commandLine);
+
+                        default:
+                            logger.debug("OS: Unknown ({})", getOperatingSystemName());
+                            logger.warn("OS {} not supported, please manually split commands!",
+                                    getOperatingSystemName());
+                            updateState(RUN, OnOffType.OFF);
+                            updateState(OUTPUT, new StringType("OS not supported, please manually split commands!"));
+                            return;
                     }
                 }
 
@@ -225,8 +230,8 @@ public class ExecHandler extends BaseThingHandler {
                 try {
                     proc = rt.exec(cmdArray);
                 } catch (Exception e) {
-                    logger.error("An exception occurred while executing '{}' : '{}'",
-                            Arrays.asList(cmdArray), e.getMessage());
+                    logger.warn("An exception occurred while executing '{}' : '{}'", Arrays.asList(cmdArray),
+                            e.getMessage());
                     updateState(RUN, OnOffType.OFF);
                     updateState(OUTPUT, new StringType(e.getMessage()));
                     return;
@@ -244,7 +249,7 @@ public class ExecHandler extends BaseThingHandler {
                     }
                     isr.close();
                 } catch (IOException e) {
-                    logger.error("An exception occurred while reading the stdout when executing '{}' : '{}'",
+                    logger.warn("An exception occurred while reading the stdout when executing '{}' : '{}'",
                             commandLine, e.getMessage());
                 }
 
@@ -257,7 +262,7 @@ public class ExecHandler extends BaseThingHandler {
                     }
                     isr.close();
                 } catch (IOException e) {
-                    logger.error("An exception occurred while reading the stderr when executing '{}' : '{}'",
+                    logger.warn("An exception occurred while reading the stderr when executing '{}' : '{}'",
                             commandLine, e.getMessage());
                 }
 
@@ -265,13 +270,13 @@ public class ExecHandler extends BaseThingHandler {
                 try {
                     exitVal = proc.waitFor(timeOut, TimeUnit.MILLISECONDS);
                 } catch (InterruptedException e) {
-                    logger.error("An exception occurred while waiting for the process ('{}') to finish : '{}'",
+                    logger.warn("An exception occurred while waiting for the process ('{}') to finish : '{}'",
                             commandLine, e.getMessage());
                 }
 
                 if (!exitVal) {
-                    logger.warn("Forcibly termininating the process ('{}') after a timeout of {} ms",
-                            commandLine, timeOut);
+                    logger.warn("Forcibly termininating the process ('{}') after a timeout of {} ms", commandLine,
+                            timeOut);
                     proc.destroyForcibly();
                 }
 
@@ -316,8 +321,8 @@ public class ExecHandler extends BaseThingHandler {
                         transformationType);
             }
         } catch (TransformationException te) {
-            logger.warn("An exception occurred while transforming '{}' with '{}' : '{}'",
-                    response, transformation, te.getMessage());
+            logger.warn("An exception occurred while transforming '{}' with '{}' : '{}'", response, transformation,
+                    te.getMessage());
 
             // in case of an error we return the response without any transformation
             transformedResponse = response;
@@ -363,29 +368,26 @@ public class ExecHandler extends BaseThingHandler {
     protected String[] createCmdArray(String[] shell, String cOption, String commandLine) {
         boolean startsWithShell = false;
         for (String sh : shell) {
-            if (commandLine.startsWith(sh+" ")) {
+            if (commandLine.startsWith(sh + " ")) {
                 startsWithShell = true;
                 break;
             }
         }
 
         if (!startsWithShell) {
-            return new String[]{shell[0], cOption, commandLine};
+            return new String[] { shell[0], cOption, commandLine };
         } else {
             logger.debug("Splitting by spaces");
             try {
                 return commandLine.split(" ");
             } catch (PatternSyntaxException e) {
-                logger.warn("An exception occurred while splitting '{}' : '{}'",
-                        commandLine, e.getMessage());
+                logger.warn("An exception occurred while splitting '{}' : '{}'", commandLine, e.getMessage());
                 updateState(RUN, OnOffType.OFF);
                 updateState(OUTPUT, new StringType(e.getMessage()));
                 return new String[] {};
             }
         }
     }
-
-
 
     /**
      * Contains information about which operating system openHAB is running on.
@@ -394,7 +396,12 @@ public class ExecHandler extends BaseThingHandler {
      * @author Constantin Piber (for Memin) - Initial contribution
      */
     public enum OS {
-        WINDOWS, LINUX, MAC, SOLARIS, UNKNOWN, NOT_SET
+        WINDOWS,
+        LINUX,
+        MAC,
+        SOLARIS,
+        UNKNOWN,
+        NOT_SET
     };
 
     private static OS os = OS.NOT_SET;
@@ -404,8 +411,7 @@ public class ExecHandler extends BaseThingHandler {
             String operSys = System.getProperty("os.name").toLowerCase();
             if (operSys.contains("win")) {
                 os = OS.WINDOWS;
-            } else if (operSys.contains("nix") || operSys.contains("nux")
-                    || operSys.contains("aix")) {
+            } else if (operSys.contains("nix") || operSys.contains("nux") || operSys.contains("aix")) {
                 os = OS.LINUX;
             } else if (operSys.contains("mac")) {
                 os = OS.MAC;
