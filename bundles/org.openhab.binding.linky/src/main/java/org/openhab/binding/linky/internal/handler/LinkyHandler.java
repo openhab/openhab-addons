5de726992fc9b993842472cd4066618dc02ad8b4
lolodomo <lg.hc@free.fr>
2020-02-01 13:29:07 +0100

    Remove usage of okhttp

--- bundles/org.openhab.binding.linky/src/main/java/org/openhab/binding/linky/internal/handler/LinkyHandler.java
+++ bundles/org.openhab.binding.linky/src/main/java/org/openhab/binding/linky/internal/handler/LinkyHandler.java
@@ -15,7 +15,9 @@ package org.openhab.binding.linky.internal.handler;
 import static org.openhab.binding.linky.internal.LinkyBindingConstants.*;
 import static org.openhab.binding.linky.internal.model.EnedisTimeScale.*;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -37,6 +39,7 @@ import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.RefreshType;
 import org.eclipse.smarthome.core.types.UnDefType;
+import org.eclipse.smarthome.io.net.http.HttpUtil;
 import org.openhab.binding.linky.internal.LinkyConfiguration;
 import org.openhab.binding.linky.internal.model.EnedisInfo;
 import org.openhab.binding.linky.internal.model.EnedisTimeScale;
@@ -44,12 +47,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
-
-import okhttp3.FormBody;
-import okhttp3.FormBody.Builder;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 /**
  * The {@link LinkyHandler} is responsible for handling commands, which are
@@ -62,14 +59,17 @@ import okhttp3.Response;
 public class LinkyHandler extends BaseThingHandler {
     private final Logger logger = LoggerFactory.getLogger(LinkyHandler.class);
     private static final String LOGIN_BASE_URI = "https://espace-client-connexion.enedis.fr/auth/UI/Login";
+    private static final String LOGIN_BODY_BUILDER = "encoded=true&gx_charset=UTF-8&SunQueryParamsString=%s&IDToken1=%s&IDToken2=%s";
     private static final String API_BASE_URI = "https://espace-client-particuliers.enedis.fr/group/espace-particuliers/suivi-de-consommation";
+    private static final String DATA_BODY_BUILDER = "p_p_id=lincspartdisplaycdc_WAR_lincspartcdcportlet&p_p_lifecycle=2&p_p_resource_id=%s&_lincspartdisplaycdc_WAR_lincspartcdcportlet_dateDebut=%s&_lincspartdisplaycdc_WAR_lincspartcdcportlet_dateFin=%s";
     private static final DateTimeFormatter ENEDIS_DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    private static final Builder LOGIN_BODY_BUILDER = new FormBody.Builder().add("encoded", "true")
-            .add("gx_charset", "UTF-8").add("SunQueryParamsString",
-                    Base64.getEncoder().encodeToString("realm=particuliers".getBytes(StandardCharsets.UTF_8)));
+    private static final int HTTP_DEFAULT_TIMEOUT_MS = (int) TimeUnit.SECONDS.toMillis(5);
+    // private static final Builder LOGIN_BODY_BUILDER = new FormBody.Builder().add("encoded", "true")
+    // .add("gx_charset", "UTF-8").add("SunQueryParamsString",
+    // Base64.getEncoder().encodeToString("realm=particuliers".getBytes(StandardCharsets.UTF_8)));
 
-    private final OkHttpClient client = new OkHttpClient.Builder().followRedirects(false)
-            .cookieJar(new LinkyCookieJar()).build();
+    // private final OkHttpClient client = new OkHttpClient.Builder().followRedirects(false)
+    // .cookieJar(new LinkyCookieJar()).build();
     private final Gson GSON = new Gson();
 
     private @NonNullByDefault({}) ScheduledFuture<?> refreshJob;
@@ -88,10 +88,18 @@ public class LinkyHandler extends BaseThingHandler {
         logger.debug("config username = '{}'", config.username);
 
         try {
-            Request requestLogin = new Request.Builder().url(LOGIN_BASE_URI)
-                    .post(LOGIN_BODY_BUILDER.add("IDToken1", config.username).add("IDToken2", config.password).build())
-                    .build();
-            client.newCall(requestLogin).execute().close();
+            // Request requestLogin = new Request.Builder().url(LOGIN_BASE_URI)
+            // .post(LOGIN_BODY_BUILDER.add("IDToken1", config.username).add("IDToken2", config.password).build())
+            // .build();
+            // client.newCall(requestLogin).execute().close();
+            String requestContent = String.format(LOGIN_BODY_BUILDER,
+                    Base64.getEncoder().encodeToString("realm=particuliers".getBytes(StandardCharsets.UTF_8)),
+                    config.username, config.password);
+            InputStream stream = new ByteArrayInputStream(requestContent.getBytes(StandardCharsets.UTF_8));
+            logger.debug("executeUrl POST {} requestContent {}", LOGIN_BASE_URI, requestContent);
+            HttpUtil.executeUrl("POST", LOGIN_BASE_URI, stream, "application/x-www-form-urlencoded",
+                    HTTP_DEFAULT_TIMEOUT_MS);
+            stream.close();
 
             // first call to test connection and to get needed cookies
             // getEnedisInfo(DAILY, LocalDate.now(), LocalDate.now());
@@ -191,21 +199,39 @@ public class LinkyHandler extends BaseThingHandler {
     private @Nullable EnedisInfo getEnedisInfo(EnedisTimeScale timeScale, LocalDate from, LocalDate to) {
         EnedisInfo result = null;
 
-        FormBody formBody = new FormBody.Builder().add("p_p_id", "lincspartdisplaycdc_WAR_lincspartcdcportlet")
-                .add("p_p_lifecycle", "2").add("p_p_resource_id", timeScale.getId())
-                .add("_lincspartdisplaycdc_WAR_lincspartcdcportlet_dateDebut", from.format(ENEDIS_DATE_FORMAT))
-                .add("_lincspartdisplaycdc_WAR_lincspartcdcportlet_dateFin", to.format(ENEDIS_DATE_FORMAT)).build();
-
-        Request requestData = new Request.Builder().url(API_BASE_URI).post(formBody).build();
-        try (Response response = client.newCall(requestData).execute()) {
-            if (response.body() != null) {
-                String body = response.body().string();
-                result = GSON.fromJson(body, EnedisInfo.class);
+        // FormBody formBody = new FormBody.Builder().add("p_p_id", "lincspartdisplaycdc_WAR_lincspartcdcportlet")
+        // .add("p_p_lifecycle", "2").add("p_p_resource_id", timeScale.getId())
+        // .add("_lincspartdisplaycdc_WAR_lincspartcdcportlet_dateDebut", from.format(ENEDIS_DATE_FORMAT))
+        // .add("_lincspartdisplaycdc_WAR_lincspartcdcportlet_dateFin", to.format(ENEDIS_DATE_FORMAT)).build();
+        //
+        // Request requestData = new Request.Builder().url(API_BASE_URI).post(formBody).build();
+        // try (Response response = client.newCall(requestData).execute()) {
+        // if (response.body() != null) {
+        // String body = response.body().string();
+        // result = GSON.fromJson(body, EnedisInfo.class);
+        // }
+        // response.close();
+        // } catch (IOException e) {
+        // logger.warn("Exception calling Enedis API : {} - {}", e.getClass().getCanonicalName(), e.getMessage());
+        // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.OFFLINE.COMMUNICATION_ERROR, e.getMessage());
+        // }
+        String requestContent = String.format(DATA_BODY_BUILDER, timeScale.getId(), from.format(ENEDIS_DATE_FORMAT),
+                to.format(ENEDIS_DATE_FORMAT));
+        InputStream stream = new ByteArrayInputStream(requestContent.getBytes(StandardCharsets.UTF_8));
+        logger.debug("executeUrl POST {} requestContent {}", API_BASE_URI, requestContent);
+        try {
+            String jsonResponse = HttpUtil.executeUrl("POST", API_BASE_URI, stream, "application/x-www-form-urlencoded",
+                    HTTP_DEFAULT_TIMEOUT_MS);
+            if (jsonResponse != null) {
+                result = GSON.fromJson(jsonResponse, EnedisInfo.class);
             }
-            response.close();
         } catch (IOException e) {
             logger.warn("Exception calling Enedis API : {} - {}", e.getClass().getCanonicalName(), e.getMessage());
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.OFFLINE.COMMUNICATION_ERROR, e.getMessage());
+        }
+        try {
+            stream.close();
+        } catch (IOException e) {
         }
         return result;
     }
