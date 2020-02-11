65efe77d55d2e1a9eb7e19b96c462be79432e925
Michael Wodniok <michi@noorganization.org>
2020-02-11 19:44:36 +0100

    [icalendar] Refactored long methods, added documentation for non overridden methods.
    Signed-off-by: Michael Wodniok <michi@noorganization.org>

--- bundles/org.openhab.binding.icalendar/src/main/java/org/openhab/binding/icalendar/internal/logic/BiweeklyPresentableCalendar.java
+++ bundles/org.openhab.binding.icalendar/src/main/java/org/openhab/binding/icalendar/internal/logic/BiweeklyPresentableCalendar.java
@@ -90,29 +90,7 @@ class BiweeklyPresentableCalendar extends AbstractPresentableCalendar {
 
         List<VEventWPeriod> positiveCandidates = new LinkedList<VEventWPeriod>();
         List<VEventWPeriod> negativeCandidates = new LinkedList<VEventWPeriod>();
-        for (VEvent currentEvent : usedCalendar.getEvents()) {
-            @Nullable
-            Status eventStatus = currentEvent.getStatus();
-            boolean additive = (eventStatus == null || (eventStatus.isTentative() || eventStatus.isConfirmed()));
-
-            DateIterator startDates = this.getRecurredEventDateIterator(currentEvent);
-            Duration duration = getEventLength(currentEvent);
-            if (duration == null) {
-                continue;
-            }
-            startDates.advanceTo(Date.from(instant));
-            while (startDates.hasNext()) {
-                Instant startInstant = startDates.next().toInstant();
-                Instant endInstant = startInstant.plus(duration);
-                if (startInstant.isAfter(instant)) {
-                    List<VEventWPeriod> candidateList = (additive ? positiveCandidates : negativeCandidates);
-                    candidateList.add(new VEventWPeriod(currentEvent, startInstant, endInstant));
-                }
-                if (startInstant.isAfter(recurrenceCalculationEnd)) {
-                    break;
-                }
-            }
-        }
+        fillNextEventCandidateLists(instant, positiveCandidates, negativeCandidates, recurrenceCalculationEnd);
         VEventWPeriod earliestNextEvent = null;
         for (VEventWPeriod positiveCandidate : positiveCandidates) {
             @Nullable
@@ -141,13 +119,88 @@ class BiweeklyPresentableCalendar extends AbstractPresentableCalendar {
         return earliestNextEvent.toEvent();
     }
 
+    /**
+     * Fills candidate lists for the next event after an Instant.
+     *
+     * @param instant The Instant to search after.
+     * @param positiveCandidates A target List of positive candidates.
+     * @param negativeCandidates A target List of negative candidates.
+     * @param recurrenceCalculationEnd The end point of recurrence calculation.
+     */
+    private void fillNextEventCandidateLists(Instant instant, List<VEventWPeriod> positiveCandidates,
+            List<VEventWPeriod> negativeCandidates, Instant recurrenceCalculationEnd) {
+        for (VEvent currentEvent : usedCalendar.getEvents()) {
+            @Nullable
+            Status eventStatus = currentEvent.getStatus();
+            boolean additive = (eventStatus == null || (eventStatus.isTentative() || eventStatus.isConfirmed()));
+
+            DateIterator startDates = this.getRecurredEventDateIterator(currentEvent);
+            Duration duration = getEventLength(currentEvent);
+            if (duration == null) {
+                continue;
+            }
+            startDates.advanceTo(Date.from(instant));
+            while (startDates.hasNext()) {
+                Instant startInstant = startDates.next().toInstant();
+                Instant endInstant = startInstant.plus(duration);
+                if (startInstant.isAfter(instant)) {
+                    List<VEventWPeriod> candidateList = (additive ? positiveCandidates : negativeCandidates);
+                    candidateList.add(new VEventWPeriod(currentEvent, startInstant, endInstant));
+                }
+                if (startInstant.isAfter(recurrenceCalculationEnd)) {
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Searches for a current event at given Instant.
+     *
+     * @param instant The Instant to use for finding events.
+     * @return A VEventWPeriod describing the event or null if there is none.
+     */
     private @Nullable VEventWPeriod getCurrentComponentWPeriod(Instant instant) {
         Instant recurrenceCalculationBegin = instant.minus(lookAround);
         Instant recurrenceCalculationEnd = instant.plus(lookAround);
-        // new DateTime(instant.plus(lookAround).getEpochSecond() * 1000));
-        // DateTime instantDate = new DateTime(instant.getEpochSecond() * 1000);
         List<VEventWPeriod> positiveCandidates = new LinkedList<VEventWPeriod>();
         List<VEventWPeriod> negativeCandidates = new LinkedList<VEventWPeriod>();
+        fillCurrentEventCandidateLists(instant, positiveCandidates, negativeCandidates, recurrenceCalculationBegin,
+                recurrenceCalculationEnd);
+
+        for (VEventWPeriod possibleCandidate : positiveCandidates) {
+            boolean cancel = false;
+            @Nullable
+            Uid candidateUid = possibleCandidate.vEvent.getUid();
+            if (candidateUid != null) {
+                for (VEventWPeriod possibleCancellation : negativeCandidates) {
+                    @Nullable
+                    Uid cancellationUid = possibleCancellation.vEvent.getUid();
+                    if (cancellationUid != null && candidateUid.getValue().contentEquals(cancellationUid.getValue())) {
+                        cancel = true;
+                        break;
+                    }
+                }
+            }
+            if (!cancel) {
+                return possibleCandidate;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Finds event candidates at given instant.
+     *
+     * @param instant The Instant to find event candidates around.
+     * @param positiveCandidates The List for the positive Candidates.
+     * @param negativeCandidates The List for the negative Candidates.
+     * @param recurrenceCalculationBegin The starting point in calculation.
+     * @param recurrenceCalculationEnd The end point in calculation.
+     */
+    private void fillCurrentEventCandidateLists(Instant instant, List<VEventWPeriod> positiveCandidates,
+            List<VEventWPeriod> negativeCandidates, Instant recurrenceCalculationBegin,
+            Instant recurrenceCalculationEnd) {
         for (VEvent currentEvent : usedCalendar.getEvents()) {
             @Nullable
             Status eventStatus = currentEvent.getStatus();
@@ -171,28 +224,14 @@ class BiweeklyPresentableCalendar extends AbstractPresentableCalendar {
                 }
             }
         }
-
-        for (VEventWPeriod possibleCandidate : positiveCandidates) {
-            boolean cancel = false;
-            @Nullable
-            Uid candidateUid = possibleCandidate.vEvent.getUid();
-            if (candidateUid != null) {
-                for (VEventWPeriod possibleCancellation : negativeCandidates) {
-                    @Nullable
-                    Uid cancellationUid = possibleCancellation.vEvent.getUid();
-                    if (cancellationUid != null && candidateUid.getValue().contentEquals(cancellationUid.getValue())) {
-                        cancel = true;
-                        break;
-                    }
-                }
-            }
-            if (!cancel) {
-                return possibleCandidate;
-            }
-        }
-        return null;
     }
 
+    /**
+     * Retrieves a DateIterator to iterate through the events occurrences.
+     *
+     * @param vEvent The VEvent to create the iterator for.
+     * @return The DateIterator for {@link VEvent}
+     */
     private DateIterator getRecurredEventDateIterator(VEvent vEvent) {
         TimezoneInfo tzinfo = this.usedCalendar.getTimezoneInfo();
 
@@ -207,6 +246,12 @@ class BiweeklyPresentableCalendar extends AbstractPresentableCalendar {
         return vEvent.getDateIterator(tz);
     }
 
+    /**
+     * Finds a duration of the event.
+     *
+     * @param vEvent The event to find out the duration.
+     * @return Either a Duration describing the events length or null, if no information is available.
+     */
     private static @Nullable Duration getEventLength(VEvent vEvent) {
         DurationProperty duration = vEvent.getDuration();
         if (duration != null) {
@@ -221,6 +266,11 @@ class BiweeklyPresentableCalendar extends AbstractPresentableCalendar {
         return Duration.between(start.getValue().toInstant(), end.getValue().toInstant());
     }
 
+    /**
+     * A Class describing an event together with a start and end instant.
+     *
+     * @author Michael Wodniok - Initial contribution.
+     */
     private static class VEventWPeriod {
         VEvent vEvent;
         Instant start;
@@ -252,12 +302,12 @@ class BiweeklyPresentableCalendar extends AbstractPresentableCalendar {
     }
 
     /**
-     * Return a list of events that have just begun within the time frame 
-     * 
-     * @param frameBegin   the start of the time frame
-     * @param frameEnd     the start of the time frame
-     * @return             list of iCalendar Events that BEGIN within the time frame 
-     *          
+     * Return a list of events that have just begun within the time frame
+     *
+     * @param frameBegin the start of the time frame
+     * @param frameEnd the start of the time frame
+     * @return list of iCalendar Events that BEGIN within the time frame
+     *
      */
     @Override
     public @Nullable List<Event> getJustBegunEvents(Instant frameBegin, Instant frameEnd) {
@@ -289,12 +339,12 @@ class BiweeklyPresentableCalendar extends AbstractPresentableCalendar {
     }
 
     /**
-     * Return a list of events that have just ended within the time frame 
-     * 
-     * @param frameBegin   the start of the time frame
-     * @param frameEnd     the start of the time frame
-     * @return             list of iCalendar Events that END within the time frame 
-     *          
+     * Return a list of events that have just ended within the time frame
+     *
+     * @param frameBegin the start of the time frame
+     * @param frameEnd the start of the time frame
+     * @return list of iCalendar Events that END within the time frame
+     *
      */
     @Override
     public @Nullable List<Event> getJustEndedEvents(Instant frameBegin, Instant frameEnd) {
