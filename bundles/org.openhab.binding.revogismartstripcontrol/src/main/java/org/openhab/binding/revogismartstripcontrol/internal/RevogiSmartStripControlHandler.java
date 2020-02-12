f3314509e11817adf5b67b229100354f1c9e95c1
Andreas Bräu <ab@andi95.de>
2020-02-12 21:43:09 +0100

    add ability to switch plugs
    Signed-off-by: Andreas Bräu <ab@andi95.de>

--- bundles/org.openhab.binding.revogismartstripcontrol/src/main/java/org/openhab/binding/revogismartstripcontrol/internal/RevogiSmartStripControlHandler.java
+++ bundles/org.openhab.binding.revogismartstripcontrol/src/main/java/org/openhab/binding/revogismartstripcontrol/internal/RevogiSmartStripControlHandler.java
@@ -22,7 +22,6 @@ import org.eclipse.smarthome.core.thing.ThingStatus;
 import org.eclipse.smarthome.core.thing.ThingStatusDetail;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.types.Command;
-import org.eclipse.smarthome.core.types.RefreshType;
 import org.openhab.binding.revogismartstripcontrol.internal.api.Status;
 import org.openhab.binding.revogismartstripcontrol.internal.api.StatusService;
 import org.openhab.binding.revogismartstripcontrol.internal.api.SwitchService;
@@ -34,6 +33,11 @@ import java.util.concurrent.TimeUnit;
 
 import static org.openhab.binding.revogismartstripcontrol.internal.RevogiSmartStripControlBindingConstants.ALL_PLUGS;
 import static org.openhab.binding.revogismartstripcontrol.internal.RevogiSmartStripControlBindingConstants.PLUG_1_SWITCH;
+import static org.openhab.binding.revogismartstripcontrol.internal.RevogiSmartStripControlBindingConstants.PLUG_2_SWITCH;
+import static org.openhab.binding.revogismartstripcontrol.internal.RevogiSmartStripControlBindingConstants.PLUG_3_SWITCH;
+import static org.openhab.binding.revogismartstripcontrol.internal.RevogiSmartStripControlBindingConstants.PLUG_4_SWITCH;
+import static org.openhab.binding.revogismartstripcontrol.internal.RevogiSmartStripControlBindingConstants.PLUG_5_SWITCH;
+import static org.openhab.binding.revogismartstripcontrol.internal.RevogiSmartStripControlBindingConstants.PLUG_6_SWITCH;
 
 /**
  * The {@link RevogiSmartStripControlHandler} is responsible for handling commands, which are
@@ -59,36 +63,49 @@ public class RevogiSmartStripControlHandler extends BaseThingHandler {
 
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
-        if (PLUG_1_SWITCH.equals(channelUID.getId())) {
-            if (command instanceof RefreshType) {
-                // TODO: handle data refresh
-            }
+        switch (channelUID.getId()) {
+            case PLUG_1_SWITCH:
+                switchPlug(command, 1);
+                break;
+            case PLUG_2_SWITCH:
+                switchPlug(command, 2);
+                break;
+            case PLUG_3_SWITCH:
+                switchPlug(command, 3);
+                break;
+            case PLUG_4_SWITCH:
+                switchPlug(command, 4);
+                break;
+            case PLUG_5_SWITCH:
+                switchPlug(command, 5);
+                break;
+            case PLUG_6_SWITCH:
+                switchPlug(command, 6);
+                break;
+            case ALL_PLUGS:
+                switchPlug(command, 0);
+                break;
+        }
+    }
 
-            // TODO: handle command
+    private void switchPlug(Command command, int port) {
+        if (command instanceof OnOffType) {
+            int state = convertOnOffTypeToState(command);
+            switchService.switchPort(config.getSerialNumber(), port, state);
+        }
+    }
 
-            // Note: if communication with thing fails for some reason,
-            // indicate that by setting the status with detail information:
-            // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
-            // "Could not control device at IP address x.x.x.x");
+    private int convertOnOffTypeToState(Command command) {
+        if (command == OnOffType.ON) {
+            return 1;
+        } else {
+            return 0;
         }
     }
 
     @Override
     public void initialize() {
-        // logger.debug("Start initializing!");
         config = getConfigAs(RevogiSmartStripControlConfiguration.class);
-
-        // TODO: Initialize the handler.
-        // The framework requires you to return from this method quickly. Also, before leaving this method a thing
-        // status from one of ONLINE, OFFLINE or UNKNOWN must be set. This might already be the real thing status in
-        // case you can decide it directly.
-        // In case you can not decide the thing status directly (e.g. for long running connection handshake using WAN
-        // access or similar) you should set status UNKNOWN here and then decide the real status asynchronously in the
-        // background.
-
-        // set the thing status to UNKNOWN temporarily and let the background task decide for the real status.
-        // the framework is then able to reuse the resources from the thing handler initialization.
-        // we set this upfront to reliably check status updates in unit tests.
         updateStatus(ThingStatus.UNKNOWN);
 
         // Example for background initialization:
@@ -100,12 +117,6 @@ public class RevogiSmartStripControlHandler extends BaseThingHandler {
         }
 
         logger.debug("Finished initializing!");
-
-        // Note: When initialization can NOT be done set the status with more details for further
-        // analysis. See also class ThingStatusDetail for all available status details.
-        // Add a description to give user information to understand why thing does not work as expected. E.g.
-        // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
-        // "Can not access device as username and/or password are invalid");
     }
 
     @Override
