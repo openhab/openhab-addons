8d7550e670dfd6bf32804c6e9ed7b7a8902c6027
Andreas Bräu <ab@andi95.de>
2020-02-12 19:43:18 +0100

    add author and licence
    Signed-off-by: Andreas Bräu <ab@andi95.de>

--- bundles/org.openhab.binding.revogismartstripcontrol/src/main/java/org/openhab/binding/revogismartstripcontrol/internal/api/SwitchResponse.java
+++ bundles/org.openhab.binding.revogismartstripcontrol/src/main/java/org/openhab/binding/revogismartstripcontrol/internal/api/SwitchResponse.java
@@ -1,9 +1,23 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.revogismartstripcontrol.internal.api;
 
 import java.util.Objects;
 
 /**
  * The class {@link SwitchResponse} describes the response when you switch a plug
+ *
+ * @author Andi Bräu - Initial contribution
  */
 public class SwitchResponse {
         private int response;
