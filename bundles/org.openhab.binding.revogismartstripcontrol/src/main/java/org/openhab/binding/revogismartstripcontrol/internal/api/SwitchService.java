74922edcde11f951aff085be46b46d00d9b8e591
Andreas Bräu <ab@andi95.de>
2020-02-12 19:43:18 +0100

    add author and licence
    Signed-off-by: Andreas Bräu <ab@andi95.de>

--- bundles/org.openhab.binding.revogismartstripcontrol/src/main/java/org/openhab/binding/revogismartstripcontrol/internal/api/SwitchService.java
+++ bundles/org.openhab.binding.revogismartstripcontrol/src/main/java/org/openhab/binding/revogismartstripcontrol/internal/api/SwitchService.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.revogismartstripcontrol.internal.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,6 +20,11 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * The {@link SwitchService} enables the binding to actually switch plugs on and of
+ *
+ * @author Andi Bräu - Initial contribution
+ */
 public class SwitchService {
 
     private static final String UDP_DISCOVERY_QUERY = "V3{\"sn\":\"%s\", \"cmd\": 20, \"port\": %d, \"state\": %d}";
--- bundles/org.openhab.binding.revogismartstripcontrol/src/test/java/org/openhab/binding/revogismartstripcontrol/internal/api/SwitchServiceTest.java
+++ bundles/org.openhab.binding.revogismartstripcontrol/src/test/java/org/openhab/binding/revogismartstripcontrol/internal/api/SwitchServiceTest.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.revogismartstripcontrol.internal.api;
 
 import org.junit.Test;
@@ -10,6 +22,9 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+/**
+ * @author Andi Bräu - Initial contribution
+ */
 public class SwitchServiceTest {
 
     private UdpSenderService udpSenderService = mock(UdpSenderService.class);
