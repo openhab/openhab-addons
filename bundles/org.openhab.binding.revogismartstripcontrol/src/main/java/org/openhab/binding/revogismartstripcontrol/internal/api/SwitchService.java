4c3ce5ac9dae442c1a06bc7d8a6516e1c9362d89
Andreas Bräu <ab@andi95.de>
2020-02-12 19:38:42 +0100

    inject udpSenderService
    Signed-off-by: Andreas Bräu <ab@andi95.de>

--- bundles/org.openhab.binding.revogismartstripcontrol/src/main/java/org/openhab/binding/revogismartstripcontrol/internal/api/SwitchService.java
+++ bundles/org.openhab.binding.revogismartstripcontrol/src/main/java/org/openhab/binding/revogismartstripcontrol/internal/api/SwitchService.java
@@ -1,7 +1,6 @@
 package org.openhab.binding.revogismartstripcontrol.internal.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.openhab.binding.revogismartstripcontrol.internal.udp.DatagramSocketWrapper;
 import org.openhab.binding.revogismartstripcontrol.internal.udp.UdpSenderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,10 +16,6 @@ public class SwitchService {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final UdpSenderService udpSenderService;
-
-    public SwitchService() {
-        udpSenderService = new UdpSenderService(new DatagramSocketWrapper());
-    }
 
     public SwitchService(UdpSenderService udpSenderService) {
         this.udpSenderService = udpSenderService;
