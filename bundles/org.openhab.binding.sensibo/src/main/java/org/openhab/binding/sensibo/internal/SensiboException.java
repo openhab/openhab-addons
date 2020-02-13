9d3069962fe29c4508359665774720ede97f553c
Arne Seime <arne.seime@gmail.com>
2020-02-13 07:46:01 +0100

    Fixed missing headers
    Signed-off-by: Arne Seime <arne.seime@gmail.com>

--- bundles/org.openhab.binding.sensibo/src/main/java/org/openhab/binding/sensibo/internal/SensiboException.java
+++ bundles/org.openhab.binding.sensibo/src/main/java/org/openhab/binding/sensibo/internal/SensiboException.java
@@ -1,7 +1,24 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.sensibo.internal;
 
 import org.eclipse.jdt.annotation.NonNull;
 
+/**
+ * The {@link SensiboException} class wraps exceptions raised when communicating with the API
+ *
+ * @author Arne Seime - Initial contribution
+ */
 public abstract class SensiboException extends Exception {
 
     /**
