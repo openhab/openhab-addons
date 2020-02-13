cbcfd574e2b66cb70aed829e1fceb55c393d4306
Andreas Bräu <ab@andi95.de>
2020-02-13 22:01:28 +0100

    complete discovery service
    Signed-off-by: Andreas Bräu <ab@andi95.de>

--- bundles/org.openhab.binding.revogismartstripcontrol/src/main/java/org/openhab/binding/revogismartstripcontrol/internal/RevogiSmartStripDiscoveryService.java
+++ bundles/org.openhab.binding.revogismartstripcontrol/src/main/java/org/openhab/binding/revogismartstripcontrol/internal/RevogiSmartStripDiscoveryService.java
@@ -12,35 +12,45 @@
  */
 package org.openhab.binding.revogismartstripcontrol.internal;
 
-import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.config.discovery.AbstractDiscoveryService;
+import org.eclipse.smarthome.config.discovery.DiscoveryResult;
+import org.eclipse.smarthome.config.discovery.DiscoveryResultBuilder;
+import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.ThingUID;
 import org.openhab.binding.revogismartstripcontrol.internal.api.DiscoveryResponse;
 import org.openhab.binding.revogismartstripcontrol.internal.api.DiscoveryService;
+import org.openhab.binding.revogismartstripcontrol.internal.udp.DatagramSocketWrapper;
 import org.openhab.binding.revogismartstripcontrol.internal.udp.UdpSenderService;
+import org.osgi.service.component.annotations.Component;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import static org.openhab.binding.revogismartstripcontrol.internal.RevogiSmartStripControlBindingConstants.SERIAL_NUMBER;
 import static org.openhab.binding.revogismartstripcontrol.internal.RevogiSmartStripControlBindingConstants.SMART_STRIP_THING_TYPE;
 
-@NonNullByDefault
+/**
+ * The {@link RevogiSmartStripDiscoveryService} helps to discover new smart strips
+ *
+ * @author Andi Bräu - Initial contribution
+ */
+@Component(service = org.eclipse.smarthome.config.discovery.DiscoveryService.class, immediate = true, configurationPid = "discovery.revogismartstripcontrol")
 public class RevogiSmartStripDiscoveryService extends AbstractDiscoveryService {
     public static final Set<ThingTypeUID> SUPPORTED_THING_TYPES = new HashSet<>(Collections.singleton(SMART_STRIP_THING_TYPE));
 
     private final DiscoveryService discoveryService;
-    private final RevogiSmartStripControlHandler revogiSmartStripControlHandler;
 
     private static final int SEARCH_TIME = 10;
 
-    public RevogiSmartStripDiscoveryService(UdpSenderService udpSenderService, RevogiSmartStripControlHandler revogiSmartStripControlHandler) throws IllegalArgumentException {
-        super(SEARCH_TIME);
-        discoveryService = new DiscoveryService(udpSenderService);
-        this.revogiSmartStripControlHandler = revogiSmartStripControlHandler;
+    public RevogiSmartStripDiscoveryService() {
+        super(SUPPORTED_THING_TYPES, SEARCH_TIME);
+        discoveryService = new DiscoveryService(new UdpSenderService(new DatagramSocketWrapper()));
     }
 
     @Override
@@ -53,16 +63,29 @@ public class RevogiSmartStripDiscoveryService extends AbstractDiscoveryService {
         List<DiscoveryResponse> discoveryResponses = discoveryService.discoverSmartStrips();
         discoveryResponses.forEach(response -> {
             ThingUID thingUID = getThingUID(response);
+                    if (thingUID != null) {
+                        Map<String, Object> properties = new HashMap<>();
+                        properties.put(Thing.PROPERTY_MODEL_ID, response.getRegId());
+                        properties.put(Thing.PROPERTY_MAC_ADDRESS, response.getMacAddress());
+                        properties.put(Thing.PROPERTY_FIRMWARE_VERSION, response.getVersion());
+                        properties.put(Thing.PROPERTY_SERIAL_NUMBER, response.getSerialNumber());
+                        DiscoveryResult discoveryResult = DiscoveryResultBuilder
+                                .create(thingUID)
+                                .withThingType(SMART_STRIP_THING_TYPE)
+                                .withProperties(properties)
+                                .withRepresentationProperty(SERIAL_NUMBER)
+                                .build();
+                        thingDiscovered(discoveryResult);
+                    }
 
                 }
         );
     }
 
     private @Nullable ThingUID getThingUID(DiscoveryResponse response) {
-        ThingTypeUID thingTypeUID = revogiSmartStripControlHandler.getThing().getThingTypeUID();
 
-        if (thingTypeUID != null && getSupportedThingTypes().contains(thingTypeUID)) {
-            return new ThingUID(thingTypeUID, response.getSerialNumber());
+        if (getSupportedThingTypes().contains(SMART_STRIP_THING_TYPE)) {
+            return new ThingUID(SMART_STRIP_THING_TYPE, response.getSerialNumber());
         } else {
             return null;
         }
