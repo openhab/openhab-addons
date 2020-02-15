7ae51ee597cd338071d234220357816ee699126a
Stefan Kaestle <stefan@mad-kow.de>
2020-02-15 13:41:11 +0100

    Attempt to get a new subscription ID when the old one is invalidated
    Signed-off-by: Stefan Kaestle <stefan@mad-kow.de>

---
+++ bundles/org.openhab.binding.boschshc/install.sh
@@ -0,0 +1,5 @@
+sudo cp target/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar /usr/share/openhab2/addons/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar
+sudo chown openhab:openhab /usr/share/openhab2/addons/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar
+
+md5sum target/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar
+md5sum /usr/share/openhab2/addons/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar
