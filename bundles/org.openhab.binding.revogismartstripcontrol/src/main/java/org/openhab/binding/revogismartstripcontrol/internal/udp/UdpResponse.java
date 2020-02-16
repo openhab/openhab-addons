a05199ab6cb65f13fa23407c44029b064295b702
Andreas Bräu <ab@andi95.de>
2020-02-16 16:26:27 +0100

    add licencing stuff
    Signed-off-by: Andreas Bräu <ab@andi95.de>

--- bundles/org.openhab.binding.revogismartstripcontrol/src/main/java/org/openhab/binding/revogismartstripcontrol/internal/udp/UdpResponse.java
+++ bundles/org.openhab.binding.revogismartstripcontrol/src/main/java/org/openhab/binding/revogismartstripcontrol/internal/udp/UdpResponse.java
@@ -1,7 +1,24 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.revogismartstripcontrol.internal.udp;
 
 import java.util.Objects;
 
+/**
+ * The class {@link UdpResponse} represents udp reponse we expect
+ *
+ * @author Andi Bräu - Initial contribution
+ */
 public class UdpResponse {
     private String answer;
     private String ipAddress;
