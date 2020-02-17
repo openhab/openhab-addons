e8a4c9c29f2b7ed220b7b99bfb8b02aff3e6ab44
stefan-hoehn <stefan.hoehn@aoe.com>
2020-02-17 20:22:35 +0100

    [nanoleaf] Fixed canvas color setting (identical to triangles) (#7020)
    * [nanoleaf] Fixed canvas color setting (identical to triangles)
    
    Signed-off-by: Stefan Höhn <stefan@andreaundstefanhoehn.de>

--- bundles/org.openhab.binding.nanoleaf/src/main/java/org/openhab/binding/nanoleaf/internal/handler/NanoleafPanelHandler.java
+++ bundles/org.openhab.binding.nanoleaf/src/main/java/org/openhab/binding/nanoleaf/internal/handler/NanoleafPanelHandler.java
@@ -163,7 +163,9 @@ public class NanoleafPanelHandler extends BaseThingHandler {
     }
 
     private void sendRenderedEffectCommand(Command command) throws NanoleafException {
+        logger.debug("Command Type: {}", command.getClass());
         HSBType currentPanelColor = getPanelColor();
+        logger.debug("currentPanelColor: {}", currentPanelColor.toString());
         if (currentPanelColor == null)
             return;
         HSBType newPanelColor = new HSBType();
@@ -220,9 +222,10 @@ public class NanoleafPanelHandler extends BaseThingHandler {
             if (handler != null) {
                 NanoleafControllerConfig config = ((NanoleafControllerHandler) handler).getControllerConfig();
                 // Light Panels and Canvas use different stream commands
-                if (config.deviceType.equals(CONFIG_DEVICE_TYPE_LIGHTPANELS)) {
+                if (config.deviceType.equals(CONFIG_DEVICE_TYPE_LIGHTPANELS) || config.deviceType.equals(CONFIG_DEVICE_TYPE_CANVAS)) {
                     write.setAnimData(String.format("1 %s 1 %d %d %d 0 10", panelID, red, green, blue));
                 } else {
+                    // this is only used in special streaming situations with canvas which is not yet supported
                     int quotient = Integer.divideUnsigned(Integer.valueOf(panelID), 256);
                     int remainder = Integer.remainderUnsigned(Integer.valueOf(panelID), 256);
                     write.setAnimData(
