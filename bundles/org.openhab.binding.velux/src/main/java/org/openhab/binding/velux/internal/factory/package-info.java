611d36483307d76772146c37fd950325cff4186c
Guenther Schreiner <gs4711@users.noreply.github.com>
2020-02-25 17:52:42 +0100

    [velux] Initial contribution (#2531)
    Initial contribution
    
    Signed-off-by: Guenther Schreiner <guenther.schreiner@smile.de>
    Signed-off-by: Samuel Brucksch <sasliga@freenet.de>

---
+++ bundles/org.openhab.binding.velux/src/main/java/org/openhab/binding/velux/internal/factory/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+/**
+ * Classes to provide the initiation of all handlers i.e. the factory for all services and things.
+ *
+ * @author Guenther Schreiner - Initial contribution
+ */
+package org.openhab.binding.velux.internal.factory;
