3ee7c8c520c4d376aed42c7cbfa1248fbcecd4ad
Stefan Kaestle <stefan@mad-kow.de>
2020-02-27 23:38:16 +0100

    Better install script
    Signed-off-by: Stefan Kaestle <stefan@mad-kow.de>

--- bundles/org.openhab.binding.boschshc/install.sh
+++ bundles/org.openhab.binding.boschshc/install.sh
@@ -1,5 +1,36 @@
-sudo cp target/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar /usr/share/openhab2/addons/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar
-sudo chown openhab:openhab /usr/share/openhab2/addons/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar
+#!/bin/bash
 
-md5sum target/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar
-md5sum /usr/share/openhab2/addons/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar
+if [[ $_ != $0 ]]
+then
+    echo "Script is being sourced, need to executed"
+
+else
+
+    set -e
+
+    mvn clean install -pl :org.openhab.binding.boschshc -DskipChecks -DskipTests
+
+    sudo cp target/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar /usr/share/openhab2/addons/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar
+    sudo chown openhab:openhab /usr/share/openhab2/addons/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar
+
+    md5sum target/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar
+    md5sum /usr/share/openhab2/addons/org.openhab.binding.boschshc-2.5.1-SNAPSHOT.jar
+
+    (
+	echo "Deleting old Bosch bundle from openhab"
+	openhab-cli console -p habopen "bundle:list" | grep -i bosch
+	openhab-cli console -p habopen "bundle:uninstall org.openhab.binding.boschshc"
+	
+	echo "After deleting it .. "
+	openhab-cli console -p habopen "bundle:list" | grep -i bosch
+    ) || true
+
+    echo "Stopping OpenHab"
+    sudo systemctl stop openhab2
+    echo "Starting Openhab"
+    sudo systemctl start openhab2
+
+    echo "Waiting 30 secs.."
+    sleep 60
+    grep 'Initializing' /var/log/openhab2/openhab.log
+fi
