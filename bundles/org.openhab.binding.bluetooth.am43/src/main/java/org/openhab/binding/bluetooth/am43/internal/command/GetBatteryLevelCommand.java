dffd267990fb58fa56ba600cbf1d6fa5747cf9d8
cpmeister <mistercpp2000@gmail.com>
2020-03-03 12:20:50 -0800

    [bluetooth.am43] AM43 Binding initial contribution (#6979)
    * Initial commit of AM43 Binding
    
    Signed-off-by: Connor Petty <cpmeister@users.noreply.github.com>

---
+++ bundles/org.openhab.binding.bluetooth.am43/src/main/java/org/openhab/binding/bluetooth/am43/internal/command/GetBatteryLevelCommand.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.bluetooth.am43.internal.command;
+
+import java.util.concurrent.Executor;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * The {@link GetBatteryLevelCommand} is used to get the current battery level of the AM43.
+ *
+ * @author Connor Petty - Initial contribution
+ */
+@NonNullByDefault
+public class GetBatteryLevelCommand extends AM43Command {
+
+    private static final byte COMMAND = (byte) 0xa2;
+
+    public GetBatteryLevelCommand() {
+        super(COMMAND, (byte) 1);
+    }
+
+    @Override
+    public boolean handleResponse(Executor executor, ResponseListener listener, byte @Nullable [] response) {
+        if (super.handleResponse(executor, listener, response)) {
+            executor.execute(() -> listener.receivedResponse(this));
+            return true;
+        }
+        return false;
+    }
+
+    public int getBatteryLevel() {
+        return getResponse()[7];
+    }
+
+    @Override
+    public int minResponseSize() {
+        return 9;
+    }
+}
