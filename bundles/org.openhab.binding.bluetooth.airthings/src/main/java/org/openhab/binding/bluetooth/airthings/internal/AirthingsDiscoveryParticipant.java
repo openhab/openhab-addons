b1ccc1a249a940debe6e77e4d3b5a138a8f55f8f
Pauli Anttila <pauli.anttila@gmail.com>
2020-03-13 18:19:29 +0200

    [bluetooth.airthing] Use connection based discovery
    Signed-off-by: Pauli Anttila <pauli.anttila@gmail.com>

--- bundles/org.openhab.binding.bluetooth.airthings/src/main/java/org/openhab/binding/bluetooth/airthings/internal/AirthingsDiscoveryParticipant.java
+++ bundles/org.openhab.binding.bluetooth.airthings/src/main/java/org/openhab/binding/bluetooth/airthings/internal/AirthingsDiscoveryParticipant.java
@@ -41,6 +41,8 @@ public class AirthingsDiscoveryParticipant implements BluetoothDiscoveryParticip
 
     private static final int AIRTHINGS_COMPANY_ID = 820; // Formerly Corentium AS
 
+    private static final String WAVE_PLUS_MODEL = "2930";
+
     @Override
     public Set<ThingTypeUID> getSupportedThingTypeUIDs() {
         return Collections.singleton(AirthingsBindingConstants.THING_TYPE_AIRTHINGS_WAVE_PLUS);
@@ -49,10 +51,11 @@ public class AirthingsDiscoveryParticipant implements BluetoothDiscoveryParticip
     @Override
     @Nullable
     public ThingUID getThingUID(BluetoothDevice device) {
-        Integer manufacturerId = device.getManufacturerId();
-        if (manufacturerId != null && manufacturerId == AIRTHINGS_COMPANY_ID) {
-            return new ThingUID(AirthingsBindingConstants.THING_TYPE_AIRTHINGS_WAVE_PLUS, device.getAdapter().getUID(),
-                    device.getAddress().toString().toLowerCase().replace(":", ""));
+        if (isAirthingsDevice(device)) {
+            if (WAVE_PLUS_MODEL.equals(device.getModel())) {
+                return new ThingUID(AirthingsBindingConstants.THING_TYPE_AIRTHINGS_WAVE_PLUS,
+                        device.getAdapter().getUID(), device.getAddress().toString().toLowerCase().replace(":", ""));
+            }
         }
         return null;
     }
@@ -60,22 +63,58 @@ public class AirthingsDiscoveryParticipant implements BluetoothDiscoveryParticip
     @Override
     @Nullable
     public DiscoveryResult createResult(BluetoothDevice device) {
+        if (!isAirthingsDevice(device)) {
+            return null;
+        }
         ThingUID thingUID = getThingUID(device);
         if (thingUID == null) {
             return null;
         }
-        String label = "Airthings Wave+";
+        if (WAVE_PLUS_MODEL.equals(device.getModel())) {
+            return createWavePlus(device, thingUID);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean requiresConnection(BluetoothDevice device) {
+        return isAirthingsDevice(device);
+    }
+
+    private boolean isAirthingsDevice(BluetoothDevice device) {
+        Integer manufacturerId = device.getManufacturerId();
+        if (manufacturerId != null && manufacturerId == AIRTHINGS_COMPANY_ID) {
+            return true;
+        }
+        return false;
+    }
+
+    private DiscoveryResult createWavePlus(BluetoothDevice device, ThingUID thingUID) {
         Map<String, Object> properties = new HashMap<>();
         properties.put(BluetoothBindingConstants.CONFIGURATION_ADDRESS, device.getAddress().toString());
         properties.put(Thing.PROPERTY_VENDOR, "Airthings AS");
-        Integer txPower = device.getTxPower();
-        if (txPower != null) {
-            properties.put(BluetoothBindingConstants.PROPERTY_TXPOWER, Integer.toString(txPower));
+        if (device.getSerialNumber() != null) {
+            properties.put(Thing.PROPERTY_SERIAL_NUMBER, device.getSerialNumber());
+        }
+        if (device.getFirmwareRevision() != null) {
+            properties.put(Thing.PROPERTY_FIRMWARE_VERSION, device.getFirmwareRevision());
+        }
+        if (device.getModel() != null) {
+            properties.put(Thing.PROPERTY_MODEL_ID, device.getModel());
+        }
+        if (device.getHardwareRevision() != null) {
+            properties.put(Thing.PROPERTY_HARDWARE_VERSION, device.getHardwareRevision());
+        }
+        if (device.getAddress() != null) {
+            properties.put(Thing.PROPERTY_MAC_ADDRESS, device.getAddress().toString());
+        }
+        if (device.getTxPower() != null) {
+            properties.put(BluetoothBindingConstants.PROPERTY_TXPOWER, Integer.toString(device.getTxPower()));
         }
 
         // Create the discovery result and add to the inbox
         return DiscoveryResultBuilder.create(thingUID).withProperties(properties)
                 .withRepresentationProperty(BluetoothBindingConstants.CONFIGURATION_ADDRESS)
-                .withBridge(device.getAdapter().getUID()).withLabel(label).build();
+                .withBridge(device.getAdapter().getUID()).withLabel("Airthings Wave+").build();
     }
 }
