b0be43c548896c87e60d613b52e6660812d493e8
Sergey Susikov <sergey.susikov@gmail.com>
2020-03-13 23:11:37 +0200

    [miio] Add support for device zhimi.humidifier.cb1 (Mi Air Humidifier) (#6976)
    * Add zhimi.humidifier.cb1 device support to miio binding
    
    Signed-off-by: Sergey Susikov <sergey.susikov@gmail.com>
    Co-authored-by: Alexander Skvortsov <skvalex@gmail.com>

--- bundles/org.openhab.binding.miio/src/main/java/org/openhab/binding/miio/internal/MiIoDevices.java
+++ bundles/org.openhab.binding.miio/src/main/java/org/openhab/binding/miio/internal/MiIoDevices.java
@@ -34,6 +34,7 @@ public enum MiIoDevices {
     AIRMONITOR_S1("cgllc.airmonitor.s1", "Mi Air Quality Monitor S1", THING_TYPE_BASIC),
     AIR_HUMIDIFIER_V1("zhimi.humidifier.v1", "Mi Air Humidifier", THING_TYPE_BASIC),
     AIR_HUMIDIFIER_CA1("zhimi.humidifier.ca1", "Mi Air Humidifier", THING_TYPE_BASIC),
+    AIR_HUMIDIFIER_CB1("zhimi.humidifier.cb1", "Mi Air Humidifier 2", THING_TYPE_BASIC),
     AIR_HUMIDIFIER_MJJSQ("deerma.humidifier.mjjsq", "Mija Smart humidifier", THING_TYPE_BASIC),
     AIR_PURIFIER1("zhimi.airpurifier.v1", "Mi Air Purifier v1", THING_TYPE_BASIC),
     AIR_PURIFIER2("zhimi.airpurifier.v2", "Mi Air Purifier v2", THING_TYPE_BASIC),
