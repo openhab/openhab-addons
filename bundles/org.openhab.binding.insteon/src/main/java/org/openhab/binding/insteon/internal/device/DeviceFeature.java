e848835fd1b728fd30be7024541421d28781a13c
Rob Nielsen <rob.nielsen@yahoo.com>
2020-03-15 15:14:55 -0500

    [insteon] initial commit of insteon binding
    Signed-off-by: Rob Nielsen <rob.nielsen@yahoo.com>

---
+++ bundles/org.openhab.binding.insteon/src/main/java/org/openhab/binding/insteon/internal/device/DeviceFeature.java
@@ -0,0 +1,429 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.insteon.internal.device;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.types.Command;
+import org.eclipse.smarthome.core.types.State;
+import org.openhab.binding.insteon.internal.config.InsteonChannelConfiguration;
+import org.openhab.binding.insteon.internal.device.DeviceFeatureListener.StateChangeType;
+import org.openhab.binding.insteon.internal.message.Msg;
+import org.openhab.binding.insteon.internal.utils.Utils.ParsingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A DeviceFeature represents a certain feature (trait) of a given Insteon device, e.g. something
+ * operating under a given InsteonAddress that can be manipulated (relay) or read (sensor).
+ *
+ * The DeviceFeature does the processing of incoming messages, and handles commands for the
+ * particular feature it represents.
+ *
+ * It uses four mechanisms for that:
+ *
+ * 1) MessageDispatcher: makes high level decisions about an incoming message and then runs the
+ * 2) MessageHandler: further processes the message, updates state etc
+ * 3) CommandHandler: translates commands from the openhab bus into an Insteon message.
+ * 4) PollHandler: creates an Insteon message to query the DeviceFeature
+ *
+ * Lastly, DeviceFeatureListeners can register with the DeviceFeature to get notifications when
+ * the state of a feature has changed. In practice, a DeviceFeatureListener corresponds to an
+ * openHAB item.
+ *
+ * The character of a DeviceFeature is thus given by a set of message and command handlers.
+ * A FeatureTemplate captures exactly that: it says what set of handlers make up a DeviceFeature.
+ *
+ * DeviceFeatures are added to a new device by referencing a FeatureTemplate (defined in device_features.xml)
+ * from the Device definition file (device_types.xml).
+ *
+ * @author Daniel Pfrommer - Initial contribution
+ * @author Bernd Pfrommer - openHAB 1 insteonplm binding
+ * @author Rob Nielsen - Port to openHAB 2 insteon binding
+ */
+@NonNullByDefault
+@SuppressWarnings("null")
+public class DeviceFeature {
+    public static enum QueryStatus {
+        NEVER_QUERIED,
+        QUERY_PENDING,
+        QUERY_ANSWERED
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(DeviceFeature.class);
+
+    private static HashMap<String, FeatureTemplate> features = new HashMap<String, FeatureTemplate>();
+
+    private InsteonDevice device = new InsteonDevice();
+    private String name = "INVALID_FEATURE_NAME";
+    private boolean isStatus = false;
+    private int directAckTimeout = 6000;
+    private QueryStatus queryStatus = QueryStatus.NEVER_QUERIED;
+
+    private @Nullable MessageHandler defaultMsgHandler = new MessageHandler.DefaultMsgHandler(this);
+    private @Nullable CommandHandler defaultCommandHandler = new CommandHandler.WarnCommandHandler(this);
+    private @Nullable PollHandler pollHandler = null;
+    private @Nullable MessageDispatcher dispatcher = null;
+
+    private HashMap<Integer, @Nullable MessageHandler> msgHandlers = new HashMap<>();
+    private HashMap<Class<? extends Command>, @Nullable CommandHandler> commandHandlers = new HashMap<>();
+    private ArrayList<DeviceFeatureListener> listeners = new ArrayList<>();
+    private ArrayList<DeviceFeature> connectedFeatures = new ArrayList<>();
+
+    /**
+     * Constructor
+     *
+     * @param device Insteon device to which this feature belongs
+     * @param name descriptive name for that feature
+     */
+    public DeviceFeature(InsteonDevice device, String name) {
+        this.name = name;
+        setDevice(device);
+    }
+
+    /**
+     * Constructor
+     *
+     * @param name descriptive name of the feature
+     */
+    public DeviceFeature(String name) {
+        this.name = name;
+    }
+
+    // various simple getters
+    public String getName() {
+        return name;
+    }
+
+    public synchronized QueryStatus getQueryStatus() {
+        return queryStatus;
+    }
+
+    public InsteonDevice getDevice() {
+        return device;
+    }
+
+    public boolean isFeatureGroup() {
+        return !connectedFeatures.isEmpty();
+    }
+
+    public boolean isStatusFeature() {
+        return isStatus;
+    }
+
+    public int getDirectAckTimeout() {
+        return directAckTimeout;
+    }
+
+    public @Nullable MessageHandler getDefaultMsgHandler() {
+        return defaultMsgHandler;
+    }
+
+    public HashMap<Integer, @Nullable MessageHandler> getMsgHandlers() {
+        return this.msgHandlers;
+    }
+
+    public ArrayList<DeviceFeature> getConnectedFeatures() {
+        return (connectedFeatures);
+    }
+
+    // various simple setters
+    public void setStatusFeature(boolean f) {
+        isStatus = f;
+    }
+
+    public void setPollHandler(@Nullable PollHandler h) {
+        pollHandler = h;
+    }
+
+    public void setDevice(InsteonDevice d) {
+        device = d;
+    }
+
+    public void setMessageDispatcher(@Nullable MessageDispatcher md) {
+        dispatcher = md;
+    }
+
+    public void setDefaultCommandHandler(@Nullable CommandHandler ch) {
+        defaultCommandHandler = ch;
+    }
+
+    public void setDefaultMsgHandler(@Nullable MessageHandler mh) {
+        defaultMsgHandler = mh;
+    }
+
+    public synchronized void setQueryStatus(QueryStatus status) {
+        logger.trace("{} set query status to: {}", name, status);
+        queryStatus = status;
+    }
+
+    public void setTimeout(@Nullable String s) {
+        if (s != null && !s.isEmpty()) {
+            try {
+                directAckTimeout = Integer.parseInt(s);
+                logger.trace("ack timeout set to {}", directAckTimeout);
+            } catch (NumberFormatException e) {
+                logger.warn("invalid number for timeout: {}", s);
+            }
+        }
+    }
+
+    /**
+     * Add a listener (item) to a device feature
+     *
+     * @param l the listener
+     */
+    public void addListener(DeviceFeatureListener l) {
+        synchronized (listeners) {
+            for (DeviceFeatureListener m : listeners) {
+                if (m.getItemName().equals(l.getItemName())) {
+                    return;
+                }
+            }
+            listeners.add(l);
+        }
+    }
+
+    /**
+     * Adds a connected feature such that this DeviceFeature can
+     * act as a feature group
+     *
+     * @param f the device feature related to this feature
+     */
+    public void addConnectedFeature(DeviceFeature f) {
+        connectedFeatures.add(f);
+    }
+
+    public boolean hasListeners() {
+        if (!listeners.isEmpty()) {
+            return true;
+        }
+        for (DeviceFeature f : connectedFeatures) {
+            if (f.hasListeners()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * removes a DeviceFeatureListener from this feature
+     *
+     * @param aItemName name of the item to remove as listener
+     * @return true if a listener was removed
+     */
+    public boolean removeListener(String aItemName) {
+        boolean listenerRemoved = false;
+        synchronized (listeners) {
+            for (Iterator<DeviceFeatureListener> it = listeners.iterator(); it.hasNext();) {
+                DeviceFeatureListener fl = it.next();
+                if (fl.getItemName().equals(aItemName)) {
+                    it.remove();
+                    listenerRemoved = true;
+                }
+            }
+        }
+        return listenerRemoved;
+    }
+
+    public boolean isReferencedByItem(String aItemName) {
+        synchronized (listeners) {
+            for (DeviceFeatureListener fl : listeners) {
+                if (fl.getItemName().equals(aItemName)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Called when message is incoming. Dispatches message according to message dispatcher
+     *
+     * @param msg The message to dispatch
+     * @param port the port from which the message came
+     * @return true if dispatch successful
+     */
+    public boolean handleMessage(Msg msg, String port) {
+        if (dispatcher == null) {
+            logger.warn("{} no dispatcher for msg {}", name, msg);
+            return false;
+        }
+        return (dispatcher.dispatch(msg, port));
+    }
+
+    /**
+     * Called when an openhab command arrives for this device feature
+     *
+     * @param c the binding config of the item which sends the command
+     * @param cmd the command to be exectued
+     */
+    public void handleCommand(InsteonChannelConfiguration c, Command cmd) {
+        Class<? extends Command> key = cmd.getClass();
+        CommandHandler h = commandHandlers.containsKey(key) ? commandHandlers.get(key) : defaultCommandHandler;
+        logger.trace("{} uses {} to handle command {} for {}", getName(), h.getClass().getSimpleName(),
+                key.getSimpleName(), getDevice().getAddress());
+        h.handleCommand(c, cmd, getDevice());
+    }
+
+    /**
+     * Make a poll message using the configured poll message handler
+     *
+     * @return the poll message
+     */
+    public @Nullable Msg makePollMsg() {
+        if (pollHandler == null) {
+            return null;
+        }
+        logger.trace("{} making poll msg for {} using handler {}", getName(), getDevice().getAddress(),
+                pollHandler.getClass().getSimpleName());
+        Msg m = pollHandler.makeMsg(device);
+        return m;
+    }
+
+    /**
+     * Publish new state to all device feature listeners, but give them
+     * additional dataKey and dataValue information so they can decide
+     * whether to publish the data to the bus.
+     *
+     * @param newState state to be published
+     * @param changeType what kind of changes to publish
+     * @param dataKey the key on which to filter
+     * @param dataValue the value that must be matched
+     */
+    public void publish(State newState, StateChangeType changeType, String dataKey, String dataValue) {
+        logger.debug("{}:{} publishing: {}", this.getDevice().getAddress(), getName(), newState);
+        synchronized (listeners) {
+            for (DeviceFeatureListener listener : listeners) {
+                listener.stateChanged(newState, changeType, dataKey, dataValue);
+            }
+        }
+    }
+
+    /**
+     * Publish new state to all device feature listeners
+     *
+     * @param newState state to be published
+     * @param changeType what kind of changes to publish
+     */
+    public void publish(State newState, StateChangeType changeType) {
+        logger.debug("{}:{} publishing: {}", this.getDevice().getAddress(), getName(), newState);
+        synchronized (listeners) {
+            for (DeviceFeatureListener listener : listeners) {
+                listener.stateChanged(newState, changeType);
+            }
+        }
+    }
+
+    /**
+     * Adds a message handler to this device feature.
+     *
+     * @param cm1 The insteon cmd1 of the incoming message for which the handler should be used
+     * @param handler the handler to invoke
+     */
+    public void addMessageHandler(int cm1, @Nullable MessageHandler handler) {
+        synchronized (msgHandlers) {
+            msgHandlers.put(cm1, handler);
+        }
+    }
+
+    /**
+     * Adds a command handler to this device feature
+     *
+     * @param c the command for which this handler is invoked
+     * @param handler the handler to call
+     */
+    public void addCommandHandler(Class<? extends Command> c, @Nullable CommandHandler handler) {
+        synchronized (commandHandlers) {
+            commandHandlers.put(c, handler);
+        }
+    }
+
+    /**
+     * Turn DeviceFeature into String
+     */
+    @Override
+    public String toString() {
+        return name + "(" + listeners.size() + ":" + commandHandlers.size() + ":" + msgHandlers.size() + ")";
+    }
+
+    /**
+     * Factory method for creating DeviceFeatures.
+     *
+     * @param s The name of the device feature to create.
+     * @return The newly created DeviceFeature, or null if requested DeviceFeature does not exist.
+     */
+    @Nullable
+    public static DeviceFeature makeDeviceFeature(String s) {
+        DeviceFeature f = null;
+        synchronized (features) {
+            if (features.containsKey(s)) {
+                f = features.get(s).build();
+            } else {
+                logger.warn("unimplemented feature requested: {}", s);
+            }
+        }
+        return f;
+    }
+
+    /**
+     * Reads the features templates from an input stream and puts them in global map
+     *
+     * @param input the input stream from which to read the feature templates
+     */
+    public static void readFeatureTemplates(InputStream input) {
+        try {
+            ArrayList<FeatureTemplate> featureTemplates = FeatureTemplateLoader.readTemplates(input);
+            synchronized (features) {
+                for (FeatureTemplate f : featureTemplates) {
+                    features.put(f.getName(), f);
+                }
+            }
+        } catch (IOException e) {
+            logger.warn("IOException while reading device features", e);
+        } catch (ParsingException e) {
+            logger.warn("Parsing exception while reading device features", e);
+        }
+    }
+
+    /**
+     * Reads the feature templates from a file and adds them to a global map
+     *
+     * @param file name of the file to read from
+     */
+    public static void readFeatureTemplates(String file) {
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            readFeatureTemplates(fis);
+        } catch (FileNotFoundException e) {
+            logger.warn("cannot read feature templates from file {} ", file, e);
+        }
+    }
+
+    /**
+     * static initializer
+     */
+    static {
+        // read features from xml file and store them in a map
+        InputStream input = DeviceFeature.class.getResourceAsStream("/device_features.xml");
+        readFeatureTemplates(input);
+    }
+}
---
+++ bundles/org.openhab.binding.insteon/src/main/java/org/openhab/binding/insteon/internal/device/FeatureTemplateLoader.java
@@ -0,0 +1,164 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.insteon.internal.device;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.library.types.IncreaseDecreaseType;
+import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.library.types.PercentType;
+import org.eclipse.smarthome.core.types.Command;
+import org.openhab.binding.insteon.internal.utils.Utils;
+import org.openhab.binding.insteon.internal.utils.Utils.ParsingException;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * Class that loads the device feature templates from an xml stream
+ *
+ * @author Daniel Pfrommer - Initial contribution
+ * @author Rob Nielsen - Port to openHAB 2 insteon binding
+ */
+@NonNullByDefault
+public class FeatureTemplateLoader {
+    public static ArrayList<FeatureTemplate> readTemplates(InputStream input) throws IOException, ParsingException {
+        ArrayList<FeatureTemplate> features = new ArrayList<>();
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            // Parse it!
+            Document doc = dBuilder.parse(input);
+            doc.getDocumentElement().normalize();
+
+            Element root = doc.getDocumentElement();
+
+            NodeList nodes = root.getChildNodes();
+
+            for (int i = 0; i < nodes.getLength(); i++) {
+                Node node = nodes.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element e = (Element) node;
+                    if (e.getTagName().equals("feature")) {
+                        features.add(parseFeature(e));
+                    }
+                }
+            }
+        } catch (SAXException e) {
+            throw new ParsingException("Failed to parse XML!", e);
+        } catch (ParserConfigurationException e) {
+            throw new ParsingException("Got parser config exception! ", e);
+        }
+        return features;
+    }
+
+    private static FeatureTemplate parseFeature(Element e) throws ParsingException {
+        String name = e.getAttribute("name");
+        boolean statusFeature = e.getAttribute("statusFeature").equals("true");
+        FeatureTemplate feature = new FeatureTemplate(name, statusFeature, e.getAttribute("timeout"));
+
+        NodeList nodes = e.getChildNodes();
+
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element child = (Element) node;
+                if (child.getTagName().equals("message-handler")) {
+                    parseMessageHandler(child, feature);
+                } else if (child.getTagName().equals("command-handler")) {
+                    parseCommandHandler(child, feature);
+                } else if (child.getTagName().equals("message-dispatcher")) {
+                    parseMessageDispatcher(child, feature);
+                } else if (child.getTagName().equals("poll-handler")) {
+                    parsePollHandler(child, feature);
+                }
+            }
+        }
+
+        return feature;
+    }
+
+    private static HandlerEntry makeHandlerEntry(Element e) throws ParsingException {
+        String handler = e.getTextContent();
+        if (handler == null) {
+            throw new ParsingException("Could not find Handler for: " + e.getTextContent());
+        }
+
+        NamedNodeMap attributes = e.getAttributes();
+        HashMap<String, @Nullable String> params = new HashMap<String, @Nullable String>();
+        for (int i = 0; i < attributes.getLength(); i++) {
+            Node n = attributes.item(i);
+            params.put(n.getNodeName(), n.getNodeValue());
+        }
+        return new HandlerEntry(handler, params);
+    }
+
+    private static void parseMessageHandler(Element e, FeatureTemplate f) throws DOMException, ParsingException {
+        HandlerEntry he = makeHandlerEntry(e);
+        if (e.getAttribute("default").equals("true")) {
+            f.setDefaultMessageHandler(he);
+        } else {
+            String attr = e.getAttribute("cmd");
+            int command = (attr == null) ? 0 : Utils.from0xHexString(attr);
+            f.addMessageHandler(command, he);
+        }
+    }
+
+    private static void parseCommandHandler(Element e, FeatureTemplate f) throws ParsingException {
+        HandlerEntry he = makeHandlerEntry(e);
+        if (e.getAttribute("default").equals("true")) {
+            f.setDefaultCommandHandler(he);
+        } else {
+            Class<? extends Command> command = parseCommandClass(e.getAttribute("command"));
+            f.addCommandHandler(command, he);
+        }
+    }
+
+    private static void parseMessageDispatcher(Element e, FeatureTemplate f) throws DOMException, ParsingException {
+        HandlerEntry he = makeHandlerEntry(e);
+        f.setMessageDispatcher(he);
+    }
+
+    private static void parsePollHandler(Element e, FeatureTemplate f) throws ParsingException {
+        HandlerEntry he = makeHandlerEntry(e);
+        f.setPollHandler(he);
+    }
+
+    private static Class<? extends Command> parseCommandClass(String c) throws ParsingException {
+        if (c.equals("OnOffType")) {
+            return OnOffType.class;
+        } else if (c.equals("PercentType")) {
+            return PercentType.class;
+        } else if (c.equals("DecimalType")) {
+            return DecimalType.class;
+        } else if (c.equals("IncreaseDecreaseType")) {
+            return IncreaseDecreaseType.class;
+        } else {
+            throw new ParsingException("Unknown Command Type");
+        }
+    }
+}
