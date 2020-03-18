6a65ca58e3c999031d1c1f03afe8345e658d5621
clinique <gael@lhopital.org>
2020-03-18 18:52:49 +0100

    Yet another one :-(
    Signed-off-by: clinique <gael@lhopital.org>

--- bundles/org.openhab.binding.sagercaster/src/main/java/org/openhab/binding/sagercaster/internal/handler/ExpiringMap.java
+++ bundles/org.openhab.binding.sagercaster/src/main/java/org/openhab/binding/sagercaster/internal/handler/ExpiringMap.java
@@ -1,9 +1,22 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.sagercaster.internal.handler;
 
 import java.util.Optional;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
 /**
  * Copyright (c) 2010-2020 Contributors to the openHAB project
  *
@@ -24,6 +37,7 @@ import org.eclipse.jdt.annotation.Nullable;
  *
  * @author Gaël L'hopital - Initial contribution
  */
+@NonNullByDefault
 class ExpiringMap<T> {
     private SortedMap<Long, T> values = new TreeMap<>();
     private @Nullable T agedValue;
