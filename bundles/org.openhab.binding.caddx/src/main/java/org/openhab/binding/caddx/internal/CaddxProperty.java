47cb2ae678400fe44775644c437c9b77514efb97
jossuar <georgios.moutsos@gmail.com>
2020-03-21 21:30:25 +0200

    Refactored CaddxProperty class to use an enum for the property type
    Signed-off-by: Georgios Moutsos <georgios.moutsos@gmail.com>

--- bundles/org.openhab.binding.caddx/src/main/java/org/openhab/binding/caddx/internal/CaddxProperty.java
+++ bundles/org.openhab.binding.caddx/src/main/java/org/openhab/binding/caddx/internal/CaddxProperty.java
@@ -25,7 +25,7 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
 public class CaddxProperty {
     // private
     private final String name;
-    private final String type; // 'Int', 'String', 'Bit'
+    private final CaddxPropertyType type; // 'Int', 'String', 'Bit'
     private final int byteFrom;
     private final int byteLength;
     private final int bitFrom;
@@ -34,8 +34,8 @@ public class CaddxProperty {
     private final String id;
 
     // Constructor
-    public CaddxProperty(String id, int byteFrom, int byteLength, int bitFrom, int bitLength, String type, String name,
-            boolean external) {
+    public CaddxProperty(String id, int byteFrom, int byteLength, int bitFrom, int bitLength, CaddxPropertyType type,
+            String name, boolean external) {
         this.id = id;
         this.name = name;
         this.type = type;
@@ -50,7 +50,7 @@ public class CaddxProperty {
         return name;
     }
 
-    public String getType() {
+    public CaddxPropertyType getType() {
         return type;
     }
 
@@ -66,69 +66,63 @@ public class CaddxProperty {
         int mask;
         int val;
 
-        if ("Int".equals(type)) {
-            if (bitFrom == 0 && bitLength == 0) {
-                mask = 255;
-                val = message[byteFrom - 1] & mask;
-            } else {
-                mask = ((1 << ((bitLength - bitFrom))) - 1) << bitFrom;
-                val = (message[byteFrom - 1] & mask) >> bitFrom;
-            }
+        switch (type) {
+            case INT:
+                if (bitFrom == 0 && bitLength == 0) {
+                    mask = 255;
+                    val = message[byteFrom - 1] & mask;
+                } else {
+                    mask = ((1 << ((bitLength - bitFrom))) - 1) << bitFrom;
+                    val = (message[byteFrom - 1] & mask) >> bitFrom;
+                }
 
-            return Integer.toString(val);
+                return Integer.toString(val);
+            case STRING:
+                byte[] str = Arrays.copyOfRange(message, byteFrom - 1, byteFrom + byteLength);
+                return new String(str);
+            case BIT:
+                return (((message[byteFrom - 1] & (1 << bitFrom)) > 0) ? "true" : "false");
+            default:
+                throw new IllegalArgumentException("type is unknown.");
         }
-
-        if ("String".equals(type)) {
-            byte[] str = Arrays.copyOfRange(message, byteFrom - 1, byteFrom + byteLength);
-            return new String(str);
-        }
-
-        if ("Bit".equals(type)) {
-            return (((message[byteFrom - 1] & (1 << bitFrom)) > 0) ? "true" : "false");
-        }
-
-        throw new IllegalArgumentException("type [" + type + "] is unknown.");
     }
 
     public String toString(byte[] message) {
         int mask;
         int val;
 
-        if ("Int".equals(type)) {
-            if (bitFrom == 0 && bitLength == 0) {
-                mask = 255;
-                val = message[byteFrom - 1];
-            } else {
-                mask = ((1 << ((bitLength - bitFrom) + 1)) - 1) << bitFrom;
-                val = (message[byteFrom - 1] & mask) >> bitFrom;
-            }
+        switch (type) {
+            case INT:
+                if (bitFrom == 0 && bitLength == 0) {
+                    mask = 255;
+                    val = message[byteFrom - 1];
+                } else {
+                    mask = ((1 << ((bitLength - bitFrom) + 1)) - 1) << bitFrom;
+                    val = (message[byteFrom - 1] & mask) >> bitFrom;
+                }
 
-            return name + ": " + String.format("%2s", Integer.toHexString(val)) + " - " + Integer.toString(val) + " - "
-                    + ((val >= 32 && val <= 'z') ? ((char) val) : "-");
+                return name + ": " + String.format("%2s", Integer.toHexString(val)) + " - " + Integer.toString(val)
+                        + " - " + ((val >= 32 && val <= 'z') ? ((char) val) : "-");
+            case STRING:
+                StringBuilder sb = new StringBuilder();
+
+                byte[] a = Arrays.copyOfRange(message, byteFrom - 1, byteFrom + byteLength);
+                sb.append(name);
+                sb.append(": ");
+                sb.append(new String(a));
+                sb.append("\r\n\r\n");
+                for (int i = 0; i < byteLength; i++) {
+                    sb.append(String.format("%2s", Integer.toHexString(message[byteFrom - 1 + i])));
+                    sb.append(" - ");
+                    sb.append((char) message[byteFrom - 1 + i]);
+                    sb.append("\r\n");
+                }
+
+                return sb.toString();
+            case BIT:
+                return name + ": " + (((message[byteFrom - 1] & (1 << bitFrom)) > 0) ? "true" : "false");
+            default:
+                return "Unknown type: " + type.toString();
         }
-
-        if ("String".contentEquals(type)) {
-            StringBuilder sb = new StringBuilder();
-
-            byte[] a = Arrays.copyOfRange(message, byteFrom - 1, byteFrom + byteLength);
-            sb.append(name);
-            sb.append(": ");
-            sb.append(new String(a));
-            sb.append("\r\n\r\n");
-            for (int i = 0; i < byteLength; i++) {
-                sb.append(String.format("%2s", Integer.toHexString(message[byteFrom - 1 + i])));
-                sb.append(" - ");
-                sb.append((char) message[byteFrom - 1 + i]);
-                sb.append("\r\n");
-            }
-
-            return sb.toString();
-        }
-
-        if ("Bit".contentEquals(type)) {
-            return name + ": " + (((message[byteFrom - 1] & (1 << bitFrom)) > 0) ? "true" : "false");
-        }
-
-        return "Unknown type: " + type;
     }
 }
