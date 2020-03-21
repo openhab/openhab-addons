67d2343f7f0a36fff05ccfd679ba058fa0db94ab
Kamil Subzda <kamil.subzda@nexwell.eu>
2020-03-21 15:03:45 +0100

    [fox] auto-generated code
    Signed-off-by: Kamil Subzda <kamil.subzda@nexwell.eu>

---
+++ bundles/org.openhab.binding.fox/src/main/java/org/openhab/binding/fox/internal/FoxBindingConstants.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.fox.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+/**
+ * The {@link FoxBindingConstants} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Kamil Subzda - Initial contribution
+ */
+@NonNullByDefault
+public class FoxBindingConstants {
+
+    private static final String BINDING_ID = "fox";
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID THING_TYPE_SAMPLE = new ThingTypeUID(BINDING_ID, "sample");
+
+    // List of all Channel ids
+    public static final String CHANNEL_1 = "channel1";
+}
