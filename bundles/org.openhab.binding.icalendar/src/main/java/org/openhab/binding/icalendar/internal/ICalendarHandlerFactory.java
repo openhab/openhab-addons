716a1678b7eae404b2c7831301879c0e5b3fc0fe
Michael Wodniok <michi@noorganization.org>
2020-03-24 09:41:10 +0100

    [icalendar] Retrieved ServiceReference in using Annotation
    Signed-off-by: Michael Wodniok <michi@noorganization.org>

--- bundles/org.openhab.binding.icalendar/src/main/java/org/openhab/binding/icalendar/internal/ICalendarHandlerFactory.java
+++ bundles/org.openhab.binding.icalendar/src/main/java/org/openhab/binding/icalendar/internal/ICalendarHandlerFactory.java
@@ -28,9 +28,7 @@ import org.eclipse.smarthome.core.thing.binding.ThingHandler;
 import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
 import org.eclipse.smarthome.io.net.http.HttpClientFactory;
 import org.openhab.binding.icalendar.internal.handler.ICalendarHandler;
-import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
-import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
@@ -51,6 +49,7 @@ public class ICalendarHandlerFactory extends BaseThingHandlerFactory {
     private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_CALENDAR);
 
     private final Logger logger = LoggerFactory.getLogger(ICalendarHandlerFactory.class);
+    private @Nullable @Reference HttpClientFactory httpClientFactory;
     private @Nullable HttpClient sharedHttpClient = null;
     private @Nullable EventPublisher eventPublisher = null;
 
@@ -96,16 +95,14 @@ public class ICalendarHandlerFactory extends BaseThingHandlerFactory {
         if (sharedHttpClient != null) {
             return;
         }
-        BundleContext currentContext = getBundleContext();
 
-        ServiceReference<HttpClientFactory> hcfReference = currentContext.getServiceReference(HttpClientFactory.class);
-        if (hcfReference == null) {
+        HttpClientFactory currentHCF = httpClientFactory;
+        if (currentHCF == null) {
             throw new BundleException(
-                    "Service Reference for HttpClientFactory is missing. This binding will not work without a valid HttpClient.");
+                    "Reference for HttpClientFactory is missing. This binding will not work without a valid HttpClient.");
         }
 
-        HttpClientFactory clientFactory = currentContext.getService(hcfReference);
-        sharedHttpClient = clientFactory.createHttpClient(BINDING_ID);
+        sharedHttpClient = currentHCF.createHttpClient(BINDING_ID);
     }
 
     /**
