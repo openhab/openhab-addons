923d5844167875b77195b2435e9a0611ec869bc8
theiding <mark.theiding@gmail.com>
2020-03-27 06:29:23 -0700

    [adorne] Adorne Binding initial contribution (#6553)
    * Initial implementation of adorne binding
    
    Signed-off-by: Mark Theiding <mark.theiding@gmail.com>
    Signed-off-by: CSchlipp <christian@schlipp.de>

---
+++ bundles/org.openhab.binding.adorne/src/main/java/org/openhab/binding/adorne/internal/handler/AdorneDimmerHandler.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.adorne.internal.handler;
+
+import static org.openhab.binding.adorne.internal.AdorneBindingConstants.CHANNEL_BRIGHTNESS;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.library.types.PercentType;
+import org.eclipse.smarthome.core.thing.ChannelUID;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.types.Command;
+import org.eclipse.smarthome.core.types.RefreshType;
+import org.openhab.binding.adorne.internal.hub.AdorneHubController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link AdorneDimmerHandler} is responsible for handling commands, which are
+ * sent to one of the channels. It supports the brightness channel in addition to the inherited switch channel.
+ *
+ * @author Mark Theiding - Initial contribution
+ */
+@NonNullByDefault
+public class AdorneDimmerHandler extends AdorneSwitchHandler {
+    private final Logger logger = LoggerFactory.getLogger(AdorneDimmerHandler.class);
+
+    public AdorneDimmerHandler(Thing thing) {
+        super(thing);
+    }
+
+    /**
+     * Handles refresh and percent commands for channel
+     * {@link org.openhab.binding.adorne.internal.AdorneBindingConstants#CHANNEL_BRIGHTNESS}
+     * It delegates all other commands to its parent class.
+     */
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        logger.trace("handleCommand (channelUID:{} command:{}", channelUID, command);
+        try {
+            if (channelUID.getId().equals(CHANNEL_BRIGHTNESS)) {
+                if (command instanceof RefreshType) {
+                    refreshBrightness();
+                } else if (command instanceof PercentType) {
+                    // Change the brightness through the hub controller
+                    AdorneHubController adorneHubController = getAdorneHubController();
+                    int level = ((PercentType) command).intValue();
+                    if (level >= 1 && level <= 100) { // Ignore commands outside of the supported 1-100 range
+                        adorneHubController.setBrightness(zoneId, level);
+                    } else {
+                        logger.debug("Ignored command to set brightness to level {}", level);
+                    }
+                }
+            } else {
+                super.handleCommand(channelUID, command); // Parent can handle everything else
+            }
+        } catch (IllegalStateException e) {
+            // Hub controller could't handle our commands. Unfortunately the framework has no mechanism to report
+            // runtime errors. If we throw the exception up the framework logs it as an error - we don't want that - we
+            // want the framework to handle it gracefully. No point to update the thing status, since the
+            // AdorneHubController already does that. So we are forced to swallow the exception here.
+            logger.debug("Failed to execute command {} for channel {} for thing {} ({})", command, channelUID,
+                    getThing().getLabel(), e.getMessage());
+        }
+    }
+
+    /**
+     * Refreshes the brightness of our thing to the actual state of the device.
+     *
+     */
+    public void refreshBrightness() {
+        // Asynchronously get our brightness from the hub controller and update our state accordingly
+        AdorneHubController adorneHubController = getAdorneHubController();
+        adorneHubController.getState(zoneId).thenAccept(state -> {
+            updateState(CHANNEL_BRIGHTNESS, new PercentType(state.brightness));
+            logger.debug("Refreshed dimmer {} with brightness {}", getThing().getLabel(), state.brightness);
+        });
+    }
+
+    /**
+     * Refreshes all supported channels.
+     *
+     */
+    @Override
+    public void refresh() {
+        super.refresh();
+        refreshBrightness();
+    }
+
+}
---
+++ bundles/org.openhab.binding.adorne/src/main/java/org/openhab/binding/adorne/internal/handler/AdorneHubHandler.java
@@ -0,0 +1,135 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.adorne.internal.handler;
+
+import static org.openhab.binding.adorne.internal.AdorneBindingConstants.*;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.library.types.PercentType;
+import org.eclipse.smarthome.core.thing.Bridge;
+import org.eclipse.smarthome.core.thing.ChannelUID;
+import org.eclipse.smarthome.core.thing.ThingStatus;
+import org.eclipse.smarthome.core.thing.ThingStatusDetail;
+import org.eclipse.smarthome.core.thing.binding.BaseBridgeHandler;
+import org.eclipse.smarthome.core.types.Command;
+import org.openhab.binding.adorne.internal.configuration.AdorneHubConfiguration;
+import org.openhab.binding.adorne.internal.hub.AdorneHubChangeNotify;
+import org.openhab.binding.adorne.internal.hub.AdorneHubController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link AdorneHubHandler} manages the state and status of the Adorne Hub's devices.
+ *
+ * @author Mark Theiding - Initial contribution
+ */
+@NonNullByDefault
+public class AdorneHubHandler extends BaseBridgeHandler implements AdorneHubChangeNotify {
+    private final Logger logger = LoggerFactory.getLogger(AdorneHubHandler.class);
+    private @Nullable AdorneHubController adorneHubController = null;
+
+    public AdorneHubHandler(Bridge bridge) {
+        super(bridge);
+    }
+
+    /**
+     * The {@link AdorneHubHandler} does not support any commands itself. This method is a NOOP and only provided since
+     * its implementation is required.
+     *
+     */
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        // Unfortunately BaseBridgeHandler doesn't provide a default implementation of handleCommand. However, hub
+        // commands could be added as a future enhancement e.g. to support hub firmware upgrades.
+    }
+
+    /**
+     * Establishes the hub controller for communication with the hub.
+     */
+    @Override
+    public void initialize() {
+        logger.debug("Initializing hub {}", getThing().getLabel());
+
+        updateStatus(ThingStatus.UNKNOWN);
+        AdorneHubConfiguration config = getConfigAs(AdorneHubConfiguration.class);
+        logger.debug("Configuration host:{} port:{}", config.host, config.port);
+
+        AdorneHubController adorneHubController = new AdorneHubController(config, scheduler, this);
+        this.adorneHubController = adorneHubController;
+        // Kick off the hub controller that handles all interactions with the hub for us
+        adorneHubController.start();
+    }
+
+    /**
+     * Disposes resources by stopping the hub controller.
+     */
+    @Override
+    public void dispose() {
+        AdorneHubController adorneHubController = this.adorneHubController;
+        if (adorneHubController != null) {
+            adorneHubController.stop();
+        }
+    }
+
+    /**
+     * Returns the hub controller. Returns <code>null</code> if hub controller has not been created yet.
+     *
+     * @return hub controller
+     */
+    public @Nullable AdorneHubController getAdorneHubController() {
+        return adorneHubController;
+    }
+
+    /**
+     * The {@link AdorneHubHandler} is notified that the state of one of its physical devices has changed. The
+     * {@link AdorneHubHandler} then asks the appropriate thing handler to update the thing to match the new state.
+     *
+     */
+    @Override
+    public void stateChangeNotify(int zoneId, boolean onOff, int brightness) {
+        logger.debug("State changed (zoneId:{} onOff:{} brightness:{})", zoneId, onOff, brightness);
+        getThing().getThings().forEach(thing -> {
+            AdorneSwitchHandler thingHandler = (AdorneSwitchHandler) thing.getHandler();
+            if (thingHandler != null && thingHandler.getZoneId() == zoneId) {
+                thingHandler.updateState(CHANNEL_POWER, OnOffType.from(onOff));
+                if (thing.getThingTypeUID().equals(THING_TYPE_DIMMER)) {
+                    thingHandler.updateState(CHANNEL_BRIGHTNESS, new PercentType(brightness));
+                }
+            }
+        });
+    }
+
+    /**
+     * The {@link AdorneHubHandler} is notified that its connectivity has changed.
+     *
+     */
+    @Override
+    public void connectionChangeNotify(boolean connected) {
+        logger.debug("Status changed (connected:{})", connected);
+
+        if (connected) {
+            // Refresh all of our things in case thing states changed while we were disconnected
+            getThing().getThings().forEach(thing -> {
+                AdorneSwitchHandler thingHandler = (AdorneSwitchHandler) thing.getHandler();
+                if (thingHandler != null) {
+                    thingHandler.refresh();
+                }
+            });
+            updateStatus(ThingStatus.ONLINE);
+        } else {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR);
+        }
+    }
+}
