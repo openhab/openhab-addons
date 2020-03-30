08ff999e9532a12c3ee9486d249ccd67c7498843
vbier <volker.bier@web.de>
2020-03-30 21:14:58 +0200

    [jeelink] fix TCP init cmds (#7246)
    * fix init cmds for tcp connected devices.
    * address review comments
    
    Signed-off-by: Volker Bier <volker.bier@web.de>
    Signed-off-by: CSchlipp <christian@schlipp.de>

--- bundles/org.openhab.binding.jeelink/src/main/java/org/openhab/binding/jeelink/internal/connection/AbstractJeeLinkConnection.java
+++ bundles/org.openhab.binding.jeelink/src/main/java/org/openhab/binding/jeelink/internal/connection/AbstractJeeLinkConnection.java
@@ -37,7 +37,6 @@ public abstract class AbstractJeeLinkConnection implements JeeLinkConnection {
 
     protected String port;
 
-    private String[] initCommands;
     private AtomicBoolean initialized = new AtomicBoolean(false);
 
     public AbstractJeeLinkConnection(String port, ConnectionListener listener) {
@@ -76,10 +75,11 @@ public abstract class AbstractJeeLinkConnection implements JeeLinkConnection {
 
     @Override
     public void sendCommands(String commands) {
-        if (commands != null && !commands.trim().isEmpty()) {
-            initCommands = commands.split(";");
-
-            try (OutputStream initStream = getInitStream()) {
+        try {
+            if (commands != null && !commands.trim().isEmpty()) {
+                // do not create in try-with-resources as this will
+                // close the undelying socket for TCP connections
+                OutputStream initStream = getInitStream();
                 if (initStream == null) {
                     throw new IOException(
                             "Connection on port " + port + " did not provide an init stream for writing init commands");
@@ -88,17 +88,17 @@ public abstract class AbstractJeeLinkConnection implements JeeLinkConnection {
                 // do not close the writer as this closes the underlying stream, and
                 // in case of tcp connections, the underlying socket
                 OutputStreamWriter w = new OutputStreamWriter(initStream);
-                for (String cmd : initCommands) {
+                for (String cmd : commands.split(";")) {
                     logger.debug("Writing to device on port {}: {} ", port, cmd);
-
+                    
                     w.write(cmd + "\n");
                 }
                 w.flush();
-            } catch (IOException ex) {
-                logger.debug("Error writing to output stream!", ex);
-                closeConnection();
-                notifyAbort("propagate: " + ex.getMessage());
             }
+        } catch (IOException ex) {
+            logger.debug("Error writing to output stream!", ex);
+            closeConnection();
+            notifyAbort("propagate: " + ex.getMessage());
         }
     }
 
