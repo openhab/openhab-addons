4dab5a56c914c1a4103f8cf7ab9626f0e28e711a
Arne Seime <seime@users.noreply.github.com>
2020-03-30 22:58:56 +0200

    [sensibo] Sensibo Binding initial contribution (#5576)
    Signed-off-by: Arne Seime <arne.seime@gmail.com>

---
+++ bundles/org.openhab.binding.sensibo/src/test/java/org/openhab/binding/sensibo/internal/dto/SetAcStatePropertyRequestTest.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.sensibo.internal.dto;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.openhab.binding.sensibo.internal.dto.setacstateproperty.SetAcStatePropertyRequest;
+
+/**
+ * @author Arne Seime - Initial contribution
+ */
+public class SetAcStatePropertyRequestTest extends AbstractSerializationDeserializationTest {
+
+    @Test
+    public void testSerializeDeserialize() throws IOException {
+        SetAcStatePropertyRequest req = new SetAcStatePropertyRequest("PODID", "targetTemperature", "mode");
+        String serializedJson = wireHelper.serialize(req);
+
+        final SetAcStatePropertyRequest deSerializedRequest = wireHelper.deSerializeFromString(serializedJson,
+                SetAcStatePropertyRequest.class);
+
+        assertEquals("mode", deSerializedRequest.newValue);
+    }
+}
