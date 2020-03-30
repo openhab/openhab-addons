7acdd92fa32455e329c27e31b8088fbf31714c00
Arne Seime <seime@users.noreply.github.com>
2020-03-30 22:58:56 +0200

    [sensibo] Sensibo Binding initial contribution (#5576)
    Signed-off-by: Arne Seime <arne.seime@gmail.com>
    Signed-off-by: Eugen Freiter <freiter@gmx.de>

---
+++ bundles/org.openhab.binding.sensibo/src/main/java/org/openhab/binding/sensibo/internal/dto/poddetails/ModeCapabilityDTO.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.sensibo.internal.dto.poddetails;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * All classes in the ..binding.sensibo.dto are data transfer classes used by the GSON mapper. This class reflects a
+ * part of a request/response data structure.
+ *
+ * @author Arne Seime - Initial contribution.
+ */
+public class ModeCapabilityDTO {
+    @SerializedName("swing")
+    public List<String> swingModes = new ArrayList<>();
+    public Map<String, TemperatureDTO> temperatures = new HashMap<>();
+    public List<String> fanLevels = new ArrayList<>();
+}
---
+++ bundles/org.openhab.binding.sensibo/src/main/java/org/openhab/binding/sensibo/internal/model/Pod.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.sensibo.internal.model;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Represents a generic Sensibo controllable thing
+ *
+ * @author Arne Seime - Initial contribution
+ */
+@NonNullByDefault
+public abstract class Pod {
+    protected String id;
+
+    public String getId() {
+        return id;
+    }
+
+    protected Pod(String id) {
+        this.id = id;
+    }
+}
