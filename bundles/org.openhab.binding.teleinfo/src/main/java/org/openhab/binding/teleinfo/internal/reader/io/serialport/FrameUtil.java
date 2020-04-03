2406ada8428bdb35392cc0bc88503b4273f72215
Olivier <hollysaiqs@marceau.ovh>
2020-04-03 22:49:54 +0200

    Apply spotless
    Signed-off-by: Olivier Marceau <hollysaiqs@marceau.ovh>

--- bundles/org.openhab.binding.teleinfo/src/main/java/org/openhab/binding/teleinfo/internal/reader/io/serialport/FrameUtil.java
+++ bundles/org.openhab.binding.teleinfo/src/main/java/org/openhab/binding/teleinfo/internal/reader/io/serialport/FrameUtil.java
@@ -27,7 +27,7 @@ public class FrameUtil {
      * Compute the checksum of the given group line.
      *
      * @param groupLine group line ("etiquette" <SPACE> "valeur"). Note: the SPACE before the checksum of the group line
-     *                      must not include in checksum computation.
+     *            must not include in checksum computation.
      * @return the checksum of the given group line.
      */
     public static char computeGroupLineChecksum(final String label, final String value) {
