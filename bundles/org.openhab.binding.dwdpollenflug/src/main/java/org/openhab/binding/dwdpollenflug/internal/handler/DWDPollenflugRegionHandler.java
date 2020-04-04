51267d48a857597962eb7d89138408be8ce45e91
Johannes DerOetzi Ott <info@johannes-ott.net>
2020-04-04 19:45:38 +0200

    [DWDPollenflug] Initial Contribution of new binding
    Signed-off-by: Johannes DerOetzi Ott <info@johannes-ott.net>

---
+++ bundles/org.openhab.binding.dwdpollenflug/src/main/java/org/openhab/binding/dwdpollenflug/internal/handler/DWDPollenflugRegionHandler.java
@@ -0,0 +1,162 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.dwdpollenflug.internal.handler;
+
+import static org.openhab.binding.dwdpollenflug.internal.DWDPollenflugBindingConstants.*;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.thing.Bridge;
+import org.eclipse.smarthome.core.thing.ChannelUID;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingStatus;
+import org.eclipse.smarthome.core.thing.ThingStatusDetail;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.eclipse.smarthome.core.types.Command;
+import org.eclipse.smarthome.core.types.State;
+import org.openhab.binding.dwdpollenflug.internal.config.DWDPollenflugRegionConfiguration;
+import org.openhab.binding.dwdpollenflug.internal.dto.DWDPollenflug;
+import org.openhab.binding.dwdpollenflug.internal.dto.DWDRegion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link DWDPollenflugRegionHandler} is the handler for bridge thing
+ *
+ * @author Johannes DerOetzi Ott - Initial contribution
+ */
+@NonNullByDefault
+public class DWDPollenflugRegionHandler extends BaseThingHandler implements DWDPollenflugRegionListener {
+
+    private final Logger logger = LoggerFactory.getLogger(DWDPollenflugRegionHandler.class);
+
+    private @NonNullByDefault({}) DWDPollenflugRegionConfiguration thingConfig = null;
+
+    private @Nullable DWDPollenflugBridgeHandler bridgeHandler;
+
+    private boolean ignoreConfigurationUpdate;
+
+    private @Nullable Date lastUpdate;
+
+    public DWDPollenflugRegionHandler(Thing thing) {
+        super(thing);
+    }
+
+    @Override
+    public void initialize() {
+        logger.debug("Initializing DWD Pollenflug region handler");
+        thingConfig = getConfigAs(DWDPollenflugRegionConfiguration.class);
+
+        if (thingConfig.isValid()) {
+            DWDPollenflugBridgeHandler handler = syncToBridge();
+            if (handler == null) {
+                updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR, "Bridge handler missing");
+            } else {
+                updateStatus(ThingStatus.ONLINE);
+            }
+        } else {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR);
+        }
+    }
+
+    private synchronized @Nullable DWDPollenflugBridgeHandler syncToBridge() {
+        if (bridgeHandler == null) {
+            Bridge bridge = getBridge();
+            if (bridge == null) {
+                return null;
+            }
+            ThingHandler handler = bridge.getHandler();
+            if (handler instanceof DWDPollenflugBridgeHandler) {
+                bridgeHandler = (DWDPollenflugBridgeHandler) handler;
+                bridgeHandler.registerRegionListener(this);
+            } else {
+                return null;
+            }
+        }
+
+        return bridgeHandler;
+    }
+
+    @Override
+    public void dispose() {
+        logger.debug("DWDPollenflug region handler disposes. Unregistering listener.");
+        DWDPollenflugBridgeHandler handler = syncToBridge();
+        if (handler != null) {
+            handler.unregisterRegionListener(this);
+            bridgeHandler = null;
+        }
+    }
+
+    @Override
+    public void handleConfigurationUpdate(Map<String, Object> configurationParameters) {
+        if (!ignoreConfigurationUpdate) {
+            super.handleConfigurationUpdate(configurationParameters);
+        }
+    }
+
+    @Override
+    public void channelLinked(ChannelUID channelUID) {
+        DWDPollenflugBridgeHandler handler = syncToBridge();
+        if (handler != null) {
+            DWDPollenflug pollenflug = handler.getPollenflug();
+            if (pollenflug != null) {
+                notifyOnUpdate(pollenflug);
+            }
+        }
+    }
+
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+    }
+
+    @Override
+    public void notifyOnUpdate(DWDPollenflug pollenflug) {
+        DWDRegion region = pollenflug.getRegion(thingConfig.getRegionID());
+        if (region == null) {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR, "Region not found");
+            return;
+        }
+
+        updateStatus(ThingStatus.ONLINE);
+
+        ignoreConfigurationUpdate = true;
+        updateProperties(region.getProperties());
+        ignoreConfigurationUpdate = false;
+
+        updateChannels(region.getChannels());
+
+        updateChannels(pollenflug.getChannels());
+
+        if (lastUpdate == null || !lastUpdate.equals(pollenflug.getLastUpdate())) {
+            triggerChannel(CHANNEL_UPDATES + "#" + CHANNEL_UPDATED, TRIGGER_REFRESHED);
+            lastUpdate = pollenflug.getLastUpdate();
+        }
+    }
+
+    private void updateChannels(Map<String, State> channels) {
+        for (Entry<String, State> entry : channels.entrySet()) {
+            String channelID = entry.getKey();
+            if (isLinked(channelID)) {
+                logger.debug("Updating channel {} to {}", channelID, entry.getValue());
+                updateState(channelID, entry.getValue());
+            } else {
+                logger.debug("Channel {} not linked to an item", channelID);
+            }
+        }
+    }
+}
