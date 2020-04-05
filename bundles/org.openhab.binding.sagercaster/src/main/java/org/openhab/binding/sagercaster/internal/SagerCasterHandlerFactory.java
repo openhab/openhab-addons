d3593d901e79eca1802f6d505726de3940a95dff
Gaël L'hopital <gael@lhopital.org>
2020-04-05 20:25:32 +0200

    [sagercaster] Sager Weather Caster Binding initial contribution (#4754)
    * First commit of v2 SagerCaster Binding
    
    Signed-off-by: Gaël L'hopital <gael@lhopital.org>

---
+++ bundles/org.openhab.binding.sagercaster/src/main/java/org/openhab/binding/sagercaster/internal/SagerCasterHandlerFactory.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.sagercaster.internal;
+
+import static org.openhab.binding.sagercaster.internal.SagerCasterBindingConstants.THING_TYPE_SAGERCASTER;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
+import org.openhab.binding.sagercaster.internal.handler.SagerCasterHandler;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * The {@link SagerCasterHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author Gaël L'hopital - Initial contribution
+ */
+@Component(service = ThingHandlerFactory.class, configurationPid = "binding.sagercaster")
+@NonNullByDefault
+public class SagerCasterHandlerFactory extends BaseThingHandlerFactory {
+    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_SAGERCASTER);
+    private final WindDirectionStateDescriptionProvider stateDescriptionProvider;
+    private final SagerWeatherCaster sagerWeatherCaster;
+
+    @Activate
+    public SagerCasterHandlerFactory(@Reference SagerWeatherCaster sagerWeatherCaster,
+            @Reference WindDirectionStateDescriptionProvider provider) {
+        this.stateDescriptionProvider = provider;
+        this.sagerWeatherCaster = sagerWeatherCaster;
+    }
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
+
+    @Override
+    protected @Nullable ThingHandler createHandler(Thing thing) {
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (thingTypeUID.equals(THING_TYPE_SAGERCASTER)) {
+            return new SagerCasterHandler(thing, stateDescriptionProvider, sagerWeatherCaster);
+        }
+        return null;
+    }
+}
