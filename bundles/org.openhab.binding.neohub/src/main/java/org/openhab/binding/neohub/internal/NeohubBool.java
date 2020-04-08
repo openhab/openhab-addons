64a809ad7c8241ad70cc25292d9f8af920eac378
Andrew Fiddian-Green <software@whitebear.ch>
2020-04-08 16:28:10 +0100

    [neohub] bug fix for new hub firmware (#7323)
    * [neohub] new NeohubBool and NeohubBoolDeserializer classes
    * [neohub] implement NeohubBool and NeohubBoolDeserializer
    * [neohub] test NeohubBool, NeohubBoolDeserializer with old/new JSON versions
    
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>
    Signed-off-by: CSchlipp <christian@schlipp.de>

---
+++ bundles/org.openhab.binding.neohub/src/main/java/org/openhab/binding/neohub/internal/NeohubBool.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.neohub.internal;
+
+/**
+ * A custom class that wraps a boolean; we need this because newer versions of
+ * NeoHub have broken JSON for some boolean values so we can't use the standard
+ * deserializer, and thus have to use a custom one
+ * 
+ * @author Andrew Fiddian-Green - Initial contribution
+ * 
+ */
+class NeohubBool {
+    public boolean value;
+
+    public NeohubBool(boolean value) {
+        this.value = value;
+    }
+
+}
---
+++ bundles/org.openhab.binding.neohub/src/main/java/org/openhab/binding/neohub/internal/NeohubBoolDeserializer.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.neohub.internal;
+
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+
+/**
+ * A custom deserializer for NeohubBool; we need this because newer versions of
+ * NeoHub have broken JSON for some boolean values so we can't use the standard
+ * deserializer, and thus have to use a custom one
+ * 
+ * @author Andrew Fiddian-Green - Initial contribution
+ * 
+ */
+public class NeohubBoolDeserializer implements JsonDeserializer<NeohubBool> {
+
+    @Override
+    public NeohubBool deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+            throws JsonParseException {
+        JsonPrimitive jsonPrimitive = json.getAsJsonPrimitive();
+        if (jsonPrimitive.isBoolean()) {
+            return new NeohubBool(jsonPrimitive.getAsBoolean());
+        } else if (jsonPrimitive.isNumber()) {
+            return new NeohubBool(jsonPrimitive.getAsNumber().intValue() != 0);
+        }
+        return new NeohubBool(false);
+    }
+
+}
