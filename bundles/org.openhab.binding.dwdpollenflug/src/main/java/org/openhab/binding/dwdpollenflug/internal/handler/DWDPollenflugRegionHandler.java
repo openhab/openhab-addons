a9cc0480ada4e8e24498f6fc68f341945799ea65
Johannes Ott <mail@johannes-ott.net>
2020-04-12 19:42:16 +0200

    [dwdpollenflug] Initial Contribution (#7298)
    * [DWDPollenflug] New Binding retry
    
    Signed-off-by: Johannes DerOetzi Ott <info@johannes-ott.net>
    Signed-off-by: Eugen Freiter <freiter@gmx.de>

---
+++ bundles/org.openhab.binding.dwdpollenflug/src/main/java/org/openhab/binding/dwdpollenflug/internal/handler/DWDPollenflugRegionHandler.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.dwdpollenflug.internal.handler;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.thing.Bridge;
+import org.eclipse.smarthome.core.thing.ChannelUID;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingStatus;
+import org.eclipse.smarthome.core.thing.ThingStatusDetail;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.eclipse.smarthome.core.types.Command;
+import org.eclipse.smarthome.core.types.RefreshType;
+import org.openhab.binding.dwdpollenflug.internal.config.DWDPollenflugRegionConfiguration;
+import org.openhab.binding.dwdpollenflug.internal.dto.DWDPollenflug;
+import org.openhab.binding.dwdpollenflug.internal.dto.DWDRegion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link DWDPollenflugRegionHandler} is the handler for bridge thing
+ *
+ * @author Johannes Ott - Initial contribution
+ */
+@NonNullByDefault
+public class DWDPollenflugRegionHandler extends BaseThingHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(DWDPollenflugRegionHandler.class);
+
+    private DWDPollenflugRegionConfiguration thingConfig = new DWDPollenflugRegionConfiguration();
+
+    public DWDPollenflugRegionHandler(Thing thing) {
+        super(thing);
+    }
+
+    @Override
+    public void initialize() {
+        logger.debug("Initializing DWD Pollenflug region handler");
+        thingConfig = getConfigAs(DWDPollenflugRegionConfiguration.class);
+
+        if (thingConfig.isValid()) {
+            DWDPollenflugBridgeHandler handler = getBridgeHandler();
+            if (handler == null) {
+                updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR, "Bridge handler missing");
+            } else {
+                updateStatus(ThingStatus.ONLINE);
+            }
+        } else {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR, "No valid region id given.");
+        }
+    }
+
+    private synchronized @Nullable DWDPollenflugBridgeHandler getBridgeHandler() {
+        Bridge bridge = getBridge();
+        if (bridge != null) {
+            ThingHandler handler = bridge.getHandler();
+            if (handler instanceof DWDPollenflugBridgeHandler) {
+                DWDPollenflugBridgeHandler bridgeHandler = (DWDPollenflugBridgeHandler) handler;
+                return bridgeHandler;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void dispose() {
+        logger.debug("DWDPollenflug region handler disposes.");
+    }
+
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        if (command instanceof RefreshType) {
+            refresh();
+        }
+    }
+
+    private void refresh() {
+        DWDPollenflugBridgeHandler handler = getBridgeHandler();
+        if (handler != null) {
+            DWDPollenflug pollenflug = handler.getPollenflug();
+            if (pollenflug != null) {
+                notifyOnUpdate(pollenflug);
+            }
+        }
+    }
+
+    public void notifyOnUpdate(DWDPollenflug pollenflug) {
+        DWDRegion region = pollenflug.getRegion(thingConfig.regionID);
+        if (region == null) {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR, "Region not found");
+            return;
+        }
+
+        updateStatus(ThingStatus.ONLINE);
+        updateProperties(region.getProperties());
+
+        region.getChannelsStateMap().forEach(this::updateState);
+    }
+}
