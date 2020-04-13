22017b337dcd4cca03ac9cf3bb00b1497b4201cf
Wouter Born <github@maindrain.net>
2020-04-13 14:10:46 +0200

    Prevent code from messing with cachedNodes
    Signed-off-by: Wouter Born <github@maindrain.net>

--- bundles/org.openhab.extensionservice.marketplace/src/main/java/org/openhab/extensionservice/marketplace/internal/MarketplaceProxy.java
+++ bundles/org.openhab.extensionservice.marketplace/src/main/java/org/openhab/extensionservice/marketplace/internal/MarketplaceProxy.java
@@ -15,6 +15,7 @@ package org.openhab.extensionservice.marketplace.internal;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -71,7 +72,7 @@ public class MarketplaceProxy {
      * @return list of marketplace nodes
      */
     public List<Node> getNodes() {
-        return Arrays.asList(cachedNodes);
+        return Collections.unmodifiableList(Arrays.asList(cachedNodes));
     }
 
     /**
