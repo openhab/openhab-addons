c46155b8bd5a1bea46008e625a1b9986afce6a94
robnielsen <rob.nielsen@yahoo.com>
2020-04-13 15:45:30 -0500

    [netatmo] fix calculation for heat index (#7360)
    Signed-off-by: Rob Nielsen <rob.nielsen@yahoo.com>
    Signed-off-by: CSchlipp <christian@schlipp.de>

--- bundles/org.openhab.binding.netatmo/src/main/java/org/openhab/binding/netatmo/internal/WeatherUtils.java
+++ bundles/org.openhab.binding.netatmo/src/main/java/org/openhab/binding/netatmo/internal/WeatherUtils.java
@@ -16,33 +16,36 @@ package org.openhab.binding.netatmo.internal;
  * This class holds various unit/measurement conversion methods
  *
  * @author Gaël L'hopital - Initial contribution
+ * @author Rob Nielsen - updated heat index
  */
 public class WeatherUtils {
 
     /**
-     * Compute the HeatIndex temperature given temperature and hygrometry
-     * https://fr.wikipedia.org/wiki/Indice_de_chaleur
+     * Calculate the heat index using temperature and humidity
+     * https://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
      *
      * @param temperature in (°C)
-     * @param hygro relative level (%)
-     * @return heatIndex
+     * @param humidity relative level (%)
+     * @return heatIndex in (°C)
      */
     public static double getHeatIndex(double temperature, double humidity) {
-        double t = (9 / 5) * temperature + 32; // switch to °F
-        double hi = 16.923 + (1.85212 * Math.pow(10, -1) * t) + (5.37941 * humidity)
-                - (1.00254 * Math.pow(10, -1) * t * humidity) + (9.41695 * Math.pow(10, -3) * Math.pow(t, 2))
-                + (7.28898 * Math.pow(10, -3) * Math.pow(humidity, 2))
-                + (3.45372 * Math.pow(10, -4) * Math.pow(t, 2) * humidity)
-                - (8.14971 * Math.pow(10, -4) * t * Math.pow(humidity, 2))
-                + (1.02102 * Math.pow(10, -5) * Math.pow(t, 2) * Math.pow(humidity, 2))
-                - (3.8646 * Math.pow(10, -5) * Math.pow(t, 3)) + (2.91583 * Math.pow(10, -5) * Math.pow(humidity, 3))
-                + (1.42721 * Math.pow(10, -6) * Math.pow(t, 3) * humidity)
-                + (1.97483 * Math.pow(10, -7) * t * Math.pow(humidity, 3))
-                - (2.18429 * Math.pow(10, -8) * Math.pow(t, 3) * Math.pow(humidity, 2))
-                + (8.43296 * Math.pow(10, -10) * Math.pow(t, 2) * Math.pow(humidity, 3))
-                - (4.81975 * Math.pow(10, -11) * Math.pow(t, 3) * Math.pow(humidity, 3));
-        hi = (5.0 / 9.0) * (hi - 32); // get back to °C
-        return hi;
+        double tempF = (temperature * 9.0 / 5.0) + 32.0; // calculations are done in Fahrenheit
+        double heatIndex;
+        if (tempF >= 80.0) {
+            heatIndex = -42.379 + (2.04901523 * tempF) + (10.14333127 * humidity) - (0.22475541 * tempF * humidity)
+                    - (0.00683783 * tempF * tempF) - (0.05481717 * humidity * humidity)
+                    + (0.00122874 * tempF * tempF * humidity) + (0.00085282 * tempF * humidity * humidity)
+                    - (0.00000199 * tempF * tempF * humidity * humidity);
+            if (humidity < 13.0 && tempF <= 112.0) {
+                heatIndex -= ((13.0 - humidity) / 4.0) * Math.sqrt((17.0 - Math.abs(tempF - 95.0)) / 17.0);
+            } else if (humidity > 85.0 && tempF <= 87.0) {
+                heatIndex += ((humidity - 85.0) / 10.0) * ((87.0 - tempF) / 5.0);
+            }
+        } else {
+            heatIndex = 0.5 * (tempF + 61.0 + ((tempF - 68.0) * 1.2) + (humidity * 0.094));
+        }
+
+        return (heatIndex - 32) * 5.0 / 9.0; // convert back to Celsius
     }
 
     public static double getDewPointDep(double temperature, double dewpoint) {
