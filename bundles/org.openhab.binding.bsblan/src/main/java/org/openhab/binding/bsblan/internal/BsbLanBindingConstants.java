d278a2c607a058b7290337789786b889f441d0bb
Peter Schraffl <p.schraffl@gmx.at>
2020-04-16 22:26:30 +0200

    [bsblan] BSB LAN Binding initial contribution (#6314)
    Signed-off-by: Schraffl Peter <p.schraffl@gmx.at>
    Signed-off-by: Eugen Freiter <freiter@gmx.de>

---
+++ bundles/org.openhab.binding.bsblan/src/main/java/org/openhab/binding/bsblan/internal/BsbLanBindingConstants.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.bsblan.internal;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+/**
+ * The {@link BsbLanBindingConstants} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Peter Schraffl - Initial contribution
+ */
+@NonNullByDefault
+public class BsbLanBindingConstants {
+
+    private static final String BINDING_ID = "bsblan";
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID THING_TYPE_BRIDGE = new ThingTypeUID(BINDING_ID, "bridge");
+    public static final ThingTypeUID THING_TYPE_PARAMETER = new ThingTypeUID(BINDING_ID, "parameter");
+
+    // List of all channel ids
+    public static final String PARAMETER_CHANNEL_NAME = "name";
+    public static final String PARAMETER_CHANNEL_NUMBER_VALUE = "number-value";
+    public static final String PARAMETER_CHANNEL_STRING_VALUE = "string-value";
+    public static final String PARAMETER_CHANNEL_SWITCH_VALUE = "switch-value";
+    public static final String PARAMETER_CHANNEL_UNIT = "unit";
+    public static final String PARAMETER_CHANNEL_DESCRIPTION = "description";
+    public static final String PARAMETER_CHANNEL_DATATYPE = "datatype";
+
+    public static final Set<String> WRITEABLE_CHANNELS = new HashSet<String>() {
+
+        private static final long serialVersionUID = 1L;
+        {
+            add(PARAMETER_CHANNEL_NUMBER_VALUE);
+            add(PARAMETER_CHANNEL_STRING_VALUE);
+            add(PARAMETER_CHANNEL_SWITCH_VALUE);
+        }
+    };
+
+    public static final int MIN_REFRESH_INTERVAL = 5;
+    public static final int DEFAULT_REFRESH_INTERVAL = 60;
+    public static final int API_TIMEOUT = 10000;
+    public static final int DEFAULT_API_PORT = 80;
+}
---
+++ bundles/org.openhab.binding.bsblan/src/main/java/org/openhab/binding/bsblan/internal/api/BsbLanApiCaller.java
@@ -0,0 +1,154 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.bsblan.internal.api;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Set;
+import java.util.HashSet;
+import java.nio.charset.Charset;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.io.net.http.HttpUtil;
+import org.openhab.binding.bsblan.internal.api.dto.BsbLanApiContentDTO;
+import org.openhab.binding.bsblan.internal.api.dto.BsbLanApiParameterQueryResponseDTO;
+import org.openhab.binding.bsblan.internal.api.dto.BsbLanApiParameterSetRequestDTO;
+import org.openhab.binding.bsblan.internal.api.dto.BsbLanApiParameterSetResponseDTO;
+import org.openhab.binding.bsblan.internal.api.dto.BsbLanApiParameterSetResultDTO;
+import org.openhab.binding.bsblan.internal.configuration.BsbLanBridgeConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.openhab.binding.bsblan.internal.BsbLanBindingConstants.*;
+
+/**
+ * Utility class to call the BSB-LAN REST API.
+ *
+ * @author Peter Schraffl - Initial contribution
+ */
+@NonNullByDefault
+public class BsbLanApiCaller {
+
+    private final Logger logger = LoggerFactory.getLogger(BsbLanApiCaller.class);
+    private final BsbLanBridgeConfiguration bridgeConfig;
+
+    public BsbLanApiCaller(BsbLanBridgeConfiguration config) {
+        bridgeConfig = config;
+    }
+
+    public @Nullable BsbLanApiParameterQueryResponseDTO queryParameter(Integer parameterId) {
+        Set<Integer> parameters = new HashSet<>();
+
+        parameters.add(parameterId);
+        return queryParameters(parameters);
+    }
+
+    public @Nullable BsbLanApiParameterQueryResponseDTO queryParameters(Set<Integer> parameterIds) {
+        // note: make the request even if parameterIds is empty as
+        // thing OFFLINE/ONLINE detection relies on a response
+
+        String apiPath = String.format("/JQ=%s", StringUtils.join(parameterIds, ","));
+        return makeRestCall(BsbLanApiParameterQueryResponseDTO.class, "GET", apiPath, null);
+    }
+
+    public boolean setParameter(Integer parameterId, String value, BsbLanApiParameterSetRequestDTO.Type type) {
+        // prepare request content
+        BsbLanApiParameterSetRequestDTO request = new BsbLanApiParameterSetRequestDTO();
+        request.parameter = parameterId.toString();
+        request.value = value;
+        request.type = type;
+
+        // make REST call and process response
+        BsbLanApiParameterSetResponseDTO setResponse = makeRestCall(BsbLanApiParameterSetResponseDTO.class, "POST", "/JS", request);
+        if (setResponse == null) {
+            logger.debug("Failed to set parameter {} to '{}': no response received", parameterId, value);
+            return false;
+        }
+
+        BsbLanApiParameterSetResultDTO result = setResponse.getOrDefault(parameterId, null);
+        if (result == null) {
+            logger.debug("Failed to set parameter {} to '{}'': result is null", parameterId, value);
+            return false;
+        }
+        if (result.status == null) {
+            logger.debug("Failed to set parameter {} to '{}': status is null", parameterId, value);
+            return false;
+        }
+        if (result.status != BsbLanApiParameterSetResultDTO.Status.SUCCESS) {
+            logger.debug("Failed to set parameter {} to '{}': status = {}", parameterId, value, result.status);
+            return false;
+        }
+        return true;
+    }
+
+    private String createApiBaseUrl() {
+        final String host = StringUtils.trimToEmpty(bridgeConfig.host);
+        final String username = StringUtils.trimToEmpty(bridgeConfig.username);
+        final String password = StringUtils.trimToEmpty(bridgeConfig.password);
+        final String passkey = StringUtils.trimToEmpty(bridgeConfig.passkey);
+
+        StringBuilder url = new StringBuilder();
+        url.append("http://");
+        if (StringUtils.isNotBlank(username) && StringUtils.isNotBlank(password)) {
+            url.append(username).append(":").append(password).append("@");
+        }
+        url.append(host);
+        if (bridgeConfig.port != 80) {
+            url.append(":").append(bridgeConfig.port);
+        }
+        if (StringUtils.isNotBlank(passkey)) {
+            url.append("/").append(passkey);
+        }
+        return url.toString();
+    }
+
+    /**
+     * @param responseType response class type
+     * @param httpMethod to execute
+     * @param apiPath to request
+     * @param content to add to request
+     * @return the object representation of the json response
+     */
+    private <T> @Nullable T makeRestCall(Class<T> responseType, String httpMethod, String apiPath, @Nullable BsbLanApiContentDTO request) {
+        try {
+            String url = createApiBaseUrl() + apiPath;
+            logger.trace("api request url = '{}'", url);
+
+            InputStream contentStream = null;
+            String contentType = null;
+            if (request != null) {
+                String content = BsbLanApiContentConverter.toJson(request);
+                logger.trace("api request content: '{}'", content);
+                if (StringUtils.isNotBlank(content)) {
+                    contentStream = new ByteArrayInputStream(content.getBytes(Charset.forName("UTF-8")));
+                    contentType = "application/json";
+                }
+            }
+
+            String response = HttpUtil.executeUrl(httpMethod, url, contentStream, contentType, API_TIMEOUT);
+            if (response == null) {
+                logger.debug("no response returned");
+                return null;
+            }
+
+            logger.trace("api response content: '{}'", response);
+            return BsbLanApiContentConverter.fromJson(response, responseType);
+        } catch (IOException | IllegalStateException e) {
+            logger.debug("Error executing bsb-lan api request: {}", e.getMessage());
+            return null;
+        }
+    }
+}
