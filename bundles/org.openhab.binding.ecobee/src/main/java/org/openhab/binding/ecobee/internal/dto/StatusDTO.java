17aeb787bebe2f12665b9ec7830ed75d10e88a62
Mark Hilbush <mark@hilbush.com>
2020-04-16 16:17:48 -0400

    [ecobee] Ecobee binding initial contribution (#6823)
    Signed-off-by: Mark Hilbush <mark@hilbush.com>
    Signed-off-by: Eugen Freiter <freiter@gmx.de>

---
+++ bundles/org.openhab.binding.ecobee/src/main/java/org/openhab/binding/ecobee/internal/dto/StatusDTO.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.ecobee.internal.dto;
+
+/**
+ * The {@link StatusDTO} The StatusDTO object contains the processing status of
+ * the request. It will contain any relevant error information should an error
+ * occur. The status object is returned with every response regardless of success or failure status. It is suitable for
+ * logging request failures.
+ *
+ * @author Mark Hilbush - Initial contribution
+ */
+public class StatusDTO {
+
+    /*
+     * The status code for this status. Success is indicated by a code of 0.
+     */
+    public Integer code;
+
+    /*
+     * The detailed message for this status.
+     */
+    public String message;
+}
---
+++ bundles/org.openhab.binding.ecobee/src/main/java/org/openhab/binding/ecobee/internal/function/AcknowledgeFunction.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.ecobee.internal.function;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.ecobee.internal.enums.AckType;
+
+/**
+ * The acknowledge function allows an alert to be acknowledged.
+ *
+ * @author John Cocula - Initial contribution
+ * @author Mark Hilbush - Adapt for OH2/3
+ */
+@NonNullByDefault
+public final class AcknowledgeFunction extends AbstractFunction {
+
+    public AcknowledgeFunction(@Nullable String thermostatIdentifier, @Nullable String ackRef,
+            @Nullable AckType ackType, @Nullable Boolean remindMeLater) {
+        super("acknowledge");
+
+        if (thermostatIdentifier == null || ackRef == null || ackType == null) {
+            throw new IllegalArgumentException("thermostatIdentifier, ackRef and ackType are required.");
+        }
+        params.put("thermostatIdentifier", thermostatIdentifier);
+        params.put("ackRef", ackRef);
+        params.put("ackType", ackType);
+        if (remindMeLater != null) {
+            params.put("remindMeLater", remindMeLater);
+        }
+    }
+}
