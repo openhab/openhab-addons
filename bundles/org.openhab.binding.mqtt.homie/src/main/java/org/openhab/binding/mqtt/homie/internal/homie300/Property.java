76650979ef7641147c90c244a648cca3bd4c19aa
szwenni <szwenni@users.noreply.github.com>
2020-04-17 19:16:34 +0200

    [mqtt.homie] Add Dimmer functionality (#6845)
    * added changes
    
    Signed-off-by: Sven Kroeger <svenni.kroeger@gmail.com>
    Signed-off-by: CSchlipp <christian@schlipp.de>

--- bundles/org.openhab.binding.mqtt.homie/src/main/java/org/openhab/binding/mqtt/homie/internal/homie300/Property.java
+++ bundles/org.openhab.binding.mqtt.homie/src/main/java/org/openhab/binding/mqtt/homie/internal/homie300/Property.java
@@ -41,6 +41,7 @@ import org.openhab.binding.mqtt.generic.mapping.AbstractMqttAttributeClass.Attri
 import org.openhab.binding.mqtt.generic.values.ColorValue;
 import org.openhab.binding.mqtt.generic.values.NumberValue;
 import org.openhab.binding.mqtt.generic.values.OnOffValue;
+import org.openhab.binding.mqtt.generic.values.PercentageValue;
 import org.openhab.binding.mqtt.generic.values.TextValue;
 import org.openhab.binding.mqtt.generic.values.Value;
 import org.openhab.binding.mqtt.homie.generic.internal.MqttBindingConstants;
@@ -193,8 +194,11 @@ public class Property implements AttributeChanged {
                 if (step != null && !isDecimal && step.intValue() <= 0) {
                     step = new BigDecimal(1);
                 }
-
-                value = new NumberValue(min, max, step, attributes.unit);
+                if (attributes.unit != null && attributes.unit.contains("%")) {
+                    value = new PercentageValue(min, max, step, null, null);
+                } else {
+                    value = new NumberValue(min, max, step, attributes.unit);
+                }
                 break;
             case string_:
             case unknown:
