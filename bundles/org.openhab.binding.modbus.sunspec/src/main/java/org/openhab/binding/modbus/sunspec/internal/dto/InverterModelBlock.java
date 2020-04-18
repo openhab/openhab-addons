b8d22c81b579a3bd48cbaaddc2d8bc23807d65f5
Nagy Attila Gábor <mrbig@sneaker.hu>
2020-04-18 09:44:28 +0200

    [modbus][sunspec] Initial contribution (#6331)
    * [sunspec] Modbus: SunSpec bundle basic version
    
    SunSpec is an open standard for solar inverters and other related devices to share data about their internal state. The standard is implemented by several vendors like ABB, Fronius, SMA, Schneider Electric, Solaredge, etc.
    
    The goal of this work is to add user friendly support of these devices to openHAB.
    
    The standard is built on the Modbus protocol, so this work is heavily based on the Modbus binding. Also the Bluetooth binding and several of it's solutions were taken as an example how to extend an already existing binding with a new bundle.
    
    Related issue: #3216
    
    This is a stripped down version of a SunSpec bundle aimed to ease reviewing.
    
    Original PW with all the features can be found here:
      https://github.com/openhab/openhab2-addons/pull/4220
    
    This version contains only a limited support for single phase inverters, but no auto discovery or any other fancy features are included.
    
    Other changes from my original PR:
     - migrated to the new build system
     - using NonNull values wherever possible
     - modell classes were moved to a dto package and highly simplified
     - other minor code changes
    
    Signed-off-by: Nagy Attila Gabor <mrbig@sneaker.hu>

---
+++ bundles/org.openhab.binding.modbus.sunspec/src/main/java/org/openhab/binding/modbus/sunspec/internal/dto/InverterModelBlock.java
@@ -0,0 +1,140 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.modbus.sunspec.internal.dto;
+
+import java.util.Optional;
+
+/**
+ * Model for SunSpec compatible inverter data
+ *
+ * @author Nagy Attila Gabor - Initial contribution
+ *
+ */
+public class InverterModelBlock {
+
+    /**
+     * Type of inverter (single phase, split phase, three phase)
+     */
+    public Integer phaseConfiguration;
+
+    /**
+     * Length of the block in 16bit words
+     */
+    public Integer length;
+
+    /**
+     * AC Total Current value
+     */
+    public Integer acCurrentTotal;
+
+    /**
+     * AC Current scale factor
+     */
+    public Short acCurrentSF;
+
+    /**
+     * AC Power value
+     */
+    public Short acPower;
+
+    /**
+     * AC Power scale factor
+     */
+    public Short acPowerSF;
+
+    /**
+     * AC Frequency value
+     */
+    public Integer acFrequency;
+
+    /**
+     * AC Frequency scale factor
+     */
+    public Short acFrequencySF;
+
+    /**
+     * Apparent power
+     */
+    public Optional<Short> acApparentPower;
+
+    /**
+     * Apparent power scale factor
+     */
+    public Optional<Short> acApparentPowerSF;
+
+    /**
+     * Reactive power
+     */
+    public Optional<Short> acReactivePower;
+
+    /**
+     * Reactive power scale factor
+     */
+    public Optional<Short> acReactivePowerSF;
+
+    /**
+     * Power factor
+     */
+    public Optional<Short> acPowerFactor;
+
+    /**
+     * Power factor scale factor
+     */
+    public Optional<Short> acPowerFactorSF;
+
+    /**
+     * AC Lifetime Energy production
+     */
+    public Long acEnergyLifetime;
+
+    /**
+     * AC Lifetime Energy scale factor
+     */
+    public Short acEnergyLifetimeSF;
+
+    /**
+     * Cabinet temperature
+     */
+    public Short temperatureCabinet;
+
+    /**
+     * Heat sink temperature
+     */
+    public Optional<Short> temperatureHeatsink;
+
+    /**
+     * Transformer temperature
+     */
+    public Optional<Short> temperatureTransformer;
+
+    /**
+     * Other temperature
+     */
+    public Optional<Short> temperatureOther;
+
+    /**
+     * Heat sink temperature scale factor
+     */
+    public Short temperatureSF;
+
+    /**
+     * Current operating state
+     */
+    public Integer status;
+
+    /**
+     * Vendor defined operating state or error code
+     */
+    public Optional<Integer> statusVendor;
+
+}
