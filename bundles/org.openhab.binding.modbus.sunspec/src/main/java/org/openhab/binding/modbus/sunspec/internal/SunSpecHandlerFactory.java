b81f62575309adb2f16b38c986222fa3692b8d00
Nagy Attila Gábor <mrbig@sneaker.hu>
2020-04-18 09:44:28 +0200

    [modbus][sunspec] Initial contribution (#6331)
    * [sunspec] Modbus: SunSpec bundle basic version
    
    SunSpec is an open standard for solar inverters and other related devices to share data about their internal state. The standard is implemented by several vendors like ABB, Fronius, SMA, Schneider Electric, Solaredge, etc.
    
    The goal of this work is to add user friendly support of these devices to openHAB.
    
    The standard is built on the Modbus protocol, so this work is heavily based on the Modbus binding. Also the Bluetooth binding and several of it's solutions were taken as an example how to extend an already existing binding with a new bundle.
    
    Related issue: #3216
    
    This is a stripped down version of a SunSpec bundle aimed to ease reviewing.
    
    Original PW with all the features can be found here:
      https://github.com/openhab/openhab2-addons/pull/4220
    
    This version contains only a limited support for single phase inverters, but no auto discovery or any other fancy features are included.
    
    Other changes from my original PR:
     - migrated to the new build system
     - using NonNull values wherever possible
     - modell classes were moved to a dto package and highly simplified
     - other minor code changes
    
    Signed-off-by: Nagy Attila Gabor <mrbig@sneaker.hu>

---
+++ bundles/org.openhab.binding.modbus.sunspec/src/main/java/org/openhab/binding/modbus/sunspec/internal/SunSpecHandlerFactory.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.modbus.sunspec.internal;
+
+import static org.openhab.binding.modbus.sunspec.internal.SunSpecConstants.THING_TYPE_INVERTER_SINGLE_PHASE;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
+import org.openhab.binding.modbus.sunspec.internal.handler.InverterHandler;
+import org.openhab.io.transport.modbus.ModbusManager;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link SunSpecHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author Nagy Attila Gábor - Initial contribution
+ */
+@NonNullByDefault
+@Component(configurationPid = "binding.sunspec", service = ThingHandlerFactory.class)
+public class SunSpecHandlerFactory extends BaseThingHandlerFactory {
+
+    /**
+     * Logger instance
+     */
+    private final Logger logger = LoggerFactory.getLogger(SunSpecHandlerFactory.class);
+
+    /**
+     * Reference to the modbus manager
+     */
+    private ModbusManager manager;
+
+    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections
+            .singleton(THING_TYPE_INVERTER_SINGLE_PHASE);
+
+    /**
+     * This factory needs a reference to the ModbusManager wich is provided
+     * by the org.openhab.io.transport.modbus bundle. Please make
+     * sure it's installed and enabled before using this bundle
+     *
+     * @param manager reference to the ModbusManager. We use this for modbus communication
+     */
+    @Activate
+    public SunSpecHandlerFactory(@Reference ModbusManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
+
+    @Override
+    protected @Nullable ThingHandler createHandler(Thing thing) {
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (thingTypeUID.equals(THING_TYPE_INVERTER_SINGLE_PHASE)) {
+            logger.debug("New InverterHandler created");
+            return new InverterHandler(thing, manager);
+        }
+
+        return null;
+    }
+}
