c58a384c9628ccbbf786bc8b99f81b1f5c1cb663
Nagy Attila Gábor <mrbig@sneaker.hu>
2020-04-18 09:44:28 +0200

    [modbus][sunspec] Initial contribution (#6331)
    * [sunspec] Modbus: SunSpec bundle basic version
    
    SunSpec is an open standard for solar inverters and other related devices to share data about their internal state. The standard is implemented by several vendors like ABB, Fronius, SMA, Schneider Electric, Solaredge, etc.
    
    The goal of this work is to add user friendly support of these devices to openHAB.
    
    The standard is built on the Modbus protocol, so this work is heavily based on the Modbus binding. Also the Bluetooth binding and several of it's solutions were taken as an example how to extend an already existing binding with a new bundle.
    
    Related issue: #3216
    
    This is a stripped down version of a SunSpec bundle aimed to ease reviewing.
    
    Original PW with all the features can be found here:
      https://github.com/openhab/openhab2-addons/pull/4220
    
    This version contains only a limited support for single phase inverters, but no auto discovery or any other fancy features are included.
    
    Other changes from my original PR:
     - migrated to the new build system
     - using NonNull values wherever possible
     - modell classes were moved to a dto package and highly simplified
     - other minor code changes
    
    Signed-off-by: Nagy Attila Gabor <mrbig@sneaker.hu>

---
+++ bundles/org.openhab.binding.modbus.sunspec/src/main/java/org/openhab/binding/modbus/sunspec/internal/dto/ModelBlock.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.modbus.sunspec.internal.dto;
+
+/**
+ * Descriptor for a model block found on the device
+ * This DTO contains only the metadata required to
+ * address the block at the modbus register space
+ *
+ * @author Nagy Attila Gabor - Initial contribution
+ */
+public class ModelBlock {
+
+    /**
+     * Base address of this block in 16bit words
+     */
+    public int address;
+
+    /**
+     * Length of this block in 16bit words
+     */
+    public int length;
+
+    /**
+     * Module identifier
+     */
+    public int moduleID;
+
+    @Override
+    public String toString() {
+        return String.format("ModelBlock type=%d address=%d length=%d", moduleID, address, length);
+    }
+}
