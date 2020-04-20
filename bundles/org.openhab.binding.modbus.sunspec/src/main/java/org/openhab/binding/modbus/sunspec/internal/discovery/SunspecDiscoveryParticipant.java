47ecc505d10ae30caf9eff43a8ded6ca21d2b77a
Nagy Attila Gábor <mrbig@sneaker.hu>
2020-04-20 17:28:34 +0200

    [sunspec] Modbus: sunspec bundle auto discovery (#6994)
    * [sunspec] Added discovery process to SunSpec bundle
    
    The discovery process will read the common model block of the device
    which must be supported by every SunSpec compatible device.
    
    For there model types are organized into block, so we hop through
    all the supported blocks and emit them as discovered.
    
    Discovered devices will contain theire start address and block length in
    the configuration properties.
    
    Signed-off-by: Nagy Attila Gabor <mrbig@sneaker.hu>

---
+++ bundles/org.openhab.binding.modbus.sunspec/src/main/java/org/openhab/binding/modbus/sunspec/internal/discovery/SunspecDiscoveryParticipant.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.modbus.sunspec.internal.discovery;
+
+import static org.openhab.binding.modbus.sunspec.internal.SunSpecConstants.SUPPORTED_THING_TYPES_UIDS;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.openhab.binding.modbus.discovery.ModbusDiscoveryListener;
+import org.openhab.binding.modbus.discovery.ModbusDiscoveryParticipant;
+import org.openhab.binding.modbus.handler.EndpointNotInitializedException;
+import org.openhab.binding.modbus.handler.ModbusEndpointThingHandler;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Discovery service for sunspec
+ *
+ * @author Nagy Attila Gabor - initial contribution
+ *
+ */
+@Component(immediate = true)
+@NonNullByDefault
+public class SunspecDiscoveryParticipant implements ModbusDiscoveryParticipant {
+
+    private final Logger logger = LoggerFactory.getLogger(SunspecDiscoveryParticipant.class);
+
+    @Override
+    public Set<ThingTypeUID> getSupportedThingTypeUIDs() {
+        return new HashSet<ThingTypeUID>(SUPPORTED_THING_TYPES_UIDS.values());
+    }
+
+    @Override
+    public void startDiscovery(ModbusEndpointThingHandler handler, ModbusDiscoveryListener listener) {
+        logger.trace("Starting sunspec discovery");
+        try {
+            new SunspecDiscoveryProcess(handler, listener).detectModel();
+        } catch (EndpointNotInitializedException ex) {
+            logger.debug("Could not start discovery process");
+            listener.discoveryFinished();
+        }
+    }
+
+}
