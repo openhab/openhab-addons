fe87b1c158a7394e242db96348199cf063eb534a
Marcel Verpaalen <marcel@verpaalen.com>
2020-04-21 00:29:57 +0200

    [miio] add copyright
    Signed-off-by: Marcel Verpaalen <marcel@verpaalen.com>

--- bundles/org.openhab.binding.miio/src/test/java/org/openhab/binding/miio/internal/ActionConditionTest.java
+++ bundles/org.openhab.binding.miio/src/test/java/org/openhab/binding/miio/internal/ActionConditionTest.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.miio.internal;
 
 import static org.junit.Assert.*;
@@ -5,6 +17,7 @@ import static org.junit.Assert.*;
 import java.util.Collections;
 import java.util.Map;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.junit.Test;
 import org.openhab.binding.miio.internal.basic.ActionConditions;
 import org.openhab.binding.miio.internal.basic.MiIoDeviceActionCondition;
@@ -12,6 +25,13 @@ import org.openhab.binding.miio.internal.basic.MiIoDeviceActionCondition;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 
+/**
+ * Test case for {@link ActionConditions}
+ *
+ * @author Marcel Verpaalen - Initial contribution
+ *
+ */
+@NonNullByDefault
 public class ActionConditionTest {
 
     @Test
