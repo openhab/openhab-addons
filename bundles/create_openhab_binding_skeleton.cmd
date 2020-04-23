a9d00a5edcb93b4527ea1d9b7b919149441e6054
Christoph Weitkamp <github@christophweitkamp.de>
2020-04-23 16:01:27 +0200

    Fix missing 2.5.5-SNAPSHOT changes (#7447)
    * Fix missing 2.5.5-SNAPSHOT changes
    
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>
    Signed-off-by: CSchlipp <christian@schlipp.de>

--- bundles/create_openhab_binding_skeleton.cmd
+++ bundles/create_openhab_binding_skeleton.cmd
@@ -11,7 +11,7 @@ IF %ARGC% NEQ 3 (
 )
 
 SET OpenhabCoreVersion="2.5.0"
-SET OpenhabVersion="2.5.4-SNAPSHOT"
+SET OpenhabVersion="2.5.5-SNAPSHOT"
 
 SET BindingIdInCamelCase=%~1
 SET BindingIdInLowerCase=%BindingIdInCamelCase%
