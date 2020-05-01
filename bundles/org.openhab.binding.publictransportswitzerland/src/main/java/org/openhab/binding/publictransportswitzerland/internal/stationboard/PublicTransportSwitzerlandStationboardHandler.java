cc26b13080171ac74902a179ca0c973e41949fe9
Jeremy Stucki <dev@jeremystucki.ch>
2020-05-01 15:59:22 +0200

    [publictransportswitzerland] Add csv stationboard
    Signed-off-by: Jeremy Stucki <dev@jeremystucki.ch>

--- bundles/org.openhab.binding.publictransportswitzerland/src/main/java/org/openhab/binding/publictransportswitzerland/internal/stationboard/PublicTransportSwitzerlandStationboardHandler.java
+++ bundles/org.openhab.binding.publictransportswitzerland/src/main/java/org/openhab/binding/publictransportswitzerland/internal/stationboard/PublicTransportSwitzerlandStationboardHandler.java
@@ -12,6 +12,7 @@
  */
 package org.openhab.binding.publictransportswitzerland.internal.stationboard;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -29,9 +30,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
+import static org.openhab.binding.publictransportswitzerland.internal.PublicTransportSwitzerlandBindingConstants.CHANNEL_CSV;
 import static org.openhab.binding.publictransportswitzerland.internal.PublicTransportSwitzerlandBindingConstants.CHANNEL_JSON;
 
 /**
@@ -75,14 +79,45 @@ public class PublicTransportSwitzerlandStationboardHandler extends BaseThingHand
             String response = HttpUtil.executeUrl("GET", "https://transport.opendata.ch/v1/stationboard?station=" + config.station, 10_000);
             JsonElement jsonObject = new JsonParser().parse(response);
 
-            updateState(CHANNEL_JSON, new StringType(jsonObject.toString()));
+            updateCsvChannel(jsonObject);
+            updateJsonChannel(jsonObject);
             updateStatus(ThingStatus.ONLINE);
-        } catch (IOException e) {
+        } catch (Exception e) {
             logger.warn("Unable to fetch stationboard data", e);
 
             updateStatus(ThingStatus.OFFLINE);
+            updateState(CHANNEL_CSV, new StringType("No data available"));
             updateState(CHANNEL_JSON, new StringType("{}"));
         }
+    }
+
+    private void updateJsonChannel(JsonElement jsonObject) {
+        updateState(CHANNEL_JSON, new StringType(jsonObject.toString()));
+    }
+
+    private void updateCsvChannel(JsonElement jsonObject) throws Exception {
+        JsonArray stationboard = jsonObject.getAsJsonObject().get("stationboard").getAsJsonArray();
+
+        List<String> departures = new ArrayList<>();
+
+        for (JsonElement jsonElement : stationboard) {
+            JsonObject departureObject = jsonElement.getAsJsonObject();
+            JsonObject stopObject = departureObject.get("stop").getAsJsonObject();
+
+            String departureTime = stopObject.get("departureTimestamp").getAsString();
+            String destination = departureObject.get("to").getAsString();
+            String track = stopObject.get("platform").getAsString();
+
+            JsonElement delayElement = departureObject.get("delay");
+            String delay = "";
+            if (delayElement != null) {
+                delay = delayElement.getAsString();
+            }
+
+            departures.add(String.join("\t", departureTime, destination, track, delay));
+        }
+
+        updateState(CHANNEL_CSV, new StringType(String.join("\n", departures)));
     }
 
 }
