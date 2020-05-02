9ed50892826cad1d6acbeb64d28bc2667299e24e
Mr_Ronfo <fabio.possieri@gmail.com>
2020-05-02 18:49:51 +0200

    Revert "Fix protocol selection for SSL in IMAP/POP3IMAPHandler (#7437)"
    This reverts commit 2cf522aeb7fca23de6a2f0301c682291d5054437.

--- bundles/org.openhab.binding.mail/src/main/java/org/openhab/binding/mail/internal/POP3IMAPHandler.java
+++ bundles/org.openhab.binding.mail/src/main/java/org/openhab/binding/mail/internal/POP3IMAPHandler.java
@@ -71,7 +71,7 @@ public class POP3IMAPHandler extends BaseThingHandler {
         protocol = baseProtocol;
 
         if (config.security == ServerSecurity.SSL) {
-            protocol = protocol.concat("s");
+            protocol.concat("s");
         }
 
         if (config.port == 0) {
