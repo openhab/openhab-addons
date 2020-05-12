db9eeabb3aad57470ce3ef9630e709809acde294
Wouter Born <github@maindrain.net>
2020-05-13 01:56:43 +0200

    [elerotransmitterstick] Use serial transport (#7619)
    Related to #7573
    
    Signed-off-by: Wouter Born <github@maindrain.net>
    Signed-off-by: CSchlipp <christian@schlipp.de>

--- bundles/org.openhab.binding.elerotransmitterstick/src/main/java/org/openhab/binding/elerotransmitterstick/internal/EleroTransmitterStickHandlerFactory.java
+++ bundles/org.openhab.binding.elerotransmitterstick/src/main/java/org/openhab/binding/elerotransmitterstick/internal/EleroTransmitterStickHandlerFactory.java
@@ -14,12 +14,16 @@ package org.openhab.binding.elerotransmitterstick.internal;
 
 import static org.openhab.binding.elerotransmitterstick.internal.EleroTransmitterStickBindingConstants.*;
 
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.config.discovery.DiscoveryService;
 import org.eclipse.smarthome.core.thing.Bridge;
 import org.eclipse.smarthome.core.thing.Thing;
@@ -28,11 +32,14 @@ import org.eclipse.smarthome.core.thing.ThingUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
 import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
+import org.eclipse.smarthome.io.transport.serial.SerialPortManager;
 import org.openhab.binding.elerotransmitterstick.internal.discovery.EleroChannelDiscoveryService;
 import org.openhab.binding.elerotransmitterstick.internal.handler.EleroChannelHandler;
 import org.openhab.binding.elerotransmitterstick.internal.handler.EleroTransmitterStickHandler;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * The {@link EleroTransmitterStickHandlerFactory} is responsible for creating things and thing
@@ -40,16 +47,21 @@ import org.osgi.service.component.annotations.Component;
  *
  * @author Volker Bier - Initial contribution
  */
+@NonNullByDefault
 @Component(service = ThingHandlerFactory.class, configurationPid = "binding.elerotransmitterstick")
 public class EleroTransmitterStickHandlerFactory extends BaseThingHandlerFactory {
 
-    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = new HashSet<>();
-    static {
-        SUPPORTED_THING_TYPES_UIDS.add(THING_TYPE_STICK);
-        SUPPORTED_THING_TYPES_UIDS.add(THING_TYPE_ELERO_CHANNEL);
-    }
+    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections
+            .unmodifiableSet(Stream.of(THING_TYPE_STICK, THING_TYPE_ELERO_CHANNEL).collect(Collectors.toSet()));
 
-    private Map<ThingUID, ServiceRegistration<?>> discoveryServiceRegs = new HashMap<>();
+    private final Map<ThingUID, ServiceRegistration<?>> discoveryServiceRegs = new HashMap<>();
+
+    private final SerialPortManager serialPortManager;
+
+    @Activate
+    public EleroTransmitterStickHandlerFactory(final @Reference SerialPortManager serialPortManager) {
+        this.serialPortManager = serialPortManager;
+    }
 
     @Override
     public boolean supportsThingType(ThingTypeUID thingTypeUID) {
@@ -57,11 +69,12 @@ public class EleroTransmitterStickHandlerFactory extends BaseThingHandlerFactory
     }
 
     @Override
-    protected ThingHandler createHandler(Thing thing) {
+    protected @Nullable ThingHandler createHandler(Thing thing) {
         ThingTypeUID thingTypeUID = thing.getThingTypeUID();
 
         if (thingTypeUID.equals(THING_TYPE_STICK)) {
-            EleroTransmitterStickHandler bridgeHandler = new EleroTransmitterStickHandler((Bridge) thing);
+            EleroTransmitterStickHandler bridgeHandler = new EleroTransmitterStickHandler((Bridge) thing,
+                    serialPortManager);
 
             EleroChannelDiscoveryService discoveryService = new EleroChannelDiscoveryService(bridgeHandler);
             discoveryServiceRegs.put(bridgeHandler.getThing().getUID(), bundleContext
