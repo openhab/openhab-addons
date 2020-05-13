5e142a93c1c1798ce5d32cf34cda0d7aa2750a67
Jochen Klein <git@jochen.susca.de>
2020-05-13 04:12:02 +0200

    [mqtt.generic] add trigger channel type (#6902)
    * add possibility to make generic string/number channels triggers
    * witespace
    * make trigger an own channel type
    * improve description
    * good point
    * review
    
    Signed-off-by: Jochen Klein <git@jochen.susca.de>
    Signed-off-by: CSchlipp <christian@schlipp.de>

--- bundles/org.openhab.binding.mqtt.generic/src/main/java/org/openhab/binding/mqtt/generic/values/ValueFactory.java
+++ bundles/org.openhab.binding.mqtt.generic/src/main/java/org/openhab/binding/mqtt/generic/values/ValueFactory.java
@@ -67,6 +67,10 @@ public class ValueFactory {
             case MqttBindingConstants.ROLLERSHUTTER:
                 value = new RollershutterValue(config.on, config.off, config.stop);
                 break;
+            case MqttBindingConstants.TRIGGER:
+                config.trigger = true;
+                value = new TextValue();
+                break;
             default:
                 throw new IllegalArgumentException("ChannelTypeUID not recognised: " + channelTypeID);
         }
