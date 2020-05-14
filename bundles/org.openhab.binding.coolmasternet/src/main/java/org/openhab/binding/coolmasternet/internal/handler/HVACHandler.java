openhab/openhab-addons

commit d867772169949a8652c6e4d68583a9ffd78166a5

[coolmasternet] General tidy up of addon (#7546)
Changes include:

* Remove unnecessary compiler warning suppression
* Fix JavaDocs (use proper syntax and tags)
* Fix incorrect constant for louve channel
* Improve matching style (string constants, switch)
* Consistent comments (inline, not block or JavaDoc)
* Eliminate magic number
* Query for controller temperature scale and set units accordingly
* Shorten lines to improve readability
* Properly handle refresh command (don't rely on poller)
* Return from failed methods earlier to improve readability
* Use final where possible
* Rename refresh to poller
* Stop using custom exception except for specific protocol case
* Rationalise exception handling (centralise catch and log)
* Synchronize on appropriate final object for polling use case
* Centralise thing state reporting logic and make more informative
* Accelerate commands by using shorter sink timeout where connected
* Add polling duration trace message
* Use appropriate logging levels to match OpenHAB standards
* Cache configuration to reduce lookup overhead
* Move static constants and initialisation to start of compilation unit
* Apply Spotless plugin

This has been tested on a CoolMasterNet system. This includes various
breaking conditions such as introducing the wrong address to the
bridge etc and ensuring bridge and HVAC states vary as
expected (including returning to online states when breaking
conditions were corrected).

Signed-off-by: Ben Alex <ben.alex@acegi.com.au>

bundles/org.openhab.binding.coolmasternet/src/main/java/org/openhab/binding/coolmasternet/internal/handler/HVACHandler.java
@@ -14,6 +14,7 @@ package org.openhab.binding.coolmasternet.internal.handler;
 
 import static org.openhab.binding.coolmasternet.internal.CoolMasterNetBindingConstants.*;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,8 +22,10 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.core.library.types.DecimalType;
 import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.library.types.QuantityType;
 import org.eclipse.smarthome.core.library.types.StringType;
 import org.eclipse.smarthome.core.thing.Bridge;
+import org.eclipse.smarthome.core.thing.Channel;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingStatus;
@@ -32,7 +35,6 @@ import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.RefreshType;
 import org.openhab.binding.coolmasternet.internal.ControllerHandler;
 import org.openhab.binding.coolmasternet.internal.ControllerHandler.CoolMasterClientError;
-import org.openhab.binding.coolmasternet.internal.config.ControllerConfiguration;
 import org.openhab.binding.coolmasternet.internal.config.HVACConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,137 +49,187 @@ import org.slf4j.LoggerFactory;
 @NonNullByDefault
 public class HVACHandler extends BaseThingHandler {
 
-    private final Logger logger = LoggerFactory.getLogger(HVACHandler.class);
+    /**
+     * The CoolMasterNet protocol's query command returns numbers 0-5 for fan
+     * speed, but the protocol's fan command (and matching binding command) use
+     * single-letter abbreviations.
+     */
+    private static final Map<String, @Nullable String> FAN_NUM_TO_STR;
 
-    public HVACHandler(Thing thing) {
-        super(thing);
-    }
-
-    private @Nullable ControllerHandler getControllerHandler() {
-        Bridge bridge = getBridge();
-        return bridge != null ? (ControllerHandler) bridge.getHandler() : null;
-    }
-
-    @Override
-    public void handleCommand(ChannelUID channelUID, Command command) {
-        String uid = getConfigAs(HVACConfiguration.class).uid;
-        String channel = channelUID.getId();
-        ControllerHandler controller = getControllerHandler();
-
-        try {
-            Bridge bridge = getBridge();
-            if (bridge == null) {
-                updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
-                        "CoolMasterNet Controller bridge not set");
-            } else if (controller == null || !controller.isConnected()) {
-                ControllerConfiguration config = bridge.getConfiguration().as(ControllerConfiguration.class);
-                updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
-                        String.format("Could not connect to CoolMasterNet unit %s:%d", config.host, config.port));
-            } else {
-                if (channel.endsWith(ON) && command instanceof OnOffType) {
-                    OnOffType onoff = (OnOffType) command;
-                    controller.sendCommand(String.format("%s %s", onoff == OnOffType.ON ? "on" : "off", uid));
-                } else if (channel.endsWith(SET_TEMP) && command instanceof DecimalType) {
-                    DecimalType temp = (DecimalType) command;
-                    controller.sendCommand(String.format("temp %s %s", uid, temp));
-                } else if (channel.endsWith(MODE) && command instanceof StringType) {
-                    /* the mode value in the command is the actual CoolMasterNet protocol command */
-                    controller.sendCommand(String.format("%s %s", command, uid));
-                } else if (channel.endsWith(FAN_SPEED) && command instanceof StringType) {
-                    controller.sendCommand(String.format("fspeed %s %s", uid, command));
-                } else if (channel.endsWith(LOUVRE) && command instanceof StringType) {
-                    controller.sendCommand(String.format("swing %s %s", uid, command));
-                } else if (command instanceof RefreshType) {
-                    refresh();
-                }
-            }
-        } catch (CoolMasterClientError e) {
-            logger.error("Failed to set channel {} -> {}: {}", channel, command, e.getMessage());
-        }
-    }
-
-    @Override
-    public void initialize() {
-        logger.debug("Initializing CoolMasterNet HVAC handler...");
-        updateStatus(ThingStatus.ONLINE);
-    }
-
-    /* Update this HVAC unit's properties from the controller */
-    public void refresh() {
-        String on = query("o");
-        if (on != null) {
-            updateState(ON, "1".equals(on) ? OnOffType.ON : OnOffType.OFF);
-        }
-
-        String currentTemp = query("a");
-        if (currentTemp != null) {
-            updateState(CURRENT_TEMP, new DecimalType(currentTemp));
-        }
-
-        String setTemp = query("t");
-        if (setTemp != null) {
-            updateState(SET_TEMP, new DecimalType(setTemp));
-        }
-
-        String mode = MODE_NUM_TO_STR.get(query("m"));
-        if (mode != null) {
-            updateState(MODE, new StringType(mode));
-        }
-
-        String louvre = query("s");
-        if (louvre != null) {
-            updateState(LOUVRE, new StringType(louvre));
-        }
-
-        String fan = FAN_NUM_TO_STR.get(query("f"));
-        if (fan != null) {
-            updateState(FAN_SPEED, new StringType(fan));
-        }
-    }
-
-    private @Nullable String query(String queryChar) {
-        String uid = getConfigAs(HVACConfiguration.class).uid;
-        String command = String.format("query %s %s", uid, queryChar);
-        ControllerHandler controller = getControllerHandler();
-        if (controller != null) {
-            try {
-                return controller.sendCommand(command);
-            } catch (CoolMasterClientError e) {
-                logger.error("Query '{}' failed: {}", command, e.getMessage());
-            }
-        }
-        return null; /* passing back null sets an invalid value on the channel */
-    }
-
-    /*
+    /**
      * The CoolMasterNet query command returns numbers 0-5 for operation modes,
      * but these don't map to any mode you can set on the device, so we use this
      * lookup table.
      */
     private static final Map<String, @Nullable String> MODE_NUM_TO_STR;
+
     static {
+        FAN_NUM_TO_STR = new HashMap<>();
+        FAN_NUM_TO_STR.put("0", "l"); // low
+        FAN_NUM_TO_STR.put("1", "m"); // medium
+        FAN_NUM_TO_STR.put("2", "h"); // high
+        FAN_NUM_TO_STR.put("3", "a"); // auto
+        FAN_NUM_TO_STR.put("4", "t"); // top
+
         MODE_NUM_TO_STR = new HashMap<>();
         MODE_NUM_TO_STR.put("0", "cool");
         MODE_NUM_TO_STR.put("1", "heat");
         MODE_NUM_TO_STR.put("2", "auto");
         MODE_NUM_TO_STR.put("3", "dry");
-        /* 4=='haux' but this mode doesn't have an equivalent command to set it! */
+        // 4=='haux' but this mode doesn't have an equivalent command to set it
         MODE_NUM_TO_STR.put("4", "heat");
         MODE_NUM_TO_STR.put("5", "fan");
     }
 
-    /*
-     * The CoolMasterNet protocol's query command returns numbers 0-5
-     * for fan speed, but the protocol's fan command (& matching
-     * binding command) use single-letter abbreviations.
+    private HVACConfiguration cfg = new HVACConfiguration();
+    private final Logger logger = LoggerFactory.getLogger(HVACHandler.class);
+
+    public HVACHandler(final Thing thing) {
+        super(thing);
+    }
+
+    /**
+     * Get the controller handler for this bridge.
+     *
+     * <p>
+     * This method does not raise any exception, but if null is returned it will
+     * always update the Thing status with the reason.
+     *
+     * <p>
+     * The returned handler may or may not be connected. This method will not
+     * change the Thing status simply because it is not connected, because a
+     * caller may wish to attempt an operation that would result in connection.
+     *
+     * @return the controller handler or null if the controller is unavailable
      */
-    private static final Map<String, @Nullable String> FAN_NUM_TO_STR;
-    static {
-        FAN_NUM_TO_STR = new HashMap<>();
-        FAN_NUM_TO_STR.put("0", "l"); /* Low */
-        FAN_NUM_TO_STR.put("1", "m"); /* Medium */
-        FAN_NUM_TO_STR.put("2", "h"); /* High */
-        FAN_NUM_TO_STR.put("3", "a"); /* Auto */
-        FAN_NUM_TO_STR.put("4", "t"); /* Top */
+    private @Nullable ControllerHandler getControllerHandler() {
+        final Bridge bridge = getBridge();
+        if (bridge == null) {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
+                    "CoolMasterNet Controller bridge not configured");
+            return null;
+        }
+
+        final ControllerHandler handler = (ControllerHandler) bridge.getHandler();
+
+        if (handler == null) {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.BRIDGE_UNINITIALIZED,
+                    "CoolMasterNet Controller bridge not initialized");
+            return null;
+        }
+
+        return handler;
+    }
+
+    @Override
+    public void handleCommand(final ChannelUID channelUID, final Command command) {
+        final ControllerHandler controller = getControllerHandler();
+        if (controller == null) {
+            return;
+        }
+
+        final String uid = cfg.uid;
+        final String channel = channelUID.getId();
+
+        try {
+            switch (channel) {
+                case CURRENT_TEMP:
+                    if (command instanceof RefreshType) {
+                        final String currentTemp = query(controller, "a");
+                        if (currentTemp != null) {
+                            final Integer temp = new Integer(currentTemp);
+                            final QuantityType<?> value = new QuantityType<>(temp, controller.getUnit());
+                            updateState(CURRENT_TEMP, value);
+                        }
+                    }
+                    break;
+                case ON:
+                    if (command instanceof RefreshType) {
+                        final String on = query(controller, "o");
+                        if (on != null) {
+                            updateState(ON, "1".equals(on) ? OnOffType.ON : OnOffType.OFF);
+                        }
+                    } else if (command instanceof OnOffType) {
+                        final OnOffType onoff = (OnOffType) command;
+                        controller.sendCommand(String.format("%s %s", onoff == OnOffType.ON ? "on" : "off", uid));
+                    }
+                    break;
+                case SET_TEMP:
+                    if (command instanceof RefreshType) {
+                        final String setTemp = query(controller, "t");
+                        if (setTemp != null) {
+                            final Integer temp = new Integer(setTemp);
+                            final QuantityType<?> value = new QuantityType<>(temp, controller.getUnit());
+                            updateState(SET_TEMP, value);
+                        }
+                    } else if (command instanceof DecimalType) {
+                        final DecimalType temp = (DecimalType) command;
+                        controller.sendCommand(String.format("temp %s %s", uid, temp));
+                    }
+                    break;
+                case MODE:
+                    if (command instanceof RefreshType) {
+                        final String mode = MODE_NUM_TO_STR.get(query(controller, "m"));
+                        if (mode != null) {
+                            updateState(MODE, new StringType(mode));
+                        }
+                    } else if (command instanceof StringType) {
+                        final String mode = ((StringType) command).toString();
+                        controller.sendCommand(String.format("%s %s", mode, uid));
+                    }
+                    break;
+                case FAN_SPEED:
+                    if (command instanceof RefreshType) {
+                        final String fan = FAN_NUM_TO_STR.get(query(controller, "f"));
+                        if (fan != null) {
+                            updateState(FAN_SPEED, new StringType(fan));
+                        }
+                    } else if (command instanceof StringType) {
+                        final String fan = ((StringType) command).toString();
+                        controller.sendCommand(String.format("fspeed %s %s", uid, fan));
+                    }
+                    break;
+                case LOUVRE:
+                    if (command instanceof RefreshType) {
+                        final String louvre = query(controller, "s");
+                        if (louvre != null) {
+                            updateState(LOUVRE, new StringType(louvre));
+                        }
+                    } else if (command instanceof StringType) {
+                        final String louvre = ((StringType) command).toString();
+                        controller.sendCommand(String.format("swing %s %s", uid, louvre));
+                    }
+                    break;
+                default:
+                    logger.warn("Unknown command '{}' on channel '{}' for unit '{}'", command, channel, uid);
+            }
+            updateStatus(ThingStatus.ONLINE);
+        } catch (final IOException ioe) {
+            logger.warn("Failed to handle command '{}' on channel '{}' for unit '{}' due to '{}'", command, channel,
+                    uid, ioe.getLocalizedMessage());
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR, ioe.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public void initialize() {
+        cfg = getConfigAs(HVACConfiguration.class);
+        updateStatus(ThingStatus.UNKNOWN);
+    }
+
+    /**
+     * Update this HVAC unit's properties from the controller.
+     */
+    public void refresh() {
+        for (final Channel channel : getThing().getChannels()) {
+            handleCommand(channel.getUID(), RefreshType.REFRESH);
+        }
+    }
+
+    private @Nullable String query(final ControllerHandler controller, final String queryChar)
+            throws IOException, CoolMasterClientError {
+        final String uid = getConfigAs(HVACConfiguration.class).uid;
+        final String command = String.format("query %s %s", uid, queryChar);
+        return controller.sendCommand(command);
     }
 }

commit 6afdedfa1e06e1fe5dc6fd6d21e8780aaa8d9872

[coolmasternet] Fix set_temp channel bug
This commit corrects a regression introduced in #7546 where the
set_temp channel was changed to a QuantityType.

This has been tested on a CoolMasterNet deployment using a Setpoint
element in the sitemap.

Signed-off-by: Ben Alex <ben.alex@acegi.com.au>

bundles/org.openhab.binding.coolmasternet/src/main/java/org/openhab/binding/coolmasternet/internal/handler/HVACHandler.java
@@ -20,7 +20,6 @@ import java.util.Map;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import org.eclipse.smarthome.core.library.types.DecimalType;
 import org.eclipse.smarthome.core.library.types.OnOffType;
 import org.eclipse.smarthome.core.library.types.QuantityType;
 import org.eclipse.smarthome.core.library.types.StringType;
@@ -162,9 +161,11 @@ public class HVACHandler extends BaseThingHandler {
                             final QuantityType<?> value = new QuantityType<>(temp, controller.getUnit());
                             updateState(SET_TEMP, value);
                         }
-                    } else if (command instanceof DecimalType) {
-                        final DecimalType temp = (DecimalType) command;
-                        controller.sendCommand(String.format("temp %s %s", uid, temp));
+                    } else if (command instanceof QuantityType) {
+                        final QuantityType<?> temp = (QuantityType) command;
+                        final QuantityType<?> converted = temp.toUnit(controller.getUnit());
+                        final String formatted = converted.format("%.1f");
+                        controller.sendCommand(String.format("temp %s %s", uid, formatted));
                     }
                     break;
                 case MODE: