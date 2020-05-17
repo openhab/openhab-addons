79fb96387178c20c270dbcfde005ee24a89ab3bd
ArjenKorevaar <a.korevaar@mephix.com>
2020-05-17 19:19:49 +0200

    [openthermgateway] Initial contribution (#6785)
    * Initial contribution
    
    Signed-off-by: Arjen Korevaar <a.korevaar@mephix.com>
    Signed-off-by: CSchlipp <christian@schlipp.de>

---
+++ bundles/org.openhab.binding.openthermgateway/src/main/java/org/openhab/binding/openthermgateway/internal/OpenThermGatewayConnector.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.openthermgateway.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link OpenThermGatewayConnector} interface is used to allow multiple types of connectors
+ * to be implemented and used to connect to the OpenTherm Gateway.
+ * 
+ * @author Arjen Korevaar - Initial contribution
+ */
+@NonNullByDefault
+public interface OpenThermGatewayConnector extends Runnable {
+    void sendCommand(GatewayCommand command);
+
+    boolean isConnected();
+
+    void stop();
+}
