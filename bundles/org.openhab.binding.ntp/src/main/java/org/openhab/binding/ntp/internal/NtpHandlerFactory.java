ce70115a55f956b36687bf3fc9ce6d12d682ad40
lolodomo <lg.hc@free.fr>
2020-05-18 23:47:01 +0200

    [ntp] Handler factory activation simplified (#7677)
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>
    Signed-off-by: CSchlipp <christian@schlipp.de>

--- bundles/org.openhab.binding.ntp/src/main/java/org/openhab/binding/ntp/internal/NtpHandlerFactory.java
+++ bundles/org.openhab.binding.ntp/src/main/java/org/openhab/binding/ntp/internal/NtpHandlerFactory.java
@@ -14,6 +14,8 @@ package org.openhab.binding.ntp.internal;
 
 import static org.openhab.binding.ntp.internal.NtpBindingConstants.*;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.core.i18n.LocaleProvider;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
@@ -21,6 +23,7 @@ import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
 import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
 import org.openhab.binding.ntp.internal.handler.NtpHandler;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -31,18 +34,15 @@ import org.osgi.service.component.annotations.Reference;
  * @author Marcel Verpaalen - Initial contribution
  * @author Markus Rathgeb - Add locale provider support
  */
+@NonNullByDefault
 @Component(service = ThingHandlerFactory.class, configurationPid = "binding.ntp")
 public class NtpHandlerFactory extends BaseThingHandlerFactory {
 
-    private LocaleProvider localeProvider;
+    private final LocaleProvider localeProvider;
 
-    @Reference
-    protected void setLocaleProvider(final LocaleProvider localeProvider) {
+    @Activate
+    public NtpHandlerFactory(final @Reference LocaleProvider localeProvider) {
         this.localeProvider = localeProvider;
-    }
-
-    protected void unsetLocaleProvider(final LocaleProvider localeProvider) {
-        this.localeProvider = null;
     }
 
     @Override
@@ -51,7 +51,7 @@ public class NtpHandlerFactory extends BaseThingHandlerFactory {
     }
 
     @Override
-    protected ThingHandler createHandler(Thing thing) {
+    protected @Nullable ThingHandler createHandler(Thing thing) {
         ThingTypeUID thingTypeUID = thing.getThingTypeUID();
 
         if (THING_TYPE_NTP.equals(thingTypeUID)) {
