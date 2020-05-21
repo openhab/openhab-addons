dffde8c8f6fa1c4cd6b6d940e0177c6747f7c29f
Jeremy Stucki <dev@jeremystucki.ch>
2020-05-21 11:40:44 +0200

    [publictransportswitzerland] Stop scheduler on config error
    Signed-off-by: Jeremy Stucki <dev@jeremystucki.ch>

--- bundles/org.openhab.binding.publictransportswitzerland/src/main/java/org/openhab/binding/publictransportswitzerland/internal/stationboard/PublicTransportSwitzerlandStationboardHandler.java
+++ bundles/org.openhab.binding.publictransportswitzerland/src/main/java/org/openhab/binding/publictransportswitzerland/internal/stationboard/PublicTransportSwitzerlandStationboardHandler.java
@@ -79,14 +79,17 @@ public class PublicTransportSwitzerlandStationboardHandler extends BaseThingHand
         PublicTransportSwitzerlandStationboardConfiguration config = getConfigAs(PublicTransportSwitzerlandStationboardConfiguration.class);
 
         if (config.station == null || config.station.isEmpty()) {
+            if (updateDataJob != null) {
+                updateDataJob.cancel(true);
+            }
+
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR);
-            return;
-        }
+        } else {
+            updateStatus(ThingStatus.INITIALIZING);
 
-        updateStatus(ThingStatus.INITIALIZING);
-
-        if (updateDataJob == null || updateDataJob.isCancelled()) {
-            updateDataJob = scheduler.scheduleWithFixedDelay(this::updateData, 0, 60, TimeUnit.SECONDS);
+            if (updateDataJob == null || updateDataJob.isCancelled()) {
+                updateDataJob = scheduler.scheduleWithFixedDelay(this::updateData, 0, 60, TimeUnit.SECONDS);
+            }
         }
     }
 
