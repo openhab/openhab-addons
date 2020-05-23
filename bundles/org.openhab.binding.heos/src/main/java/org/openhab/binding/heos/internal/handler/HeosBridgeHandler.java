7625a47dba518bb20d8e0f5f8475ca7481361b56
Martin van Wingerden <martin@martinvw.nl>
2020-05-23 21:42:32 +0200

    Add missing import for Optional
    Signed-off-by: Martin van Wingerden <martin@martinvw.nl>

--- bundles/org.openhab.binding.heos/src/main/java/org/openhab/binding/heos/internal/handler/HeosBridgeHandler.java
+++ bundles/org.openhab.binding.heos/src/main/java/org/openhab/binding/heos/internal/handler/HeosBridgeHandler.java
@@ -24,6 +24,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
