275ae7be3ba0dde36ac3b7ccd89ccd9d7f3ef994
lolodomo <lg.hc@free.fr>
2020-05-23 13:09:33 +0200

    [rfxcom] Removed dependency on 'org.apache.commons.io.IOUtils' (#7729)
    Related to #7722
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/internal/connector/RFXComTcpConnector.java
+++ bundles/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/internal/connector/RFXComTcpConnector.java
@@ -17,7 +17,6 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 
-import org.apache.commons.io.IOUtils;
 import org.eclipse.smarthome.core.util.HexUtils;
 import org.openhab.binding.rfxcom.internal.config.RFXComBridgeConfiguration;
 import org.slf4j.Logger;
@@ -69,16 +68,28 @@ public class RFXComTcpConnector extends RFXComBaseConnector {
 
         if (out != null) {
             logger.debug("Close tcp out stream");
-            IOUtils.closeQuietly(out);
+            try {
+                out.close();
+            } catch (IOException e) {
+                logger.debug("Error while closing the out stream: {}", e.getMessage());
+            }
         }
         if (in != null) {
             logger.debug("Close tcp in stream");
-            IOUtils.closeQuietly(in);
+            try {
+                in.close();
+            } catch (IOException e) {
+                logger.debug("Error while closing the in stream: {}", e.getMessage());
+            }
         }
 
         if (socket != null) {
             logger.debug("Close socket");
-            IOUtils.closeQuietly(socket);
+            try {
+                socket.close();
+            } catch (IOException e) {
+                logger.debug("Error while closing the socket: {}", e.getMessage());
+            }
         }
 
         readerThread = null;
