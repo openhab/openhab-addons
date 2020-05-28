a8a647d048fde5817a600de40d0d4c7caa8ff879
Markus Pfleger <Markus@pfleger.local>
2020-05-28 22:53:27 +0200

    Initial contribution of the automower binding
    Signed-off-by: Markus Pfleger <pfleger_markus@gmx.at>

---
+++ bundles/org.openhab.binding.automower/src/main/java/org/openhab/binding/automower/internal/things/AutomowerConfiguration.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.automower.internal.things;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * The {@link AutomowerConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Markus Pfleger - Initial contribution
+ */
+@NonNullByDefault
+public class AutomowerConfiguration {
+
+    @Nullable
+    public String mowerId;
+    public int pollingInterval;
+
+    @Nullable
+    public String getMowerId() {
+        return mowerId;
+    }
+
+    public void setMowerId(String mowerId) {
+        this.mowerId = mowerId;
+    }
+
+    public int getPollingInterval() {
+        return pollingInterval;
+    }
+
+    public void setPollingInterval(int pollingInterval) {
+        this.pollingInterval = pollingInterval;
+    }
+
+}
