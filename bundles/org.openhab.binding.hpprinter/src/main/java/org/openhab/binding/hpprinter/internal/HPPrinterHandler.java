6319a3b0e2678613f72f4fac2a205971114076c6
Stewart Cossey <stewart.cossey@gmail.com>
2020-05-31 15:54:43 +1200

    [hpprinter] Add additional data points and refactoring (#7805)
    * Remove Binder Event and directly expose handler and move to same package.
    Tidy up some code.
    Add data points for Scanner function.
    * Refactor some items.
    * Add scanner and app groups and data points.
    * Change HPType to HPProductUsageFeatures.
    Add additional feature checks.
    Add additional data points for the Scanner.
    Additional check for status message in the ProductStatus data.
    Only run the status checkers on when the relevant channels are linked.
    Check if the tray is open or empty.
    Code refactoring.
    * Tidy up some warnings.
    * Added Pages Remaining Data Points.
    Adjust Channel names and also made more channels advanced.
    * Add pages remaining data points.
    * Make all channels dynamic.
    Add scanner data points.
    Add copy data points.
    Tweaks to some of the channel groups and channel names.
    
    Signed-off-by: Stewart Cossey <stewart.cossey@gmail.com>

--- bundles/org.openhab.binding.hpprinter/src/main/java/org/openhab/binding/hpprinter/internal/HPPrinterHandler.java
+++ bundles/org.openhab.binding.hpprinter/src/main/java/org/openhab/binding/hpprinter/internal/HPPrinterHandler.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.hpprinter.internal.handler;
+package org.openhab.binding.hpprinter.internal;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,12 +26,12 @@ import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingStatus;
 import org.eclipse.smarthome.core.thing.ThingStatusDetail;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
+import org.eclipse.smarthome.core.thing.binding.ThingHandlerCallback;
+import org.eclipse.smarthome.core.thing.binding.builder.ThingBuilder;
 import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.State;
-import org.openhab.binding.hpprinter.internal.HPPrinterConfiguration;
-import org.openhab.binding.hpprinter.internal.binder.HPPrinterBinder;
-import org.openhab.binding.hpprinter.internal.binder.HPPrinterBinderEvent;
 
+import static org.openhab.binding.hpprinter.internal.HPPrinterBindingConstants.*;
 /**
  * The {@link HPPrinterHandler} is responsible for handling commands, which are
  * sent to one of the channels.
@@ -39,32 +39,32 @@ import org.openhab.binding.hpprinter.internal.binder.HPPrinterBinderEvent;
  * @author Stewart Cossey - Initial contribution
  */
 @NonNullByDefault
-public class HPPrinterHandler extends BaseThingHandler implements HPPrinterBinderEvent {
+public class HPPrinterHandler extends BaseThingHandler {
     private final HttpClient httpClient;
     private @Nullable HPPrinterBinder binder;
 
-    public HPPrinterHandler(Thing thing, HttpClient httpClient) {
+    public HPPrinterHandler(final Thing thing, final HttpClient httpClient) {
         super(thing);
         this.httpClient = httpClient;
     }
 
     @Override
-    public void thingUpdated(Thing thing) {
+    public void thingUpdated(final Thing thing) {
         super.thingUpdated(thing);
 
-        HPPrinterBinder printerBinder = this.binder;
+        final HPPrinterBinder printerBinder = this.binder;
         if (printerBinder != null) {
             printerBinder.dynamicallyAddChannels(thing.getUID());
         }
     }
 
     @Override
-    public void handleCommand(ChannelUID channelUID, Command command) {
+    public void handleCommand(final ChannelUID channelUID, final Command command) {
     }
 
     @Override
     public void initialize() {
-        HPPrinterConfiguration config = getConfigAs(HPPrinterConfiguration.class);
+        final HPPrinterConfiguration config = getConfigAs(HPPrinterConfiguration.class);
 
         if (config != null && !"".equals(config.ipAddress)) {
             binder = new HPPrinterBinder(this, httpClient, scheduler, config);
@@ -83,45 +83,68 @@ public class HPPrinterHandler extends BaseThingHandler implements HPPrinterBinde
             binder = null;
         }
     }
+    
+    protected Boolean areStatusChannelsLinked(final String[] channels) {
+        for (int i = 0; i < channels.length; i++) {
+           if (isLinked(new ChannelUID(thing.getUID(), CGROUP_STATUS, channels[i]))) {
+               return true;
+           }
+        }
+        return false;
+    }
 
-    @Override
-    public void binderStatus(ThingStatus status) {
-        updateStatus(status);
+    protected void updateStatus(final ThingStatus status) {
+        super.updateStatus(status);
     }
 
     @Override
-    public void binderStatus(ThingStatus status, ThingStatusDetail thingStatusDetail, String message) {
-        updateStatus(status, thingStatusDetail, message);
+    public void channelLinked(ChannelUID channelUID) {
+        binder.channelsChanged();
     }
 
     @Override
-    public void binderChannel(String group, String channel, State state) {
-        updateState(new ChannelUID(thing.getUID(), group, channel), state);
+    public void channelUnlinked(ChannelUID channelUID) {
+        binder.channelsChanged();
     }
 
-    @Override
-    public void binderAddChannels(List<Channel> channels) {
-        List<Channel> thingChannels = new ArrayList<>(getThing().getChannels());
+    protected void updateStatus(final ThingStatus status, final ThingStatusDetail thingStatusDetail,
+    @Nullable final String message) {
+        super.updateStatus(status, thingStatusDetail, message);
+    }
 
-        for (Channel channel : channels) {
+    public void updateState(final String group, final String channel, final State state) {
+        super.updateState(new ChannelUID(thing.getUID(), group, channel), state);
+    }
+
+    public void binderAddChannels(final List<Channel> channels) {
+        final List<Channel> thingChannels = new ArrayList<>(getThing().getChannels());
+
+        for (final Channel channel : channels) {
             addOrUpdateChannel(channel, thingChannels);
         }
 
         updateThing(editThing().withChannels(thingChannels).build());
     }
 
-    @Override
-    public void binderProperties(Map<String, String> properties) {
-        updateProperties(properties);
+    protected ThingBuilder editThing() {
+        return super.editThing();
     }
 
-    private static void addOrUpdateChannel(Channel newChannel, List<Channel> thingChannels) {
+    protected @Nullable ThingHandlerCallback getCallback() {
+        return super.getCallback();
+    }
+
+    protected void updateProperties(final Map<String, String> properties) {
+        super.updateProperties(properties);
+    }
+
+    private static void addOrUpdateChannel(final Channel newChannel, final List<Channel> thingChannels) {
         removeChannelByUID(thingChannels, newChannel.getUID());
         thingChannels.add(newChannel);
     }
 
-    private static void removeChannelByUID(List<Channel> thingChannels, ChannelUID channelUIDtoRemove) {
-        Predicate<Channel> channelPredicate = c -> c.getUID().getId().equals(channelUIDtoRemove.getId());
+    private static void removeChannelByUID(final List<Channel> thingChannels, final ChannelUID channelUIDtoRemove) {
+        final Predicate<Channel> channelPredicate = c -> c.getUID().getId().equals(channelUIDtoRemove.getId());
         thingChannels.removeIf(channelPredicate);
     }
 }
