2984b8f82cb1e7e1935beb0ea3ff058ac5bbdb85
lolodomo <lg.hc@free.fr>
2020-05-31 21:49:38 +0200

    Console command: retrieve the thing handler (#7822)
    * Console command: retrieve the thing handler
    * Built usage string when only one command
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/org.openhab.binding.max/src/main/java/org/openhab/binding/max/internal/MaxConsoleCommandExtension.java
+++ bundles/org.openhab.binding.max/src/main/java/org/openhab/binding/max/internal/MaxConsoleCommandExtension.java
@@ -23,6 +23,7 @@ import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingRegistry;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.ThingUID;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
 import org.eclipse.smarthome.io.console.Console;
 import org.eclipse.smarthome.io.console.extensions.AbstractConsoleCommandExtension;
 import org.eclipse.smarthome.io.console.extensions.ConsoleCommandExtension;
@@ -108,9 +109,11 @@ public class MaxConsoleCommandExtension extends AbstractConsoleCommandExtension 
         try {
             ThingUID bridgeUID = new ThingUID(thingId);
             Thing thing = thingRegistry.get(bridgeUID);
-            if ((thing != null) && (thing.getHandler() != null)
-                    && (thing.getHandler() instanceof MaxCubeBridgeHandler)) {
-                handler = (MaxCubeBridgeHandler) thing.getHandler();
+            if (thing != null) {
+                ThingHandler thingHandler = thing.getHandler();
+                if (thingHandler instanceof MaxCubeBridgeHandler) {
+                    handler = (MaxCubeBridgeHandler) thingHandler;
+                }
             }
         } catch (Exception e) {
             handler = null;
