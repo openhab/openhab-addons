2378eeda9ab27ba2a0bef6edf14ac356362a94b3
Gaël L'hopital <gael@lhopital.org>
2020-06-05 10:25:32 +0200

    [vigiecrues] Vigicrues binding : track river level (#7503)
    Signed-off-by: clinique <gael@lhopital.org>
    Signed-off-by: DrRSatzteil <thomas_lauterbach@arcor.de>

---
+++ bundles/org.openhab.binding.vigicrues/src/main/java/org/openhab/binding/vigicrues/internal/json/Parameters.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.vigicrues.internal.json;
+
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * The {@link Parameters} is the Java class used to map the JSON
+ * response to the webservice request.
+ *
+ * @author Gaël L'hopital - Initial contribution
+ */
+@NonNullByDefault
+public class Parameters {
+    private String[] dataset = {};
+    private String timezone = "";
+    private int rows;
+    private String format = "";
+    private @Nullable Refine refine;
+    private String[] facet = {};
+
+    public Optional<String> getDataset() {
+        return Arrays.stream(dataset).findFirst();
+    }
+
+    public ZoneId getTimezone() {
+        return ZoneId.of(timezone);
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public Optional<Refine> getRefine() {
+        Refine refine = this.refine;
+        if (refine != null) {
+            return Optional.of(refine);
+        }
+        return Optional.empty();
+    }
+
+    public String[] getFacets() {
+        return facet;
+    }
+}
