0ae2319ddc3564f690215c31e941fbe3803b06d0
Fabian Wolter <github@fabian-wolter.de>
2020-06-17 21:20:29 +0200

    [lcn] Add LCN binding (#7509)
    * [lcn] Add LCN binding
    
    Migrates the Local Control Network Binding from OH1 to OH2.
    
    Closes #108
    
    Signed-off-by: Fabian Wolter <github@fabian-wolter.de>

---
+++ bundles/org.openhab.binding.lcn/src/main/java/org/openhab/binding/lcn/internal/connection/ConnectionStateConnecting.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.lcn.internal.connection;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.StandardSocketOptions;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.lcn.internal.common.LcnException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This state is active during the socket creation, host name resolving and waiting for the TCP connection to become
+ * established.
+ *
+ * @author Fabian Wolter - Initial Contribution
+ */
+@NonNullByDefault
+public class ConnectionStateConnecting extends AbstractConnectionState {
+    private final Logger logger = LoggerFactory.getLogger(ConnectionStateConnecting.class);
+
+    public ConnectionStateConnecting(ConnectionStateMachine context) {
+        super(context);
+    }
+
+    @Override
+    public void startWorking() {
+        connection.clearRuntimeData();
+
+        logger.debug("Connecting to {}:{} ...", connection.getSettings().getAddress(),
+                connection.getSettings().getPort());
+
+        try {
+            // Open Channel by using the system-wide default AynchronousChannelGroup.
+            // So, Threads are used or re-used on demand by the JVM.
+            AsynchronousSocketChannel channel = AsynchronousSocketChannel.open();
+            // Do not wait until some buffer is filled, send PCK commands immediately
+            channel.setOption(StandardSocketOptions.TCP_NODELAY, true);
+            connection.setSocketChannel(channel);
+
+            InetSocketAddress address = new InetSocketAddress(connection.getSettings().getAddress(),
+                    connection.getSettings().getPort());
+
+            if (address.isUnresolved()) {
+                throw new LcnException("Could not resolve hostname");
+            }
+
+            channel.connect(address, null, new CompletionHandler<@Nullable Void, @Nullable Void>() {
+                @Override
+                public void completed(@Nullable Void result, @Nullable Void attachment) {
+                    connection.readAndProcess();
+                    nextState(ConnectionStateSendUsername::new);
+                }
+
+                @Override
+                public void failed(@Nullable Throwable e, @Nullable Void attachment) {
+                    handleConnectionFailure(e);
+                }
+            });
+        } catch (IOException | LcnException e) {
+            handleConnectionFailure(e);
+        }
+    }
+
+    private void handleConnectionFailure(@Nullable Throwable e) {
+        String message;
+        if (e != null) {
+            logger.warn("Could not connect to {}:{}: {}", connection.getSettings().getAddress(),
+                    connection.getSettings().getPort(), e.getMessage());
+            message = e.getMessage();
+        } else {
+            message = "";
+        }
+        connection.getCallback().onOffline(message);
+        context.handleConnectionFailed(e);
+    }
+
+    @Override
+    public void onPckMessageReceived(String data) {
+        // nothing
+    }
+}
---
+++ bundles/org.openhab.binding.lcn/src/main/java/org/openhab/binding/lcn/internal/subhandler/LcnModuleRelaySubHandler.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.lcn.internal.subhandler;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.library.types.PercentType;
+import org.eclipse.smarthome.core.library.types.UpDownType;
+import org.openhab.binding.lcn.internal.LcnBindingConstants;
+import org.openhab.binding.lcn.internal.LcnModuleHandler;
+import org.openhab.binding.lcn.internal.common.LcnChannelGroup;
+import org.openhab.binding.lcn.internal.common.LcnDefs;
+import org.openhab.binding.lcn.internal.common.LcnDefs.RelayStateModifier;
+import org.openhab.binding.lcn.internal.common.LcnException;
+import org.openhab.binding.lcn.internal.common.PckGenerator;
+import org.openhab.binding.lcn.internal.connection.ModInfo;
+
+/**
+ * Handles Commands and State changes of Relays of an LCN module.
+ * 
+ * @author Fabian Wolter - Initial contribution
+ */
+@NonNullByDefault
+public class LcnModuleRelaySubHandler extends AbstractLcnModuleSubHandler {
+    private static final Pattern PATTERN = Pattern.compile(LcnBindingConstants.ADDRESS_REGEX + "Rx(?<byteValue>\\d+)");
+
+    public LcnModuleRelaySubHandler(LcnModuleHandler handler, ModInfo info) {
+        super(handler, info);
+    }
+
+    @Override
+    public void handleRefresh(LcnChannelGroup channelGroup, int number) {
+        info.refreshRelays();
+    }
+
+    @Override
+    public void handleCommandOnOff(OnOffType command, LcnChannelGroup channelGroup, int number) throws LcnException {
+        RelayStateModifier[] relayStateModifiers = createRelayStateModifierArray();
+        relayStateModifiers[number] = command == OnOffType.ON ? LcnDefs.RelayStateModifier.ON
+                : LcnDefs.RelayStateModifier.OFF;
+        handler.sendPck(PckGenerator.controlRelays(relayStateModifiers));
+    }
+
+    @Override
+    public void handleCommandPercent(PercentType command, LcnChannelGroup channelGroup, int number)
+            throws LcnException {
+        // don't use OnOffType.as(), because it returns @Nullable
+        handleCommandOnOff(command.intValue() > 0 ? OnOffType.ON : OnOffType.OFF, channelGroup, number);
+    }
+
+    @Override
+    public void handleStatusMessage(Matcher matcher) {
+        info.onRelayResponseReceived();
+
+        boolean[] states = LcnDefs.getBooleanValue(Integer.parseInt(matcher.group("byteValue")));
+
+        IntStream.range(0, LcnChannelGroup.RELAY.getCount())
+                .forEach(i -> fireUpdate(LcnChannelGroup.RELAY, i, OnOffType.from(states[i])));
+
+        IntStream.range(0, LcnChannelGroup.ROLLERSHUTTERRELAY.getCount()).forEach(i -> {
+            UpDownType state = states[i * 2 + 1] ? UpDownType.DOWN : UpDownType.UP;
+            fireUpdate(LcnChannelGroup.ROLLERSHUTTERRELAY, i, state);
+        });
+    }
+
+    @Override
+    public Collection<Pattern> getPckStatusMessagePatterns() {
+        return Collections.singleton(PATTERN);
+    }
+}
