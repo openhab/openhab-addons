0a144a99ea75ca6b524efc7af55f71326765a5c1
Fabian Wolter <github@fabian-wolter.de>
2020-06-17 21:20:29 +0200

    [lcn] Add LCN binding (#7509)
    * [lcn] Add LCN binding
    
    Migrates the Local Control Network Binding from OH1 to OH2.
    
    Closes #108
    
    Signed-off-by: Fabian Wolter <github@fabian-wolter.de>
    Signed-off-by: Bernd <bernd.weymann@gmail.com>

---
+++ bundles/org.openhab.binding.lcn/src/main/java/org/openhab/binding/lcn/internal/PckGatewayConfiguration.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.lcn.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link PckGatewayConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Fabian Wolter - Initial contribution
+ */
+@NonNullByDefault
+public class PckGatewayConfiguration {
+    private @NonNullByDefault({}) String hostname;
+    private int port;
+    private @NonNullByDefault({}) String username;
+    private @NonNullByDefault({}) String password;
+    private @NonNullByDefault({}) String mode;
+    private @NonNullByDefault({}) int timeoutMs;
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public int getTimeoutMs() {
+        return timeoutMs;
+    }
+}
