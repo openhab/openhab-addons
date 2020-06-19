abc926ddd03ca667b6e1dfb1b37cb77491154f79
lolodomo <lg.hc@free.fr>
2020-06-19 22:53:58 +0200

    [ntp] Clean up (#7834)
    * [ntp] Clean up
    
    Add null annotations
    Suppress one unused configutation setting (locale)
    Get the default timezone from TimeZoneProvider
    Map thing configuration and channel configuration to classes
    Use constructor injection for the discovery service
    * Use TimeZoneProvider each time the timezone is required
    * Call activate in the discovery service constructor
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

---
+++ bundles/org.openhab.binding.ntp/src/main/java/org/openhab/binding/ntp/internal/config/NtpThingConfiguration.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.ntp.internal.config;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * The {@link NtpThingConfiguration} is responsible for holding
+ * the thing configuration settings
+ *
+ * @author Laurent Garnier - Initial contribution
+ */
+@NonNullByDefault
+public class NtpThingConfiguration {
+
+    public String hostname = "0.pool.ntp.org";
+    public int refreshInterval = 60;
+    public int refreshNtp = 30;
+    public int serverPort = 123;
+    public @Nullable String timeZone;
+}
