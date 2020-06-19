dedabcb57565a85a52882bbadda319b5cdf17b7c
Maximilian Hess <mail@ne0h.de>
2020-06-19 20:54:23 +0200

    [KVV] Make use of HttpUtil to retrieve data from KVV api
    Signed-off-by: Maximilian Hess <mail@ne0h.de>

--- bundles/org.openhab.binding.kvv/src/main/java/org/openhab/binding/kvv/internal/KVVBridgeHandler.java
+++ bundles/org.openhab.binding.kvv/src/main/java/org/openhab/binding/kvv/internal/KVVBridgeHandler.java
@@ -12,21 +12,19 @@
  */
 package org.openhab.binding.kvv.internal;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 import com.google.gson.Gson;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.library.types.RawType;
 import org.eclipse.smarthome.core.thing.Bridge;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.ThingStatus;
 import org.eclipse.smarthome.core.thing.binding.BaseBridgeHandler;
 import org.eclipse.smarthome.core.types.Command;
+import org.eclipse.smarthome.io.net.http.HttpUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -56,28 +54,20 @@ public class KVVBridgeHandler extends BaseBridgeHandler {
     }
 
     /**
-     * Returns the latest {@link DepartureResult}.
+     * Returns the latest {@link DepartureResult}. Returns {@code null} if the result could not be retrieved.
      *
      * @return the latest {@link DepartureResult}.
      */
     public @Nullable DepartureResult queryKVV(final KVVStationConfig config) {
         final String url = KVVBindingConstants.API_URL + "/departures/bystop/" + config.stationId + "?key="
                 + config.apiKey + "&maxInfos=" + config.maxTrains;
-        try {
-            final HttpURLConnection conn = (HttpURLConnection) new URL(url.toString()).openConnection();
-            conn.setRequestMethod("GET");
-            final BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
-            final StringBuilder json = new StringBuilder();
-            String line;
-            while ((line = in.readLine()) != null) {
-                json.append(line);
-            }
-
-            return new Gson().fromJson(json.toString(), DepartureResult.class);
-        } catch (IOException e) {
-            logger.error("Failed to connect to '{}'", url, e);
+        final RawType data = HttpUtil.downloadData(url, null, false, -1);
+        if (data == null) {
+            logger.warn("Failed to get departures from '{}'", url);
             return null;
         }
+
+        return new Gson().fromJson(new String(data.getBytes(), StandardCharsets.UTF_8), DepartureResult.class);
     }
 }
