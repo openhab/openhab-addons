1813b1e2848a074831e735e0a27d6c8f56e9a6e0
Fabio Possieri <fabio.possieri@gmail.com>
2020-06-23 08:58:52 +0200

    Refactored binding naming from smarther to bticinosmarther
    Just refactored the binding naming to bticinosmarther (smarther alone
    was too generic), as BTicino also provides other protocols that can be
    integrated over time in OH by other developers
    
    Signed-off-by: Fabio Possieri <fabio.possieri@gmail.com>

---
+++ bundles/org.openhab.binding.bticinosmarther/src/main/java/org/openhab/binding/bticinosmarther/internal/api/dto/Plants.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.bticinosmarther.internal.api.dto;
+
+import java.util.List;
+
+/**
+ * The {@code Plants} class defines the dto for Smarther API list of plants.
+ *
+ * @author Fabio Possieri - Initial contribution
+ */
+public class Plants {
+
+    private List<Plant> plants;
+
+    /**
+     * Returns the list of plants contained in this object.
+     *
+     * @return the list of plants
+     */
+    public List<Plant> getPlants() {
+        return plants;
+    }
+
+}
