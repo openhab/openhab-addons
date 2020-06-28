0e567c32b6765c01d83cf938e22dfb32eec6dea4
Fabio Possieri <fabio.possieri@gmail.com>
2020-06-28 23:01:00 +0200

    [bticinosmarther] Initial contribution (#7533)
    Signed-off-by: Fabio Possieri <fabio.possieri@gmail.com>
    Signed-off-by: CSchlipp <christian@schlipp.de>

---
+++ bundles/org.openhab.binding.bticinosmarther/src/main/java/org/openhab/binding/bticinosmarther/internal/api/exception/SmartherNotificationException.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.bticinosmarther.internal.api.exception;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Signals that a generic C2C Webhook notification issue with API gateway has occurred.
+ *
+ * @author Fabio Possieri - Initial contribution
+ */
+@NonNullByDefault
+public class SmartherNotificationException extends RuntimeException {
+
+    private static final long serialVersionUID = -634107708647244174L;
+
+    /**
+     * Constructs a {@code SmartherNotificationException} with the specified detail message.
+     *
+     * @param message
+     *            the error message returned from the API gateway
+     */
+    public SmartherNotificationException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a {@code SmartherNotificationException} with the specified detail message and cause.
+     *
+     * @param message
+     *            the error message returned from the API gateway
+     * @param cause
+     *            the cause (a null value is permitted, and indicates that the cause is nonexistent or unknown)
+     */
+    public SmartherNotificationException(String message, Throwable exception) {
+        super(message, exception);
+    }
+
+}
---
+++ bundles/org.openhab.binding.bticinosmarther/src/main/java/org/openhab/binding/bticinosmarther/internal/model/BridgeStatus.java
@@ -0,0 +1,128 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.bticinosmarther.internal.model;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@code BridgeStatus} class defines the internal status of a Smarther Bridge.
+ *
+ * @author Fabio Possieri - Initial contribution
+ */
+@NonNullByDefault
+public class BridgeStatus {
+
+    private long apiCallsHandled;
+    private long notificationsReceived;
+    private long notificationsRejected;
+
+    /**
+     * Constructs a new {@code BridgeStatus}.
+     */
+    public BridgeStatus() {
+        this.apiCallsHandled = 0;
+        this.notificationsReceived = 0;
+        this.notificationsRejected = 0;
+    }
+
+    /**
+     * Returns the total number of API gateway calls made by the bridge.
+     *
+     * @return the total number of API calls made.
+     */
+    public long getApiCallsHandled() {
+        return apiCallsHandled;
+    }
+
+    /**
+     * Increment the total number of API gateway calls made by the bridge.
+     *
+     * @return the total number of API calls made, after the increment.
+     */
+    public long incrementApiCallsHandled() {
+        return ++apiCallsHandled;
+    }
+
+    /**
+     * Sets the total number of API gateway calls made by the bridge.
+     *
+     * @param totalNumber
+     *            the total number of API calls to be set as made
+     */
+    public void setApiCallsHandled(long totalNumber) {
+        this.apiCallsHandled = totalNumber;
+    }
+
+    /**
+     * Returns the total number of module status notifications received by the bridge.
+     *
+     * @return the total number of received notifications.
+     */
+    public long getNotificationsReceived() {
+        return notificationsReceived;
+    }
+
+    /**
+     * Increment the total number of module status notifications received by the bridge.
+     *
+     * @return the total number of received notification, after the increment.
+     */
+    public long incrementNotificationsReceived() {
+        return ++notificationsReceived;
+    }
+
+    /**
+     * Sets the total number of module status notifications received by the bridge.
+     *
+     * @param totalNumber
+     *            the total number of notifications to be set as received
+     */
+    public void setNotificationsReceived(long totalNumber) {
+        this.notificationsReceived = totalNumber;
+    }
+
+    /**
+     * Returns the total number of module status notifications rejected by the bridge.
+     *
+     * @return the total number of rejected notifications.
+     */
+    public long getNotificationsRejected() {
+        return notificationsRejected;
+    }
+
+    /**
+     * Increment the total number of module status notifications rejected by the bridge.
+     *
+     * @return the total number of rejected notification, after the increment.
+     */
+    public long incrementNotificationsRejected() {
+        return ++notificationsRejected;
+    }
+
+    /**
+     * Sets the total number of module status notifications rejected by the bridge.
+     *
+     * @param totalNumber
+     *            the total number of notifications to be set as rejected
+     */
+    public void setNotificationsRejected(long totalNumber) {
+        this.notificationsRejected = totalNumber;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("apiCallsHandled=%s, notifsReceived=%s, notifsRejected=%s", apiCallsHandled,
+                notificationsReceived, notificationsRejected);
+    }
+
+}
