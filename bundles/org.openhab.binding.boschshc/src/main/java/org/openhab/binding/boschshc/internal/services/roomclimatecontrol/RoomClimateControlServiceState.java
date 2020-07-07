375b2d13730a22082a0be06e4664494c909a390e
Christian Oeing <christian.oeing@scalamat.de>
2020-07-07 23:36:42 +0200

    Handle command to set setpoint temperature and move conversion from service state data to thing states into service state classes
    Signed-off-by: Christian Oeing <christian.oeing@slashgames.org>

--- bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/services/roomclimatecontrol/RoomClimateControlServiceState.java
+++ bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/services/roomclimatecontrol/RoomClimateControlServiceState.java
@@ -1,11 +1,24 @@
 package org.openhab.binding.boschshc.internal.services.roomclimatecontrol;
 
+import javax.measure.quantity.Temperature;
+
+import org.eclipse.smarthome.core.library.types.QuantityType;
+import org.eclipse.smarthome.core.types.State;
 import org.openhab.binding.boschshc.internal.services.BoschSHCServiceState;
+
+import tec.uom.se.unit.Units;
 
 public class RoomClimateControlServiceState extends BoschSHCServiceState {
 
+    private static final String Type = "climateControlState";
+
     public RoomClimateControlServiceState() {
-        super("climateControlState");
+        super(Type);
+    }
+
+    public RoomClimateControlServiceState(double setpointTemperature) {
+        super(Type);
+        this.setpointTemperature = setpointTemperature;
     }
 
     /**
@@ -14,5 +27,14 @@ public class RoomClimateControlServiceState extends BoschSHCServiceState {
      * @apiNote Min: 5.0, Max: 30.0.
      * @apiNote Can be set in 0.5 steps.
      */
-    public Double setpointTemperature;
+    public double setpointTemperature;
+
+    /**
+     * Desired temperature state to set for a thing.
+     * 
+     * @return Desired temperature state to set for a thing.
+     */
+    public State getSetpointTemperatureState() {
+        return new QuantityType<Temperature>(this.setpointTemperature, Units.CELSIUS);
+    }
 }
--- bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/services/valvetappet/ValveTappetServiceState.java
+++ bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/services/valvetappet/ValveTappetServiceState.java
@@ -1,5 +1,7 @@
 package org.openhab.binding.boschshc.internal.services.valvetappet;
 
+import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.types.State;
 import org.openhab.binding.boschshc.internal.services.BoschSHCServiceState;
 
 public class ValveTappetServiceState extends BoschSHCServiceState {
@@ -11,4 +13,11 @@ public class ValveTappetServiceState extends BoschSHCServiceState {
      * Current open percentage of valve tappet (0 [closed] - 100 [open]).
      */
     public Integer position;
+
+    /**
+     * Current position state of valve tappet.
+     */
+    public State getPositionState() {
+        return new DecimalType(this.position);
+    }
 }
