1a78745ced945ca8cd388f4a26154f55da25a167
Sami Salonen <ssalonen@gmail.com>
2020-07-13 00:37:47 +0300

    [dynamodb] tests for paging (#8004)
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- bundles/org.openhab.persistence.dynamodb/src/test/java/org/openhab/persistence/dynamodb/internal/BaseIntegrationTest.java
+++ bundles/org.openhab.persistence.dynamodb/src/test/java/org/openhab/persistence/dynamodb/internal/BaseIntegrationTest.java
@@ -195,7 +195,10 @@ public class BaseIntegrationTest {
         }
 
         service.activate(null, config);
+        clearData();
+    }
 
+    protected static void clearData() {
         // Clear data
         for (String table : new String[] { "dynamodb-integration-tests-bigdecimal",
                 "dynamodb-integration-tests-string" }) {
@@ -203,6 +206,8 @@ public class BaseIntegrationTest {
                 service.getDb().getDynamoClient().deleteTable(table);
                 service.getDb().getDynamoDB().getTable(table).waitForDelete();
             } catch (ResourceNotFoundException e) {
+            } catch (InterruptedException e) {
+                LOGGER.warn("Interrupted! Table might not have been deleted");
             }
         }
     }
