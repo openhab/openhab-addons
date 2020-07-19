e7a716c2824a1fafa51b59db449efb2fca00cf9c
Georgios Moutsos <50378548+jossuar@users.noreply.github.com>
2020-07-19 20:48:54 +0300

    [caddx] Initial contribution (#6430)
    Signed-off-by: Georgios Moutsos <georgios.moutsos@gmail.com>
    Signed-off-by: CSchlipp <christian@schlipp.de>

---
+++ bundles/org.openhab.binding.caddx/src/main/java/org/openhab/binding/caddx/internal/config/CaddxPartitionConfiguration.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.caddx.internal.config;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Configuration class for the Caddx Partition Thing.
+ *
+ * @author Georgios Moutsos - Initial contribution
+ */
+@NonNullByDefault
+public class CaddxPartitionConfiguration {
+
+    // Partition Thing constants
+    public static final String PARTITION_NUMBER = "partitionNumber";
+
+    /**
+     * The Partition Number. Can be in the range of 1-8. This is a required parameter for a partition.
+     */
+    private int partitionNumber;
+
+    /**
+     * The User Number of the user that will execute commands against the partition.
+     */
+    private int userNumber;
+
+    public int getPartitionNumber() {
+        return partitionNumber;
+    }
+
+    public int getUserNumber() {
+        return userNumber;
+    }
+}
