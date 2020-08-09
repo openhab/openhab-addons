19482e76b80801bd543c7c615b5099f87cfe590b
Andrew Schofield <the.uncle.fungus@gmail.com>
2020-08-09 20:28:31 +0100

    [draytonwiser] Drayton Wiser Binding initial contribution (#3168)
    Also-by: Hilbrand Bouwkamp <hilbrand@h72.nl>
    Signed-off-by: Andrew Schofield <the.uncle.fungus@gmail.com>
    Signed-off-by: Sven Strohschein <sven.strohschein@gmail.com>

---
+++ bundles/org.openhab.binding.draytonwiser/src/main/java/org/openhab/binding/draytonwiser/internal/handler/DraytonWiserPropertyHelper.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.draytonwiser.internal.handler;
+
+import static org.openhab.binding.draytonwiser.internal.DraytonWiserBindingConstants.PROP_SERIAL_NUMBER;
+
+import java.util.Map;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.binding.draytonwiser.internal.model.DeviceDTO;
+
+/**
+ *
+ * @author Andrew Schofield - Initial contribution
+ * @author Hilbrand Bouwkamp - Put all device property setting in a separate class
+ */
+@NonNullByDefault
+public final class DraytonWiserPropertyHelper {
+
+    private DraytonWiserPropertyHelper() {
+        // helper class
+    }
+
+    public static void setPropertiesWithSerialNumber(final DeviceDTO device, final Map<String, Object> properties) {
+        properties.put(PROP_SERIAL_NUMBER, device.getSerialNumber());
+        setGeneralDeviceProperties(device, properties);
+    }
+
+    public static void setGeneralDeviceProperties(final DeviceDTO device,
+            final Map<String, ? super String> properties) {
+        properties.put("Device Type", device.getProductIdentifier());
+        properties.put("Firmware Version", device.getActiveFirmwareVersion());
+        properties.put("Manufacturer", device.getManufacturer());
+        properties.put("Model", device.getModelIdentifier());
+    }
+}
---
+++ bundles/org.openhab.binding.draytonwiser/src/main/java/org/openhab/binding/draytonwiser/internal/model/DhcpStatusDTO.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.draytonwiser.internal.model;
+
+/**
+ * @author Andrew Schofield - Initial contribution
+ */
+public class DhcpStatusDTO {
+
+    private String status;
+    private Long leaseStartTime;
+    private Long leaseTime;
+    private String iPv4Address;
+    private String iPv4SubnetMask;
+    private String iPv4DefaultGateway;
+    private String iPv4PrimaryDNS;
+    private String iPv4SecondaryDNS;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Long getLeaseStartTime() {
+        return leaseStartTime;
+    }
+
+    public Long getLeaseTime() {
+        return leaseTime;
+    }
+
+    public String getIPv4Address() {
+        return iPv4Address;
+    }
+
+    public String getIPv4SubnetMask() {
+        return iPv4SubnetMask;
+    }
+
+    public String getIPv4DefaultGateway() {
+        return iPv4DefaultGateway;
+    }
+
+    public String getIPv4PrimaryDNS() {
+        return iPv4PrimaryDNS;
+    }
+
+    public String getIPv4SecondaryDNS() {
+        return iPv4SecondaryDNS;
+    }
+}
