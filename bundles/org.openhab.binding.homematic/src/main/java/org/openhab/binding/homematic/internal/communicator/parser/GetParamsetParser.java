6b03b8bc781d520e2c3e9e041ae209f189a04ba9
Martin Herbst <develop@mherbst.de>
2020-08-28 20:39:04 +0200

    Another exception for data point added
    according to:
    https://github.com/openhab/openhab-addons/issues/5048#issuecomment-678940460
    
    
    Signed-off-by: Martin Herbst <develop@mherbst.de>

--- bundles/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/communicator/parser/GetParamsetParser.java
+++ bundles/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/communicator/parser/GetParamsetParser.java
@@ -59,7 +59,8 @@ public class GetParamsetParser extends CommonRpcParser<Object[], Void> {
                 // suppress warning for this datapoint due wrong CCU metadata
                 String deviceType = channel.getDevice().getType();
                 boolean isHmSenMdirNextTrans = dpInfo.getName().equals("NEXT_TRANSMISSION")
-                        && (deviceType.startsWith("HM-Sen-MDIR-O") || deviceType.startsWith("HM-Sen-MDIR-WM55"));
+                        && (deviceType.startsWith("HM-Sen-MDIR-O") || deviceType.startsWith("HM-Sen-MDIR-WM55")
+                                || deviceType.startsWith("HM-Sec-MDIR-2"));
                 if (!isHmSenMdirNextTrans) {
                     if (dpInfo.getParamsetType() == HmParamsetType.MASTER
                             && channel.getDevice().getHmInterface() == HmInterface.HMIP) {
