b61e17dfba574b96e9b4d518098fbb13fe6bcdb4
Paul Frank <pail@gmx.net>
2020-08-28 08:13:47 +0200

    initial commit of the skeletton
    Signed-off-by: Paul Frank <pail@gmx.net>

---
+++ bundles/org.openhab.binding.mystrom/src/main/java/org/openhab/binding/mystrom/internal/MyStromConfiguration.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.mystrom.internal;
+
+import static org.openhab.binding.mystrom.internal.MyStromBindingConstants.DEFAULT_REFRESH_RATE;
+
+
+/**
+ * The {@link MyStromConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Paul Frank - Initial contribution
+ */
+public class MyStromConfiguration {
+
+    /**
+     * Hostname of the OpenSprinkler API.
+     */
+    public String hostname = null;
+    /**
+     * Number of seconds in between refreshes from the OpenSprinkler device.
+     */
+    public int refresh = DEFAULT_REFRESH_RATE;    
+
+}
