7881a3469c41a5311f8d3926deb455ad918c1eb6
Pascal Bies <pascal@pakab.de>
2020-08-29 12:41:04 +0200

    fix control flow

--- bundles/org.openhab.binding.dlinksmarthome/src/main/java/org/openhab/binding/dlinksmarthome/internal/handler/DLinkSmartPlugHandler.java
+++ bundles/org.openhab.binding.dlinksmarthome/src/main/java/org/openhab/binding/dlinksmarthome/internal/handler/DLinkSmartPlugHandler.java
@@ -27,7 +27,6 @@ import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import javax.measure.Unit;
 import javax.naming.CommunicationException;
-import javax.security.auth.login.LoginException;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -35,6 +34,7 @@ import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.HttpRequest;
 import org.eclipse.jetty.client.api.ContentResponse;
 import org.eclipse.jetty.client.util.BytesContentProvider;
+import org.eclipse.smarthome.config.core.Configuration;
 import org.eclipse.smarthome.core.library.types.OnOffType;
 import org.eclipse.smarthome.core.library.types.QuantityType;
 import org.eclipse.smarthome.core.library.unit.SIUnits;
@@ -72,16 +72,17 @@ public class DLinkSmartPlugHandler extends BaseThingHandler {
     private static final String USER = "admin";
     private final HttpClient httpClient;
     private ScheduledFuture<?> pollFuture;
+    private boolean invalid_configuration = false;
+    Authentication authentication;
     private static final int DETECT_POLL_S = 1;
     private final Runnable poller = new Runnable() {
         @Override
         public void run() {
-            try {
+            if (authenticate()) {
+                updateStatus(ThingStatus.ONLINE);
                 for (final String channelID : DLinkSmartHomeBindingConstants.SMART_PLUG_CHANNEL_IDS) {
                     updateState(getChannelUID(channelID), getNewChannelState(channelID));
                 }
-            } catch (CommunicationException | LoginException e) {
-                // thing status is updated and error is logged when the exception is thrown.
             }
         }
     };
@@ -100,13 +101,6 @@ public class DLinkSmartPlugHandler extends BaseThingHandler {
         super(thing);
         httpClient = new HttpClient();
         url = "http://" + ip + "/HNAP1/";
-        try {
-            httpClient.start();
-        } catch (Exception e) {
-            this.updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.HANDLER_INITIALIZING_ERROR,
-                    "Unexpected internal error.");
-            logger.error("Failed to start http client.");
-        }
     }
 
     @Override
@@ -119,17 +113,15 @@ public class DLinkSmartPlugHandler extends BaseThingHandler {
 
     @Override
     public void handleCommand(final ChannelUID channelUID, final Command command) {
-        if (command == RefreshType.REFRESH) {
-            try {
+        if (authenticate()) {
+            if (command == RefreshType.REFRESH) {
                 updateState(channelUID, getNewChannelState(channelUID.getId()));
-            } catch (CommunicationException | LoginException e) {
-                // thing status is updated and error is logged when the exception is thrown.
-            }
-        } else if (channelUID.equals(getChannelUID(DLinkSmartHomeBindingConstants.STATE))) {
-            try {
-                setState((OnOffType) command);
-            } catch (ClassCastException e) {
-                logger.error("Unexpected command type for channel '{}'.", DLinkSmartHomeBindingConstants.STATE);
+            } else if (channelUID.equals(getChannelUID(DLinkSmartHomeBindingConstants.STATE))) {
+                try {
+                    setState((OnOffType) command);
+                } catch (ClassCastException e) {
+                    logger.error("Unexpected command type for channel '{}'.", DLinkSmartHomeBindingConstants.STATE);
+                }
             }
         }
     }
@@ -141,33 +133,46 @@ public class DLinkSmartPlugHandler extends BaseThingHandler {
         this.ip = config.ipAddress;
         this.url = "http://" + ip + "/HNAP1/";
         try {
+            httpClient.start();
+        } catch (Exception e) {
+            this.updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.HANDLER_INITIALIZING_ERROR,
+                    "Unexpected internal error.");
+            logger.error("Failed to start http client.");
+        }
+        try {
             pollFuture = scheduler.scheduleWithFixedDelay(poller, 0, DETECT_POLL_S, TimeUnit.SECONDS);
         } catch (Exception e) {
             logger.error("Failed to poll state.");
         }
     }
 
-    private State getCurrentConsuption() throws CommunicationException, LoginException {
+    private State getCurrentConsuption() {
         return getQuantity("GetCurrentPowerConsumption", "CurrentConsumption", "2", SmartHomeUnits.WATT);
     }
 
-    private State getTotalConsumption() throws CommunicationException, LoginException {
+    private State getTotalConsumption() {
         return getQuantity("GetPMWarningThreshold", "TotalConsumption", "2", SmartHomeUnits.KILOWATT_HOUR);
     }
 
-    private State getTemperature() throws CommunicationException, LoginException {
+    private State getTemperature() {
         return getQuantity("GetCurrentTemperature", "CurrentTemperature", "3", SIUnits.CELSIUS);
     }
 
-    private State getState() throws CommunicationException, LoginException {
-        final String state = getString("GetSocketSettings", "OPStatus", "1").toLowerCase();
-        if ("true".contentEquals(state)) {
-            return OnOffType.ON;
-        } else if ("false".equals(state)) {
-            return OnOffType.OFF;
-        } else {
-            return UnDefType.UNDEF;
+    private State getState() {
+        try {
+            String state = soapAction("GetSocketSettings", "OPStatus", moduleParameters("1")).toLowerCase();
+            if ("true".contentEquals(state)) {
+                return OnOffType.ON;
+            } else if ("false".equals(state)) {
+                return OnOffType.OFF;
+            } else {
+                logger.error("Unexpected state: " + state);
+            }
+        } catch (CommunicationException e) {
+            logger.error("Failed to get state (communication error).");
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR);
         }
+        return UnDefType.UNDEF;
     }
 
     private void setState(final OnOffType state) {
@@ -189,48 +194,67 @@ public class DLinkSmartPlugHandler extends BaseThingHandler {
         return "<ModuleID>" + module + "</ModuleID>";
     }
 
-    private State getQuantity(final String action, final String name, final String module, final Unit<?> unit)
-            throws CommunicationException, LoginException {
-        final String s = getString(action, name, module);
-        if (s == null) {
-            return UnDefType.UNDEF;
-        } else {
+    private State getQuantity(final String action, final String name, final String module, final Unit<?> unit) {
+        try {
+            final String s = soapAction(action, name, moduleParameters(module));
             try {
-                return new QuantityType<>(Double.valueOf(s), unit);
+                if (s == null) {
+                    return UnDefType.UNDEF;
+                } else {
+                    return new QuantityType<>(Double.valueOf(s), unit);
+                }
             } catch (NumberFormatException e) {
-                return UnDefType.UNDEF;
+                logger.error("Unexpected number format: " + s);
             }
+        } catch (CommunicationException e) {
+            logger.error("Failed to get quantity (communication error).");
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR);
         }
-    }
-
-    private String getString(final String action, final String name, final String module)
-            throws CommunicationException, LoginException {
-        return soapAction(action, name, moduleParameters(module));
+        return UnDefType.UNDEF;
     }
 
     private String soapAction(final String action, final String name, final String params)
-            throws CommunicationException, LoginException {
-        Document document = post(action, requestBody(action, params), authenticate());
+            throws CommunicationException {
+        Document document = post(action, requestBody(action, params), authentication);
         return getElementByTagName(document, name);
     }
 
-    public Authentication authenticate() throws LoginException, CommunicationException {
-        Document document = post("Login", INITIAL_AUTH_PAYLOAD);
-        final String challenge = getElementByTagName(document, "Challenge");
-        final String cookie = getElementByTagName(document, "Cookie");
-        final String publicKey = getElementByTagName(document, "PublicKey");
-        final String privateKey = calculateHMAC(challenge, publicKey + pin);
-        final String loginPassword = calculateHMAC(challenge, privateKey);
+    public boolean authenticate() {
+        if (invalid_configuration) {
+            // If the configuration (i.e., PIN) is invalid, trying to long in does not make sense.
+            authentication = null; // it should be null already, but for sake of consistency...
+            return false;
+        } else if (authentication != null) {
+            // We are already authenticated, nothing to do.
+            // If you want to force re-authentication, set this.authentication = null.
+            return true;
+        }
 
-        Authentication authentication = new Authentication(privateKey, cookie);
-        Document document2 = post("Login", authPayload(loginPassword), authentication);
-        final String loginStatus = getElementByTagName(document2, "LoginResult").toLowerCase();
-        if ("success".equals(loginStatus)) {
-            updateStatus(ThingStatus.ONLINE);
-            return authentication;
-        } else {
+        try {
+            Document document = post("Login", INITIAL_AUTH_PAYLOAD);
+            final String challenge = getElementByTagName(document, "Challenge");
+            final String cookie = getElementByTagName(document, "Cookie");
+            final String publicKey = getElementByTagName(document, "PublicKey");
+            final String privateKey = calculateHMAC(challenge, publicKey + pin);
+            final String loginPassword = calculateHMAC(challenge, privateKey);
+
+            authentication = new Authentication(privateKey, cookie);
+            Document document2 = post("Login", authPayload(loginPassword), authentication);
+            final String loginStatus = getElementByTagName(document2, "LoginResult").toLowerCase();
+            if ("success".equals(loginStatus)) {
+                updateStatus(ThingStatus.ONLINE);
+                invalid_configuration = false;
+                return true;
+            } else {
+                updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR, "Failed to authenticate.");
+                invalid_configuration = true;
+                authentication = null;
+                return false;
+            }
+        } catch (CommunicationException e) {
+            authentication = null;
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR, "Failed to authenticate.");
-            throw new LoginException("Failed to authenticate.");
+            return false;
         }
     }
 
@@ -241,6 +265,7 @@ public class DLinkSmartPlugHandler extends BaseThingHandler {
     private Document post(final String action, final String payload, final Authentication authentication)
             throws CommunicationException {
         HttpRequest request = (HttpRequest) httpClient.newRequest(url);
+        request.timeout(5, TimeUnit.SECONDS);
         request.method("POST");
         request.header("Content-Type", "\"text/xml; charset=utf-8\"");
         final String actionUrl = "\"http://purenetworks.com/HNAP1/" + action + "\"";
@@ -252,8 +277,8 @@ public class DLinkSmartPlugHandler extends BaseThingHandler {
         request.content(new BytesContentProvider(payload.getBytes()));
         try {
             final ContentResponse response = request.send();
-            return DocumentBuilderFactory.newInstance().newDocumentBuilder()
-                    .parse(new ByteArrayInputStream(response.getContent()));
+            final byte[] content = response.getContent();
+            return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new ByteArrayInputStream(content));
         } catch (InterruptedException | TimeoutException | ExecutionException | SAXException | IOException e) {
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR, e.getMessage());
             throw new CommunicationException("Post request failed: " + e.getMessage());
@@ -268,6 +293,7 @@ public class DLinkSmartPlugHandler extends BaseThingHandler {
         final int n = items.getLength();
         if (n != 1) {
             logger.error("Failed to get element with tag name '{}'.", name);
+            authentication = null; // force re-authentication.
             throw new CommunicationException(
                     "Expected exactly one node with name '" + name + "', but got " + n + " many.");
         } else {
@@ -324,7 +350,7 @@ public class DLinkSmartPlugHandler extends BaseThingHandler {
         }
     }
 
-    public State getNewChannelState(final String channelID) throws CommunicationException, LoginException {
+    public State getNewChannelState(final String channelID) {
         if (thing.getStatus().equals(ThingStatus.ONLINE)) {
             switch (channelID) {
                 case DLinkSmartHomeBindingConstants.CURRENT_CONSUMPTION:
@@ -342,6 +368,18 @@ public class DLinkSmartPlugHandler extends BaseThingHandler {
         } else {
             return UnDefType.UNDEF;
         }
+    }
+
+    @Override
+    protected void updateConfiguration(Configuration configuration) {
+        super.updateConfiguration(configuration);
+
+        // force re-authentication before the next action.
+        authentication = null;
+
+        // the configuration may be correct.
+        // if it was incorrect, the `authenticate()`-method will set the flag.
+        invalid_configuration = false;
     }
 
     static String requestBody(final String action, final String params) {
