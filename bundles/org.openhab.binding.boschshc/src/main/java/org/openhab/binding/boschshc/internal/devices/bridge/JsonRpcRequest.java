72be34503755f661fe28ceb2e3900ab3c3dcce33
Christian Oeing <christian.oeing@slashgames.org>
2020-08-31 20:02:17 +0200

    #45 Increase year in copyright headers
    Signed-off-by: Christian Oeing <christian.oeing@slashgames.org>

--- bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/devices/bridge/JsonRpcRequest.java
+++ bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/devices/bridge/JsonRpcRequest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2019 Contributors to the openHAB project
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -59,5 +59,4 @@ class JsonRpcRequest {
     public String jsonrpc;
     public String method;
     public String[] params;
-
 }
--- bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/devices/bridge/SubscribeResult.java
+++ bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/devices/bridge/SubscribeResult.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2019 Contributors to the openHAB project
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -32,5 +32,4 @@ public class SubscribeResult {
     public @Nullable String getJsonrpc() {
         return this.jsonrpc;
     }
-
 }
