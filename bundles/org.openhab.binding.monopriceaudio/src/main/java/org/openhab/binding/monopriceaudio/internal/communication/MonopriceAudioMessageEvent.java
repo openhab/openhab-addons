d574e37c6ab8ab14bfe6371ec7afa0a74e24b24d
mlobstein <michael.lobstein@gmail.com>
2020-09-03 01:31:35 -0500

    [monopriceaudio] Monoprice Whole House Amplifier Binding - initial contribution (#7371)
    Signed-off-by: Michael Lobstein <michael.lobstein@gmail.com>

---
+++ bundles/org.openhab.binding.monopriceaudio/src/main/java/org/openhab/binding/monopriceaudio/internal/communication/MonopriceAudioMessageEvent.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.monopriceaudio.internal.communication;
+
+import java.util.EventObject;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * MonopriceAudio event used to notify changes coming from messages received from the MonopriceAudio device
+ *
+ * @author Michael Lobstein - Initial contribution
+ */
+@NonNullByDefault
+public class MonopriceAudioMessageEvent extends EventObject {
+
+    private static final long serialVersionUID = 1L;
+    private final String key;
+    private final String value;
+
+    public MonopriceAudioMessageEvent(Object source, String key, String value) {
+        super(source);
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
