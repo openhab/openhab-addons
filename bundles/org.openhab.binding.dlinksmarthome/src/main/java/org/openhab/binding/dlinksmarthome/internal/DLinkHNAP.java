8b427c673585d141a84f09342a33e6338c269945
Pascal Bies <pascal.bies@k-lens.de>
2020-09-12 17:22:12 +0200

    [DLinkSmartHome] add header, fix modifier order
    Signed-off-by: Pascal Bies <pascal.bies@k-lens.de>

--- bundles/org.openhab.binding.dlinksmarthome/src/main/java/org/openhab/binding/dlinksmarthome/internal/DLinkHNAP.java
+++ bundles/org.openhab.binding.dlinksmarthome/src/main/java/org/openhab/binding/dlinksmarthome/internal/DLinkHNAP.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.dlinksmarthome.internal;
 
 import java.io.ByteArrayInputStream;
@@ -50,9 +62,9 @@ import org.xml.sax.SAXException;
  */
 public abstract class DLinkHNAP {
 
-    abstract public void poll();
+    protected abstract void poll();
 
-    abstract public void handleCommandAuthenticated(final ChannelUID channelUID, final Command command);
+    protected abstract void handleCommandAuthenticated(final ChannelUID channelUID, final Command command);
 
     protected void updateStatus(ThingStatus status) {
         updateStatus(status, ThingStatusDetail.NONE);
