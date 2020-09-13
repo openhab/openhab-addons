186da7060186e40e393702ef22ea5a6c3b5adea5
Christoph Weitkamp <github@christophweitkamp.de>
2020-09-13 18:02:52 +0200

    [kodi] Changed 'systemcommand' channel to command options (#7369)
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

--- bundles/org.openhab.binding.kodi/src/main/java/org/openhab/binding/kodi/internal/KodiDynamicCommandDescriptionProvider.java
+++ bundles/org.openhab.binding.kodi/src/main/java/org/openhab/binding/kodi/internal/KodiDynamicCommandDescriptionProvider.java
@@ -13,23 +13,23 @@
 package org.openhab.binding.kodi.internal;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.smarthome.core.thing.binding.BaseDynamicStateDescriptionProvider;
+import org.eclipse.smarthome.core.thing.binding.BaseDynamicCommandDescriptionProvider;
 import org.eclipse.smarthome.core.thing.i18n.ChannelTypeI18nLocalizationService;
-import org.eclipse.smarthome.core.thing.type.DynamicStateDescriptionProvider;
+import org.eclipse.smarthome.core.thing.type.DynamicCommandDescriptionProvider;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * Dynamic provider of state options while leaving other state description fields as original.
+ * Dynamic provider of command options.
  *
  * @author Christoph Weitkamp - Initial contribution
  */
-@Component(service = { DynamicStateDescriptionProvider.class, KodiDynamicStateDescriptionProvider.class })
+@Component(service = { DynamicCommandDescriptionProvider.class, KodiDynamicCommandDescriptionProvider.class })
 @NonNullByDefault
-public class KodiDynamicStateDescriptionProvider extends BaseDynamicStateDescriptionProvider {
+public class KodiDynamicCommandDescriptionProvider extends BaseDynamicCommandDescriptionProvider {
     @Activate
-    public KodiDynamicStateDescriptionProvider(
+    public KodiDynamicCommandDescriptionProvider(
             final @Reference ChannelTypeI18nLocalizationService channelTypeI18nLocalizationService) {
         this.channelTypeI18nLocalizationService = channelTypeI18nLocalizationService;
     }
