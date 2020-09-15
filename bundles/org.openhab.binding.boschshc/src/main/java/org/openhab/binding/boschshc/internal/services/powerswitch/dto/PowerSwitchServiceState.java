6cd4f2fb677db3fcc792facbf380b2bce5a93cc7
Christian Oeing <christian.oeing@slashgames.org>
2020-09-15 21:09:56 +0200

    Migrate PowerSwitch to new service architecture to get rid of BoschSHCBridgeHandler.updateSwitchState
    Signed-off-by: Christian Oeing <christian.oeing@slashgames.org>
    Signed-off-by: Gerd Zanker <gerd.zanker@web.de>

---
+++ bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/services/powerswitch/dto/PowerSwitchServiceState.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.boschshc.internal.services.powerswitch.dto;
+
+import org.openhab.binding.boschshc.internal.services.dto.BoschSHCServiceState;
+import org.openhab.binding.boschshc.internal.services.powerswitch.PowerSwitchState;
+
+/**
+ * Represents the state of a power switch device as reported from the Smart Home Controller
+ *
+ * @author Stefan Kästle - Initial contribution
+ * @author Christian Oeing - Adjustments to match general service state structure
+ */
+public class PowerSwitchServiceState extends BoschSHCServiceState {
+
+    public PowerSwitchServiceState() {
+        super("powerSwitchState");
+    }
+
+    /**
+     * Current state of power switch.
+     */
+    public PowerSwitchState switchState;
+}
