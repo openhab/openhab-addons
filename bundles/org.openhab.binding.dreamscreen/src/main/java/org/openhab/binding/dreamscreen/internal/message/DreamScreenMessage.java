cea989d98e66606af9ee89b8510bfa5cf606f74b
d8ahazard <d8ahazard@gmail.com>
2020-09-17 11:55:21 -0500

    Maven/spotless cleanup
    Signed-off-by: d8ahazard <d8ahazard@gmail.com>

--- bundles/org.openhab.binding.dreamscreen/src/main/java/org/openhab/binding/dreamscreen/internal/message/DreamScreenMessage.java
+++ bundles/org.openhab.binding.dreamscreen/src/main/java/org/openhab/binding/dreamscreen/internal/message/DreamScreenMessage.java
@@ -1,13 +1,13 @@
 /**
  * Copyright (c) 2010-2020 Contributors to the openHAB project
- *
+ * <p>
  * See the NOTICE file(s) distributed with this work for additional
  * information.
- *
+ * <p>
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
- *
+ * <p>
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.openhab.binding.dreamscreen.internal.message;
@@ -17,7 +17,6 @@ import java.net.InetAddress;
 import java.nio.ByteBuffer;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.smarthome.core.types.RefreshType;
 
 /**
  * The {@link DreamScreenMessage} implements general message handling.
