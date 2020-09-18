41e5ff344331638c11ed51a9ff7834d8cf47d4aa
Marcus Better <marcusb@users.noreply.github.com>
2020-09-18 08:40:59 -0400

    [upb] UPB Binding initial contribution (#6742)
    Signed-off-by: Marcus Better <marcus@better.se>

---
+++ bundles/org.openhab.binding.upb/src/main/java/org/openhab/binding/upb/internal/UPBDevice.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.upb.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * A device on the UPB network.
+ *
+ * @author Marcus Better - Initial contribution
+ *
+ */
+@NonNullByDefault
+public class UPBDevice {
+    private final byte networkId;
+    private final byte unitId;
+
+    private DeviceState state = DeviceState.INITIALIZING;
+
+    public enum DeviceState {
+        INITIALIZING,
+        ALIVE,
+        DEAD,
+        FAILED
+    }
+
+    public UPBDevice(final byte networkId, final byte unitId) {
+        this.networkId = networkId;
+        this.unitId = unitId;
+    }
+
+    public byte getNetworkId() {
+        return networkId;
+    }
+
+    public byte getUnitId() {
+        return unitId;
+    }
+
+    public DeviceState getState() {
+        return state;
+    }
+
+    public void setState(final DeviceState state) {
+        this.state = state;
+    }
+}
