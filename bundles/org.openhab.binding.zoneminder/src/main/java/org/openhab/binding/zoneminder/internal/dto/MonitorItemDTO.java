20d9a6d6d8c1b8ac6de1c167ffdf0de1af49b64d
Mark Hilbush <mark@hilbush.com>
2020-09-21 16:00:02 -0400

    Replacement for zoneminder binding
    Signed-off-by: Mark Hilbush <mark@hilbush.com>

---
+++ bundles/org.openhab.binding.zoneminder/src/main/java/org/openhab/binding/zoneminder/internal/dto/MonitorItemDTO.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.zoneminder.internal.dto;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * The {@link MonitorItemDTO} holds monitor and monitor status information.
+ *
+ * @author Mark Hilbush - Initial contribution
+ */
+public class MonitorItemDTO {
+
+    /**
+     * Information about how the monitor is defined in Zoneminder
+     */
+    @SerializedName("Monitor")
+    public MonitorDTO monitor;
+
+    /**
+     * Current status of the monitor in Zoneminder
+     */
+    @SerializedName("Monitor_Status")
+    public MonitorStatusDTO monitorStatus;
+}
