c68cbfad477d5444b5fc39a535bb193622e44250
Oleg Davydyuk <ilveann@gmail.com>
2020-10-05 18:52:16 +0300

    Support AirVisual pro version
    Signed-off-by: Oleg Davydyuk <ilveann@gmail.com>

--- bundles/org.openhab.binding.airvisualnode/src/main/java/org/openhab/binding/airvisualnode/internal/handler/AirVisualNodeHandler.java
+++ bundles/org.openhab.binding.airvisualnode/src/main/java/org/openhab/binding/airvisualnode/internal/handler/AirVisualNodeHandler.java
@@ -47,7 +47,10 @@ import org.eclipse.smarthome.core.types.RefreshType;
 import org.eclipse.smarthome.core.types.State;
 import org.eclipse.smarthome.core.types.UnDefType;
 import org.openhab.binding.airvisualnode.internal.config.AirVisualNodeConfig;
-import org.openhab.binding.airvisualnode.internal.json.NodeData;
+import org.openhab.binding.airvisualnode.internal.json.MeasurementsInterface;
+import org.openhab.binding.airvisualnode.internal.json.NodeDataInterface;
+import org.openhab.binding.airvisualnode.internal.json.airvisual.NodeData;
+import org.openhab.binding.airvisualnode.internal.json.airvisualpro.ProNodeData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -85,7 +88,9 @@ public class AirVisualNodeHandler extends BaseThingHandler {
 
     private String nodeShareName;
 
-    private NodeData nodeData;
+    private NodeDataInterface nodeData;
+
+    private boolean isProVersion;
 
     public AirVisualNodeHandler(Thing thing) {
         super(thing);
@@ -115,6 +120,8 @@ public class AirVisualNodeHandler extends BaseThingHandler {
         this.nodeShareName = config.share;
 
         this.refreshInterval = config.refresh * 1000L;
+
+        this.isProVersion = config.isProVersion;
 
         schedulePoll();
     }
@@ -164,7 +171,14 @@ public class AirVisualNodeHandler extends BaseThingHandler {
 
     private void pollNode() throws IOException {
         String jsonData = getNodeJsonData();
-        NodeData currentNodeData = gson.fromJson(jsonData, NodeData.class);
+
+        NodeDataInterface currentNodeData;
+        if (isProVersion) {
+            currentNodeData = gson.fromJson(jsonData, ProNodeData.class);
+        } else {
+            currentNodeData = gson.fromJson(jsonData, NodeData.class);
+        }
+
         if (nodeData == null || currentNodeData.getStatus().getDatetime() > nodeData.getStatus().getDatetime()) {
             nodeData = currentNodeData;
             // Update all channels from the updated Node data
@@ -190,7 +204,7 @@ public class AirVisualNodeHandler extends BaseThingHandler {
         }
     }
 
-    private State getChannelState(String channelId, NodeData nodeData) {
+    private State getChannelState(String channelId, NodeDataInterface nodeData) {
         State state = UnDefType.UNDEF;
 
         // Handle system channel IDs separately, because 'switch/case' expressions must be constant expressions
@@ -200,24 +214,33 @@ public class AirVisualNodeHandler extends BaseThingHandler {
             state = new DecimalType(
                     BigDecimal.valueOf(Math.max(0, nodeData.getStatus().getWifiStrength() - 1)).longValue());
         } else {
+
+            MeasurementsInterface measurements = nodeData.getMeasurements();
             // Handle binding-specific channel IDs
             switch (channelId) {
                 case CHANNEL_CO2:
-                    state = new QuantityType<>(nodeData.getMeasurements().getCo2Ppm(), PARTS_PER_MILLION);
+                    state = new QuantityType<>(measurements.getCo2Ppm(), PARTS_PER_MILLION);
                     break;
                 case CHANNEL_HUMIDITY:
-                    state = new QuantityType<>(nodeData.getMeasurements().getHumidityRH(), PERCENT);
+                    state = new QuantityType<>(measurements.getHumidityRH(), PERCENT);
                     break;
                 case CHANNEL_AQI_US:
-                    state = new QuantityType<>(nodeData.getMeasurements().getPm25AQIUS(), ONE);
+                    state = new QuantityType<>(measurements.getPm25AQIUS(), ONE);
                     break;
                 case CHANNEL_PM_25:
                     // PM2.5 is in ug/m3
-                    state = new QuantityType<>(nodeData.getMeasurements().getPm25Ugm3(),
-                            MICRO(GRAM).divide(CUBIC_METRE));
+                    state = new QuantityType<>(measurements.getPm25Ugm3(), MICRO(GRAM).divide(CUBIC_METRE));
+                    break;
+                case CHANNEL_PM_10:
+                    // PM10 is in ug/m3
+                    state = new QuantityType<>(measurements.getPm10Ugm3(), MICRO(GRAM).divide(CUBIC_METRE));
+                    break;
+                case CHANNEL_PM_01:
+                    // PM0.1 is in ug/m3
+                    state = new QuantityType<>(measurements.getPm01Ugm3(), MICRO(GRAM).divide(CUBIC_METRE));
                     break;
                 case CHANNEL_TEMP_CELSIUS:
-                    state = new QuantityType<>(nodeData.getMeasurements().getTemperatureC(), CELSIUS);
+                    state = new QuantityType<>(measurements.getTemperatureC(), CELSIUS);
                     break;
                 case CHANNEL_TIMESTAMP:
                     // It seem the Node timestamp is Unix timestamp converted from UTC time plus timezone offset.
