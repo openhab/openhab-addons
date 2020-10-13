48cff2ffcf2e5a1890b43853b062c42a99751ff6
Matthew Skinner <matt@pcmus.com>
2020-10-13 21:40:15 +1100

    Fix checkstyle issues.
    Signed-off-by: Matthew Skinner <matt@pcmus.com>

--- bundles/org.openhab.binding.ipcamera/src/main/java/org/openhab/binding/ipcamera/internal/StreamServerHandler.java
+++ bundles/org.openhab.binding.ipcamera/src/main/java/org/openhab/binding/ipcamera/internal/StreamServerHandler.java
@@ -177,19 +177,13 @@ public class StreamServerHandler extends ChannelInboundHandlerAdapter {
             }
             if (msg instanceof HttpContent) {
                 HttpContent content = (HttpContent) msg;
-                int index = 0;
                 if (recievedBytes == 0) {
-                    incomingJpeg = new byte[content.content().capacity()];
+                    incomingJpeg = content.content().array();
                 } else {
                     byte[] temp = incomingJpeg;
                     incomingJpeg = new byte[recievedBytes + content.content().capacity()];
-
-                    for (; index < temp.length; index++) {
-                        incomingJpeg[index] = temp[index];
-                    }
-                }
-                for (int i = 0; i < content.content().capacity(); i++) {
-                    incomingJpeg[index++] = content.content().getByte(i);
+                    System.arraycopy(temp, 0, incomingJpeg, 0, temp.length);
+                    System.arraycopy(content.content(), 0, incomingJpeg, recievedBytes, content.content().capacity());
                 }
                 recievedBytes = incomingJpeg.length;
                 if (content instanceof LastHttpContent) {
