bef1046258e666ce180d5b5c73a0f60a100eb4e7
Wouter Born <github@maindrain.net>
2020-10-14 19:01:12 +0200

    Rework ThingActions (#8732)
    * Remove proxy workarounds
    * Move ThingActions and a few other classes into the internal package
    * Use more consistent action labels/descriptions
    
    Related to:
    
    * openhab/openhab-core#1714
    * openhab/openhab-core#1639
    
    Signed-off-by: Wouter Born <github@maindrain.net>

---
+++ bundles/org.openhab.binding.xmppclient/src/main/java/org/openhab/binding/xmppclient/internal/XMPPClientBindingConstants.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.xmppclient.internal;
+
+import org.openhab.core.thing.ThingTypeUID;
+
+/**
+ * The {@link XMPPClientBindingConstants} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Pavel Gololobov - Initial contribution
+ */
+public class XMPPClientBindingConstants {
+    private static final String BINDING_ID = "xmppclient";
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID BRIDGE_TYPE_XMPP = new ThingTypeUID(BINDING_ID, "xmppBridge");
+    public static final String PUBLISH_TRIGGER_CHANNEL = "publishTrigger";
+}
