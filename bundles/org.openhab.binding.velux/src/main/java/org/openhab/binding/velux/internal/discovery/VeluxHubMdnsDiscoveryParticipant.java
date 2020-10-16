21de8eafc53b8fd44d593a75a0d3c71bb718c0d3
Andrew Fiddian-Green <software@whitebear.ch>
2020-10-16 13:00:21 +0100

    [velux] finalize mdns
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.binding.velux/src/main/java/org/openhab/binding/velux/internal/discovery/VeluxHubMdnsDiscoveryParticipant.java
+++ bundles/org.openhab.binding.velux/src/main/java/org/openhab/binding/velux/internal/discovery/VeluxHubMdnsDiscoveryParticipant.java
@@ -14,13 +14,11 @@ package org.openhab.binding.velux.internal.discovery;
 
 import static org.openhab.binding.velux.internal.VeluxBindingConstants.THING_TYPE_BRIDGE;
 
-import java.io.IOException;
-import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Collections;
 import java.util.Set;
 
-import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -41,52 +39,35 @@ import org.slf4j.LoggerFactory;
  * @author Andrew Fiddian-Green - Initial contribution
  */
 @NonNullByDefault
-@Component
+@Component(service = MDNSDiscoveryParticipant.class, immediate = true)
 public class VeluxHubMdnsDiscoveryParticipant implements MDNSDiscoveryParticipant {
 
     private final Logger logger = LoggerFactory.getLogger(VeluxHubMdnsDiscoveryParticipant.class);
 
     private static final String VELUX_KLF_LAN = "VELUX_KLF_LAN";
     private static final String HTTP_TCP_LOCAL = "_http._tcp.local.";
-    private static final int TIMEOUT = 6000;
-
-    private final @Nullable JmDNS mdnsResolver = newJmDNS();
-
-    private @Nullable JmDNS newJmDNS() {
-        try {
-            return JmDNS.create(InetAddress.getLocalHost(), VELUX_KLF_LAN);
-        } catch (IOException e) {
-            // fall through
-        }
-        return null;
-    }
 
     /**
-     * Checks if the passed serviceInfo refers to a Velux hub, and if so tries to resolve and return its ipv4 address
+     * Checks if the passed {@link ServiceInfo} refers to a Velux hub, and if so tries to resolve and return its ipv4
+     * address
      *
      * @param serviceInfo contains an mDNS discovered service info
      * @return returns the ipv4 address if the passed serviceInfo relates to a Velux hub whose host name can be resolved
      *         to an ipv4 address (i.e. if it is online); otherwise returns null
      */
     private @Nullable String getIpAddressIfOnlineVeluxHub(ServiceInfo serviceInfo) {
-        String serviceName = serviceInfo.getName();
-        logger.trace("getIpAddressIfOnlineVeluxHub(): check if {} is an online Velux hub", serviceName);
-        if (serviceName.startsWith(VELUX_KLF_LAN)) {
-            JmDNS mdnsResolver = this.mdnsResolver;
-            if (mdnsResolver != null) {
-                for (ServiceInfo foundInfo : mdnsResolver.list(serviceInfo.getServer(), TIMEOUT)) {
-                    for (Inet4Address ipAddress : foundInfo.getInet4Addresses()) {
-                        String ipString = ipAddress.getHostAddress();
-                        if (!ipString.isEmpty()) {
-                            logger.trace("getIpAddressIfOnlineVeluxHub(): {} is a Velux hub on {}", serviceName,
-                                    ipString);
-                            return ipString;
-                        }
-                    }
-                }
+        String name = serviceInfo.getName();
+        logger.trace("getIpAddressIfOnlineVeluxHub(): valid hub check for '{}'", name);
+        if (name.startsWith(VELUX_KLF_LAN)) {
+            try {
+                String addr = InetAddress.getByName(name + "." + serviceInfo.getDomain()).getHostAddress();
+                logger.trace("getIpAddressIfOnlineVeluxHub(): valid hub '{}' on ip '{}'", name, addr);
+                return addr;
+            } catch (UnknownHostException e) {
+                // fall through
             }
         }
-        logger.trace("getIpAddressIfOnlineVeluxHub(): {} is not an online Velux hub", serviceName);
+        logger.trace("getIpAddressIfOnlineVeluxHub(): device '{}' is not a hub", name);
         return null;
     }
 
@@ -102,14 +83,15 @@ public class VeluxHubMdnsDiscoveryParticipant implements MDNSDiscoveryParticipan
 
     @Override
     public @Nullable DiscoveryResult createResult(ServiceInfo serviceInfo) {
-        String ipString = getIpAddressIfOnlineVeluxHub(serviceInfo);
-        if (ipString != null) {
-            ThingUID thingUID = new ThingUID(THING_TYPE_BRIDGE, ipString.replace(".", "_"));
+        logger.trace("createResult(): called..");
+        String ipAddr = getIpAddressIfOnlineVeluxHub(serviceInfo);
+        if (ipAddr != null) {
+            ThingUID thingUID = new ThingUID(THING_TYPE_BRIDGE, ipAddr.replace(".", "_"));
             DiscoveryResult result = DiscoveryResultBuilder.create(thingUID).withThingType(THING_TYPE_BRIDGE)
-                    .withProperty(VeluxBridgeConfiguration.BRIDGE_IPADDRESS, ipString)
+                    .withProperty(VeluxBridgeConfiguration.BRIDGE_IPADDRESS, ipAddr)
                     .withRepresentationProperty(VeluxBridgeConfiguration.BRIDGE_IPADDRESS)
-                    .withLabel(String.format("Velux Hub (%s)", ipString)).build();
-            logger.trace("createResult(): return discovered hub with uid {} on {}", thingUID, ipString);
+                    .withLabel(String.format("Velux Hub (%s)", ipAddr)).build();
+            logger.trace("createResult(): return discovered hub uid '{}' on ip '{}'", thingUID, ipAddr);
             return result;
         }
         return null;
@@ -117,10 +99,11 @@ public class VeluxHubMdnsDiscoveryParticipant implements MDNSDiscoveryParticipan
 
     @Override
     public @Nullable ThingUID getThingUID(ServiceInfo serviceInfo) {
-        String ipString = getIpAddressIfOnlineVeluxHub(serviceInfo);
-        if (ipString != null) {
-            ThingUID thingUID = new ThingUID(THING_TYPE_BRIDGE, ipString.replace(".", "_"));
-            logger.trace("getThingUID(): return uid {} for hub discovered on {}", thingUID, ipString);
+        logger.trace("getThingUID(): called..");
+        String ipAddr = getIpAddressIfOnlineVeluxHub(serviceInfo);
+        if (ipAddr != null) {
+            ThingUID thingUID = new ThingUID(THING_TYPE_BRIDGE, ipAddr.replace(".", "_"));
+            logger.trace("getThingUID(): return uid '{}' for hub on ip '{}'", thingUID, ipAddr);
             return thingUID;
         }
         return null;
