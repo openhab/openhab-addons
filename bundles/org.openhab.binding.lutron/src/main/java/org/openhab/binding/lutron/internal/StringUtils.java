d6ed85d2a8384f2f11d0dbdbe0a836b1bb4af2a2
Bob Adair <bob.github@att.net>
2020-10-16 01:08:26 -0400

    [lutron] Minor code and doc updates
    Signed-off-by: Bob Adair <bob.github@att.net>

---
+++ bundles/org.openhab.binding.lutron/src/main/java/org/openhab/binding/lutron/internal/StringUtils.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.lutron.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * Supply some string utility methods formerly provided by org.apache.commons.lang.StringUtils.
+ *
+ * @author Bob Adair - Initial contribution
+ *
+ */
+@NonNullByDefault
+public class StringUtils {
+
+    public static boolean equals(@Nullable String s1, @Nullable String s2) {
+        if (s1 == null && s2 == null) {
+            return true;
+        } else if (s1 == null || s2 == null) {
+            return false;
+        } else {
+            return s1.equals(s2);
+        }
+    }
+
+    public static boolean isEmpty(@Nullable String s1) {
+        return (s1 == null || s1.isEmpty());
+    }
+}
