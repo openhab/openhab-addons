3c96dd4aa7fde7e52e9276b2d83bd337ac4b40d0
Roie Geron <roie.geron@gmail.com>
2020-10-18 03:19:40 +0300

    [touchwand] Touchwand Binding initial contribution  - migration to OH3  (#8754)
    * initial migration to OH 3
    
    Signed-off-by: Roie Geron <roie.geron@gmail.com>

---
+++ bundles/org.openhab.binding.touchwand/src/main/java/org/openhab/binding/touchwand/internal/TouchWandWebSockets.java
@@ -0,0 +1,193 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.touchwand.internal;
+
+import static org.openhab.binding.touchwand.internal.TouchWandBindingConstants.SUPPORTED_TOUCHWAND_TYPES;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
+import org.openhab.binding.touchwand.internal.dto.TouchWandUnitData;
+import org.openhab.binding.touchwand.internal.dto.TouchWandUnitFromJson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+/**
+ * The {@link TouchWandWebSockets} class implements WebSockets API to TouchWand controller
+ *
+ * @author Roie Geron - Initial contribution
+ */
+@NonNullByDefault
+public class TouchWandWebSockets {
+
+    private static final int CONNECT_TIMEOUT_SEC = 10;
+    private static final int WEBSOCKET_RECONNECT_INTERVAL_SEC = CONNECT_TIMEOUT_SEC * 2;
+    private static final int WEBSOCKET_IDLE_TIMEOUT_MS = CONNECT_TIMEOUT_SEC * 10 * 1000;
+    private final Logger logger = LoggerFactory.getLogger(TouchWandWebSockets.class);
+    private static final String WS_ENDPOINT_TOUCHWAND = "/async";
+
+    private WebSocketClient client;
+    private String controllerAddress;
+    private TouchWandSocket touchWandSocket;
+    private boolean isShutDown = false;
+    private CopyOnWriteArraySet<TouchWandUnitStatusUpdateListener> listeners = new CopyOnWriteArraySet<>();
+    private @Nullable ScheduledFuture<?> socketReconnect;
+    private @Nullable URI uri;
+
+    private ScheduledExecutorService scheduler;
+
+    public TouchWandWebSockets(String ipAddress, ScheduledExecutorService scheduler) {
+        client = new WebSocketClient();
+        touchWandSocket = new TouchWandSocket();
+        this.controllerAddress = ipAddress;
+        this.scheduler = scheduler;
+        socketReconnect = null;
+    }
+
+    public void connect() {
+        try {
+            uri = new URI("ws://" + controllerAddress + WS_ENDPOINT_TOUCHWAND);
+        } catch (URISyntaxException e) {
+            logger.warn("URI not valid {} message {}", uri, e.getMessage());
+            return;
+        }
+
+        client.setConnectTimeout(CONNECT_TIMEOUT_SEC);
+        ClientUpgradeRequest request = new ClientUpgradeRequest();
+        request.setSubProtocols("relay_protocol");
+
+        try {
+            client.start();
+            client.connect(touchWandSocket, uri, request);
+        } catch (Exception e) {
+            logger.warn("Could not connect webSocket URI {} message {}", uri, e.getMessage());
+            return;
+        }
+    }
+
+    public void dispose() {
+        isShutDown = true;
+        try {
+            client.stop();
+            ScheduledFuture<?> mySocketReconnect = socketReconnect;
+            if (mySocketReconnect != null) {
+                mySocketReconnect.cancel(true);
+            }
+        } catch (Exception e) {
+            logger.warn("Could not stop webSocketClient,  message {}", e.getMessage());
+        }
+    }
+
+    public void registerListener(TouchWandUnitStatusUpdateListener listener) {
+        if (!listeners.contains(listener)) {
+            logger.debug("Adding TouchWandWebSocket listener {}", listener);
+            listeners.add(listener);
+        }
+    }
+
+    public void unregisterListener(TouchWandUnitStatusUpdateListener listener) {
+        logger.debug("Removing TouchWandWebSocket listener {}", listener);
+        listeners.remove(listener);
+    }
+
+    @WebSocket(maxIdleTime = WEBSOCKET_IDLE_TIMEOUT_MS)
+    public class TouchWandSocket {
+
+        @OnWebSocketClose
+        public void onClose(int statusCode, String reason) {
+            logger.debug("Connection closed: {} - {}", statusCode, reason);
+            if (!isShutDown) {
+                logger.debug("weSocket Closed - reconnecting");
+                asyncWeb();
+            }
+        }
+
+        @OnWebSocketConnect
+        public void onConnect(Session session) {
+            logger.debug("TouchWandWebSockets connected to {}", session.getRemoteAddress().toString());
+            try {
+                session.getRemote().sendString("{\"myopenhab\": \"myopenhab\"}");
+            } catch (IOException e) {
+                logger.warn("sendString : {}", e.getMessage());
+            }
+        }
+
+        @OnWebSocketMessage
+        public void onMessage(String msg) {
+            TouchWandUnitData touchWandUnit;
+            JsonParser jsonParser = new JsonParser();
+            try {
+                JsonObject unitObj = jsonParser.parse(msg).getAsJsonObject();
+                boolean eventUnitChanged = unitObj.get("type").getAsString().equals("UNIT_CHANGED");
+                if (!eventUnitChanged) {
+                    return;
+                }
+                touchWandUnit = TouchWandUnitFromJson.parseResponse(unitObj.get("unit").getAsJsonObject());
+                if (!touchWandUnit.getStatus().equals("ALIVE")) {
+                    return;
+                }
+                boolean supportedUnitType = Arrays.asList(SUPPORTED_TOUCHWAND_TYPES).contains(touchWandUnit.getType());
+                if (!supportedUnitType) {
+                    logger.debug("UNIT_CHANGED for unsupported unit type {}", touchWandUnit.getType());
+                    return;
+                }
+                logger.debug("UNIT_CHANGED: name {} id {} status {}", touchWandUnit.getName(), touchWandUnit.getId(),
+                        touchWandUnit.getCurrStatus());
+
+                for (TouchWandUnitStatusUpdateListener listener : listeners) {
+                    listener.onDataReceived(touchWandUnit);
+
+                }
+            } catch (JsonSyntaxException e) {
+                logger.warn("jsonParser.parse {} ", e.getMessage());
+            }
+        }
+
+        @OnWebSocketError
+        public void onError(Throwable cause) {
+            logger.warn("WebSocket Error: {}", cause.getMessage());
+            if (!isShutDown) {
+                logger.debug("WebSocket onError - reconnecting");
+                asyncWeb();
+            }
+        }
+
+        private void asyncWeb() {
+            ScheduledFuture<?> mySocketReconnect = socketReconnect;
+            if (mySocketReconnect == null || mySocketReconnect.isDone()) {
+                socketReconnect = scheduler.schedule(TouchWandWebSockets.this::connect,
+                        WEBSOCKET_RECONNECT_INTERVAL_SEC, TimeUnit.SECONDS);
+            }
+        }
+    }
+}
---
+++ bundles/org.openhab.binding.touchwand/src/main/java/org/openhab/binding/touchwand/internal/discovery/TouchWandUnitDiscoveryService.java
@@ -0,0 +1,223 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package org.openhab.binding.touchwand.internal.discovery;
+
+import static org.openhab.binding.touchwand.internal.TouchWandBindingConstants.*;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.touchwand.internal.TouchWandBridgeHandler;
+import org.openhab.binding.touchwand.internal.TouchWandUnitStatusUpdateListener;
+import org.openhab.binding.touchwand.internal.dto.TouchWandUnitData;
+import org.openhab.binding.touchwand.internal.dto.TouchWandUnitFromJson;
+import org.openhab.core.config.discovery.AbstractDiscoveryService;
+import org.openhab.core.config.discovery.DiscoveryResultBuilder;
+import org.openhab.core.config.discovery.DiscoveryService;
+import org.openhab.core.thing.ThingStatus;
+import org.openhab.core.thing.ThingTypeUID;
+import org.openhab.core.thing.ThingUID;
+import org.openhab.core.thing.binding.ThingHandler;
+import org.openhab.core.thing.binding.ThingHandlerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+/**
+ * The {@link TouchWandUnitDiscoveryService} Discovery service for TouchWand units.
+ *
+ * @author Roie Geron - Initial contribution
+ */
+@NonNullByDefault
+public class TouchWandUnitDiscoveryService extends AbstractDiscoveryService
+        implements DiscoveryService, ThingHandlerService {
+
+    private static final int SEARCH_TIME_SEC = 10;
+    private static final int SCAN_INTERVAL_SEC = 60;
+    private static final int LINK_DISCOVERY_SERVICE_INITIAL_DELAY_SEC = 5;
+    private static final String[] CONNECTIVITY_OPTIONS = { CONNECTIVITY_KNX, CONNECTIVITY_ZWAVE };
+    private @NonNullByDefault({}) TouchWandBridgeHandler touchWandBridgeHandler;
+    private final Logger logger = LoggerFactory.getLogger(TouchWandUnitDiscoveryService.class);
+
+    private @Nullable ScheduledFuture<?> scanningJob;
+    private CopyOnWriteArraySet<TouchWandUnitStatusUpdateListener> listeners = new CopyOnWriteArraySet<>();
+
+    public TouchWandUnitDiscoveryService() {
+        super(SUPPORTED_THING_TYPES_UIDS, SEARCH_TIME_SEC, true);
+    }
+
+    @Override
+    protected void startScan() {
+        if (touchWandBridgeHandler.getThing().getStatus() != ThingStatus.ONLINE) {
+            logger.warn("Could not scan units while bridge offline");
+            return;
+        }
+
+        logger.debug("Starting TouchWand discovery on bridge {}", touchWandBridgeHandler.getThing().getUID());
+        String response = touchWandBridgeHandler.touchWandClient.cmdListUnits();
+        if (response.isEmpty()) {
+            return;
+        }
+
+        JsonParser jsonParser = new JsonParser();
+        try {
+            JsonArray jsonArray = jsonParser.parse(response).getAsJsonArray();
+            if (jsonArray.isJsonArray()) {
+                try {
+                    for (JsonElement unit : jsonArray) {
+                        TouchWandUnitData touchWandUnit;
+                        touchWandUnit = TouchWandUnitFromJson.parseResponse(unit.getAsJsonObject());
+                        if (touchWandUnit == null) {
+                            continue;
+                        }
+                        if (!touchWandBridgeHandler.isAddSecondaryControllerUnits()) {
+                            if (!Arrays.asList(CONNECTIVITY_OPTIONS).contains(touchWandUnit.getConnectivity())) {
+                                continue;
+                            }
+                        }
+                        String type = touchWandUnit.getType();
+                        if (!Arrays.asList(SUPPORTED_TOUCHWAND_TYPES).contains(type)) {
+                            logger.debug("Unit discovery skipping unsupported unit type : {} ", type);
+                            continue;
+                        }
+                        switch (type) {
+                            case TYPE_WALLCONTROLLER:
+                                addDeviceDiscoveryResult(touchWandUnit, THING_TYPE_WALLCONTROLLER);
+                                break;
+                            case TYPE_SWITCH:
+                                addDeviceDiscoveryResult(touchWandUnit, THING_TYPE_SWITCH);
+                                notifyListeners(touchWandUnit);
+                                break;
+                            case TYPE_DIMMER:
+                                addDeviceDiscoveryResult(touchWandUnit, THING_TYPE_DIMMER);
+                                notifyListeners(touchWandUnit);
+                                break;
+                            case TYPE_SHUTTER:
+                                addDeviceDiscoveryResult(touchWandUnit, THING_TYPE_SHUTTER);
+                                break;
+                            default:
+                                continue;
+                        }
+                    }
+                } catch (JsonSyntaxException e) {
+                    logger.warn("Could not parse unit {}", e.getMessage());
+                }
+            }
+        } catch (JsonSyntaxException msg) {
+            logger.warn("Could not parse list units response {}", msg.getMessage());
+        }
+    }
+
+    private void notifyListeners(TouchWandUnitData touchWandUnit) {
+        for (TouchWandUnitStatusUpdateListener listener : listeners) {
+            listener.onDataReceived(touchWandUnit);
+        }
+    }
+
+    @Override
+    protected void stopScan() {
+        removeOlderResults(getTimestampOfLastScan());
+        super.stopScan();
+    }
+
+    @Override
+    public void activate() {
+        super.activate(null);
+        removeOlderResults(new Date().getTime(), touchWandBridgeHandler.getThing().getUID());
+    }
+
+    @Override
+    public void deactivate() {
+        removeOlderResults(new Date().getTime(), touchWandBridgeHandler.getThing().getUID());
+        super.deactivate();
+    }
+
+    @Override
+    protected void startBackgroundDiscovery() {
+        ScheduledFuture<?> localScanningJob = scanningJob;
+        if (localScanningJob == null || localScanningJob.isCancelled()) {
+            scanningJob = scheduler.scheduleWithFixedDelay(this::startScan, LINK_DISCOVERY_SERVICE_INITIAL_DELAY_SEC,
+                    SCAN_INTERVAL_SEC, TimeUnit.SECONDS);
+        }
+    }
+
+    @Override
+    protected void stopBackgroundDiscovery() {
+        ScheduledFuture<?> myScanningJob = scanningJob;
+        if (myScanningJob != null) {
+            myScanningJob.cancel(true);
+            scanningJob = null;
+        }
+    }
+
+    public void registerListener(TouchWandUnitStatusUpdateListener listener) {
+        if (!listeners.contains(listener)) {
+            logger.debug("Adding TouchWandWebSocket listener {}", listener);
+            listeners.add(listener);
+        }
+    }
+
+    public void unregisterListener(TouchWandUnitStatusUpdateListener listener) {
+        logger.debug("Removing TouchWandWebSocket listener {}", listener);
+        listeners.remove(listener);
+    }
+
+    @Override
+    public int getScanTimeout() {
+        return SEARCH_TIME_SEC;
+    }
+
+    private void addDeviceDiscoveryResult(TouchWandUnitData unit, ThingTypeUID typeUID) {
+        ThingUID bridgeUID = touchWandBridgeHandler.getThing().getUID();
+        ThingUID thingUID = new ThingUID(typeUID, bridgeUID, unit.getId().toString());
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(HANDLER_PROPERTIES_ID, unit.getId().toString());
+        properties.put(HANDLER_PROPERTIES_NAME, unit.getName());
+        // @formatter:off
+        thingDiscovered(DiscoveryResultBuilder.create(thingUID)
+                .withThingType(typeUID)
+                .withLabel(unit.getName())
+                .withBridge(bridgeUID)
+                .withProperties(properties)
+                .withRepresentationProperty(HANDLER_PROPERTIES_ID)
+                .build()
+        );
+        // @formatter:on
+    }
+
+    @Override
+    public void setThingHandler(@NonNullByDefault({}) ThingHandler handler) {
+        if (handler instanceof TouchWandBridgeHandler) {
+            touchWandBridgeHandler = (TouchWandBridgeHandler) handler;
+            registerListener(touchWandBridgeHandler);
+        }
+    }
+
+    @Override
+    public @NonNull ThingHandler getThingHandler() {
+        return touchWandBridgeHandler;
+    }
+}
