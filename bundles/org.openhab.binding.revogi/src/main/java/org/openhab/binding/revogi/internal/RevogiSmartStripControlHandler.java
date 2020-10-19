c908d919945911ab823b29b556d5693137007cef
Andreas Bräu <ab@andi95.de>
2020-10-19 14:25:15 +0200

    typo
    Signed-off-by: Andreas Bräu <ab@andi95.de>

--- bundles/org.openhab.binding.revogi/src/main/java/org/openhab/binding/revogi/internal/RevogiSmartStripControlHandler.java
+++ bundles/org.openhab.binding.revogi/src/main/java/org/openhab/binding/revogi/internal/RevogiSmartStripControlHandler.java
@@ -83,7 +83,7 @@ public class RevogiSmartStripControlHandler extends BaseThingHandler {
                 switchPlug(command, 0);
                 break;
             default:
-                logger.info("Sometring went wrong, we've got a message for {}", channelUID.getId());
+                logger.info("Something went wrong, we've got a message for {}", channelUID.getId());
         }
     }
 
