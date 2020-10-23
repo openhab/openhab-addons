640852a11cbcd86ab9443f682b8b86f6859788bd
Korbinian Probst <kp.droid.dev@gmail.com>
2020-10-23 21:29:20 +0200

    Implement Current, Voltage and Power for all three Phases
    Signed-off-by: Korbinian Probst <kp.droid.dev@gmail.com>

--- bundles/org.openhab.binding.senechome/src/main/java/org/openhab/binding/senechome/internal/json/SenecHomeGrid.java
+++ bundles/org.openhab.binding.senechome/src/main/java/org/openhab/binding/senechome/internal/json/SenecHomeGrid.java
@@ -17,7 +17,7 @@ import java.io.Serializable;
 import com.google.gson.annotations.SerializedName;
 
 /**
- * Json model of senec home devices: This sub model provides the current power limitation by the inverter.
+ * Json model of senec home devices: This sub model provides the current power statistics by the inverter.
  *
  * @author Steven Schwarznau - Initial Contribution
  */
@@ -30,8 +30,33 @@ public class SenecHomeGrid implements Serializable {
      */
     public @SerializedName("P_TOTAL") String currentGridValue;
 
+    /**
+     * grid voltage for each phase
+     */
+    public @SerializedName("PM1OBJ1U_AC0") String currentGridVoltagePh1;
+    public @SerializedName("PM1OBJ1U_AC1") String currentGridVoltagePh2;
+    public @SerializedName("PM1OBJ1U_AC2") String currentGridVoltagePh3;
+
+    /**
+     * grid current for each phase, draw (for values larger zero) or supply (for negative values)
+     */
+    public @SerializedName("PM1OBJ1I_AC0") String currentGridCurrentPh1;
+    public @SerializedName("PM1OBJ1I_AC1") String currentGridCurrentPh2;
+    public @SerializedName("PM1OBJ1I_AC2") String currentGridCurrentPh3;
+
+    /**
+     * grid power for each phase, draw (for values larger zero) or supply (for negative values)
+     */
+    public @SerializedName("PM1OBJ1P_AC0") String currentGridPowerPh1;
+    public @SerializedName("PM1OBJ1P_AC1") String currentGridPowerPh2;
+    public @SerializedName("PM1OBJ1P_AC2") String currentGridPowerPh3;
+
     @Override
     public String toString() {
-        return "SenecHomeGrid [currentGridValue=" + currentGridValue + "]";
+        return "SenecHomeGrid [currentGridValue=" + currentGridValue + "currentGridVoltagePh1=" + currentGridVoltagePh1
+                + "currentGridVoltagePh2=" + currentGridVoltagePh2 + "currentGridVoltagePh3=" + currentGridVoltagePh3
+                + "currentGridCurrentPh1=" + currentGridCurrentPh1 + "currentGridCurrentPh2=" + currentGridCurrentPh2
+                + "currentGridCurrentPh3=" + currentGridCurrentPh3 + "currentGridPowerPh1=" + currentGridPowerPh1
+                + "currentGridPowerPh2=" + currentGridPowerPh2 + "currentGridPowerPh3=" + currentGridPowerPh3 + "]";
     }
 }
