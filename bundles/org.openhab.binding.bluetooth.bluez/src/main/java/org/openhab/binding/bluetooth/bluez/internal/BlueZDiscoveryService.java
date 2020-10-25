c902953485d5aa30a04d34bc7cb07eda121fda03
Benjamin Lafois <benjamin.lafois@gmail.com>
2020-10-25 15:54:33 +0100

    [bluetooth.bluez] Complete Bluez rewrite (#8819)
    Also-by: Connor Petty <mistercpp2000+gitsignoff@gmail.com>

--- bundles/org.openhab.binding.bluetooth.bluez/src/main/java/org/openhab/binding/bluetooth/bluez/internal/BlueZDiscoveryService.java
+++ bundles/org.openhab.binding.bluetooth.bluez/src/main/java/org/openhab/binding/bluetooth/bluez/internal/BlueZDiscoveryService.java
@@ -10,22 +10,26 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.bluetooth.bluez.internal.discovery;
+package org.openhab.binding.bluetooth.bluez.internal;
 
 import java.util.Collections;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
-import org.openhab.binding.bluetooth.bluez.BlueZAdapterConstants;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.core.config.discovery.AbstractDiscoveryService;
 import org.openhab.core.config.discovery.DiscoveryResult;
 import org.openhab.core.config.discovery.DiscoveryResultBuilder;
 import org.openhab.core.config.discovery.DiscoveryService;
 import org.openhab.core.thing.ThingUID;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import tinyb.BluetoothAdapter;
-import tinyb.BluetoothManager;
+import com.github.hypfvieh.bluetooth.wrapper.BluetoothAdapter;
 
 /**
  * This is a discovery service, which checks whether we are running on a Linux with a BlueZ stack.
@@ -33,41 +37,59 @@ import tinyb.BluetoothManager;
  *
  * @author Kai Kreuzer - Initial Contribution and API
  * @author Hilbrand Bouwkamp - Moved background scan to actual background method
+ * @author Connor Petty - Replaced tinyB with bluezDbus
  *
  */
+@NonNullByDefault
 @Component(service = DiscoveryService.class, configurationPid = "discovery.bluetooth.bluez")
 public class BlueZDiscoveryService extends AbstractDiscoveryService {
 
     private final Logger logger = LoggerFactory.getLogger(BlueZDiscoveryService.class);
 
-    private BluetoothManager manager;
+    private final DeviceManagerFactory deviceManagerFactory;
+    private @Nullable Future<?> backgroundScan;
 
-    public BlueZDiscoveryService() {
+    @Activate
+    public BlueZDiscoveryService(@Reference DeviceManagerFactory deviceManagerFactory) {
         super(Collections.singleton(BlueZAdapterConstants.THING_TYPE_BLUEZ), 1, true);
+        this.deviceManagerFactory = deviceManagerFactory;
+    }
+
+    private static void cancel(@Nullable Future<?> future) {
+        if (future != null) {
+            future.cancel(false);
+        }
     }
 
     @Override
     protected void startBackgroundDiscovery() {
-        startScan();
+        backgroundScan = scheduler.scheduleWithFixedDelay(() -> {
+            DeviceManagerWrapper deviceManager = deviceManagerFactory.getDeviceManager();
+            if (deviceManager == null) {
+                return;
+            }
+            startScan();
+        }, 5, 10, TimeUnit.SECONDS);
+    }
+
+    @Override
+    protected void stopBackgroundDiscovery() {
+        cancel(backgroundScan);
+        backgroundScan = null;
     }
 
     @Override
     protected void startScan() {
-        try {
-            manager = BluetoothManager.getBluetoothManager();
-            manager.getAdapters().stream().map(this::createDiscoveryResult).forEach(this::thingDiscovered);
-        } catch (UnsatisfiedLinkError e) {
-            logger.debug("Not possible to initialize the BlueZ stack. ", e);
+        DeviceManagerWrapper deviceManager = deviceManagerFactory.getDeviceManager();
+        if (deviceManager == null) {
+            logger.warn("The DeviceManager is not available");
             return;
-        } catch (RuntimeException e) {
-            // we do not get anything more specific from TinyB here
-            if (e.getMessage() != null && e.getMessage().contains("AccessDenied")) {
-                logger.warn(
-                        "Cannot access BlueZ stack due to permission problems. Make sure that your OS user is part of the 'bluetooth' group of BlueZ.");
-            } else {
-                logger.warn("Failed to scan for Bluetooth devices", e);
-            }
         }
+        // the first time the device manager is not null we can cancel background discovery
+        stopBackgroundDiscovery();
+        deviceManager.scanForBluetoothAdapters().stream()//
+                .map(this::createDiscoveryResult)//
+                .forEach(this::thingDiscovered);
     }
 
     private DiscoveryResult createDiscoveryResult(BluetoothAdapter adapter) {
@@ -78,6 +100,6 @@ public class BlueZDiscoveryService extends AbstractDiscoveryService {
     }
 
     private String getId(BluetoothAdapter adapter) {
-        return adapter.getInterfaceName().replaceAll("[^a-zA-Z0-9_]", "");
+        return adapter.getDeviceName().replaceAll("[^a-zA-Z0-9_]", "");
     }
 }
---
+++ bundles/org.openhab.binding.bluetooth.bluez/src/main/java/org/openhab/binding/bluetooth/bluez/internal/events/NameEvent.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.bluetooth.bluez.internal.events;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * This event is triggered when a device's 'Name' bluez property changes
+ *
+ * @author Benjamin Lafois - Initial Contribution
+ *
+ */
+@NonNullByDefault
+public class NameEvent extends BlueZEvent {
+
+    private String name;
+
+    public NameEvent(String dbusPath, String name) {
+        super(dbusPath);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void dispatch(BlueZEventListener listener) {
+        listener.onNameUpdate(this);
+    }
+}
