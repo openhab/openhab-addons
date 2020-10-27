e2869a5db689ffec7fef08f0dcc255dfa2828d95
Matthew Skinner <matt@pcmus.com>
2020-10-27 22:26:31 +1100

    Update bundles/org.openhab.binding.wled/src/main/java/org/openhab/binding/wled/internal/WLedActions.java
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    Co-authored-by: Fabian Wolter <github@fabian-wolter.de>
    Signed-off-by: Matthew Skinner <matt@pcmus.com>

--- bundles/org.openhab.binding.wled/src/main/java/org/openhab/binding/wled/internal/WLedActions.java
+++ bundles/org.openhab.binding.wled/src/main/java/org/openhab/binding/wled/internal/WLedActions.java
@@ -46,7 +46,7 @@ public class WLedActions implements ThingActions {
 
     @RuleAction(label = "save state to preset", description = "Save a WLED state to a preset slot")
     public void savePreset(
-            @ActionInput(name = "presetNumber", label = "Save State to Preset Slot", description = "Enter the number for the preset slot you wish to use") int presetNumber) {
+            @ActionInput(name = "presetNumber", label = "Preset Slot", description = "Number for the preset slot you wish to use") int presetNumber) {
         if (presetNumber > 0 && handler != null) {
             handler.savePreset(presetNumber);
         }
