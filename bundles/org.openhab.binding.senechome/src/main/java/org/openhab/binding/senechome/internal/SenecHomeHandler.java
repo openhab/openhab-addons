70408038406e3af60cd2bcc4fd678ec7a204eb1b
Korbinian Probst <kp.droid.dev@gmail.com>
2020-10-31 21:54:33 +0100

    Implement Review findings, change channelBatteryStateValue to QuantityType
    Signed-off-by: Korbinian Probst <kp.droid.dev@gmail.com>

--- bundles/org.openhab.binding.senechome/src/main/java/org/openhab/binding/senechome/internal/SenecHomeHandler.java
+++ bundles/org.openhab.binding.senechome/src/main/java/org/openhab/binding/senechome/internal/SenecHomeHandler.java
@@ -36,7 +36,6 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.jetty.client.HttpClient;
 import org.openhab.binding.senechome.internal.json.SenecHomeResponse;
 import org.openhab.core.cache.ExpiringCache;
-import org.openhab.core.library.types.DecimalType;
 import org.openhab.core.library.types.OnOffType;
 import org.openhab.core.library.types.QuantityType;
 import org.openhab.core.library.types.StringType;
@@ -228,7 +227,7 @@ public class SenecHomeHandler extends BaseThingHandler {
             Channel channelBatteryStateValue = getThing()
                     .getChannel(SenecHomeBindingConstants.CHANNEL_SENEC_BATTERY_STATE_VALUE);
             updateState(channelBatteryStateValue.getUID(),
-                    new DecimalType(getSenecValue(response.energy.batteryState).intValue()));
+                    new QuantityType<>(getSenecValue(response.energy.batteryState).intValue(), SmartHomeUnits.ONE));
 
             Channel channelLiveBatCharge = getThing()
                     .getChannel(SenecHomeBindingConstants.CHANNEL_SENEC_LIVE_BAT_CHARGE);
