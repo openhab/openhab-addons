7b011605c724f0c0bc318705d9ada428a9207030
Andi Bräu <andibraeu@users.noreply.github.com>
2020-11-03 18:13:08 +0100

    [revogi] Initial contribution - Resubmitted for OH3 (#8534)
    * Rename binding and resubmit to OH3
    
    Signed-off-by: Andreas Bräu <ab@andi95.de>

---
+++ bundles/org.openhab.binding.revogi/src/main/java/org/openhab/binding/revogi/internal/udp/DatagramSocketWrapper.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.revogi.internal.udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * The {@link DatagramSocketWrapper} wraps Java's DatagramSocket for better testing
+ * UdpSenderService
+ *
+ * @author Andi Bräu - Initial contribution
+ */
+@NonNullByDefault
+public class DatagramSocketWrapper {
+
+    @Nullable
+    private DatagramSocket datagramSocket;
+
+    public void initSocket() throws SocketException {
+        closeSocket();
+        DatagramSocket localDatagramSocket = new DatagramSocket();
+        localDatagramSocket.setBroadcast(true);
+        localDatagramSocket.setSoTimeout(3);
+        datagramSocket = localDatagramSocket;
+    }
+
+    public void closeSocket() {
+        DatagramSocket localDatagramSocket = this.datagramSocket;
+        if (localDatagramSocket != null && !localDatagramSocket.isClosed()) {
+            localDatagramSocket.close();
+        }
+    }
+
+    public void sendPacket(DatagramPacket datagramPacket) throws IOException {
+        DatagramSocket localDatagramSocket = this.datagramSocket;
+        if (localDatagramSocket != null && !localDatagramSocket.isClosed()) {
+            localDatagramSocket.send(datagramPacket);
+        } else {
+            throw new SocketException("Datagram Socket closed or not initialized");
+        }
+    }
+
+    public void receiveAnswer(DatagramPacket datagramPacket) throws IOException {
+        DatagramSocket localDatagramSocket = this.datagramSocket;
+        if (localDatagramSocket != null && !localDatagramSocket.isClosed()) {
+            localDatagramSocket.receive(datagramPacket);
+        } else {
+            throw new SocketException("Datagram Socket closed or not initialized");
+        }
+    }
+}
