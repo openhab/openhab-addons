aeb96359e989733cb5064a175e3f8df3e010088c
clinique <gael@lhopital.org>
2020-11-10 09:57:47 +0100

    Adding missing header to HubEauResponse
    Signed-off-by: clinique <gael@lhopital.org>

--- bundles/org.openhab.binding.vigicrues/src/main/java/org/openhab/binding/vigicrues/internal/dto/hubeau/HubEauResponse.java
+++ bundles/org.openhab.binding.vigicrues/src/main/java/org/openhab/binding/vigicrues/internal/dto/hubeau/HubEauResponse.java
@@ -1,9 +1,28 @@
+
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.vigicrues.internal.dto.hubeau;
 
 import java.util.List;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * The {@link HubEauResponse} is the Java class used to map the JSON
+ * response to an HubEau webservice endpoint request.
+ *
+ * @author Gaël L'hopital - Initial contribution
+ */
 public class HubEauResponse {
     public class StationData {
         @SerializedName("en_service")
@@ -130,7 +149,7 @@ public class HubEauResponse {
     public List<StationData> stations;
 
     /*
-     * Currently unused, maybe interesting in the future
+     * Currently unused, could be interesting in the future
      * public String first;
      * public String last;
      * public String prev;
