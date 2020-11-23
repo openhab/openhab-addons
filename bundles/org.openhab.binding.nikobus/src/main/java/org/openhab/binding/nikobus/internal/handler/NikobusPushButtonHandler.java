afc4f8ebbfc1f56ea41143f6a7ad1a8a214a1e97
Boris Krivonog <boris.krivonog@inova.si>
2020-11-23 20:27:25 +0100

    * fixed refresh handing due `channelLinked` not called anymore on startup for example (similar to https://github.com/openhab/openhab-core/issues/1707), * "Impacted Modules" can be empty if button is configured as "standalone" and does not impact modules, * minor doc fix
    Signed-off-by: Boris Krivonog <boris.krivonog@inova.si>

--- bundles/org.openhab.binding.nikobus/src/main/java/org/openhab/binding/nikobus/internal/handler/NikobusPushButtonHandler.java
+++ bundles/org.openhab.binding.nikobus/src/main/java/org/openhab/binding/nikobus/internal/handler/NikobusPushButtonHandler.java
@@ -118,26 +118,29 @@ public class NikobusPushButtonHandler extends NikobusBaseThingHandler {
 
         impactedModules.clear();
 
-        try {
-            ThingUID bridgeUID = thing.getBridgeUID();
-            if (bridgeUID == null) {
-                throw new IllegalArgumentException("Bridge does not exist!");
+        Object impactedModulesObject = getConfig().get(CONFIG_IMPACTED_MODULES);
+        if (impactedModulesObject != null) {
+            try {
+                ThingUID bridgeUID = thing.getBridgeUID();
+                if (bridgeUID == null) {
+                    throw new IllegalArgumentException("Bridge does not exist!");
+                }
+
+                String[] impactedModulesString = impactedModulesObject.toString().split(",");
+                for (String impactedModuleString : impactedModulesString) {
+                    ImpactedModuleUID impactedModuleUID = new ImpactedModuleUID(impactedModuleString.trim());
+                    ThingTypeUID thingTypeUID = new ThingTypeUID(bridgeUID.getBindingId(),
+                            impactedModuleUID.getThingTypeId());
+                    ThingUID thingUID = new ThingUID(thingTypeUID, bridgeUID, impactedModuleUID.getThingId());
+                    impactedModules.add(new ImpactedModule(thingUID, impactedModuleUID.getGroup()));
+                }
+            } catch (RuntimeException e) {
+                updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR, e.getMessage());
+                return;
             }
 
-            String[] impactedModulesString = getConfig().get(CONFIG_IMPACTED_MODULES).toString().split(",");
-            for (String impactedModuleString : impactedModulesString) {
-                ImpactedModuleUID impactedModuleUID = new ImpactedModuleUID(impactedModuleString.trim());
-                ThingTypeUID thingTypeUID = new ThingTypeUID(bridgeUID.getBindingId(),
-                        impactedModuleUID.getThingTypeId());
-                ThingUID thingUID = new ThingUID(thingTypeUID, bridgeUID, impactedModuleUID.getThingId());
-                impactedModules.add(new ImpactedModule(thingUID, impactedModuleUID.getGroup()));
-            }
-        } catch (RuntimeException e) {
-            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR, e.getMessage());
-            return;
+            logger.debug("Impacted modules for {} = {}", thing.getUID(), impactedModules);
         }
-
-        logger.debug("Impacted modules for {} = {}", thing.getUID(), impactedModules);
 
         NikobusPcLinkHandler pcLink = getPcLink();
         if (pcLink != null) {
@@ -183,8 +186,10 @@ public class NikobusPushButtonHandler extends NikobusBaseThingHandler {
 
         updateState(CHANNEL_BUTTON, OnOffType.ON);
 
-        Utils.cancel(requestUpdateFuture);
-        requestUpdateFuture = scheduler.schedule(this::update, 400, TimeUnit.MILLISECONDS);
+        if (!impactedModules.isEmpty()) {
+            Utils.cancel(requestUpdateFuture);
+            requestUpdateFuture = scheduler.schedule(this::update, 400, TimeUnit.MILLISECONDS);
+        }
     }
 
     private void update() {
