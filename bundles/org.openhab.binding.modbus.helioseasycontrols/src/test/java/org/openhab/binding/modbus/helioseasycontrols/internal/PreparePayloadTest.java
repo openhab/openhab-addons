a599a01a730ce064b81d97afd1bd1b0bdd0debad
Sami Salonen <ssalonen@gmail.com>
2020-11-26 18:21:58 +0200

    [modbus.helioseasycontrols] Test fixed
    - Copyright notice added
    - assertEquals parameter order fixed
    
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- bundles/org.openhab.binding.modbus.helioseasycontrols/src/test/java/org/openhab/binding/modbus/helioseasycontrols/internal/PreparePayloadTest.java
+++ bundles/org.openhab.binding.modbus.helioseasycontrols/src/test/java/org/openhab/binding/modbus/helioseasycontrols/internal/PreparePayloadTest.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.modbus.helioseasycontrols.internal;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -52,7 +64,7 @@ public class PreparePayloadTest {
             int expectedRegisterDataUnsigned = expectedRegisters.getRegister(i);
             int actualUnsigned = actual.getRegister(i);
 
-            assertEquals(actualUnsigned, expectedRegisterDataUnsigned,
+            assertEquals(expectedRegisterDataUnsigned, actualUnsigned,
                     String.format("register index i=%d, payload=%s", i, payload));
         }
     }
