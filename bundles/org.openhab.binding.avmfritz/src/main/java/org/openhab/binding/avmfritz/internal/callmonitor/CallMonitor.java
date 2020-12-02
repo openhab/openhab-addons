26927d6916da2ad15de727ae268e53abec52e3ac
Kai Kreuzer <kai@openhab.org>
2020-12-02 17:15:54 +0100

    Properly interrupt monitor thread upon dispose
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- bundles/org.openhab.binding.avmfritz/src/main/java/org/openhab/binding/avmfritz/internal/callmonitor/CallMonitor.java
+++ bundles/org.openhab.binding.avmfritz/src/main/java/org/openhab/binding/avmfritz/internal/callmonitor/CallMonitor.java
@@ -75,6 +75,9 @@ public class CallMonitor {
      */
     public void dispose() {
         reconnectJob.cancel(true);
+        if (monitorThread != null) {
+            monitorThread.interrupt();
+        }
     }
 
     public class CallMonitorThread extends Thread {
@@ -120,11 +123,13 @@ public class CallMonitor {
                     handler.setStatusInfo(ThingStatus.ONLINE, ThingStatusDetail.NONE, null);
                     while (!interrupted) {
                         try {
-                            String line = reader.readLine();
-                            if (line != null) {
-                                logger.debug("Received raw call string from fbox: {}", line);
-                                CallEvent ce = new CallEvent(line);
-                                handleCallEvent(ce);
+                            if (reader.ready()) {
+                                String line = reader.readLine();
+                                if (line != null) {
+                                    logger.debug("Received raw call string from fbox: {}", line);
+                                    CallEvent ce = new CallEvent(line);
+                                    handleCallEvent(ce);
+                                }
                             }
                         } catch (IOException e) {
                             if (interrupted) {
@@ -136,8 +141,9 @@ public class CallMonitor {
                             break;
                         } finally {
                             try {
-                                sleep(1000L);
+                                sleep(500L);
                             } catch (InterruptedException e) {
+                                interrupted = true;
                             }
                         }
                     }
