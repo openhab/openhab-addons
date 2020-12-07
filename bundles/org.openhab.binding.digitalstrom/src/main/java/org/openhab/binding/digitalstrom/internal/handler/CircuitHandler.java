4e4aa7e4fd7ed0e39069c1cce85f1fd74b81b479
J-N-K <J-N-K@users.noreply.github.com>
2020-12-07 21:35:35 +0100

    [digitalstrom] remove unnecessary call to bridgeStatusChanged (#9263)

--- bundles/org.openhab.binding.digitalstrom/src/main/java/org/openhab/binding/digitalstrom/internal/handler/CircuitHandler.java
+++ bundles/org.openhab.binding.digitalstrom/src/main/java/org/openhab/binding/digitalstrom/internal/handler/CircuitHandler.java
@@ -151,9 +151,6 @@ public class CircuitHandler extends BaseThingHandler implements DeviceStatusList
         if (bridgeStatusInfo.getStatus().equals(ThingStatus.OFFLINE)) {
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.BRIDGE_OFFLINE);
         }
-        if (bridgeStatusInfo.getStatus().equals(ThingStatus.REMOVED)) {
-            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.NONE, "Bridge has been removed.");
-        }
         logger.debug("Set status to {}", getThing().getStatusInfo());
     }
 
