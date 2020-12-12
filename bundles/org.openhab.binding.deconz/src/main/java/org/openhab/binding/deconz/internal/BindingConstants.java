6be21558a8542050d755c9393a61033dd0994416
J-N-K <J-N-K@users.noreply.github.com>
2020-12-13 00:30:43 +0100

    [deconz] initial support for scenes (#9345)
    * initial support for scenes
    * add documentation

--- bundles/org.openhab.binding.deconz/src/main/java/org/openhab/binding/deconz/internal/BindingConstants.java
+++ bundles/org.openhab.binding.deconz/src/main/java/org/openhab/binding/deconz/internal/BindingConstants.java
@@ -116,6 +116,7 @@ public class BindingConstants {
     public static final String CHANNEL_LOCK = "lock";
     public static final String CHANNEL_EFFECT = "effect";
     public static final String CHANNEL_EFFECT_SPEED = "effectSpeed";
+    public static final String CHANNEL_SCENE = "scene";
 
     // channel uids
     public static final ChannelTypeUID CHANNEL_EFFECT_TYPE_UID = new ChannelTypeUID(BINDING_ID, CHANNEL_EFFECT);
