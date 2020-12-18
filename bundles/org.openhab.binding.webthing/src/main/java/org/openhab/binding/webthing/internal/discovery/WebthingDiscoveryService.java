5a46b10b4f2a58681792ef32b1c1f69c9dddbc99
Gregor Roth <gregor.roth@web.de>
2020-12-18 11:03:37 +0100

    typo fixed
    Signed-off-by: Gregor Roth <gregor.roth@web.de>

--- bundles/org.openhab.binding.webthing/src/main/java/org/openhab/binding/webthing/internal/discovery/WebthingDiscoveryService.java
+++ bundles/org.openhab.binding.webthing/src/main/java/org/openhab/binding/webthing/internal/discovery/WebthingDiscoveryService.java
@@ -174,9 +174,9 @@ public class WebthingDiscoveryService extends AbstractDiscoveryService implement
                 for (var discoveryResult : discoverWebThing(serviceInfo)) {
                     results.add(discoveryResult);
                     thingDiscovered(discoveryResult);
-                    logger.debug("WebThing '{}' (uri: {}, id: {}, schema: {}) discovered", discoveryResult.getLabel(),
+                    logger.debug("WebThing '{}' (uri: {}, id: {}, schemas: {}) discovered", discoveryResult.getLabel(),
                             discoveryResult.getProperties().get("webThingURI"),
-                            discoveryResult.getProperties().get("id"), discoveryResult.getProperties().get("schema"));
+                            discoveryResult.getProperties().get("id"), discoveryResult.getProperties().get("schemas"));
                 }
             } catch (Exception e) {
                 logger.warn("error occurred by discovering {}", serviceInfo.getNiceTextString(), e);
@@ -263,7 +263,7 @@ public class WebthingDiscoveryService extends AbstractDiscoveryService implement
             var thingUID = new ThingUID(THING_TYPE_UID, id);
             Map<String, Object> properties = new HashMap<>(1);
             properties.put("id", id);
-            properties.put("schema", description.contextKeyword);
+            properties.put("schemas", description.contextKeyword);
             return Optional.of(DiscoveryResultBuilder.create(thingUID).withThingType(THING_TYPE_UID)
                     .withProperty("webThingURI", uri).withLabel(description.title).withProperties(properties)
                     .withRepresentationProperty("id").build());
