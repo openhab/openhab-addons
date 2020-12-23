7dc582b147006e3c30f1cf5fd013df68a78b7411
J-N-K <J-N-K@users.noreply.github.com>
2020-12-23 19:45:26 +0100

    [tr064] fix wrong soapconnector (#9488)
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>
    Signed-off-by: Andras Uhrin <andras.uhrin@gmail.com>

--- bundles/org.openhab.binding.tr064/src/main/java/org/openhab/binding/tr064/internal/Tr064RootHandler.java
+++ bundles/org.openhab.binding.tr064/src/main/java/org/openhab/binding/tr064/internal/Tr064RootHandler.java
@@ -133,6 +133,7 @@ public class Tr064RootHandler extends BaseBridgeHandler implements PhonebookProv
         }
 
         endpointBaseURL = "http://" + config.host + ":49000";
+        soapConnector = new SOAPConnector(httpClient, endpointBaseURL);
         updateStatus(ThingStatus.UNKNOWN);
 
         connectFuture = scheduler.scheduleWithFixedDelay(this::internalInitialize, 0, RETRY_INTERVAL, TimeUnit.SECONDS);
