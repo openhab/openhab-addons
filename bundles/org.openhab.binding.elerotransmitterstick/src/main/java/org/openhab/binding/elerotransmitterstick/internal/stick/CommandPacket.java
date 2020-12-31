f5ea68dabcc4a8d3f9a8f5d46d6e89f03a710541
vbier <volker.bier@web.de>
2020-12-31 11:01:01 +0100

    [elerotransmitterstick] Drop responses that do not fit the command that was sent. (#9562)
    Signed-off-by: vbier <volker.bier@web.de>
    Signed-off-by: Andras Uhrin <andras.uhrin@gmail.com>

--- bundles/org.openhab.binding.elerotransmitterstick/src/main/java/org/openhab/binding/elerotransmitterstick/internal/stick/CommandPacket.java
+++ bundles/org.openhab.binding.elerotransmitterstick/src/main/java/org/openhab/binding/elerotransmitterstick/internal/stick/CommandPacket.java
@@ -28,7 +28,7 @@ public class CommandPacket {
         data = new byte[bytes.length + 1];
         System.arraycopy(bytes, 0, data, 0, bytes.length);
 
-        data[bytes.length] = checksum(data);
+        data[bytes.length] = checksum(bytes);
     }
 
     public byte[] getBytes() {
@@ -36,7 +36,7 @@ public class CommandPacket {
     }
 
     public long getResponseTimeout() {
-        if (data[2] == EASY_CHECK) {
+        if (isEasyCheck()) {
             return 1000;
         }
 
@@ -52,6 +52,10 @@ public class CommandPacket {
 
         val = val % 256;
         return (byte) (256 - val);
+    }
+
+    public boolean isEasyCheck() {
+        return data[2] == EASY_CHECK;
     }
 
     @Override
