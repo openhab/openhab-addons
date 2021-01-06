ad6e17a720836ea40d9f75862bc8af3f4b82375a
chingon007 <tron81@gmx.de>
2021-01-06 08:46:37 +0100

    First commmit of org.openhab.binding.haassohnpelletoven
    Signed-off-by: chingon007 <tron81@gmx.de>
    Signed-off-by: chingon007 <76529461+chingon007@users.noreply.github.com>
    Signed-off-by: chingon007 <tron81@gmx.de>
    Signed-off-by: Christian Feininger <tron81@gmx.de>

---
+++ bundles/org.openhab.binding.haassohnpelletoven/src/main/java/org/openhab/binding/haassohnpelletoven/data/HaassohnpelletstoveJsonData.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.haassohnpelletoven.data;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * The {@link HaassohnpelletstoveJsonData} is the Java class used to map the JSON
+ * response to a Oven request.
+ *
+ * @author Christian Feininger - Initial contribution
+ */
+public class HaassohnpelletstoveJsonData {
+    metadata meta;
+    boolean prg;
+    boolean wprg;
+    String mode;
+    String sp_temp;
+    String is_temp;
+    String ht_char;
+    wprogram[] weekprogram;
+    err[] error;
+    boolean eco_mode;
+    boolean pgi;
+    String ignitions;
+    String on_time;
+    String consumption;
+    String maintenance_in;
+    String cleaning_in;
+
+    public String GetNonce() {
+        return this.meta.Getnonce();
+    }
+
+    /**
+     * Returns the is Temperature of the Oven
+     *
+     * @return
+     */
+    public String getisTemp() {
+        return is_temp;
+    }
+
+    public HaassohnpelletstoveJsonData getResponse() {
+        return this;
+    }
+
+    public class metadata {
+        String sw_version;
+        String hw_version;
+        String bootl_version;
+        String wifi_sw_version;
+        String wifi_bootl_version;
+        String sn;
+        String typ;
+        String language;
+        String nonce;
+        String eco_editable;
+        String ts;
+        String ean;
+        boolean rau;
+        String[] wlan_features;
+
+        public String Getnonce() {
+            return nonce;
+        }
+    }
+
+    public class err {
+        String time;
+        String nr;
+    }
+
+    public class wprogram {
+        String day;
+        String begin;
+        String end;
+        String temp;
+    }
+
+    public @Nullable String getMode() {
+        return mode;
+    }
+
+    public @Nullable String getspTemp() {
+        return sp_temp;
+    }
+
+    public boolean getPrg() {
+        return prg;
+    }
+}
---
+++ bundles/org.openhab.binding.haassohnpelletoven/src/main/java/org/openhab/binding/haassohnpelletoven/encryption/MD5Utils.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.haassohnpelletoven.encryption;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link MD5Utils} is responsible for generating the MD5 hash
+ *
+ *
+ * @author Christian Feininger - Initial contribution
+ */
+@NonNullByDefault
+public class MD5Utils {
+
+    private static final Charset UTF_8 = StandardCharsets.UTF_8;
+
+    private static byte[] digest(byte[] input) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalArgumentException(e);
+        }
+        byte[] result = md.digest(input);
+        return result;
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
+    public static String GetMD5String(String input) {
+        byte[] md5InBytes = MD5Utils.digest(input.getBytes(UTF_8));
+        return bytesToHex(md5InBytes);
+    }
+}
