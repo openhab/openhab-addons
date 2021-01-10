ade859f8823c4803550936334de71b6293bbc02e
Jan-Philipp Bolle <pbolle@gmx.de>
2021-01-10 15:20:40 +0100

    add bindingstub
    Signed-off-by: Jan-Philipp Bolle <pbolle@gmx.de>

---
+++ bundles/org.openhab.binding.novelanheatpump/src/main/java/org/openhab/binding/novelanheatpump/internal/NovelanHeatpumpConfiguration.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.novelanheatpump.internal;
+
+/**
+ * The {@link NovelanHeatpumpConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Jan-Philipp Bolle - Initial contribution
+ */
+public class NovelanHeatpumpConfiguration {
+
+    /**
+     * Sample configuration parameter. Replace with your own.
+     */
+    // public String config1;
+    public Integer port;
+    public Integer refresh;
+    public String address;
+}
