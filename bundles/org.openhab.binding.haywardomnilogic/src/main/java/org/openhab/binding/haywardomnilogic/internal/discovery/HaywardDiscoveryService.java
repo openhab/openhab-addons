7af05395c5b9166e9a314baf63648267d9878d61
matchews <mmyers75@icloud.com>
2021-01-13 12:48:33 -0500

    update headers 2021
    Signed-off-by: matchews <mmyers75@icloud.com>

--- bundles/org.openhab.binding.haywardomnilogic/src/main/java/org/openhab/binding/haywardomnilogic/internal/discovery/HaywardDiscoveryService.java
+++ bundles/org.openhab.binding.haywardomnilogic/src/main/java/org/openhab/binding/haywardomnilogic/internal/discovery/HaywardDiscoveryService.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2020 Contributors to the openHAB project
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -97,7 +97,6 @@ public class HaywardDiscoveryService extends AbstractDiscoveryService implements
         HaywardBridgeHandler bridgehandler = discoveryBridgehandler;
 
         if (bridgehandler != null) {
-
             // Find Backyard
             names = bridgehandler.evaluateXPath("//Backyard/Name/text()", xmlResponse);
 
