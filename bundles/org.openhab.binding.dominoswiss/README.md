c77ebdcf04312efcac526beed44f36bc440b4d2c
Frieso Aeschbacher <frieso.aeschbacher@gmail.com>
2021-01-17 07:51:35 +0100

    update
    Signed-off-by: Frieso Aeschbacher <frieso.aeschbacher@gmail.com>

---
+++ bundles/org.openhab.binding.dominoswiss/README.md
@@ -0,0 +1,110 @@
+# dominoswiss Binding
+
+This binding allows the controll of rollershutters, using an egate as gateway and dominoswiss radio receivers. The egate-gateway is connectet via ethernet to openhab and sends it's commands via radio to all rollershutters. See https://www.brelag.com/ for more information.
+
+
+## Supported Things
+
+EGate: Dominoswiss EGate Server. The egate is the gateway which sends the commands to the connected rollershutters. 
+Blind: represents one rollershutter, that can be controlled via EGate
+
+## Discovery
+
+Unfortunately no automatic discovery is possible due to protocol restrictions. Every rollershutter must be known by egate and can be called by it's number of storage-place on egate gateway.
+
+
+## Binding Configuration
+
+_If your binding requires or supports general configuration settings, please create a folder ```cfg``` and place the configuration file ```<bindingId>.cfg``` inside it. In this section, you should link to this file and provide some information about the options. The file could e.g. look like:_
+
+```
+# Configuration for the Philips Hue Binding
+#
+# Default secret key for the pairing of the Philips Hue Bridge.
+# It has to be between 10-40 (alphanumeric) characters
+# This may be changed by the user for security reasons.
+secret=openHABSecret
+```
+
+_Note that it is planned to generate some part of this based on the information that is available within ```src/main/resources/OH-INF/binding``` of your binding._
+
+_If your binding does not offer any generic configurations, you can remove this section completely._
+
+## Thing Configuration
+
+The bridge "egate" has one channel "getconfig" which returns the config of this bridge. 
+The egate is configured with both an ipaddress and a port. The ip address might be in format like 192.168.1.1 or as the hostname representing the egate in your network.  Port is represented as a number.`
+
+```java
+Bridge dominoswiss:egate:myEgate "My EGate Server" @ "Home" [ ipAddres="localhost", port="5700" ]
+```
+
+
+The thing blind represents one blind on the egate. Each blind is represented by an id set on your egate.
+  
+```java 
+Thing blind officeBlind "Office" @ "1stFloor" [ id="1"]
+```
+
+The blind-Thing has the following channels:
+| Channel Type ID  | Item Type | Description                                                                                       |   |   |
+|------------------|-----------|---------------------------------------------------------------------------------------------------|---|---|
+| pulseup            |Blind      |sends one pulse up to this blind.|
+| pulsedown          |Blind      |sends one pulse down to this blind|
+| continuousup       |Blind      |sends a continuous up to this blind. The blind will automatically stop as it is fully up.|
+|continuousdown      |Blind      | send a continous down to this blind. The blind will automatically stop as it is fully down.|
+| stop               |Blind      | stop the action of the blind. The command will be imadiatly sent to the blind.|
+| shutter            |Blind      |this is used to bind the channel to the shutter item. There are no further rules needed this channel will handel the up/down/stop commands. See example for usage.|
+| tilt               |Blind      | same as shutter, this will handel all up/down/stop - tilt commands to be used with the shutter-item.| 
+| tiltup             |Blind      | sends 3 pulse-up commands to this blind to tilt the blind. If your blind needs more than 3 pulse-up, create a rule yourself with three pluse-up commands. Between each pulse-up you should wait 150ms to let the command be processed. 
+| tiltdown           |Blind      |sends 3 pulse-down commands to this blind to tilt the blind. If your blind needs more than 3 pulse-down, create a rule yourself with three pluse-down commands. Between each pulse-down you should wait 150ms to let the command be processed. |
+
+## Full Example
+
+Sample things file:
+
+``` java
+Bridge dominoswiss:egate:myEgate "My EGate Server" @ "Home" [ ipAddres="localhost", port="5500" ]
+{
+    Thing blind officeBlind "Office" @ "1stFloor" [ id="1"]
+    Thing blind diningRoomBlind "Dining Room" @ "EG" [id="2"]
+    Thing blind kitchenBlind "Kitchen" @ "EG" [id="12"]
+}
+```
+
+
+Sample items file:
+
+``` java
+Rollershutter OfficeBlindShutter "Office blind" <rollershutter>  (g_blinds) { channel="dominoswiss:blind:myEgate:officeBlind:shutter"}
+
+Rollershutter OfficeBlindShutterTilt "Tilt Office" <rollershutter>  (g_blinds_tilt) { channel="dominoswiss:blind:myEgate:bueroBlind:tilt"}
+
+
+```
+
+Sample sitemap file
+
+``` java
+Switch  item=OfficeBlindShutter
+Switch  item=OfficeBlindShutterTilt
+```
+
+
+Sample rule file
+
+This example moves the blind of the office up as the sun passed 110 azimuth (so the sun is no longer shineing directly into the office).
+
+```java
+rule "OneSide up"
+when 
+    Item Azimuth changed 
+then 
+    val azimuth = Math::round((Azimuth.state as DecimalType).intValue)
+    if (azimuth == 110)
+    {
+        OfficeBlindShutter.sendCommand(UP)
+    }
+end
+
+```
---
+++ bundles/org.openhab.binding.dominoswiss/src/main/java/org/openhab/binding/dominoswiss/internal/eGateHandler.java
@@ -0,0 +1,380 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.dominoswiss.internal;
+
+import static org.openhab.binding.dominoswiss.internal.dominoswissBindingConstants.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.config.core.Configuration;
+import org.openhab.core.thing.Bridge;
+import org.openhab.core.thing.Channel;
+import org.openhab.core.thing.ChannelUID;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.thing.ThingStatus;
+import org.openhab.core.thing.ThingStatusDetail;
+import org.openhab.core.thing.ThingUID;
+import org.openhab.core.thing.binding.BaseBridgeHandler;
+import org.openhab.core.types.Command;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link EgateHandler} is responsible for handling commands, which are
+ * sent to one of the channels.
+ *
+ * @author Frieso Aeschbacher - Initial contribution
+ */
+@NonNullByDefault
+public class eGateHandler extends BaseBridgeHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(eGateHandler.class);
+    private @Nullable Socket egateSocket;
+
+    private int port;
+    private @Nullable String host;
+    private static final int SOCKET_TIMEOUT = 250;
+    private final Object lock = new Object();
+    private @Nullable BufferedWriter writer;
+    private @Nullable BufferedReader reader;
+    private @Nullable ScheduledFuture<?> refreshJob;
+    private @Nullable Map<String, ThingUID> registeredBlinds;
+    private final ScheduledExecutorService scheduler;
+
+    public eGateHandler(Bridge thing) {
+        super(thing);
+        registeredBlinds = new HashMap<String, ThingUID>();
+        scheduler = Executors.newScheduledThreadPool(1);
+    }
+
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        if (channelUID.getId().equals(GETCONFIG)) {
+            sendCommand("EthernetGet;\r");
+        }
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    public void initialize() {
+        logger.debug("Initializing the Dominoswiss EGate bridge handler.");
+
+        Configuration config = this.getConfig();
+        host = (String) config.get("ipAddress");
+        port = 1318; // default Port
+        if (!config.get("port").toString().isEmpty()) {
+            port = Integer.parseInt(config.get("port").toString()); // take configured Port
+        }
+
+        if (host != null && port > 0) {
+            // Create a socket to eGate
+            synchronized (this.lock) {
+                try {
+                    egateSocket = new Socket();
+                    egateSocket.connect(new InetSocketAddress(host, port));
+                    egateSocket.setSoTimeout(SOCKET_TIMEOUT);
+                    writer = new BufferedWriter(new OutputStreamWriter(egateSocket.getOutputStream()));
+                    writer.write("SilenceModeSet;Value=0;" + CR);
+                    writer.flush();
+
+                } catch (UnknownHostException e) {
+                    logger.error("unknown socket host {}", host);
+                    try {
+                        egateSocket.close();
+                        updateStatus(ThingStatus.OFFLINE);
+                    } catch (IOException e1) {
+                        if (logger.isErrorEnabled()) {
+                            logger.error("EGate Socket not closed {}", e1.toString());
+                        }
+                    }
+                    egateSocket = null;
+                } catch (SocketException e) {
+                    if (logger.isErrorEnabled()) {
+                        logger.error("{}", e.getLocalizedMessage());
+                    }
+                    try {
+                        egateSocket.close();
+                        updateStatus(ThingStatus.OFFLINE);
+                    } catch (IOException e1) {
+                        if (logger.isErrorEnabled()) {
+                            logger.error("EGate Socket not closed {}", e1.toString());
+                        }
+                    }
+                    egateSocket = null;
+                } catch (Exception e) {
+                    if (logger.isErrorEnabled()) {
+                        logger.error(
+                                "Error while establishing connection to Dominoswiss eGate Server with Port: {} and IP: {} Error: {}",
+                                port, host, e.toString());
+                    }
+                    updateStatus(ThingStatus.OFFLINE);
+                }
+                if (egateSocket != null) {
+                    updateStatus(ThingStatus.ONLINE);
+                }
+                startAutomaticRefresh();
+                if (logger.isDebugEnabled()) {
+                    logger.debug("EGate Handler connected and online, Status {} ",
+                            this.getThing().getStatus().toString());
+                }
+            }
+        } else {
+            if (logger.isErrorEnabled()) {
+                logger.error("Invalid IP address for dominoswiss eGate or wrong port : '{}'/'{}'", host, port);
+            }
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.OFFLINE.CONFIGURATION_ERROR,
+                    "Cannot connect to dominoswiss eGate gateway. host IP address or port are not set.");
+        }
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    public void dispose() {
+        try {
+            egateSocket.close();
+            refreshJob.cancel(true);
+            logger.debug("EGate Handler connection closed, disposing");
+        } catch (IOException e) {
+            if (logger.isErrorEnabled()) {
+                logger.error("EGate Handler Error on dispose: {} ", e.toString());
+            }
+
+        }
+    }
+
+    @SuppressWarnings("null")
+    public synchronized boolean isConnected() {
+        if (egateSocket == null) {
+            return false;
+        }
+
+        // NOTE: isConnected() returns true once a connection is made and will
+        // always return true even after the socket is closed
+        // http://stackoverflow.com/questions/10163358/
+        return egateSocket.isConnected() && !egateSocket.isClosed();
+    }
+
+    /**
+     * Possible Instructions are:
+     * FssTransmit 1 Kommandoabsetzung (Controller > eGate > Dominoswiss)
+     * FssReceive 2 Empfangenes Funkpaket (Dominoswiss > eGate > Controller)
+     *
+     */
+
+    public void tiltUp(String id) {
+        for (int i = 0; i < 3; i++) {
+            pulseUp(id);
+            try {
+                Thread.sleep(150); // sleep to not confues the blinds
+            } catch (InterruptedException e) {
+                if (logger.isErrorEnabled()) {
+                    logger.error("EGate tipUp error: {}", e.toString());
+                }
+            }
+        }
+    }
+
+    public void tiltDown(String id) {
+        for (int i = 0; i < 3; i++) {
+            pulseDown(id);
+            try {
+                Thread.sleep(150);// sleep to not confues the blinds
+            } catch (InterruptedException e) {
+                if (logger.isErrorEnabled()) {
+                    logger.error("EGate tipdown error: {} ", e.toString());
+                }
+            }
+        }
+    }
+
+    public void pulseUp(String id) {
+        sendCommand("Instruction=1;ID=" + id + ";Command=1;Priority=1;CheckNr=3415347;" + CR);
+    }
+
+    public void pulseDown(String id) {
+        sendCommand("Instruction=1;ID=" + id + ";Command=2;Priority=1;CheckNr=2764516;" + CR);
+    }
+
+    public void continuousUp(String id) {
+        sendCommand("Instruction=1;ID=" + id + ";Command=3;Priority=1;CheckNr=2867016;" + CR, 20000);
+    }
+
+    public void continuousDown(String id) {
+        sendCommand("Instruction=1;ID=" + id + ";Command=4;Priority=1;CheckNr=973898;" + CR, 20000);
+    }
+
+    public void stop(String id) {
+        sendCommand("Instruction=1;ID=" + id + ";Command=5;Priority=1;CheckNr=5408219;" + CR);
+    }
+
+    @SuppressWarnings("null")
+    public void registerBlind(String id, ThingUID uid) {
+        logger.debug("Registring Blind id {} with thingUID {}", id, uid);
+        registeredBlinds.put(id, uid);
+    }
+
+    /**
+     * Send a command to the eGate Server.
+     */
+
+    private void sendCommand(String command) {
+        sendCommand(command, SOCKET_TIMEOUT);
+    }
+
+    @SuppressWarnings("null")
+    private synchronized void sendCommand(String command, int timeout) {
+        logger.debug("EGate got command: {}", command);
+
+        if (!isConnected()) {
+            logger.debug("no connection to Dominoswiss eGate server when trying to send command, returning...");
+            return;
+        }
+
+        // Send plain string to eGate Server,
+        logger.debug("Sending command: {}", command);
+        try {
+
+            // BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(egateSocket.getOutputStream()));
+
+            egateSocket.setSoTimeout(SOCKET_TIMEOUT);
+            writer.write(command);
+            writer.flush();
+            logger.debug("Sent message");
+        } catch (IOException e) {
+            logger.error("Error while sending command {} to Dominoswiss eGate Server {} ", command, e.toString());
+        }
+        /*
+         * try {
+         * BufferedReader reader = new BufferedReader(new InputStreamReader(egateSocket.getInputStream()));
+         * logger.debug("Socket State: " + egateSocket.isConnected() + " to: " + egateSocket.toString());
+         * egateSocket.setSoTimeout(SOCKET_TIMEOUT);
+         * while (!reader.ready()) {
+         *
+         * }
+         * String input = reader.readLine();
+         * logger.debug("Reader got from EGATE: {}", input);
+         *
+         * } catch (IOException e) {
+         * logger.error("Error while reading command {} from Dominoswiss eGate Server {} ", command, e.toString());
+         * }
+         */
+    }
+
+    private void startAutomaticRefresh() {
+        Runnable runnable = new Runnable() {
+            @SuppressWarnings("null")
+            @Override
+            public void run() {
+                try {
+                    if (reader == null) {
+                        reader = new BufferedReader(new InputStreamReader(egateSocket.getInputStream()));
+                    }
+
+                    // logger.debug("Socket State: " + egateSocket.isConnected() + " to: " + egateSocket.toString());
+                    // egateSocket.setSoTimeout(SOCKET_TIMEOUT);
+                    if (reader.ready()) {
+                        String input = reader.readLine();
+                        logger.debug("Reader got from EGATE: {}", input);
+                        onData(input);
+                    }
+                } catch (IOException e) {
+                    logger.error("Error while reading command from Dominoswiss eGate Server {} ", e.toString());
+                    updateStatus(ThingStatus.OFFLINE);
+                }
+            }
+        };
+        refreshJob = scheduler.scheduleWithFixedDelay(runnable, 0, 1, TimeUnit.SECONDS);
+    }
+
+    /**
+     * Finds and returns a child thing for a given UID of this bridge.
+     *
+     * @param uid uid of the child thing
+     * @return child thing with the given uid or null if thing was not found
+     */
+    public @Nullable Thing getThingByUID(ThingUID uid) {
+        Bridge bridge = getThing();
+
+        List<Thing> things = bridge.getThings();
+
+        for (Thing thing : things) {
+            if (thing.getUID().equals(uid)) {
+                return thing;
+            }
+        }
+
+        return null;
+    }
+
+    @SuppressWarnings("null")
+    protected void onData(String input) {
+
+        // Instruction=2;ID=19;Command=1;Value=0;Priority=0;
+
+        Map<String, String> map = new HashMap<String, String>();
+        Thing blind;
+        // split on ;
+        String[] parts = input.split(";");
+
+        for (int i = 0; i < parts.length; i += 2) {
+            map.put(parts[i], parts[i + 1]);
+        }
+
+        // only use FSSReceive Commands
+        if (map.get("Instruction").toString() == "2") {
+            String id = map.get("ID");
+            ThingUID uid = registeredBlinds.get(map.get(id));
+            if (uid != null) {
+                blind = getThingByUID(uid);
+
+                List<Channel> channels = blind.getChannels();
+                logger.debug("Channels: {} of Blind {}", channels.toString(), blind.getConfiguration());
+
+                switch (map.get("Command")) {
+                    case "1":
+                        break;
+
+                    case "2":
+                        break;
+
+                    case "3":
+                        break;
+
+                    case "4":
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                logger.error("Error no blind registered in eGate Bridge. Please add some blinds");
+            }
+        }
+    }
+}
