988e0c79488f83d97648c4029d348f635656f288
Matthew Skinner <matt@pcmus.com>
2021-01-23 11:39:41 +1100

    Spotless fixes
    Signed-off-by: Matthew Skinner <matt@pcmus.com>

--- bundles/org.openhab.binding.mqtt.espmilighthub/src/main/java/org/openhab/binding/mqtt/espmilighthub/internal/discovery/EspMilightHubDiscoveryService.java
+++ bundles/org.openhab.binding.mqtt.espmilighthub/src/main/java/org/openhab/binding/mqtt/espmilighthub/internal/discovery/EspMilightHubDiscoveryService.java
@@ -67,7 +67,7 @@ public class EspMilightHubDiscoveryService extends AbstractMQTTDiscovery {
                 index = cutTopic.indexOf("/");
                 if (index != -1) {
                     String globeType = (cutTopic.substring(0, index));
-                    String remoteGroupID = (cutTopic.substring(index+1, index+2));
+                    String remoteGroupID = (cutTopic.substring(index + 1, index + 2));
                     // openHAB's framework has better code for handling groups then the firmware does
                     if (!remoteGroupID.equals("0")) {// Users can manually add group 0 things if they wish
                         publishDevice(connectionBridge, connection, topic, remoteCode, globeType, remoteGroupID);
