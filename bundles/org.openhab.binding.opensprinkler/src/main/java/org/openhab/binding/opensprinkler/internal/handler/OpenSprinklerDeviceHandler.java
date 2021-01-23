29d2d27e035cdf163f24125a8ba72de689c7af0b
Matthew Skinner <matt@pcmus.com>
2021-01-23 21:09:13 +1100

    Fix bug
    Signed-off-by: Matthew Skinner <matt@pcmus.com>

--- bundles/org.openhab.binding.opensprinkler/src/main/java/org/openhab/binding/opensprinkler/internal/handler/OpenSprinklerDeviceHandler.java
+++ bundles/org.openhab.binding.opensprinkler/src/main/java/org/openhab/binding/opensprinkler/internal/handler/OpenSprinklerDeviceHandler.java
@@ -68,7 +68,7 @@ public class OpenSprinklerDeviceHandler extends OpenSprinklerBaseHandler {
                 updateState(channel, new QuantityType<ElectricCurrent>(localAPI.currentDraw(), MILLI(Units.AMPERE)));
                 break;
             case SENSOR_SIGNAL_STRENGTH:
-                updateState(channel, new QuantityType<Dimensionless>(localAPI.flowSensorCount(), Units.DECIBEL));
+                updateState(channel, new QuantityType<Dimensionless>(localAPI.signalStrength(), Units.DECIBEL));
                 break;
             case SENSOR_FLOW_COUNT:
                 updateState(channel, new QuantityType<Dimensionless>(localAPI.flowSensorCount(), Units.ONE));
