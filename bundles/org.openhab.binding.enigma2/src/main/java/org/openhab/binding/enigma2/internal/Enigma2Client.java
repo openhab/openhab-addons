3d4b40aa7be768bcf7b0eb4325b24be2db30ae7f
Kai Kreuzer <kai@openhab.org>
2021-01-24 15:06:00 +0100

    Merge pull request from GHSA-r2hc-pmr7-4c9r
    * Configured XML parsers to resist XXE attacks
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * added fix for avmfritz
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * added fix for sonos
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * added fix for vitotronic and bosesoundtouch
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * changed avmfritz to singleton pattern
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * addressed roku binding
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * address all uses of DocumentBuilderFactory
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * fixed other occurrences in roku binding
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    Signed-off-by: Koen Schockaert <ks@qbus.be>

--- bundles/org.openhab.binding.enigma2/src/main/java/org/openhab/binding/enigma2/internal/Enigma2Client.java
+++ bundles/org.openhab.binding.enigma2/src/main/java/org/openhab/binding/enigma2/internal/Enigma2Client.java
@@ -81,9 +81,16 @@ public class Enigma2Client {
     private final Enigma2HttpClient enigma2HttpClient;
     private final DocumentBuilderFactory factory;
 
-    public Enigma2Client(String host, @Nullable String user, @Nullable String password, int requestTimeout) {
-        this.enigma2HttpClient = new Enigma2HttpClient(requestTimeout);
-        this.factory = DocumentBuilderFactory.newInstance();
+    public Enigma2Client(String host, @Nullable String user, @Nullable String password, int requestTimeout)
+            throws ParserConfigurationException {
+        enigma2HttpClient = new Enigma2HttpClient(requestTimeout);
+        factory = DocumentBuilderFactory.newInstance();
+        // see https://cheatsheetseries.owasp.org/cheatsheets/XML_External_Entity_Prevention_Cheat_Sheet.html
+        factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+        factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        factory.setXIncludeAware(false);
+        factory.setExpandEntityReferences(false);
         if (StringUtils.isNotEmpty(user) && StringUtils.isNotEmpty(password)) {
             this.host = "http://" + user + ":" + password + "@" + host;
         } else {
