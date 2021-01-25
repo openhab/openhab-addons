a006924140390525f3cc4315fe61adefccaba1e0
Martin van Wingerden <martin@martinvw.nl>
2021-01-25 22:56:08 +0100

    [HEOS] Update pause/stop handling (#9941)
    Update PAUSE/STOP handling depending on the item that is currently playing.
    
    Signed-off-by: Martin van Wingerden <martin@martinvw.nl>
    Signed-off-by: John Marshall <john.marshall.au@gmail.com>

--- bundles/org.openhab.binding.heos/src/main/java/org/openhab/binding/heos/internal/resources/HeosEventListener.java
+++ bundles/org.openhab.binding.heos/src/main/java/org/openhab/binding/heos/internal/resources/HeosEventListener.java
@@ -12,14 +12,11 @@
  */
 package org.openhab.binding.heos.internal.resources;
 
-import java.util.EventListener;
-
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.openhab.binding.heos.internal.api.HeosEventController;
 import org.openhab.binding.heos.internal.exception.HeosFunctionalException;
 import org.openhab.binding.heos.internal.json.dto.HeosEventObject;
 import org.openhab.binding.heos.internal.json.dto.HeosResponseObject;
-import org.openhab.binding.heos.internal.json.payload.Media;
 
 /**
  * The {@link HeosEventListener } is an Event Listener
@@ -28,15 +25,14 @@ import org.openhab.binding.heos.internal.json.payload.Media;
  * implement this class and register itself at the {@link HeosEventController}
  *
  * @author Johannes Einig - Initial contribution
+ * @author Martin van Wingerden - change handling of stop/pause depending on playing item type
  */
 @NonNullByDefault
-public interface HeosEventListener extends EventListener {
+public interface HeosEventListener extends HeosMediaEventListener {
 
     void playerStateChangeEvent(HeosEventObject eventObject);
 
     void playerStateChangeEvent(HeosResponseObject<?> responseObject) throws HeosFunctionalException;
-
-    void playerMediaChangeEvent(String pid, Media media);
 
     void bridgeChangeEvent(String event, boolean success, Object command);
 }
