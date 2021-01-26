d5ebf5b6b9ef5267a68a94a2d708963207f84653
Sami Salonen <ssalonen@gmail.com>
2021-01-26 18:18:32 +0200

    [modbus] bit profile: unit tests for inverted parameter
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- bundles/org.openhab.binding.modbus/src/test/java/org/openhab/binding/modbus/internal/profiles/ModbusBitProfileTest.java
+++ bundles/org.openhab.binding.modbus/src/test/java/org/openhab/binding/modbus/internal/profiles/ModbusBitProfileTest.java
@@ -22,6 +22,7 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -50,62 +51,70 @@ public class ModbusBitProfileTest {
                 // 0b0011_0100_0000_1111 = 13327 sint16
                 //
                 // low byte
-                Arguments.of("1", "0", (short) 0b0011_0100_0000_1111),
-                Arguments.of("1", "1", (short) 0b0011_0100_0000_1111),
-                Arguments.of("1", "2", (short) 0b0011_0100_0000_1111),
-                Arguments.of("1", "3", (short) 0b0011_0100_0000_1111),
+                Arguments.of("1", "0", null, (short) 0b0011_0100_0000_1111),
+                Arguments.of("1", "1", null, (short) 0b0011_0100_0000_1111),
+                Arguments.of("1", "2", null, (short) 0b0011_0100_0000_1111),
+                Arguments.of("1", "3", null, (short) 0b0011_0100_0000_1111),
                 // 2nd low byte
-                Arguments.of("0", "4", (short) 0b0011_0100_0000_1111),
-                Arguments.of("0", "5", (short) 0b0011_0100_0000_1111),
-                Arguments.of("0", "6", (short) 0b0011_0100_0000_1111),
-                Arguments.of("0", "7", (short) 0b0011_0100_0000_1111),
+                Arguments.of("0", "4", null, (short) 0b0011_0100_0000_1111),
+                Arguments.of("0", "5", null, (short) 0b0011_0100_0000_1111),
+                Arguments.of("0", "6", null, (short) 0b0011_0100_0000_1111),
+                Arguments.of("0", "7", null, (short) 0b0011_0100_0000_1111),
                 // 3rd low byte
-                Arguments.of("0", "8", (short) 0b0011_0100_0000_1111),
-                Arguments.of("0", "9", (short) 0b0011_0100_0000_1111),
-                Arguments.of("1", "10", (short) 0b0011_0100_0000_1111),
-                Arguments.of("0", "11", (short) 0b0011_0100_0000_1111),
+                Arguments.of("0", "8", null, (short) 0b0011_0100_0000_1111),
+                Arguments.of("0", "9", null, (short) 0b0011_0100_0000_1111),
+                Arguments.of("1", "10", null, (short) 0b0011_0100_0000_1111),
+                Arguments.of("0", "11", null, (short) 0b0011_0100_0000_1111),
                 // hi byte
-                Arguments.of("1", "12", (short) 0b0011_0100_0000_1111),
-                Arguments.of("1", "13", (short) 0b0011_0100_0000_1111),
-                Arguments.of("0", "14", (short) 0b0011_0100_0000_1111),
-                Arguments.of("0", "15", (short) 0b0011_0100_0000_1111),
+                Arguments.of("1", "12", null, (short) 0b0011_0100_0000_1111),
+                Arguments.of("1", "13", null, (short) 0b0011_0100_0000_1111),
+                Arguments.of("0", "14", null, (short) 0b0011_0100_0000_1111),
+                Arguments.of("0", "15", null, (short) 0b0011_0100_0000_1111),
+
+                // Some tests with inverted parameter
+                Arguments.of("0", "13", "true", (short) 0b0011_0100_0000_1111),
+                Arguments.of("1", "14", "TRue", (short) 0b0011_0100_0000_1111),
+                Arguments.of("1", "13", "false", (short) 0b0011_0100_0000_1111),
+                Arguments.of("0", "14", "false", (short) 0b0011_0100_0000_1111),
+                Arguments.of("1", "13", "", (short) 0b0011_0100_0000_1111),
+                Arguments.of("0", "14", "", (short) 0b0011_0100_0000_1111),
 
                 //
                 // 0b1011_0100_0000_1111 = -19441 sint16 (or 46095 uint16)
                 //
                 // low byte
-                Arguments.of("1", "0", (short) 0b1011_0100_0000_1111),
-                Arguments.of("1", "1", (short) 0b1011_0100_0000_1111),
-                Arguments.of("1", "2", (short) 0b1011_0100_0000_1111),
-                Arguments.of("1", "3", (short) 0b1011_0100_0000_1111),
+                Arguments.of("1", "0", null, (short) 0b1011_0100_0000_1111),
+                Arguments.of("1", "1", null, (short) 0b1011_0100_0000_1111),
+                Arguments.of("1", "2", null, (short) 0b1011_0100_0000_1111),
+                Arguments.of("1", "3", null, (short) 0b1011_0100_0000_1111),
                 // 2nd low byte
-                Arguments.of("0", "4", (short) 0b1011_0100_0000_1111),
-                Arguments.of("0", "5", (short) 0b1011_0100_0000_1111),
-                Arguments.of("0", "6", (short) 0b1011_0100_0000_1111),
-                Arguments.of("0", "7", (short) 0b1011_0100_0000_1111),
+                Arguments.of("0", "4", null, (short) 0b1011_0100_0000_1111),
+                Arguments.of("0", "5", null, (short) 0b1011_0100_0000_1111),
+                Arguments.of("0", "6", null, (short) 0b1011_0100_0000_1111),
+                Arguments.of("0", "7", null, (short) 0b1011_0100_0000_1111),
                 // 3rd low byte
-                Arguments.of("0", "8", (short) 0b1011_0100_0000_1111),
-                Arguments.of("0", "9", (short) 0b1011_0100_0000_1111),
-                Arguments.of("1", "10", (short) 0b1011_0100_0000_1111),
-                Arguments.of("0", "11", (short) 0b1011_0100_0000_1111),
+                Arguments.of("0", "8", null, (short) 0b1011_0100_0000_1111),
+                Arguments.of("0", "9", null, (short) 0b1011_0100_0000_1111),
+                Arguments.of("1", "10", null, (short) 0b1011_0100_0000_1111),
+                Arguments.of("0", "11", null, (short) 0b1011_0100_0000_1111),
                 // hi byte
-                Arguments.of("1", "12", (short) 0b1011_0100_0000_1111),
-                Arguments.of("1", "13", (short) 0b1011_0100_0000_1111),
-                Arguments.of("0", "14", (short) 0b1011_0100_0000_1111),
-                Arguments.of("1", "15", (short) 0b1011_0100_0000_1111),
+                Arguments.of("1", "12", null, (short) 0b1011_0100_0000_1111),
+                Arguments.of("1", "13", null, (short) 0b1011_0100_0000_1111),
+                Arguments.of("0", "14", null, (short) 0b1011_0100_0000_1111),
+                Arguments.of("1", "15", null, (short) 0b1011_0100_0000_1111),
 
                 // value = uint16 max
-                Arguments.of("1", "0", "65535"),
+                Arguments.of("1", "0", null, "65535"),
                 // value > uint16 max. Value is truncated and only low 16 bits (all zero with 65536!) are considered
-                Arguments.of("0", "0", "65536"), Arguments.of("1", "0", "65537"),
+                Arguments.of("0", "0", null, "65536"), Arguments.of("1", "0", null, "65537"),
 
                 // value = sint16 min -32768
-                Arguments.of("0", "0", "-32768"),
+                Arguments.of("0", "0", null, "-32768"),
                 // value < sint16 min. . Value is truncated and only low 16 bits (all one with -32769!) are considered
-                Arguments.of("1", "0", "-32769"), Arguments.of("0", "0", "-32770"),
+                Arguments.of("1", "0", null, "-32769"), Arguments.of("0", "0", null, "-32770"),
 
                 // UNDEF passes the profile unchanged
-                Arguments.of(UnDefType.UNDEF, "0", UnDefType.UNDEF)
+                Arguments.of(UnDefType.UNDEF, "0", null, UnDefType.UNDEF)
         //
         );
     }
@@ -113,19 +122,19 @@ public class ModbusBitProfileTest {
     @ParameterizedTest
     @MethodSource("provideArgsForSingleUpdateFromHandler")
     public void testDecimalTypeOnStateUpdateFromHandler(Object expectedStateUpdateTowardsItem, String profileBitIndex,
-            Object updateFromHandler) {
-        testDecimalTypeOnUpdateFromHandlerGeneric(expectedStateUpdateTowardsItem, profileBitIndex, updateFromHandler,
-                true);
+            @Nullable String profileInverted, Object updateFromHandler) {
+        testDecimalTypeOnUpdateFromHandlerGeneric(expectedStateUpdateTowardsItem, profileBitIndex, profileInverted,
+                updateFromHandler, true);
     }
 
     @ParameterizedTest
     @MethodSource("provideArgsForSingleUpdateFromHandler")
     public void testDecimalTypeOnCommandFromHandler(Object expectedStateUpdateTowardsItem, String profileBitIndex,
-            Object updateFromHandler) {
+            @Nullable String profileInverted, Object updateFromHandler) {
         // UNDEF is not a command, cannot be sent by handler
         assumeTrue(updateFromHandler != UnDefType.UNDEF);
-        testDecimalTypeOnUpdateFromHandlerGeneric(expectedStateUpdateTowardsItem, profileBitIndex, updateFromHandler,
-                false);
+        testDecimalTypeOnUpdateFromHandlerGeneric(expectedStateUpdateTowardsItem, profileBitIndex, profileInverted,
+                updateFromHandler, false);
     }
 
     /**
@@ -133,14 +142,15 @@ public class ModbusBitProfileTest {
      * @param expectedStateUpdateTowardsItem expected update from the profile towards the item. If String it
      *            provided, it is treated as DecimalType. Otherwise, state is used as-is
      * @param profileBitIndex bitIndex of the profile
+     * @param inverted inverted parameter of the profile
      * @param updateFromHandler update from handler. If String it provided, it is treated as DecimalType.
      *            Otherwise, state is used as-is
      * @param stateUpdateFromHandler whether there is state update from handler. Otherwise command
      */
     private void testDecimalTypeOnUpdateFromHandlerGeneric(Object expectedUpdateTowardsItemObj, String profileBitIndex,
-            Object updateFromHandler, boolean stateUpdateFromHandler) {
+            @Nullable String profileInverted, Object updateFromHandler, boolean stateUpdateFromHandler) {
         ProfileCallback callback = mock(ProfileCallback.class);
-        ModbusBitProfile profile = createProfile(callback, profileBitIndex);
+        ModbusBitProfile profile = createProfile(callback, profileBitIndex, profileInverted);
 
         Type actualUpdateTowardsItem;
         if (stateUpdateFromHandler) {
@@ -184,12 +194,29 @@ public class ModbusBitProfileTest {
     {
         return Stream.of(//
                 // ON/OFF commands
-                Arguments.of((short) 0b1011_0100_0000_1111, "1", (short) 0b1011_0100_0000_1101, OnOffType.OFF),
-                Arguments.of((short) 0b1011_0100_0000_1111, "4", (short) 0b1011_0100_0001_1111, OnOffType.ON),
+                Arguments.of((short) 0b1011_0100_0000_1111, "1", null, (short) 0b1011_0100_0000_1101, OnOffType.OFF),
+                Arguments.of((short) 0b1011_0100_0000_1111, "4", null, (short) 0b1011_0100_0001_1111, OnOffType.ON),
+                Arguments.of((short) 0b1011_0100_0000_1111, "1", "true",
+                        // same result than above test case with OFF since inverted
+                        (short) 0b1011_0100_0000_1101, OnOffType.ON),
+                Arguments.of((short) 0b1011_0100_0000_1111, "4", "true",
+                        // same result than above test case with ON since inverted
+                        (short) 0b1011_0100_0001_1111, OnOffType.OFF),
+
                 //
                 // DecimalType commands
-                Arguments.of((short) 0b1011_0100_0000_1111, "1", (short) 0b1011_0100_0000_1101, new DecimalType(0)),
-                Arguments.of((short) 0b1011_0100_0000_1111, "4", (short) 0b1011_0100_0001_1111, new DecimalType(5))
+                Arguments.of((short) 0b1011_0100_0000_1111, "1", null, (short) 0b1011_0100_0000_1101,
+                        new DecimalType(0)),
+                Arguments.of((short) 0b1011_0100_0010_1111, "5", null, (short) 0b1011_0100_0000_1111,
+                        new DecimalType(0)),
+                Arguments.of((short) 0b1011_0100_0000_1111, "5", "true",
+                        // Since inverted=true, bit is *set* even-though command is falsy
+                        (short) 0b1011_0100_0010_1111, new DecimalType(0)),
+                Arguments.of((short) 0b1011_0100_0000_1111, "4", null, (short) 0b1011_0100_0001_1111,
+                        new DecimalType(5)),
+                Arguments.of((short) 0b1011_0100_0001_1111, "4", "TRUE",
+                        // Since inverted=true, 4th bit is *unset* with true-like command
+                        (short) 0b1011_0100_0000_1111, new DecimalType(5))
 
         //
         );
@@ -198,10 +225,10 @@ public class ModbusBitProfileTest {
     @ParameterizedTest
     @MethodSource("provideArgsForUpdateFromHandlerThenCommandFromItem")
     public void testUpdateFromHandlerThenCommandFromItem(short stateUpdateFromHandler, String profileBitIndex,
-            short expectedStateAfterCommand, Command commandFromItem) {
+            @Nullable String profileInverted, short expectedStateAfterCommand, Command commandFromItem) {
         DecimalType stateUpdateFromHandlerDecimal = new DecimalType(stateUpdateFromHandler);
         ProfileCallback callback = mock(ProfileCallback.class);
-        ModbusBitProfile profile = createProfile(callback, profileBitIndex);
+        ModbusBitProfile profile = createProfile(callback, profileBitIndex, profileInverted);
 
         profile.onStateUpdateFromHandler(stateUpdateFromHandlerDecimal);
 
@@ -219,10 +246,11 @@ public class ModbusBitProfileTest {
                 capturedCommandToBinding);
     }
 
-    private ModbusBitProfile createProfile(ProfileCallback callback, String bitIndex) {
+    private ModbusBitProfile createProfile(ProfileCallback callback, String bitIndex, @Nullable String inverted) {
         ProfileContext context = mock(ProfileContext.class);
         Configuration config = new Configuration();
         config.put("bit-index", bitIndex);
+        config.put("inverted", inverted);
         when(context.getConfiguration()).thenReturn(config);
 
         return new ModbusBitProfile(callback, context);
@@ -236,7 +264,8 @@ public class ModbusBitProfileTest {
         // assertion method designed only with length-2 arrays (16 bit numbers)
         assert expected.length == 2;
         String actualBinary = Integer.toBinaryString(new BigInteger(actual).intValue()).substring(15);
-        assertArrayEquals(expected, actual, String.format("Expected %s, got %s (16bits: %s)", Arrays.toString(expected),
-                Arrays.toString(actual), actualBinary));
+        assertArrayEquals(expected, actual,
+                String.format("Expected %s, got %s (%s vs %s)", Arrays.toString(expected), Arrays.toString(actual),
+                        Integer.toBinaryString(new BigInteger(expected).intValue()).substring(15), actualBinary));
     }
 }
