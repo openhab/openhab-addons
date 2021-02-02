e7efd07e30571ecb10ea478f8960113413442ac0
Fabian Wolter <github@fabian-wolter.de>
2021-02-02 08:28:58 +0100

    [modbus.sbc] Initial contribution (#9174)
    Signed-off-by: Fabian Wolter <github@fabian-wolter.de>
    Signed-off-by: John Marshall <john.marshall.au@gmail.com>

---
+++ bundles/org.openhab.binding.modbus.sbc/src/main/java/org/openhab/binding/modbus/sbc/internal/ALD1Configuration.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.modbus.sbc.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link ALD1Configuration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Fabian Wolter - Initial contribution
+ */
+@NonNullByDefault
+public class ALD1Configuration {
+    public int pollInterval;
+}
