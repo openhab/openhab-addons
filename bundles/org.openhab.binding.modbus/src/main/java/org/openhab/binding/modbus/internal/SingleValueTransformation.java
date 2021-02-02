c2395dec362203de883cb9d00cfa3ca3abd47a8b
Sami Salonen <ssalonen@gmail.com>
2021-02-02 09:33:07 +0200

    [modbus] Modbus transformations: cascaded/chained transformations and new-style transformation string (#9945)
    * [modbus] Cascaded transforms with ∩
    * [modbus] README to mention cascaded transformations
    * [modbus] Take cascaded transformation into use
    * [modbus] README to show preference towards new syntax
    * [modbus] examples to use new syntax
    * [modbus] fix test
    * [modbus] remove apache commons lang dependency
    - see also PR #10002
    - I removed equals and hashCode implementation all-together, I could not see they played any role in practice.
    
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>
    Signed-off-by: Koen Schockaert <ks@qbus.be>

--- bundles/org.openhab.binding.modbus/src/main/java/org/openhab/binding/modbus/internal/SingleValueTransformation.java
+++ bundles/org.openhab.binding.modbus/src/main/java/org/openhab/binding/modbus/internal/SingleValueTransformation.java
@@ -12,17 +12,12 @@
  */
 package org.openhab.binding.modbus.internal;
 
-import static org.apache.commons.lang.StringUtils.isEmpty;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.StandardToStringStyle;
-import org.apache.commons.lang.builder.ToStringBuilder;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.core.library.types.DecimalType;
@@ -32,7 +27,6 @@ import org.openhab.core.transform.TransformationException;
 import org.openhab.core.transform.TransformationHelper;
 import org.openhab.core.transform.TransformationService;
 import org.openhab.core.types.Command;
-import org.openhab.core.types.State;
 import org.openhab.core.types.TypeParser;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
@@ -47,13 +41,15 @@ import org.slf4j.LoggerFactory;
  *
  */
 @NonNullByDefault
-public class Transformation {
+public class SingleValueTransformation implements ValueTransformation {
 
     public static final String TRANSFORM_DEFAULT = "default";
-    public static final Transformation IDENTITY_TRANSFORMATION = new Transformation(TRANSFORM_DEFAULT, null, null);
+    public static final ValueTransformation IDENTITY_TRANSFORMATION = new SingleValueTransformation(TRANSFORM_DEFAULT,
+            null, null);
 
     /** RegEx to extract and parse a function String <code>'(.*?)\((.*)\)'</code> */
-    private static final Pattern EXTRACT_FUNCTION_PATTERN = Pattern.compile("(?<service>.*?)\\((?<arg>.*)\\)");
+    private static final Pattern EXTRACT_FUNCTION_PATTERN_OLD = Pattern.compile("(?<service>.*?)\\((?<arg>.*)\\)");
+    private static final Pattern EXTRACT_FUNCTION_PATTERN_NEW = Pattern.compile("(?<service>.*?):(?<arg>.*)");
 
     /**
      * Ordered list of types that are tried out first when trying to parse transformed command
@@ -65,17 +61,11 @@ public class Transformation {
         DEFAULT_TYPES.add(OnOffType.class);
     }
 
-    private final Logger logger = LoggerFactory.getLogger(Transformation.class);
-
-    private static StandardToStringStyle toStringStyle = new StandardToStringStyle();
-
-    static {
-        toStringStyle.setUseShortClassName(true);
-    }
+    private final Logger logger = LoggerFactory.getLogger(SingleValueTransformation.class);
 
     private final @Nullable String transformation;
-    private final @Nullable String transformationServiceName;
-    private final @Nullable String transformationServiceParam;
+    final @Nullable String transformationServiceName;
+    final @Nullable String transformationServiceParam;
 
     /**
      *
@@ -83,17 +73,25 @@ public class Transformation {
      *            (output equals input)) or some other value (output is a constant). Futhermore, empty string is
      *            considered the same way as "default".
      */
-    public Transformation(@Nullable String transformation) {
+    public SingleValueTransformation(@Nullable String transformation) {
         this.transformation = transformation;
         //
         // Parse transformation configuration here on construction, but delay the
         // construction of TransformationService to call-time
-        if (isEmpty(transformation) || transformation.equalsIgnoreCase(TRANSFORM_DEFAULT)) {
+        if (transformation == null || transformation.isEmpty() || transformation.equalsIgnoreCase(TRANSFORM_DEFAULT)) {
             // no-op (identity) transformation
             transformationServiceName = null;
             transformationServiceParam = null;
         } else {
-            Matcher matcher = EXTRACT_FUNCTION_PATTERN.matcher(transformation);
+            int colonIndex = transformation.indexOf(":");
+            int parenthesisOpenIndex = transformation.indexOf("(");
+
+            final Matcher matcher;
+            if (parenthesisOpenIndex != -1 && (colonIndex == -1 || parenthesisOpenIndex < colonIndex)) {
+                matcher = EXTRACT_FUNCTION_PATTERN_OLD.matcher(transformation);
+            } else {
+                matcher = EXTRACT_FUNCTION_PATTERN_NEW.matcher(transformation);
+            }
             if (matcher.matches()) {
                 matcher.reset();
                 matcher.find();
@@ -116,13 +114,14 @@ public class Transformation {
      * @param transformationServiceName
      * @param transformationServiceParam
      */
-    Transformation(String transformation, @Nullable String transformationServiceName,
+    SingleValueTransformation(String transformation, @Nullable String transformationServiceName,
             @Nullable String transformationServiceParam) {
         this.transformation = transformation;
         this.transformationServiceName = transformationServiceName;
         this.transformationServiceParam = transformationServiceParam;
     }
 
+    @Override
     public String transform(BundleContext context, String value) {
         String transformedResponse;
         String transformationServiceName = this.transformationServiceName;
@@ -163,6 +162,7 @@ public class Transformation {
         return transformedResponse == null ? "" : transformedResponse;
     }
 
+    @Override
     public boolean isIdentityTransform() {
         return TRANSFORM_DEFAULT.equalsIgnoreCase(this.transformation);
     }
@@ -172,52 +172,9 @@ public class Transformation {
         return transformedCommand;
     }
 
-    /**
-     * Transform state to another state using this transformation
-     *
-     * @param context
-     * @param types types to used to parse the transformation result
-     * @param command
-     * @return Transformed command, or null if no transformation was possible
-     */
-    public @Nullable State transformState(BundleContext context, List<Class<? extends State>> types, State state) {
-        // Note that even identity transformations go through the State -> String -> State steps. This does add some
-        // overhead but takes care of DecimalType -> PercentType conversions, for example.
-        final String stateAsString = state.toString();
-        final String transformed = transform(context, stateAsString);
-        return TypeParser.parseState(types, transformed);
-    }
-
-    public boolean hasTransformationService() {
-        return transformationServiceName != null;
-    }
-
-    @Override
-    public boolean equals(@Nullable Object obj) {
-        if (null == obj) {
-            return false;
-        }
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof Transformation)) {
-            return false;
-        }
-        Transformation that = (Transformation) obj;
-        EqualsBuilder eb = new EqualsBuilder();
-        if (hasTransformationService()) {
-            eb.append(this.transformationServiceName, that.transformationServiceName);
-            eb.append(this.transformationServiceParam, that.transformationServiceParam);
-        } else {
-            eb.append(this.transformation, that.transformation);
-        }
-        return eb.isEquals();
-    }
-
     @Override
     public String toString() {
-        return new ToStringBuilder(this, toStringStyle).append("tranformation", transformation)
-                .append("transformationServiceName", transformationServiceName)
-                .append("transformationServiceParam", transformationServiceParam).toString();
+        return "SingleValueTransformation [transformation=" + transformation + ", transformationServiceName="
+                + transformationServiceName + ", transformationServiceParam=" + transformationServiceParam + "]";
     }
 }
