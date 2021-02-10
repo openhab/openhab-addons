49fe9a0c363084aaaca971d5532aa01b821d8aa1
Joshua Bacher <bacher@bash-it.de>
2021-02-11 00:39:35 +0100

    next steps in todo.

--- bundles/org.openhab.binding.avmfritz/src/main/java/org/openhab/binding/avmfritz/internal/handler/AVMFritzDimmingLightingDeviceHandler.java
+++ bundles/org.openhab.binding.avmfritz/src/main/java/org/openhab/binding/avmfritz/internal/handler/AVMFritzDimmingLightingDeviceHandler.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 public class AVMFritzDimmingLightingDeviceHandler extends DeviceHandler implements ThingHandler {
     private final Logger logger = LoggerFactory.getLogger(AVMFritzDimmingLightingDeviceHandler.class);
 
+    // TODO: when getting here, we know that we need to retrieve the default colors. Maybe we can use that.  
     public AVMFritzDimmingLightingDeviceHandler(Thing thing) {
         super(thing);
         logger.debug("initialized avm fritz dimming light device handler.");
