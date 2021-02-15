f5154e662b289b1201d3ffa6905e8ca658087807
Björn Lange <bjoern.lange@itemis.de>
2021-02-15 07:26:51 +0000

    Include cause message
    Also-by: Bert Plonus <bert.plonus@miele.com>
    Also-by: Martin Lepsy <martin.lepsy@miele.com>
    Also-by: Benjamin Bolte <benjamin.bolte@itemis.de>
    Signed-off-by: Björn Lange <bjoern.lange@itemis.de>

--- bundles/org.openhab.binding.mielecloud/src/main/java/org/openhab/binding/mielecloud/internal/auth/OpenHabOAuthTokenRefresher.java
+++ bundles/org.openhab.binding.mielecloud/src/main/java/org/openhab/binding/mielecloud/internal/auth/OpenHabOAuthTokenRefresher.java
@@ -73,7 +73,8 @@ public final class OpenHabOAuthTokenRefresher implements OAuthTokenRefresher {
                 OAuthClientService clientService = getOAuthClientService(serviceHandle);
                 clientService.removeAccessTokenRefreshListener(refreshListener);
             } catch (OAuthException e) {
-                logger.warn("Failed to remove refresh listener: OAuth client service is unavailable.");
+                logger.warn("Failed to remove refresh listener: OAuth client service is unavailable. Cause: "
+                        + e.getMessage());
             }
         }
         listenerByServiceHandle.remove(serviceHandle);
