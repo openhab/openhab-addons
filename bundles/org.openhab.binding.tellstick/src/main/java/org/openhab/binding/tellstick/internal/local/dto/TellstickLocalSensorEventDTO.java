5d4ba9ad5e399572d36f87f2f962978bcec06aa6
Jan Gustafsson <jannegpriv@gmail.com>
2021-02-18 09:33:22 +0100

    [tellstick] Fix for #9841, adding support for Tellstick local API. (#10020)
    * Fix for #9841.
    
    Signed-off-by: Jan Gustafsson <jannegpriv@gmail.com>

---
+++ bundles/org.openhab.binding.tellstick/src/main/java/org/openhab/binding/tellstick/internal/local/dto/TellstickLocalSensorEventDTO.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.tellstick.internal.local.dto;
+
+import org.openhab.binding.tellstick.internal.TellstickRuntimeException;
+import org.tellstick.device.TellstickSensorEvent;
+import org.tellstick.device.iface.TellstickEvent;
+import org.tellstick.enums.DataType;
+
+/**
+ * This class is used for events for the telldus live sensors.
+ *
+ * @author Jan Gustafsson - Initial contribution
+ */
+public class TellstickLocalSensorEventDTO extends TellstickSensorEvent implements TellstickEvent {
+
+    private LocalDataTypeValueDTO dataType;
+
+    public TellstickLocalSensorEventDTO(int sensorId, String data, LocalDataTypeValueDTO dataValue, String protocol,
+            String model, long timeStamp) {
+        super(sensorId, data, null, protocol, model, timeStamp);
+        this.dataType = dataValue;
+    }
+
+    public LocalDataTypeValueDTO getDataTypeValue() {
+        return dataType;
+    }
+
+    @Override
+    public DataType getDataType() {
+        throw new TellstickRuntimeException("Should not call this method");
+    }
+}
