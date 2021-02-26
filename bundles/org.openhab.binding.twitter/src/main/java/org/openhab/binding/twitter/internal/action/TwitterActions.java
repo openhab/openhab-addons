82daa7af20bf3163f3c5ba301dc5d11529e8e447
Scott Hanson <scooter_seh@yahoo.com>
2021-02-26 11:02:58 -0500

    fix class name
    Signed-off-by: Scott Hanson <scooter_seh@yahoo.com>

--- bundles/org.openhab.binding.twitter/src/main/java/org/openhab/binding/twitter/internal/action/TwitterActions.java
+++ bundles/org.openhab.binding.twitter/src/main/java/org/openhab/binding/twitter/internal/action/TwitterActions.java
@@ -25,7 +25,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The {@link SendTweetActions} class defines rule actions for sending tweet
+ * The {@link TweetActions} class defines rule actions for sending tweet
  *
  * @author Scott Hanson - Initial contribution
  */
