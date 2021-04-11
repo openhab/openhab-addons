d3efb143114fd4416f92466ffdf974fd567fe3f3
grro <grro@users.noreply.github.com>
2021-04-11 19:47:27 +0200

    [WebThing] Initial contribution (#9555)
    Signed-off-by: Gregor Roth <gregor.roth@web.de>

---
+++ bundles/org.openhab.binding.webthing/src/main/java/org/openhab/binding/webthing/internal/link/TypeConverter.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.webthing.internal.link;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.types.Command;
+import org.openhab.core.types.State;
+
+/**
+ * The {@link TypeConverter} class map Item state <-> Property value
+ *
+ * @author Gregor Roth - Initial contribution
+ */
+@NonNullByDefault
+interface TypeConverter {
+
+    /**
+     * * maps a Property value to an Item state command
+     * 
+     * @param propertyValue the Property value
+     * @return the Item state command
+     */
+    Command toStateCommand(Object propertyValue);
+
+    /**
+     * maps an Item state to a Property value
+     * 
+     * @param state the Item state
+     * @return the Property value
+     */
+    Object toPropertyValue(State state);
+}
