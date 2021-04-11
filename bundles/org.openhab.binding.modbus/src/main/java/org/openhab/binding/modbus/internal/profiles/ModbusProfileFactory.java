3845e87ca613acb301442f720252d3542565a3d9
Sami Salonen <ssalonen@gmail.com>
2021-04-11 21:09:53 +0300

    [modbus] Remove unused localization
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- bundles/org.openhab.binding.modbus/src/main/java/org/openhab/binding/modbus/internal/profiles/ModbusProfileFactory.java
+++ bundles/org.openhab.binding.modbus/src/main/java/org/openhab/binding/modbus/internal/profiles/ModbusProfileFactory.java
@@ -15,17 +15,11 @@ package org.openhab.binding.modbus.internal.profiles;
 import static org.openhab.binding.modbus.internal.profiles.ModbusProfiles.*;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import org.openhab.core.i18n.LocalizedKey;
-import org.openhab.core.thing.UID;
 import org.openhab.core.thing.profiles.Profile;
 import org.openhab.core.thing.profiles.ProfileCallback;
 import org.openhab.core.thing.profiles.ProfileContext;
@@ -33,12 +27,7 @@ import org.openhab.core.thing.profiles.ProfileFactory;
 import org.openhab.core.thing.profiles.ProfileType;
 import org.openhab.core.thing.profiles.ProfileTypeProvider;
 import org.openhab.core.thing.profiles.ProfileTypeUID;
-import org.openhab.core.thing.profiles.i18n.ProfileTypeI18nLocalizationService;
-import org.openhab.core.util.BundleResolver;
-import org.osgi.framework.Bundle;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * A factory and advisor for modbus profiles.
@@ -46,25 +35,13 @@ import org.osgi.service.component.annotations.Reference;
  *
  * @author Sami Salonen - Initial contribution
  */
-@Component(service = { ProfileFactory.class, ProfileTypeProvider.class })
 @NonNullByDefault
+@Component(service = { ProfileFactory.class, ProfileTypeProvider.class })
 public class ModbusProfileFactory implements ProfileFactory, ProfileTypeProvider {
 
     private static final Set<ProfileType> SUPPORTED_PROFILE_TYPES = Set.of(GAIN_OFFSET_TYPE);
 
     private static final Set<ProfileTypeUID> SUPPORTED_PROFILE_TYPE_UIDS = Set.of(GAIN_OFFSET);
-
-    private final Map<LocalizedKey, ProfileType> localizedProfileTypeCache = new ConcurrentHashMap<>();
-
-    private final ProfileTypeI18nLocalizationService profileTypeI18nLocalizationService;
-    private final Bundle bundle;
-
-    @Activate
-    public ModbusProfileFactory(final @Reference ProfileTypeI18nLocalizationService profileTypeI18nLocalizationService,
-            final @Reference BundleResolver bundleResolver) {
-        this.profileTypeI18nLocalizationService = profileTypeI18nLocalizationService;
-        this.bundle = bundleResolver.resolveBundle(ModbusProfileFactory.class);
-    }
 
     @Override
     public @Nullable Profile createProfile(ProfileTypeUID profileTypeUID, ProfileCallback callback,
@@ -78,8 +55,7 @@ public class ModbusProfileFactory implements ProfileFactory, ProfileTypeProvider
 
     @Override
     public Collection<ProfileType> getProfileTypes(@Nullable Locale locale) {
-        return Collections.unmodifiableList(SUPPORTED_PROFILE_TYPES.stream()
-                .map(p -> createLocalizedProfileType(p, locale)).collect(Collectors.toList()));
+        return SUPPORTED_PROFILE_TYPES;
     }
 
     @Override
@@ -87,28 +63,4 @@ public class ModbusProfileFactory implements ProfileFactory, ProfileTypeProvider
         return SUPPORTED_PROFILE_TYPE_UIDS;
     }
 
-    private ProfileType createLocalizedProfileType(ProfileType profileType, @Nullable Locale locale) {
-        LocalizedKey localizedKey = getLocalizedProfileTypeKey(profileType.getUID(), locale);
-
-        ProfileType cachedEntry = localizedProfileTypeCache.get(localizedKey);
-        if (cachedEntry != null) {
-            return cachedEntry;
-        }
-
-        ProfileType localizedProfileType = localize(profileType, locale);
-        if (localizedProfileType != null) {
-            localizedProfileTypeCache.put(localizedKey, localizedProfileType);
-            return localizedProfileType;
-        } else {
-            return profileType;
-        }
-    }
-
-    private @Nullable ProfileType localize(ProfileType profileType, @Nullable Locale locale) {
-        return profileTypeI18nLocalizationService.createLocalizedProfileType(bundle, profileType, locale);
-    }
-
-    private LocalizedKey getLocalizedProfileTypeKey(UID uid, @Nullable Locale locale) {
-        return new LocalizedKey(uid, locale != null ? locale.toLanguageTag() : null);
-    }
 }
