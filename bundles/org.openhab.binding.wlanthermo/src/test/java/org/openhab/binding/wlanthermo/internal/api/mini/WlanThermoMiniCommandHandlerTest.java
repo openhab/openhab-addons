9b70d4507719cadccb29c91f4c178afb69c330d5
Wouter Born <github@maindrain.net>
2021-04-15 12:47:15 +0200

    Avoid star imports and add missing braces
    Fixes the following SAT findings:
    
    * AvoidStarImportCheck (125)
    * NeedBracesCheck (39)
    
    Signed-off-by: Wouter Born <github@maindrain.net>

--- bundles/org.openhab.binding.wlanthermo/src/test/java/org/openhab/binding/wlanthermo/internal/api/mini/WlanThermoMiniCommandHandlerTest.java
+++ bundles/org.openhab.binding.wlanthermo/src/test/java/org/openhab/binding/wlanthermo/internal/api/mini/WlanThermoMiniCommandHandlerTest.java
@@ -14,7 +14,7 @@ package org.openhab.binding.wlanthermo.internal.api.mini;
 
 import static org.openhab.binding.wlanthermo.internal.WlanThermoBindingConstants.*;
 
-import java.awt.*;
+import java.awt.Color;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -33,7 +33,11 @@ import org.openhab.binding.wlanthermo.internal.WlanThermoException;
 import org.openhab.binding.wlanthermo.internal.WlanThermoUnknownChannelException;
 import org.openhab.binding.wlanthermo.internal.WlanThermoUtil;
 import org.openhab.binding.wlanthermo.internal.api.mini.dto.builtin.App;
-import org.openhab.core.library.types.*;
+import org.openhab.core.library.types.DecimalType;
+import org.openhab.core.library.types.HSBType;
+import org.openhab.core.library.types.OnOffType;
+import org.openhab.core.library.types.QuantityType;
+import org.openhab.core.library.types.StringType;
 import org.openhab.core.library.unit.ImperialUnits;
 import org.openhab.core.thing.ChannelUID;
 import org.openhab.core.thing.ThingUID;
