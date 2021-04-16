2e238b6f36b21791c85d0c9c21952730c69af303
Kai Kreuzer <kai@openhab.org>
2021-04-16 21:38:20 +0200

    [airthings] Add support for Airthings Wave Mini (#10456)
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- bundles/org.openhab.binding.bluetooth.airthings/src/main/java/org/openhab/binding/bluetooth/airthings/internal/AirthingsWaveMiniHandler.java
+++ bundles/org.openhab.binding.bluetooth.airthings/src/main/java/org/openhab/binding/bluetooth/airthings/internal/AirthingsWaveMiniHandler.java
@@ -18,11 +18,9 @@ import java.util.Map;
 import java.util.UUID;
 
 import javax.measure.quantity.Dimensionless;
-import javax.measure.quantity.Pressure;
 import javax.measure.quantity.Temperature;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.openhab.core.library.dimension.Density;
 import org.openhab.core.library.types.QuantityType;
 import org.openhab.core.library.unit.SIUnits;
 import org.openhab.core.library.unit.Units;
@@ -31,29 +29,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The {@link AirthingsWavePlusHandler} is responsible for handling commands, which are
+ * The {@link AirthingsWaveMiniHandler} is responsible for handling commands, which are
  * sent to one of the channels.
  *
- * @author Pauli Anttila - Initial contribution
- * @author Kai Kreuzer - Added Airthings Wave Mini support
+ * @author Kai Kreuzer - Initial contribution
  */
 @NonNullByDefault
-public class AirthingsWavePlusHandler extends AbstractAirthingsHandler {
+public class AirthingsWaveMiniHandler extends AbstractAirthingsHandler {
 
-    private static final String DATA_UUID = "b42e2a68-ade7-11e4-89d3-123b93f75cba";
+    private static final String DATA_UUID = "b42e3b98-ade7-11e4-89d3-123b93f75cba";
 
-    public AirthingsWavePlusHandler(Thing thing) {
+    public AirthingsWaveMiniHandler(Thing thing) {
         super(thing);
     }
 
-    private final Logger logger = LoggerFactory.getLogger(AirthingsWavePlusHandler.class);
+    private final Logger logger = LoggerFactory.getLogger(AirthingsWaveMiniHandler.class);
+
     private final UUID uuid = UUID.fromString(DATA_UUID);
 
     @Override
     protected void updateChannels(int[] is) {
         Map<String, Number> data;
         try {
-            data = AirthingsDataParser.parseWavePlusData(is);
+            data = AirthingsDataParser.parseWaveMiniData(is);
             logger.debug("Parsed data: {}", data);
             Number humidity = data.get(AirthingsDataParser.HUMIDITY);
             if (humidity != null) {
@@ -63,27 +61,9 @@ public class AirthingsWavePlusHandler extends AbstractAirthingsHandler {
             if (temperature != null) {
                 updateState(CHANNEL_ID_TEMPERATURE, new QuantityType<Temperature>(temperature, SIUnits.CELSIUS));
             }
-            Number pressure = data.get(AirthingsDataParser.PRESSURE);
-            if (pressure != null) {
-                updateState(CHANNEL_ID_PRESSURE, new QuantityType<Pressure>(pressure, Units.MILLIBAR));
-            }
-            Number co2 = data.get(AirthingsDataParser.CO2);
-            if (co2 != null) {
-                updateState(CHANNEL_ID_CO2, new QuantityType<Dimensionless>(co2, Units.PARTS_PER_MILLION));
-            }
             Number tvoc = data.get(AirthingsDataParser.TVOC);
             if (tvoc != null) {
                 updateState(CHANNEL_ID_TVOC, new QuantityType<Dimensionless>(tvoc, PARTS_PER_BILLION));
-            }
-            Number radonShortTermAvg = data.get(AirthingsDataParser.RADON_SHORT_TERM_AVG);
-            if (radonShortTermAvg != null) {
-                updateState(CHANNEL_ID_RADON_ST_AVG,
-                        new QuantityType<Density>(radonShortTermAvg, BECQUEREL_PER_CUBIC_METRE));
-            }
-            Number radonLongTermAvg = data.get(AirthingsDataParser.RADON_LONG_TERM_AVG);
-            if (radonLongTermAvg != null) {
-                updateState(CHANNEL_ID_RADON_LT_AVG,
-                        new QuantityType<Density>(radonLongTermAvg, BECQUEREL_PER_CUBIC_METRE));
             }
         } catch (AirthingsParserException e) {
             logger.error("Failed to parse data received from Airthings sensor: {}", e.getMessage());
---
+++ bundles/org.openhab.binding.bluetooth.airthings/src/test/java/org/openhab/binding/bluetooth/airthings/AirthingsParserTest.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.bluetooth.airthings;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.junit.jupiter.api.Test;
+import org.openhab.binding.bluetooth.airthings.internal.AirthingsDataParser;
+import org.openhab.binding.bluetooth.airthings.internal.AirthingsParserException;
+
+/**
+ * Tests {@link AirthingsParserTest}.
+ *
+ * @author Pauli Anttila - Initial contribution
+ */
+@NonNullByDefault
+public class AirthingsParserTest {
+
+    @Test
+    public void testWrongVersion() {
+        int[] data = { 5, 55, 51, 0, 122, 0, 61, 0, 119, 9, 11, 194, 169, 2, 46, 0, 0, 0, 4, 20 };
+        assertThrows(AirthingsParserException.class, () -> AirthingsDataParser.parseWavePlusData(data));
+    }
+
+    @Test
+    public void testEmptyData() {
+        int[] data = {};
+        assertThrows(AirthingsParserException.class, () -> AirthingsDataParser.parseWavePlusData(data));
+    }
+
+    @Test
+    public void testWrongDataLen() throws AirthingsParserException {
+        int[] data = { 1, 55, 51, 0, 122, 0, 61, 0, 119, 9, 11, 194, 169, 2, 46, 0, 0 };
+        assertThrows(AirthingsParserException.class, () -> AirthingsDataParser.parseWavePlusData(data));
+    }
+
+    @Test
+    public void testParsingPlus() throws AirthingsParserException {
+        int[] data = { 1, 55, 51, 0, 122, 0, 61, 0, 119, 9, 11, 194, 169, 2, 46, 0, 0, 0, 4, 20 };
+        Map<String, Number> result = AirthingsDataParser.parseWavePlusData(data);
+
+        assertEquals(27.5, result.get(AirthingsDataParser.HUMIDITY));
+        assertEquals(681, result.get(AirthingsDataParser.CO2));
+        assertEquals(46, result.get(AirthingsDataParser.TVOC));
+        assertEquals(24.23, result.get(AirthingsDataParser.TEMPERATURE));
+        assertEquals(993.5, result.get(AirthingsDataParser.PRESSURE));
+        assertEquals(61, result.get(AirthingsDataParser.RADON_LONG_TERM_AVG));
+        assertEquals(122, result.get(AirthingsDataParser.RADON_SHORT_TERM_AVG));
+    }
+
+    @Test
+    public void testParsingMini() throws AirthingsParserException {
+        int[] data = { 12, 0, 248, 112, 201, 193, 136, 14, 150, 0, 1, 0, 217, 176, 14, 0, 255, 255, 255, 255 };
+        Map<String, Number> result = AirthingsDataParser.parseWaveMiniData(data);
+
+        assertEquals(37.2, result.get(AirthingsDataParser.HUMIDITY));
+        assertEquals(150, result.get(AirthingsDataParser.TVOC));
+        assertEquals(16.05, result.get(AirthingsDataParser.TEMPERATURE));
+    }
+}
