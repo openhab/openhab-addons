2901820c4dcdca26bbb08116b4c8d24dc86878f6
Anton Kharuzhy <antroids@gmail.com>
2021-05-13 16:18:42 +0300

    MQTT.Homeassistant switch test added
    Signed-off-by: Anton Kharuzhy <antroids@gmail.com>

---
+++ bundles/org.openhab.binding.mqtt.homeassistant/src/test/java/org/openhab/binding/mqtt/homeassistant/internal/component/AbstractComponentTests.java
@@ -0,0 +1,86 @@
+package org.openhab.binding.mqtt.homeassistant.internal.component;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.openhab.binding.mqtt.generic.AvailabilityTracker;
+import org.openhab.binding.mqtt.generic.ChannelStateUpdateListener;
+import org.openhab.binding.mqtt.homeassistant.internal.AbstractHomeAssistantTests;
+import org.openhab.binding.mqtt.homeassistant.internal.DiscoverComponents;
+import org.openhab.binding.mqtt.homeassistant.internal.HaID;
+import org.openhab.binding.mqtt.homeassistant.internal.config.AbstractChannelConfiguration;
+import org.openhab.binding.mqtt.homeassistant.internal.config.ChannelConfigurationTypeAdapterFactory;
+import org.openhab.core.common.ThreadPoolManager;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@ExtendWith(MockitoExtension.class)
+public abstract class AbstractComponentTests extends AbstractHomeAssistantTests {
+    protected LatchComponentDiscoveredListener componentDiscoveredListener = new LatchComponentDiscoveredListener();
+    protected ScheduledExecutorService executorService = ThreadPoolManager
+            .getScheduledPool(ThreadPoolManager.THREAD_POOL_NAME_COMMON);
+    protected @Mock ChannelStateUpdateListener channelStateUpdateListener;
+    protected @Mock AvailabilityTracker availabilityTracker;
+    protected Gson gson = new GsonBuilder().registerTypeAdapterFactory(new ChannelConfigurationTypeAdapterFactory())
+            .create();
+    protected DiscoverComponents discoverComponents = new DiscoverComponents(HA_UID, executorService,
+            channelStateUpdateListener, availabilityTracker, gson, transformationServiceProvider);
+
+    protected AbstractComponent<@NonNull ? extends AbstractChannelConfiguration> discoverComponent(String topic,
+            String json) {
+        return discoverComponent(topic, json.getBytes(StandardCharsets.UTF_8));
+    }
+
+    protected AbstractComponent<@NonNull ? extends AbstractChannelConfiguration> discoverComponent(String topic,
+            byte[] jsonPayload) {
+        var haID = new HaID(topic);
+        var latch = componentDiscoveredListener.createWaitForComponentDiscoveredLatch(1);
+        discoverComponents.startDiscovery(bridgeConnection, 0, Set.of(haID), componentDiscoveredListener);
+        discoverComponents.processMessage(topic, jsonPayload);
+        try {
+            assert latch.await(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            assertThat(e.getMessage(), false);
+        }
+        var component = componentDiscoveredListener.getDiscoveredComponent();
+        assertThat(component, CoreMatchers.notNullValue());
+        return component;
+    }
+
+    @NonNullByDefault
+    protected static class LatchComponentDiscoveredListener implements DiscoverComponents.ComponentDiscovered {
+        private @Nullable CountDownLatch latch;
+        private @Nullable AbstractComponent<@NonNull ? extends AbstractChannelConfiguration> discoveredComponent;
+
+        public void componentDiscovered(HaID homeAssistantTopicID, AbstractComponent<@NonNull ?> component) {
+            discoveredComponent = component;
+            if (latch != null) {
+                latch.countDown();
+            }
+        }
+
+        public CountDownLatch createWaitForComponentDiscoveredLatch(int count) {
+            final var newLatch = new CountDownLatch(count);
+            latch = newLatch;
+            return newLatch;
+        }
+
+        public @Nullable AbstractComponent<@NonNull ? extends AbstractChannelConfiguration> getDiscoveredComponent() {
+            return discoveredComponent;
+        }
+    }
+}
---
+++ bundles/org.openhab.binding.mqtt.homeassistant/src/test/java/org/openhab/binding/mqtt/homeassistant/internal/component/SwitchTests.java
@@ -0,0 +1,89 @@
+package org.openhab.binding.mqtt.homeassistant.internal.component;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.openhab.binding.mqtt.generic.values.OnOffValue;
+
+@SuppressWarnings("ConstantConditions")
+public class SwitchTests extends AbstractComponentTests {
+
+    @Test
+    public void testSwitchWithStateAndCommand() {
+        var component = discoverComponent("homeassistant/switch/0x847127fffe11dd6a_auto_lock_zigbee2mqtt/config",
+                "" + "{\n" + "  \"availability\": [\n" + "    {\n" + "      \"topic\": \"zigbee2mqtt/bridge/state\"\n"
+                        + "    }\n" + "  ],\n" + "  \"command_topic\": \"zigbee2mqtt/th1/set/auto_lock\",\n"
+                        + "  \"device\": {\n" + "    \"identifiers\": [\n"
+                        + "      \"zigbee2mqtt_0x847127fffe11dd6a\"\n" + "    ],\n"
+                        + "    \"manufacturer\": \"TuYa\",\n"
+                        + "    \"model\": \"Radiator valve with thermostat (TS0601_thermostat)\",\n"
+                        + "    \"name\": \"th1\",\n" + "    \"sw_version\": \"Zigbee2MQTT 1.18.2\"\n" + "  },\n"
+                        + "  \"json_attributes_topic\": \"zigbee2mqtt/th1\",\n" + "  \"name\": \"th1 auto lock\",\n"
+                        + "  \"payload_off\": \"MANUAL\",\n" + "  \"payload_on\": \"AUTO\",\n"
+                        + "  \"state_off\": \"MANUAL\",\n" + "  \"state_on\": \"AUTO\",\n"
+                        + "  \"state_topic\": \"zigbee2mqtt/th1\",\n"
+                        + "  \"unique_id\": \"0x847127fffe11dd6a_auto_lock_zigbee2mqtt\",\n"
+                        + "  \"value_template\": \"{{ value_json.auto_lock }}\"\n" + "}");
+
+        assertThat(component.channels.size(), is(1));
+        assertThat(component.getName(), is("th1 auto lock"));
+
+        var stateChannel = component.getChannel(Switch.switchChannelID);
+        assertThat(stateChannel.getChannel().getLabel(), is("state"));
+        assertThat(stateChannel.getState().getStateTopic(), is("zigbee2mqtt/th1"));
+        assertThat(stateChannel.getState().getCommandTopic(), is("zigbee2mqtt/th1/set/auto_lock"));
+        assertThat(stateChannel.getState().getCache(), is(instanceOf(OnOffValue.class)));
+    }
+
+    @Test
+    public void testSwitchWithState() {
+        var component = discoverComponent("homeassistant/switch/0x847127fffe11dd6a_auto_lock_zigbee2mqtt/config",
+                "" + "{\n" + "  \"availability\": [\n" + "    {\n" + "      \"topic\": \"zigbee2mqtt/bridge/state\"\n"
+                        + "    }\n" + "  ],\n" + "  \"device\": {\n" + "    \"identifiers\": [\n"
+                        + "      \"zigbee2mqtt_0x847127fffe11dd6a\"\n" + "    ],\n"
+                        + "    \"manufacturer\": \"TuYa\",\n"
+                        + "    \"model\": \"Radiator valve with thermostat (TS0601_thermostat)\",\n"
+                        + "    \"name\": \"th1\",\n" + "    \"sw_version\": \"Zigbee2MQTT 1.18.2\"\n" + "  },\n"
+                        + "  \"json_attributes_topic\": \"zigbee2mqtt/th1\",\n" + "  \"name\": \"th1 auto lock\",\n"
+                        + "  \"state_off\": \"MANUAL\",\n" + "  \"state_on\": \"AUTO\",\n"
+                        + "  \"state_topic\": \"zigbee2mqtt/th1\",\n"
+                        + "  \"unique_id\": \"0x847127fffe11dd6a_auto_lock_zigbee2mqtt\",\n"
+                        + "  \"value_template\": \"{{ value_json.auto_lock }}\"\n" + "}");
+
+        assertThat(component.channels.size(), is(1));
+        assertThat(component.getName(), is("th1 auto lock"));
+
+        var stateChannel = component.getChannel(Switch.switchChannelID);
+        assertThat(stateChannel.getChannel().getLabel(), is("state"));
+        assertThat(stateChannel.getState().getStateTopic(), is("zigbee2mqtt/th1"));
+        assertThat(stateChannel.getState().getCommandTopic(), is(""));
+        assertThat(stateChannel.getState().getCache(), is(instanceOf(OnOffValue.class)));
+    }
+
+    @Test
+    public void testSwitchWithCommand() {
+        var component = discoverComponent("homeassistant/switch/0x847127fffe11dd6a_auto_lock_zigbee2mqtt/config",
+                "" + "{\n" + "  \"availability\": [\n" + "    {\n" + "      \"topic\": \"zigbee2mqtt/bridge/state\"\n"
+                        + "    }\n" + "  ],\n" + "  \"command_topic\": \"zigbee2mqtt/th1/set/auto_lock\",\n"
+                        + "  \"device\": {\n" + "    \"identifiers\": [\n"
+                        + "      \"zigbee2mqtt_0x847127fffe11dd6a\"\n" + "    ],\n"
+                        + "    \"manufacturer\": \"TuYa\",\n"
+                        + "    \"model\": \"Radiator valve with thermostat (TS0601_thermostat)\",\n"
+                        + "    \"name\": \"th1\",\n" + "    \"sw_version\": \"Zigbee2MQTT 1.18.2\"\n" + "  },\n"
+                        + "  \"json_attributes_topic\": \"zigbee2mqtt/th1\",\n" + "  \"name\": \"th1 auto lock\",\n"
+                        + "  \"payload_off\": \"MANUAL\",\n" + "  \"payload_on\": \"AUTO\",\n"
+                        + "  \"unique_id\": \"0x847127fffe11dd6a_auto_lock_zigbee2mqtt\",\n"
+                        + "  \"value_template\": \"{{ value_json.auto_lock }}\"\n" + "}");
+
+        assertThat(component.channels.size(), is(1));
+        assertThat(component.getName(), is("th1 auto lock"));
+
+        var stateChannel = component.getChannel(Switch.switchChannelID);
+        assertThat(stateChannel.getChannel().getLabel(), is("state"));
+        assertThat(stateChannel.getState().getStateTopic(), is(""));
+        assertThat(stateChannel.getState().getCommandTopic(), is("zigbee2mqtt/th1/set/auto_lock"));
+        assertThat(stateChannel.getState().getCache(), is(instanceOf(OnOffValue.class)));
+    }
+}
