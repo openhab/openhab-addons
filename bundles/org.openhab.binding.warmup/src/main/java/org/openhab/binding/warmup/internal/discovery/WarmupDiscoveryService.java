db05079e6fd9d93c2757b480e65cd431e9d88de8
James Melville <jamesmelville@gmail.com>
2021-05-13 14:37:05 +0100

    [warmup] Initial contribution (#8562)
    Signed-off-by: James Melville <jamesmelville@gmail.com>

---
+++ bundles/org.openhab.binding.warmup/src/main/java/org/openhab/binding/warmup/internal/discovery/WarmupDiscoveryService.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.warmup.internal.discovery;
+
+import static org.openhab.binding.warmup.internal.WarmupBindingConstants.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.warmup.internal.handler.MyWarmupAccountHandler;
+import org.openhab.binding.warmup.internal.handler.WarmupRefreshListener;
+import org.openhab.binding.warmup.internal.model.query.LocationDTO;
+import org.openhab.binding.warmup.internal.model.query.QueryResponseDTO;
+import org.openhab.binding.warmup.internal.model.query.RoomDTO;
+import org.openhab.core.config.discovery.AbstractDiscoveryService;
+import org.openhab.core.config.discovery.DiscoveryResultBuilder;
+import org.openhab.core.config.discovery.DiscoveryService;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.thing.ThingUID;
+import org.openhab.core.thing.binding.ThingHandler;
+import org.openhab.core.thing.binding.ThingHandlerService;
+
+/**
+ * The {@link WarmupDiscoveryService} is used to discover devices that are connected to a My Warmup account.
+ *
+ * @author James Melville - Initial contribution
+ */
+@NonNullByDefault
+public class WarmupDiscoveryService extends AbstractDiscoveryService
+        implements DiscoveryService, ThingHandlerService, WarmupRefreshListener {
+
+    private @Nullable MyWarmupAccountHandler bridgeHandler;
+    private @Nullable ThingUID bridgeUID;
+
+    public WarmupDiscoveryService() {
+        super(DISCOVERABLE_THING_TYPES_UIDS, 5, false);
+    }
+
+    @Override
+    public void deactivate() {
+    }
+
+    @Override
+    public void startScan() {
+        final MyWarmupAccountHandler handler = bridgeHandler;
+        if (handler != null) {
+            removeOlderResults(getTimestampOfLastScan());
+            handler.setDiscoveryService(this);
+        }
+    }
+
+    /**
+     * Process device list and populate discovery list with things
+     *
+     * @param domain Data model representing all devices
+     */
+    @Override
+    public void refresh(@Nullable QueryResponseDTO domain) {
+        if (domain != null) {
+            HashSet<ThingUID> discoveredThings = new HashSet<ThingUID>();
+            for (LocationDTO location : domain.getData().getUser().getLocations()) {
+                for (RoomDTO room : location.getRooms()) {
+                    discoverRoom(location, room, discoveredThings);
+                }
+            }
+        }
+    }
+
+    private void discoverRoom(LocationDTO location, RoomDTO room, HashSet<ThingUID> discoveredThings) {
+        if (room.getThermostat4ies() != null && !room.getThermostat4ies().isEmpty()) {
+            final String deviceSN = room.getThermostat4ies().get(0).getDeviceSN();
+            ThingUID localBridgeUID = this.bridgeUID;
+            if (localBridgeUID != null && deviceSN != null) {
+                final Map<String, Object> roomProperties = new HashMap<>();
+                roomProperties.put(Thing.PROPERTY_SERIAL_NUMBER, deviceSN);
+                roomProperties.put(PROPERTY_ROOM_ID, room.getId());
+                roomProperties.put(PROPERTY_ROOM_NAME, room.getName());
+                roomProperties.put(PROPERTY_LOCATION_ID, location.getId());
+                roomProperties.put(PROPERTY_LOCATION_NAME, location.getName());
+
+                ThingUID roomThingUID = new ThingUID(THING_TYPE_ROOM, localBridgeUID, deviceSN);
+                thingDiscovered(DiscoveryResultBuilder.create(roomThingUID).withBridge(localBridgeUID)
+                        .withProperties(roomProperties).withLabel(location.getName() + " - " + room.getName())
+                        .withRepresentationProperty(Thing.PROPERTY_SERIAL_NUMBER).build());
+
+                discoveredThings.add(roomThingUID);
+            }
+        }
+    }
+
+    @Override
+    public void setThingHandler(@Nullable final ThingHandler handler) {
+        if (handler instanceof MyWarmupAccountHandler) {
+            bridgeHandler = (MyWarmupAccountHandler) handler;
+            bridgeUID = handler.getThing().getUID();
+        } else {
+            bridgeHandler = null;
+            bridgeUID = null;
+        }
+    }
+
+    @Override
+    public @Nullable ThingHandler getThingHandler() {
+        return bridgeHandler;
+    }
+}
---
+++ bundles/org.openhab.binding.warmup/src/main/java/org/openhab/binding/warmup/internal/handler/MyWarmupAccountHandler.java
@@ -0,0 +1,134 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.warmup.internal.handler;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jetty.client.HttpClient;
+import org.openhab.binding.warmup.internal.api.MyWarmupApi;
+import org.openhab.binding.warmup.internal.api.MyWarmupApiException;
+import org.openhab.binding.warmup.internal.discovery.WarmupDiscoveryService;
+import org.openhab.binding.warmup.internal.model.query.QueryResponseDTO;
+import org.openhab.core.thing.Bridge;
+import org.openhab.core.thing.ChannelUID;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.thing.ThingStatus;
+import org.openhab.core.thing.ThingStatusDetail;
+import org.openhab.core.thing.binding.BaseBridgeHandler;
+import org.openhab.core.thing.binding.ThingHandlerService;
+import org.openhab.core.types.Command;
+
+/**
+ * @author James Melville - Initial contribution
+ */
+@NonNullByDefault
+public class MyWarmupAccountHandler extends BaseBridgeHandler {
+
+    private final MyWarmupApi api;
+
+    private @Nullable QueryResponseDTO queryResponse = null;
+
+    private @Nullable ScheduledFuture<?> refreshJob;
+    private @Nullable WarmupDiscoveryService discoveryService;
+
+    public MyWarmupAccountHandler(Bridge thing, final HttpClient httpClient) {
+        super(thing);
+        api = new MyWarmupApi(httpClient, getConfigAs(MyWarmupConfigurationDTO.class));
+    }
+
+    @Override
+    public void initialize() {
+        MyWarmupConfigurationDTO config = getConfigAs(MyWarmupConfigurationDTO.class);
+        if (config.username.length() == 0) {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR, "Username not configured");
+        } else if (config.password.length() == 0) {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR, "Password not configured");
+        } else if (config.refreshInterval >= 10) {
+            api.setConfiguration(config);
+            refreshJob = scheduler.scheduleWithFixedDelay(this::refreshFromServer, 0, config.refreshInterval,
+                    TimeUnit.SECONDS);
+        } else {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
+                    "Refresh interval misconfigured (minimum 10s)");
+        }
+    }
+
+    @Override
+    public Collection<Class<? extends ThingHandlerService>> getServices() {
+        return Collections.singleton(WarmupDiscoveryService.class);
+    }
+
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+    }
+
+    @Override
+    public void dispose() {
+        cancelRefresh();
+    }
+
+    public void cancelRefresh() {
+        if (refreshJob != null) {
+            refreshJob.cancel(true);
+            refreshJob = null;
+        }
+    }
+
+    public synchronized void refreshFromServer() {
+        try {
+            queryResponse = api.getStatus();
+            updateStatus(ThingStatus.ONLINE);
+        } catch (MyWarmupApiException e) {
+            queryResponse = null;
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR, e.getMessage());
+        }
+        refreshFromCache();
+    }
+
+    /**
+     * Trigger updates to all devices
+     */
+    public synchronized void refreshFromCache() {
+        notifyListeners(queryResponse);
+    }
+
+    public void setDiscoveryService(final WarmupDiscoveryService discoveryService) {
+        this.discoveryService = discoveryService;
+        refreshFromServer();
+    }
+
+    public void unsetDiscoveryService() {
+        discoveryService = null;
+    }
+
+    /**
+     *
+     * @return reference to the bridge's API
+     */
+    public MyWarmupApi getApi() {
+        return api;
+    }
+
+    private void notifyListeners(@Nullable QueryResponseDTO domain) {
+        if (discoveryService != null && queryResponse != null) {
+            discoveryService.refresh(queryResponse);
+        }
+        getThing().getThings().stream().filter(thing -> thing.getHandler() instanceof WarmupRefreshListener)
+                .map(Thing::getHandler).map(WarmupRefreshListener.class::cast).forEach(thing -> thing.refresh(domain));
+    }
+}
