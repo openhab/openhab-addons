487ed2d22148aaa8aba015010d2c830bee2b6946
Christoph Weitkamp <github@christophweitkamp.de>
2021-05-23 13:49:39 +0200

    Added support for HAN-FUN outlets / simple on-off devices
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

---
+++ bundles/org.openhab.binding.avmfritz/src/main/java/org/openhab/binding/avmfritz/internal/dto/SimpleOnOffModel.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.avmfritz.internal.dto;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * See {@link DeviceListModel}.
+ *
+ * @author Joshua Bacher - Initial contribution
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = { "state" })
+public class SimpleOnOffModel {
+
+    public boolean state;
+
+    @Override
+    public String toString() {
+        return new StringBuilder("[state=").append(state).append(']').toString();
+    }
+}
