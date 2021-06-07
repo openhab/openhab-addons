6141d69da3f5b4e525b2b4e7b8388da3b40e83eb
mlobstein <michael.lobstein@gmail.com>
2021-06-07 13:58:19 -0500

    [radiothermostat] Add job to set thermostat clock automatically from the binding (#10819)
    * add thermostat clock sync job
    
    Signed-off-by: Michael Lobstein <michael.lobstein@gmail.com>

--- bundles/org.openhab.binding.radiothermostat/src/main/java/org/openhab/binding/radiothermostat/internal/handler/RadioThermostatHandler.java
+++ bundles/org.openhab.binding.radiothermostat/src/main/java/org/openhab/binding/radiothermostat/internal/handler/RadioThermostatHandler.java
@@ -19,6 +19,7 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -86,11 +87,13 @@ public class RadioThermostatHandler extends BaseThingHandler implements RadioThe
 
     private @Nullable ScheduledFuture<?> refreshJob;
     private @Nullable ScheduledFuture<?> logRefreshJob;
+    private @Nullable ScheduledFuture<?> clockSyncJob;
 
     private int refreshPeriod = DEFAULT_REFRESH_PERIOD;
     private int logRefreshPeriod = DEFAULT_LOG_REFRESH_PERIOD;
     private boolean isCT80 = false;
     private boolean disableLogs = false;
+    private boolean clockSync = false;
     private String setpointCmdKeyPrefix = "t_";
 
     public RadioThermostatHandler(Thing thing, RadioThermostatStateDescriptionProvider stateDescriptionProvider,
@@ -111,8 +114,9 @@ public class RadioThermostatHandler extends BaseThingHandler implements RadioThe
         final Integer logRefresh = config.logRefresh;
         this.isCT80 = config.isCT80;
         this.disableLogs = config.disableLogs;
+        this.clockSync = config.clockSync;
 
-        if (hostName == null || hostName.equals("")) {
+        if (hostName == null || "".equals(hostName)) {
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
                     "Thermostat Host Name must be specified");
             return;
@@ -145,12 +149,18 @@ public class RadioThermostatHandler extends BaseThingHandler implements RadioThe
             channels.removeIf(c -> (c.getUID().getId().equals(PROGRAM_MODE)));
             updateThing(editThing().withChannels(channels).build());
         }
+
+        updateStatus(ThingStatus.UNKNOWN);
+
         startAutomaticRefresh();
+
         if (!this.disableLogs || this.isCT80) {
             startAutomaticLogRefresh();
         }
 
-        updateStatus(ThingStatus.UNKNOWN);
+        if (this.clockSync) {
+            scheduleClockSyncJob();
+        }
     }
 
     @Override
@@ -172,6 +182,35 @@ public class RadioThermostatHandler extends BaseThingHandler implements RadioThe
             refreshJob = null;
             this.refreshJob = scheduler.scheduleWithFixedDelay(runnable, 0, refreshPeriod, TimeUnit.MINUTES);
         }
+    }
+
+    /**
+     * Schedule the clock sync job
+     */
+    private void scheduleClockSyncJob() {
+        ScheduledFuture<?> clockSyncJob = this.clockSyncJob;
+        if (clockSyncJob == null || clockSyncJob.isCancelled()) {
+            clockSyncJob = null;
+            this.clockSyncJob = scheduler.scheduleWithFixedDelay(this::syncThermostatClock, 1, 60, TimeUnit.MINUTES);
+        }
+    }
+
+    /**
+     * Sync the thermostat's clock with the host system clock
+     */
+    private void syncThermostatClock() {
+        Calendar c = Calendar.getInstance();
+
+        // The thermostat week starts as Monday = 0, subtract 2 since in standard DoW Monday = 2
+        int thermDayOfWeek = c.get(Calendar.DAY_OF_WEEK) - 2;
+        // Sunday will be -1, so add 7 to make it 6
+        if (thermDayOfWeek < 0) {
+            thermDayOfWeek += 7;
+        }
+
+        connector.sendCommand(null, null,
+                String.format(JSON_TIME, thermDayOfWeek, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE)),
+                TIME_RESOURCE);
     }
 
     /**
@@ -213,6 +252,12 @@ public class RadioThermostatHandler extends BaseThingHandler implements RadioThe
         if (logRefreshJob != null) {
             logRefreshJob.cancel(true);
             this.logRefreshJob = null;
+        }
+
+        ScheduledFuture<?> clockSyncJob = this.clockSyncJob;
+        if (clockSyncJob != null) {
+            clockSyncJob.cancel(true);
+            this.clockSyncJob = null;
         }
     }
 
