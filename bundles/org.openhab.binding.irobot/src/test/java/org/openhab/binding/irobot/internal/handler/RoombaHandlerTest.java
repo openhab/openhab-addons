7429964b787c73d708145fb361258e18e3bdd5a0
rimago <fb@java4.info>
2021-06-18 10:38:51 +0200

    [irobot] Fix password discovery and command sending for Roomba I-Models. (using gson) (#10860)
    * Fix password discovery for Roomba I-Models.
    
    Signed-off-by: Alexander Falkenstern <alexander.falkenstern@gmail.com>
    
    * [irobot] remove json-path dependency (use gson instead)
    
    Signed-off-by: Florian Binder <fb@java4.info>
    
    * [irobot] fix checkstyle warnings, preserve backward compatibility, and
    remove unused parameters
    
    Signed-off-by: Florian Binder <fb@java4.info>
    
    Co-authored-by: Alexander Falkenstern <alexander.falkenstern@gmail.com>
    Signed-off-by: dschoepel <dave@theschoepels.com>

--- bundles/org.openhab.binding.irobot/src/test/java/org/openhab/binding/irobot/internal/handler/RoombaHandlerTest.java
+++ bundles/org.openhab.binding.irobot/src/test/java/org/openhab/binding/irobot/internal/handler/RoombaHandlerTest.java
@@ -12,22 +12,32 @@
  */
 package org.openhab.binding.irobot.internal.handler;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.IOException;
 import java.lang.reflect.Field;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.openhab.binding.irobot.internal.config.IRobotConfiguration;
 import org.openhab.core.config.core.Configuration;
 import org.openhab.core.library.types.StringType;
 import org.openhab.core.thing.ChannelUID;
 import org.openhab.core.thing.Thing;
+import org.openhab.core.thing.ThingStatus;
+import org.openhab.core.thing.ThingStatusDetail;
+import org.openhab.core.thing.ThingStatusInfo;
+import org.openhab.core.thing.ThingTypeUID;
 import org.openhab.core.thing.ThingUID;
 import org.openhab.core.thing.binding.ThingHandlerCallback;
+import org.openhab.core.thing.internal.ThingImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.spi.LocationAwareLogger;
@@ -39,82 +49,80 @@ import org.slf4j.spi.LocationAwareLogger;
  * @author Florian Binder - Initial contribution
  */
 
+@NonNullByDefault
 @ExtendWith(MockitoExtension.class)
-@TestInstance(Lifecycle.PER_CLASS)
-class RoombaHandlerTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class RoombaHandlerTest {
 
     private static final String IP_ADDRESS = "<iRobotIP>";
     private static final String PASSWORD = "<PasswordForIRobot>";
 
+    @Nullable
     private RoombaHandler handler;
-    private @Mock Thing myThing;
+    // We have to initialize it to avoid compile errors
+    private @Mock Thing thing = new ThingImpl(new ThingTypeUID("AA:BB"), "");
+    @Nullable
     private ThingHandlerCallback callback;
 
     @BeforeEach
     void setUp() throws Exception {
-        Logger l = LoggerFactory.getLogger(RoombaHandler.class);
-        Field f = l.getClass().getDeclaredField("currentLogLevel");
-        f.setAccessible(true);
-        f.set(l, LocationAwareLogger.TRACE_INT);
+        Logger logger = LoggerFactory.getLogger(RoombaHandler.class);
+        Field logLevelField = logger.getClass().getDeclaredField("currentLogLevel");
+        logLevelField.setAccessible(true);
+        logLevelField.set(logger, LocationAwareLogger.TRACE_INT);
 
         Configuration config = new Configuration();
         config.put("ipaddress", RoombaHandlerTest.IP_ADDRESS);
         config.put("password", RoombaHandlerTest.PASSWORD);
-
-        Mockito.when(myThing.getConfiguration()).thenReturn(config);
-        Mockito.when(myThing.getUID()).thenReturn(new ThingUID("mocked", "irobot", "uid"));
+        Mockito.when(thing.getConfiguration()).thenReturn(config);
+        Mockito.when(thing.getStatusInfo())
+                .thenReturn(new ThingStatusInfo(ThingStatus.UNINITIALIZED, ThingStatusDetail.NONE, "mocked"));
+        Mockito.lenient().when(thing.getUID()).thenReturn(new ThingUID("mocked", "irobot", "uid"));
 
         callback = Mockito.mock(ThingHandlerCallback.class);
 
-        handler = new RoombaHandler(myThing);
+        handler = new RoombaHandler(thing);
         handler.setCallback(callback);
     }
 
-    // @Test
-    void testInit() throws InterruptedException, IOException {
+    @Test
+    void testConfiguration() throws InterruptedException, IOException {
         handler.initialize();
-        Mockito.verify(myThing, Mockito.times(1)).getConfiguration();
 
-        System.in.read();
+        IRobotConfiguration config = thing.getConfiguration().as(IRobotConfiguration.class);
+        assertEquals(config.getIpAddress(), IP_ADDRESS);
+        assertEquals(config.getPassword(), PASSWORD);
+
         handler.dispose();
     }
 
-    // @Test
-    void testCleanRegion() throws IOException, InterruptedException {
+    @Test
+    void testCleanRegion() throws InterruptedException, IOException {
         handler.initialize();
 
-        System.in.read();
-
-        ChannelUID cmd = new ChannelUID("my:thi:blabla:command");
+        ChannelUID cmd = new ChannelUID(thing.getUID(), "command");
         handler.handleCommand(cmd, new StringType("cleanRegions:AABBCCDDEEFFGGHH;2,3"));
 
-        System.in.read();
         handler.dispose();
     }
 
-    // @Test
-    void testDock() throws IOException, InterruptedException {
+    @Test
+    void testDock() throws InterruptedException, IOException {
         handler.initialize();
 
-        System.in.read();
-
-        ChannelUID cmd = new ChannelUID("my:thi:blabla:command");
+        ChannelUID cmd = new ChannelUID(thing.getUID(), "command");
         handler.handleCommand(cmd, new StringType("dock"));
 
-        System.in.read();
         handler.dispose();
     }
 
-    // @Test
-    void testStop() throws IOException, InterruptedException {
+    @Test
+    void testStop() throws InterruptedException, IOException {
         handler.initialize();
 
-        System.in.read();
-
-        ChannelUID cmd = new ChannelUID("my:thi:blabla:command");
+        ChannelUID cmd = new ChannelUID(thing.getUID(), "command");
         handler.handleCommand(cmd, new StringType("stop"));
 
-        System.in.read();
         handler.dispose();
     }
 }
