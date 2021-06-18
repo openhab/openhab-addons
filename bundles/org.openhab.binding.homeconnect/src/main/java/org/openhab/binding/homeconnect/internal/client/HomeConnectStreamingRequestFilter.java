b3ce4ffca7060d9b466b6255e78a5398c18b962a
bruestel <jonas@bruestel.net>
2021-06-18 11:25:13 +0200

    [homeconnect] Fix invalid authorization header during Server-Sent Events (SSE) client creation (#10848)
    * [homeconnect] Improve logging of SSE connection and add backoff interval in case of connection error
    
    Signed-off-by: Jonas Brüstel <jonas@bruestel.net>
    
    * [homeconnect] Fix SSE authorization header problem
    
    Signed-off-by: Jonas Brüstel <jonas@bruestel.net>
    
    * [homeconnect] Fix synchronized monitor
    
    Signed-off-by: Jonas Brüstel <jonas@bruestel.net>
    Signed-off-by: dschoepel <dave@theschoepels.com>

--- bundles/org.openhab.binding.homeconnect/src/main/java/org/openhab/binding/homeconnect/internal/client/HomeConnectStreamingRequestFilter.java
+++ bundles/org.openhab.binding.homeconnect/src/main/java/org/openhab/binding/homeconnect/internal/client/HomeConnectStreamingRequestFilter.java
@@ -13,14 +13,19 @@
 package org.openhab.binding.homeconnect.internal.client;
 
 import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.client.ClientResponseContext;
+import javax.ws.rs.client.ClientResponseFilter;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MultivaluedMap;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Inserts Authorization header for requests on the streaming REST API.
@@ -28,23 +33,49 @@ import org.eclipse.jdt.annotation.Nullable;
  * @author Laurent Garnier - Initial contribution
  */
 @NonNullByDefault
-public class HomeConnectStreamingRequestFilter implements ClientRequestFilter {
+public class HomeConnectStreamingRequestFilter implements ClientRequestFilter, ClientResponseFilter {
 
     private static final String TEXT_EVENT_STREAM = "text/event-stream";
 
-    private final String authorizationHeader;
-
-    public HomeConnectStreamingRequestFilter(String authorizationHeader) {
-        this.authorizationHeader = authorizationHeader;
-    }
+    private final Logger logger = LoggerFactory.getLogger(HomeConnectStreamingRequestFilter.class);
+    private final ConcurrentHashMap<String, String> authorizationHeaders = new ConcurrentHashMap<>();
 
     @Override
     public void filter(@Nullable ClientRequestContext requestContext) throws IOException {
         if (requestContext != null) {
             MultivaluedMap<String, Object> headers = requestContext.getHeaders();
-            headers.putSingle(HttpHeaders.AUTHORIZATION, authorizationHeader);
+            String authorizationHeader = authorizationHeaders.get(requestContext.getUri().toString());
+            if (authorizationHeader != null) {
+                headers.putSingle(HttpHeaders.AUTHORIZATION, authorizationHeader);
+            } else {
+                logger.warn("No authorization header set! uri={}", requestContext.getUri());
+            }
             headers.putSingle(HttpHeaders.CACHE_CONTROL, "no-cache");
             headers.putSingle(HttpHeaders.ACCEPT, TEXT_EVENT_STREAM);
         }
+    }
+
+    @Override
+    public void filter(@Nullable ClientRequestContext requestContext, @Nullable ClientResponseContext responseContext)
+            throws IOException {
+        if (logger.isDebugEnabled() && requestContext != null) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("SSE connection: ");
+            sb.append(requestContext.getUri()).append("\n");
+            requestContext.getHeaders()
+                    .forEach((name, value) -> sb.append("> ").append(name).append(": ").append(value).append("\n"));
+
+            if (responseContext != null) {
+                responseContext.getHeaders()
+                        .forEach((name, value) -> sb.append("< ").append(name).append(": ").append(value).append("\n"));
+            }
+
+            logger.debug("{}", sb);
+        }
+    }
+
+    public void setAuthorizationHeader(String target, String header) {
+        logger.debug("Set authorization header. target={}, header={}", target, header);
+        authorizationHeaders.put(target, header);
     }
 }
