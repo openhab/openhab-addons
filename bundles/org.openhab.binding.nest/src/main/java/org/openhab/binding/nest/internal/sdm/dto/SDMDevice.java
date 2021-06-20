50c094005f21882fb2d0a8e08953bbc0bbc27823
Wouter Born <github@maindrain.net>
2021-06-20 19:59:46 +0200

    [nest] Add support for Smart Device Management (SDM) API (#8947)
    * [nest] Add support for Smart Device Management (SDM) API
    
    * Reworks WWN implementation so that the thing types have a wwn_ prefix and the classes have a WWN prefix and reside in a 'wwn' package
    * Adds an SDM implementation which is also based on: https://github.com/bhigg-code/openhab-addons/tree/2.5.x/bundles/org.openhab.binding.nestdeviceaccess
    * Adds unit tests for (de)serialization of the SDM and Pub/Sub API requests and responses
    * Updates the binding documentation for the changes and additions
    
    Fixes #8664
    
    Also-by: Brian Higginbotham <brianhigginbothamtx@gmail.com>
    Signed-off-by: Wouter Born <github@maindrain.net>
    
    * Fix and improve documentation
    
    Signed-off-by: Wouter Born <github@maindrain.net>
    
    * Always use UTF8 when decoding SDM events
    
    Signed-off-by: Wouter Born <github@maindrain.net>

---
+++ bundles/org.openhab.binding.nest/src/main/java/org/openhab/binding/nest/internal/sdm/dto/SDMDevice.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.nest.internal.sdm.dto;
+
+import java.util.List;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * An instance of enterprise managed device in the property.
+ *
+ * @author Wouter Born - Initial contribution
+ */
+@NonNullByDefault
+public class SDMDevice {
+    /**
+     * The resource name of the device.
+     */
+    public SDMResourceName name = SDMResourceName.NAMELESS;
+
+    /**
+     * Type of the device for general display purposes.
+     */
+    public @Nullable SDMDeviceType type;
+
+    /**
+     * Device traits.
+     */
+    public SDMTraits traits = new SDMTraits();
+
+    /**
+     * Assignee details of the device.
+     */
+    public List<SDMParentRelation> parentRelations = List.of();
+}
