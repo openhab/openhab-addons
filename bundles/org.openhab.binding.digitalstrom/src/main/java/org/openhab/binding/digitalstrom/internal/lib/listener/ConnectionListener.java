openhab/openhab-addons

commit 9aedfdb454a32b2dbcc04b9461e6c5d4a2c0ead7

Added missing </li> attributes in last comment section (#10890)

bundles/org.openhab.binding.digitalstrom/src/main/java/org/openhab/binding/digitalstrom/internal/lib/listener/ConnectionListener.java
@@ -93,10 +93,9 @@ public interface ConnectionListener {
      * <ul>
      * <li>{@link #WRONG_APP_TOKEN} if the given application-token can't be used.</li>
      * <li>{@link #WRONG_USER_OR_PASSWORD} if the given user name or password can't be used.</li>
-     * <li>{@link #NO_USER_PASSWORD} if no user name or password is set and the given application-token can't be used.
-     * <li>{@link #HOST_NOT_FOUND} if the host can't be found.
-     * <li>{@link #INVALID_URL} if the the URL is invalid.
-     * </li>
+     * <li>{@link #NO_USER_PASSWORD} if no user name or password is set and the given application-token can't be used.</li>
+     * <li>{@link #HOST_NOT_FOUND} if the host can't be found.</li>
+     * <li>{@link #INVALID_URL} if the the URL is invalid.</li>
      * </ul>
      *
      * @param newConnectionState of the connection

commit d53ccc8b0dfd055ded361c11cb026c8dccdb3510

Fix build
Caused by https://github.com/openhab/openhab-addons/pull/10890

Signed-off-by: Wouter Born <github@maindrain.net>

bundles/org.openhab.binding.digitalstrom/src/main/java/org/openhab/binding/digitalstrom/internal/lib/listener/ConnectionListener.java
@@ -93,7 +93,8 @@ public interface ConnectionListener {
      * <ul>
      * <li>{@link #WRONG_APP_TOKEN} if the given application-token can't be used.</li>
      * <li>{@link #WRONG_USER_OR_PASSWORD} if the given user name or password can't be used.</li>
-     * <li>{@link #NO_USER_PASSWORD} if no user name or password is set and the given application-token can't be used.</li>
+     * <li>{@link #NO_USER_PASSWORD} if no user name or password is set and the given application-token can't be
+     * used.</li>
      * <li>{@link #HOST_NOT_FOUND} if the host can't be found.</li>
      * <li>{@link #INVALID_URL} if the the URL is invalid.</li>
      * </ul>