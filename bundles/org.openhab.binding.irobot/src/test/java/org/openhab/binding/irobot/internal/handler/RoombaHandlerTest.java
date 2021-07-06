50284d52f9fd6d00dd47dbb2224e9d7d518dfbd3
Fabian Wolter <github@fabian-wolter.de>
2021-07-06 20:31:46 +0200

    Disable unstable tests
    Signed-off-by: Fabian Wolter <github@fabian-wolter.de>

--- bundles/org.openhab.binding.irobot/src/test/java/org/openhab/binding/irobot/internal/handler/RoombaHandlerTest.java
+++ bundles/org.openhab.binding.irobot/src/test/java/org/openhab/binding/irobot/internal/handler/RoombaHandlerTest.java
@@ -19,6 +19,7 @@ import java.lang.reflect.Field;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -107,6 +108,7 @@ public class RoombaHandlerTest {
     }
 
     @Test
+    @Ignore(value = "Test is unstable see #10954")
     void testDock() throws InterruptedException, IOException {
         handler.initialize();
 
