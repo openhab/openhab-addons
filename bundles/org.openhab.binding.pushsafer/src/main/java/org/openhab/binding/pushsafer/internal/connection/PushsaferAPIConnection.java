fc9864f4348b737bcca26cbcd27a7869a82acf92
Christoph Weitkamp <github@christophweitkamp.de>
2021-07-11 21:45:23 +0200

    [pushsafer] Initial Contribution of Pushsafer Binding (#10790)
    * Pushsafer binding
    
    Signed-off-by: Pushsafer.com (Kevin Siml) <info@appzer.de>
    
    * Improvements and comments from code review
    
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>
    
    * Incorporated comments from review
    
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>
    
    Co-authored-by: Pushsafer.com (Kevin Siml) <info@appzer.de>

---
+++ bundles/org.openhab.binding.pushsafer/src/main/java/org/openhab/binding/pushsafer/internal/connection/PushsaferAPIConnection.java
@@ -0,0 +1,223 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.pushsafer.internal.connection;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentProvider;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.http.HttpMethod;
+import org.eclipse.jetty.http.HttpStatus;
+import org.openhab.binding.pushsafer.internal.config.PushsaferAccountConfiguration;
+import org.openhab.binding.pushsafer.internal.dto.Icon;
+import org.openhab.binding.pushsafer.internal.dto.Sound;
+import org.openhab.core.cache.ExpiringCacheMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+/**
+ * The {@link PushsaferAPIConnection} is responsible for handling the connections to Pushsafer Messages API.
+ *
+ * @author Kevin Siml - Initial contribution, forked from Christoph Weitkamp
+ */
+@NonNullByDefault
+public class PushsaferAPIConnection {
+
+    private final Logger logger = LoggerFactory.getLogger(PushsaferAPIConnection.class);
+
+    private static final String VALIDATE_URL = "https://www.pushsafer.com/api-k";
+    private static final String MESSAGE_URL = "https://www.pushsafer.com/api";
+    private static final String CANCEL_MESSAGE_URL = "https://www.pushsafer.com/api-m";
+    private static final String SOUNDS_URL = "https://www.pushsafer.com/api-s";
+    private static final String ICONS_URL = "https://www.pushsafer.com/api-i";
+
+    private final HttpClient httpClient;
+    private final PushsaferAccountConfiguration config;
+
+    private final ExpiringCacheMap<String, String> cache = new ExpiringCacheMap<>(TimeUnit.DAYS.toMillis(1));
+
+    public PushsaferAPIConnection(HttpClient httpClient, PushsaferAccountConfiguration config) {
+        this.httpClient = httpClient;
+        this.config = config;
+    }
+
+    public boolean validateUser() throws PushsaferCommunicationException, PushsaferConfigurationException {
+        final String localApikey = config.apikey;
+        if (localApikey == null || localApikey.isEmpty()) {
+            throw new PushsaferConfigurationException("@text/offline.conf-error-missing-apikey");
+        }
+        final String localUser = config.user;
+        if (localUser == null || localUser.isEmpty()) {
+            throw new PushsaferConfigurationException("@text/offline.conf-error-missing-user");
+        }
+
+        final String content = get(buildURL(VALIDATE_URL, Map.of(PushsaferMessageBuilder.MESSAGE_KEY_TOKEN, localApikey,
+                PushsaferMessageBuilder.MESSAGE_KEY_USER, localUser)));
+        final JsonObject json = content == null || content.isBlank() ? null
+                : JsonParser.parseString(content).getAsJsonObject();
+        return json == null ? false : getMessageStatus(json);
+    }
+
+    public boolean sendPushsaferMessage(PushsaferMessageBuilder message)
+            throws PushsaferCommunicationException, PushsaferConfigurationException {
+        return getMessageStatus(post(MESSAGE_URL, message.build()));
+    }
+
+    public String sendPushsaferPriorityMessage(PushsaferMessageBuilder message)
+            throws PushsaferCommunicationException, PushsaferConfigurationException {
+        final JsonObject json = JsonParser.parseString(post(MESSAGE_URL, message.build())).getAsJsonObject();
+        return getMessageStatus(json) && json.has("receipt") ? json.get("receipt").getAsString() : "";
+    }
+
+    public boolean cancelPushsaferPriorityMessage(String receipt)
+            throws PushsaferCommunicationException, PushsaferConfigurationException {
+        return getMessageStatus(post(CANCEL_MESSAGE_URL.replace("{receipt}", receipt),
+                PushsaferMessageBuilder.getInstance(config.apikey, config.device).build()));
+    }
+
+    public List<Sound> getSounds() throws PushsaferCommunicationException, PushsaferConfigurationException {
+        final String localApikey = config.apikey;
+        if (localApikey == null || localApikey.isEmpty()) {
+            throw new PushsaferConfigurationException("@text/offline.conf-error-missing-apikey");
+        }
+
+        final Map<String, String> params = new HashMap<>(1);
+        params.put(PushsaferMessageBuilder.MESSAGE_KEY_TOKEN, localApikey);
+
+        final String content = getFromCache(buildURL(SOUNDS_URL, params));
+        final JsonObject json = content == null || content.isBlank() ? null
+                : JsonParser.parseString(content).getAsJsonObject();
+        final JsonObject sounds = json == null || !json.has("sounds") ? null : json.get("sounds").getAsJsonObject();
+
+        return sounds == null ? List.of()
+                : sounds.entrySet().stream().map(entry -> new Sound(entry.getKey(), entry.getValue().getAsString()))
+                        .collect(Collectors.toUnmodifiableList());
+    }
+
+    public List<Icon> getIcons() throws PushsaferCommunicationException, PushsaferConfigurationException {
+        final String localApikey = config.apikey;
+        if (localApikey == null || localApikey.isEmpty()) {
+            throw new PushsaferConfigurationException("@text/offline.conf-error-missing-apikey");
+        }
+
+        final Map<String, String> params = new HashMap<>(1);
+        params.put(PushsaferMessageBuilder.MESSAGE_KEY_TOKEN, localApikey);
+
+        final String content = getFromCache(buildURL(ICONS_URL, params));
+        final JsonObject json = content == null || content.isBlank() ? null
+                : JsonParser.parseString(content).getAsJsonObject();
+        final JsonObject icons = json == null || !json.has("icons") ? null : json.get("icons").getAsJsonObject();
+
+        return icons == null ? List.of()
+                : icons.entrySet().stream().map(entry -> new Icon(entry.getKey(), entry.getValue().getAsString()))
+                        .collect(Collectors.toUnmodifiableList());
+    }
+
+    private String buildURL(String url, Map<String, String> requestParams) {
+        return requestParams.keySet().stream().map(key -> key + "=" + encodeParam(requestParams.get(key)))
+                .collect(Collectors.joining("&", url + "?", ""));
+    }
+
+    private String encodeParam(@Nullable String value) {
+        return value == null ? "" : URLEncoder.encode(value, StandardCharsets.UTF_8);
+    }
+
+    private @Nullable String getFromCache(String url)
+            throws PushsaferCommunicationException, PushsaferConfigurationException {
+        return cache.putIfAbsentAndGet(url, () -> get(url));
+    }
+
+    private String get(String url) throws PushsaferCommunicationException, PushsaferConfigurationException {
+        return executeRequest(HttpMethod.GET, url, null);
+    }
+
+    private String post(String url, ContentProvider body)
+            throws PushsaferCommunicationException, PushsaferConfigurationException {
+        return executeRequest(HttpMethod.POST, url, body);
+    }
+
+    private String executeRequest(HttpMethod httpMethod, String url, @Nullable ContentProvider body)
+            throws PushsaferCommunicationException, PushsaferConfigurationException {
+        logger.trace("Pushsafer request: {} - URL = '{}'", httpMethod, uglifyApikey(url));
+        try {
+            final Request request = httpClient.newRequest(url).method(httpMethod).timeout(10, TimeUnit.SECONDS);
+
+            if (body != null) {
+                if (logger.isTraceEnabled()) {
+                    logger.trace("Pushsafer request body: '{}'", body);
+                }
+                request.content(body);
+            }
+
+            final ContentResponse contentResponse = request.send();
+
+            final int httpStatus = contentResponse.getStatus();
+            final String content = contentResponse.getContentAsString();
+            logger.trace("Pushsafer response: status = {}, content = '{}'", httpStatus, content);
+            switch (httpStatus) {
+                case HttpStatus.OK_200:
+                    return content;
+                case 250:
+                case HttpStatus.BAD_REQUEST_400:
+                    logger.debug("Pushsafer server responded with status code {}: {}", httpStatus, content);
+                    throw new PushsaferConfigurationException(getMessageError(content));
+                default:
+                    logger.debug("Pushsafer server responded with status code {}: {}", httpStatus, content);
+                    throw new PushsaferCommunicationException(content);
+            }
+        } catch (ExecutionException e) {
+            logger.debug("Exception occurred during execution: {}", e.getLocalizedMessage(), e);
+            throw new PushsaferCommunicationException(e.getLocalizedMessage(), e.getCause());
+        } catch (InterruptedException | TimeoutException e) {
+            logger.debug("Exception occurred during execution: {}", e.getLocalizedMessage(), e);
+            throw new PushsaferCommunicationException(e.getLocalizedMessage());
+        }
+    }
+
+    private String uglifyApikey(String url) {
+        return url.replaceAll("(k=)+\\w+", "k=*****");
+    }
+
+    private String getMessageError(String content) {
+        final JsonObject json = JsonParser.parseString(content).getAsJsonObject();
+        final JsonElement errorsElement = json.get("errors");
+        if (errorsElement != null && errorsElement.isJsonArray()) {
+            return errorsElement.getAsJsonArray().toString();
+        }
+        return "@text/offline.conf-error-unknown";
+    }
+
+    private boolean getMessageStatus(String content) {
+        return getMessageStatus(JsonParser.parseString(content).getAsJsonObject());
+    }
+
+    private boolean getMessageStatus(JsonObject json) {
+        return json.has("status") ? json.get("status").getAsInt() == 1 : false;
+    }
+}
---
+++ bundles/org.openhab.binding.pushsafer/src/main/java/org/openhab/binding/pushsafer/internal/connection/PushsaferMessageBuilder.java
@@ -0,0 +1,355 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.pushsafer.internal.connection;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jetty.client.api.ContentProvider;
+import org.eclipse.jetty.client.util.MultiPartContentProvider;
+import org.eclipse.jetty.client.util.StringContentProvider;
+import org.openhab.core.io.net.http.HttpUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link PushsaferMessageBuilder} builds the body for Pushsafer Messages API requests.
+ *
+ * @author Kevin Siml - Initial contribution, forked from Christoph Weitkamp
+ */
+@NonNullByDefault
+public class PushsaferMessageBuilder {
+
+    private final Logger logger = LoggerFactory.getLogger(PushsaferMessageBuilder.class);
+
+    public static final String MESSAGE_KEY_TOKEN = "k";
+    public static final String MESSAGE_KEY_USER = "u";
+    private static final String MESSAGE_KEY_MESSAGE = "m";
+    private static final String MESSAGE_KEY_TITLE = "t";
+    private static final String MESSAGE_KEY_DEVICE = "d";
+    private static final String MESSAGE_KEY_ICON = "i";
+    private static final String MESSAGE_KEY_COLOR = "c";
+    private static final String MESSAGE_KEY_VIBRATION = "v";
+    private static final String MESSAGE_KEY_PRIORITY = "pr";
+    private static final String MESSAGE_KEY_RETRY = "re";
+    private static final String MESSAGE_KEY_EXPIRE = "ex";
+    private static final String MESSAGE_KEY_URL = "u";
+    private static final String MESSAGE_KEY_URL_TITLE = "ut";
+    private static final String MESSAGE_KEY_SOUND = "s";
+    private static final String MESSAGE_KEY_TIME2LIVE = "l";
+    private static final String MESSAGE_KEY_ANSWER = "a";
+    private static final String MESSAGE_KEY_CONFIRM = "cr";
+    private static final String MESSAGE_KEY_ATTACHMENT = "p";
+    public static final String MESSAGE_KEY_HTML = "html";
+    public static final String MESSAGE_KEY_MONOSPACE = "monospace";
+
+    private static final int MAX_MESSAGE_LENGTH = 4096;
+    private static final int MAX_TITLE_LENGTH = 250;
+    private static final int MAX_DEVICE_LENGTH = 25;
+    private static final List<Integer> VALID_PRIORITY_LIST = Arrays.asList(-2, -1, 0, 1, 2);
+    private static final int DEFAULT_PRIORITY = 0;
+    public static final int EMERGENCY_PRIORITY = 2;
+    private static final int MIN_RETRY_SECONDS = 0;
+    private static final int MAX_EXPIRE_SECONDS = 10800;
+    private static final int MAX_URL_LENGTH = 512;
+    private static final int MAX_URL_TITLE_LENGTH = 100;
+    public static final String DEFAULT_CONTENT_TYPE = "jpeg";
+    public static final String DEFAULT_AUTH = "";
+
+    private final MultiPartContentProvider body = new MultiPartContentProvider();
+
+    private @Nullable String message;
+    private @Nullable String title;
+    private @Nullable String device;
+    private int priority = DEFAULT_PRIORITY;
+    private int retry = 300;
+    private int expire = 3600;
+    private @Nullable String url;
+    private @Nullable String urlTitle;
+    private @Nullable String sound;
+    private @Nullable String icon;
+    private int confirm;
+    private int time2live;
+    private boolean answer;
+    private @Nullable String color;
+    private @Nullable String vibration;
+    private @Nullable String attachment;
+    private String contentType = DEFAULT_CONTENT_TYPE;
+    private String authentication = DEFAULT_AUTH;
+    private boolean html = false;
+    private boolean monospace = false;
+
+    private PushsaferMessageBuilder(String apikey, String device) throws PushsaferConfigurationException {
+        body.addFieldPart(MESSAGE_KEY_TOKEN, new StringContentProvider(apikey), null);
+        body.addFieldPart(MESSAGE_KEY_DEVICE, new StringContentProvider(device), null);
+    }
+
+    public static PushsaferMessageBuilder getInstance(@Nullable String apikey, @Nullable String device)
+            throws PushsaferConfigurationException {
+        if (apikey == null || apikey.isEmpty()) {
+            throw new PushsaferConfigurationException("@text/offline.conf-error-missing-apikey");
+        }
+
+        if (device == null || device.isEmpty()) {
+            throw new PushsaferConfigurationException("@text/offline.conf-error-missing-device");
+        }
+
+        return new PushsaferMessageBuilder(apikey, device);
+    }
+
+    public PushsaferMessageBuilder withMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withDevice(String device) {
+        this.device = device;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withPriority(int priority) {
+        this.priority = priority;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withRetry(int retry) {
+        this.retry = retry;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withExpire(int expire) {
+        this.expire = expire;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withUrlTitle(String urlTitle) {
+        this.urlTitle = urlTitle;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withSound(String sound) {
+        this.sound = sound;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withIcon(String icon) {
+        this.icon = icon;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withColor(String color) {
+        this.color = color;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withVibration(String vibration) {
+        this.vibration = vibration;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withAnswer(boolean answer) {
+        this.answer = answer;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withTime2live(int time2live) {
+        this.time2live = time2live;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withConfirm(int confirm) {
+        this.confirm = confirm;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withAttachment(String attachment) {
+        this.attachment = attachment;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withContentType(String contentType) {
+        this.contentType = contentType;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withAuthentication(String authentication) {
+        this.authentication = authentication;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withHtmlFormatting() {
+        this.html = true;
+        return this;
+    }
+
+    public PushsaferMessageBuilder withMonospaceFormatting() {
+        this.monospace = true;
+        return this;
+    }
+
+    public ContentProvider build() throws PushsaferCommunicationException {
+        if (message != null) {
+            if (message.length() > MAX_MESSAGE_LENGTH) {
+                throw new IllegalArgumentException(String.format(
+                        "Skip sending the message as 'message' is longer than %d characters.", MAX_MESSAGE_LENGTH));
+            }
+            body.addFieldPart(MESSAGE_KEY_MESSAGE, new StringContentProvider(message), null);
+        }
+
+        if (title != null) {
+            if (title.length() > MAX_TITLE_LENGTH) {
+                throw new IllegalArgumentException(String
+                        .format("Skip sending the message as 'title' is longer than %d characters.", MAX_TITLE_LENGTH));
+            }
+            body.addFieldPart(MESSAGE_KEY_TITLE, new StringContentProvider(title), null);
+        }
+
+        if (device != null) {
+            if (device.length() > MAX_DEVICE_LENGTH) {
+                logger.warn("Skip 'device' as it is longer than {} characters. Got: {}.", MAX_DEVICE_LENGTH, device);
+            } else {
+                body.addFieldPart(MESSAGE_KEY_DEVICE, new StringContentProvider(device), null);
+            }
+        }
+
+        if (priority != DEFAULT_PRIORITY) {
+            if (VALID_PRIORITY_LIST.contains(priority)) {
+                body.addFieldPart(MESSAGE_KEY_PRIORITY, new StringContentProvider(String.valueOf(priority)), null);
+
+                if (priority == EMERGENCY_PRIORITY) {
+                    if (retry < MIN_RETRY_SECONDS) {
+                        logger.warn("Retry value of {} is too small. Using default value of {}.", retry,
+                                MIN_RETRY_SECONDS);
+                        body.addFieldPart(MESSAGE_KEY_RETRY,
+                                new StringContentProvider(String.valueOf(MIN_RETRY_SECONDS)), null);
+                    } else {
+                        body.addFieldPart(MESSAGE_KEY_RETRY, new StringContentProvider(String.valueOf(retry)), null);
+                    }
+
+                    if (0 < expire && expire <= MAX_EXPIRE_SECONDS) {
+                        body.addFieldPart(MESSAGE_KEY_EXPIRE, new StringContentProvider(String.valueOf(expire)), null);
+                    } else {
+                        logger.warn("Expire value of {} is invalid. Using default value of {}.", expire,
+                                MAX_EXPIRE_SECONDS);
+                        body.addFieldPart(MESSAGE_KEY_EXPIRE,
+                                new StringContentProvider(String.valueOf(MAX_EXPIRE_SECONDS)), null);
+                    }
+                }
+            } else {
+                logger.warn("Invalid 'priority', skipping. Expected: {}. Got: {}.",
+                        VALID_PRIORITY_LIST.stream().map(i -> i.toString()).collect(Collectors.joining(",")), priority);
+            }
+        }
+
+        if (url != null) {
+            if (url.length() > MAX_URL_LENGTH) {
+                throw new IllegalArgumentException(String
+                        .format("Skip sending the message as 'url' is longer than %d characters.", MAX_URL_LENGTH));
+            }
+            body.addFieldPart(MESSAGE_KEY_URL, new StringContentProvider(url), null);
+
+            if (urlTitle != null) {
+                if (urlTitle.length() > MAX_URL_TITLE_LENGTH) {
+                    throw new IllegalArgumentException(
+                            String.format("Skip sending the message as 'urlTitle' is longer than %d characters.",
+                                    MAX_URL_TITLE_LENGTH));
+                }
+                body.addFieldPart(MESSAGE_KEY_URL_TITLE, new StringContentProvider(urlTitle), null);
+            }
+        }
+
+        if (sound != null) {
+            body.addFieldPart(MESSAGE_KEY_SOUND, new StringContentProvider(sound), null);
+        }
+
+        if (icon != null) {
+            body.addFieldPart(MESSAGE_KEY_ICON, new StringContentProvider(icon), null);
+        }
+
+        if (color != null) {
+            body.addFieldPart(MESSAGE_KEY_COLOR, new StringContentProvider(color), null);
+        }
+
+        if (vibration != null) {
+            body.addFieldPart(MESSAGE_KEY_VIBRATION, new StringContentProvider(vibration), null);
+        }
+
+        body.addFieldPart(MESSAGE_KEY_CONFIRM, new StringContentProvider(String.valueOf(confirm)), null);
+
+        body.addFieldPart(MESSAGE_KEY_ANSWER, new StringContentProvider(String.valueOf(answer)), null);
+
+        body.addFieldPart(MESSAGE_KEY_TIME2LIVE, new StringContentProvider(String.valueOf(time2live)), null);
+
+        if (attachment != null) {
+            final String encodedString;
+            try {
+                if (attachment.startsWith("http")) {
+                    Properties headers = new Properties();
+                    headers.put("User-Agent", "Mozilla/5.0");
+                    if (!authentication.isBlank()) {
+                        headers.put("Authorization", "Basic "
+                                + Base64.getEncoder().encodeToString(authentication.getBytes(StandardCharsets.UTF_8)));
+                    }
+                    String content = HttpUtil.executeUrl("GET", attachment, headers, null, null, 10);
+                    if (content == null) {
+                        throw new IllegalArgumentException(
+                                String.format("Skip sending the message as content '%s' does not exist.", attachment));
+                    }
+                    encodedString = "data:" + contentType + ";base64," + content;
+                } else {
+                    File file = new File(attachment);
+                    if (!file.exists()) {
+                        throw new IllegalArgumentException(
+                                String.format("Skip sending the message as file '%s' does not exist.", attachment));
+                    }
+                    byte[] fileContent = Files.readAllBytes(file.toPath());
+                    encodedString = "data:image/" + contentType + ";base64,"
+                            + Base64.getEncoder().encodeToString(fileContent);
+                }
+                body.addFieldPart(MESSAGE_KEY_ATTACHMENT, new StringContentProvider(encodedString), null);
+            } catch (IOException e) {
+                logger.debug("IOException occurred - skip sending message: {}", e.getLocalizedMessage(), e);
+                throw new PushsaferCommunicationException(
+                        String.format("Skip sending the message: %s", e.getLocalizedMessage()), e);
+            }
+        }
+
+        if (html) {
+            body.addFieldPart(MESSAGE_KEY_HTML, new StringContentProvider("1"), null);
+        } else if (monospace) {
+            body.addFieldPart(MESSAGE_KEY_MONOSPACE, new StringContentProvider("1"), null);
+        }
+
+        body.close();
+        return body;
+    }
+}
