4545b8e3cb08cc016946cec8677fae8d68631b1e
morph166955 <rosenblumb@gmail.com>
2021-07-11 20:41:04 -0500

    Adds support for new codec channel for Arc and Arc SL
    Signed-off-by: morph166955 <rosenblumb@gmail.com>

--- bundles/org.openhab.binding.sonos/src/main/java/org/openhab/binding/sonos/internal/handler/ZonePlayerHandler.java
+++ bundles/org.openhab.binding.sonos/src/main/java/org/openhab/binding/sonos/internal/handler/ZonePlayerHandler.java
@@ -1478,7 +1478,45 @@ public class ZonePlayerHandler extends BaseThingHandler implements UpnpIOPartici
     }
 
     public @Nullable String getCodec() {
-        return stateMap.get("HTAudioIn");
+        String codec = stateMap.get("HTAudioIn");
+        switch (codec) {
+            case "0":
+                codec = "No Signal";
+                break;
+            case "21":
+                codec = "No Signal";
+                break;
+            case "22":
+                codec = "Silence";
+                break;
+            case "32":
+                codec = "DTS";
+                break;
+            case "59":
+                codec = "Dolby Atmos";
+                break;
+            case "63":
+                codec = "Dolby Atmos";
+                break;
+            case "33554454":
+                codec = "Silence 2.0";
+                break;
+            case "33554434":
+                codec = "Dolby Digital 2.0";
+                break;
+            case "84934713":
+                codec = "Dolby Digital 5.1";
+                break;
+            case "84934714":
+                codec = "Dolby Digital Plus 5.1";
+                break;
+            case "84934718":
+                codec = "Dolby Multichannel PCM 5.1";
+                break;
+            default:
+                codec = "Unknown - " + codec;
+        }
+        return codec;
     }
 
     public @Nullable String getSubwooferEnabled() {
