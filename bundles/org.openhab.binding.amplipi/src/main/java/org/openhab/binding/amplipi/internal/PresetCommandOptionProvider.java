7b2bee8706de5ee43f2821c3ed9fc65fedb8ae57
Kai Kreuzer <kai@openhab.org>
2021-08-01 12:25:29 +0200

    [amplipi] Initial contribution of AmpliPi binding (#10983)
    * Initial contribution of AmpliPi binding
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * change http client from cxf jax-rs to Jetty
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * applied spotless
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * Remove Jackson dependency
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * Add support for input handling
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * Clean up, improvements and documentation
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * Remove unused password from configuration class
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * Remove example properties file
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * revert change in .gitignore
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * Update README
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * Address review feedback
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * Handle ExecutionException as network error
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

---
+++ bundles/org.openhab.binding.amplipi/src/main/java/org/openhab/binding/amplipi/internal/PresetCommandOptionProvider.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.amplipi.internal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.amplipi.internal.model.Preset;
+import org.openhab.core.thing.Channel;
+import org.openhab.core.thing.binding.BaseDynamicCommandDescriptionProvider;
+import org.openhab.core.thing.binding.ThingHandler;
+import org.openhab.core.thing.binding.ThingHandlerService;
+import org.openhab.core.thing.type.ChannelTypeUID;
+import org.openhab.core.types.CommandDescription;
+import org.openhab.core.types.CommandOption;
+
+/**
+ * This class provides the list of valid commands for the preset channel.
+ *
+ * @author Kai Kreuzer - Initial contribution
+ *
+ */
+@NonNullByDefault
+public class PresetCommandOptionProvider extends BaseDynamicCommandDescriptionProvider implements ThingHandlerService {
+
+    private @Nullable AmpliPiHandler handler;
+
+    @Override
+    public void setThingHandler(ThingHandler handler) {
+        this.handler = (AmpliPiHandler) handler;
+    }
+
+    @Override
+    public @Nullable ThingHandler getThingHandler() {
+        return handler;
+    }
+
+    @Override
+    public @Nullable CommandDescription getCommandDescription(Channel channel,
+            @Nullable CommandDescription originalCommandDescription, @Nullable Locale locale) {
+        ChannelTypeUID typeUID = channel.getChannelTypeUID();
+        List<CommandOption> options = new ArrayList<>();
+        if (typeUID != null && AmpliPiBindingConstants.CHANNEL_PRESET.equals(typeUID.getId()) && handler != null) {
+            List<Preset> presets = handler.getPresets();
+            for (Preset preset : presets) {
+                options.add(new CommandOption(preset.getId().toString(), preset.getName()));
+            }
+            setCommandOptions(channel.getUID(), options);
+        }
+        return super.getCommandDescription(channel, originalCommandDescription, locale);
+    }
+}
