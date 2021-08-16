6257a7a40b867f096357be8248c012b883028c90
Luca Calcaterra <calcaterra.luca@gmail.com>
2021-08-16 20:52:49 +0200

    Update SoulissT11Handler.java
    wrong header
    
    Signed-off-by: Luca Calcaterra <calcaterra.luca@gmail.com>

--- bundles/org.openhab.binding.souliss/src/main/java/org/openhab/binding/souliss/internal/handler/SoulissT11Handler.java
+++ bundles/org.openhab.binding.souliss/src/main/java/org/openhab/binding/souliss/internal/handler/SoulissT11Handler.java
@@ -9,12 +9,6 @@
  * http://www.eclipse.org/legal/epl-2.0
  *
  * SPDX-License-Identifier: EPL-2.0
-=======
- * Copyright (c) 2014-2019 by the respective copyright holders.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
  */
 package org.openhab.binding.souliss.internal.handler;
 
