158d600263e23a57285a94e876a1328517204830
Matthias Herrmann <matthias.mh.herrmann@gmail.com>
2021-08-27 08:25:58 +0200

    Proteus Eco Meter Binding
    Signed-off-by: Matthias Herrmann <matthias.mh.herrmann@gmail.com>

---
+++ bundles/org.openhab.binding.proteusecometer/README.md
@@ -0,0 +1,134 @@
+# ProteusEcoMeter Binding
+
+This is the binding for the Proteus EcoMeter S, which is able to report the level of a cistern or tank.
+
+Note that this binding currently supports no write channels. This means you have to configure your sensor by
+considering the manual of the product (using wireless display). After doing that the binding comes into play
+and helps you to get your measured values into openHAB. Please be patient while waiting for the first received
+data. The sensor reports at an interval of approx. 1h, except when the water level changes relatively fast.
+
+## Supported Things
+
+Proteus EcoMeter S. The binding has been tested with this EcoMeter sensor only.
+
+## Discovery
+
+No auto discovery implemented yet.
+
+## Thing Configuration
+
+Plug the wireless display into an USB port. Note [openHAB Serial Port documentation](https://www.openhab.org/docs/administration/serial.html) for general serial port configuration. After that you can add the device as thing and configure the usbPort your OS generated for the display.
+
+```
+UID: proteusecometer:EcoMeterS:e90705eaa4
+label: Proteus EcoMeter S
+thingTypeUID: proteusecometer:EcoMeterS
+configuration:
+  usbPort: /dev/ttyUSB0
+```
+
+## Channels
+
+| channel               | type              | description                                          |
+|-----------------------|-------------------|------------------------------------------------------|
+| temperature           | Number:CELSIUS    | Temperature measured by the sensor                   |
+| sensorLevel           | Number:centiMetre | Distance between sensor and water surface            |
+| usableLevelInLitre    | Number:LITRE      | How much liquid is usable                            |
+| usableLevelInPercent  | Number:LITRE      | How much liquid is usable relative to total capacity |
+| totalCapacity         | Number:LITRE      | Total capacity of measured cistern/tank              |
+
+## Full Example
+
+```
+{
+  "statusInfo": {
+    "status": "ONLINE",
+    "statusDetail": "NONE"
+  },
+  "editable": true,
+  "label": "Proteus EcoMeter S",
+  "configuration": {
+    "usbPort": "/dev/ttyUSB0"
+  },
+  "properties": {},
+  "UID": "proteusecometer:EcoMeterS:e90705eaa4",
+  "thingTypeUID": "proteusecometer:EcoMeterS",
+  "channels": [
+    {
+      "linkedItems": [
+        "ProteusEcoMeterS_Temperature"
+      ],
+      "uid": "proteusecometer:EcoMeterS:e90705eaa4:temperature",
+      "id": "temperature",
+      "channelTypeUID": "proteusecometer:Temperature",
+      "itemType": "Number:CELSIUS",
+      "kind": "STATE",
+      "label": "Temperature",
+      "description": "Temperature measured by the sensor",
+      "defaultTags": [],
+      "properties": {},
+      "configuration": {}
+    },
+    {
+      "linkedItems": [
+        "ProteusEcoMeterS_SensorLevelCm"
+      ],
+      "uid": "proteusecometer:EcoMeterS:e90705eaa4:sensorLevel",
+      "id": "sensorLevel",
+      "channelTypeUID": "proteusecometer:SensorLevel",
+      "itemType": "Number:centiMetre",
+      "kind": "STATE",
+      "label": "Sensor Level",
+      "description": "The distance between the sensor and the water surface",
+      "defaultTags": [],
+      "properties": {},
+      "configuration": {}
+    },
+    {
+      "linkedItems": [
+        "ProteusEcoMeterS_UsableLevelinliter"
+      ],
+      "uid": "proteusecometer:EcoMeterS:e90705eaa4:usableLevelInLitre",
+      "id": "usableLevelInLitre",
+      "channelTypeUID": "proteusecometer:UsableLevelInLitre",
+      "itemType": "Number:LITRE",
+      "kind": "STATE",
+      "label": "Usable Level in litre",
+      "description": "The usable level in litre",
+      "defaultTags": [],
+      "properties": {},
+      "configuration": {}
+    },
+    {
+      "linkedItems": [
+        "ProteusEcoMeterS_UsableLevelinpercent"
+      ],
+      "uid": "proteusecometer:EcoMeterS:e90705eaa4:usableLevelInPercent",
+      "id": "usableLevelInPercent",
+      "channelTypeUID": "proteusecometer:UsableLevelInPercent",
+      "itemType": "Number:PERCENT",
+      "kind": "STATE",
+      "label": "Usable Level in percent",
+      "description": "The usable level in percent",
+      "defaultTags": [],
+      "properties": {},
+      "configuration": {}
+    },
+    {
+      "linkedItems": [
+        "ProteusEcoMeterS_TotalCapacityinliter"
+      ],
+      "uid": "proteusecometer:EcoMeterS:e90705eaa4:totalCapacity",
+      "id": "totalCapacity",
+      "channelTypeUID": "proteusecometer:TotalCapacity",
+      "itemType": "Number:LITRE",
+      "kind": "STATE",
+      "label": "Total Capacity",
+      "description": "The total capacity of your cistern/tank",
+      "defaultTags": [],
+      "properties": {},
+      "configuration": {}
+    }
+  ]
+}
+```
---
+++ bundles/org.openhab.binding.proteusecometer/src/main/java/org/openhab/binding/proteusecometer/internal/ecometers/handler/ProteusEcoMeterSHandler.java
@@ -0,0 +1,157 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.proteusecometer.internal.ecometers.handler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Duration;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.proteusecometer.internal.ProteusEcoMeterBindingConstants;
+import org.openhab.binding.proteusecometer.internal.ProteusEcoMeterConfiguration;
+import org.openhab.binding.proteusecometer.internal.WrappedException;
+import org.openhab.binding.proteusecometer.internal.ecometers.ProteusEcoMeterSReply;
+import org.openhab.binding.proteusecometer.internal.ecometers.ProteusEcoMeterSService;
+import org.openhab.binding.proteusecometer.internal.serialport.SerialPortService;
+import org.openhab.core.library.types.QuantityType;
+import org.openhab.core.library.unit.MetricPrefix;
+import org.openhab.core.library.unit.SIUnits;
+import org.openhab.core.library.unit.Units;
+import org.openhab.core.thing.ChannelUID;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.thing.ThingStatus;
+import org.openhab.core.thing.ThingStatusDetail;
+import org.openhab.core.thing.binding.BaseThingHandler;
+import org.openhab.core.types.Command;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fazecast.jSerialComm.SerialPort;
+
+/**
+ * The {@link ProteusEcoMeterSHandler} updates thing channels when receiving data
+ *
+ * @author Matthias Herrmann - Initial contribution
+ */
+@NonNullByDefault
+public class ProteusEcoMeterSHandler extends BaseThingHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(ProteusEcoMeterSHandler.class);
+    private @Nullable SerialPort serialPort;
+    private @Nullable ProteusEcoMeterConfiguration config;
+    private @Nullable ScheduledFuture<?> job;
+
+    public ProteusEcoMeterSHandler(final Thing thing) {
+        super(thing);
+    }
+
+    @Override
+    public void initialize() {
+        config = getConfigAs(ProteusEcoMeterConfiguration.class);
+        final ProteusEcoMeterConfiguration localConfig = config;
+        if (localConfig == null) {
+            final String msg = "Expected initialize to be called ONLY when config is not null";
+            logger.error(msg);
+            throw new IllegalStateException(msg);
+        }
+        updateStatus(ThingStatus.UNKNOWN);
+        job = scheduler.schedule(() -> {
+
+            try {
+                final ProteusEcoMeterSService ecoMeterSService = new ProteusEcoMeterSService();
+                final Stream<ProteusEcoMeterSReply> replyStream = ecoMeterSService.read(localConfig.usbPort,
+                        serialPortService);
+                updateStatus(ThingStatus.ONLINE);
+
+                replyStream.forEach(reply -> {
+                    updateState(ProteusEcoMeterBindingConstants.SENSOR_LEVEL,
+                            new QuantityType<>(reply.sensorLevelInCm, MetricPrefix.CENTI(SIUnits.METRE)));
+                    updateState(ProteusEcoMeterBindingConstants.USABLE_LEVEL_IN_LITRE,
+                            new QuantityType<>(reply.usableLevelInLiter, Units.LITRE));
+                    updateState(ProteusEcoMeterBindingConstants.USABLE_LEVEL_IN_PERCENT, new QuantityType<>(
+                            100d / reply.totalCapacityInLiter * reply.usableLevelInLiter, Units.PERCENT));
+                    updateState(ProteusEcoMeterBindingConstants.TEMPERATURE,
+                            new QuantityType<>(reply.tempInCelsius, SIUnits.CELSIUS));
+                    updateState(ProteusEcoMeterBindingConstants.TOTAL_CAPACITY,
+                            new QuantityType<>(reply.totalCapacityInLiter, Units.LITRE));
+                });
+                closeSerialPort();
+            } catch (final Exception e) {
+                updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.OFFLINE.GONE,
+                        "Error reading from Port: " + e.getMessage());
+                final Duration retryInitDelay = Duration.ofSeconds(10);
+                logger.debug("Error communicating with eco meter s. Retrying in {}", retryInitDelay, e);
+                closeSerialPort();
+                job = scheduler.schedule(this::initialize, retryInitDelay.getSeconds(), TimeUnit.SECONDS);
+            }
+        }, 0, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        // at the moment there are no commands supported. The Eco Meter S would support configuration
+        // commands, but this is not implemented yet
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        closeSerialPort();
+        final ScheduledFuture<?> localJob = job;
+        if (localJob != null) {
+            localJob.cancel(true);
+            job = null;
+        }
+    }
+
+    private void closeSerialPort() {
+        if (serialPort != null) {
+            final boolean closed = serialPort.closePort();
+            logger.debug("serialPort.closePort() returned {}", closed);
+            serialPort = null;
+        }
+    }
+
+    private SerialPortService serialPortService = new SerialPortService() {
+        @NonNullByDefault
+        public InputStream getInputStream(String portId, int baudRate, int numDataBits, int numStopBits, int parity) {
+            try {
+                ProteusEcoMeterSHandler.this.serialPort = SerialPort.getCommPort(portId);
+                final SerialPort localSerialPort = ProteusEcoMeterSHandler.this.serialPort;
+                if (localSerialPort == null) {
+                    throw new IOException("SerialPort.getCommPort(" + portId + ") returned null");
+                }
+                localSerialPort.closePort();
+
+                localSerialPort.setBaudRate(baudRate);
+                localSerialPort.setNumDataBits(numDataBits);
+                localSerialPort.setNumStopBits(numStopBits);
+                localSerialPort.setParity(parity);
+                localSerialPort.openPort();
+                localSerialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+                final InputStream inputStream = localSerialPort.getInputStream();
+                if (inputStream == null) {
+                    throw new IOException("serialPort.getInputStream() returned null");
+                }
+                return inputStream;
+            } catch (final Exception e) {
+                closeSerialPort();
+                throw new WrappedException(e);
+            }
+        }
+    };
+}
