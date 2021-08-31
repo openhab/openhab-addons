dfc00764ea4fdfe43d565c5bf0eb0d905cbd4f55
Christoph Weitkamp <github@christophweitkamp.de>
2021-08-31 08:04:35 +0200

    Added support for DECT500 and HAN-FUN bulbs
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

---
+++ bundles/org.openhab.binding.avmfritz/src/main/java/org/openhab/binding/avmfritz/internal/handler/AVMFritzColorLightDeviceHandler.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.avmfritz.internal.handler;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.thing.Thing;
+
+/**
+ * Handler for a FRITZ! color light device. Handles commands, which are sent to one of the channels.
+ *
+ * @author Joshua Bacher - Initial contribution
+ * @author Christoph Weitkamp - Initial contribution
+ */
+@NonNullByDefault
+public class AVMFritzColorLightDeviceHandler extends DeviceHandler {
+
+    // TODO: when getting here, we know that we need to retrieve the default colors. Maybe we can use that.
+    public AVMFritzColorLightDeviceHandler(Thing thing) {
+        super(thing);
+    }
+}
