db77afef4793d498b6b0920d999a888ea0c2bbad
Bernd Weymann <bernd.weymann@gmail.com>
2021-09-18 19:15:26 +0200

    remove repeated navigation callback message
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

--- bundles/org.openhab.binding.bmwconnecteddrive/src/main/java/org/openhab/binding/bmwconnecteddrive/internal/handler/VehicleHandler.java
+++ bundles/org.openhab.binding.bmwconnecteddrive/src/main/java/org/openhab/binding/bmwconnecteddrive/internal/handler/VehicleHandler.java
@@ -691,7 +691,6 @@ public class VehicleHandler extends VehicleChannelHandler {
     public class NavigationStatusCallback implements StringResponseCallback {
         @Override
         public void onResponse(@Nullable String content) {
-            logger.debug("Navigation: {}", content);
             if (content != null) {
                 try {
                     NavigationContainer nav = Converter.getGson().fromJson(content, NavigationContainer.class);
