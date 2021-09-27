c93ec4cf49743e23063ad8cb1b82f6a2a6db67a0
Wouter Born <github@maindrain.net>
2021-09-27 22:00:01 +0200

    [lifx] Add channels for controlling absolute color temperature in Kelvin (#11309)
    * [lifx] Add channels for controlling absolute color temperature in Kelvin
    
    Adds an 'abstemperature' channel to all Thing Types for controlling the absolute color temperature in Kelvin.
    MultiZone lights also have 'abstemperaturezone' channels that allow for controlling the color temperature of a zone in Kelvin.
    These channels make it easier to use the same color temperature with lights that have a different color temperature range.
    Furthermore the channel type tags have been updated which simplifies using the generated items with the semantic model.
    
    Signed-off-by: Wouter Born <github@maindrain.net>

--- bundles/org.openhab.binding.lifx/src/main/java/org/openhab/binding/lifx/internal/LifxBindingConstants.java
+++ bundles/org.openhab.binding.lifx/src/main/java/org/openhab/binding/lifx/internal/LifxBindingConstants.java
@@ -49,6 +49,8 @@ public class LifxBindingConstants {
     public static final PercentType DEFAULT_BRIGHTNESS = PercentType.HUNDRED;
 
     // List of all Channel IDs
+    public static final String CHANNEL_ABS_TEMPERATURE = "abstemperature";
+    public static final String CHANNEL_ABS_TEMPERATURE_ZONE = "abstemperaturezone";
     public static final String CHANNEL_BRIGHTNESS = "brightness";
     public static final String CHANNEL_COLOR = "color";
     public static final String CHANNEL_COLOR_ZONE = "colorzone";
@@ -62,12 +64,10 @@ public class LifxBindingConstants {
     // List of all Channel Type UIDs
     public static final ChannelTypeUID CHANNEL_TYPE_BRIGHTNESS = new ChannelTypeUID(BINDING_ID, CHANNEL_BRIGHTNESS);
     public static final ChannelTypeUID CHANNEL_TYPE_COLOR = new ChannelTypeUID(BINDING_ID, CHANNEL_COLOR);
-    public static final ChannelTypeUID CHANNEL_TYPE_COLOR_ZONE = new ChannelTypeUID(BINDING_ID, CHANNEL_COLOR_ZONE);
     public static final ChannelTypeUID CHANNEL_TYPE_EFFECT = new ChannelTypeUID(BINDING_ID, CHANNEL_EFFECT);
+    public static final ChannelTypeUID CHANNEL_TYPE_HEV_CYCLE = new ChannelTypeUID(BINDING_ID, CHANNEL_HEV_CYCLE);
     public static final ChannelTypeUID CHANNEL_TYPE_INFRARED = new ChannelTypeUID(BINDING_ID, CHANNEL_INFRARED);
     public static final ChannelTypeUID CHANNEL_TYPE_TEMPERATURE = new ChannelTypeUID(BINDING_ID, CHANNEL_TEMPERATURE);
-    public static final ChannelTypeUID CHANNEL_TYPE_TEMPERATURE_ZONE = new ChannelTypeUID(BINDING_ID,
-            CHANNEL_TEMPERATURE_ZONE);
 
     // List of options for effect channel
     public static final String CHANNEL_TYPE_EFFECT_OPTION_OFF = "off";
