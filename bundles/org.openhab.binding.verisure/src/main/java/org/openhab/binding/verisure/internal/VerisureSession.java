2eb6b82357e10fc0896ddf29041aac056aed1332
Jan Gustafsson <jannegpriv@gmail.com>
2021-10-16 11:28:08 +0200

    [verisure] Adapted to new authentication process and support for non MFA activated user. (#11228) (#11265)
    * [verisure] Adapted to new authentication process and support for non MFA activated user. (#11228)
    
    Signed-off-by: Jan Gustafsson <jannegpriv@gmail.com>
    
    * Updated after code review.
    
    Signed-off-by: Jan Gustafsson <jannegpriv@gmail.com>
    
    * Updated after code review.
    
    Signed-off-by: Jan Gustafsson <jannegpriv@gmail.com>
    
    * Updated after code review.
    
    Signed-off-by: Jan Gustafsson <jannegpriv@gmail.com>

--- bundles/org.openhab.binding.verisure/src/main/java/org/openhab/binding/verisure/internal/VerisureSession.java
+++ bundles/org.openhab.binding.verisure/src/main/java/org/openhab/binding/verisure/internal/VerisureSession.java
@@ -17,9 +17,12 @@ import static org.openhab.binding.verisure.internal.VerisureBindingConstants.*;
 import java.math.BigDecimal;
 import java.net.CookieStore;
 import java.net.HttpCookie;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -86,24 +89,30 @@ public class VerisureSession {
     private int apiServerInUseIndex = 0;
     private int numberOfEvents = 15;
     private static final String USER_NAME = "username";
-    private static final String PASSWORD_NAME = "vid";
+    private static final String VID = "vid";
+    private static final String VS_STEPUP = "vs-stepup";
+    private static final String VS_ACCESS = "vs-access";
     private String apiServerInUse = APISERVERLIST.get(apiServerInUseIndex);
     private String authstring = "";
     private @Nullable String csrf;
     private @Nullable String pinCode;
     private HttpClient httpClient;
-    private @Nullable String userName = "";
-    private @Nullable String password = "";
+    private String userName = "";
+    private String password = "";
+    private String vid = "";
+    private String vsAccess = "";
+    private String vsStepup = "";
 
     public VerisureSession(HttpClient httpClient) {
         this.httpClient = httpClient;
     }
 
-    public boolean initialize(@Nullable String authstring, @Nullable String pinCode, @Nullable String userName) {
+    public boolean initialize(@Nullable String authstring, @Nullable String pinCode, String userName, String password) {
         if (authstring != null) {
             this.authstring = authstring.substring(0);
             this.pinCode = pinCode;
             this.userName = userName;
+            this.password = password;
             // Try to login to Verisure
             if (logIn()) {
                 return getInstallations();
@@ -119,12 +128,9 @@ public class VerisureSession {
             if (logIn()) {
                 if (updateStatus()) {
                     return true;
-                } else {
-                    return false;
                 }
-            } else {
-                return false;
             }
+            return false;
         } catch (HttpResponseException e) {
             logger.warn("Failed to do a refresh {}", e.getMessage());
             return false;
@@ -258,15 +264,21 @@ public class VerisureSession {
         }
     }
 
-    private void setPasswordFromCookie() {
+    private void analyzeCookies() {
         CookieStore c = httpClient.getCookieStore();
         List<HttpCookie> cookies = c.getCookies();
         final List<HttpCookie> unmodifiableList = List.of(cookies.toArray(new HttpCookie[] {}));
         unmodifiableList.forEach(cookie -> {
             logger.trace("Response Cookie: {}", cookie);
-            if (cookie.getName().equals(PASSWORD_NAME)) {
-                password = cookie.getValue();
-                logger.debug("Fetching vid {} from cookie", password);
+            if (VID.equals(cookie.getName())) {
+                vid = cookie.getValue();
+                logger.debug("Fetching vid {} from cookie", vid);
+            } else if (VS_ACCESS.equals(cookie.getName())) {
+                vsAccess = cookie.getValue();
+                logger.debug("Fetching vs-access {} from cookie", vsAccess);
+            } else if (VS_STEPUP.equals(cookie.getName())) {
+                vsStepup = cookie.getValue();
+                logger.debug("Fetching vs-stepup {} from cookie", vsStepup);
             }
         });
     }
@@ -290,7 +302,6 @@ public class VerisureSession {
         switch (response.getStatus()) {
             case HttpStatus.OK_200:
                 if (content.contains("<link href=\"/newapp")) {
-                    setPasswordFromCookie();
                     return true;
                 } else {
                     logger.debug("We need to login again!");
@@ -313,9 +324,9 @@ public class VerisureSession {
 
     private <T> @Nullable T getJSONVerisureAPI(String url, Class<T> jsonClass)
             throws ExecutionException, InterruptedException, TimeoutException, JsonSyntaxException {
-        logger.debug("HTTP GET: {}", BASEURL + url);
+        logger.debug("HTTP GET: {}", BASE_URL + url);
 
-        ContentResponse response = httpClient.GET(BASEURL + url + "?_=" + System.currentTimeMillis());
+        ContentResponse response = httpClient.GET(BASE_URL + url + "?_=" + System.currentTimeMillis());
         String content = response.getContentAsString();
         logTraceWithPattern(response.getStatus(), content);
 
@@ -325,6 +336,7 @@ public class VerisureSession {
     private ContentResponse postVerisureAPI(String url, String data, boolean isJSON)
             throws ExecutionException, InterruptedException, TimeoutException {
         logger.debug("postVerisureAPI URL: {} Data:{}", url, data);
+
         Request request = httpClient.newRequest(url).method(HttpMethod.POST);
         if (isJSON) {
             request.header("content-type", "application/json");
@@ -334,14 +346,29 @@ public class VerisureSession {
             }
         }
         request.header("Accept", "application/json");
-        if (!data.equals("empty")) {
+
+        if (url.contains(AUTH_LOGIN)) {
+            request.header("APPLICATION_ID", "OpenHAB Verisure");
+            String basicAuhentication = Base64.getEncoder().encodeToString((userName + ":" + password).getBytes());
+            request.header("authorization", "Basic " + basicAuhentication);
+        } else {
+            if (!vid.isEmpty()) {
+                request.cookie(new HttpCookie(VID, vid));
+                logger.debug("Setting cookie with vid {}", vid);
+            }
+            if (!vsAccess.isEmpty()) {
+                request.cookie(new HttpCookie(VS_ACCESS, vsAccess));
+                logger.debug("Setting cookie with vs-access {}", vsAccess);
+            }
+            logger.debug("Setting cookie with username {}", userName);
+            request.cookie(new HttpCookie(USER_NAME, userName));
+        }
+
+        if (!"empty".equals(data)) {
             request.content(new BytesContentProvider(data.getBytes(StandardCharsets.UTF_8)),
                     "application/x-www-form-urlencoded; charset=UTF-8");
-        } else {
-            logger.debug("Setting cookie with username {} and vid {}", userName, password);
-            request.cookie(new HttpCookie(USER_NAME, userName));
-            request.cookie(new HttpCookie(PASSWORD_NAME, password));
         }
+
         logger.debug("HTTP POST Request {}.", request.toString());
         return request.send();
     }
@@ -400,6 +427,9 @@ public class VerisureSession {
                         logTraceWithPattern(httpStatus, content);
                         return httpStatus;
                     }
+                } else if (httpStatus == HttpStatus.BAD_REQUEST_400) {
+                    setApiServerInUse(getNextApiServer());
+                    url = apiServerInUse + urlString;
                 } else {
                     logger.debug("Failed to send POST, Http status code: {}", response.getStatus());
                 }
@@ -417,7 +447,11 @@ public class VerisureSession {
         logTraceWithPattern(response.getStatus(), response.getContentAsString());
 
         url = AUTH_LOGIN;
-        return postVerisureAPI(url, "empty");
+        int httpStatusCode = postVerisureAPI(url, "empty");
+        analyzeCookies();
+
+        // return response.getStatus();
+        return httpStatusCode;
     }
 
     private boolean getInstallations() {
@@ -488,10 +522,26 @@ public class VerisureSession {
     private synchronized boolean logIn() {
         try {
             if (!areWeLoggedIn()) {
-                logger.debug("Attempting to log in to mypages.verisure.com");
-                String url = LOGON_SUF;
+                vid = "";
+                vsAccess = "";
+                logger.debug("Attempting to log in to {}, remove all cookies to ensure a fresh session", BASE_URL);
+                URI authUri = new URI(BASE_URL);
+                CookieStore store = httpClient.getCookieStore();
+                store.get(authUri).forEach(cookie -> {
+                    store.remove(authUri, cookie);
+                });
+
+                String url = AUTH_LOGIN;
+                int httpStatusCode = postVerisureAPI(url, "empty");
+                analyzeCookies();
+                if (!vsStepup.isEmpty()) {
+                    logger.warn("MFA is activated on this user! Not supported by binding!");
+                    return false;
+                }
+
+                url = LOGON_SUF;
                 logger.debug("Login URL: {}", url);
-                int httpStatusCode = postVerisureAPI(url, authstring);
+                httpStatusCode = postVerisureAPI(url, authstring);
                 if (httpStatusCode != HttpStatus.OK_200) {
                     logger.debug("Failed to login, HTTP status code: {}", httpStatusCode);
                     return false;
@@ -500,7 +550,7 @@ public class VerisureSession {
             } else {
                 return true;
             }
-        } catch (ExecutionException | InterruptedException | TimeoutException e) {
+        } catch (ExecutionException | InterruptedException | TimeoutException | URISyntaxException e) {
             logger.warn("Failed to login {}", e.getMessage());
         }
         return false;
@@ -617,16 +667,17 @@ public class VerisureSession {
                     // Set location
                     slThing.setLocation(doorLock.getDevice().getArea());
                     slThing.setDeviceId(deviceId);
+
                     // Fetch more info from old endpoint
                     try {
                         VerisureSmartLockDTO smartLockThing = getJSONVerisureAPI(SMARTLOCK_PATH + slThing.getDeviceId(),
                                 VerisureSmartLockDTO.class);
                         logger.debug("REST Response ({})", smartLockThing);
                         slThing.setSmartLockJSON(smartLockThing);
-                        notifyListenersIfChanged(slThing, installation, deviceId);
                     } catch (ExecutionException | InterruptedException | TimeoutException | JsonSyntaxException e) {
                         logger.warn("Failed to query for smartlock status: {}", e.getMessage());
                     }
+                    notifyListenersIfChanged(slThing, installation, deviceId);
                 }
             });
 
@@ -740,7 +791,7 @@ public class VerisureSession {
                                 cThing.setBatteryStatus(batteryStatus);
                             }
                         } catch (ExecutionException | InterruptedException | TimeoutException | JsonSyntaxException e) {
-                            logger.warn("Failed to query for smartlock status: {}", e.getMessage());
+                            logger.debug("Failed to query for battery status: {}", e.getMessage());
                         }
                         // Set location
                         cThing.setLocation(climate.getDevice().getArea());
@@ -789,7 +840,7 @@ public class VerisureSession {
                             dThing.setBatteryStatus(batteryStatus);
                         }
                     } catch (ExecutionException | InterruptedException | TimeoutException | JsonSyntaxException e) {
-                        logger.warn("Failed to query for smartlock status: {}", e.getMessage());
+                        logger.warn("Failed to query for door&window status: {}", e.getMessage());
                     }
                     // Set location
                     dThing.setLocation(doorWindow.getDevice().getArea());
@@ -847,7 +898,7 @@ public class VerisureSession {
                     .getUserTrackings();
             userTrackingList.forEach(userTracking -> {
                 String localUserTrackingStatus = userTracking.getStatus();
-                if (localUserTrackingStatus != null && localUserTrackingStatus.equals("ACTIVE")) {
+                if ("ACTIVE".equals(localUserTrackingStatus)) {
                     VerisureUserPresencesDTO upThing = new VerisureUserPresencesDTO();
                     VerisureUserPresencesDTO.Installation inst = new VerisureUserPresencesDTO.Installation();
                     inst.setUserTrackings(Collections.singletonList(userTracking));
