894ae20f50039a400f0eae09ed30d901606209e2
Matthew Skinner <matt@pcmus.com>
2021-10-24 20:36:20 +1100

    [ipcamera] Improvements and fix 503 errors go to offline with Hik (#11419)
    * Stop hik logging 401 with digest.
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * Improve and fix generic cams
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * Stop dahua IntelliFrame logging
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * Catch IllegalStateException
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * Trial reusing channels.
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * Tidy up
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * cleanup 2
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * Cleanup 3
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * Disable checking connection with event stream.
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * Bug fix
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * more cleanup
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * more cleanup
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * Reduce logging to only whats needed.
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * fix offline detection.
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * fixes to ipcamera.mjpeg
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    
    * reverse some connection checks
    
    Signed-off-by: Matthew Skinner <matt@pcmus.com>
    Signed-off-by: Dave J Schoepel <dave@theschoepels.com>

--- bundles/org.openhab.binding.ipcamera/src/main/java/org/openhab/binding/ipcamera/internal/ChannelTracking.java
+++ bundles/org.openhab.binding.ipcamera/src/main/java/org/openhab/binding/ipcamera/internal/ChannelTracking.java
@@ -15,6 +15,7 @@ package org.openhab.binding.ipcamera.internal;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 
 /**
  * The {@link ChannelTracking} Can be used to find the handle for a HTTP channel if you know the URL. The reply can
@@ -41,6 +42,15 @@ public class ChannelTracking {
 
     public Channel getChannel() {
         return channel;
+    }
+
+    /**
+     * Closes the channel, but keeps the HTTP reply stored in the tracker.
+     *
+     * @return ChannelFuture
+     */
+    public ChannelFuture closeChannel() {
+        return channel.close();
     }
 
     public String getReply() {
