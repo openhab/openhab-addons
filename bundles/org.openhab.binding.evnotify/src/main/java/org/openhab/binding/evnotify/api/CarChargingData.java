144867ba5c607e8b41526ca7f5bf8dc986c104fd
Michael Schmidt <mi.schmidt.83@gmail.com>
2021-10-31 09:32:48 +0100

    feat: add inteface for data of evnotify online service
    Signed-off-by: Michael Schmidt <mi.schmidt.83@gmail.com>

---
+++ bundles/org.openhab.binding.evnotify/src/main/java/org/openhab/binding/evnotify/api/CarChargingData.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.evnotify.api;
+
+import java.time.OffsetDateTime;
+
+/**
+ * Interface for the data from the API of the evnotify online service.
+ *
+ * @author Michael Schmidt - Initial contribution
+ */
+public interface CarChargingData {
+
+    Boolean isCharging();
+
+    Boolean isRapidChargePort();
+
+    Boolean isNormalChargePort();
+
+    Boolean isSlowChargePort();
+
+    Float getStateOfHealth();
+
+    Float getAuxBatteryVoltage();
+
+    Float getDcBatteryVoltage();
+
+    Float getDcBatteryCurrent();
+
+    Float getDcBatteryPower();
+
+    Float getCumulativeEnergyCharged();
+
+    Float getCumulativeEnergyDischarged();
+
+    Float getBatteryMinTemperature();
+
+    Float getBatteryMaxTemperature();
+
+    Float getBatteryInletTemperature();
+
+    Float getExternalTemperature();
+
+    OffsetDateTime getLastExtended();
+}
