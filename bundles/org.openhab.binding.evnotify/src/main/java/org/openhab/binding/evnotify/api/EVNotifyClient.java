b99a715a91fb751df42073e7378c675f908c230e
Michael Schmidt <mi.schmidt.83@gmail.com>
2021-10-31 09:33:12 +0100

    feat: add inteface for client for evnotify online service
    Signed-off-by: Michael Schmidt <mi.schmidt.83@gmail.com>

---
+++ bundles/org.openhab.binding.evnotify/src/main/java/org/openhab/binding/evnotify/api/EVNotifyClient.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.evnotify.api;
+
+import java.io.IOException;
+
+/**
+ * Interface for the client to get data from the API of the evnotify online service.
+ *
+ * @author Michael Schmidt - Initial contribution
+ */
+public interface EVNotifyClient {
+
+    /**
+     * returns a {@link CarChargingData}
+     *
+     * @return state of a car
+     */
+    CarChargingData getCarChargingData() throws IOException, InterruptedException;
+}
