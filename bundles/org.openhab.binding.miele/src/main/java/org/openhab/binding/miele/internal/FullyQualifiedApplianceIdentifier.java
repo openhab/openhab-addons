0248db4a17c4a408a5e98315c57ca335508f417f
jlaur <jacob-github@vindvejr.dk>
2021-11-01 18:44:19 +0100

    [miele] Clean up properties and improve reliability and performance (#11423)
    * Use appliance cache for getting full UID with protocol prefix instead of relying on property.
    * Set bare protocol name as property.
    * Fix potential null pointer access warnings.
    * Remove unused import.
    * Renamed property protocol to protocolAdapter for correctness.
    * Add connectionType property.
    * Add appliance model property.
    * Remove useless properties brandId and companyId always having value MI.
    * Rename property dc to deviceClass and set it consistently (not only from auto-discovered things).
    * Added constants for remaining handlers with hardcoded device classes.
    * Fix SCA: AuthorContributionDescriptionCheck
    * Fix SCA: ModifierOrderCheck
    * Rename ExtendedDeviceStateUtil to be a bit more generic.
    * Extract device class string parsing to utility method.
    * Fix SCA: ForbiddenPackageUsageCheck
    * Fix redundant null check.
    * Fix potential null pointer access warnings.
    * Fix unsafe null type conversion (type annotations)
    * Share same configuration (UID) for all appliance types.
    * Refer to gateway instead of ZigBee network in configuration.
    * Remove dependency to seriaNumber property for multicast channel updates.
    * Simplified filtering of irrelevant device class.
    * Remove devices from remoteUid cache also when disappearing from gateway, although this is a quite rare scenario.
    * Add default i18n properties file.
    * Add partial Danish translation.
    
    Fixes #11422
    
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>
    Signed-off-by: Dave J Schoepel <dave@theschoepels.com>

--- bundles/org.openhab.binding.miele/src/main/java/org/openhab/binding/miele/internal/FullyQualifiedApplianceIdentifier.java
+++ bundles/org.openhab.binding.miele/src/main/java/org/openhab/binding/miele/internal/FullyQualifiedApplianceIdentifier.java
@@ -16,7 +16,7 @@ package org.openhab.binding.miele.internal;
  * The {@link FullyQualifiedApplianceIdentifier} class represents a fully qualified appliance identifier.
  * Example: "hdm:ZigBee:0123456789abcdef#210"
  *
- * @author Jacob Laursen - Fixed multicast and protocol support (ZigBee/LAN)
+ * @author Jacob Laursen - Initial contribution
  */
 public class FullyQualifiedApplianceIdentifier {
     private String uid;
