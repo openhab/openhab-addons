661a9d734bcef1129b6b521c4e0c8a49807f748d
Michael Weger <weger.michael@gmx.net>
2021-11-05 18:10:30 +0100

    inital commit of skeleton
    Signed-off-by: Michael Weger <weger.michael@gmx.net>

---
+++ bundles/org.openhab.binding.guntamatic/src/main/java/org/openhab/binding/guntamatic/internal/GuntamaticConfiguration.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.guntamatic.internal;
+
+/**
+ * The {@link GuntamaticConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Weger Michael - Initial contribution
+ */
+public class GuntamaticConfiguration {
+
+    /**
+     * Sample configuration parameters. Replace with your own.
+     */
+    public String hostname;
+    public String password;
+    public int refreshInterval;
+}
