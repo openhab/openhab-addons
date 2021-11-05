0d4a56763fa68536eecf1b5e7083992e319a5288
lolodomo <lg.hc@free.fr>
2021-11-05 12:10:10 +0100

    [weathercompany] Fix internationalization of discovery service (#11499)
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/org.openhab.binding.weathercompany/src/main/java/org/openhab/binding/weathercompany/internal/WeatherCompanyHandlerFactory.java
+++ bundles/org.openhab.binding.weathercompany/src/main/java/org/openhab/binding/weathercompany/internal/WeatherCompanyHandlerFactory.java
@@ -29,6 +29,7 @@ import org.openhab.core.config.discovery.DiscoveryService;
 import org.openhab.core.i18n.LocaleProvider;
 import org.openhab.core.i18n.LocationProvider;
 import org.openhab.core.i18n.TimeZoneProvider;
+import org.openhab.core.i18n.TranslationProvider;
 import org.openhab.core.i18n.UnitProvider;
 import org.openhab.core.io.net.http.HttpClientFactory;
 import org.openhab.core.thing.Bridge;
@@ -58,16 +59,19 @@ public class WeatherCompanyHandlerFactory extends BaseThingHandlerFactory {
     private HttpClient httpClient;
     private LocationProvider locationProvider;
     private LocaleProvider localeProvider;
+    private TranslationProvider i18nProvider;
 
     @Activate
     public WeatherCompanyHandlerFactory(@Reference TimeZoneProvider timeZoneProvider,
             @Reference UnitProvider unitProvider, @Reference HttpClientFactory httpClientFactory,
-            @Reference LocationProvider locationProvider, @Reference LocaleProvider localeProvider) {
+            @Reference LocationProvider locationProvider, @Reference LocaleProvider localeProvider,
+            @Reference TranslationProvider i18nProvider) {
         this.timeZoneProvider = timeZoneProvider;
         this.unitProvider = unitProvider;
         this.httpClient = httpClientFactory.getCommonHttpClient();
         this.locationProvider = locationProvider;
         this.localeProvider = localeProvider;
+        this.i18nProvider = i18nProvider;
     }
 
     @Override
@@ -103,7 +107,7 @@ public class WeatherCompanyHandlerFactory extends BaseThingHandlerFactory {
 
     private synchronized void registerDeviceDiscoveryService(WeatherCompanyBridgeHandler bridgeHandler) {
         WeatherCompanyDiscoveryService discoveryService = new WeatherCompanyDiscoveryService(bridgeHandler,
-                locationProvider, localeProvider);
+                locationProvider, localeProvider, i18nProvider);
         discoveryService.activate(null);
         this.discoveryServiceRegs.put(bridgeHandler.getThing().getUID(),
                 bundleContext.registerService(DiscoveryService.class.getName(), discoveryService, new Hashtable<>()));
