ae794a233ba41ad8c9a1fc65931e9bc2089d3bfe
Gaël L'hopital <gael@lhopital.org>
2021-11-20 18:48:03 +0100

    [XMLTV] Preparing for Crowdin and code refining. (#11594)
    * Preparing for Crowdin and code refining.
    
    Signed-off-by: Gaël L'hopital <gael@lhopital.org>
    
    * Satisfying SAT
    
    Signed-off-by: Gaël L'hopital <gael@lhopital.org>
    
    * Preventing two potential NPE
    
    Signed-off-by: Gaël L'hopital <gael@lhopital.org>
    
    * Code review comments taken in account
    
    Signed-off-by: clinique <gael@lhopital.org>
    
    * Reverting description removal
    
    Signed-off-by: clinique <gael@lhopital.org>
    
    * Forgot spotless apply
    
    Signed-off-by: clinique <gael@lhopital.org>

--- bundles/org.openhab.binding.xmltv/src/main/java/org/openhab/binding/xmltv/internal/configuration/XmlTVConfiguration.java
+++ bundles/org.openhab.binding.xmltv/src/main/java/org/openhab/binding/xmltv/internal/configuration/XmlTVConfiguration.java
@@ -12,14 +12,17 @@
  */
 package org.openhab.binding.xmltv.internal.configuration;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
 /**
  * The {@link XmlTVConfiguration} class contains fields mapping TV bridge
  * configuration parameters.
  *
  * @author Gaël L'hopital - Initial contribution
  */
+@NonNullByDefault
 public class XmlTVConfiguration {
-    public String filePath;
-    public Integer refresh;
-    public String encoding;
+    public String filePath = "";
+    public int refresh = 24;
+    public String encoding = "UTF8";
 }
