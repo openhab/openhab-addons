fcf29ad37661dd5c784f1e53383afcb0aaf31430
Jacob Laursen <jacob-github@vindvejr.dk>
2021-11-21 12:12:58 +0100

    Remove temporary gateway fallback logic.
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>

--- bundles/org.openhab.binding.miele/src/main/java/org/openhab/binding/miele/internal/MieleTranslationProvider.java
+++ bundles/org.openhab.binding.miele/src/main/java/org/openhab/binding/miele/internal/MieleTranslationProvider.java
@@ -34,14 +34,12 @@ public class MieleTranslationProvider {
     private final LocaleProvider localeProvider;
     @Nullable
     private final Locale locale;
-    private final Locale englishLocale;
 
     public MieleTranslationProvider(TranslationProvider i18nProvider, LocaleProvider localeProvider) {
         this.bundle = FrameworkUtil.getBundle(this.getClass());
         this.i18nProvider = i18nProvider;
         this.localeProvider = localeProvider;
         this.locale = null;
-        this.englishLocale = new Locale.Builder().setLanguageTag("en").build();
     }
 
     public MieleTranslationProvider(TranslationProvider i18nProvider, LocaleProvider localeProvider, Locale locale) {
@@ -49,7 +47,6 @@ public class MieleTranslationProvider {
         this.i18nProvider = i18nProvider;
         this.localeProvider = localeProvider;
         this.locale = locale;
-        this.englishLocale = new Locale.Builder().setLanguageTag("en").build();
     }
 
     public String getText(String key, String defaultText, @Nullable Object... arguments) {
@@ -57,17 +54,6 @@ public class MieleTranslationProvider {
                 locale != null ? locale : localeProvider.getLocale(), arguments);
         if (text == null) {
             return defaultText;
-        }
-        if (!englishLocale.equals(locale)) {
-            /**
-             * FIXME: When texts have been harvested for all languages supported by the XGW 3000 gateway,
-             * this logic should be removed. Or if it would be possible to check existence of a text
-             * in a given language, this could be used for fallback to gateway langauge, then English.
-             */
-            String englishText = i18nProvider.getText(bundle, key, defaultText, englishLocale, arguments);
-            if (text.equals(englishText)) {
-                return defaultText;
-            }
         }
         return text;
     }
