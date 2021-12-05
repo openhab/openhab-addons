b26d6544677f04fda5b2db45cf30968c4318ce06
Mark Hilbush <mark@hilbush.com>
2021-12-05 05:20:20 -0500

    [sleepiq] Use constructor injection for ClientBuilder (#11700)
    Fixes #11696
    
    Signed-off-by: Mark Hilbush <mark@hilbush.com>

--- bundles/org.openhab.binding.sleepiq/src/3rdparty/java/org/openhab/binding/sleepiq/api/model/TimeSince.java
+++ bundles/org.openhab.binding.sleepiq/src/3rdparty/java/org/openhab/binding/sleepiq/api/model/TimeSince.java
@@ -16,46 +16,37 @@
 package org.openhab.binding.sleepiq.api.model;
 
 import java.time.Duration;
-import java.time.format.DateTimeParseException;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class TimeSince
-{
+public class TimeSince {
     private static final Pattern PATTERN = Pattern.compile("(([0-9]+) d )?([0-9]{2}):([0-9]{2}):([0-9]{2})",
-                                                           Pattern.CASE_INSENSITIVE);
+            Pattern.CASE_INSENSITIVE);
 
     private Duration duration;
 
-    public Duration getDuration()
-    {
+    public Duration getDuration() {
         return duration;
     }
 
-    public void setDuration(Duration duration)
-    {
+    public void setDuration(Duration duration) {
         this.duration = duration == null ? null : duration.abs();
     }
 
-    public TimeSince withDuration(long days, long hours, long minutes, long seconds)
-    {
-        return withDuration(Duration.ofSeconds(TimeUnit.DAYS.toSeconds(days)
-                                               + TimeUnit.HOURS.toSeconds(hours)
-                                               + TimeUnit.MINUTES.toSeconds(minutes)
-                                               + seconds));
+    public TimeSince withDuration(long days, long hours, long minutes, long seconds) {
+        return withDuration(Duration.ofSeconds(TimeUnit.DAYS.toSeconds(days) + TimeUnit.HOURS.toSeconds(hours)
+                + TimeUnit.MINUTES.toSeconds(minutes) + seconds));
     }
 
-    public TimeSince withDuration(Duration duration)
-    {
+    public TimeSince withDuration(Duration duration) {
         setDuration(duration);
         return this;
     }
 
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((duration == null) ? 0 : duration.hashCode());
@@ -63,38 +54,29 @@ public class TimeSince
     }
 
     @Override
-    public boolean equals(Object obj)
-    {
-        if (this == obj)
-        {
+    public boolean equals(Object obj) {
+        if (this == obj) {
             return true;
         }
-        if (obj == null)
-        {
+        if (obj == null) {
             return false;
         }
-        if (!(obj instanceof TimeSince))
-        {
+        if (!(obj instanceof TimeSince)) {
             return false;
         }
-        TimeSince other = (TimeSince)obj;
-        if (duration == null)
-        {
-            if (other.duration != null)
-            {
+        TimeSince other = (TimeSince) obj;
+        if (duration == null) {
+            if (other.duration != null) {
                 return false;
             }
-        }
-        else if (!duration.equals(other.duration))
-        {
+        } else if (!duration.equals(other.duration)) {
             return false;
         }
         return true;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         long totalDays = duration.toDays();
         long totalHours = duration.toHours();
         long totalMinutes = duration.toMinutes();
@@ -104,22 +86,19 @@ public class TimeSince
         long minutes = totalMinutes - TimeUnit.HOURS.toMinutes(totalHours);
         long seconds = totalSeconds - TimeUnit.MINUTES.toSeconds(totalMinutes);
 
-        if (totalDays > 0)
-        {
+        if (totalDays > 0) {
             return String.format("%d d %02d:%02d:%02d", totalDays, hours, minutes, seconds);
         }
 
         return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
 
-    public static TimeSince parse(CharSequence text)
-    {
+    public static TimeSince parse(CharSequence text) {
         Objects.requireNonNull(text, "text");
 
         Matcher matcher = PATTERN.matcher(text);
-        if (!matcher.matches())
-        {
-            throw new DateTimeParseException("Text cannot be parsed", text, 0);
+        if (!matcher.matches()) {
+            return new TimeSince().withDuration(Duration.ZERO);
         }
 
         String dayMatch = matcher.group(2);
@@ -128,17 +107,10 @@ public class TimeSince
         String secondMatch = matcher.group(5);
 
         StringBuilder sb = new StringBuilder("P");
-        if (dayMatch != null)
-        {
+        if (dayMatch != null) {
             sb.append(dayMatch).append('D');
         }
-        sb.append('T')
-          .append(hourMatch)
-          .append('H')
-          .append(minuteMatch)
-          .append('M')
-          .append(secondMatch)
-          .append('S');
+        sb.append('T').append(hourMatch).append('H').append(minuteMatch).append('M').append(secondMatch).append('S');
 
         return new TimeSince().withDuration(Duration.parse(sb.toString()));
     }
