585b8c9ee4649bc0fd1dbb744fb2aee48ff8d4d2
Sascha Volkenandt <sascha@akv-soft.de>
2021-12-15 08:35:38 +0100

    [wled] add configuration to sort state options of channels effects and palettes
    Signed-off-by: Sascha Volkenandt <sascha@akv-soft.de>

--- bundles/org.openhab.binding.wled/src/main/java/org/openhab/binding/wled/internal/api/WledApiV084.java
+++ bundles/org.openhab.binding.wled/src/main/java/org/openhab/binding/wled/internal/api/WledApiV084.java
@@ -17,6 +17,7 @@ import static org.openhab.binding.wled.internal.WLedBindingConstants.*;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -230,6 +231,9 @@ public class WledApiV084 implements WledApi {
         for (String value : state.jsonResponse.effects) {
             fxOptions.add(new StateOption(Integer.toString(counter++), value));
         }
+        if (handler.config.sortEffects) {
+            fxOptions.sort(Comparator.comparing(o -> o.getValue().equals("0") ? "" : o.getLabel()));
+        }
         handler.stateDescriptionProvider.setStateOptions(new ChannelUID(handler.getThing().getUID(), CHANNEL_FX),
                 fxOptions);
     }
@@ -239,6 +243,9 @@ public class WledApiV084 implements WledApi {
         int counter = 0;
         for (String value : state.jsonResponse.palettes) {
             palleteOptions.add(new StateOption(Integer.toString(counter++), value));
+        }
+        if (handler.config.sortPalettes) {
+            palleteOptions.sort(Comparator.comparing(o -> o.getValue().equals("0") ? "" : o.getLabel()));
         }
         handler.stateDescriptionProvider.setStateOptions(new ChannelUID(handler.getThing().getUID(), CHANNEL_PALETTES),
                 palleteOptions);
