bd5cada2855a7ee3ddcc1d6d9247bf5115686f16
Christoph Weitkamp <github@christophweitkamp.de>
2022-01-04 21:26:52 +0100

    Do not create table on 'query', only on 'store' (#11961)
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>
    Signed-off-by: basse04 <orjan.backsell@google.com>

--- bundles/org.openhab.persistence.jdbc/src/main/java/org/openhab/persistence/jdbc/internal/JdbcPersistenceService.java
+++ bundles/org.openhab.persistence.jdbc/src/main/java/org/openhab/persistence/jdbc/internal/JdbcPersistenceService.java
@@ -193,19 +193,16 @@ public class JdbcPersistenceService extends JdbcMapper implements QueryablePersi
 
         String table = sqlTables.get(itemName);
         if (table == null) {
-            logger.warn(
-                    "JDBC::query: unable to find table for query, no data in database for item '{}'. Current number of tables in the database: {}",
-                    itemName, sqlTables.size());
-            // if enabled, table will be created immediately
-            logger.warn("JDBC::query: try to generate the table for item '{}'", itemName);
-            table = getTable(item);
+            logger.debug("JDBC::query: unable to find table for item with name: '{}', no data in database.", itemName);
+            return List.of();
         }
 
         long timerStart = System.currentTimeMillis();
         List<HistoricItem> items = getHistItemFilterQuery(filter, conf.getNumberDecimalcount(), table, item);
-
-        logger.debug("JDBC::query: query for {} returned {} rows in {} ms", itemName, items.size(),
-                System.currentTimeMillis() - timerStart);
+        if (logger.isDebugEnabled()) {
+            logger.debug("JDBC: Query for item '{}' returned {} rows in {} ms", itemName, items.size(),
+                    System.currentTimeMillis() - timerStart);
+        }
 
         // Success
         errCnt = 0;
