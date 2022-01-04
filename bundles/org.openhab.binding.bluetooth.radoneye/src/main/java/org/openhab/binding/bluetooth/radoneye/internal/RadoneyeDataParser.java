d87fcfc2841ef54408782ba10bfc74817f0d5448
Peter Obel <peter@ecomerc.com>
2022-01-04 12:34:22 +0100

    Almost there
    Signed-off-by: Peter Obel <peter@ecomerc.com>

---
+++ bundles/org.openhab.binding.bluetooth.radoneye/src/main/java/org/openhab/binding/bluetooth/radoneye/internal/RadoneyeDataParser.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.bluetooth.radoneye.internal;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link RadoneyeDataParser} is responsible for parsing data from Wave Plus device format.
+ *
+ * @author Pauli Anttila - Initial contribution
+ * @author Kai Kreuzer - Added Radoneye Wave Mini support
+ */
+@NonNullByDefault
+public class RadoneyeDataParser {
+    public static final String RADON = "radon";
+
+    private static final int EXPECTED_DATA_LEN = 20;
+    private static final int EXPECTED_VER_PLUS = 1;
+
+    private final Logger logger = LoggerFactory.getLogger(RadoneyeHandler.class);
+
+    private RadoneyeDataParser() {
+    }
+
+    public static Map<String, Number> parseRd200Data(int[] data) throws RadoneyeParserException {
+        logger.debug("Parsed data length: {}", data.length);
+        logger.debug("Parsed data: {}", data);
+        //if (data.length == EXPECTED_DATA_LEN) {
+            final Map<String, Number> result = new HashMap<>();
+
+            int[] radonArray = subArray(data, 2, 6);
+            result.put(RADON, 
+                    new BigDecimal(fromByteArrayLE(radonArray) * 37));
+            return result;
+        //} else {
+        //    throw new RadoneyeParserException(String.format("Illegal data structure length '%d'", data.length));
+        //}
+    }
+
+    private static int intFromBytes(int lowByte, int highByte) {
+        return (highByte & 0xFF) << 8 | (lowByte & 0xFF);
+    }
+
+    //Little endian
+    private static int fromByteArrayLE(int[] bytes) {
+        int result = 0;
+        for (int i = 0; i < bytes.length; i++) {
+            if (i == 0) {
+                result |= bytes[i] << (8 * i);
+            } else {
+                result |= bytes[i] << (8 * i);
+            }
+        }
+        return result;
+    }
+
+    // Generic method to get subarray of a non-primitive array
+    // between specified indices
+    private static<T> T[] subArray(T[] array, int beg, int end) {
+        return Arrays.copyOfRange(array, beg, end + 1);
+    }
+}
---
+++ bundles/org.openhab.binding.bluetooth.radoneye/src/main/java/org/openhab/binding/bluetooth/radoneye/internal/RadoneyeHandler.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.bluetooth.radoneye.internal;
+
+import static org.openhab.binding.bluetooth.radoneye.internal.RadoneyeBindingConstants.*;
+
+import java.util.Map;
+import java.util.UUID;
+
+import javax.measure.quantity.Dimensionless;
+import javax.measure.quantity.Temperature;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.library.types.QuantityType;
+import org.openhab.core.library.unit.SIUnits;
+import org.openhab.core.library.unit.Units;
+import org.openhab.core.thing.Thing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link RadoneyeHandler} is responsible for handling commands, which are
+ * sent to one of the channels.
+ *
+ * @author Kai Kreuzer - Initial contribution
+ */
+@NonNullByDefault
+public class RadoneyeHandler extends AbstractRadoneyeHandler {
+
+    private static final String SERVICE_UUID = "00001523-1212-efde-1523-785feabcd123";
+    private static final String TRIGGER_UID = "00001524-1212-efde-1523-785feabcd123";
+    private static final String DATA_UUID = "00001525-1212-efde-1523-785feabcd123";
+
+    public RadoneyeHandler(Thing thing) {
+        super(thing);
+    }
+
+    private final Logger logger = LoggerFactory.getLogger(RadoneyeHandler.class);
+
+    private final UUID dataUuid = UUID.fromString(DATA_UUID);
+    private final UUID triggerUuid = UUID.fromString(TRIGGER_UID);
+    private final byte[] triggerData = new byte[] { 0x50 };
+
+    @Override
+    protected void updateChannels(int[] is) {
+        Map<String, Number> data;
+        try {
+            data = RadoneyeDataParser.parseRd200Data(is);
+            logger.debug("Parsed data: {}", data);
+            Number radon = data.get(RadoneyeDataParser.RADON);
+            if (radon != null) {
+                updateState(CHANNEL_ID_RADON, new QuantityType<Dimensionless>(radon, Units.BECQUEREL_PER_CUBIC_METRE));
+            }
+        } catch (RadoneyeParserException e) {
+            logger.error("Failed to parse data received from Radoneye sensor: {}", e.getMessage());
+        }
+    }
+
+    @Override
+    protected UUID getDataUUID() {
+        return dataUuid;
+    }
+
+    @Override
+    protected UUID getTriggerUUID() {
+        return triggerUuid;
+    }
+
+    @Override
+    protected byte[] getTriggerData() {
+        return triggerData;
+    }
+}
