90c78fec568bfa6ff935a2ee2abb9e1452b0ba18
Jacob Laursen <jacob-github@vindvejr.dk>
2022-01-07 10:33:52 +0100

    [hdpowerview] Add firmware information properties for hub and shade. (#11980)
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>
    Signed-off-by: basse04 <orjan.backsell@google.com>

--- bundles/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/internal/api/responses/Shades.java
+++ bundles/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/internal/api/responses/Shades.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.hdpowerview.internal.api.Firmware;
 import org.openhab.binding.hdpowerview.internal.api.ShadePosition;
 
 /**
@@ -52,6 +53,8 @@ public class Shades {
         public @Nullable Boolean timedOut;
         public int signalStrength;
         public @Nullable Integer capabilities;
+        public @Nullable Firmware firmware;
+        public @Nullable Firmware motor;
 
         public String getName() {
             return new String(Base64.getDecoder().decode(name));
