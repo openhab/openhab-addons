894d7b264af1f87196300ec679bbae0677f4fcea
chingon007 <tron81@gmx.de>
2022-01-07 14:24:23 +0100

    Changing header to match 2022
    Signed-off-by: chingon007 <tron81@gmx.de>

--- bundles/org.openhab.binding.sonnen/src/main/java/org/openhab/binding/sonnen/internal/SonnenHandlerFactory.java
+++ bundles/org.openhab.binding.sonnen/src/main/java/org/openhab/binding/sonnen/internal/SonnenHandlerFactory.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2021 Contributors to the openHAB project
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -12,7 +12,7 @@
  */
 package org.openhab.binding.sonnen.internal;
 
-import static org.openhab.binding.sonnen.internal.SonnenBindingConstants.*;
+import static org.openhab.binding.sonnen.internal.SonnenBindingConstants.THING_TYPE_BATTERY;
 
 import java.util.Set;
 
