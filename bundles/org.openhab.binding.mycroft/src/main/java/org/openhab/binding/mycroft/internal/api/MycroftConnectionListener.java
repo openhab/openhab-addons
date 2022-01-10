772376f92b05fa43185b434db69a7a88fc1e326d
dalgwen <dalgwen@users.noreply.github.com>
2022-01-10 16:51:07 +0100

    [mycroft] Initial contribution (#11040)
    This binding will connect to Mycroft A.I. in order to control it or react to event by listening on the message bus.
    
    Signed-off-by: Gwendal Roulleau <gwendal.roulleau@gmail.com>

---
+++ bundles/org.openhab.binding.mycroft/src/main/java/org/openhab/binding/mycroft/internal/api/MycroftConnectionListener.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.mycroft.internal.api;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Informs about the websocket connection.
+ *
+ * @author Gwendal Roulleau - Initial contribution
+ */
+@NonNullByDefault
+public interface MycroftConnectionListener {
+    /**
+     * Connection successfully established.
+     */
+    void connectionEstablished();
+
+    /**
+     * Connection lost. A reconnect timer has been started.
+     *
+     * @param reason A reason for the disconnection
+     */
+    void connectionLost(String reason);
+}
