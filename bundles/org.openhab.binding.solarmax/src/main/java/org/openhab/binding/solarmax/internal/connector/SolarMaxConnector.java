83788208720560c3ad450c3183d4cb64582a6c7e
Jamie Townsend <jamie_townsend@hotmail.com>
2022-01-10 08:50:26 +0100

    Updated copyright years
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>

--- bundles/org.openhab.binding.solarmax/src/main/java/org/openhab/binding/solarmax/internal/connector/SolarMaxConnector.java
+++ bundles/org.openhab.binding.solarmax/src/main/java/org/openhab/binding/solarmax/internal/connector/SolarMaxConnector.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2021 Contributors to the openHAB project
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -148,7 +148,7 @@ public class SolarMaxConnector {
     static String getCommandString(List<SolarMaxCommandKey> commandList) {
         String commandString = "";
         for (SolarMaxCommandKey command : commandList) {
-            if (!commandString.equals("")) {
+            if (!"".equals(commandString)) {
                 commandString = commandString + ";";
             }
             commandString = commandString + command.getCommandKey();
