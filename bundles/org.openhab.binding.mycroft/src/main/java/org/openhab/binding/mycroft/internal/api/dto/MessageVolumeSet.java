40b593281716a5d52a0c290e398f408751e5043b
dalgwen <dalgwen@users.noreply.github.com>
2022-01-10 16:51:07 +0100

    [mycroft] Initial contribution (#11040)
    This binding will connect to Mycroft A.I. in order to control it or react to event by listening on the message bus.
    
    Signed-off-by: Gwendal Roulleau <gwendal.roulleau@gmail.com>

---
+++ bundles/org.openhab.binding.mycroft/src/main/java/org/openhab/binding/mycroft/internal/api/dto/MessageVolumeSet.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.mycroft.internal.api.dto;
+
+import org.openhab.binding.mycroft.internal.api.MessageType;
+
+/**
+ * This message asks IN THEORY Mycroft to set the volume to an amount
+ * specified in the data payload.
+ * But it seems in fact to be a message to inform third party of a
+ * volume change
+ *
+ * @author Gwendal Roulleau - Initial contribution
+ */
+public class MessageVolumeSet extends BaseMessage {
+
+    public Data data = new Data();
+
+    public MessageVolumeSet() {
+        this.type = MessageType.mycroft_volume_set;
+    }
+
+    public static class Data {
+        public float percent = 0;
+    }
+}
