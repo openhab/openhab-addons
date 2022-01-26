299a069780c19ac39d924e8a3cc5cac257fd778a
Benjamin Lafois <benjamin.lafois@gmail.com>
2022-01-26 20:12:46 +0100

    [somfytahoma] [Improvement] Fix cozytouch support and add waterheatersystem support (#11855)
    * Add support for Cozytouch and WaterHeaterSystem
    
    Signed-off-by: Benjamin Lafois <benjamin.lafois@gmail.com>

---
+++ bundles/org.openhab.binding.somfytahoma/src/main/java/org/openhab/binding/somfytahoma/internal/model/SomfyTahomaOauth2Reponse.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.somfytahoma.internal.model;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * The {@link SomfyTahomaOauth2Reponse} holds information about Oauth2 login
+ * response to your CozyTouch account.
+ *
+ * @author Benjamin Lafois - Initial contribution
+ */
+@NonNullByDefault
+public class SomfyTahomaOauth2Reponse {
+    private String scope = "";
+
+    @SerializedName("token_type")
+    private String tokenType = "";
+
+    @SerializedName("expires_in")
+    private int expiresIn = 0;
+
+    @SerializedName("refresh_token")
+    private String refreshToken = "";
+
+    @SerializedName("access_token")
+    private String accessToken = "";
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public int getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(int expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
