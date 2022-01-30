8fb1ad13b3d397375eb9b964548fbab4f3a9bdf8
Kai Kreuzer <kai@openhab.org>
2022-01-30 16:56:10 +0100

    Introduce "allowWakeUpForCommands" option
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- bundles/org.openhab.binding.tesla/src/main/java/org/openhab/binding/tesla/internal/handler/TeslaAccountHandler.java
+++ bundles/org.openhab.binding.tesla/src/main/java/org/openhab/binding/tesla/internal/handler/TeslaAccountHandler.java
@@ -71,7 +71,7 @@ import com.google.gson.JsonParser;
  */
 public class TeslaAccountHandler extends BaseBridgeHandler {
 
-    public static final int API_MAXIMUM_ERRORS_IN_INTERVAL = 2;
+    public static final int API_MAXIMUM_ERRORS_IN_INTERVAL = 3;
     public static final int API_ERROR_INTERVAL_SECONDS = 15;
     private static final int CONNECT_RETRY_INTERVAL = 15000;
     private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
@@ -219,7 +219,7 @@ public class TeslaAccountHandler extends BaseBridgeHandler {
             Vehicle[] vehicleArray = gson.fromJson(jsonObject.getAsJsonArray("response"), Vehicle[].class);
 
             for (Vehicle vehicle : vehicleArray) {
-                String responseString = invokeAndParse(vehicle.id, VEHICLE_CONFIG, null, dataRequestTarget);
+                String responseString = invokeAndParse(vehicle.id, VEHICLE_CONFIG, null, dataRequestTarget, 0);
                 if (responseString == null || responseString.isBlank()) {
                     continue;
                 }
@@ -308,7 +308,8 @@ public class TeslaAccountHandler extends BaseBridgeHandler {
         return new ThingStatusInfo(ThingStatus.ONLINE, ThingStatusDetail.NONE, null);
     }
 
-    protected String invokeAndParse(String vehicleId, String command, String payLoad, WebTarget target) {
+    protected String invokeAndParse(String vehicleId, String command, String payLoad, WebTarget target,
+            int noOfretries) {
         logger.debug("Invoking: {}", command);
 
         if (vehicleId != null) {
@@ -339,6 +340,16 @@ public class TeslaAccountHandler extends BaseBridgeHandler {
                 logger.debug("An error occurred while communicating with the vehicle during request {}: {}: {}",
                         command, (response != null) ? response.getStatus() : "",
                         (response != null) ? response.getStatusInfo().getReasonPhrase() : "No Response");
+                if (response.getStatus() == 408 && noOfretries > 0) {
+                    try {
+                        // we give the vehicle a moment to wake up and try the request again
+                        Thread.sleep(TimeUnit.SECONDS.toMillis(API_ERROR_INTERVAL_SECONDS));
+                        logger.debug("Retrying to send the command {}.", command);
+                        return invokeAndParse(vehicleId, command, payLoad, target, noOfretries - 1);
+                    } catch (InterruptedException e) {
+                        return null;
+                    }
+                }
                 return null;
             }
 
@@ -445,16 +456,21 @@ public class TeslaAccountHandler extends BaseBridgeHandler {
 
     protected class Request implements Runnable {
 
+        private static final int NO_OF_RETRIES = 3;
+
         private TeslaVehicleHandler handler;
         private String request;
         private String payLoad;
         private WebTarget target;
+        private boolean allowWakeUpForCommands;
 
-        public Request(TeslaVehicleHandler handler, String request, String payLoad, WebTarget target) {
+        public Request(TeslaVehicleHandler handler, String request, String payLoad, WebTarget target,
+                boolean allowWakeUpForCommands) {
             this.handler = handler;
             this.request = request;
             this.payLoad = payLoad;
             this.target = target;
+            this.allowWakeUpForCommands = allowWakeUpForCommands;
         }
 
         @Override
@@ -463,7 +479,8 @@ public class TeslaAccountHandler extends BaseBridgeHandler {
                 String result = "";
 
                 if (getThing().getStatus() == ThingStatus.ONLINE) {
-                    result = invokeAndParse(handler.getVehicleId(), request, payLoad, target);
+                    result = invokeAndParse(handler.getVehicleId(), request, payLoad, target,
+                            allowWakeUpForCommands ? NO_OF_RETRIES : 0);
                     if (result != null && !"".equals(result)) {
                         handler.parseAndUpdate(request, payLoad, result);
                     }
@@ -474,9 +491,9 @@ public class TeslaAccountHandler extends BaseBridgeHandler {
         }
     }
 
-    public Request newRequest(TeslaVehicleHandler teslaVehicleHandler, String command, String payLoad,
-            WebTarget target) {
-        return new Request(teslaVehicleHandler, command, payLoad, target);
+    public Request newRequest(TeslaVehicleHandler teslaVehicleHandler, String command, String payLoad, WebTarget target,
+            boolean allowWakeUpForCommands) {
+        return new Request(teslaVehicleHandler, command, payLoad, target, allowWakeUpForCommands);
     }
 
     @Override
