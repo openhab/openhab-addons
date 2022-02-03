ebcce0940a78283885db5c98940fb833c78bef22
Nemer Daud <nemer.daud@gmail.com>
2022-02-03 12:19:08 -0300

    [lgthinq] Repackaging, Enhanced data binding and some logs for troubleshooting
    Signed-off-by: nemerdaud <nemer.daud@gmail.com>

--- bundles/org.openhab.binding.lgthinq/src/main/java/org/openhab/binding/lgthinq/lgservices/model/ACSnapShot.java
+++ bundles/org.openhab.binding.lgthinq/src/main/java/org/openhab/binding/lgthinq/lgservices/model/ACSnapShot.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.lgthinq.lgapi.model;
+package org.openhab.binding.lgthinq.lgservices.model;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
