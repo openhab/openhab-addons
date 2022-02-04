f3b4f5ccdd9b940edb146b639104526843d97c71
Pascal Larin <plarin@gmail.com>
2022-02-04 15:16:54 -0500

    Code revert and updated javadoc
    - Revert a change
    - Update javadoc
    
    Signed-off-by: Pascal Larin <plarin@gmail.com>

--- bundles/org.openhab.binding.sinope/src/main/java/org/openhab/binding/sinope/internal/core/base/SinopeFrame.java
+++ bundles/org.openhab.binding.sinope/src/main/java/org/openhab/binding/sinope/internal/core/base/SinopeFrame.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2022 Contributors to the openHAB project
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -48,16 +48,6 @@ abstract class SinopeFrame {
 
     /** The internal payload. */
     protected byte[] internal_payload;
-
-    /** The entity */
-    private String entity;
-
-    public SinopeFrame(String entity) {
-        this.entity = entity;
-    }
-
-    private SinopeFrame() {
-    }
 
     /**
      * Gets the command.
--- bundles/org.openhab.binding.sinope/src/main/java/org/openhab/binding/sinope/internal/core/base/SinopeRequest.java
+++ bundles/org.openhab.binding.sinope/src/main/java/org/openhab/binding/sinope/internal/core/base/SinopeRequest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2022 Contributors to the openHAB project
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -35,17 +35,8 @@ public abstract class SinopeRequest extends SinopeFrame {
     /** The Constant logger. */
     private static final Logger logger = LoggerFactory.getLogger(SinopeRequest.class);
 
-    public SinopeRequest() {
-        super(entity);
-    }
-
     /**
      * @see org.openhab.binding.sinope.internal.core.base.SinopeFrame#getPayload()
-     */
-    /*
-     *
-     *
-     * @see ca.tulip.sinope.core.internal.SinopeFrame#getPayload()
      */
     @Override
     public byte[] getPayload() {
