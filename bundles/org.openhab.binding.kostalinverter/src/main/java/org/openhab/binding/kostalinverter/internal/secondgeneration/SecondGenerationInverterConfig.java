38191393b8ea9a7ddeb643923403b7e6bbed88ca
basse04 <orjan.backsell@gmail.com>
2022-02-06 11:02:32 +0100

    [kostalinverter] Fix to, prevent randomly stops of binding. (#12124)
    * [kostalinverter] Fix to, prevent randomly stops of binding.
    
    Signed-off-by: basse04 <orjan.backsell@gmail.com>
    Signed-off-by: Simon Dengler <simon.dengler@faps.fau.de>

--- bundles/org.openhab.binding.kostalinverter/src/main/java/org/openhab/binding/kostalinverter/internal/secondgeneration/SecondGenerationInverterConfig.java
+++ bundles/org.openhab.binding.kostalinverter/src/main/java/org/openhab/binding/kostalinverter/internal/secondgeneration/SecondGenerationInverterConfig.java
@@ -25,12 +25,11 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
 
 @NonNullByDefault
 public class SecondGenerationInverterConfig {
-    public static final long REFRESHINTERVAL_SEC = 60;
-
     public String url = "";
     public String username = "";
     public String password = "";
+    public int refreshInterval = 60;
     public String dxsIdConf = "";
     public String valueConf = "";
-    public boolean hasBattery;
+    public boolean hasBattery = false;
 }
