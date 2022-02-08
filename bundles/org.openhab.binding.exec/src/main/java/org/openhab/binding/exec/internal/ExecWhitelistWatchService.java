e6e4d4de6133c42dc5c2222a4e0eb4f34cf512ec
J-N-K <github@klug.nrw>
2022-02-08 18:35:16 +0100

    Adjustments after changing null-annotations for updating properties (#12221)
    Signed-off-by: Jan N. Klug <github@klug.nrw>
    Signed-off-by: Simon Dengler <simon.dengler@faps.fau.de>

--- bundles/org.openhab.binding.exec/src/main/java/org/openhab/binding/exec/internal/ExecWhitelistWatchService.java
+++ bundles/org.openhab.binding.exec/src/main/java/org/openhab/binding/exec/internal/ExecWhitelistWatchService.java
@@ -59,7 +59,7 @@ public class ExecWhitelistWatchService extends AbstractWatchService {
     }
 
     @Override
-    protected Kind<?>[] getWatchEventKinds(@Nullable Path directory) {
+    protected Kind<?> @Nullable [] getWatchEventKinds(@Nullable Path directory) {
         return new Kind<?>[] { ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY };
     }
 
