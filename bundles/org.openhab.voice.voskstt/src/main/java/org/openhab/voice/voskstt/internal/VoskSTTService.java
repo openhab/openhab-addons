a8865140c90bdfe42b2e352f02ccc346c08e8f57
Miguel Álvarez Díez <miguelwork92@gmail.com>
2022-02-09 00:00:50 +0100

    [voskstt] initial contribution
    Signed-off-by: Miguel Álvarez Díez <miguelwork92@gmail.com>

---
+++ bundles/org.openhab.voice.voskstt/src/main/java/org/openhab/voice/voskstt/internal/VoskSTTService.java
@@ -0,0 +1,226 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.voice.voskstt.internal;
+
+import static org.openhab.voice.voskstt.internal.VoskSTTConstants.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.OpenHAB;
+import org.openhab.core.audio.AudioFormat;
+import org.openhab.core.audio.AudioStream;
+import org.openhab.core.common.ThreadPoolManager;
+import org.openhab.core.config.core.ConfigurableService;
+import org.openhab.core.config.core.Configuration;
+import org.openhab.core.io.rest.LocaleService;
+import org.openhab.core.voice.RecognitionStartEvent;
+import org.openhab.core.voice.RecognitionStopEvent;
+import org.openhab.core.voice.STTException;
+import org.openhab.core.voice.STTListener;
+import org.openhab.core.voice.STTService;
+import org.openhab.core.voice.STTServiceHandle;
+import org.openhab.core.voice.SpeechRecognitionErrorEvent;
+import org.openhab.core.voice.SpeechRecognitionEvent;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.vosk.Model;
+import org.vosk.Recognizer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * The {@link VoskSTTService} class is a service implementation to use Vosk-API for Speech-to-Text.
+ *
+ * @author Miguel Álvarez - Initial contribution
+ */
+@NonNullByDefault
+@Component(configurationPid = SERVICE_PID, property = Constants.SERVICE_PID + "=" + SERVICE_PID)
+@ConfigurableService(category = SERVICE_CATEGORY, label = SERVICE_NAME
+        + " Speech-to-Text", description_uri = SERVICE_CATEGORY + ":" + SERVICE_ID)
+public class VoskSTTService implements STTService {
+    private final Logger logger = LoggerFactory.getLogger(VoskSTTService.class);
+    private final ScheduledExecutorService executor = ThreadPoolManager.getScheduledPool("OH-voice-voskstt");
+    private final LocaleService localeService;
+    private VoskSTTConfiguration config = new VoskSTTConfiguration();
+    private static final String VOSK_FOLDER = Path.of(OpenHAB.getUserDataFolder(), "vosk").toString();
+    private static final String MODEL_PATH = Path.of(VOSK_FOLDER, "model").toString();
+    static {
+        Logger logger = LoggerFactory.getLogger(VoskSTTService.class);
+        File directory = new File(VOSK_FOLDER);
+        if (!directory.exists()) {
+            if (directory.mkdir()) {
+                logger.info("vosk dir created {}", VOSK_FOLDER);
+            }
+        }
+    }
+
+    @Activate
+    public VoskSTTService(@Reference LocaleService localeService) {
+        this.localeService = localeService;
+    }
+
+    @Activate
+    protected void activate(Map<String, Object> config) {
+        this.config = new Configuration(config).as(VoskSTTConfiguration.class);
+    }
+
+    @Modified
+    protected void modified(Map<String, Object> config) {
+        this.config = new Configuration(config).as(VoskSTTConfiguration.class);
+    }
+
+    @Override
+    public String getId() {
+        return SERVICE_ID;
+    }
+
+    @Override
+    public String getLabel(@Nullable Locale locale) {
+        return SERVICE_NAME;
+    }
+
+    @Override
+    public Set<Locale> getSupportedLocales() {
+        return Set.of(localeService.getLocale(null));
+    }
+
+    @Override
+    public Set<AudioFormat> getSupportedFormats() {
+        return Set.of(
+                new AudioFormat(AudioFormat.CONTAINER_WAVE, AudioFormat.CODEC_PCM_SIGNED, false, null, null, 16000L));
+    }
+
+    @Override
+    public STTServiceHandle recognize(STTListener sttListener, AudioStream audioStream, Locale locale, Set<String> set)
+            throws STTException {
+        Future<?> task;
+        AtomicBoolean keepStreaming = new AtomicBoolean(true);
+        try {
+            var frequency = audioStream.getFormat().getFrequency();
+            if (frequency == null) {
+                throw new IOException("missing audio stream frequency");
+            }
+            var modelFile = new File(MODEL_PATH);
+            if (!modelFile.exists() && modelFile.isDirectory()) {
+                throw new IOException("missing model dir: " + MODEL_PATH);
+            }
+            task = backgroundRecognize(sttListener, audioStream, frequency, keepStreaming);
+        } catch (IOException e) {
+            throw new STTException(e);
+        }
+        return () -> {
+            if (keepStreaming.getAndSet(false)) {
+                trySleep(100);
+            }
+            task.cancel(true);
+        };
+    }
+
+    private Future<?> backgroundRecognize(STTListener sttListener, InputStream audioStream, long frequency,
+            AtomicBoolean keepStreaming) {
+        StringBuilder transcriptBuilder = new StringBuilder();
+        long maxTranscriptionMillis = (config.maxTranscriptionSeconds * 1000L);
+        long maxSilenceMillis = (config.maxSilenceSeconds * 1000L);
+        long startTime = System.currentTimeMillis();
+        return executor.submit(() -> {
+            try (Model model = new Model(MODEL_PATH); Recognizer recognizer = new Recognizer(model, frequency)) {
+                long lastInputTime = System.currentTimeMillis();
+                int nbytes;
+                byte[] b = new byte[4096];
+                sttListener.sttEventReceived(new RecognitionStartEvent());
+                while (keepStreaming.get()) {
+                    nbytes = audioStream.read(b);
+                    if (!keepStreaming.get()) {
+                        break;
+                    }
+                    if (isExpiredInterval(maxTranscriptionMillis, startTime)) {
+                        logger.debug("Stops listening, max transcription time reached");
+                        break;
+                    }
+                    if (!config.singleUtteranceMode && isExpiredInterval(maxSilenceMillis, lastInputTime)) {
+                        logger.debug("Stops listening, max silence time reached");
+                        break;
+                    }
+                    if (nbytes == 0) {
+                        trySleep(100);
+                        continue;
+                    }
+                    if (recognizer.acceptWaveForm(b, nbytes)) {
+                        lastInputTime = System.currentTimeMillis();
+                        var result = recognizer.getResult();
+                        logger.debug("Result: {}", result);
+                        ObjectMapper mapper = new ObjectMapper();
+                        var json = mapper.readTree(result);
+                        transcriptBuilder.append(json.get("text").asText()).append(" ");
+                        if (config.singleUtteranceMode) {
+                            break;
+                        }
+                    } else {
+                        logger.debug("Partial: {}", recognizer.getPartialResult());
+                    }
+                }
+                sttListener.sttEventReceived(new RecognitionStopEvent());
+                var transcript = transcriptBuilder.toString().trim();
+                logger.debug("Final: {}", transcript);
+                if (!transcript.isBlank()) {
+                    sttListener.sttEventReceived(new SpeechRecognitionEvent(transcript, 1F));
+                } else {
+                    if (!config.noResultsMessage.isBlank()) {
+                        sttListener.sttEventReceived(new SpeechRecognitionErrorEvent(config.noResultsMessage));
+                    } else {
+                        sttListener.sttEventReceived(new SpeechRecognitionErrorEvent("No results"));
+                    }
+                }
+            } catch (IOException e) {
+                logger.warn("Error running speech to text: {}", e.getMessage());
+                if (config.errorMessage.isBlank()) {
+                    sttListener.sttEventReceived(new SpeechRecognitionErrorEvent("Error"));
+                } else if (!config.errorMessage.isBlank()) {
+                    sttListener.sttEventReceived(new SpeechRecognitionErrorEvent(config.errorMessage));
+                }
+            }
+            try {
+                audioStream.close();
+            } catch (IOException e) {
+                logger.warn("IOException on close: {}", e.getMessage());
+            }
+        });
+    }
+
+    private void trySleep(long ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException ignored) {
+        }
+    }
+
+    private boolean isExpiredInterval(long interval, long referenceTime) {
+        return System.currentTimeMillis() - referenceTime > interval;
+    }
+}
