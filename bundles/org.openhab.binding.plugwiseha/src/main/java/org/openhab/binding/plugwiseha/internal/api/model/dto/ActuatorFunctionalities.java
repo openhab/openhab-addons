openhab/openhab-addons

commit 82b345db5303b3642270fd3117fe4c10a2ebebfa

[plugwiseha] Add and enhance channels (#12349)
* Add two-channels, fix scene and docs

Signed-off-by: Leo Siepel <leosiepel@gmail.com>

bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/api/model/dto/ActuatorFunctionalities.java
@@ -35,6 +35,15 @@ public class ActuatorFunctionalities extends PlugwiseHACollection<ActuatorFuncti
                 .map(Boolean::parseBoolean);
     }
 
+    public Optional<String> getRegulationControl() {
+        return this.getFunctionalityThermostat().flatMap(ActuatorFunctionality::getRegulationControl);
+    }
+
+    public Optional<Boolean> getCoolingAllowed() {
+        return this.getFunctionalityThermostat().flatMap(ActuatorFunctionality::getCoolingAllowed)
+                .map(Boolean::parseBoolean);
+    }
+
     public Optional<Boolean> getPreHeatState() {
         return this.getFunctionalityThermostat().flatMap(ActuatorFunctionality::getPreHeatState)
                 .map(Boolean::parseBoolean);

bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/api/model/dto/ActuatorFunctionality.java
@@ -35,6 +35,12 @@ public class ActuatorFunctionality extends PlugwiseBaseModel implements Plugwise
     private String resolution;
     private String lock;
 
+    @XStreamAlias("regulation_control")
+    private String regulationControl;
+
+    @XStreamAlias("cooling_allowed")
+    private String coolingAllowed;
+
     @XStreamAlias("preheating_allowed")
     private String preHeat;
 
@@ -73,6 +79,14 @@ public class ActuatorFunctionality extends PlugwiseBaseModel implements Plugwise
 
     public ZonedDateTime getUpdatedDate() {
         return updatedDate;
+    }
+
+    public Optional<String> getRegulationControl() {
+        return Optional.ofNullable(regulationControl);
+    }
+
+    public Optional<String> getCoolingAllowed() {
+        return Optional.ofNullable(coolingAllowed);
     }
 
     public Optional<String> getPreHeatState() {

bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/api/model/dto/Location.java
@@ -47,6 +47,10 @@ public class Location extends PlugwiseBaseModel implements PlugwiseComparableDat
     @XStreamImplicit(itemFieldName = "actuator_functionality", keyFieldName = "type")
     private ActuatorFunctionalities actuatorFunctionalities;
 
+    public Location(String presetScene) {
+        this.preset = presetScene;
+    }
+
     public String getName() {
         return name;
     }
@@ -99,6 +103,14 @@ public class Location extends PlugwiseBaseModel implements PlugwiseComparableDat
 
     public Optional<Boolean> getPreHeatState() {
         return this.actuatorFunctionalities.getPreHeatState();
+    }
+
+    public Optional<Boolean> getCoolingAllowed() {
+        return this.actuatorFunctionalities.getCoolingAllowed();
+    }
+
+    public Optional<String> getRegulationControl() {
+        return this.actuatorFunctionalities.getRegulationControl();
     }
 
     public int applianceCount() {

bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/handler/PlugwiseHAZoneHandler.java
@@ -118,6 +118,16 @@ public class PlugwiseHAZoneHandler extends PlugwiseHABaseHandler<Location, Plugw
             PlugwiseHAController controller = bridge.getController();
             if (controller != null) {
                 switch (channelID) {
+                    case ZONE_COOLING_CHANNEL:
+                        if (command instanceof OnOffType) {
+                            try {
+                                controller.setAllowCooling(entity, command == OnOffType.ON);
+                            } catch (PlugwiseHAException e) {
+                                logger.warn("Unable to switch allow cooling {} for zone '{}'", (State) command,
+                                        entity.getName());
+                            }
+                        }
+                        break;
                     case ZONE_SETPOINT_CHANNEL:
                         if (command instanceof QuantityType) {
                             Unit<Temperature> unit = entity.getSetpointTemperatureUnit().orElse(UNIT_CELSIUS)
@@ -143,6 +153,26 @@ public class PlugwiseHAZoneHandler extends PlugwiseHABaseHandler<Location, Plugw
                             }
                         }
                         break;
+                    case ZONE_REGULATION_CHANNEL:
+                        if (command instanceof StringType) {
+                            try {
+                                controller.setRegulationControl(entity, command.toString());
+                            } catch (PlugwiseHAException e) {
+                                logger.warn("Unable to switch regulation control {} for zone '{}'", (State) command,
+                                        entity.getName());
+                            }
+                        }
+                        break;
+                    case ZONE_PRESETSCENE_CHANNEL:
+                        if (command instanceof StringType) {
+                            try {
+                                controller.setPresetScene(entity, command.toString());
+                            } catch (PlugwiseHAException e) {
+                                logger.warn("Unable to switch preset scene {} for zone '{}'", (State) command,
+                                        entity.getName());
+                            }
+                        }
+                        break;
                     default:
                         logger.warn("Ignoring unsupported command = {} for channel = {}", command, channelUID);
                 }
@@ -153,8 +183,10 @@ public class PlugwiseHAZoneHandler extends PlugwiseHABaseHandler<Location, Plugw
     private State getDefaultState(String channelID) {
         State state = UnDefType.NULL;
         switch (channelID) {
+            case ZONE_COOLING_CHANNEL:
             case ZONE_PREHEAT_CHANNEL:
             case ZONE_PRESETSCENE_CHANNEL:
+            case ZONE_REGULATION_CHANNEL:
             case ZONE_SETPOINT_CHANNEL:
             case ZONE_TEMPERATURE_CHANNEL:
                 state = UnDefType.NULL;
@@ -169,6 +201,12 @@ public class PlugwiseHAZoneHandler extends PlugwiseHABaseHandler<Location, Plugw
         State state = getDefaultState(channelID);
 
         switch (channelID) {
+            case ZONE_COOLING_CHANNEL:
+                Optional<Boolean> allowCoolingState = entity.getCoolingAllowed();
+                if (allowCoolingState.isPresent()) {
+                    state = OnOffType.from(allowCoolingState.get());
+                }
+                break;
             case ZONE_PREHEAT_CHANNEL:
                 Optional<Boolean> preHeatState = entity.getPreHeatState();
                 if (preHeatState.isPresent()) {
@@ -184,6 +222,9 @@ public class PlugwiseHAZoneHandler extends PlugwiseHABaseHandler<Location, Plugw
                             .equals(UNIT_CELSIUS) ? SIUnits.CELSIUS : ImperialUnits.FAHRENHEIT;
                     state = new QuantityType<Temperature>(entity.getSetpointTemperature().get(), unit);
                 }
+                break;
+            case ZONE_REGULATION_CHANNEL:
+                state = new StringType(entity.getRegulationControl().orElse(null));
                 break;
             case ZONE_TEMPERATURE_CHANNEL:
                 if (entity.getTemperature().isPresent()) {

bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/PlugwiseHABindingConstants.java
@@ -85,10 +85,12 @@ public class PlugwiseHABindingConstants {
     public static final String LOCATION_PROPERTY_FUNCTIONALITIES = "functionalities";
 
     // List of all Channel IDs
+    public static final String ZONE_COOLING_CHANNEL = "coolingAllowed";
     public static final String ZONE_SETPOINT_CHANNEL = "setpointTemperature";
     public static final String ZONE_TEMPERATURE_CHANNEL = "temperature";
     public static final String ZONE_PRESETSCENE_CHANNEL = "presetScene";
     public static final String ZONE_PREHEAT_CHANNEL = "preHeat";
+    public static final String ZONE_REGULATION_CHANNEL = "regulationControl";
 
     public static final String APPLIANCE_SETPOINT_CHANNEL = "setpointTemperature";
     public static final String APPLIANCE_TEMPERATURE_CHANNEL = "temperature";

bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/api/model/PlugwiseHAController.java
@@ -322,17 +322,6 @@ public class PlugwiseHAController {
         }
     }
 
-    public void switchRelay(Appliance appliance, String state) throws PlugwiseHAException {
-        List<String> allowStates = Arrays.asList("on", "off");
-        if (allowStates.contains(state.toLowerCase())) {
-            if (state.toLowerCase().equals("on")) {
-                switchRelayOn(appliance);
-            } else {
-                switchRelayOff(appliance);
-            }
-        }
-    }
-
     public void setPreHeating(Location location, Boolean state) throws PlugwiseHAException {
         PlugwiseHAControllerRequest<Void> request = newRequest(Void.class);
         Optional<ActuatorFunctionality> thermostat = location.getActuatorFunctionalities().getFunctionalityThermostat();
@@ -340,58 +329,73 @@ public class PlugwiseHAController {
         request.setPath("/core/locations");
         request.addPathParameter("id", String.format("%s/thermostat", location.getId()));
         request.addPathParameter("id", String.format("%s", thermostat.get().getId()));
-        request.setBodyParameter(new ActuatorFunctionalityThermostat(state));
+        request.setBodyParameter(new ActuatorFunctionalityThermostat(state, null, null));
 
         executeRequest(request);
     }
 
-    public void switchRelayOn(Appliance appliance) throws PlugwiseHAException {
+    public void setAllowCooling(Location location, Boolean state) throws PlugwiseHAException {
         PlugwiseHAControllerRequest<Void> request = newRequest(Void.class);
+        Optional<ActuatorFunctionality> thermostat = location.getActuatorFunctionalities().getFunctionalityThermostat();
 
-        request.setPath("/core/appliances");
-        request.addPathParameter("id", String.format("%s/relay", appliance.getId()));
-        request.setBodyParameter(new ActuatorFunctionalityRelay("on"));
-
-        executeRequest(request);
-    }
-
-    public void switchRelayOff(Appliance appliance) throws PlugwiseHAException {
-        PlugwiseHAControllerRequest<Void> request = newRequest(Void.class);
-
-        request.setPath("/core/appliances");
-        request.addPathParameter("id", String.format("%s/relay", appliance.getId()));
-        request.setBodyParameter(new ActuatorFunctionalityRelay("off"));
+        request.setPath("/core/locations");
+        request.addPathParameter("id", String.format("%s/thermostat", location.getId()));
+        request.addPathParameter("id", String.format("%s", thermostat.get().getId()));
+        request.setBodyParameter(new ActuatorFunctionalityThermostat(null, state, null));
 
         executeRequest(request);
     }
 
-    public void switchRelayLock(Appliance appliance, String state) throws PlugwiseHAException {
-        List<String> allowStates = Arrays.asList("on", "off");
-        if (allowStates.contains(state.toLowerCase())) {
-            if (state.toLowerCase().equals("on")) {
-                switchRelayLockOn(appliance);
-            } else {
-                switchRelayLockOff(appliance);
-            }
+    public void setRegulationControl(Location location, String state) throws PlugwiseHAException {
+        List<String> allowStates = Arrays.asList("active", "passive", "off");
+        if (!allowStates.contains(state.toLowerCase())) {
+            this.logger.warn("Trying to set the regulation control to an invalid state");
+            return;
         }
-    }
 
-    public void switchRelayLockOff(Appliance appliance) throws PlugwiseHAException {
         PlugwiseHAControllerRequest<Void> request = newRequest(Void.class);
+        Optional<ActuatorFunctionality> thermostat = location.getActuatorFunctionalities().getFunctionalityThermostat();
 
-        request.setPath("/core/appliances");
-        request.addPathParameter("id", String.format("%s/relay", appliance.getId()));
-        request.setBodyParameter(new ActuatorFunctionalityRelay(null, false));
+        request.setPath("/core/locations");
+        request.addPathParameter("id", String.format("%s/thermostat", location.getId()));
+        request.addPathParameter("id", String.format("%s", thermostat.get().getId()));
+        request.setBodyParameter(new ActuatorFunctionalityThermostat(null, null, state));
 
         executeRequest(request);
     }
 
-    public void switchRelayLockOn(Appliance appliance) throws PlugwiseHAException {
+    public void setRelay(Appliance appliance, Boolean state) throws PlugwiseHAException {
         PlugwiseHAControllerRequest<Void> request = newRequest(Void.class);
 
         request.setPath("/core/appliances");
         request.addPathParameter("id", String.format("%s/relay", appliance.getId()));
-        request.setBodyParameter(new ActuatorFunctionalityRelay(null, true));
+        request.setBodyParameter(new ActuatorFunctionalityRelay(state ? "on" : "off"));
+
+        executeRequest(request);
+    }
+
+    public void setRelayLock(Appliance appliance, Boolean state) throws PlugwiseHAException {
+        PlugwiseHAControllerRequest<Void> request = newRequest(Void.class);
+
+        request.setPath("/core/appliances");
+        request.addPathParameter("id", String.format("%s/relay", appliance.getId()));
+        request.setBodyParameter(new ActuatorFunctionalityRelay(null, state));
+
+        executeRequest(request);
+    }
+
+    public void setPresetScene(Location location, String state) throws PlugwiseHAException {
+        List<String> allowStates = Arrays.asList("home", "asleep", "away", "vacation", "no_frost");
+        if (!allowStates.contains(state.toLowerCase())) {
+            this.logger.warn("Trying to set the preset scene to an invalid state");
+            return;
+        }
+
+        PlugwiseHAControllerRequest<Void> request = newRequest(Void.class);
+
+        request.setPath("/core/locations");
+        request.addPathParameter("id", String.format("%s", location.getId()));
+        request.setBodyParameter(new Location(state));
 
         executeRequest(request);
     }

bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/api/model/dto/ActuatorFunctionalityThermostat.java
@@ -28,11 +28,22 @@ public class ActuatorFunctionalityThermostat extends ActuatorFunctionality {
     @XStreamAlias("preheating_allowed")
     private Boolean preheatingAllowed;
 
+    @SuppressWarnings("unused")
+    @XStreamAlias("cooling_allowed")
+    private Boolean coolingAllowed;
+
+    @SuppressWarnings("unused")
+    @XStreamAlias("regulation_control")
+    private String regulationControl;
+
     public ActuatorFunctionalityThermostat(Double temperature) {
         this.setpoint = temperature;
     }
 
-    public ActuatorFunctionalityThermostat(Boolean preheatingAllowed) {
+    public ActuatorFunctionalityThermostat(Boolean preheatingAllowed, Boolean coolingAllowed,
+            String regulationControl) {
         this.preheatingAllowed = preheatingAllowed;
+        this.coolingAllowed = coolingAllowed;
+        this.regulationControl = regulationControl;
     }
 }

bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/handler/PlugwiseHAApplianceHandler.java
@@ -142,11 +142,7 @@ public class PlugwiseHAApplianceHandler extends PlugwiseHABaseHandler<Appliance,
             case APPLIANCE_LOCK_CHANNEL:
                 if (command instanceof OnOffType) {
                     try {
-                        if (command == OnOffType.ON) {
-                            controller.switchRelayLockOn(entity);
-                        } else {
-                            controller.switchRelayLockOff(entity);
-                        }
+                        controller.setRelay(entity, (command == OnOffType.ON));
                     } catch (PlugwiseHAException e) {
                         logger.warn("Unable to switch relay lock {} for appliance '{}'", (State) command,
                                 entity.getName());
@@ -173,11 +169,7 @@ public class PlugwiseHAApplianceHandler extends PlugwiseHABaseHandler<Appliance,
             case APPLIANCE_POWER_CHANNEL:
                 if (command instanceof OnOffType) {
                     try {
-                        if (command == OnOffType.ON) {
-                            controller.switchRelayOn(entity);
-                        } else {
-                            controller.switchRelayOff(entity);
-                        }
+                        controller.setRelay(entity, command == OnOffType.ON);
                     } catch (PlugwiseHAException e) {
                         logger.warn("Unable to switch relay {} for appliance '{}'", (State) command, entity.getName());
                     }

commit fa1ea319fb96387365c59d3f5804c90d5278e34d

Fix bug introduced with #12349
Signed-off-by: Leo Siepel <leosiepel@gmail.com>

bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/api/model/dto/ActuatorFunctionalities.java
@@ -35,8 +35,12 @@ public class ActuatorFunctionalities extends PlugwiseHACollection<ActuatorFuncti
                 .map(Boolean::parseBoolean);
     }
 
-    public Optional<String> getRegulationControl() {
-        return this.getFunctionalityThermostat().flatMap(ActuatorFunctionality::getRegulationControl);
+    public String getRegulationControl() {
+        ActuatorFunctionality functionality = this.getFunctionalityThermostat().orElse(null);
+        if (functionality != null) {
+            return functionality.getRegulationControl();
+        }
+        return null;
     }
 
     public Optional<Boolean> getCoolingAllowed() {

bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/api/model/dto/ActuatorFunctionality.java
@@ -81,8 +81,8 @@ public class ActuatorFunctionality extends PlugwiseBaseModel implements Plugwise
         return updatedDate;
     }
 
-    public Optional<String> getRegulationControl() {
-        return Optional.ofNullable(regulationControl);
+    public String getRegulationControl() {
+        return regulationControl;
     }
 
     public Optional<String> getCoolingAllowed() {

bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/api/model/dto/Location.java
@@ -109,8 +109,11 @@ public class Location extends PlugwiseBaseModel implements PlugwiseComparableDat
         return this.actuatorFunctionalities.getCoolingAllowed();
     }
 
-    public Optional<String> getRegulationControl() {
-        return this.actuatorFunctionalities.getRegulationControl();
+    public String getRegulationControl() {
+        if (this.actuatorFunctionalities != null) {
+            return this.actuatorFunctionalities.getRegulationControl();
+        }
+        return null;
     }
 
     public int applianceCount() {

bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/handler/PlugwiseHABridgeHandler.java
@@ -175,12 +175,16 @@ public class PlugwiseHABridgeHandler extends BaseBridgeHandler {
         } catch (PlugwiseHAUnauthorizedException | PlugwiseHANotAuthorizedException e) {
             updateStatus(OFFLINE, CONFIGURATION_ERROR, STATUS_DESCRIPTION_INVALID_CREDENTIALS);
         } catch (PlugwiseHACommunicationException e) {
+            this.logger.trace("Bridge encountered an error {} {}", e.getMessage(), e.getStackTrace());
             updateStatus(OFFLINE, COMMUNICATION_ERROR, STATUS_DESCRIPTION_COMMUNICATION_ERROR);
         } catch (PlugwiseHATimeoutException e) {
+            this.logger.trace("Bridge encountered an error {} {}", e.getMessage(), e.getStackTrace());
             updateStatus(OFFLINE, COMMUNICATION_ERROR, STATUS_DESCRIPTION_TIMEOUT);
         } catch (PlugwiseHAException e) {
+            this.logger.trace("Bridge encountered an error {} {}", e.getMessage(), e.getStackTrace());
             updateStatus(OFFLINE, COMMUNICATION_ERROR, e.getMessage());
         } catch (RuntimeException e) {
+            this.logger.trace("Bridge encountered an error {} {}", e.getMessage(), e.getStackTrace());
             updateStatus(OFFLINE, COMMUNICATION_ERROR, e.getMessage());
         }
     }

bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/handler/PlugwiseHAZoneHandler.java
@@ -224,7 +224,10 @@ public class PlugwiseHAZoneHandler extends PlugwiseHABaseHandler<Location, Plugw
                 }
                 break;
             case ZONE_REGULATION_CHANNEL:
-                state = new StringType(entity.getRegulationControl().orElse(null));
+                String value = entity.getRegulationControl();
+                if (value != null) {
+                    state = new StringType(entity.getRegulationControl());
+                }
                 break;
             case ZONE_TEMPERATURE_CHANNEL:
                 if (entity.getTemperature().isPresent()) {