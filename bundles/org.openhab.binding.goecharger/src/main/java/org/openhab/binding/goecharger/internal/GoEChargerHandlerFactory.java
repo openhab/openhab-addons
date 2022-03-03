a129004a44a6bbacdcd663197fd6523c899553de
ranzen84 <reinhardplaim@gmail.com>
2022-03-03 08:26:49 +0100

    remove unnecessary imports
    Signed-off-by: Reinhard Plaim <reinhardplaim@gmail.com>

--- bundles/org.openhab.binding.goecharger/src/main/java/org/openhab/binding/goecharger/internal/GoEChargerHandlerFactory.java
+++ bundles/org.openhab.binding.goecharger/src/main/java/org/openhab/binding/goecharger/internal/GoEChargerHandlerFactory.java
@@ -31,8 +31,6 @@ import org.openhab.core.thing.binding.ThingHandlerFactory;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * The {@link GoEChargerHandlerFactory} is responsible for creating things and
