7693942bd83f9462695714f8d1e40399e884a0a3
Leo Siepel <leosiepel@gmail.com>
2022-03-22 21:48:04 +0100

    Apply spotless
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/api/model/dto/Locations.java
+++ bundles/org.openhab.binding.plugwiseha/src/main/java/org/openhab/binding/plugwiseha/internal/api/model/dto/Locations.java
@@ -41,7 +41,7 @@ public class Locations extends PlugwiseHACollection<Location> {
                     if (updatedPointLogs != null) {
                         updatedPointLogs.merge(originalLocation.getPointLogs());
                     }
-                    
+
                     ActuatorFunctionalities updatedActuatorFunctionalities = location.getActuatorFunctionalities();
                     if (updatedActuatorFunctionalities != null) {
                         updatedActuatorFunctionalities.merge(originalLocation.getActuatorFunctionalities());
