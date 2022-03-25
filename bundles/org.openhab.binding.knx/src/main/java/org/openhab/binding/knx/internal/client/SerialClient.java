bce7573876f6e9c48927b8faf11abeaaf6166ec3
Holger Friedrich <mail@holger-friedrich.de>
2022-03-25 23:46:28 +0100

    [kxn] reduce compiler warnings
    added NonNullByDefault annotations
    rename functions and local variables not matching the naming scheme
    
    Signed-off-by: Holger Friedrich <mail@holger-friedrich.de>

--- bundles/org.openhab.binding.knx/src/main/java/org/openhab/binding/knx/internal/client/SerialClient.java
+++ bundles/org.openhab.binding.knx/src/main/java/org/openhab/binding/knx/internal/client/SerialClient.java
@@ -15,7 +15,7 @@ package org.openhab.binding.knx.internal.client;
 import java.util.Enumeration;
 import java.util.concurrent.ScheduledExecutorService;
 
-import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.openhab.core.thing.ThingUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +33,7 @@ import tuwien.auto.calimero.link.medium.TPSettings;
  * @author Simon Kaufmann - initial contribution and API.
  *
  */
+@NonNullByDefault
 public class SerialClient extends AbstractKNXClient {
 
     private final Logger logger = LoggerFactory.getLogger(SerialClient.class);
@@ -48,7 +49,7 @@ public class SerialClient extends AbstractKNXClient {
     }
 
     @Override
-    protected @NonNull KNXNetworkLink establishConnection() throws KNXException, InterruptedException {
+    protected KNXNetworkLink establishConnection() throws KNXException, InterruptedException {
         try {
             RXTXVersion.getVersion();
             logger.debug("Establishing connection to KNX bus through FT1.2 on serial port {}.", serialPort);
@@ -59,12 +60,13 @@ public class SerialClient extends AbstractKNXClient {
                     "The serial FT1.2 KNX connection requires the RXTX libraries to be available, but they could not be found!",
                     e);
         } catch (KNXException e) {
-            if (e.getMessage().startsWith("can not open serial port")) {
+            final String msg = e.getMessage();
+            if ((msg != null) && (msg.startsWith("can not open serial port"))) {
                 StringBuilder sb = new StringBuilder("Available ports are:\n");
                 Enumeration<?> portList = CommPortIdentifier.getPortIdentifiers();
                 while (portList.hasMoreElements()) {
                     CommPortIdentifier id = (CommPortIdentifier) portList.nextElement();
-                    if (id.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+                    if ((id != null) && (id.getPortType() == CommPortIdentifier.PORT_SERIAL)) {
                         sb.append(id.getName());
                         sb.append("\n");
                     }
