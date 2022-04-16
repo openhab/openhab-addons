35f09ab508114290d2cbc4f8255b4048bda7ee7d
Laurent Garnier <lg.hc@free.fr>
2022-04-16 16:16:22 +0200

    Unit tests added for method extractModelName
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

---
+++ bundles/org.openhab.binding.sonos/src/test/java/org/openhab/binding/sonos/internal/SonosXMLParserTest.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.sonos.internal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Laurent Garnier - Initial contribution
+ */
+@NonNullByDefault
+public class SonosXMLParserTest {
+
+    @Test
+    public void extractModelName() {
+        assertEquals("Move", SonosXMLParser.extractModelName("Sonos Move"));
+        assertEquals("Roam SL", SonosXMLParser.extractModelName("Sonos Roam SL"));
+        assertEquals("Five", SonosXMLParser.extractModelName("Sonos Five (OpenHome)"));
+        assertEquals("One SL", SonosXMLParser.extractModelName("Sonos One SL (OpenHome)"));
+    }
+}
