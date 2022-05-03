0e33adb1562c8496d4edf56870efb7f547919bb1
nemerdaud <nemer.daud@gmail.com>
2022-05-03 17:27:41 -0300

    [lgthinq][Feat] Addition of support to binary data monitoring.
    Signed-off-by: nemerdaud <nemer.daud@gmail.com>
    Signed-off-by: Nemer Daud <nemer@smartsw.com.br>

--- bundles/org.openhab.binding.lgthinq/src/main/java/org/openhab/binding/lgthinq/lgservices/model/SnapshotFactory.java
+++ bundles/org.openhab.binding.lgthinq/src/main/java/org/openhab/binding/lgthinq/lgservices/model/SnapshotFactory.java
@@ -14,16 +14,23 @@ package org.openhab.binding.lgthinq.lgservices.model;
 
 import static org.openhab.binding.lgthinq.internal.LGThinQBindingConstants.WM_SNAPSHOT_WASHER_DRYER_NODE_V2;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.openhab.binding.lgthinq.internal.errors.LGThinqApiException;
+import org.openhab.binding.lgthinq.internal.errors.LGThinqUnmarshallException;
 import org.openhab.binding.lgthinq.lgservices.model.ac.ACSnapshot;
 import org.openhab.binding.lgthinq.lgservices.model.dryer.DryerSnapshot;
 import org.openhab.binding.lgthinq.lgservices.model.washer.WasherSnapshot;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -47,27 +54,84 @@ public class SnapshotFactory {
 
     /**
      * Create a Snapshot result based on snapshotData collected from LG API (V1/C2)
+     *
+     * @param binaryData V1: decoded returnedData
+     *
+     * @return returns Snapshot implementation based on device type provided
+     * @throws LGThinqApiException any error.
+     */
+    public <S extends Snapshot> S createFromBinary(String binaryData, List<MonitoringBinaryProtocol> prot,
+            Class<S> clazz) throws LGThinqUnmarshallException, LGThinqApiException {
+        try {
+            byte[] data = binaryData.getBytes();
+            BeanInfo beanInfo = Introspector.getBeanInfo(clazz);
+            S snap = clazz.getConstructor().newInstance();
+            PropertyDescriptor[] pds = beanInfo.getPropertyDescriptors();
+            for (Iterator<MonitoringBinaryProtocol> it = prot.iterator(); it.hasNext();) {
+                MonitoringBinaryProtocol protField = it.next();
+                String fName = protField.fieldName;
+                for (PropertyDescriptor property : pds) {
+                    // all attributes of class.
+                    Method m = property.getReadMethod(); // getter
+                    List<String> aliases = new ArrayList<>();
+                    if (m.isAnnotationPresent(JsonProperty.class)) {
+                        aliases.add(m.getAnnotation(JsonProperty.class).value());
+                    }
+                    if (m.isAnnotationPresent(JsonAlias.class)) {
+                        aliases.addAll(Arrays.asList(m.getAnnotation(JsonAlias.class).value()));
+                    }
+
+                    if (aliases.contains(fName)) {
+                        // found property. Get bit value
+                        int value = 0;
+                        for (int i = protField.startByte; i < protField.startByte + protField.length; i++) {
+                            value = (value << 8) + data[i];
+                        }
+                        m = property.getWriteMethod();
+                        if (m.getParameters()[0].getType() == String.class) {
+                            m.invoke(snap, String.valueOf(value));
+                        } else if (m.getParameters()[0].getType() == Double.class) {
+                            m.invoke(snap, (double) value);
+                        } else if (m.getParameters()[0].getType() == Integer.class) {
+                            m.invoke(snap, value);
+                        } else {
+                            throw new IllegalArgumentException(
+                                    String.format("Parameter type not supported for this factory:%s",
+                                            m.getParameters()[0].getType().toString()));
+                        }
+                    }
+                }
+            }
+            return snap;
+        } catch (IntrospectionException | InvocationTargetException | InstantiationException | IllegalAccessException
+                | NoSuchMethodException e) {
+            throw new LGThinqUnmarshallException("Unexpected Error unmarshalling binary data", e);
+        }
+    }
+
+    /**
+     * Create a Snapshot result based on snapshotData collected from LG API (V1/C2)
      * 
      * @param snapshotDataJson V1: decoded returnedData; V2: snapshot body
      * @param deviceType device type
      * @return returns Snapshot implementation based on device type provided
      * @throws LGThinqApiException any error.
      */
-    public <S extends Snapshot> S create(String snapshotDataJson, DeviceTypes deviceType, Class<S> clazz)
-            throws LGThinqApiException {
+    public <S extends Snapshot> S createFromJson(String snapshotDataJson, DeviceTypes deviceType, Class<S> clazz)
+            throws LGThinqUnmarshallException, LGThinqApiException {
         try {
             Map<String, Object> snapshotMap = objectMapper.readValue(snapshotDataJson, new TypeReference<>() {
             });
             Map<String, Object> deviceSetting = new HashMap<>();
             deviceSetting.put("deviceType", deviceType.deviceTypeId());
             deviceSetting.put("snapshot", snapshotMap);
-            return create(deviceSetting, clazz);
+            return createFromJson(deviceSetting, clazz);
         } catch (JsonProcessingException e) {
-            throw new LGThinqApiException("Unexpected Error unmarshalling json to map", e);
+            throw new LGThinqUnmarshallException("Unexpected Error unmarshalling json to map", e);
         }
     }
 
-    public <S extends Snapshot> S create(Map<String, Object> deviceSettings, Class<S> clazz)
+    public <S extends Snapshot> S createFromJson(Map<String, Object> deviceSettings, Class<S> clazz)
             throws LGThinqApiException {
         DeviceTypes type = getDeviceType(deviceSettings);
         Map<String, Object> snapMap = ((Map<String, Object>) deviceSettings.get("snapshot"));
