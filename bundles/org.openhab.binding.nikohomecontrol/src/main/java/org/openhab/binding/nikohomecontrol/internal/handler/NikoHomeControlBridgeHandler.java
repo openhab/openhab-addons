b183a1e92e3f990aedc858c03561b40757899af0
Mark Herwege <mark.herwege@telenet.be>
2022-06-04 12:20:28 +0200

    Check bridge status in handler init
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.binding.nikohomecontrol/src/main/java/org/openhab/binding/nikohomecontrol/internal/handler/NikoHomeControlBridgeHandler.java
+++ bundles/org.openhab.binding.nikohomecontrol/src/main/java/org/openhab/binding/nikohomecontrol/internal/handler/NikoHomeControlBridgeHandler.java
@@ -75,6 +75,8 @@ public abstract class NikoHomeControlBridgeHandler extends BaseBridgeHandler imp
             return;
         }
 
+        updateStatus(ThingStatus.UNKNOWN);
+
         scheduler.submit(() -> {
             comm.startCommunication();
             if (!comm.communicationActive()) {
