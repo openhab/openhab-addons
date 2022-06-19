90e3a511dc7e5ddeff00d0432267e8114cc65cb3
Daniel Demus <daniel-github@demus.dk>
2022-06-19 15:01:54 +0000

    [wundergroundupdatereceiver] Initial contribution (#10105)
    * [wundergroundupdatereceiver] Initial implementation
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Code review
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Fix some description formatting
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Add some more channel types per request
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Add more headers to response
    
    
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    Based on the observed headers from actual traffic to wunderground.com.
    
    * [wundergroundupdatereceiver] Discovery service
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Upgrade to 3.2, fix group name constants
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Add a list of channel types
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Add dateutc as synthetic DateTime channel
    
    Also add som emore constants
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Do more programmatic configuration
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] More readme
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Update copyright year
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Handle multiple instance request parameters
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    Ie. those that can have an index number in the name, fx. temp1f, temp2f
    
    * [wundergroundupdatereceiver] Add unmapped but mappable channels
    
    To support multiple devices any new parameters that are submitted cause a new channel to be created
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Don't pass superfluous config to channels
    
    Also documentation additions
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Add generated i18n file
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Re-case test method names
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    * [wundergroundupdatereceiver] Re-word thing file section of README
    
    Signed-off-by: Daniel Demus <daniel-github@demus.dk>
    
    Co-authored-by: Daniel Demus <dde@nine.dk>
    Signed-off-by: Simon Dengler <simon.dengler@faps.fau.de>

---
+++ bundles/org.openhab.binding.wundergroundupdatereceiver/src/main/java/org/openhab/binding/wundergroundupdatereceiver/internal/WundergroundUpdateReceiverConfiguration.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.wundergroundupdatereceiver.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link WundergroundUpdateReceiverConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Daniel Demus - Initial contribution
+ */
+@NonNullByDefault
+public class WundergroundUpdateReceiverConfiguration {
+
+    /**
+     * The station id as registered by wunderground.com or any string if not forwarding data to wunderground.com.
+     * This value will be used as the representation property.
+     */
+    public String stationId = "";
+}
