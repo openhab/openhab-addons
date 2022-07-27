3b8567bd9e8ccc4f7aaddf000f5356608f35aaff
0x4d4d <20473294+0x4d4d@users.noreply.github.com>
2022-07-27 10:38:06 +0200

    [somneo] Initial contribution (#12321)
    * [somneo] Initial contribution
    
    Signed-off-by: Michael Myrcik <michael.myrcik@web.de>

---
+++ bundles/org.openhab.binding.somneo/src/main/java/org/openhab/binding/somneo/internal/model/RadioData.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.somneo.internal.model;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.library.types.StringType;
+import org.openhab.core.types.State;
+import org.openhab.core.types.UnDefType;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * This class represents the radio state from the API.
+ *
+ * @author Michael Myrcik - Initial contribution
+ */
+@NonNullByDefault
+public class RadioData {
+
+    private static final String LABEL_TEMPLATE = "%s fm";
+
+    private static final String CMD_SEEK_UP = "seekup";
+
+    private static final String CMD_SEEK_DOWN = "seekdown";
+
+    @SerializedName("fmfrq")
+    private @Nullable String frequency;
+
+    @SerializedName("fmcmd")
+    private @Nullable String command;
+
+    public State getFrequency() {
+        final String frequency = this.frequency;
+        if (frequency == null) {
+            return UnDefType.NULL;
+        }
+        return new StringType(String.format(LABEL_TEMPLATE, frequency));
+    }
+
+    public void setCmdSeekUp() {
+        this.command = CMD_SEEK_UP;
+    }
+
+    public void setCmdSeekDown() {
+        this.command = CMD_SEEK_DOWN;
+    }
+
+    public boolean isSeeking() {
+        return CMD_SEEK_UP.equals(command) || CMD_SEEK_DOWN.equals(command);
+    }
+}
