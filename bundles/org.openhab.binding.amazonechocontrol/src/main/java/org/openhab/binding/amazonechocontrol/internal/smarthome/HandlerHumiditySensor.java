8d3828a9a4cd3091bddb719bb0a9f2c007f76383
Daniel Campbell <djcampbell79@gmail.com>
2022-08-01 05:48:37 -0500

    [amazonechocontrol] Add channels to thermostatController (#13067)
    * enhance: add thermostat channels to amazonechocontrol
    
    Signed-off-by: Daniel Campbell <djcampbell79@gmail.com>

--- bundles/org.openhab.binding.amazonechocontrol/src/main/java/org/openhab/binding/amazonechocontrol/internal/smarthome/HandlerHumiditySensor.java
+++ bundles/org.openhab.binding.amazonechocontrol/src/main/java/org/openhab/binding/amazonechocontrol/internal/smarthome/HandlerHumiditySensor.java
@@ -13,12 +13,13 @@
 package org.openhab.binding.amazonechocontrol.internal.smarthome;
 
 import static org.openhab.binding.amazonechocontrol.internal.smarthome.Constants.*;
+import static org.openhab.core.library.unit.Units.*;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-import javax.measure.quantity.Temperature;
+import javax.measure.Unit;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
@@ -27,9 +28,8 @@ import org.openhab.binding.amazonechocontrol.internal.handler.SmartHomeDeviceHan
 import org.openhab.binding.amazonechocontrol.internal.jsons.JsonSmartHomeCapabilities.SmartHomeCapability;
 import org.openhab.binding.amazonechocontrol.internal.jsons.JsonSmartHomeDevices.SmartHomeDevice;
 import org.openhab.core.library.types.QuantityType;
-import org.openhab.core.library.unit.ImperialUnits;
-import org.openhab.core.library.unit.SIUnits;
 import org.openhab.core.types.Command;
+import org.openhab.core.types.State;
 import org.openhab.core.types.StateDescription;
 import org.openhab.core.types.UnDefType;
 import org.slf4j.Logger;
@@ -38,23 +38,22 @@ import org.slf4j.LoggerFactory;
 import com.google.gson.JsonObject;
 
 /**
- * The {@link HandlerTemperatureSensor} is responsible for the Alexa.TemperatureSensorInterface
+ * The {@link HandlerHumiditySensor} is responsible for the Alexa.HumiditySensorInterface
  *
- * @author Lukas Knoeller - Initial contribution
- * @author Michael Geramb - Initial contribution
+ * @author Daniel Campbell - Initial contribution
  */
 @NonNullByDefault
-public class HandlerTemperatureSensor extends HandlerBase {
+public class HandlerHumiditySensor extends HandlerBase {
     // Logger
-    private final Logger logger = LoggerFactory.getLogger(HandlerTemperatureSensor.class);
+    private final Logger logger = LoggerFactory.getLogger(HandlerHumiditySensor.class);
     // Interface
-    public static final String INTERFACE = "Alexa.TemperatureSensor";
+    public static final String INTERFACE = "Alexa.HumiditySensor";
     // Channel definitions
-    private static final ChannelInfo TEMPERATURE = new ChannelInfo("temperature" /* propertyName */ ,
-            "temperature" /* ChannelId */, CHANNEL_TYPE_TEMPERATURE /* Channel Type */ ,
-            ITEM_TYPE_NUMBER_TEMPERATURE /* Item Type */);
+    private static final ChannelInfo HUMIDITY = new ChannelInfo("relativeHumidity" /* propertyName */ ,
+            "relativeHumidity" /* ChannelId */, CHANNEL_TYPE_HUMIDITY /* Channel Type */ ,
+            ITEM_TYPE_HUMIDITY /* Item Type */);
 
-    public HandlerTemperatureSensor(SmartHomeDeviceHandler smartHomeDeviceHandler) {
+    public HandlerHumiditySensor(SmartHomeDeviceHandler smartHomeDeviceHandler) {
         super(smartHomeDeviceHandler);
     }
 
@@ -65,32 +64,27 @@ public class HandlerTemperatureSensor extends HandlerBase {
 
     @Override
     protected ChannelInfo @Nullable [] findChannelInfos(SmartHomeCapability capability, String property) {
-        if (TEMPERATURE.propertyName.equals(property)) {
-            return new ChannelInfo[] { TEMPERATURE };
+        if (HUMIDITY.propertyName.equals(property)) {
+            return new ChannelInfo[] { HUMIDITY };
         }
         return null;
     }
 
     @Override
     public void updateChannels(String interfaceName, List<JsonObject> stateList, UpdateChannelResult result) {
-        QuantityType<Temperature> temperatureValue = null;
         for (JsonObject state : stateList) {
+            State humidityValue = null;
             logger.debug("Updating {} with state: {}", interfaceName, state.toString());
-            if (TEMPERATURE.propertyName.equals(state.get("name").getAsString())) {
-                JsonObject value = state.get("value").getAsJsonObject();
+            if (HUMIDITY.propertyName.equals(state.get("name").getAsString())) {
                 // For groups take the first
-                if (temperatureValue == null) {
-                    float temperature = value.get("value").getAsFloat();
-                    String scale = value.get("scale").getAsString();
-                    if ("CELSIUS".equals(scale)) {
-                        temperatureValue = new QuantityType<Temperature>(temperature, SIUnits.CELSIUS);
-                    } else {
-                        temperatureValue = new QuantityType<Temperature>(temperature, ImperialUnits.FAHRENHEIT);
-                    }
-                }
+                humidityValue = getQuantityTypeState(state.get("value").getAsInt(), PERCENT);
+                updateState(HUMIDITY.channelId, humidityValue == null ? UnDefType.UNDEF : humidityValue);
             }
         }
-        updateState(TEMPERATURE.channelId, temperatureValue == null ? UnDefType.UNDEF : temperatureValue);
+    }
+
+    protected State getQuantityTypeState(@Nullable Number value, Unit<?> unit) {
+        return (value == null) ? UnDefType.UNDEF : new QuantityType<>(value, unit);
     }
 
     @Override
