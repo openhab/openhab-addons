f94e614a301d3e33792895fc53464a9a067b6f46
mesetka <83845942+mesetka@users.noreply.github.com>
2022-08-24 13:58:44 +0300

    Revert "Ipcamera"

--- bundles/org.openhab.binding.ipcamera/src/main/java/org/openhab/binding/ipcamera/internal/handler/IpCameraHandler.java
+++ bundles/org.openhab.binding.ipcamera/src/main/java/org/openhab/binding/ipcamera/internal/handler/IpCameraHandler.java
@@ -842,7 +842,6 @@ public class IpCameraHandler extends BaseThingHandler {
                 ffmpegRecord = new Ffmpeg(this, format, cameraConfig.getFfmpegLocation(), inputOptions, rtspUri,
                         cameraConfig.getMp4OutOptions(), cameraConfig.getFfmpegOutput() + mp4Filename + ".mp4",
                         cameraConfig.getUser(), cameraConfig.getPassword());
-                logger.debug("ffmpegRecord output {}", ffmpegRecord);
                 Ffmpeg localRecord = ffmpegRecord;
                 if (localRecord != null) {
                     localRecord.startConverting();
@@ -1590,8 +1589,7 @@ public class IpCameraHandler extends BaseThingHandler {
             case AMCREST_THING:
             case DAHUA_THING:
                 if (mjpegUri.isEmpty()) {
-                    mjpegUri = "/cgi-bin/mjpg/video.cgi?channel=" + cameraConfig.getNvrChannel() + "&subtype="
-                            + cameraConfig.getNvrChannelSubtype();
+                    mjpegUri = "/cgi-bin/mjpg/video.cgi?channel=" + cameraConfig.getNvrChannel() + "&subtype=1";
                 }
                 if (snapshotUri.isEmpty()) {
                     snapshotUri = "/cgi-bin/snapshot.cgi?channel=" + cameraConfig.getNvrChannel();
