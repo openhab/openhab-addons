1f1de41bd3d7ccf50202aa6a5cf0260fecc5596f
alexf2015 <alexf2015@users.noreply.github.com>
2022-09-10 16:57:21 +0200

    [Easee] Initial contribution (#12954)
    * initial binding version
    
    Signed-off-by: Alexander Friese <af944580@gmail.com>

---
+++ bundles/org.openhab.binding.easee/src/main/java/org/openhab/binding/easee/internal/EaseeHandlerFactory.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.easee.internal;
+
+import static org.openhab.binding.easee.internal.EaseeBindingConstants.*;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jetty.client.HttpClient;
+import org.openhab.binding.easee.internal.handler.EaseeChargerHandler;
+import org.openhab.binding.easee.internal.handler.EaseeMasterChargerHandler;
+import org.openhab.binding.easee.internal.handler.EaseeSiteHandler;
+import org.openhab.core.io.net.http.HttpClientFactory;
+import org.openhab.core.thing.Bridge;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.thing.ThingTypeUID;
+import org.openhab.core.thing.binding.BaseThingHandlerFactory;
+import org.openhab.core.thing.binding.ThingHandler;
+import org.openhab.core.thing.binding.ThingHandlerFactory;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link EaseeHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author Alexander Friese - Initial contribution
+ */
+@NonNullByDefault
+@Component(configurationPid = "binding.easee", service = ThingHandlerFactory.class)
+public class EaseeHandlerFactory extends BaseThingHandlerFactory {
+
+    private final Logger logger = LoggerFactory.getLogger(EaseeHandlerFactory.class);
+
+    /**
+     * the shared http client
+     */
+    private final HttpClient httpClient;
+
+    @Activate
+    public EaseeHandlerFactory(final @Reference HttpClientFactory httpClientFactory) {
+        this.httpClient = httpClientFactory.getCommonHttpClient();
+    }
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
+
+    @Override
+    protected @Nullable ThingHandler createHandler(Thing thing) {
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (THING_TYPE_SITE.equals(thingTypeUID)) {
+            return new EaseeSiteHandler((Bridge) thing, httpClient);
+        } else if (THING_TYPE_MASTER_CHARGER.equals(thingTypeUID)) {
+            return new EaseeMasterChargerHandler(thing);
+        } else if (THING_TYPE_CHARGER.equals(thingTypeUID)) {
+            return new EaseeChargerHandler(thing);
+        } else {
+            logger.warn("Unsupported Thing-Type: {}", thingTypeUID.getAsString());
+        }
+
+        return null;
+    }
+}
