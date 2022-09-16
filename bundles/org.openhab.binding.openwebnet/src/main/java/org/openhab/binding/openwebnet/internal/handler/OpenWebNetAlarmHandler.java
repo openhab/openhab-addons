6843d1a94e2800be91e96ad194ca6624b8900627
Massimo Valla <mvcode00@gmail.com>
2022-09-16 19:16:35 +0200

    [openwebnet] fix text formatting
    Signed-off-by: Massimo Valla <mvcode00@gmail.com>

--- bundles/org.openhab.binding.openwebnet/src/main/java/org/openhab/binding/openwebnet/internal/handler/OpenWebNetAlarmHandler.java
+++ bundles/org.openhab.binding.openwebnet/src/main/java/org/openhab/binding/openwebnet/internal/handler/OpenWebNetAlarmHandler.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2021 Contributors to the openHAB project
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -134,12 +134,10 @@ public class OpenWebNetAlarmHandler extends OpenWebNetThingHandler {
             default:
                 break;
         }
-
     }
 
     private void updateZone(Alarm msg) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
