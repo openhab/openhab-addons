487811fdfd59ec594464ab90153c900380da49c8
Wouter Born <github@maindrain.net>
2022-09-21 12:19:19 +0200

    Update Shelly and Tradfri bindings for upgrade to Californium 2.7.3 (#13407)
    * Update Shelly and Tradfri bindings for upgrade to Californium 2.7.3
    
    * Fixes deprecated API usages
    * Resolves itest runbundles
    
    Signed-off-by: Wouter Born <github@maindrain.net>

--- bundles/org.openhab.binding.tradfri/src/main/java/org/openhab/binding/tradfri/internal/handler/TradfriGatewayHandler.java
+++ bundles/org.openhab.binding.tradfri/src/main/java/org/openhab/binding/tradfri/internal/handler/TradfriGatewayHandler.java
@@ -32,7 +32,7 @@ import org.eclipse.californium.core.network.CoapEndpoint;
 import org.eclipse.californium.elements.exception.ConnectorException;
 import org.eclipse.californium.scandium.DTLSConnector;
 import org.eclipse.californium.scandium.config.DtlsConnectorConfig;
-import org.eclipse.californium.scandium.dtls.pskstore.StaticPskStore;
+import org.eclipse.californium.scandium.dtls.pskstore.AdvancedSinglePskStore;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.binding.tradfri.internal.CoapCallback;
@@ -156,7 +156,8 @@ public class TradfriGatewayHandler extends BaseBridgeHandler implements CoapCall
         }
 
         DtlsConnectorConfig.Builder builder = new DtlsConnectorConfig.Builder();
-        builder.setPskStore(new StaticPskStore(configuration.identity, configuration.preSharedKey.getBytes()));
+        builder.setAdvancedPskStore(
+                new AdvancedSinglePskStore(configuration.identity, configuration.preSharedKey.getBytes()));
         builder.setMaxConnections(100);
         builder.setStaleConnectionThreshold(60);
         dtlsConnector = new DTLSConnector(builder.build());
@@ -186,7 +187,7 @@ public class TradfriGatewayHandler extends BaseBridgeHandler implements CoapCall
         String responseText = null;
         try {
             DtlsConnectorConfig.Builder builder = new DtlsConnectorConfig.Builder();
-            builder.setPskStore(new StaticPskStore("Client_identity", configuration.code.getBytes()));
+            builder.setAdvancedPskStore(new AdvancedSinglePskStore("Client_identity", configuration.code.getBytes()));
 
             DTLSConnector dtlsConnector = new DTLSConnector(builder.build());
             CoapEndpoint.Builder authEndpointBuilder = new CoapEndpoint.Builder();
