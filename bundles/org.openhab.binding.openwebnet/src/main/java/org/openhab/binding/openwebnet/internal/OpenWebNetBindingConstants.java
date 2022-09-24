0def4d46550d1ba99e5448bc7d43dcac9fb319c1
Massimo Valla <mvcode00@gmail.com>
2022-09-24 17:51:44 +0200

    [openwebnet] formatting
    Signed-off-by: Massimo Valla <mvcode00@gmail.com>

--- bundles/org.openhab.binding.openwebnet/src/main/java/org/openhab/binding/openwebnet/internal/OpenWebNetBindingConstants.java
+++ bundles/org.openhab.binding.openwebnet/src/main/java/org/openhab/binding/openwebnet/internal/OpenWebNetBindingConstants.java
@@ -173,5 +173,4 @@ public class OpenWebNetBindingConstants {
     public static final String PROPERTY_FIRMWARE_VERSION = "firmwareVersion";
     public static final String PROPERTY_MODEL = "model";
     public static final String PROPERTY_SERIAL_NO = "serialNumber";
-
 }
