2c316c539beb2648aa5fdbc13a152a65e9ca96b1
Michael Barker <mikeb01@gmail.com>
2022-09-24 12:35:00 +1200

    Remove unused files.
    Signed-off-by: Michael Barker <mikeb01@gmail.com>

--- bundles/org.openhab.binding.echonetlite/fb.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mvn spotless:apply -pl :org.openhab.binding.echonetlite && mvn clean install -pl :org.openhab.binding.echonetlite && cp ./org.openhab.binding.echonetlite/target/org.openhab.binding.echonetlite-3.3.0-SNAPSHOT.jar ~/opt/openhab/addons/.
