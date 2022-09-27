015a370392a49fc43c02564b739f906e0d777150
Jamie Townsend <jamie_townsend@hotmail.com>
2022-09-27 07:51:10 +0200

    [solarmax] Initial contribution (#10414)
    * SolarMax Binding Initial implementation
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * #10413 camelCaserizeTheChannelNames
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * #10413 Delete commented code and Refactor Brute Force Command Discovery into something commitable
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * 10413 Delete commented code and Refactor Brute Force Command Discovery into something commitable
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * #10413 Codestyle
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * 10413 Codestyle
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * #10413 corrected sat-plugin errors
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * #10413 updates from code reviews in PR #10414
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * 10413 mvn spotless:apply
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * 10413 Updated to 3.2.0-SNAPSHOT
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * Fixed conflicts introduced by foreign commit.
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * Updated copyright years
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * Ran  mvn spotless:apply to resolve formatting issues
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * Updates from review
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * Switch to using Units & move softwareVersion & buildNumber to properties
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * A couple of review related updates
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * A couple more review related changes.
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * Added Full Example to README.md
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * Update parent pom.xml version
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    
    * Update bundles/org.openhab.binding.solarmax/src/main/java/org/openhab/binding/solarmax/internal/SolarMaxHandlerFactory.java
    
    Signed-off-by: Fabian Wolter <github@fabian-wolter.de>
    
    * Update bundles/org.openhab.binding.solarmax/src/main/java/org/openhab/binding/solarmax/internal/SolarMaxHandlerFactory.java
    
    Signed-off-by: Fabian Wolter <github@fabian-wolter.de>
    
    Signed-off-by: Jamie Townsend <jamie_townsend@hotmail.com>
    Signed-off-by: Fabian Wolter <github@fabian-wolter.de>
    Co-authored-by: Fabian Wolter <github@fabian-wolter.de>

---
+++ bundles/org.openhab.binding.solarmax/src/main/java/org/openhab/binding/solarmax/internal/connector/SolarMaxException.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.solarmax.internal.connector;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link SolarMaxException} Exception is used for general exceptions related to communications with the SolarMax
+ * device.
+ *
+ * @author Jamie Townsend - Initial contribution
+ */
+@NonNullByDefault
+public class SolarMaxException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public SolarMaxException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public SolarMaxException(final Throwable cause) {
+        super(cause);
+    }
+
+    public SolarMaxException(final String message) {
+        super(message);
+    }
+}
