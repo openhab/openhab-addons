88e01894a55d426729e54df595009c9f1ed16fee
Alexander Friese <af944580@googlemail.com>
2022-10-01 19:52:56 +0200

    reworked fix according to review
    Signed-off-by: Alexander Friese <af944580@googlemail.com>

--- bundles/org.openhab.binding.easee/src/main/java/org/openhab/binding/easee/internal/connector/WebInterface.java
+++ bundles/org.openhab.binding.easee/src/main/java/org/openhab/binding/easee/internal/connector/WebInterface.java
@@ -223,7 +223,7 @@ public class WebInterface implements AtomicReferenceTrait {
             Instant now = Instant.now();
 
             if (tokenExpiry == OUTDATED_DATE || tokenRefreshDate == OUTDATED_DATE
-                    || now.isAfter(tokenExpiry.minus(WEB_REQUEST_TOKEN_EXPIRY_BUFFER_MINUTES, ChronoUnit.MINUTES))
+                    || now.plus(WEB_REQUEST_TOKEN_EXPIRY_BUFFER_MINUTES, ChronoUnit.MINUTES).isAfter(tokenExpiry)
                     || now.isAfter(tokenRefreshDate.plus(WEB_REQUEST_TOKEN_MAX_AGE_MINUTES, ChronoUnit.MINUTES))) {
                 logger.debug("access token needs to be refreshed, last refresh: {}, expiry: {}",
                         Utils.formatDate(tokenRefreshDate), Utils.formatDate(tokenExpiry));
