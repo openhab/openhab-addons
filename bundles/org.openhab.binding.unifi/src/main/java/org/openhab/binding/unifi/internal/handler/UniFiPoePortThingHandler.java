0560a0e333c3d7a1c26c2c4c9dfb658d35b03d11
Hilbrand Bouwkamp <hilbrand@h72.nl>
2022-10-03 13:02:46 +0200

    [unifi] Detect all PoE ports, and set PoE thing offline if no data could be found (#13455)
    * [unifi] Set PoE thing offline if no data could be found
    
    This would better reflect the PoE thing status if there is a problem with either the data from the api or a configuration problem (like invalid port number).
    
    * [unifi] Fix bug to detect PoE ports when first port is not PoE port
    
    The binding assumed either all ports or no ports were PoE, and asssumed if port 0 was not PoE none was PoE.
    However, some switches have ports starting at port 5 to be PoE. Therefor changed code to just test each port if it is PoE.
    
    Signed-off-by: Hilbrand Bouwkamp <hilbrand@h72.nl>

--- bundles/org.openhab.binding.unifi/src/main/java/org/openhab/binding/unifi/internal/handler/UniFiPoePortThingHandler.java
+++ bundles/org.openhab.binding.unifi/src/main/java/org/openhab/binding/unifi/internal/handler/UniFiPoePortThingHandler.java
@@ -98,12 +98,15 @@ public class UniFiPoePortThingHandler
 
     @Override
     protected State getChannelState(final Map<Integer, UniFiPortTuple> ports, final String channelId) {
-        final UniFiPortTable port = getPort(ports).getTable();
+        final UniFiPortTuple portTuple = getPort(ports);
+
+        if (portTuple == null) {
+            return setOfflineOnNoPoEPortData();
+        }
+        final UniFiPortTable port = portTuple.getTable();
 
         if (port == null) {
-            logger.debug("No PoE port for thing '{}' could be found in the data. Refresh ignored.",
-                    getThing().getUID());
-            return UnDefType.NULL;
+            return setOfflineOnNoPoEPortData();
         }
         final State state;
 
@@ -130,6 +133,14 @@ public class UniFiPoePortThingHandler
                 state = UnDefType.UNDEF;
         }
         return state;
+    }
+
+    private State setOfflineOnNoPoEPortData() {
+        if (getThing().getStatus() != ThingStatus.OFFLINE) {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
+                    "@text/error.thing.poe.offline.nodata_error");
+        }
+        return UnDefType.NULL;
     }
 
     private @Nullable UniFiPortTuple getPort(final Map<Integer, UniFiPortTuple> ports) {
