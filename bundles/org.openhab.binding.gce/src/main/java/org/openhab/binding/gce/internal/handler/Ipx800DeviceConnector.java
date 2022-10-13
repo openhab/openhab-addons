cf21bd86fad86a0bd25603df0924a0d27fd53e31
clinique <gael@lhopital.org>
2022-10-14 01:11:27 +0200

    [gce] Correcting non updated input channels
    Signed-off-by: clinique <gael@lhopital.org>

--- bundles/org.openhab.binding.gce/src/main/java/org/openhab/binding/gce/internal/handler/Ipx800DeviceConnector.java
+++ bundles/org.openhab.binding.gce/src/main/java/org/openhab/binding/gce/internal/handler/Ipx800DeviceConnector.java
@@ -110,7 +110,7 @@ public class Ipx800DeviceConnector extends Thread {
     /**
      * Stop the device thread
      */
-    public void destroyAndExit() {
+    public void dispose() {
         interrupt();
         disconnect();
     }
@@ -161,7 +161,7 @@ public class Ipx800DeviceConnector extends Thread {
         try {
             Thread.sleep(DEFAULT_RECONNECT_TIMEOUT_MS);
         } catch (InterruptedException e) {
-            destroyAndExit();
+            dispose();
         }
     }
 
