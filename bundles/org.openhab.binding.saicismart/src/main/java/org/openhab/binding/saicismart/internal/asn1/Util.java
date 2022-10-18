35fc39e90882898e34ccde7e33a605599048ad93
Markus Heberling <markus@heberling.net>
2022-10-18 18:01:50 +0200

    use ThreadLocalRandom
    Signed-off-by: Markus Heberling <markus@heberling.net>

--- bundles/org.openhab.binding.saicismart/src/main/java/org/openhab/binding/saicismart/internal/asn1/Util.java
+++ bundles/org.openhab.binding.saicismart/src/main/java/org/openhab/binding/saicismart/internal/asn1/Util.java
@@ -12,15 +12,18 @@
  */
 package org.openhab.binding.saicismart.internal.asn1;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  *
  * @author Markus Heberling - Initial contribution
  */
-public class Util {
+public final class Util {
+    private Util() {
+    }
+
     public static void fillReserved(byte[] reservedBytes) {
-        System.arraycopy(((new Random(System.currentTimeMillis())).nextLong() + "1111111111111111").getBytes(), 0,
-                reservedBytes, 0, 16);
+        System.arraycopy((ThreadLocalRandom.current().nextLong() + "1111111111111111").getBytes(), 0, reservedBytes, 0,
+                16);
     }
 }
