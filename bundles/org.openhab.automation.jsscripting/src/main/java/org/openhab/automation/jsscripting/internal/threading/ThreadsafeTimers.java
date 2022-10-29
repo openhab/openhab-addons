7b100eb126edd0fb16149c083769112e76ecc2c4
Florian Hotze <florianh_dev@icloud.com>
2022-10-30 00:37:50 +0200

    [jsscripting] Reimplement timers to conform standard JS
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- bundles/org.openhab.automation.jsscripting/src/main/java/org/openhab/automation/jsscripting/internal/threading/ThreadsafeTimers.java
+++ bundles/org.openhab.automation.jsscripting/src/main/java/org/openhab/automation/jsscripting/internal/threading/ThreadsafeTimers.java
@@ -13,125 +13,92 @@
 package org.openhab.automation.jsscripting.internal.threading;
 
 import java.time.ZonedDateTime;
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
 
-import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.core.model.script.ScriptServiceUtil;
-import org.openhab.core.model.script.actions.Timer;
 import org.openhab.core.scheduler.ScheduledCompletableFuture;
 import org.openhab.core.scheduler.Scheduler;
-import org.openhab.core.scheduler.SchedulerRunnable;
 
 /**
  * A replacement for the timer functionality of {@link org.openhab.core.model.script.actions.ScriptExecution
  * ScriptExecution} which controls multithreaded execution access to the single-threaded GraalJS contexts.
  *
  * @author Florian Hotze - Initial contribution
+ * @author Florian Hotze - Reimplementation to conform standard JS setTimeout and setInterval
  */
 public class ThreadsafeTimers {
     private final Object lock;
+    private final Scheduler scheduler;
+    // Mapping of positive, non-zero integer values (used as timeoutID or intervalID) and the Scheduler
+    private final HashMap<Integer, ScheduledCompletableFuture<Object>> idSchedulerMapping = new HashMap<>();
 
     public ThreadsafeTimers(Object lock) {
         this.lock = lock;
+        this.scheduler = ScriptServiceUtil.getScheduler();
     }
 
-    public Timer createTimer(ZonedDateTime instant, Runnable callable) {
-        return createTimer(null, instant, callable);
-    }
-
-    public Timer createTimer(@Nullable String identifier, ZonedDateTime instant, Runnable callable) {
-        Scheduler scheduler = ScriptServiceUtil.getScheduler();
-
-        return new TimerImpl(scheduler, instant, () -> {
+    private ScheduledCompletableFuture<Object> createFuture(ZonedDateTime zdt, Runnable callback) {
+        return scheduler.schedule(() -> {
             synchronized (lock) {
-                callable.run();
+                callback.run();
             }
-
-        }, identifier);
+        }, zdt.toInstant());
     }
 
-    public Timer createTimerWithArgument(ZonedDateTime instant, Object arg1, Runnable callable) {
-        return createTimerWithArgument(null, instant, arg1, callable);
+    private Integer createNewId() {
+        Integer id = idSchedulerMapping.size() + 1;
+        while (idSchedulerMapping.get(id) != null)
+            id++;
+        return id;
     }
 
-    public Timer createTimerWithArgument(@Nullable String identifier, ZonedDateTime instant, Object arg1,
-            Runnable callable) {
-        Scheduler scheduler = ScriptServiceUtil.getScheduler();
-        return new TimerImpl(scheduler, instant, () -> {
+    public Integer setTimeout(Runnable callback, Long delay) {
+        return setTimeout(callback, delay, null);
+    }
+
+    public Integer setTimeout(Runnable callback, Long delay, Object... args) {
+        Integer id = createNewId();
+        ScheduledCompletableFuture<Object> future = createFuture(ZonedDateTime.now().plusNanos(delay * 1000000),
+                callback);
+        idSchedulerMapping.put(id, future);
+        return id;
+    }
+
+    public void clearTimeout(Integer id) {
+        ScheduledCompletableFuture<Object> scheduled = idSchedulerMapping.get(id);
+        scheduled.cancel(true);
+        idSchedulerMapping.remove(id);
+    }
+
+    private void createLoopingFuture(Integer id, Long delay, Runnable callback) {
+        ScheduledCompletableFuture<Object> future = scheduler.schedule(() -> {
             synchronized (lock) {
-                callable.run();
+                callback.run();
+                if (idSchedulerMapping.get(id) != null)
+                    createLoopingFuture(id, delay, callback);
             }
-
-        }, identifier);
+        }, ZonedDateTime.now().plusNanos(delay * 1000000).toInstant());
+        idSchedulerMapping.put(id, future);
     }
 
-    /**
-     * This is an implementation of the {@link Timer} interface.
-     * Copy of {@link org.openhab.core.model.script.internal.actions.TimerImpl} as this is not accessible from outside
-     * the
-     * package.
-     *
-     * @author Kai Kreuzer - Initial contribution
-     */
-    @NonNullByDefault
-    public static class TimerImpl implements Timer {
+    public Integer setInterval(Runnable callback, Long delay) {
+        return setInterval(callback, delay, null);
+    }
 
-        private final Scheduler scheduler;
-        private final ZonedDateTime startTime;
-        private final SchedulerRunnable runnable;
-        private final @Nullable String identifier;
-        private ScheduledCompletableFuture<?> future;
+    public Integer setInterval(Runnable callback, Long delay, Object... args) {
+        Integer id = createNewId();
+        createLoopingFuture(id, delay, callback);
+        return id;
+    }
 
-        public TimerImpl(Scheduler scheduler, ZonedDateTime startTime, SchedulerRunnable runnable) {
-            this(scheduler, startTime, runnable, null);
-        }
+    public void clearInterval(Integer id) {
+        clearTimeout(id);
+    }
 
-        public TimerImpl(Scheduler scheduler, ZonedDateTime startTime, SchedulerRunnable runnable,
-                @Nullable String identifier) {
-            this.scheduler = scheduler;
-            this.startTime = startTime;
-            this.runnable = runnable;
-            this.identifier = identifier;
-
-            future = scheduler.schedule(runnable, identifier, startTime.toInstant());
-        }
-
-        @Override
-        public boolean cancel() {
-            return future.cancel(true);
-        }
-
-        @Override
-        public synchronized boolean reschedule(ZonedDateTime newTime) {
-            future.cancel(false);
-            future = scheduler.schedule(runnable, identifier, newTime.toInstant());
-            return true;
-        }
-
-        @Override
-        public @Nullable ZonedDateTime getExecutionTime() {
-            return future.isCancelled() ? null : ZonedDateTime.now().plusNanos(future.getDelay(TimeUnit.NANOSECONDS));
-        }
-
-        @Override
-        public boolean isActive() {
-            return !future.isDone();
-        }
-
-        @Override
-        public boolean isCancelled() {
-            return future.isCancelled();
-        }
-
-        @Override
-        public boolean isRunning() {
-            return isActive() && ZonedDateTime.now().isAfter(startTime);
-        }
-
-        @Override
-        public boolean hasTerminated() {
-            return future.isDone();
-        }
+    public void cancelAll() {
+        idSchedulerMapping.forEach((id, future) -> {
+            future.cancel(true);
+        });
+        idSchedulerMapping.clear();
     }
 }
