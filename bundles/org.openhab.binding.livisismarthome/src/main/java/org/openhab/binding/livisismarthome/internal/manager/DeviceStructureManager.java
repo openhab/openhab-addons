794b9cc1abc2ad81e13ce71c0962bcd31e751c86
Leo Siepel <leosiepel@gmail.com>
2022-12-09 09:18:15 +0100

    Fix build error
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.livisismarthome/src/main/java/org/openhab/binding/livisismarthome/internal/manager/DeviceStructureManager.java
+++ bundles/org.openhab.binding.livisismarthome/src/main/java/org/openhab/binding/livisismarthome/internal/manager/DeviceStructureManager.java
@@ -15,7 +15,6 @@ package org.openhab.binding.livisismarthome.internal.manager;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
