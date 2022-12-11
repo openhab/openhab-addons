e39b55cb9159d48c4870908af5c6e65ea590313b
morph166955 <rosenblumb@gmail.com>
2022-12-11 17:10:07 -0600

    Added protocol folder
    Signed-off-by: Ben Rosenblum <rosenblumb@gmail.com>

---
+++ bundles/org.openhab.binding.shieldtv/src/main/java/org/openhab/binding/shieldtv/internal/protocol/shieldtv/ShieldTVMessageParser.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.shieldtv.internal.protocol.shieldtv;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class responsible for parsing incoming ShieldTV messages. Calls back to an object implementing the
+ * ShieldTVMessageParserCallbacks interface.
+ *
+ * Adapted from Lutron Leap binding
+ *
+ * @author Ben Rosenblum - Initial contribution
+ */
+
+@NonNullByDefault
+public class ShieldTVMessageParser {
+    private final Logger logger = LoggerFactory.getLogger(ShieldTVMessageParser.class);
+
+    public void handleMessage(String msg) {
+        return;
+    }
+}
