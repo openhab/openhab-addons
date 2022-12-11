6069097796eec2f086e29a38b54227b9b2b08a51
Jørgen Austvik <jaustvik@acm.org>
2022-12-11 15:52:28 +0100

    Fix windows build
    Signed-off-by: Jørgen Austvik <jaustvik@acm.org>

--- bundles/org.openhab.binding.nanoleaf/src/test/java/org/openhab/binding/nanoleaf/internal/layout/NanoleafLayoutTest.java
+++ bundles/org.openhab.binding.nanoleaf/src/test/java/org/openhab/binding/nanoleaf/internal/layout/NanoleafLayoutTest.java
@@ -18,10 +18,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.attribute.FileAttribute;
-import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.PosixFilePermissions;
-import java.util.Set;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
@@ -65,14 +61,11 @@ public class NanoleafLayoutTest {
         assertNotNull(result, "Should be able to render the layout: " + fileName);
         assertTrue(result.length > 0, "Should get content back, but got " + result.length + "bytes");
 
-        Set<PosixFilePermission> permissions = PosixFilePermissions.fromString("rw-r--r--");
-        FileAttribute<Set<PosixFilePermission>> attributes = PosixFilePermissions.asFileAttribute(permissions);
-        Path outFile = Files.createTempFile(temporaryDirectory, fileName.replace(".json", ""), ".png", attributes);
+        Path outFile = Files.createTempFile(temporaryDirectory, fileName.replace(".json", ""), ".png");
         Files.write(outFile, result);
 
         // For inspecting images on own computer
-        // Path permanentOutFile = Files.createFile(Path.of("/tmp", fileName.replace(".json", "") + ".png"),
-        // attributes);
+        // Path permanentOutFile = Files.createFile(Path.of("/tmp", fileName.replace(".json", "") + ".png"));
         // Files.write(permanentOutFile, result);
     }
 
