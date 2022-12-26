83fbbafa09b14372297d3b45d673a5af3c516104
lsiepel <leosiepel@gmail.com>
2022-12-26 22:53:40 +0100

    Final pass for changing generic packages/folder
    Signed-off-by: lsiepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.enocean/src/main/java/org/openhab/binding/enocean/internal/eep/generic/GenericVLD.java
+++ bundles/org.openhab.binding.enocean/src/main/java/org/openhab/binding/enocean/internal/eep/generic/GenericVLD.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.enocean.internal.eep.temp_generic;
+package org.openhab.binding.enocean.internal.eep.generic;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.openhab.binding.enocean.internal.messages.ERP1Message;
