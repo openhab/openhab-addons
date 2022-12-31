71ef3d9f4efe5b0e4def7271bbe08ee20975dbc1
lsiepel <leosiepel@gmail.com>
2022-12-31 15:04:06 +0100

    [astro] Fix returning wrong sun phase name (#14078)
    * Add tests and fix very minor bug
    * Correct wrong test
    * Update tests and fix sorting
    * Some checkstyle improvements
    
    Signed-off-by: lsiepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.astro/src/main/java/org/openhab/binding/astro/internal/model/MoonPhase.java
+++ bundles/org.openhab.binding.astro/src/main/java/org/openhab/binding/astro/internal/model/MoonPhase.java
@@ -31,7 +31,7 @@ public class MoonPhase {
     private Calendar firstQuarter;
     private Calendar full;
     private Calendar thirdQuarter;
-    private Calendar _new;
+    private Calendar newCalendar;
     private double age;
     private double illumination;
     private double agePercent;
@@ -85,14 +85,14 @@ public class MoonPhase {
      * Returns the date of the new moon.
      */
     public Calendar getNew() {
-        return _new;
+        return newCalendar;
     }
 
     /**
      * Sets the date of the new moon.
      */
-    public void setNew(Calendar _new) {
-        this._new = _new;
+    public void setNew(Calendar newCalendar) {
+        this.newCalendar = newCalendar;
     }
 
     /**
