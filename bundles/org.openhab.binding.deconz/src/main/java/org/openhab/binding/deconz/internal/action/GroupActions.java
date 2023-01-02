4bf75b55f10b7214767ebf2f1da3da47a2f56f7a
Jacob Laursen <jacob-github@vindvejr.dk>
2023-01-02 02:04:47 +0100

    Add SmartHome/J copyright headers
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>

--- bundles/org.openhab.binding.deconz/src/main/java/org/openhab/binding/deconz/internal/action/GroupActions.java
+++ bundles/org.openhab.binding.deconz/src/main/java/org/openhab/binding/deconz/internal/action/GroupActions.java
@@ -10,6 +10,19 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
+
+/**
+ * Copyright (c) 2021-2022 Contributors to the SmartHome/J project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.deconz.internal.action;
 
 import java.lang.reflect.Type;
