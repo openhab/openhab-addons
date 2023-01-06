d5dd3c9ea3a2ca609707d7e9b3b51bebf9a48432
lolodomo <lg.hc@free.fr>
2023-01-06 16:28:38 +0100

    [sonos] Correct identification of tuneIn started from Alexa (#14164)
    Contained URL is now HTTPS instead of HTTP before.
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/org.openhab.binding.sonos/src/main/java/org/openhab/binding/sonos/internal/handler/ZonePlayerHandler.java
+++ bundles/org.openhab.binding.sonos/src/main/java/org/openhab/binding/sonos/internal/handler/ZonePlayerHandler.java
@@ -93,7 +93,7 @@ public class ZonePlayerHandler extends BaseThingHandler implements UpnpIOPartici
     private static final String RADIO_URI = "x-sonosapi-radio:";
     private static final String RADIO_MP3_URI = "x-rincon-mp3radio:";
     private static final String RADIOAPP_URI = "x-sonosapi-hls:radioapp_";
-    private static final String OPML_TUNE = "http://opml.radiotime.com/Tune.ashx";
+    private static final String OPML_TUNE = "://opml.radiotime.com/Tune.ashx";
     private static final String FILE_URI = "x-file-cifs:";
     private static final String SPDIF = ":spdif";
     private static final String TUNEIN_URI = "x-sonosapi-stream:s%s?sid=%s&flags=32";
