f719db2ab789035a9b94b086468934bb3a639869
Ben Rosenblum <rosenblumb@gmail.com>
2023-01-07 16:58:09 -0600

    Fixes copywrite.  Adds dispose().
    Signed-off-by: Ben Rosenblum <rosenblumb@gmail.com>

--- bundles/org.openhab.binding.shieldtv/src/main/java/org/openhab/binding/shieldtv/internal/ShieldTVHandler.java
+++ bundles/org.openhab.binding.shieldtv/src/main/java/org/openhab/binding/shieldtv/internal/ShieldTVHandler.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2022 Contributors to the openHAB project
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -786,5 +786,14 @@ public class ShieldTVHandler extends BaseThingHandler implements ShieldTVMessage
                 sendCommand(new ShieldTVCommand(message));
             }
         }
+    }
+
+    @Override
+    public void dispose() {
+        Future<?> asyncInitializeTask = this.asyncInitializeTask;
+        if (asyncInitializeTask != null && !asyncInitializeTask.isDone()) {
+            asyncInitializeTask.cancel(true); // Interrupt async init task if it isn't done yet
+        }
+        disconnect(true);
     }
 }
