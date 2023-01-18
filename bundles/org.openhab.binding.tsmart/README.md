b599b10d8e1cf550f8b203d43f53087460d2f62d
James Melville <jamesmelville@gmail.com>
2023-01-18 22:07:34 +0000

    [tsmart] Initial contribution
    Signed-off-by: James Melville <jamesmelville@gmail.com>

---
+++ bundles/org.openhab.binding.tsmart/README.md
@@ -0,0 +1,53 @@
+# Tesla T-Smart Binding
+
+This binding is designed to connect to the Tesla T-Smart Smart Immersion Heater / Thermostat device https://www.tsmart.co.uk/.
+
+The device allows monitoring and control over the temperature and heating of water in a water cylinder.
+
+The build is derived from the API docs shared here https://community.home-assistant.io/t/telsa-t-smart-immersion-heater/345853 and some inspiration from https://github.com/britkat1980/tsmart.
+
+## Supported Things
+
+This supports a single thing type, the Tesla T-Smart Device.
+
+## Discovery
+
+The binding will discover Tesla T-Smart devices on your local network using a UDP Broadcast packet.
+
+## Thing Configuration
+
+| Name            | Type    | Description                           | Default | Required | Advanced |
+|-----------------|---------|---------------------------------------|---------|----------|----------|
+| hostname        | text    | Hostname or IP address of the device  | N/A     | yes      | no       |
+| refreshInterval | integer | Interval the device is polled in sec. | 60      | no       | yes      |
+
+## Channels
+
+| Channel         | Type               | Read/Write | Description                                |
+|-----------------|--------------------|------------|--------------------------------------------|
+| `power`           | Switch             | RW         | Power for the thermostat                   |
+| `mode`            | String             | RW         | Operating mode of the thermostat           |
+| `setpoint`        | Number:Temperature | RW         | Setpoint of the hot water temperature      |
+| `temperature`     | Number:Temperature | R          | Measured temperature of the water cylinder |
+| `temperatureHigh` | Number:Temperature | R          | Temperature of the high thermostat         |
+| `temperatureLow`  | Number:Temperature | R          | Temperature of the low thermostat          |
+| `relay`           | Switch             | R          | Relay state of the immersion heater        |
+| `smartState`           | String             | R          | State of the thermostat's Smart Mode     |
+
+## Full Example
+
+### .things file
+
+```java
+Thing tSmart:tSmart:heater  "Immersion Heater" [hostname="192.168.0.100"]
+```
+
+### .items file
+
+```java
+Switch             TSmart_Power       "Immersion Heater Power"       {channel="tSmart:tSmart:heater:power"}
+String             TSmart_Mode        "Immersion Heater Mode"        {channel="tSmart:tSmart:heater:mode"}
+Number:Temperature TSmart_Setpoint    "Immersion Heater Setpoint"    {channel="tSmart:tSmart:heater:setpoint"}
+Number:Temperature TSmart_Temperature "Hot Water Temperature"        {channel="tSmart:tSmart:heater:temperature"}
+Switch             TSmart_Relay       "Immersion Heater Relay State" {channel="tSmart:tSmart:heater:power"}
+```
---
+++ bundles/org.openhab.binding.tsmart/src/main/java/org/openhab/binding/tsmart/internal/TSmartUDPListener.java
@@ -0,0 +1,142 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.tsmart.internal;
+
+import static org.openhab.binding.tsmart.internal.TSmartBindingConstants.T_SMART_PORT;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link TSmartUDPListener} is responsible for listening for UDP packets and
+ * routing the information to the correct ThingHandler or Discovery Service
+ *
+ * @author James Melville - Initial contribution
+ */
+@NonNullByDefault
+public class TSmartUDPListener implements Runnable {
+
+    private final Logger logger = LoggerFactory.getLogger(TSmartUDPListener.class);
+
+    private static @Nullable TSmartUDPListener listener;
+    private static @Nullable TSmartDiscoveryService discoService;
+    private volatile static Map<InetAddress, TSmartHandler> handlerAddressMap = new ConcurrentHashMap<InetAddress, TSmartHandler>();
+
+    private static void enableListener() {
+        if (listener == null) {
+            listener = new TSmartUDPListener();
+            Thread t = new Thread(listener);
+            t.start();
+        }
+    }
+
+    /**
+     * Maintain a mapping between a device address and the openHAB handler
+     * in order to route any received UDP packets to the correct handler.
+     * This method adds the address and ThingHandler to the map.
+     *
+     * @param addr Network address of device
+     * @param handler Instance of ThingHander
+     */
+    public static void addHandler(InetAddress addr, TSmartHandler handler) {
+        if (handlerAddressMap.containsKey(addr)) {
+            // throw error
+        } else {
+            handlerAddressMap.put(addr, handler);
+            enableListener();
+        }
+    }
+
+    /**
+     * Maintain a mapping between a device address and the openHAB handler
+     * in order to route any received UDP packets to the correct handler.
+     * This method removes the mapping.
+     *
+     * @param addr Network address of device
+     */
+    public static void removeHandler(InetAddress addr) {
+        if (handlerAddressMap.containsKey(addr)) {
+            handlerAddressMap.remove(addr);
+        }
+    }
+
+    /**
+     * Add the DiscoveryService to handle any UDP packets as responses
+     * to discovery broadcast packets.
+     *
+     * @param discoService Instance of the TSmartDiscoveryService
+     */
+    public static void startDiscovery(TSmartDiscoveryService discoService) {
+        TSmartUDPListener.discoService = discoService;
+        enableListener();
+    }
+
+    /**
+     * Stop listening for responses to discovery broadcast packets
+     */
+    public static void stopDiscovery() {
+        discoService = null;
+    }
+
+    /**
+     * Method to receive UDP packets and route them to the appropriate
+     * ThingHandler or DiscoveryService.
+     */
+    @Override
+    public void run() {
+
+        try {
+            DatagramSocket socket = new DatagramSocket(T_SMART_PORT);
+
+            while (isEnabled()) {
+
+                byte[] buffer = new byte[512];
+                DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+                socket.receive(response);
+                if (handlerAddressMap.containsKey(response.getAddress())) {
+
+                    TSmartHandler handler = handlerAddressMap.get(response.getAddress());
+
+                    if (buffer[0] == (byte) 0xF1) {
+                        handler.updateStatusHandler(buffer);
+                    }
+                } else if (discoService != null) {
+                    if (buffer[0] == (byte) 0x01 && buffer[3] != (byte) 0x54) {
+                        discoService.handleDiscoveryResponse(response.getAddress(), buffer);
+                    }
+                }
+            }
+            socket.close();
+            listener = null;
+            discoService = null;
+        } catch (SocketTimeoutException ex) {
+            logger.debug("Timeout error: {}", ex.getMessage());
+        } catch (IOException ex) {
+            logger.debug("Client error: {}", ex.getMessage());
+        }
+    }
+
+    private boolean isEnabled() {
+        return !handlerAddressMap.isEmpty() || discoService != null;
+    }
+}
