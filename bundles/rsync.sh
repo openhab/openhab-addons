6aed1e700fce2944b0773cce41b964fe5a46ebe8
Jeremy Rumpf <rumpf.99@gmail.com>
2023-01-19 17:09:19 -0500

    Remove accidental checkin of rsync.sh
    Signed-off-by: Jeremy Rumpf <rumpf.99@gmail.com>

--- bundles/rsync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# rsync -avz --exclude="internal/netty"  org.openhab.binding.dahuacamera root@fishernet:/root/openhab-devel/openhab-addons/bundles
-# rsync -avz --exclude="internal/netty"  org.openhab.binding.anpvizcamera root@fishernet:/root/openhab-devel/openhab-addons/bundles
- rsync -avz --exclude="internal/netty"  org.openhab.binding.gpio root@fishernet:/root/openhab-devel/openhab-addons/bundles
