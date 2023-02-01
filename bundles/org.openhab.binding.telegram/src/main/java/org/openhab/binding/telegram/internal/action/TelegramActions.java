openhab/openhab-addons

commit 1d23c32de1e3831644494751c961d17d10628ec8

Solves issue #11691 (#13758)
Signed-off-by: clinique <gael@lhopital.org>

bundles/org.openhab.binding.telegram/src/main/java/org/openhab/binding/telegram/internal/action/TelegramActions.java
@@ -237,7 +237,8 @@ public class TelegramActions implements ThingActions {
         }
         TelegramHandler localHandler = handler;
         if (localHandler != null) {
-            SendMessage sendMessage = new SendMessage(chatId, message);
+            String escapedMessage = message.replace("_", "\\_");
+            SendMessage sendMessage = new SendMessage(chatId, escapedMessage);
             if (localHandler.getParseMode() != null) {
                 sendMessage.parseMode(localHandler.getParseMode());
             }

commit c1b2f76f9dcdf62364af661fb9b1a47b0b13923c

Commit 1d23c32de1e3831644494751c961d17d10628ec8 broke sending underscore character in messages (#14204)
This PR reverts commit 1d23c32de1e3831644494751c961d17d10628ec8 and fix #14204.

Signed-off-by: Daniel Schröter d.schroeter@gmx.de
Signed-off-by: Daniel Schröter <d.schroeter@gmx.de>

bundles/org.openhab.binding.telegram/src/main/java/org/openhab/binding/telegram/internal/action/TelegramActions.java
@@ -237,8 +237,7 @@ public class TelegramActions implements ThingActions {
         }
         TelegramHandler localHandler = handler;
         if (localHandler != null) {
-            String escapedMessage = message.replace("_", "\\_");
-            SendMessage sendMessage = new SendMessage(chatId, escapedMessage);
+            SendMessage sendMessage = new SendMessage(chatId, message);
             if (localHandler.getParseMode() != null) {
                 sendMessage.parseMode(localHandler.getParseMode());
             }