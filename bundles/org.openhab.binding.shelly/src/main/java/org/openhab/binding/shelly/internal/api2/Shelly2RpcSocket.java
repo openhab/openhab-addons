5e493a4463c87f1c03be9abfb07a3c5bb1bcb64c
Laurent Garnier <lg.hc@free.fr>
2023-02-05 09:48:23 +0100

    [shelly] Destroy the web socket client when disconnecting
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/api2/Shelly2RpcSocket.java
+++ bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/api2/Shelly2RpcSocket.java
@@ -212,6 +212,7 @@ public class Shelly2RpcSocket {
             if (client.isStarted()) {
                 client.stop();
             }
+            client.destroy();
         } catch (Exception e) {
             if (e.getCause() instanceof InterruptedException) {
                 logger.debug("{}: Unable to close socket - interrupted", thingName); // e.g. device was rebooted
