d3476842b46de82a3e02632423588f2b9fac35fc
Laurent ARNAL <laurent@clae.net>
2023-02-12 16:14:24 +0100

    remove logs
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.binding.siemenshvac/src/main/java/org/openhab/binding/siemenshvac/internal/network/SiemensHvacConnectorImpl.java
+++ bundles/org.openhab.binding.siemenshvac/src/main/java/org/openhab/binding/siemenshvac/internal/network/SiemensHvacConnectorImpl.java
@@ -238,7 +238,7 @@ public class SiemensHvacConnectorImpl implements SiemensHvacConnector {
 
         if (config.containsKey("baseUrl")) {
             baseUrl = (String) config.get("baseUrl");
-
+        }
 
         if (config.containsKey("userName")) {
             userName = (String) config.get("userName");
@@ -365,7 +365,7 @@ public class SiemensHvacConnectorImpl implements SiemensHvacConnector {
                 mUri = mUri + "SessionId=" + sessionId;
             }
 
-            logger.info("Execute request: {}", uri);
+            logger.debug("Execute request: {}", uri);
             CookieStore c = httpClient.getCookieStore();
             java.net.HttpCookie cookie = new HttpCookie("SessionId", sessionIdHttp);
             cookie.setPath("/");
