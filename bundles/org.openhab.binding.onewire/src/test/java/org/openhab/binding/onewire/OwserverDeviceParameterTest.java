a7217269d0fc7fa345c8c5fc3c4c28eb69b3d9a8
Jan N. Klug <github@klug.nrw>
2023-02-16 00:35:43 +0100

    [onewire] Code improvements and bug fixes
    Signed-off-by: Jan N. Klug <github@klug.nrw>

--- bundles/org.openhab.binding.onewire/src/test/java/org/openhab/binding/onewire/OwserverDeviceParameterTest.java
+++ bundles/org.openhab.binding.onewire/src/test/java/org/openhab/binding/onewire/OwserverDeviceParameterTest.java
@@ -37,6 +37,7 @@ public class OwserverDeviceParameterTest {
         assertEquals("/1F.0123456789ab/main/00.1234567890ab/humidity", owserverDeviceParameter.getPath(sensorId));
     }
 
+    @Test
     public void withPrefixTest() {
         OwserverDeviceParameter owserverDeviceParameter = new OwserverDeviceParameter("uncached", "/humidity");
         assertEquals("/uncached/1F.0123456789ab/main/00.1234567890ab/humidity",
