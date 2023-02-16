35cb20d184ef0ba98d09634cec13b50d3da51895
Jan N. Klug <github@klug.nrw>
2023-02-16 16:13:07 +0100

    fixes
    Signed-off-by: Jan N. Klug <github@klug.nrw>

--- bundles/org.openhab.binding.onewire/src/test/java/org/openhab/binding/onewire/OwserverDeviceParameterTest.java
+++ bundles/org.openhab.binding.onewire/src/test/java/org/openhab/binding/onewire/OwserverDeviceParameterTest.java
@@ -40,23 +40,23 @@ public class OwserverDeviceParameterTest {
     @Test
     public void withPrefixTest() {
         OwserverDeviceParameter owserverDeviceParameter = new OwserverDeviceParameter("uncached", "/humidity");
-        assertEquals("/uncached/1F.0123456789ab/main/00.1234567890ab/humidity",
+        assertEquals("uncached/1F.0123456789ab/main/00.1234567890ab/humidity",
                 owserverDeviceParameter.getPath(sensorId));
 
         owserverDeviceParameter = new OwserverDeviceParameter("uncached", "/humidity");
-        assertEquals("/uncached/1F.0123456789ab/main/00.1234567890ab/humidity",
+        assertEquals("uncached/1F.0123456789ab/main/00.1234567890ab/humidity",
                 owserverDeviceParameter.getPath(sensorId));
 
         owserverDeviceParameter = new OwserverDeviceParameter("/uncached", "/humidity");
-        assertEquals("/uncached/1F.0123456789ab/main/00.1234567890ab/humidity",
+        assertEquals("uncached/1F.0123456789ab/main/00.1234567890ab/humidity",
                 owserverDeviceParameter.getPath(sensorId));
 
         owserverDeviceParameter = new OwserverDeviceParameter("/uncached/", "/humidity");
-        assertEquals("/uncached/1F.0123456789ab/main/00.1234567890ab/humidity",
+        assertEquals("uncached/1F.0123456789ab/main/00.1234567890ab/humidity",
                 owserverDeviceParameter.getPath(sensorId));
 
         owserverDeviceParameter = new OwserverDeviceParameter("uncached/", "/humidity");
-        assertEquals("/uncached/1F.0123456789ab/main/00.1234567890ab/humidity",
+        assertEquals("uncached/1F.0123456789ab/main/00.1234567890ab/humidity",
                 owserverDeviceParameter.getPath(sensorId));
     }
 }
