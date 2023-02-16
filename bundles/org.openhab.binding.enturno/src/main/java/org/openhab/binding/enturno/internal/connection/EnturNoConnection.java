bdd3e07853ba635059bce77cdab987d19fbd4939
lsiepel <leosiepel@gmail.com>
2023-02-16 21:59:16 +0100

    sat
    Signed-off-by: lsiepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.enturno/src/main/java/org/openhab/binding/enturno/internal/connection/EnturNoConnection.java
+++ bundles/org.openhab.binding.enturno/src/main/java/org/openhab/binding/enturno/internal/connection/EnturNoConnection.java
@@ -233,7 +233,7 @@ public class EnturNoConnection {
 
     private String getIsoDateTime(String dateTimeWithoutColonInZone) {
         String dateTime = dateTimeWithoutColonInZone.substring(0, dateTimeWithoutColonInZone.lastIndexOf("+"));
-        String offset = dateTimeWithoutColonInZone.substring(dateTimeWithoutColonInZone.lastIndexOf("+")+1);
+        String offset = dateTimeWithoutColonInZone.substring(dateTimeWithoutColonInZone.lastIndexOf("+") + 1);
 
         StringBuilder builder = new StringBuilder();
         return builder.append(dateTime).append("+").append(offset.substring(0, 2)).append(":00").toString();
