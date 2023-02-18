8ca2c0997aba162070a3199733d95d8b04eface1
lsiepel <leosiepel@gmail.com>
2023-02-18 18:15:27 +0100

    [digitalstrom] code refactoring (#14402)
    Signed-off-by: lsiepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.digitalstrom/src/main/java/org/openhab/binding/digitalstrom/internal/providers/BaseDsI18n.java
+++ bundles/org.openhab.binding.digitalstrom/src/main/java/org/openhab/binding/digitalstrom/internal/providers/BaseDsI18n.java
@@ -26,8 +26,8 @@ import org.osgi.service.component.ComponentContext;
  * digitalSTROM-Bindings. So this class can be implement e.g. by provider implementations like the
  * {@link org.openhab.core.thing.type.ChannelTypeProvider}.
  *
- * @author Michael Ochel - initial contributer
- * @author Matthias Siegele - initial contributer
+ * @author Michael Ochel - Initial contribution
+ * @author Matthias Siegele - Initial contribution
  */
 public abstract class BaseDsI18n {
 
