ce417086220a897a8e72d005287ec4b1ca585fa2
Kai Kreuzer <kai@openhab.org>
2023-02-18 15:01:41 +0100

    [lametrictime] Move external library to internal code (#14425)
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

---
+++ bundles/org.openhab.binding.lametrictime/src/main/java/org/openhab/binding/lametrictime/internal/api/local/LaMetricTimeException.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.lametrictime.internal.api.local;
+
+import java.util.List;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.binding.lametrictime.internal.api.local.dto.Error;
+import org.openhab.binding.lametrictime.internal.api.local.dto.Failure;
+
+/**
+ * Parent class for LaMetricTime exceptions.
+ *
+ * @author Gregory Moyer - Initial contribution
+ */
+@NonNullByDefault
+public class LaMetricTimeException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public LaMetricTimeException() {
+        super();
+    }
+
+    public LaMetricTimeException(String message) {
+        super(message);
+    }
+
+    public LaMetricTimeException(Throwable cause) {
+        super(cause);
+    }
+
+    public LaMetricTimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public LaMetricTimeException(String message, Throwable cause, boolean enableSuppression,
+            boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public LaMetricTimeException(Failure failure) {
+        super(buildMessage(failure));
+    }
+
+    private static String buildMessage(Failure failure) {
+        StringBuilder builder = new StringBuilder();
+
+        List<Error> errors = failure.getErrors();
+        if (!errors.isEmpty()) {
+            builder.append(errors.get(0).getMessage());
+        }
+
+        for (int i = 1; i < errors.size(); i++) {
+            builder.append("; ").append(errors.get(i).getMessage());
+        }
+
+        return builder.toString();
+    }
+}
