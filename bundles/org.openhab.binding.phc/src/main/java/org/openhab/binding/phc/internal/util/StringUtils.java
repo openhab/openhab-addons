555a6b7cc9c4e7b948b3491dab92dc1a7abb10c3
lsiepel <leosiepel@gmail.com>
2023-02-19 21:23:01 +0100

    NonNullByDefault
    Signed-off-by: lsiepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.phc/src/main/java/org/openhab/binding/phc/internal/util/StringUtils.java
+++ bundles/org.openhab.binding.phc/src/main/java/org/openhab/binding/phc/internal/util/StringUtils.java
@@ -12,6 +12,7 @@
  */
 package org.openhab.binding.phc.internal.util;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 
 /**
@@ -19,6 +20,7 @@ import org.eclipse.jdt.annotation.Nullable;
  *
  * @author Leo Siepel - Initial contribution
  */
+@NonNullByDefault
 public class StringUtils {
 
     public static String padLeft(@Nullable String input, int minSize, String padString) {
