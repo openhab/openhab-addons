94497e0a590b8dabe10ba48167dec9569e93577a
Simon Spielmann <simon.spielmann@gmx.de>
2023-03-09 08:27:38 +0100

    Handle HTTP 400 and codes >= 500 as errors
    Signed-off-by: Simon Spielmann <simon.spielmann@gmx.de>

--- bundles/org.openhab.binding.icloud/src/main/java/org/openhab/binding/icloud/internal/ICloudService.java
+++ bundles/org.openhab.binding.icloud/src/main/java/org/openhab/binding/icloud/internal/ICloudService.java
@@ -246,10 +246,11 @@ public class ICloudService {
             this.session.post(AUTH_ENDPOINT + "/verify/trusteddevice/securitycode", JsonUtils.toJson(requestBody),
                     headers);
         } catch (ICloudApiResponseException ex) {
+            this.logger.debug("Exception on code verification. Verification might still be successful.", ex);
             // iCloud API returns different 4xx error codes even if validation is successful
             // currently 400 seems to show that verification "really" failed.
-            if (ex.getStatusCode() == 400) {
-                this.logger.debug("Code verification failed.", ex);
+            if ((ex.getStatusCode() == 400 || ex.getStatusCode() >= 500)) {
+                this.logger.debug("Verification failed with HTTP status {}.", ex.getStatusCode());
                 return false;
             }
         }
