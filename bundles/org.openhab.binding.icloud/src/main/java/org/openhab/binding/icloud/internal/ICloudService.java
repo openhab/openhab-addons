4eb4c0b753f9adbb376a6cebc10dc0b0d8d74d1c
Simon Spielmann <simon.spielmann@gmx.de>
2023-03-10 21:19:43 +0100

    Remove debug log
    Signed-off-by: Simon Spielmann <simon.spielmann@gmx.de>

--- bundles/org.openhab.binding.icloud/src/main/java/org/openhab/binding/icloud/internal/ICloudService.java
+++ bundles/org.openhab.binding.icloud/src/main/java/org/openhab/binding/icloud/internal/ICloudService.java
@@ -246,8 +246,6 @@ public class ICloudService {
             this.session.post(AUTH_ENDPOINT + "/verify/trusteddevice/securitycode", JsonUtils.toJson(requestBody),
                     headers);
         } catch (ICloudApiResponseException ex) {
-            logger.debug("Exception on code verification with HTTP status {}. Verification might still be successful.",
-                    ex.getStatusCode());
             logger.trace("Exception on code verification with HTTP status {}. Verification might still be successful.",
                     ex.getStatusCode(), ex);
             // iCloud API returns different 4xx error codes even if validation is successful
