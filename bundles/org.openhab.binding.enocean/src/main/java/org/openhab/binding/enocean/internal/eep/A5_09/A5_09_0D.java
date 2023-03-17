7bd239459d4605632f6941892ced09e0c2b13f73
Zhivka Dimova <zhivka.dimova@myforest.net>
2023-03-17 22:27:30 +0100

    [enocean]add NonNullByDefault, regenerate i18n
    Seting category to Gas
    
    Signed-off-by: Zhivka Dimova <zhivka.dimova@myforest.net>

--- bundles/org.openhab.binding.enocean/src/main/java/org/openhab/binding/enocean/internal/eep/A5_09/A5_09_0D.java
+++ bundles/org.openhab.binding.enocean/src/main/java/org/openhab/binding/enocean/internal/eep/A5_09/A5_09_0D.java
@@ -16,6 +16,7 @@ import static org.openhab.binding.enocean.internal.EnOceanBindingConstants.*;
 
 import java.util.function.Function;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.openhab.binding.enocean.internal.eep.EEPHelper;
 import org.openhab.binding.enocean.internal.messages.ERP1Message;
 import org.openhab.core.config.core.Configuration;
@@ -27,6 +28,7 @@ import org.openhab.core.types.State;
  *
  * @author Zhivka Dimova - Initial contribution
  */
+@NonNullByDefault
 public class A5_09_0D extends A5_09 {
 
     public A5_09_0D(ERP1Message packet) {
