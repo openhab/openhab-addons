ed20ebb41b15c7b4fcae8d953e130ebc8aeb1773
David Pace <dev@davidpace.de>
2023-03-30 18:31:54 +0200

    [boschshc] Code Enhancements (#14667)
    * [boschshc] Code Enhancements
    
    This commit fixes several compiler warnings as well as several
    Checkstyle, FindBugs, PMD and Sonar issues.
    
    * instantiate loggers consistently
    * add missing logger calls
    * add chained exceptions to logger calls
    * provide central Gson instance that does not serialize or deserialize
    loggers
    * avoid catching NullPointerExceptions
    * extract methods where cyclomatic complexity was too high
    * remove unnecessary null check in combination with instanceof
    * rename local variables that shadow fields
    * add missing @NonNull and @Nullable annotations
    
    Signed-off-by: David Pace <dev@davidpace.de>

---
+++ bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/serialization/GsonUtils.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.boschshc.internal.serialization;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * Utilities for JSON serialization and deserialization using Google Gson.
+ *
+ * @author David Pace - Initial contribution
+ *
+ */
+@NonNullByDefault
+public final class GsonUtils {
+    private GsonUtils() {
+        // Utility Class
+    }
+
+    /**
+     * The default Gson instance to be used for serialization and deserialization.
+     * <p>
+     * This instance does not serialize or deserialize fields named <code>logger</code>.
+     */
+    public static final Gson DEFAULT_GSON_INSTANCE = new GsonBuilder()
+            .addSerializationExclusionStrategy(new LoggerExclusionStrategy())
+            .addDeserializationExclusionStrategy(new LoggerExclusionStrategy()).create();
+}
