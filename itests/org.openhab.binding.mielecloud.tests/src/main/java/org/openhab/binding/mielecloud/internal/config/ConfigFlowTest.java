29ff419e662bf34e1d1f8835a3a7b0213aa43407
Björn Lange <bjoern.lange@itemis.de>
2023-04-12 07:47:41 +0200

    Revert "Disable mielecloud tests (#14762)"
    This reverts commit 09c394b9284ba18486b065c54121edf5669174eb.
    
    Signed-off-by: Björn Lange <bjoern.lange@itemis.de>

--- itests/org.openhab.binding.mielecloud.tests/src/main/java/org/openhab/binding/mielecloud/internal/config/ConfigFlowTest.java
+++ itests/org.openhab.binding.mielecloud.tests/src/main/java/org/openhab/binding/mielecloud/internal/config/ConfigFlowTest.java
@@ -20,7 +20,6 @@ import static org.openhab.binding.mielecloud.internal.util.ReflectionUtil.setPri
 import java.util.Objects;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openhab.binding.mielecloud.internal.config.servlet.CreateBridgeServlet;
 import org.openhab.binding.mielecloud.internal.config.servlet.ForwardToLoginServlet;
@@ -104,7 +103,6 @@ public class ConfigFlowTest extends AbstractConfigFlowTest {
     }
 
     @Test
-    @Disabled
     public void configFlowHappyPathCreatesABridge() throws Exception {
         // given:
         setUpAuthorizationHandler();
@@ -122,7 +120,6 @@ public class ConfigFlowTest extends AbstractConfigFlowTest {
     }
 
     @Test
-    @Disabled
     public void configFlowWaitTimeoutExpiresWhenBridgeDoesNotComeOnline() throws Exception {
         // given:
         setUpAuthorizationHandler();
