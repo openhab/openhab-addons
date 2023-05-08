976a1368afc0f834d8fbadc23c4f5d6a107f5dc8
Christoph Pfeifer <Christoph.Pfeifer@web.de>
2023-05-08 23:02:24 +0200

    [groupepsa] Opel JSON response getting vehicle status changed
    Fixes #14853
    spotless code style
    
    Signed-off-by: Christoph Pfeifer <christoph.pfeifer@web.de>

--- bundles/org.openhab.binding.groupepsa/src/test/java/org/openhab/binding/groupepsa/internal/rest/api/dto/JsonResponseVehicleStatusTest.java
+++ bundles/org.openhab.binding.groupepsa/src/test/java/org/openhab/binding/groupepsa/internal/rest/api/dto/JsonResponseVehicleStatusTest.java
@@ -37,35 +37,35 @@ import com.google.gson.JsonParseException;
  */
 class JsonResponseVehicleStatusTest {
 
-	private static final String API_RESPONSE = "{\"createdAt\":\"2023-04-16T16:22:12Z\",\"updatedAt\":\"2023-04-16T16:22:12Z\",\"engines\":[{\"type\":\"Thermic\",\"extension\":{\"thermic\":{\"coolant\":{\"temp\":215},\"air\":{\"temp\":215}}},\"createdAt\":\"2023-01-03T13:40:01Z\"}],\"ignition\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"type\":\"Stop\"},\"battery\":{\"voltage\":88,\"createdAt\":\"2023-04-16T16:22:12Z\"},\"privacy\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"state\":\"None\"},\"service\":{\"createdAt\":\"2023-01-03T11:58:01Z\",\"type\":\"Electric\"},\"environment\":{\"luminosity\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"day\":true},\"air\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"temp\":12.5}},\"odometer\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"mileage\":2613.7},\"_links\":{\"self\":{\"href\":\"https://api.groupe-psa.com/connectedcar/v4/user/vehicles/1ef345/status?profile=endUser\"},\"vehicle\":{\"href\":\"https://api.groupe-psa.com/connectedcar/v4/user/vehicles/1ef345\"}},\"preconditioning\":{\"airConditioning\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"updatedAt\":\"2023-04-16T16:22:12Z\",\"status\":\"Disabled\",\"programs\":[{\"enabled\":false,\"slot\":1,\"recurrence\":\"Daily\",\"start\":\"PT0S\"},{\"enabled\":false,\"slot\":2,\"recurrence\":\"Daily\",\"start\":\"PT0S\"},{\"enabled\":false,\"slot\":3,\"recurrence\":\"Daily\",\"start\":\"PT0S\"},{\"enabled\":false,\"slot\":4,\"recurrence\":\"Daily\",\"start\":\"PT0S\"}]}},\"energies\":[{\"createdAt\":\"2023-01-03T13:40:01Z\",\"type\":\"Fuel\",\"subType\":\"FossilEnergy\",\"extension\":{\"fuel\":{\"consumptions\":{\"total\":0}}}},{\"createdAt\":\"2023-04-16T16:22:12Z\",\"type\":\"Electric\",\"subType\":\"ElectricEnergy\",\"level\":91,\"autonomy\":338,\"extension\":{\"electric\":{\"battery\":{\"load\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"capacity\":36608,\"residual\":13024}},\"charging\":{\"plugged\":false,\"status\":\"Disconnected\",\"chargingRate\":0,\"chargingMode\":\"No\",\"nextDelayedTime\":\"PT0S\"}}}}],\"preconditionning\":{\"airConditioning\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"updatedAt\":\"2023-04-16T16:22:12Z\",\"status\":\"Disabled\",\"programs\":[{\"enabled\":false,\"slot\":1,\"recurrence\":\"Daily\",\"start\":\"PT0S\"},{\"enabled\":false,\"slot\":2,\"recurrence\":\"Daily\",\"start\":\"PT0S\"},{\"enabled\":false,\"slot\":3,\"recurrence\":\"Daily\",\"start\":\"PT0S\"},{\"enabled\":false,\"slot\":4,\"recurrence\":\"Daily\",\"start\":\"PT0S\"}]}},\"energy\":[{\"createdAt\":\"2023-01-03T13:40:01Z\",\"updatedAt\":\"2023-01-03T13:40:01Z\",\"type\":\"Fuel\"},{\"createdAt\":\"2023-04-16T16:22:12Z\",\"updatedAt\":\"2023-04-16T16:22:12Z\",\"type\":\"Electric\",\"level\":91,\"autonomy\":338,\"charging\":{\"plugged\":false,\"status\":\"Disconnected\",\"chargingRate\":0,\"chargingMode\":\"No\",\"nextDelayedTime\":\"PT0S\"}}]}";
+    private static final String API_RESPONSE = "{\"createdAt\":\"2023-04-16T16:22:12Z\",\"updatedAt\":\"2023-04-16T16:22:12Z\",\"engines\":[{\"type\":\"Thermic\",\"extension\":{\"thermic\":{\"coolant\":{\"temp\":215},\"air\":{\"temp\":215}}},\"createdAt\":\"2023-01-03T13:40:01Z\"}],\"ignition\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"type\":\"Stop\"},\"battery\":{\"voltage\":88,\"createdAt\":\"2023-04-16T16:22:12Z\"},\"privacy\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"state\":\"None\"},\"service\":{\"createdAt\":\"2023-01-03T11:58:01Z\",\"type\":\"Electric\"},\"environment\":{\"luminosity\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"day\":true},\"air\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"temp\":12.5}},\"odometer\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"mileage\":2613.7},\"_links\":{\"self\":{\"href\":\"https://api.groupe-psa.com/connectedcar/v4/user/vehicles/1ef345/status?profile=endUser\"},\"vehicle\":{\"href\":\"https://api.groupe-psa.com/connectedcar/v4/user/vehicles/1ef345\"}},\"preconditioning\":{\"airConditioning\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"updatedAt\":\"2023-04-16T16:22:12Z\",\"status\":\"Disabled\",\"programs\":[{\"enabled\":false,\"slot\":1,\"recurrence\":\"Daily\",\"start\":\"PT0S\"},{\"enabled\":false,\"slot\":2,\"recurrence\":\"Daily\",\"start\":\"PT0S\"},{\"enabled\":false,\"slot\":3,\"recurrence\":\"Daily\",\"start\":\"PT0S\"},{\"enabled\":false,\"slot\":4,\"recurrence\":\"Daily\",\"start\":\"PT0S\"}]}},\"energies\":[{\"createdAt\":\"2023-01-03T13:40:01Z\",\"type\":\"Fuel\",\"subType\":\"FossilEnergy\",\"extension\":{\"fuel\":{\"consumptions\":{\"total\":0}}}},{\"createdAt\":\"2023-04-16T16:22:12Z\",\"type\":\"Electric\",\"subType\":\"ElectricEnergy\",\"level\":91,\"autonomy\":338,\"extension\":{\"electric\":{\"battery\":{\"load\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"capacity\":36608,\"residual\":13024}},\"charging\":{\"plugged\":false,\"status\":\"Disconnected\",\"chargingRate\":0,\"chargingMode\":\"No\",\"nextDelayedTime\":\"PT0S\"}}}}],\"preconditionning\":{\"airConditioning\":{\"createdAt\":\"2023-04-16T16:22:12Z\",\"updatedAt\":\"2023-04-16T16:22:12Z\",\"status\":\"Disabled\",\"programs\":[{\"enabled\":false,\"slot\":1,\"recurrence\":\"Daily\",\"start\":\"PT0S\"},{\"enabled\":false,\"slot\":2,\"recurrence\":\"Daily\",\"start\":\"PT0S\"},{\"enabled\":false,\"slot\":3,\"recurrence\":\"Daily\",\"start\":\"PT0S\"},{\"enabled\":false,\"slot\":4,\"recurrence\":\"Daily\",\"start\":\"PT0S\"}]}},\"energy\":[{\"createdAt\":\"2023-01-03T13:40:01Z\",\"updatedAt\":\"2023-01-03T13:40:01Z\",\"type\":\"Fuel\"},{\"createdAt\":\"2023-04-16T16:22:12Z\",\"updatedAt\":\"2023-04-16T16:22:12Z\",\"type\":\"Electric\",\"level\":91,\"autonomy\":338,\"charging\":{\"plugged\":false,\"status\":\"Disconnected\",\"chargingRate\":0,\"chargingMode\":\"No\",\"nextDelayedTime\":\"PT0S\"}}]}";
 
-	private Gson gson;
+    private Gson gson;
 
-	@BeforeEach
-	public void setUp() {
-		gson = new GsonBuilder().registerTypeAdapterFactory(new GeometryAdapterFactory())
-				.registerTypeAdapter(ZonedDateTime.class, new JsonDeserializer<ZonedDateTime>() {
-					@Override
-					public @Nullable ZonedDateTime deserialize(JsonElement json, Type typeOfT,
-							JsonDeserializationContext context) throws JsonParseException {
-						return ZonedDateTime.parse(json.getAsJsonPrimitive().getAsString());
-					}
-				}).registerTypeAdapter(Duration.class, new JsonDeserializer<Duration>() {
-					@Override
-					public @Nullable Duration deserialize(JsonElement json, Type typeOfT,
-							JsonDeserializationContext context) throws JsonParseException {
-						return Duration.parse(json.getAsJsonPrimitive().getAsString());
-					}
-				}).create();
-	}
+    @BeforeEach
+    public void setUp() {
+        gson = new GsonBuilder().registerTypeAdapterFactory(new GeometryAdapterFactory())
+                .registerTypeAdapter(ZonedDateTime.class, new JsonDeserializer<ZonedDateTime>() {
+                    @Override
+                    public @Nullable ZonedDateTime deserialize(JsonElement json, Type typeOfT,
+                            JsonDeserializationContext context) throws JsonParseException {
+                        return ZonedDateTime.parse(json.getAsJsonPrimitive().getAsString());
+                    }
+                }).registerTypeAdapter(Duration.class, new JsonDeserializer<Duration>() {
+                    @Override
+                    public @Nullable Duration deserialize(JsonElement json, Type typeOfT,
+                            JsonDeserializationContext context) throws JsonParseException {
+                        return Duration.parse(json.getAsJsonPrimitive().getAsString());
+                    }
+                }).create();
+    }
 
-	@Test
-	void testGetVehicleStatus() {
-		final VehicleStatus vehicleStatus = gson.fromJson(API_RESPONSE, VehicleStatus.class);
-		Assertions.assertNotNull(vehicleStatus);
-		Assertions.assertNotNull(vehicleStatus.getOdometer().getMileage());
-		Assertions.assertNotNull(vehicleStatus.getIgnition().getType());
-		Assertions.assertNotNull(vehicleStatus.getEnvironment().getAir().getTemp());
-		Assertions.assertNotNull(vehicleStatus.getEnvironment().getLuminosity().isDay());
-	}
+    @Test
+    void testGetVehicleStatus() {
+        final VehicleStatus vehicleStatus = gson.fromJson(API_RESPONSE, VehicleStatus.class);
+        Assertions.assertNotNull(vehicleStatus);
+        Assertions.assertNotNull(vehicleStatus.getOdometer().getMileage());
+        Assertions.assertNotNull(vehicleStatus.getIgnition().getType());
+        Assertions.assertNotNull(vehicleStatus.getEnvironment().getAir().getTemp());
+        Assertions.assertNotNull(vehicleStatus.getEnvironment().getLuminosity().isDay());
+    }
 }
