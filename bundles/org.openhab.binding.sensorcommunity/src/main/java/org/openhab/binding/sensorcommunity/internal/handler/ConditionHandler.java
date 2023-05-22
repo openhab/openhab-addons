d8376aa6d05dd82c8d6c2fefc65e4fba4e4037f2
Holger Friedrich <holgerfriedrich@users.noreply.github.com>
2023-05-22 07:57:23 +0200

    [luftdateninfo][sensorcommunity] Rename binding to Sensor.Community (#15012)
    * [luftdateninfo][sensorcommunity] Rename binding to match the new name Sensor.Community
    
    Signed-off-by: Holger Friedrich <mail@holger-friedrich.de>

---
+++ bundles/org.openhab.binding.sensorcommunity/src/main/java/org/openhab/binding/sensorcommunity/internal/handler/ConditionHandler.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.sensorcommunity.internal.handler;
+
+import static org.openhab.binding.sensorcommunity.internal.SensorCommunityBindingConstants.*;
+import static org.openhab.binding.sensorcommunity.internal.utils.Constants.*;
+import static org.openhab.core.library.unit.MetricPrefix.HECTO;
+
+import java.util.List;
+
+import javax.measure.quantity.Dimensionless;
+import javax.measure.quantity.Pressure;
+import javax.measure.quantity.Temperature;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.sensorcommunity.internal.dto.SensorDataValue;
+import org.openhab.binding.sensorcommunity.internal.utils.NumberUtils;
+import org.openhab.core.library.types.QuantityType;
+import org.openhab.core.library.unit.SIUnits;
+import org.openhab.core.library.unit.Units;
+import org.openhab.core.thing.Thing;
+
+/**
+ * The {@link ConditionHandler} is responsible for handling commands, which are
+ * sent to one of the channels.
+ *
+ * @author Bernd Weymann - Initial contribution
+ */
+@NonNullByDefault
+public class ConditionHandler extends BaseSensorHandler {
+    protected QuantityType<Temperature> temperatureCache = QuantityType.valueOf(-1, SIUnits.CELSIUS);
+    protected QuantityType<Dimensionless> humidityCache = QuantityType.valueOf(-1, Units.PERCENT);
+    protected QuantityType<Pressure> pressureCache = QuantityType.valueOf(-1, HECTO(SIUnits.PASCAL));
+    protected QuantityType<Pressure> pressureSeaCache = QuantityType.valueOf(-1, HECTO(SIUnits.PASCAL));
+
+    public ConditionHandler(Thing thing) {
+        super(thing);
+    }
+
+    @Override
+    public UpdateStatus updateChannels(@Nullable String json) {
+        if (json != null) {
+            List<SensorDataValue> valueList = HTTPHandler.getHandler().getLatestValues(json);
+            if (valueList != null) {
+                if (HTTPHandler.getHandler().isCondition(valueList)) {
+                    valueList.forEach(v -> {
+                        if (v.getValueType().endsWith(TEMPERATURE)) {
+                            temperatureCache = QuantityType.valueOf(NumberUtils.round(v.getValue(), 1),
+                                    SIUnits.CELSIUS);
+                            updateState(TEMPERATURE_CHANNEL, temperatureCache);
+                        } else if (v.getValueType().endsWith(HUMIDITY)) {
+                            humidityCache = QuantityType.valueOf(NumberUtils.round(v.getValue(), 1), Units.PERCENT);
+                            updateState(HUMIDITY_CHANNEL, humidityCache);
+                        } else if (v.getValueType().endsWith(PRESSURE)) {
+                            pressureCache = QuantityType.valueOf(
+                                    NumberUtils.round(NumberUtils.convert(v.getValue()) / 100, 1),
+                                    HECTO(SIUnits.PASCAL));
+                            updateState(PRESSURE_CHANNEL, pressureCache);
+                        } else if (v.getValueType().endsWith(PRESSURE_SEALEVEL)) {
+                            pressureSeaCache = QuantityType.valueOf(
+                                    NumberUtils.round(NumberUtils.convert(v.getValue()) / 100, 1),
+                                    HECTO(SIUnits.PASCAL));
+                            updateState(PRESSURE_SEA_CHANNEL, pressureSeaCache);
+                        }
+                    });
+                    return UpdateStatus.OK;
+                } else {
+                    return UpdateStatus.VALUE_ERROR;
+                }
+            } else {
+                return UpdateStatus.VALUE_EMPTY;
+            }
+        } else {
+            return UpdateStatus.CONNECTION_ERROR;
+        }
+    }
+
+    @Override
+    protected void updateFromCache() {
+        updateState(TEMPERATURE_CHANNEL, temperatureCache);
+        updateState(HUMIDITY_CHANNEL, humidityCache);
+        updateState(PRESSURE_CHANNEL, pressureCache);
+        updateState(PRESSURE_SEA_CHANNEL, pressureSeaCache);
+    }
+}
---
+++ bundles/org.openhab.binding.sensorcommunity/src/main/java/org/openhab/binding/sensorcommunity/internal/utils/NumberUtils.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.sensorcommunity.internal.utils;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link NumberUtils} class provides helpers for converting Numbers.
+ *
+ * @author Bernd Weymann - Initial contribution
+ */
+@NonNullByDefault
+public class NumberUtils {
+    public static final double UNDEF = Double.NaN;
+
+    public static double round(Object o, int places) {
+        double value = convert(o);
+
+        // for negative places return plain number
+        if (places < 0) {
+            return value;
+        }
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
+
+    public static double convert(Object o) {
+        // ensure value not null
+        double value = UNDEF;
+        if (o instanceof Number) {
+            value = ((Number) o).doubleValue();
+        } else if (o instanceof String) {
+            value = Double.parseDouble(o.toString());
+        }
+        return value;
+    }
+}
