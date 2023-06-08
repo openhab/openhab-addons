e354f1da97450e56b8bb44e1a8e430d67b29e1c5
Christian Jonak-Möchel <christian@jonak.org>
2023-06-08 18:15:26 +0200

    Add blade status channel in Robonect binding
    Signed-off-by: Christian Jonak-Möchel <christian@jonak.org>

--- bundles/org.openhab.binding.robonect/src/main/java/org/openhab/binding/robonect/internal/RobonectBindingConstants.java
+++ bundles/org.openhab.binding.robonect/src/main/java/org/openhab/binding/robonect/internal/RobonectBindingConstants.java
@@ -54,6 +54,10 @@ public class RobonectBindingConstants {
     public static final String CHANNEL_HEALTH_TEMP = "health-temperature";
     public static final String CHANNEL_HEALTH_HUM = "health-humidity";
 
+    public static final String CHANNEL_BLADES_QUALITY = "blades-quality";
+    public static final String CHANNEL_BLADES_DAYS = "blades-days";
+    public static final String CHANNEL_BLADES_HOURS = "blades-hours";
+
     public static final String PROPERTY_COMPILED = "compiled";
     public static final String PROPERTY_COMMENT = "comment";
 }
