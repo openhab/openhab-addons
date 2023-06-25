d12e66bca94e024345708a94789cc0845214c0c1
Florian Hotze <florianh_dev@icloud.com>
2023-06-25 12:06:24 +0200

    [yamahamusiccast] Address review
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- bundles/org.openhab.binding.yamahamusiccast/src/main/java/org/openhab/binding/yamahamusiccast/internal/YamahaMusiccastHandler.java
+++ bundles/org.openhab.binding.yamahamusiccast/src/main/java/org/openhab/binding/yamahamusiccast/internal/YamahaMusiccastHandler.java
@@ -228,7 +228,16 @@ public class YamahaMusiccastHandler extends BaseThingHandler {
                     }
                     break;
                 case CHANNEL_VOLUMEDB:
-                    setVolumeDb(Float.parseFloat(command.toString().replace(" dB", "")), zone, this.host);
+                    float volumeDb;
+                    if (command instanceof QuantityType<?> qt) {
+                        volumeDb = qt.toUnit(Units.DECIBEL).floatValue();
+                    } else if (command instanceof DecimalType dt) {
+                        volumeDb = dt.floatValue();
+                    } else {
+                        logger.debug("Command has wrong type, QuantityType or DecimalType required!");
+                        return;
+                    }
+                    setVolumeDb(volumeDb, zone, this.host);
                     localSyncVolume = Boolean.parseBoolean(getThing().getConfiguration().get("syncVolume").toString());
                     if (localSyncVolume == Boolean.TRUE) {
                         tmpString = getDistributionInfo(this.host);
@@ -238,8 +247,8 @@ public class YamahaMusiccastHandler extends BaseThingHandler {
                             if ("server".equals(localRole)) {
                                 for (JsonElement ip : distributioninfo.getClientList()) {
                                     JsonObject clientObject = ip.getAsJsonObject();
-                                    setVolumeDbLinkedDevice(Float.parseFloat(command.toString().replace(" dB", "")),
-                                            zone, clientObject.get("ip_address").getAsString());
+                                    setVolumeDbLinkedDevice(volumeDb, zone,
+                                            clientObject.get("ip_address").getAsString());
                                 }
                             }
                         }
