092dbaa3b4c7043a61f4d839124e8398fd199088
Hans-Jörg Merk <hans-joerg.merk@t-online.de>
2023-06-26 17:11:11 +0200

    Update bundles/org.openhab.binding.intesis/src/main/java/org/openhab/binding/intesis/internal/config/IntesisHomeConfiguration.java
    Co-authored-by: Jacob Laursen <jacob-github@vindvejr.dk>
    Signed-off-by: Hans-Jörg Merk <hans-joerg.merk@t-online.de>

--- bundles/org.openhab.binding.intesis/src/main/java/org/openhab/binding/intesis/internal/config/IntesisHomeConfiguration.java
+++ bundles/org.openhab.binding.intesis/src/main/java/org/openhab/binding/intesis/internal/config/IntesisHomeConfiguration.java
@@ -23,5 +23,5 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
 public class IntesisHomeConfiguration {
     public String ipAddress = "";
     public String password = "";
-    public int pollingIntervall;
+    public int pollingInterval = 30;
 }
