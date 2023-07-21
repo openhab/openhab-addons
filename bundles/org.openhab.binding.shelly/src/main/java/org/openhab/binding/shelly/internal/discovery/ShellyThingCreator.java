250f545a5c75b02d5fa49268bfead15b88ec65e4
Markus Michels <markus7017@gmail.com>
2023-07-21 22:01:34 +0200

    Reimplement Auth Digist to return response as http header field instead of being part of post data; avoid sending Basic Auth for Gen2 and /shelly
    Signed-off-by: Markus Michels <markus7017@gmail.com>

--- bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/discovery/ShellyThingCreator.java
+++ bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/discovery/ShellyThingCreator.java
@@ -470,6 +470,7 @@ public class ShellyThingCreator {
         if (THING_TYPE_SHELLYPROTECTED_STR.equals(hostname)) {
             return THING_TYPE_SHELLYPROTECTED_STR;
         }
+        int i = 1;
         String name = hostname.toLowerCase();
         String type = substringBefore(name, "-").toLowerCase();
         String devid = substringAfterLast(name, "-");
