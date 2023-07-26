34aed96ebc8b7967d48f4221ebdbd38afb4ca98d
grindfiner <125708464+grindfiner@users.noreply.github.com>
2023-07-26 09:14:58 +0200

    changes ws-channel to energy-consumed-since-last-call-channel
    Signed-off-by: grindfiner <125708464+grindfiner@users.noreply.github.com>

--- bundles/org.openhab.binding.mystrom/src/main/java/org/openhab/binding/mystrom/internal/MyStromPlugHandler.java
+++ bundles/org.openhab.binding.mystrom/src/main/java/org/openhab/binding/mystrom/internal/MyStromPlugHandler.java
@@ -12,10 +12,10 @@
  */
 package org.openhab.binding.mystrom.internal;
 
+import static org.openhab.binding.mystrom.internal.MyStromBindingConstants.CHANNEL_ENERGY_CONSUMED_SINCE_LAST_CALL;
 import static org.openhab.binding.mystrom.internal.MyStromBindingConstants.CHANNEL_POWER;
 import static org.openhab.binding.mystrom.internal.MyStromBindingConstants.CHANNEL_SWITCH;
 import static org.openhab.binding.mystrom.internal.MyStromBindingConstants.CHANNEL_TEMPERATURE;
-import static org.openhab.binding.mystrom.internal.MyStromBindingConstants.CHANNEL_WS;
 import static org.openhab.core.library.unit.SIUnits.CELSIUS;
 import static org.openhab.core.library.unit.Units.WATT;
 import static org.openhab.core.library.unit.Units.WATT_SECOND;
@@ -99,7 +99,7 @@ public class MyStromPlugHandler extends AbstractMyStromHandler {
         if (report != null) {
             updateState(CHANNEL_SWITCH, report.relay ? OnOffType.ON : OnOffType.OFF);
             updateState(CHANNEL_POWER, QuantityType.valueOf(report.power, WATT));
-            updateState(CHANNEL_WS, QuantityType.valueOf(report.Ws, WATT_SECOND));
+            updateState(CHANNEL_ENERGY_CONSUMED_SINCE_LAST_CALL, QuantityType.valueOf(report.Ws, WATT_SECOND));
             updateState(CHANNEL_TEMPERATURE, QuantityType.valueOf(report.temperature, CELSIUS));
         }
     }
