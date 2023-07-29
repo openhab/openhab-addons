98ad61d4acd995e2295e1001aab2f5f7f8fd617f
lsiepel <leosiepel@gmail.com>
2023-07-29 14:10:57 +0200

    Fix another exception check
    Signed-off-by: lsiepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.irtrans/src/main/java/org/openhab/binding/irtrans/internal/handler/EthernetBridgeHandler.java
+++ bundles/org.openhab.binding.irtrans/src/main/java/org/openhab/binding/irtrans/internal/handler/EthernetBridgeHandler.java
@@ -277,9 +277,13 @@ public class EthernetBridgeHandler extends BaseBridgeHandler implements Transcei
                 if (channel.getChannelTypeUID() != null
                         && channel.getChannelTypeUID().getId().equals(IRtransBindingConstants.BLASTER_CHANNEL_TYPE)) {
                     if (command instanceof StringType) {
-                        String remoteName = command.toString().substring(0, command.toString().indexOf(","));
-                        String irCommandName = command.toString().substring(command.toString().indexOf(",") + 1);
-
+                        String remoteName = command.toString();
+                        String irCommandName = command.toString();
+    
+                        if (command.toString().indexOf(",") > 0) {
+                            remoteName = command.toString().substring(0, command.toString().indexOf(","));
+                            irCommandName = command.toString().substring(command.toString().indexOf(",") + 1);
+                        }
                         IrCommand ircommand = new IrCommand();
                         ircommand.setRemote(remoteName);
                         ircommand.setCommand(irCommandName);
