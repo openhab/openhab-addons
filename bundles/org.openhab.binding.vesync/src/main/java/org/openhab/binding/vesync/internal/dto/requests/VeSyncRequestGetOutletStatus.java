05399cc1f7cc707cfb1ba587ea41b240351f6d47
Marcel Goerentz <m.goerentz@t-online.de>
2023-07-31 12:39:57 +0200

    [VeSync] Add missing license and author tag
    Signed-off-by: Marcel Goerentz <m.goerentz@t-online.de>

--- bundles/org.openhab.binding.vesync/src/main/java/org/openhab/binding/vesync/internal/dto/requests/VeSyncRequestGetOutletStatus.java
+++ bundles/org.openhab.binding.vesync/src/main/java/org/openhab/binding/vesync/internal/dto/requests/VeSyncRequestGetOutletStatus.java
@@ -1,9 +1,27 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.vesync.internal.dto.requests;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * The {@link VeSyncRequestGetOutletStatus} is a Java class used as a DTO to hold the Vesync's API's common
+ * request data for V2 ByPass payloads.
+ *
+ * @author Marcel Goerentz - Add outlets to the supported devices
+ */
 @NonNullByDefault
 public class VeSyncRequestGetOutletStatus extends VeSyncRequest {
 
--- bundles/org.openhab.binding.vesync/src/main/java/org/openhab/binding/vesync/internal/dto/responses/VeSyncV2BypassOutletStatus.java
+++ bundles/org.openhab.binding.vesync/src/main/java/org/openhab/binding/vesync/internal/dto/responses/VeSyncV2BypassOutletStatus.java
@@ -1,7 +1,25 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.vesync.internal.dto.responses;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * The {@link VeSyncV2BypassOutletStatus} is a Java class used as a DTO to hold the Vesync's API's common response
+ * data, in regards to an outlet device.
+ *
+ * @author Marcel Goerentz - Add outlets to the supported devices
+ */
 public class VeSyncV2BypassOutletStatus extends VeSyncResponse {
 
     @SerializedName("result")
