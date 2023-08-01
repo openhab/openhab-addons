8b2b1d1b989f71fd48e6375616daea0918f3f255
Marcel Goerentz <m.goerentz@t-online.de>
2023-08-01 11:02:15 +0200

    [VeSync] Add missing file header and author tag
    Signed-off-by: Marcel Goerentz <m.goerentz@t-online.de>

--- bundles/org.openhab.binding.vesync/src/main/java/org/openhab/binding/vesync/internal/dto/responses/VeSyncV2BypassEnergyHistory.java
+++ bundles/org.openhab.binding.vesync/src/main/java/org/openhab/binding/vesync/internal/dto/responses/VeSyncV2BypassEnergyHistory.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.vesync.internal.dto.responses;
 
 import java.util.ArrayList;
@@ -5,6 +17,12 @@ import java.util.List;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * The {@link VeSyncV2BypassEnergyHistory} is a Java class used as a DTO to hold the Vesync's API's common response
+ * data, in regards to an outlet device.
+ *
+ * @author Marcel Goerentz - Add outlets to the supported devices
+ */
 public class VeSyncV2BypassEnergyHistory extends VeSyncResponse {
 
     @SerializedName("result")
