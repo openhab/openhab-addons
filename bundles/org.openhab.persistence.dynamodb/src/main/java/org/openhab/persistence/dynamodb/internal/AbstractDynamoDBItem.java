1433af7fa1bca0eb1d5211e544aeb4acf10b6410
Holger Friedrich <mail@holger-friedrich.de>
2023-08-19 16:38:49 +0200

    [persistence] Code optimization for Java17: instanceof matching and multiline strings
    Signed-off-by: Holger Friedrich <mail@holger-friedrich.de>

--- bundles/org.openhab.persistence.dynamodb/src/main/java/org/openhab/persistence/dynamodb/internal/AbstractDynamoDBItem.java
+++ bundles/org.openhab.persistence.dynamodb/src/main/java/org/openhab/persistence/dynamodb/internal/AbstractDynamoDBItem.java
@@ -302,8 +302,8 @@ public abstract class AbstractDynamoDBItem<T> implements DynamoDBItem<T> {
         } else if (item instanceof NumberItem) {
             return new DynamoDBBigDecimalItem(name, convert(state, DecimalType.class).toBigDecimal(), time, expireDays);
         } else if (item instanceof PlayerItem) {
-            if (state instanceof PlayPauseType) {
-                switch ((PlayPauseType) state) {
+            if (state instanceof PlayPauseType type) {
+                switch (type) {
                     case PLAY:
                         return new DynamoDBBigDecimalItem(name, PLAY_BIGDECIMAL, time, expireDays);
                     case PAUSE:
@@ -311,8 +311,8 @@ public abstract class AbstractDynamoDBItem<T> implements DynamoDBItem<T> {
                     default:
                         throw new IllegalArgumentException("Unexpected enum with PlayPauseType: " + state.toString());
                 }
-            } else if (state instanceof RewindFastforwardType) {
-                switch ((RewindFastforwardType) state) {
+            } else if (state instanceof RewindFastforwardType type) {
+                switch (type) {
                     case FASTFORWARD:
                         return new DynamoDBBigDecimalItem(name, FAST_FORWARD_BIGDECIMAL, time, expireDays);
                     case REWIND:
@@ -329,12 +329,11 @@ public abstract class AbstractDynamoDBItem<T> implements DynamoDBItem<T> {
             // Normalize UP/DOWN to %
             return new DynamoDBBigDecimalItem(name, convert(state, PercentType.class).toBigDecimal(), time, expireDays);
         } else if (item instanceof StringItem) {
-            if (state instanceof StringType) {
-                return new DynamoDBStringItem(name, ((StringType) state).toString(), time, expireDays);
-            } else if (state instanceof DateTimeType) {
-                return new DynamoDBStringItem(name,
-                        ZONED_DATE_TIME_CONVERTER_STRING.toString(((DateTimeType) state).getZonedDateTime()), time,
-                        expireDays);
+            if (state instanceof StringType type) {
+                return new DynamoDBStringItem(name, type.toString(), time, expireDays);
+            } else if (state instanceof DateTimeType type) {
+                return new DynamoDBStringItem(name, ZONED_DATE_TIME_CONVERTER_STRING.toString(type.getZonedDateTime()),
+                        time, expireDays);
             } else {
                 throw new IllegalStateException(
                         String.format("Unexpected state type %s with StringItem", state.getClass().getSimpleName()));
@@ -411,8 +410,8 @@ public abstract class AbstractDynamoDBItem<T> implements DynamoDBItem<T> {
                     if (numberState == null) {
                         return null;
                     }
-                    if (item instanceof NumberItem) {
-                        NumberItem numberItem = ((NumberItem) item);
+                    if (item instanceof NumberItem numberItem1) {
+                        NumberItem numberItem = numberItem1;
                         Unit<? extends Quantity<?>> unit = targetUnit == null ? numberItem.getUnit() : targetUnit;
                         if (unit != null) {
                             return new QuantityType<>(numberState, unit);
