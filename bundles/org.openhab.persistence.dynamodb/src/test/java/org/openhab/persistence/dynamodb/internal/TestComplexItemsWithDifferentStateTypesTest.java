c233ee1ede63e4e20a63075d5a7ee4cbd3bbf7ab
Wouter Born <github@maindrain.net>
2023-08-23 20:54:50 +0200

    Remove build server specific timing from dynamodb and ruuvigateway (#15472)
    * Remove build server specific timing from dynamodb and ruuvigateway
    
    There should be no difference between running tests on your own PC or on a build server.
    The ruuvigateway tests always fail for me during parallel builds due to its unrealistic timing.
    
    Signed-off-by: Wouter Born <github@maindrain.net>
    Signed-off-by: Jørgen Austvik <jaustvik@acm.org>

--- bundles/org.openhab.persistence.dynamodb/src/test/java/org/openhab/persistence/dynamodb/internal/TestComplexItemsWithDifferentStateTypesTest.java
+++ bundles/org.openhab.persistence.dynamodb/src/test/java/org/openhab/persistence/dynamodb/internal/TestComplexItemsWithDifferentStateTypesTest.java
@@ -13,7 +13,7 @@
 package org.openhab.persistence.dynamodb.internal;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assumptions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -305,8 +305,6 @@ public class TestComplexItemsWithDifferentStateTypesTest extends BaseIntegration
 
     @Test
     public void testGroupDummyItem() {
-        // Do not want to slow down CI runs
-        assumeFalse(isRunningInCI());
         // only with the fast local server
         assumeTrue(hasFakeServer());
         try {
