95409fa12bf4b8cc39957fe6897a44ca550c0a27
Danny Baumann <dannybaumann@web.de>
2023-08-30 11:52:37 +0200

    [gardena] Improve and fix UoM support
    Properly convert incoming UoM values for command durations, and output
    measurements as UoM values where possible.
    
    Signed-off-by: Danny Baumann <dannybaumann@web.de>

--- bundles/org.openhab.binding.gardena/src/main/java/org/openhab/binding/gardena/internal/model/dto/api/CommonService.java
+++ bundles/org.openhab.binding.gardena/src/main/java/org/openhab/binding/gardena/internal/model/dto/api/CommonService.java
@@ -12,6 +12,12 @@
  */
 package org.openhab.binding.gardena.internal.model.dto.api;
 
+import javax.measure.Unit;
+import javax.measure.quantity.Dimensionless;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.openhab.core.library.unit.Units;
+
 /**
  * Represents a Gardena object that is sent via the Gardena API.
  *
@@ -20,8 +26,10 @@ package org.openhab.binding.gardena.internal.model.dto.api;
 public class CommonService {
     public UserDefinedNameWrapper name;
     public TimestampedIntegerValue batteryLevel;
+    public @NonNull Unit<@NonNull Dimensionless> batteryLevelUnit = Units.PERCENT;
     public TimestampedStringValue batteryState;
     public TimestampedIntegerValue rfLinkLevel;
+    public @NonNull Unit<@NonNull Dimensionless> rfLinkLevelUnit = Units.PERCENT;
     public StringValue serial;
     public StringValue modelType;
     public TimestampedStringValue rfLinkState;
