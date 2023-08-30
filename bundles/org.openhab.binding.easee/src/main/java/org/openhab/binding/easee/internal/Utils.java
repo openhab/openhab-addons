20a2ef536368f9f13d9258e0801bb41a746906a5
Alexander Friese <alexf2015@users.noreply.github.com>
2023-08-30 20:10:23 +0200

    [easee] fix date formatting issues (#15501)
    * fix DateTimeException when formatting date out of range (in this case Instant.MIN)
    
    Signed-off-by: Alexander Friese <af944580@googlemail.com>

--- bundles/org.openhab.binding.easee/src/main/java/org/openhab/binding/easee/internal/Utils.java
+++ bundles/org.openhab.binding.easee/src/main/java/org/openhab/binding/easee/internal/Utils.java
@@ -14,8 +14,6 @@ package org.openhab.binding.easee.internal;
 
 import static org.openhab.binding.easee.internal.EaseeBindingConstants.*;
 
-import java.time.Instant;
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -65,18 +63,6 @@ public final class Utils {
         LOGGER.trace("parsing: {}", date);
         ZonedDateTime zdt = ZonedDateTime.parse(date, formatter);
         return zdt;
-    }
-
-    /**
-     * returns a date in a readable format
-     *
-     * @param date
-     * @return
-     */
-    public static String formatDate(Instant date) {
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-                .withZone(ZoneId.systemDefault());
-        return formatter.format(date);
     }
 
     /**
