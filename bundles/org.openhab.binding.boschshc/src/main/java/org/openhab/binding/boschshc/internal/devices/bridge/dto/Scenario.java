be3856c80ee8cb269ecf11e78e0c13fa22c839ec
Patrick Gell <patrick.gell@siemens.com>
2023-09-03 18:42:32 +0200

    chore: fix compile errors
    Signed-off-by: Patrick Gell <patgit023@gmail.com>

--- bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/devices/bridge/dto/Scenario.java
+++ bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/devices/bridge/dto/Scenario.java
@@ -1,7 +1,32 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.boschshc.internal.devices.bridge.dto;
 
 import org.openhab.binding.boschshc.internal.services.dto.BoschSHCServiceState;
 
+/**
+ * A scenario as represented by the controller.
+ *
+ * Json example:
+ * {
+ * "@type": "scenarioTriggered",
+ * "name": "My scenario",
+ * "id": "509bd737-eed0-40b7-8caa-e8686a714399",
+ * "lastTimeTriggered": "1693758693032"
+ * }
+ *
+ * @author Patrick Gell - Initial contribution
+ */
 public class Scenario extends BoschSHCServiceState {
 
     public String name;
--- bundles/org.openhab.binding.boschshc/src/test/java/org/openhab/binding/boschshc/internal/serialization/BoschServiceDataDeserializerTest.java
+++ bundles/org.openhab.binding.boschshc/src/test/java/org/openhab/binding/boschshc/internal/serialization/BoschServiceDataDeserializerTest.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.boschshc.internal.serialization;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -6,11 +18,19 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.junit.jupiter.api.Test;
 import org.openhab.binding.boschshc.internal.devices.bridge.dto.DeviceServiceData;
 import org.openhab.binding.boschshc.internal.devices.bridge.dto.LongPollResult;
 import org.openhab.binding.boschshc.internal.devices.bridge.dto.Scenario;
 
+/**
+ * Unit tests for {@link BoschServiceDataDeserializer}.
+ *
+ * @author Patrick Gell - Initial contribution
+ *
+ */
+@NonNullByDefault
 public class BoschServiceDataDeserializerTest {
 
     @Test
