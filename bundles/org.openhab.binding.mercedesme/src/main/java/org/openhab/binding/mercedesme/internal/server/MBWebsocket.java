935ce31c6b4d1fcc9d21731a5f53f7b46f20b7fb
Bernd Weymann <bernd.weymann@gmail.com>
2023-09-10 14:54:17 +0200

    readme adaptions
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

--- bundles/org.openhab.binding.mercedesme/src/main/java/org/openhab/binding/mercedesme/internal/server/MBWebsocket.java
+++ bundles/org.openhab.binding.mercedesme/src/main/java/org/openhab/binding/mercedesme/internal/server/MBWebsocket.java
@@ -13,8 +13,6 @@
 package org.openhab.binding.mercedesme.internal.server;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -155,11 +153,6 @@ public class MBWebsocket {
             // data update
             if (pm.hasVepUpdates()) {
                 accountHandler.distributeVepUpdates(pm.getVepUpdates().getUpdatesMap());
-                // create proto blob for debugging
-                File blob = new File("/tmp/proto.blob");
-                if (!blob.exists()) {
-                    pm.writeTo(new FileOutputStream("/tmp/proto.blob"));
-                }
             } else if (pm.hasAssignedVehicles()) {
                 for (int i = 0; i < pm.getAssignedVehicles().getVinsCount(); i++) {
                     String vin = pm.getAssignedVehicles().getVins(0);
