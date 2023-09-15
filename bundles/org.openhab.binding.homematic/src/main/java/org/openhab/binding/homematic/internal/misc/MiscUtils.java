93bc10cb992873f82ce262dbf1e802a41a449cab
Leo Siepel <leosiepel@gmail.com>
2023-09-15 21:57:29 +0200

    Checkstyle fixes
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/misc/MiscUtils.java
+++ bundles/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/misc/MiscUtils.java
@@ -21,7 +21,7 @@ import org.slf4j.LoggerFactory;
  * @author Gerhard Riegler - Initial contribution
  */
 public class MiscUtils {
-    private static final Logger logger = LoggerFactory.getLogger(MiscUtils.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MiscUtils.class);
 
     /**
      * Replaces invalid characters of the text to fit into an openHAB UID.
@@ -32,7 +32,7 @@ public class MiscUtils {
         }
         String cleanedText = text.replaceAll("[^A-Za-z0-9_-]", replaceChar);
         if (!text.equals(cleanedText)) {
-            logger.info("{} '{}' contains invalid characters, new {} '{}'", textType, text, textType, cleanedText);
+            LOGGER.info("{} '{}' contains invalid characters, new {} '{}'", textType, text, textType, cleanedText);
         }
         return cleanedText;
     }
