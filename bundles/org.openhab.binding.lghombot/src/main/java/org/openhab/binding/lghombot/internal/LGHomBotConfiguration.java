f896527a6be9e09cd5c87196ce5a489dbdee33f9
lsiepel <leosiepel@gmail.com>
2023-09-23 18:31:27 +0200

    Fix checkstyle (#15603)
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>
    Signed-off-by: Jørgen Austvik <jaustvik@acm.org>

--- bundles/org.openhab.binding.lghombot/src/main/java/org/openhab/binding/lghombot/internal/LGHomBotConfiguration.java
+++ bundles/org.openhab.binding.lghombot/src/main/java/org/openhab/binding/lghombot/internal/LGHomBotConfiguration.java
@@ -13,7 +13,6 @@
 package org.openhab.binding.lghombot.internal;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.openhab.binding.lghombot.internal.discovery.LGHomBotDiscovery;
 
 /**
  * The {@link LGHomBotConfiguration} class contains fields mapping thing configuration parameters.
@@ -24,7 +23,8 @@ import org.openhab.binding.lghombot.internal.discovery.LGHomBotDiscovery;
 public class LGHomBotConfiguration {
 
     /**
-     * Constant field used in {@link LGHomBotDiscovery} to set the configuration property during discovery. Value of
+     * Constant field used in {@link org.openhab.binding.lghombot.internal.discovery.LGHomBotDiscovery} to set the
+     * configuration property during discovery. Value of
      * this field needs to match {@link #ipAddress}
      */
     public static final String IP_ADDRESS = "ipAddress";
