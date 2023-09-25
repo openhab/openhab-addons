42a6dc88af3f82d3068bef2fe0b2f63728d9c420
Laurent ARNAL <laurent@clae.net>
2023-09-25 09:45:55 +0200

    start implementing type providers
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

---
+++ bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/type/SmartthingsConfigDescriptionProviderImpl.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.smartthings.internal.type;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.config.core.ConfigDescription;
+import org.openhab.core.config.core.ConfigDescriptionProvider;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ *
+ * @author Laurent Arnal - Initial contribution
+ */
+@NonNullByDefault
+@Component(service = { SmartthingsConfigDescriptionProvider.class, ConfigDescriptionProvider.class })
+public class SmartthingsConfigDescriptionProviderImpl implements SmartthingsConfigDescriptionProvider {
+    private Map<URI, ConfigDescription> configDescriptionsByURI = new HashMap<>();
+
+    @Override
+    public Collection<ConfigDescription> getConfigDescriptions(@Nullable Locale locale) {
+        Collection<ConfigDescription> result = new ArrayList<>();
+        for (URI configDescriptionURI : configDescriptionsByURI.keySet()) {
+            ConfigDescription desc = configDescriptionsByURI.get(configDescriptionURI);
+            if (desc != null) {
+                result.add(desc);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public @Nullable ConfigDescription getConfigDescription(URI uri, @Nullable Locale locale) {
+        return configDescriptionsByURI.get(uri);
+    }
+
+    @Nullable
+    @Override
+    public ConfigDescription getInternalConfigDescription(URI uri) {
+        return configDescriptionsByURI.get(uri);
+    }
+
+    @Override
+    public void addConfigDescription(ConfigDescription configDescription) {
+        configDescriptionsByURI.put(configDescription.getUID(), configDescription);
+    }
+}
