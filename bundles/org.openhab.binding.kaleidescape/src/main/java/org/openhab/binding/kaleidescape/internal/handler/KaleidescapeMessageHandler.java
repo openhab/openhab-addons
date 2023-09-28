c2e479376a2edeaebaadb105411f8a1fc695a8f5
Michael Lobstein <michael.lobstein@gmail.com>
2023-09-27 20:00:53 -0500

    Update Play/Pause status to Player channel widget
    Signed-off-by: Michael Lobstein <michael.lobstein@gmail.com>

--- bundles/org.openhab.binding.kaleidescape/src/main/java/org/openhab/binding/kaleidescape/internal/handler/KaleidescapeMessageHandler.java
+++ bundles/org.openhab.binding.kaleidescape/src/main/java/org/openhab/binding/kaleidescape/internal/handler/KaleidescapeMessageHandler.java
@@ -34,6 +34,7 @@ import org.openhab.binding.kaleidescape.internal.communication.KaleidescapeStatu
 import org.openhab.core.library.types.DecimalType;
 import org.openhab.core.library.types.OnOffType;
 import org.openhab.core.library.types.PercentType;
+import org.openhab.core.library.types.PlayPauseType;
 import org.openhab.core.library.types.QuantityType;
 import org.openhab.core.library.types.RawType;
 import org.openhab.core.library.types.StringType;
@@ -110,6 +111,10 @@ public enum KaleidescapeMessageHandler {
             if (matcher.find()) {
                 handler.updateChannel(PLAY_MODE,
                         new StringType(KaleidescapeStatusCodes.PLAY_MODE.get(matcher.group(1))));
+
+                handler.updateChannel(CONTROL,
+                        (ZERO.equals(matcher.group(1)) || ONE.equals(matcher.group(1))) ? PlayPauseType.PAUSE
+                                : PlayPauseType.PLAY);
 
                 handler.updateChannel(PLAY_SPEED, new StringType(matcher.group(2)));
 
@@ -311,6 +316,10 @@ public enum KaleidescapeMessageHandler {
             if (matcher.find()) {
                 handler.updateChannel(MUSIC_PLAY_MODE,
                         new StringType(KaleidescapeStatusCodes.PLAY_MODE.get(matcher.group(1))));
+
+                handler.updateChannel(MUSIC_CONTROL,
+                        (ZERO.equals(matcher.group(1)) || ONE.equals(matcher.group(1))) ? PlayPauseType.PAUSE
+                                : PlayPauseType.PLAY);
 
                 handler.updateChannel(MUSIC_PLAY_SPEED, new StringType(matcher.group(2)));
 
