00740a6a9b5bfe9f6ef7ba292aa9b5e60fca43f2
Stefan Höhn <mail@stefanhoehn.com>
2023-10-08 22:15:02 +0200

    rename from goveelan to govee
    Signed-off-by: Stefan Höhn <mail@stefanhoehn.com>

---
+++ bundles/org.openhab.binding.govee/src/main/java/org/openhab/binding/govee/internal/GoveeBindingConstants.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.govee.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.thing.ThingTypeUID;
+
+/**
+ * The {@link GoveeBindingConstants} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Stefan Höhn - Initial contribution
+ */
+@NonNullByDefault
+public class GoveeBindingConstants {
+
+    // Thing properties
+    public static final String MAC_ADDRESS = "macAddress";
+    public static final String IP_ADDRESS = "hostname";
+    public static final String DEVICE_TYPE = "deviceType";
+    public static final String PRODUCT_NAME = "productName";
+    private static final String BINDING_ID = "govee";
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID THING_TYPE_LIGHT = new ThingTypeUID(BINDING_ID, "govee-light");
+
+    // List of all Channel ids
+    public static final String SWITCH = "switch";
+    public static final String COLOR = "color";
+    public static final String COLOR_TEMPERATURE_ABS = "color-temperature-abs";
+    public static final String BRIGHTNESS = "brightness";
+}
---
+++ bundles/org.openhab.binding.govee/src/test/java/org/openhab/binding/govee/internal/GoveeDiscoveryTest.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.govee.internal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Map;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Stefan Höhn - Initial contribution
+ */
+@NonNullByDefault
+public class GoveeDiscoveryTest {
+
+    String response = """
+             {
+                "msg":{
+                   "cmd":"scan",
+                   "data":{
+                      "ip":"192.168.178.171",
+                      "device":"7D:31:C3:35:33:33:44:15",
+                      "sku":"H6076",
+                      "bleVersionHard":"3.01.01",
+                      "bleVersionSoft":"1.04.04",
+                      "wifiVersionHard":"1.00.10",
+                      "wifiVersionSoft":"1.02.11"
+                   }
+                }
+            }
+             """;
+
+    @Test
+    public void testProcessScanMessage() {
+        GoveeDiscoveryService service = new GoveeDiscoveryService();
+        Map<String, Object> deviceProperties = service.getDeviceProperties(response);
+        assertNotNull(deviceProperties);
+        assertEquals(deviceProperties.get(GoveeBindingConstants.DEVICE_TYPE), "H6076");
+        assertEquals(deviceProperties.get(GoveeBindingConstants.IP_ADDRESS), "192.168.178.171");
+        assertEquals(deviceProperties.get(GoveeBindingConstants.MAC_ADDRESS), "7D:31:C3:35:33:33:44:15");
+    }
+}
