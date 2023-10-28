05ad0e2a8db2ad6f4cdaf1eb7c545b6b99968db2
Hakan Tandogan <hakan@tandogan.com>
2023-10-28 20:41:26 +0200

    [tesla] Review comment - keep 'version' as a property, not channel
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

--- bundles/org.openhab.binding.tesla/src/main/java/org/openhab/binding/tesla/internal/TeslaChannelSelectorProxy.java
+++ bundles/org.openhab.binding.tesla/src/main/java/org/openhab/binding/tesla/internal/TeslaChannelSelectorProxy.java
@@ -160,6 +160,7 @@ public class TeslaChannelSelectorProxy {
                 return super.getState(s);
             }
         },
+        CAR_VERSION("car_version", "version", StringType.class, true),
         CENTER_DISPLAY("center_display_state", "centerdisplay", DecimalType.class, false),
         CHARGE(null, "charge", OnOffType.class, false) {
             @Override
@@ -932,7 +933,6 @@ public class TeslaChannelSelectorProxy {
             }
         },
         SOC("soc", "soc", PercentType.class, false),
-        SOFTWARE_VERSION("car_version", "softwareversion", StringType.class, false),
         SOFTWARE_UPDATE_STATUS("status", "softwareupdatestatus", StringType.class, false),
         SOFTWARE_UPDATE_VERSION("version", "softwareupdateversion", StringType.class, false),
         SPEED("speed", "speed", DecimalType.class, false) {
