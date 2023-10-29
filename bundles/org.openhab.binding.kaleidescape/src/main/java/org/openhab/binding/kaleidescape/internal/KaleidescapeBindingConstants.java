01da358674e99d740559e57a65a69a8adf6cd86d
mlobstein <michael.lobstein@gmail.com>
2023-10-29 05:13:55 -0500

    Register connection in Kaleidescape System log (#15820)
    Signed-off-by: Michael Lobstein <michael.lobstein@gmail.com>
    Signed-off-by: Jørgen Austvik <jaustvik@acm.org>

--- bundles/org.openhab.binding.kaleidescape/src/main/java/org/openhab/binding/kaleidescape/internal/KaleidescapeBindingConstants.java
+++ bundles/org.openhab.binding.kaleidescape/src/main/java/org/openhab/binding/kaleidescape/internal/KaleidescapeBindingConstants.java
@@ -183,6 +183,7 @@ public class KaleidescapeBindingConstants {
     public static final String MUSIC_RANDOM_ON = "MUSIC_RANDOM_ON";
     public static final String MUSIC_RANDOM_OFF = "MUSIC_RANDOM_OFF";
 
+    public static final String SEND_TO_SYSLOG = "SEND_TO_SYSLOG:INFORMATION:";
     public static final String SEND_EVENT_VOLUME_CAPABILITIES_15 = "SEND_EVENT:VOLUME_CAPABILITIES=15";
     public static final String SEND_EVENT_VOLUME_LEVEL_EQ = "SEND_EVENT:VOLUME_LEVEL=";
     public static final String SEND_EVENT_MUTE = "SEND_EVENT:MUTE_";
