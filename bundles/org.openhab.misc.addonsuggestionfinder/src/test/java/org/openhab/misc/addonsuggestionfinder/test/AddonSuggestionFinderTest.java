9a4769bd0d83b6a2bcabc3eab5ec8d5a903ace68
Andrew Fiddian-Green <software@whitebear.ch>
2023-10-29 15:48:41 +0000

    [addonsuggestionfinder] add regex syntax checks
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.misc.addonsuggestionfinder/src/test/java/org/openhab/misc/addonsuggestionfinder/test/AddonSuggestionFinderTest.java
+++ bundles/org.openhab.misc.addonsuggestionfinder/src/test/java/org/openhab/misc/addonsuggestionfinder/test/AddonSuggestionFinderTest.java
@@ -14,12 +14,17 @@ package org.openhab.misc.addonsuggestionfinder.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Set;
+import java.util.regex.PatternSyntaxException;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.junit.jupiter.api.Test;
+import org.openhab.core.addon.AddonDiscoveryMethod;
 import org.openhab.core.addon.AddonInfo;
+import org.openhab.core.addon.AddonMatchProperty;
 import org.openhab.misc.addonsuggestionfinder.AddonSuggestionInfoProvider;
 
 /**
@@ -30,13 +35,40 @@ import org.openhab.misc.addonsuggestionfinder.AddonSuggestionInfoProvider;
 @NonNullByDefault
 class AddonSuggestionFinderTest {
 
+    private static final String DELIMITER = "\n\t";
+
     @Test
     void testAddonSuggestionInfoProvider() {
         AddonSuggestionInfoProvider provider = new AddonSuggestionInfoProvider();
         assertNotNull(provider);
         Set<AddonInfo> addonInfos = provider.getAddonInfos(Locale.US);
         assertNotNull(addonInfos);
-        AddonInfo addonInfo = provider.getAddonInfo("flying-aardvarks", Locale.US);
-        assertNull(addonInfo);
+
+        AddonInfo addonInfoBad = provider.getAddonInfo("flying-aardvarks", Locale.US);
+        assertNull(addonInfoBad);
+
+        /*
+         * At build time we run a regular expression syntax check on all the add-on infos resp. discovery methods and
+         * match properties in the bundle, in order to help developers identify regex syntax errors in their addon.xml
+         * files.
+         */
+        List<String> patternErrors = new ArrayList<>();
+        for (AddonInfo addonInfo : addonInfos) {
+            for (AddonDiscoveryMethod discoveryMethod : addonInfo.getDiscoveryMethods()) {
+                for (AddonMatchProperty matchProperty : discoveryMethod.getMatchProperties()) {
+                    try {
+                        matchProperty.getPattern();
+                    } catch (PatternSyntaxException e) {
+                        patternErrors.add(String.format(
+                                "Regex syntax error in org.openhab.%s.%s addon.xml => %s in \"%s\" position %d",
+                                addonInfo.getType(), addonInfo.getId(), e.getDescription(), e.getPattern(),
+                                e.getIndex()));
+                    }
+                }
+            }
+        }
+        if (!patternErrors.isEmpty()) {
+            fail(DELIMITER + String.join(DELIMITER, patternErrors));
+        }
     }
 }
