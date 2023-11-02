a13c8bb22fc7ec0b37f01790cd09f0b92b595bd8
Laurent ARNAL <laurent@clae.net>
2023-11-02 21:19:20 +0100

    add Metadata/SiemensHvacMetadataLanguage.java add Metadata/SiemensHvacMetadataUser.java class
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

---
+++ bundles/org.openhab.binding.siemenshvac/src/main/java/org/openhab/binding/siemenshvac/internal/Metadata/SiemensHvacMetadataLanguage.java
@@ -0,0 +1,41 @@
+package org.openhab.binding.siemenshvac.internal.Metadata;
+
+public class SiemensHvacMetadataLanguage {
+    private String name;
+    private int id;
+    private String language;
+    private int languageId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(int languageId) {
+        this.languageId = languageId;
+    }
+
+}
