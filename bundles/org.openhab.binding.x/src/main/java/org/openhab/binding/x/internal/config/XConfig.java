45b751a6d810575f9773ab3f2bd0d5f71cc90214
lsiepel <leosiepel@gmail.com>
2023-11-15 09:12:24 +0100

    [twitter] rename binding to X (#15809)
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.x/src/main/java/org/openhab/binding/x/internal/config/XConfig.java
+++ bundles/org.openhab.binding.x/src/main/java/org/openhab/binding/x/internal/config/XConfig.java
@@ -10,18 +10,18 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.twitter.internal.config;
+package org.openhab.binding.x.internal.config;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
 /**
- * The {@link TwitterConfig} class contains fields mapping thing configuration parameters.
+ * The {@link XConfig} class contains fields mapping thing configuration parameters.
  *
  * @author Scott Hanson - Initial contribution
  */
 
 @NonNullByDefault
-public class TwitterConfig {
+public class XConfig {
     public String consumerKey = "";
     public String consumerSecret = "";
     public String accessToken = "";
