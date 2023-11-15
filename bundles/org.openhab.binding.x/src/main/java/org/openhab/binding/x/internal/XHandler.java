38a9ac46f825740c66e30c21046c8dc98a185dba
lsiepel <leosiepel@gmail.com>
2023-11-15 09:12:24 +0100

    [twitter] rename binding to X (#15809)
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>
    Signed-off-by: Jørgen Austvik <jaustvik@acm.org>

--- bundles/org.openhab.binding.x/src/main/java/org/openhab/binding/x/internal/XHandler.java
+++ bundles/org.openhab.binding.x/src/main/java/org/openhab/binding/x/internal/XHandler.java
@@ -10,9 +10,9 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.twitter.internal;
+package org.openhab.binding.x.internal;
 
-import static org.openhab.binding.twitter.internal.TwitterBindingConstants.CHANNEL_LASTTWEET;
+import static org.openhab.binding.x.internal.XBindingConstants.CHANNEL_LASTPOST;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -27,8 +27,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import org.openhab.binding.twitter.internal.action.TwitterActions;
-import org.openhab.binding.twitter.internal.config.TwitterConfig;
+import org.openhab.binding.x.internal.action.XActions;
+import org.openhab.binding.x.internal.config.XConfig;
 import org.openhab.core.io.net.http.HttpUtil;
 import org.openhab.core.library.types.RawType;
 import org.openhab.core.library.types.StringType;
@@ -41,28 +41,26 @@ import org.openhab.core.types.Command;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import twitter4j.DirectMessage;
-import twitter4j.ResponseList;
-import twitter4j.Status;
-import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.auth.AccessToken;
+import twitter4j.v1.DirectMessage;
+import twitter4j.v1.ResponseList;
+import twitter4j.v1.Status;
+import twitter4j.v1.StatusUpdate;
 
 /**
- * The {@link TwitterHandler} is responsible for handling commands, which are
+ * The {@link XHandler} is responsible for handling commands, which are
  * sent to one of the channels.
  *
  * @author Scott Hanson - Initial contribution
  */
 
 @NonNullByDefault
-public class TwitterHandler extends BaseThingHandler {
+public class XHandler extends BaseThingHandler {
 
-    private final Logger logger = LoggerFactory.getLogger(TwitterHandler.class);
+    private final Logger logger = LoggerFactory.getLogger(XHandler.class);
 
-    private TwitterConfig config = new TwitterConfig();
+    private XConfig config = new XConfig();
 
     private @Nullable ScheduledFuture<?> refreshTask;
 
@@ -71,7 +69,7 @@ public class TwitterHandler extends BaseThingHandler {
     private static @Nullable Twitter client = null;
     boolean isProperlyConfigured = false;
 
-    public TwitterHandler(Thing thing) {
+    public XHandler(Thing thing) {
         super(thing);
     }
 
@@ -82,14 +80,14 @@ public class TwitterHandler extends BaseThingHandler {
     // creates list of available Actions
     @Override
     public Collection<Class<? extends ThingHandlerService>> getServices() {
-        return List.of(TwitterActions.class);
+        return List.of(XActions.class);
     }
 
     @Override
     public void initialize() {
-        config = getConfigAs(TwitterConfig.class);
+        config = getConfigAs(XConfig.class);
 
-        // create a New Twitter Client
+        // create a New X/Twitter Client
         Twitter localClient = createClient();
         client = localClient;
         refresh();// Get latest status
@@ -107,7 +105,7 @@ public class TwitterHandler extends BaseThingHandler {
     }
 
     /**
-     * Internal method for Getting Twitter Status
+     * Internal method for Getting X Status
      *
      */
     private void refresh() {
@@ -117,83 +115,83 @@ public class TwitterHandler extends BaseThingHandler {
             }
             Twitter localClient = client;
             if (localClient != null) {
-                ResponseList<Status> statuses = localClient.getUserTimeline();
+                ResponseList<Status> statuses = localClient.v1().timelines().getUserTimeline();
                 if (!statuses.isEmpty()) {
-                    updateState(CHANNEL_LASTTWEET, StringType.valueOf(statuses.get(0).getText()));
+                    updateState(CHANNEL_LASTPOST, StringType.valueOf(statuses.get(0).getText()));
                 } else {
                     logger.debug("No Statuses Found");
                 }
             }
         } catch (TwitterException e) {
-            logger.debug("Error when trying to refresh Twitter Account: {}", e.getMessage());
+            logger.debug("Error when trying to refresh X Account: {}", e.getMessage());
         }
     }
 
     /**
-     * Internal method for sending a tweet, with or without image
+     * Internal method for sending a post, with or without image
      *
-     * @param tweetTxt
-     *            text string to be sent as a Tweet
+     * @param postTxt
+     *            text string to be sent as a Post
      * @param fileToAttach
      *            the file to attach. May be null if no attached file.
      *
-     * @return <code>true</code>, if sending the tweet has been successful and
+     * @return <code>true</code>, if sending the post has been successful and
      *         <code>false</code> in all other cases.
      */
-    private boolean sendTweet(final String tweetTxt, final @Nullable File fileToAttach) {
+    private boolean sendPost(final String postTxt, final @Nullable File fileToAttach) {
         if (!checkPrerequisites()) {
             return false;
         }
-        // abbreviate the Tweet to meet the 280 character limit ...
-        String abbreviatedTweetTxt = abbreviateString(tweetTxt, CHARACTER_LIMIT);
+        // abbreviate the Post to meet the 280 character limit ...
+        String abbreviatedPostTxt = abbreviateString(postTxt, CHARACTER_LIMIT);
         try {
             Twitter localClient = client;
             if (localClient != null) {
-                // send the Tweet
-                StatusUpdate status = new StatusUpdate(abbreviatedTweetTxt);
+                // send the Post
+                StatusUpdate status = StatusUpdate.of(abbreviatedPostTxt);
                 if (fileToAttach != null && fileToAttach.isFile()) {
-                    status.setMedia(fileToAttach);
+                    status = status.media(fileToAttach);
                 }
-                Status updatedStatus = localClient.updateStatus(status);
-                logger.debug("Successfully sent Tweet '{}'", updatedStatus.getText());
-                updateState(CHANNEL_LASTTWEET, StringType.valueOf(updatedStatus.getText()));
+                Status updatedStatus = localClient.v1().tweets().updateStatus(status);
+                logger.debug("Successfully sent Post '{}'", updatedStatus.getText());
+                updateState(CHANNEL_LASTPOST, StringType.valueOf(updatedStatus.getText()));
                 return true;
             }
         } catch (TwitterException e) {
-            logger.warn("Failed to send Tweet '{}' because of : {}", abbreviatedTweetTxt, e.getLocalizedMessage());
+            logger.warn("Failed to send Post '{}' because of : {}", abbreviatedPostTxt, e.getLocalizedMessage());
         }
         return false;
     }
 
     /**
-     * Sends a standard Tweet.
+     * Sends a standard Post.
      *
-     * @param tweetTxt
-     *            text string to be sent as a Tweet
+     * @param postTxt
+     *            text string to be sent as a Post
      *
-     * @return <code>true</code>, if sending the tweet has been successful and
+     * @return <code>true</code>, if sending the post has been successful and
      *         <code>false</code> in all other cases.
      */
-    public boolean sendTweet(String tweetTxt) {
+    public boolean sendPost(String postTxt) {
         if (!checkPrerequisites()) {
             return false;
         }
-        return sendTweet(tweetTxt, (File) null);
+        return sendPost(postTxt, (File) null);
     }
 
     /**
-     * Sends a Tweet with an image
+     * Sends a Post with an image
      *
-     * @param tweetTxt
-     *            text string to be sent as a Tweet
-     * @param tweetPicture
+     * @param postTxt
+     *            text string to be sent as a Post
+     * @param postPicture
      *            the path of the picture that needs to be attached (either an url,
      *            either a path pointing to a local file)
      *
-     * @return <code>true</code>, if sending the tweet has been successful and
+     * @return <code>true</code>, if sending the post has been successful and
      *         <code>false</code> in all other cases.
      */
-    public boolean sendTweet(String tweetTxt, String tweetPicture) {
+    public boolean sendPost(String postTxt, String postPicture) {
         if (!checkPrerequisites()) {
             return false;
         }
@@ -201,18 +199,18 @@ public class TwitterHandler extends BaseThingHandler {
         // prepare the image attachment
         File fileToAttach = null;
         boolean deleteTemporaryFile = false;
-        if (tweetPicture.startsWith("http://") || tweetPicture.startsWith("https://")) {
+        if (postPicture.startsWith("http://") || postPicture.startsWith("https://")) {
             try {
                 // we have a remote url and need to download the remote file to a temporary location
                 Path tDir = Files.createTempDirectory("TempDirectory");
-                String path = tDir + File.separator + "openhab-twitter-remote_attached_file" + "."
-                        + getExtension(tweetPicture);
+                String path = tDir + File.separator + "openhab-x-remote_attached_file" + "."
+                        + getExtension(postPicture);
 
-                // URL url = new URL(tweetPicture);
+                // URL url = new URL(postPicture);
                 fileToAttach = new File(path);
                 deleteTemporaryFile = true;
 
-                RawType rawPicture = HttpUtil.downloadImage(tweetPicture);
+                RawType rawPicture = HttpUtil.downloadImage(postPicture);
                 if (rawPicture != null) {
                     try (FileOutputStream fos = new FileOutputStream(path)) {
                         fos.write(rawPicture.getBytes(), 0, rawPicture.getBytes().length);
@@ -222,24 +220,24 @@ public class TwitterHandler extends BaseThingHandler {
                         logger.debug("Could not write {} to temp dir. {}", path, ex.getMessage());
                     }
                 } else {
-                    logger.debug("Could not download tweet file from {}", tweetPicture);
+                    logger.debug("Could not download post file from {}", postPicture);
                 }
             } catch (IOException ex) {
-                logger.debug("Could not write {} to temp dir. {}", tweetPicture, ex.getMessage());
+                logger.debug("Could not write {} to temp dir. {}", postPicture, ex.getMessage());
             }
         } else {
             // we have a local file and can just use it directly
-            fileToAttach = new File(tweetPicture);
+            fileToAttach = new File(postPicture);
         }
 
         if (fileToAttach != null && fileToAttach.isFile()) {
-            logger.debug("Image '{}' correctly found, will be included in tweet", tweetPicture);
+            logger.debug("Image '{}' correctly found, will be included in post", postPicture);
         } else {
-            logger.warn("Image '{}' not found, will only tweet text", tweetPicture);
+            logger.warn("Image '{}' not found, will only post text", postPicture);
         }
 
-        // send the Tweet
-        boolean result = sendTweet(tweetTxt, fileToAttach);
+        // send the Post
+        boolean result = sendPost(postTxt, fileToAttach);
         // delete temp file (if needed)
         if (deleteTemporaryFile) {
             if (fileToAttach != null) {
@@ -272,10 +270,11 @@ public class TwitterHandler extends BaseThingHandler {
         try {
             Twitter localClient = client;
             if (localClient != null) {
-                // abbreviate the Tweet to meet the allowed character limit ...
+                // abbreviate the Post to meet the allowed character limit ...
                 String abbreviatedMessageTxt = abbreviateString(messageTxt, CHARACTER_LIMIT);
                 // send the direct message
-                DirectMessage message = localClient.sendDirectMessage(recipientId, abbreviatedMessageTxt);
+                DirectMessage message = localClient.v1().directMessages().sendDirectMessage(recipientId,
+                        abbreviatedMessageTxt);
                 logger.debug("Successfully sent direct message '{}' to @'{}'", message.getText(),
                         message.getRecipientId());
                 return true;
@@ -287,18 +286,18 @@ public class TwitterHandler extends BaseThingHandler {
     }
 
     /**
-     * check if twitter account was created with prerequisites
-     *
-     * @return <code>true</code>, if twitter account was initialized
+     * check if X account was created with prerequisites
+     * 
+     * @return <code>true</code>, if X account was initialized
      *         <code>false</code> in all other cases.
      */
     private boolean checkPrerequisites() {
         if (client == null) {
-            logger.debug("Twitter client is not yet configured > execution aborted!");
+            logger.debug("X client is not yet configured > execution aborted!");
             return false;
         }
         if (!isProperlyConfigured) {
-            logger.debug("Twitter client is not yet configured > execution aborted!");
+            logger.debug("X client is not yet configured > execution aborted!");
             return false;
         }
         return true;
@@ -310,9 +309,9 @@ public class TwitterHandler extends BaseThingHandler {
      * @return a new instance of a Twitter4J Twitter client.
      */
     private twitter4j.Twitter createClient() {
-        twitter4j.Twitter client = TwitterFactory.getSingleton();
-        client.setOAuthConsumer(config.consumerKey, config.consumerSecret);
-        client.setOAuthAccessToken(new AccessToken(config.accessToken, config.accessTokenSecret));
+        Twitter client = Twitter.newBuilder().oAuthConsumer(config.consumerKey, config.consumerSecret)
+                .oAuthAccessToken(config.accessToken, config.accessTokenSecret).build();
+
         return client;
     }
 
