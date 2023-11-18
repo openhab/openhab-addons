d28c86050488326873b0b5d97522f4e76a30b075
Jacob Laursen <jacob-github@vindvejr.dk>
2023-11-18 12:44:09 +0100

    [hdpowerview] Improve color channel for Repeater and remove redundant brightness channel (#15880)
    * Improve color channel
    * Log warning for unexpected color command
    
    ---------
    
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>
    Signed-off-by: Jørgen Austvik <jaustvik@acm.org>

--- bundles/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/internal/HDPowerViewBindingConstants.java
+++ bundles/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/internal/HDPowerViewBindingConstants.java
@@ -51,7 +51,6 @@ public class HDPowerViewBindingConstants {
     public static final String CHANNEL_SHADE_REPEATER_RSSI = "repeaterRssi";
 
     public static final String CHANNEL_REPEATER_COLOR = "color";
-    public static final String CHANNEL_REPEATER_BRIGHTNESS = "brightness";
     public static final String CHANNEL_REPEATER_IDENTIFY = "identify";
     public static final String CHANNEL_REPEATER_BLINKING_ENABLED = "blinkingEnabled";
 
