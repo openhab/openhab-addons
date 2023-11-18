a535caa13c657c443934c8eb15a98461b31b7254
Markus Michels <markus7017@gmail.com>
2023-11-18 21:50:10 +0100

    [shelly] BLU Motion,  optimize ShellyManager for BLU devices (#15401)
    Signed-off-by: Markus Michels <markus7017@gmail.com>

--- bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/handler/ShellyBaseHandler.java
+++ bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/handler/ShellyBaseHandler.java
@@ -1019,6 +1019,7 @@ public abstract class ShellyBaseHandler extends BaseThingHandler
             config.password = bindingConfig.defaultPassword;
             logger.debug("{}: Using default password from bindingConfig (userId={})", thingName, config.userId);
         }
+
         if (config.updateInterval == 0) {
             config.updateInterval = UPDATE_STATUS_INTERVAL_SECONDS * UPDATE_SKIP_COUNT;
         }
