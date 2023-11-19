233edfef48995c057b4020e516fbac8a78a45170
Markus Michels <markus7017@gmail.com>
2023-11-19 19:59:04 +0100

    review change
    Signed-off-by: Markus Michels <markus7017@gmail.com>

--- bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/api1/Shelly1CoapHandler.java
+++ bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/api1/Shelly1CoapHandler.java
@@ -276,8 +276,6 @@ public class Shelly1CoapHandler implements Shelly1CoapListener {
             // If we received a CoAP message successful the thing must be online
             thingHandler.setThingOnline();
 
-            thingHandler.setThingOnline();
-
             // The device changes the serial on every update, receiving a message with the same serial is a
             // duplicate, excep for battery devices! Those reset the serial every time when they wake-up
             if ((serial == lastSerial) && payload.equals(lastPayload) && (!profile.hasBattery
