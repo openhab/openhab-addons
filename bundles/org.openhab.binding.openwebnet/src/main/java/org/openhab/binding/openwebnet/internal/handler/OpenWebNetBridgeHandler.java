3b12960a6492df40dc4cd654478f6c811ea34731
Massimo Valla <mvcode00@gmail.com>
2023-12-08 15:15:22 +0100

    [openwebnet] updated README and where param description
    Signed-off-by: Massimo Valla <mvcode00@gmail.com>

--- bundles/org.openhab.binding.openwebnet/src/main/java/org/openhab/binding/openwebnet/internal/handler/OpenWebNetBridgeHandler.java
+++ bundles/org.openhab.binding.openwebnet/src/main/java/org/openhab/binding/openwebnet/internal/handler/OpenWebNetBridgeHandler.java
@@ -112,8 +112,8 @@ public class OpenWebNetBridgeHandler extends ConfigStatusBridgeHandler implement
                 if (oneHandler == null) {
                     oneHandler = h;
                 }
-                logger.warn("/////////////////////// +++++++++++++++ Added handler {} to Area {}", oId, area);
-                logger.warn("/////////////////////// Map: {}", this.toString());
+                logger.debug("/////////////////////// +++++++++++++++ Added handler {} to Area {}", oId, area);
+                logger.debug("/////////////////////// Map: {}", this.toString());
             }
         }
 
@@ -127,9 +127,9 @@ public class OpenWebNetBridgeHandler extends ConfigStatusBridgeHandler implement
                     if (removed && oh != null && oh.equals(h)) {
                         oneHandler = getFirst();
                     }
-                    logger.warn("/////////////////////// ---------------- Removed handler {} from Area {}", h.ownId,
+                    logger.debug("/////////////////////// ---------------- Removed handler {} from Area {}", h.ownId,
                             area);
-                    logger.warn("/////////////////////// Map: {}", this.toString());
+                    logger.debug("/////////////////////// Map: {}", this.toString());
                 }
             }
         }
