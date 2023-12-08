42a7b36ca8e5efe0130bb54458935286d7ff7d52
Massimo Valla <mvcode00@gmail.com>
2023-12-08 11:58:55 +0100

    [openwebnet] correct bug with area events + stylecheck
    Signed-off-by: Massimo Valla <mvcode00@gmail.com>

--- bundles/org.openhab.binding.openwebnet/src/main/java/org/openhab/binding/openwebnet/internal/handler/OpenWebNetBridgeHandler.java
+++ bundles/org.openhab.binding.openwebnet/src/main/java/org/openhab/binding/openwebnet/internal/handler/OpenWebNetBridgeHandler.java
@@ -93,7 +93,7 @@ public class OpenWebNetBridgeHandler extends ConfigStatusBridgeHandler implement
      * AREA.
      *
      */
-    protected class LightAutomHandlersMap {
+    public class LightAutomHandlersMap {
 
         private Map<Integer, Map<String, OpenWebNetThingHandler>> hndlrsMap;
         private @Nullable OpenWebNetThingHandler oneHandler = null;
@@ -108,7 +108,7 @@ public class OpenWebNetBridgeHandler extends ConfigStatusBridgeHandler implement
             }
             Map<String, OpenWebNetThingHandler> areaHndlrs = hndlrsMap.get(Integer.valueOf(area));
             final String oId = h.ownId;
-            if (oId != null) {
+            if (areaHndlrs != null && oId != null) {
                 areaHndlrs.put(oId, h);
                 if (oneHandler == null) {
                     oneHandler = h;
@@ -123,7 +123,9 @@ public class OpenWebNetBridgeHandler extends ConfigStatusBridgeHandler implement
                 Map<String, OpenWebNetThingHandler> areaHndlrs = hndlrsMap.get(Integer.valueOf(area));
                 if (areaHndlrs != null) {
                     boolean removed = areaHndlrs.remove(h.ownId, h);
-                    if (removed && oneHandler.equals(h)) {
+                    OpenWebNetThingHandler oh = oneHandler;
+                    // if the removed handler was linked by oneHandler, find another one
+                    if (removed && oh != null && oh.equals(h)) {
                         oneHandler = getFirst();
                     }
                     logger.warn("/////////////////////// ---------------- Removed handler {} from Area {}", h.ownId,
@@ -175,7 +177,7 @@ public class OpenWebNetBridgeHandler extends ConfigStatusBridgeHandler implement
                     OpenWebNetThingHandler hndlr = innerEntry.getValue();
                     if (hndlr != null) {
                         WhereLightAutom wl = (WhereLightAutom) hndlr.deviceWhere;
-                        if (wl.isArea()) {
+                        if (wl != null && wl.isAPL()) {
                             return hndlr;
                         }
                     }
@@ -198,8 +200,11 @@ public class OpenWebNetBridgeHandler extends ConfigStatusBridgeHandler implement
                 }
             }
             log += "\n# getAllHandlers: ";
-            for (OpenWebNetThingHandler e : getAllHandlers()) {
-                log += " " + e.ownId;
+            List<OpenWebNetThingHandler> allHandlers = getAllHandlers();
+            if (allHandlers != null) {
+                for (OpenWebNetThingHandler e : allHandlers) {
+                    log += " " + e.ownId;
+                }
             }
             OpenWebNetThingHandler one = this.getOne();
             log += "\n# getOne() = " + (one == null ? "null" : one.ownId);
@@ -598,6 +603,16 @@ public class OpenWebNetBridgeHandler extends ConfigStatusBridgeHandler implement
         LightAutomHandlersMap lm = lightsMap;
         if (lm != null) {
             return lm.getAllHandlers();
+        } else {
+            return null;
+        }
+    }
+
+    @Nullable
+    public LightAutomHandlersMap getLightsMap() {
+        LightAutomHandlersMap lm = lightsMap;
+        if (lm != null) {
+            return lm;
         } else {
             return null;
         }
