3f3ba2cfe3ec3a7c6b65c46d6b27989efd40f680
Sönke Küper <soenkekueper@gmx.de>
2023-12-18 21:15:06 +0100

    0000: Generated Binding Skeleton
    Signed-off-by: Sönke Küper <soenkekueper@gmx.de>

---
+++ bundles/org.openhab.binding.modbus.sungrow/src/main/java/org/openhab/binding/modbus/sungrow/internal/ModbusSungrowConfiguration.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2010-2023 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.modbus.sungrow.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link ModbusSungrowConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Sönke Küper - Initial contribution
+ */
+@NonNullByDefault
+public class ModbusSungrowConfiguration {
+
+    /**
+     * Sample configuration parameters. Replace with your own.
+     */
+    public String hostname = "";
+    public String password = "";
+    public int refreshInterval = 600;
+}
