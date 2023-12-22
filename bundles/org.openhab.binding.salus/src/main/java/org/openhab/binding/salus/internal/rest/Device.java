e51f4305c7a646b37dc20555e65ceb62e6a00394
Martin Grześlowski <martin.grzeslowski@gmail.com>
2023-12-22 20:07:35 +0100

    'if' construct must use '{}'s.
    Signed-off-by: Martin Grześlowski <martin.grzeslowski@gmail.com>

--- bundles/org.openhab.binding.salus/src/main/java/org/openhab/binding/salus/internal/rest/Device.java
+++ bundles/org.openhab.binding.salus/src/main/java/org/openhab/binding/salus/internal/rest/Device.java
@@ -44,10 +44,12 @@ public record Device(@NotNull String dsn, @NotNull String name,
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
-        if (o == null || getClass() != o.getClass())
+        }
+        if (o == null || getClass() != o.getClass()) {
             return false;
+        }
 
         Device device = (Device) o;
 
