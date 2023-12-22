a20bfa5409952c7928a5b438afccc7c7506d38c1
Martin Grześlowski <martin.grzeslowski@gmail.com>
2023-12-22 20:08:20 +0100

    'if' construct must use '{}'s.
    Signed-off-by: Martin Grześlowski <martin.grzeslowski@gmail.com>

--- bundles/org.openhab.binding.salus/src/main/java/org/openhab/binding/salus/internal/rest/DeviceProperty.java
+++ bundles/org.openhab.binding.salus/src/main/java/org/openhab/binding/salus/internal/rest/DeviceProperty.java
@@ -85,10 +85,12 @@ public abstract sealed class DeviceProperty<T> implements Comparable<DevicePrope
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
-        if (o == null || getClass() != o.getClass())
+        }
+        if (o == null || getClass() != o.getClass()) {
             return false;
+        }
 
         DeviceProperty<?> that = (DeviceProperty<?>) o;
 
