624197476f59fe0496c78642ad82e5c48e8ebfb4
Jan N. Klug <github@klug.nrw>
2023-12-28 18:51:08 +0100

    boschindego
    Signed-off-by: Jan N. Klug <github@klug.nrw>

--- bundles/org.openhab.binding.boschindego/src/main/java/org/openhab/binding/boschindego/internal/discovery/IndegoDiscoveryService.java
+++ bundles/org.openhab.binding.boschindego/src/main/java/org/openhab/binding/boschindego/internal/discovery/IndegoDiscoveryService.java
@@ -19,19 +19,18 @@ import java.util.Collection;
 import java.util.Set;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.binding.boschindego.internal.IndegoTypeDatabase;
 import org.openhab.binding.boschindego.internal.dto.response.DevicePropertiesResponse;
 import org.openhab.binding.boschindego.internal.exceptions.IndegoException;
 import org.openhab.binding.boschindego.internal.handler.BoschAccountHandler;
-import org.openhab.core.config.discovery.AbstractDiscoveryService;
+import org.openhab.core.config.discovery.AbstractThingHandlerDiscoveryService;
 import org.openhab.core.config.discovery.DiscoveryResult;
 import org.openhab.core.config.discovery.DiscoveryResultBuilder;
 import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.ThingTypeUID;
 import org.openhab.core.thing.ThingUID;
-import org.openhab.core.thing.binding.ThingHandler;
-import org.openhab.core.thing.binding.ThingHandlerService;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,29 +39,16 @@ import org.slf4j.LoggerFactory;
  *
  * @author Jacob Laursen - Initial contribution
  */
+@Component(scope = ServiceScope.PROTOTYPE, service = IndegoDiscoveryService.class)
 @NonNullByDefault
-public class IndegoDiscoveryService extends AbstractDiscoveryService implements ThingHandlerService {
+public class IndegoDiscoveryService extends AbstractThingHandlerDiscoveryService<BoschAccountHandler> {
 
     private static final int TIMEOUT_SECONDS = 60;
 
     private final Logger logger = LoggerFactory.getLogger(IndegoDiscoveryService.class);
 
-    private @NonNullByDefault({}) BoschAccountHandler accountHandler;
-
     public IndegoDiscoveryService() {
-        super(Set.of(THING_TYPE_ACCOUNT), TIMEOUT_SECONDS, false);
-    }
-
-    @Override
-    public @Nullable ThingHandler getThingHandler() {
-        return accountHandler;
-    }
-
-    @Override
-    public void setThingHandler(ThingHandler handler) {
-        if (handler instanceof BoschAccountHandler accountHandler) {
-            this.accountHandler = accountHandler;
-        }
+        super(BoschAccountHandler.class, Set.of(THING_TYPE_ACCOUNT), TIMEOUT_SECONDS, false);
     }
 
     @Override
@@ -73,6 +59,10 @@ public class IndegoDiscoveryService extends AbstractDiscoveryService implements 
     @Override
     public void startScan() {
         try {
+            BoschAccountHandler accountHandler = this.thingHandler;
+            if (accountHandler == null) {
+                return;
+            }
             Collection<DevicePropertiesResponse> devices = accountHandler.getDevices();
 
             ThingUID bridgeUID = accountHandler.getThing().getUID();
@@ -97,7 +87,7 @@ public class IndegoDiscoveryService extends AbstractDiscoveryService implements 
     }
 
     @Override
-    public void deactivate() {
+    public void dispose() {
         removeOlderResults(Instant.now().getEpochSecond());
     }
 }
