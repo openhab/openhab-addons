0e7a1980d4d2dc25b7cfe5005282ee2d4dc60768
Jan N. Klug <github@klug.nrw>
2023-12-30 17:45:52 +0100

    qolsysiq
    Signed-off-by: Jan N. Klug <github@klug.nrw>

--- bundles/org.openhab.binding.qolsysiq/src/main/java/org/openhab/binding/qolsysiq/internal/discovery/QolsysIQChildDiscoveryService.java
+++ bundles/org.openhab.binding.qolsysiq/src/main/java/org/openhab/binding/qolsysiq/internal/discovery/QolsysIQChildDiscoveryService.java
@@ -15,10 +15,9 @@ package org.openhab.binding.qolsysiq.internal.discovery;
 import java.util.Set;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.binding.qolsysiq.internal.QolsysIQBindingConstants;
 import org.openhab.binding.qolsysiq.internal.handler.QolsysIQChildDiscoveryHandler;
-import org.openhab.core.config.discovery.AbstractDiscoveryService;
+import org.openhab.core.config.discovery.AbstractThingHandlerDiscoveryService;
 import org.openhab.core.config.discovery.DiscoveryResult;
 import org.openhab.core.config.discovery.DiscoveryResultBuilder;
 import org.openhab.core.config.discovery.DiscoveryService;
@@ -36,30 +35,20 @@ import org.slf4j.LoggerFactory;
  *
  */
 @NonNullByDefault
-public class QolsysIQChildDiscoveryService extends AbstractDiscoveryService
+public class QolsysIQChildDiscoveryService extends AbstractThingHandlerDiscoveryService<QolsysIQChildDiscoveryHandler>
         implements DiscoveryService, ThingHandlerService {
     private final Logger logger = LoggerFactory.getLogger(QolsysIQChildDiscoveryService.class);
 
     private static final Set<ThingTypeUID> SUPPORTED_DISCOVERY_THING_TYPES_UIDS = Set
             .of(QolsysIQBindingConstants.THING_TYPE_PARTITION, QolsysIQBindingConstants.THING_TYPE_ZONE);
 
-    private @Nullable ThingHandler thingHandler;
-
     public QolsysIQChildDiscoveryService() throws IllegalArgumentException {
-        super(SUPPORTED_DISCOVERY_THING_TYPES_UIDS, 5, false);
+        super(QolsysIQChildDiscoveryHandler.class, SUPPORTED_DISCOVERY_THING_TYPES_UIDS, 5, false);
     }
 
     @Override
-    public void setThingHandler(ThingHandler handler) {
-        if (handler instanceof QolsysIQChildDiscoveryHandler childDiscoveryHandler) {
-            childDiscoveryHandler.setDiscoveryService(this);
-            this.thingHandler = handler;
-        }
-    }
-
-    @Override
-    public @Nullable ThingHandler getThingHandler() {
-        return thingHandler;
+    public void initialize() {
+        thingHandler.setDiscoveryService(this);
     }
 
     @Override
@@ -68,16 +57,6 @@ public class QolsysIQChildDiscoveryService extends AbstractDiscoveryService
         if (handler != null) {
             ((QolsysIQChildDiscoveryHandler) handler).startDiscovery();
         }
-    }
-
-    @Override
-    public void activate() {
-        super.activate(null);
-    }
-
-    @Override
-    public void deactivate() {
-        super.deactivate();
     }
 
     public void discoverQolsysIQChildThing(ThingUID thingUID, ThingUID bridgeUID, Integer id, String label) {
