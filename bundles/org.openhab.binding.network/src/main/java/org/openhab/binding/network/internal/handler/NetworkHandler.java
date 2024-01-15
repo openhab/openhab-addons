84d4dd3ef4c4e571ff0f1f06e16e6811132af229
lsiepel <leosiepel@gmail.com>
2024-01-15 22:37:38 +0100

    [network] Add configuration parameter for using iOS wake-up (#16259)
    Fixes #9575
    
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.network/src/main/java/org/openhab/binding/network/internal/handler/NetworkHandler.java
+++ bundles/org.openhab.binding.network/src/main/java/org/openhab/binding/network/internal/handler/NetworkHandler.java
@@ -184,11 +184,7 @@ public class NetworkHandler extends BaseThingHandler
             }
             presenceDetection.setServicePorts(Set.of(port));
         } else {
-            // It does not harm to send an additional UDP packet to a device,
-            // therefore we assume all ping devices are iOS devices. If this
-            // does not work for all users for some obscure reason, we can make
-            // this a thing configuration variable.
-            presenceDetection.setIOSDevice(true);
+            presenceDetection.setIOSDevice(handlerConfiguration.useIOSWakeUp);
             // Hand over binding configurations to the network service
             presenceDetection.setUseDhcpSniffing(configuration.allowDHCPlisten);
             presenceDetection.setUseIcmpPing(configuration.allowSystemPings);
@@ -214,7 +210,6 @@ public class NetworkHandler extends BaseThingHandler
         properties.put(NetworkBindingConstants.PROPERTY_ARP_STATE, presenceDetection.getArpPingState());
         properties.put(NetworkBindingConstants.PROPERTY_ICMP_STATE, presenceDetection.getIPPingState());
         properties.put(NetworkBindingConstants.PROPERTY_PRESENCE_DETECTION_TYPE, "");
-        properties.put(NetworkBindingConstants.PROPERTY_IOS_WAKEUP, presenceDetection.isIOSdevice() ? "Yes" : "No");
         properties.put(NetworkBindingConstants.PROPERTY_DHCP_STATE, presenceDetection.getDhcpState());
         updateProperties(properties);
     }
