839a384107317cc00288413288fa45ce3002a711
AntonJansen <gradius@fmf.nl>
2024-01-25 22:10:58 +0100

    Created a distinction between a RM 4 Mini and RM 4 Pro. Cleaned up the code.
    Signed-off-by: AntonJansen <gradius@fmf.nl>

---
+++ bundles/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/handler/BroadlinkRemoteModel4ProHandler.java
@@ -0,0 +1,13 @@
+package org.openhab.binding.broadlink.handler;
+
+import org.openhab.binding.broadlink.internal.BroadlinkRemoteDynamicCommandDescriptionProvider;
+import org.openhab.core.thing.Thing;
+
+public class BroadlinkRemoteModel4ProHandler extends BroadlinkRemoteModel4MiniHandler {
+
+    public BroadlinkRemoteModel4ProHandler(Thing thing,
+            BroadlinkRemoteDynamicCommandDescriptionProvider commandDescriptionProvider) {
+        super(thing, commandDescriptionProvider);
+        // TODO Auto-generated constructor stub
+    }
+}
