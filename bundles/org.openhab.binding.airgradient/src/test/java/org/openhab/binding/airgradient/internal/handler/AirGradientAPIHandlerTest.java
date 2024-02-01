7d26b3eeee5bafb730849f9a7c46da64e3f44c82
Jørgen Austvik <jaustvik@acm.org>
2024-02-01 20:27:38 +0100

    Add extra test case with updated sensors
    Signed-off-by: Jørgen Austvik <jaustvik@acm.org>

--- bundles/org.openhab.binding.airgradient/src/test/java/org/openhab/binding/airgradient/internal/handler/AirGradientAPIHandlerTest.java
+++ bundles/org.openhab.binding.airgradient/src/test/java/org/openhab/binding/airgradient/internal/handler/AirGradientAPIHandlerTest.java
@@ -58,6 +58,9 @@ public class AirGradientAPIHandlerTest {
              {"locationId":1234,"locationName":"Some Name","pm01":1,"pm02":2,"pm10":2,"pm003Count":536,"atmp":20.45,"rhum":16.61,"rco2":null,"tvoc":null,"wifi":-54,"timestamp":"2024-01-07T13:00:20.000Z","ledMode":"co2","ledCo2Threshold1":1000,"ledCo2Threshold2":2000,"ledCo2ThresholdEnd":4000,"serialno":"serial","firmwareVersion":null,"tvocIndex":null,"noxIndex":null},
              {"locationId":4321,"locationName":"Some other name","pm01":1,"pm02":2,"pm10":2,"pm003Count":536,"atmp":20.45,"rhum":16.61,"rco2":null,"tvoc":null,"wifi":-54,"timestamp":"2024-01-07T13:00:20.000Z","ledMode":"co2","ledCo2Threshold1":1000,"ledCo2Threshold2":2000,"ledCo2ThresholdEnd":4000,"serialno":"serial","firmwareVersion":null,"tvocIndex":null,"noxIndex":null}
             ]""";
+    private static final String MULTI_CONTENT2 = """
+            [{"locationId":654321,"locationName":"xxxx","pm01":0,"pm02":1,"pm10":1,"pm003Count":null,"atmp":24.2,"rhum":18,"rco2":519,"tvoc":50.793266,"wifi":-62,"timestamp":"2024-02-01T19:15:37.000Z","ledMode":"co2","ledCo2Threshold1":1000,"ledCo2Threshold2":2000,"ledCo2ThresholdEnd":4000,"serialno":"ecda3b1a2a50","firmwareVersion":null,"tvocIndex":52,"noxIndex":1},{"locationId":123456,"locationName":"yyyy","pm01":0,"pm02":0,"pm10":0,"pm003Count":105,"atmp":22.33,"rhum":24,"rco2":468,"tvoc":130.95694,"wifi":-50,"timestamp":"2024-02-01T19:15:34.000Z","ledMode":"co2","ledCo2Threshold1":1000,"ledCo2Threshold2":2000,"ledCo2ThresholdEnd":4000,"serialno":"84fce612e644","firmwareVersion":null,"tvocIndex":137,"noxIndex":1}]
+            """;
 
     @Nullable
     private AirGradientAPIHandler sut;
@@ -121,6 +124,20 @@ public class AirGradientAPIHandlerTest {
         assertThat(res.size(), is(2));
         assertThat(res.get(0).locationName, is("Some Name"));
         assertThat(res.get(1).locationName, is("Some other name"));
+    }
+
+    @Test
+    public void testGetMeasuresMulti2() throws Exception {
+        ContentResponse response = Mockito.mock(ContentResponse.class);
+        Mockito.when(httpClientMock.GET(anyString())).thenReturn(response);
+        Mockito.when(response.getStatus()).thenReturn(200);
+        Mockito.when(response.getContentAsString()).thenReturn(MULTI_CONTENT2);
+
+        var res = sut.getMeasures();
+        assertThat(res, is(not(empty())));
+        assertThat(res.size(), is(2));
+        assertThat(res.get(0).locationName, is("xxxx"));
+        assertThat(res.get(1).locationName, is("yyyy"));
     }
 
     @Test
