d8e56901c431541571b7cc340d0fe9bf68213a09
Henrik Tóth <realthk@gmail.com>
2024-02-10 15:17:43 +0100

    [solax] Add support for Solax X3MIC / G2 inverter and workmode channel (#16248)
    * Support for Solax X3MIC / G2 inverter, and workmode channel for the existing X1, X3 also
    * Add update instructions and make raise the target version to 1
    * Refactor the workmode to be enum instead of string constants
    
    Signed-off-by: Henrik Tóth <realthk@gmail.com>
    Signed-off-by: Konstantin Polihronov <polychronov@gmail.com>
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>
    Signed-off-by: Jørgen Austvik <jaustvik@acm.org>

--- bundles/org.openhab.binding.solax/src/main/java/org/openhab/binding/solax/internal/model/impl/X3MicOrProG2InverterData.java
+++ bundles/org.openhab.binding.solax/src/main/java/org/openhab/binding/solax/internal/model/impl/X3MicOrProG2InverterData.java
@@ -19,12 +19,13 @@ import org.openhab.binding.solax.internal.connectivity.rawdata.LocalConnectRawDa
  * The {@link X3HybridG4InverterData} is responsible for handling commands, which are
  * sent to one of the channels.
  *
- * @author Konstantin Polihronov - Initial contribution
+ * @author Henrik Tóth - Initial contribution
+ *         (based on X1/X3 G4 parser from Konstantin Polihronov)
  */
 @NonNullByDefault
-public class X3HybridG4InverterData extends CommonInverterData {
+public class X3MicOrProG2InverterData extends CommonInverterData {
 
-    public X3HybridG4InverterData(LocalConnectRawDataBean data) {
+    public X3MicOrProG2InverterData(LocalConnectRawDataBean data) {
         super(data);
     }
 
@@ -76,18 +77,13 @@ public class X3HybridG4InverterData extends CommonInverterData {
     }
 
     @Override
-    public short getTotalOutputPower() {
-        return getData(9);
-    }
-
-    @Override
     public double getPV1Voltage() {
-        return ((double) getData(10)) / 10;
+        return ((double) getData(9)) / 10;
     }
 
     @Override
     public double getPV2Voltage() {
-        return ((double) getData(11)) / 10;
+        return ((double) getData(10)) / 10;
     }
 
     @Override
@@ -102,127 +98,56 @@ public class X3HybridG4InverterData extends CommonInverterData {
 
     @Override
     public short getPV1Power() {
-        return getData(14);
-    }
-
-    @Override
-    public short getPV2Power() {
         return getData(15);
     }
 
     @Override
+    public short getPV2Power() {
+        return getData(16);
+    }
+
+    @Override
     public double getFrequencyPhase1() {
-        return ((double) getData(16)) / 100;
-    }
-
-    @Override
-    public double getFrequencyPhase2() {
-        return ((double) getData(17)) / 100;
-    }
-
-    @Override
-    public double getFrequencyPhase3() {
         return ((double) getData(18)) / 100;
     }
 
     @Override
+    public double getFrequencyPhase2() {
+        return ((double) getData(19)) / 100;
+    }
+
+    @Override
+    public double getFrequencyPhase3() {
+        return ((double) getData(20)) / 100;
+    }
+
+    @Override
     public short getInverterWorkModeCode() {
-        return getData(19);
-    }
-
-    // Battery
-
-    @Override
-    public double getBatteryVoltage() {
-        return ((double) getData(39)) / 100;
-    }
-
-    @Override
-    public double getBatteryCurrent() {
-        return ((double) getData(40)) / 100;
-    }
-
-    @Override
-    public short getBatteryPower() {
-        return getData(41);
-    }
-
-    @Override
-    public short getBatteryTemperature() {
-        return getData(105);
-    }
-
-    @Override
-    public short getBatteryLevel() {
-        return getData(103);
-    }
-
-    // Feed in power
-
-    @Override
-    public short getFeedInPower() {
-        return (short) (getData(34) - getData(35));
-    }
-
-    // Totals
-
-    @Override
-    public short getPowerUsage() {
-        return getData(47);
+        return getData(21);
     }
 
     @Override
     public double getTotalEnergy() {
-        return ((double) getData(68)) / 10;
-    }
-
-    @Override
-    public short getTotalBatteryDischargeEnergy() {
-        return getData(74);
-    }
-
-    @Override
-    public short getTotalBatteryChargeEnergy() {
-        return getData(76);
-    }
-
-    @Override
-    public double getTotalPVEnergy() {
-        return ((double) getData(80)) / 10;
-    }
-
-    @Override
-    public short getTotalFeedInEnergy() {
-        return getData(86);
-    }
-
-    @Override
-    public double getTotalConsumption() {
-        return ((double) getData(88)) / 10;
+        return ((double) getData(22)) / 10;
     }
 
     @Override
     public double getTodayEnergy() {
-        return ((double) getData(82)) / 10;
+        return ((double) getData(24)) / 10;
     }
 
     @Override
-    public double getTodayFeedInEnergy() {
-        return ((double) getData(90)) / 100;
+    public short getInverterTemperature1() {
+        return getData(26);
     }
 
     @Override
-    public double getTodayConsumption() {
-        return ((double) getData(92)) / 100;
+    public short getInverterTemperature2() {
+        return getData(27);
     }
 
     @Override
-    public double getTodayBatteryDischargeEnergy() {
-        return ((double) getData(78)) / 10;
-    }
-
-    @Override
-    public double getTodayBatteryChargeEnergy() {
-        return ((double) getData(79)) / 10;
+    public short getTotalOutputPower() {
+        return getData(78);
     }
 }
