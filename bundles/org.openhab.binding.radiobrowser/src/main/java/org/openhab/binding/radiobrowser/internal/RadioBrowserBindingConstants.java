ff4502531dc42843d2abb423055d15fadb67cf5e
Matthew Skinner <matt@pcmus.com>
2024-02-13 19:24:45 +1100

    Update bundles/org.openhab.binding.radiobrowser/src/main/java/org/openhab/binding/radiobrowser/internal/RadioBrowserBindingConstants.java
    Co-authored-by: lsiepel <leosiepel@gmail.com>
    Signed-off-by: Matthew Skinner <matt@pcmus.com>

--- bundles/org.openhab.binding.radiobrowser/src/main/java/org/openhab/binding/radiobrowser/internal/RadioBrowserBindingConstants.java
+++ bundles/org.openhab.binding.radiobrowser/src/main/java/org/openhab/binding/radiobrowser/internal/RadioBrowserBindingConstants.java
@@ -40,5 +40,5 @@ public class RadioBrowserBindingConstants {
     public static final String CHANNEL_ICON = "icon";
     public static final String CHANNEL_STREAM = "stream";
     public static final String CHANNEL_ADD_FAVORITE = "addFavorite";
-    public static final String CHANNEL_REMOVE_FAVORITE = "removeFavorite";
+    public static final String CHANNEL_REMOVE_FAVORITE = "remove-favorite";
 }
