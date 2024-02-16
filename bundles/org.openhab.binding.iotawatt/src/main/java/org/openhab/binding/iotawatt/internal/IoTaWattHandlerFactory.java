3bdcdbd6075c34d4241af0a741bed95590f41001
Peter Rosenberg <prosenb.dev@gmail.com>
2024-02-16 23:40:31 +1100

    [iotawatt] generate new binding
    Signed-off-by: Peter Rosenberg <prosenb.dev@gmail.com>

---
+++ bundles/org.openhab.binding.iotawatt/src/main/java/org/openhab/binding/iotawatt/internal/IoTaWattHandlerFactory.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.iotawatt.internal;
+
+import static org.openhab.binding.iotawatt.internal.IoTaWattBindingConstants.*;
+
+import java.util.Set;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.thing.ThingTypeUID;
+import org.openhab.core.thing.binding.BaseThingHandlerFactory;
+import org.openhab.core.thing.binding.ThingHandler;
+import org.openhab.core.thing.binding.ThingHandlerFactory;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * The {@link IoTaWattHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author Peter Rosenberg - Initial contribution
+ */
+@NonNullByDefault
+@Component(configurationPid = "binding.iotawatt", service = ThingHandlerFactory.class)
+public class IoTaWattHandlerFactory extends BaseThingHandlerFactory {
+
+    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Set.of(THING_TYPE_SAMPLE);
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
+
+    @Override
+    protected @Nullable ThingHandler createHandler(Thing thing) {
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (THING_TYPE_SAMPLE.equals(thingTypeUID)) {
+            return new IoTaWattHandler(thing);
+        }
+
+        return null;
+    }
+}
