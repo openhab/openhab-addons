97b82c8823cee53feec627c43bfb71eeb764d9c8
Holger Friedrich <mail@holger-friedrich.de>
2024-03-04 01:18:27 +0100

    next try
    Signed-off-by: Holger Friedrich <mail@holger-friedrich.de>

--- bundles/org.openhab.binding.knx/src/main/java/org/openhab/binding/knx/internal/dpt/ValueDecoder.java
+++ bundles/org.openhab.binding.knx/src/main/java/org/openhab/binding/knx/internal/dpt/ValueDecoder.java
@@ -213,9 +213,11 @@ public class ValueDecoder {
                     // time is last block and may contain . and ,
                     int sep = java.lang.Math.max(value.indexOf(" % "), value.indexOf(" K "));
                     String time = value.substring(sep + 3);
+                    LOGGER.warn("RAW \"{}\", \"{}\"", value, time);
                     if (TSD_SEPARATOR.matcher(time).matches()) {
                         int dp = time.indexOf('.');
                         value = value.substring(0, sep + dp + 3) + time.substring(dp + 1);
+                        LOGGER.warn("FIX \"{}\", {}", value, dp);
                     }
                     return StringType.valueOf(value.replace(',', '.').replace(". ", ", "));
                 case "232":
