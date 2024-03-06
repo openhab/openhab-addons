68caed220a30b80bce202cabbbf5a27a3da5a69d
Doug Culnane <32482395+dougculnane@users.noreply.github.com>
2024-03-06 15:28:45 +0100

    Update bundles/org.openhab.binding.saicismart/src/main/java/org/openhab/binding/saicismart/internal/SAICiSMARTBridgeHandler.java
    Co-authored-by: lsiepel <leosiepel@gmail.com>
    Signed-off-by: Doug Culnane <32482395+dougculnane@users.noreply.github.com>

--- bundles/org.openhab.binding.saicismart/src/main/java/org/openhab/binding/saicismart/internal/SAICiSMARTBridgeHandler.java
+++ bundles/org.openhab.binding.saicismart/src/main/java/org/openhab/binding/saicismart/internal/SAICiSMARTBridgeHandler.java
@@ -164,8 +164,7 @@ public class SAICiSMARTBridgeHandler extends BaseBridgeHandler {
             updateStatus(ThingStatus.ONLINE);
         } catch (TimeoutException | URISyntaxException | ExecutionException | InterruptedException
                 | NoSuchAlgorithmException | IOException e) {
-            updateStatus(ThingStatus.OFFLINE);
-            logger.warn("Could not login to SAIC iSMART account", e);
+updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR, ex.getMessage());
         }
     }
 
