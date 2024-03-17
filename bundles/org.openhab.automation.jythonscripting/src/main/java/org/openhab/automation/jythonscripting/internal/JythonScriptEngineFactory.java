00c28b3c29fec8470f07b9cc49166af73309c42d
Holger Hees <holger.hees@gmail.com>
2024-03-17 19:24:15 +0100

    organized imports in JythonScriptEngineFactory
    Signed-off-by: Holger Hees <holger.hees@gmail.com>

--- bundles/org.openhab.automation.jythonscripting/src/main/java/org/openhab/automation/jythonscripting/internal/JythonScriptEngineFactory.java
+++ bundles/org.openhab.automation.jythonscripting/src/main/java/org/openhab/automation/jythonscripting/internal/JythonScriptEngineFactory.java
@@ -18,7 +18,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.script.ScriptEngine;
