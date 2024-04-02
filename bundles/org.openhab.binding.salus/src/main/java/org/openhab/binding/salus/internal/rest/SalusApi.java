ccf58a957c1ad075994bde70d5c0b071996339ca
Martin <martin.grzeslowski@gmail.com>
2024-04-02 22:23:50 +0200

    times->retries
    Co-authored-by: lsiepel <leosiepel@gmail.com>
    Signed-off-by: Martin <martin.grzeslowski@gmail.com>

--- bundles/org.openhab.binding.salus/src/main/java/org/openhab/binding/salus/internal/rest/SalusApi.java
+++ bundles/org.openhab.binding.salus/src/main/java/org/openhab/binding/salus/internal/rest/SalusApi.java
@@ -76,7 +76,7 @@ public class SalusApi {
                 forceRefreshAccessToken();
                 return get(url, header, times + 1);
             }
-            logger.debug("Could not refresh access token after {} times", MAX_TIMES);
+            logger.debug("Could not refresh access token after {} retries", MAX_TIMES);
             return response;
         }
         return response;
