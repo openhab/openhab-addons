bd8a905013312546ea2b2ae82d4f8b337d2a07c7
Martin Grześlowski <martin.grzeslowski@gmail.com>
2024-04-02 22:56:21 +0200

    Add java docs totally not AI generated
    Signed-off-by: Martin Grześlowski <martin.grzeslowski@gmail.com>

--- bundles/org.openhab.binding.salus/src/main/java/org/openhab/binding/salus/internal/rest/RestClient.java
+++ bundles/org.openhab.binding.salus/src/main/java/org/openhab/binding/salus/internal/rest/RestClient.java
@@ -40,24 +40,54 @@ public interface RestClient {
      * 
      * @param url to send request
      * @param headers to send
+     * @param content to send
      * @return response from server
      */
     Response<@Nullable String> post(String url, Content content, @Nullable Header... headers)
             throws ExecutionException, InterruptedException, TimeoutException;
 
+    /**
+     * Represents content with a body and a type.
+     */
     record Content(String body, String type) {
+        /**
+         * Creates a Content instance with the given body and default type ("application/json").
+         *
+         * @param body The content body.
+         */
         public Content(String body) {
             this(body, "application/json");
         }
     }
 
+    /**
+     * Represents an HTTP header with a name and a list of values.
+     */
     record Header(String name, List<String> values) {
+        /**
+         * Creates a Header instance with the given name and a single value.
+         *
+         * @param name The header name.
+         * @param value The header value.
+         */
         public Header(String name, String value) {
             this(name, List.of(value));
         }
     }
 
+    /**
+     * Represents an HTTP response with a status code and a body.
+     *
+     * @param <T> The type of the response body.
+     */
     record Response<T> (int statusCode, T body) {
+        /**
+         * Maps the response body to a new type using the provided mapper function.
+         *
+         * @param <Y> The target type.
+         * @param mapper The mapping function.
+         * @return A new Response with the mapped body.
+         */
         public <Y> Response<Y> map(Function<T, Y> mapper) {
             return new Response<>(statusCode, mapper.apply(body));
         }
