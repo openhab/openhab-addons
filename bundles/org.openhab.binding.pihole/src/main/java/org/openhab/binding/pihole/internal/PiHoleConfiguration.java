66fbb8ff4ce42274ec8ccfbdf3df9ef6a5fd699a
Martin Grześlowski <martin.grzeslowski@gmail.com>
2024-04-07 11:08:17 +0200

    Init PiHole binding
    God save the King!
    
    Signed-off-by: Martin Grześlowski <martin.grzeslowski@gmail.com>

---
+++ bundles/org.openhab.binding.pihole/src/main/java/org/openhab/binding/pihole/internal/PiHoleConfiguration.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.pihole.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link PiHoleConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Martin Grzeslowski - Initial contribution
+ */
+@NonNullByDefault
+public class PiHoleConfiguration {
+
+    /**
+     * Sample configuration parameters. Replace with your own.
+     */
+    public String hostname = "";
+    public String password = "";
+    public int refreshInterval = 600;
+}
