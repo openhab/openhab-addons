37a67a20be4b5bae2d1b8ef58bf0f5b96a5e8f69
Laurent ARNAL <laurent@clae.net>
2024-04-21 17:14:11 +0200

    refactor binding (WIP)
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.binding.linky/src/main/java/org/openhab/binding/linky/internal/handler/EnedisBridgeHandler.java
+++ bundles/org.openhab.binding.linky/src/main/java/org/openhab/binding/linky/internal/handler/EnedisBridgeHandler.java
@@ -51,5 +51,4 @@ public class EnedisBridgeHandler extends ApiBridgeHandler {
 
         super.dispose();
     }
-
 }
--- bundles/org.openhab.binding.linky/src/main/java/org/openhab/binding/linky/internal/handler/MyElectricalDataBridgeHandler.java
+++ bundles/org.openhab.binding.linky/src/main/java/org/openhab/binding/linky/internal/handler/MyElectricalDataBridgeHandler.java
@@ -51,5 +51,4 @@ public class MyElectricalDataBridgeHandler extends ApiBridgeHandler {
 
         super.dispose();
     }
-
 }
