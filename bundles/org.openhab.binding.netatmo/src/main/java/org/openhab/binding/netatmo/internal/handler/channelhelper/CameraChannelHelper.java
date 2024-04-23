da3b7c9399944102bfc5bcdd8394da3aac309e12
gael@lhopital.org <gael@lhopital.org>
2024-04-23 17:53:30 +0200

    Adressing issue on live picture
    Signed-off-by: gael@lhopital.org <gael@lhopital.org>

--- bundles/org.openhab.binding.netatmo/src/main/java/org/openhab/binding/netatmo/internal/handler/channelhelper/CameraChannelHelper.java
+++ bundles/org.openhab.binding.netatmo/src/main/java/org/openhab/binding/netatmo/internal/handler/channelhelper/CameraChannelHelper.java
@@ -15,6 +15,7 @@ package org.openhab.binding.netatmo.internal.handler.channelhelper;
 import static org.openhab.binding.netatmo.internal.NetatmoBindingConstants.*;
 import static org.openhab.binding.netatmo.internal.utils.ChannelTypeUtils.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -36,8 +37,8 @@ import org.openhab.core.types.UnDefType;
 public class CameraChannelHelper extends ChannelHelper {
     private static final String QUALITY_CONF_ENTRY = "quality";
     private static final String LIVE_PICTURE = "/live/snapshot_720.jpg";
-    private boolean isLocal;
-    private @Nullable String vpnUrl;
+
+    private @NonNullByDefault({}) String vpnUrl;
     private @Nullable String localUrl;
 
     public CameraChannelHelper(Set<String> providedGroups) {
@@ -45,55 +46,66 @@ public class CameraChannelHelper extends ChannelHelper {
     }
 
     public void setUrls(String vpnUrl, @Nullable String localUrl) {
-        this.localUrl = localUrl;
         this.vpnUrl = vpnUrl;
-        this.isLocal = localUrl != null;
-    }
-
-    public @Nullable String getLocalURL() {
-        return localUrl;
+        this.localUrl = localUrl;
     }
 
     @Override
     protected @Nullable State internalGetProperty(String channelId, NAThing naThing, Configuration config) {
         if (naThing instanceof HomeStatusModule camera) {
-            boolean isMonitoring = OnOffType.ON.equals(camera.getMonitoring());
-            switch (channelId) {
-                case CHANNEL_MONITORING:
-                    return camera.getMonitoring();
-                case CHANNEL_SD_CARD:
-                    return toStringType(camera.getSdStatus());
-                case CHANNEL_ALIM_STATUS:
-                    return toStringType(camera.getAlimStatus());
-                case CHANNEL_LIVEPICTURE_VPN_URL:
-                    return toStringType(getLivePictureURL(false, isMonitoring));
-                case CHANNEL_LIVEPICTURE_LOCAL_URL:
-                    return toStringType(getLivePictureURL(true, isMonitoring));
-                case CHANNEL_LIVEPICTURE:
-                    return toRawType(getLivePictureURL(isLocal, isMonitoring));
-                case CHANNEL_LIVESTREAM_VPN_URL:
-                    return getLiveStreamURL(false, (String) config.get(QUALITY_CONF_ENTRY), isMonitoring);
-                case CHANNEL_LIVESTREAM_LOCAL_URL:
-                    return getLiveStreamURL(true, (String) config.get(QUALITY_CONF_ENTRY), isMonitoring);
-            }
+            return switch (channelId) {
+                case CHANNEL_MONITORING -> camera.getMonitoring();
+                case CHANNEL_SD_CARD -> toStringType(camera.getSdStatus());
+                case CHANNEL_ALIM_STATUS -> toStringType(camera.getAlimStatus());
+                default -> liveChannels(channelId, config, camera, OnOffType.ON.equals(camera.getMonitoring()),
+                        localUrl != null);
+            };
         }
         return null;
     }
 
-    private @Nullable String getLivePictureURL(boolean local, boolean isMonitoring) {
-        String url = local ? localUrl : vpnUrl;
-        if (!isMonitoring || (local && !isLocal) || url == null) {
+    private @Nullable State liveChannels(String channelId, Configuration config, HomeStatusModule camera,
+            boolean isMonitoring, boolean isLocal) {
+        if (vpnUrl == null) {
+            setUrls(Objects.requireNonNull(camera.getVpnUrl()), localUrl);
+        }
+        return switch (channelId) {
+            case CHANNEL_LIVEPICTURE_VPN_URL -> toStringType(getLivePictureURL(false, isMonitoring));
+            case CHANNEL_LIVEPICTURE_LOCAL_URL ->
+                isLocal ? toStringType(getLivePictureURL(true, isMonitoring)) : UnDefType.NULL;
+            case CHANNEL_LIVEPICTURE -> {
+                State result = toRawType(getLivePictureURL(isLocal, isMonitoring));
+                if (UnDefType.NULL.equals(result) && isLocal) {
+                    // If local read of the picture is unsuccessfull, try the VPN version
+                    result = toRawType(getLivePictureURL(false, isMonitoring));
+                }
+                yield result;
+            }
+            case CHANNEL_LIVESTREAM_VPN_URL ->
+                getLiveStreamURL(false, (String) config.get(QUALITY_CONF_ENTRY), isMonitoring);
+            case CHANNEL_LIVESTREAM_LOCAL_URL ->
+                isLocal ? getLiveStreamURL(true, (String) config.get(QUALITY_CONF_ENTRY), isMonitoring)
+                        : UnDefType.NULL;
+            default -> null;
+        };
+    }
+
+    private String getUrl(boolean local) {
+        return Objects.requireNonNull(local ? localUrl : vpnUrl);
+    }
+
+    public @Nullable String getLivePictureURL(boolean local, boolean isMonitoring) {
+        if (!isMonitoring || (local && (localUrl != null))) {
             return null;
         }
-        return "%s%s".formatted(url, LIVE_PICTURE);
+        return "%s%s".formatted(getUrl(local), LIVE_PICTURE);
     }
 
     private State getLiveStreamURL(boolean local, @Nullable String configQual, boolean isMonitoring) {
-        String url = local ? localUrl : vpnUrl;
-        if (!isMonitoring || (local && !isLocal) || url == null) {
+        if (!isMonitoring || (local && (localUrl != null))) {
             return UnDefType.NULL;
         }
         String finalQual = configQual != null ? configQual : "poor";
-        return toStringType("%s/live/%s", url, local ? "files/%s/index.m3u8".formatted(finalQual) : "index.m3u8");
+        return toStringType("%s/live/%sindex.m3u8", getUrl(local), local ? "files/%s/".formatted(finalQual) : "");
     }
 }
