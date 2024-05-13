9700e4dabbc0c41c1d275268479ab7d8902b6178
Alexander Drent <Alex@Drent-ict.nl>
2024-05-13 16:04:55 +0200

    [ONECTA] Change Copyright Year
     -
    
    Signed-off-by: Alexander Drent <Alex@Drent-ict.nl>

--- bundles/org.openhab.binding.onecta/src/main/java/org/openhab/binding/onecta/internal/api/Enums.java
+++ bundles/org.openhab.binding.onecta/src/main/java/org/openhab/binding/onecta/internal/api/Enums.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2023 Contributors to the openHAB project
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -356,7 +356,7 @@ public class Enums {
         FIXED("fixed"),
         SCHEDULED("scheduled");
 
-        private static final Logger LOGGER = LoggerFactory.getLogger(FanMovement.class);
+        private static final Logger LOGGER = LoggerFactory.getLogger(DemandControl.class);
         private final String value;
 
         DemandControl(String value) {
