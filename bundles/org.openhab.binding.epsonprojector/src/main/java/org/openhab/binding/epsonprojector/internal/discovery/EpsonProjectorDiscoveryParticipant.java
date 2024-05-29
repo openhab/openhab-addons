5778187b19b701986da3368a870ad6ed734c45f2
Michael Lobstein <michael.lobstein@gmail.com>
2024-05-28 20:03:34 -0500

    updates
    Signed-off-by: Michael Lobstein <michael.lobstein@gmail.com>

--- bundles/org.openhab.binding.epsonprojector/src/main/java/org/openhab/binding/epsonprojector/internal/discovery/EpsonProjectorDiscoveryParticipant.java
+++ bundles/org.openhab.binding.epsonprojector/src/main/java/org/openhab/binding/epsonprojector/internal/discovery/EpsonProjectorDiscoveryParticipant.java
@@ -78,7 +78,7 @@ public class EpsonProjectorDiscoveryParticipant implements SddpDiscoveryParticip
         if (device.manufacturer.toUpperCase(Locale.ENGLISH).contains(EPSON)
                 && device.type.toUpperCase(Locale.ENGLISH).contains(TYPE_PROJECTOR) && !device.macAddress.isBlank()) {
 
-            logger.debug("Epson projector with id {} found at {}", device.macAddress, device.ipAddress);
+            logger.debug("Epson projector with mac {} found at {}", device.macAddress, device.ipAddress);
             return new ThingUID(THING_TYPE_PROJECTOR_TCP, device.macAddress);
         }
         return null;
