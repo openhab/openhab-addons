d5d456eed581fee2dea77a9c5ea1384a7018040f
Martin Grześlowski <martin.grzeslowski@gmail.com>
2024-06-06 22:27:45 +0200

    meh
    Signed-off-by: Martin Grześlowski <martin.grzeslowski@gmail.com>

--- bundles/org.openhab.binding.pihole/src/main/java/org/openhab/binding/pihole/internal/PiHoleException.java
+++ bundles/org.openhab.binding.pihole/src/main/java/org/openhab/binding/pihole/internal/PiHoleException.java
@@ -12,9 +12,9 @@
  */
 package org.openhab.binding.pihole.internal;
 
-import org.eclipse.jdt.annotation.NonNullByDefault;
-
 import java.io.Serial;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
 
 /**
  * @author Martin Grzeslowski - Initial contribution
