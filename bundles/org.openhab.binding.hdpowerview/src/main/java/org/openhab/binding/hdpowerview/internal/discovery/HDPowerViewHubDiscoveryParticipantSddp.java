0f95ff7a12262442b98d694acd0eddb5acfbe537
Andrew Fiddian-Green <software@whitebear.ch>
2024-06-08 12:09:39 +0100

    [hdpowerview] support gen3; adopt reviewer suggestions
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/internal/discovery/HDPowerViewHubDiscoveryParticipantSddp.java
+++ bundles/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/internal/discovery/HDPowerViewHubDiscoveryParticipantSddp.java
@@ -39,24 +39,26 @@ import org.slf4j.LoggerFactory;
 public class HDPowerViewHubDiscoveryParticipantSddp implements SddpDiscoveryParticipant {
 
     private static final String LABEL_KEY = "discovery.hub.label";
+    private static final String HUNTER_DOUGLAS = "hunterdouglas:";
+    private static final String POWERVIEW_HUB_ID = "hub:powerview";
+    private static final String POWERVIEW_GEN3_ID = "powerview:gen3:gateway";
 
     private final Logger logger = LoggerFactory.getLogger(HDPowerViewHubDiscoveryParticipantSddp.class);
 
     @Override
     public Set<ThingTypeUID> getSupportedThingTypeUIDs() {
-        return Set.of(THING_TYPE_HUB);
+        return Set.of(THING_TYPE_HUB, THING_TYPE_GATEWAY);
     }
 
     @Override
     public @Nullable DiscoveryResult createResult(SddpDevice device) {
-        ThingUID thingUID = getThingUID(device);
+        final ThingUID thingUID = getThingUID(device);
         if (thingUID != null) {
-            String ipAddress = device.ipAddress;
             DiscoveryResult hub = DiscoveryResultBuilder.create(thingUID)
-                    .withProperty(HDPowerViewHubConfiguration.HOST, ipAddress)
+                    .withProperty(HDPowerViewHubConfiguration.HOST, device.ipAddress)
                     .withRepresentationProperty(HDPowerViewHubConfiguration.HOST)
-                    .withLabel(String.format("@text/%s [\"%s\"]", LABEL_KEY, ipAddress)).build();
-            logger.debug("SDDP discovered hub on host '{}'", ipAddress);
+                    .withLabel(String.format("@text/%s [\"%s\"]", LABEL_KEY, device.ipAddress)).build();
+            logger.debug("SDDP discovered hub/gateway '{}' on host '{}'", thingUID, device.ipAddress);
             return hub;
         }
         return null;
@@ -64,9 +66,14 @@ public class HDPowerViewHubDiscoveryParticipantSddp implements SddpDiscoveryPart
 
     @Override
     public @Nullable ThingUID getThingUID(SddpDevice device) {
-        String ipAddress = device.ipAddress;
-        if (VALID_IP_V4_ADDRESS.matcher(ipAddress).matches()) {
-            return new ThingUID(THING_TYPE_HUB, ipAddress.replace('.', '_'));
+        if (device.type.startsWith(HUNTER_DOUGLAS)) {
+            final ThingTypeUID bridgeTypeUID = device.type.contains(POWERVIEW_HUB_ID) ? THING_TYPE_HUB
+                    : device.type.contains(POWERVIEW_GEN3_ID) ? THING_TYPE_GATEWAY : null;
+            if (bridgeTypeUID != null) {
+                if (VALID_IP_V4_ADDRESS.matcher(device.ipAddress).matches()) {
+                    return new ThingUID(bridgeTypeUID, device.ipAddress.replace('.', '_'));
+                }
+            }
         }
         return null;
     }
