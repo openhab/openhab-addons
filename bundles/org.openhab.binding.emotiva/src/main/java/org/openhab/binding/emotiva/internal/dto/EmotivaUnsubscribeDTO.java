30860884e369ca729017deb49aca9ddfa7deca5a
Espen Fossen <espenaf@junta.no>
2024-06-13 22:33:14 +0200

    [emotiva] Initial contribution (#16499)
    * [emotiva] Initial contribution
    
    Signed-off-by: Espen Fossen <espenaf@junta.no>
    Signed-off-by: Ciprian Pascu <contact@ciprianpascu.ro>

---
+++ bundles/org.openhab.binding.emotiva/src/main/java/org/openhab/binding/emotiva/internal/dto/EmotivaUnsubscribeDTO.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.emotiva.internal.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.openhab.binding.emotiva.internal.protocol.EmotivaControlCommands;
+import org.openhab.binding.emotiva.internal.protocol.EmotivaSubscriptionTags;
+
+/**
+ * The EmotivaUnsubscriptionDTO message type. Use to remove subscription after registration via {
+ * 
+ * @link EmotivaSubscriptionRequest}.
+ *
+ * @author Espen Fossen - Initial contribution
+ */
+@XmlRootElement(name = "emotivaUnsubscribe")
+public class EmotivaUnsubscribeDTO extends AbstractJAXBElementDTO {
+
+    @SuppressWarnings("unused")
+    public EmotivaUnsubscribeDTO() {
+    }
+
+    public EmotivaUnsubscribeDTO(List<EmotivaCommandDTO> commands) {
+        this.commands = commands;
+    }
+
+    public EmotivaUnsubscribeDTO(EmotivaSubscriptionTags[] emotivaCommandTypes) {
+        List<EmotivaCommandDTO> list = new ArrayList<>();
+        for (EmotivaSubscriptionTags commandType : emotivaCommandTypes) {
+            list.add(EmotivaCommandDTO.fromType(commandType));
+        }
+        this.commands = list;
+    }
+
+    public EmotivaUnsubscribeDTO(EmotivaSubscriptionTags tag) {
+        this.commands = List.of(EmotivaCommandDTO.fromType(tag));
+    }
+
+    public EmotivaUnsubscribeDTO(EmotivaControlCommands commandType) {
+        this.commands = List.of(EmotivaCommandDTO.fromType(commandType));
+    }
+}
---
+++ bundles/org.openhab.binding.emotiva/src/test/java/org/openhab/binding/emotiva/internal/dto/EmotivaSubscriptionRequestTest.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.emotiva.internal.dto;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.openhab.binding.emotiva.internal.EmotivaBindingConstants.CHANNEL_TUNER_RDS;
+import static org.openhab.binding.emotiva.internal.protocol.EmotivaProtocolVersion.PROTOCOL_V2;
+
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.junit.jupiter.api.Test;
+import org.openhab.binding.emotiva.internal.AbstractDTOTestBase;
+import org.openhab.binding.emotiva.internal.protocol.EmotivaControlCommands;
+import org.openhab.binding.emotiva.internal.protocol.EmotivaSubscriptionTags;
+
+/**
+ * Unit tests for EmotivaSubscription requests.
+ *
+ * @author Espen Fossen - Initial contribution
+ */
+@NonNullByDefault
+class EmotivaSubscriptionRequestTest extends AbstractDTOTestBase {
+
+    public EmotivaSubscriptionRequestTest() throws JAXBException {
+    }
+
+    @Test
+    void marshalFromChannelUID() {
+        EmotivaSubscriptionTags subscriptionChannel = EmotivaSubscriptionTags.fromChannelUID(CHANNEL_TUNER_RDS);
+        EmotivaSubscriptionRequest emotivaSubscriptionRequest = new EmotivaSubscriptionRequest(subscriptionChannel);
+        String xmlString = xmlUtils.marshallJAXBElementObjects(emotivaSubscriptionRequest);
+        assertThat(xmlString, containsString("<emotivaSubscription protocol=\"2.0\">"));
+        assertThat(xmlString, containsString("<tuner_RDS ack=\"yes\" />"));
+        assertThat(xmlString, containsString("</emotivaSubscription>"));
+    }
+
+    @Test
+    void marshallWithTwoSubscriptionsNoAck() {
+        EmotivaCommandDTO command1 = new EmotivaCommandDTO(EmotivaControlCommands.volume, "10", "yes");
+        EmotivaCommandDTO command2 = new EmotivaCommandDTO(EmotivaControlCommands.power_off);
+
+        EmotivaSubscriptionRequest dto = new EmotivaSubscriptionRequest(List.of(command1, command2),
+                PROTOCOL_V2.value());
+
+        String xmlString = xmlUtils.marshallJAXBElementObjects(dto);
+        assertThat(xmlString, containsString("<emotivaSubscription protocol=\"2.0\">"));
+        assertThat(xmlString, containsString("<volume value=\"10\" ack=\"yes\" />"));
+        assertThat(xmlString, containsString("<power_off />"));
+        assertThat(xmlString, containsString("</emotivaSubscription>"));
+        assertThat(xmlString, not(containsString("<volume>")));
+        assertThat(xmlString, not(containsString("<command>")));
+    }
+
+    @Test
+    void unmarshall() throws JAXBException {
+        var dto = (EmotivaSubscriptionResponse) xmlUtils.unmarshallToEmotivaDTO(emotivaSubscriptionRequest);
+        assertThat(dto, is(notNullValue()));
+        assertThat(dto.getTags().size(), is(3));
+        assertThat(dto.getProperties(), is(nullValue()));
+
+        List<EmotivaNotifyDTO> commands = xmlUtils.unmarshallToNotification(dto.getTags());
+
+        assertThat(commands.get(0).getName(), is(EmotivaSubscriptionTags.selected_mode.name()));
+        assertThat(commands.get(0).getStatus(), is(nullValue()));
+        assertThat(commands.get(0).getValue(), is(nullValue()));
+        assertThat(commands.get(0).getVisible(), is(nullValue()));
+
+        assertThat(commands.get(1).getName(), is(EmotivaSubscriptionTags.power.name()));
+        assertThat(commands.get(1).getStatus(), is(nullValue()));
+        assertThat(commands.get(1).getValue(), is(nullValue()));
+        assertThat(commands.get(1).getVisible(), is(nullValue()));
+
+        assertThat(commands.get(2).getName(), is("unknown"));
+        assertThat(commands.get(2).getStatus(), is(nullValue()));
+        assertThat(commands.get(2).getValue(), is(nullValue()));
+        assertThat(commands.get(2).getVisible(), is(nullValue()));
+    }
+}
