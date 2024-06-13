aa63fa13a24165d173abbdff886352514c3a0104
Laurent ARNAL <laurent@clae.net>
2024-06-13 19:55:22 +0200

    fix checkstyle errors
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.binding.siemenshvac/src/main/java/org/openhab/binding/siemenshvac/internal/thirdparty/RuntimeTypeAdapterFactory.java
+++ bundles/org.openhab.binding.siemenshvac/src/main/java/org/openhab/binding/siemenshvac/internal/thirdparty/RuntimeTypeAdapterFactory.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 /*
  * Copyright (C) 2011 Google Inc.
  *
@@ -167,6 +179,9 @@ import com.google.gson.stream.MalformedJsonException;
  *             .registerSubtype(Rectangle.class).registerSubtype(Circle.class).registerSubtype(Diamond.class);
  * }
  * </pre>
+ */
+/**
+ * @author Jesse Wilson (swankjesse) - Initial contribution
  */
 @NonNullByDefault
 public final class RuntimeTypeAdapterFactory<T> implements TypeAdapterFactory {
