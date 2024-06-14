ff7ebd7ac46893ce12b149e3c14b23012e5761ae
lolodomo <lg.hc@free.fr>
2024-06-14 17:48:02 +0200

    [sonyprojector] Add discovery through SDDP (#16849)
    Only applicable to projector models having a network connector
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>
    Signed-off-by: Ciprian Pascu <contact@ciprianpascu.ro>

--- bundles/org.openhab.binding.sonyprojector/src/main/java/org/openhab/binding/sonyprojector/internal/configuration/SonyProjectorEthernetConfiguration.java
+++ bundles/org.openhab.binding.sonyprojector/src/main/java/org/openhab/binding/sonyprojector/internal/configuration/SonyProjectorEthernetConfiguration.java
@@ -13,7 +13,6 @@
 package org.openhab.binding.sonyprojector.internal.configuration;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.jdt.annotation.Nullable;
 
 /**
  * The {@link SonyProjectorEthernetConfiguration} class contains fields mapping thing configuration parameters.
@@ -23,9 +22,12 @@ import org.eclipse.jdt.annotation.Nullable;
  */
 @NonNullByDefault
 public class SonyProjectorEthernetConfiguration {
+    public static final int DEFAULT_PORT = 53484;
+    private static final String DEFAULT_COMMUNITY = "SONY";
+    public static final String MODEL_AUTO = "AUTO";
 
-    public @NonNullByDefault({}) String host;
-    public @Nullable Integer port;
-    public @Nullable String community;
-    public @Nullable String model;
+    public String host = "";
+    public int port = DEFAULT_PORT;
+    public String community = DEFAULT_COMMUNITY;
+    public String model = MODEL_AUTO;
 }
