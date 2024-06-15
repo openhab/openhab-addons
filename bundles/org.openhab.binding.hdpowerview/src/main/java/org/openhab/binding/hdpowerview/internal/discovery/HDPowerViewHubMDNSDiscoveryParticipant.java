5e157262c5227afa70f166279502a8fcbf28369d
Jacob Laursen <jacob-github@vindvejr.dk>
2024-06-15 19:34:48 +0200

    [hdpowerview] Improve Gen 1/2 discovery label (#16865)
    * Improve Gen 1/2 discovery label
    
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>

--- bundles/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/internal/discovery/HDPowerViewHubMDNSDiscoveryParticipant.java
+++ bundles/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/internal/discovery/HDPowerViewHubMDNSDiscoveryParticipant.java
@@ -20,13 +20,21 @@ import javax.jmdns.ServiceInfo;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jetty.client.HttpClient;
+import org.openhab.binding.hdpowerview.internal.HDPowerViewWebTargets;
 import org.openhab.binding.hdpowerview.internal.config.HDPowerViewHubConfiguration;
+import org.openhab.binding.hdpowerview.internal.dto.Firmware;
+import org.openhab.binding.hdpowerview.internal.dto.HubFirmware;
+import org.openhab.binding.hdpowerview.internal.exceptions.HubException;
 import org.openhab.core.config.discovery.DiscoveryResult;
 import org.openhab.core.config.discovery.DiscoveryResultBuilder;
 import org.openhab.core.config.discovery.mdns.MDNSDiscoveryParticipant;
+import org.openhab.core.io.net.http.HttpClientFactory;
 import org.openhab.core.thing.ThingTypeUID;
 import org.openhab.core.thing.ThingUID;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,11 +45,17 @@ import org.slf4j.LoggerFactory;
  */
 @NonNullByDefault
 @Component
-public class HDPowerViewHubDiscoveryParticipant implements MDNSDiscoveryParticipant {
+public class HDPowerViewHubMDNSDiscoveryParticipant implements MDNSDiscoveryParticipant {
 
-    private static final String LABEL_KEY = "discovery.hub.label";
+    public static final String LABEL_KEY_HUB = "discovery.hub.label";
 
-    private final Logger logger = LoggerFactory.getLogger(HDPowerViewHubDiscoveryParticipant.class);
+    private final Logger logger = LoggerFactory.getLogger(HDPowerViewHubMDNSDiscoveryParticipant.class);
+    private final HttpClient httpClient;
+
+    @Activate
+    public HDPowerViewHubMDNSDiscoveryParticipant(@Reference HttpClientFactory httpClientFactory) {
+        httpClient = httpClientFactory.getCommonHttpClient();
+    }
 
     @Override
     public Set<ThingTypeUID> getSupportedThingTypeUIDs() {
@@ -58,11 +72,12 @@ public class HDPowerViewHubDiscoveryParticipant implements MDNSDiscoveryParticip
         for (String host : service.getHostAddresses()) {
             if (VALID_IP_V4_ADDRESS.matcher(host).matches()) {
                 ThingUID thingUID = new ThingUID(THING_TYPE_HUB, host.replace('.', '_'));
+                String generation = this.getGeneration(host);
                 DiscoveryResult hub = DiscoveryResultBuilder.create(thingUID)
                         .withProperty(HDPowerViewHubConfiguration.HOST, host)
                         .withRepresentationProperty(HDPowerViewHubConfiguration.HOST)
-                        .withLabel(String.format("@text/%s [\"%s\"]", LABEL_KEY, host)).build();
-                logger.debug("mDNS discovered Gen 1/2 hub on host '{}'", host);
+                        .withLabel(String.format("@text/%s [\"%s\", \"%s\"]", LABEL_KEY_HUB, generation, host)).build();
+                logger.debug("mDNS discovered Gen {} hub on host '{}'", generation, host);
                 return hub;
             }
         }
@@ -77,5 +92,19 @@ public class HDPowerViewHubDiscoveryParticipant implements MDNSDiscoveryParticip
             }
         }
         return null;
+    }
+
+    private String getGeneration(String host) {
+        var webTargets = new HDPowerViewWebTargets(httpClient, host);
+        try {
+            HubFirmware firmware = webTargets.getFirmwareVersions();
+            Firmware mainProcessor = firmware.mainProcessor;
+            if (mainProcessor != null) {
+                return String.valueOf(mainProcessor.revision);
+            }
+        } catch (HubException e) {
+            logger.debug("Failed to discover hub firmware versions", e);
+        }
+        return "1/2";
     }
 }
