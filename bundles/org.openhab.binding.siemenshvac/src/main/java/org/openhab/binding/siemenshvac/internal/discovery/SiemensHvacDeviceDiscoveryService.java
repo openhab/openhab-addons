c09798cbe4cee748d5c5ce4d1b673eb22991952e
Laurent ARNAL <laurent@clae.net>
2024-06-18 08:51:39 +0200

    reintroduce serial in ThingUID when using discovery
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.binding.siemenshvac/src/main/java/org/openhab/binding/siemenshvac/internal/discovery/SiemensHvacDeviceDiscoveryService.java
+++ bundles/org.openhab.binding.siemenshvac/src/main/java/org/openhab/binding/siemenshvac/internal/discovery/SiemensHvacDeviceDiscoveryService.java
@@ -115,7 +115,7 @@ public class SiemensHvacDeviceDiscoveryService extends AbstractDiscoveryService
                 String typeSn = UidUtils.sanetizeId(type);
                 ThingTypeUID thingTypeUID = new ThingTypeUID(SiemensHvacBindingConstants.BINDING_ID, typeSn);
 
-                ThingUID thingUID = getThingUID(thingTypeUID, typeSn);
+                ThingUID thingUID = getThingUID(thingTypeUID, serialNr);
 
                 if (lcSiemensHvacBridgeHandler != null) {
                     ThingUID bridgeUID = lcSiemensHvacBridgeHandler.getThing().getUID();
