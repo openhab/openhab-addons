a04c1c8d7a6175e194bd8b22a52c53ab0bab31f1
J-N-K <github@klug.nrw>
2024-07-12 22:02:44 +0200

    [http] Properly escape + character in query string (#17042)
    * [http] Properly escape + character in query string
    
    Signed-off-by: Jan N. Klug <github@klug.nrw>

--- bundles/org.openhab.binding.http/src/main/java/org/openhab/binding/http/internal/HttpThingHandler.java
+++ bundles/org.openhab.binding.http/src/main/java/org/openhab/binding/http/internal/HttpThingHandler.java
@@ -369,7 +369,7 @@ public class HttpThingHandler extends BaseThingHandler implements HttpStatusList
     private void sendHttpValue(String commandUrl, String command, boolean isRetry) {
         try {
             // format URL
-            URI uri = Util.uriFromString(String.format(commandUrl, new Date(), command));
+            URI uri = Util.uriFromString(Util.wrappedStringFormat(commandUrl, new Date(), command));
 
             // build request
             rateLimitedHttpClient.newPriorityRequest(uri, config.commandMethod, command, config.contentType)
