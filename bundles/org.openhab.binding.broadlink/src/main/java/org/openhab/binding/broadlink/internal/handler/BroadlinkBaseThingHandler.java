3622b48450166d097043298cf5a4b554813e3594
AntonJansen <gradius@fmf.nl>
2024-07-17 21:58:37 +0200

    Reverted improvement, as it did not work.
    Signed-off-by: AntonJansen <gradius@fmf.nl>

--- bundles/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/internal/handler/BroadlinkBaseThingHandler.java
+++ bundles/org.openhab.binding.broadlink/src/main/java/org/openhab/binding/broadlink/internal/handler/BroadlinkBaseThingHandler.java
@@ -109,7 +109,12 @@ public abstract class BroadlinkBaseThingHandler extends BaseThingHandler impleme
         updateItemStatus();
 
         if (thingConfig.getPollingInterval() != 0) {
-            refreshHandle = scheduler.scheduleWithFixedDelay(this::updateItemStatus(), 0, thingConfig.getPollingInterval(), TimeUnit.SECONDS);
+            refreshHandle = scheduler.scheduleWithFixedDelay(new Runnable() {
+                @Override
+                public void run() {
+                    updateItemStatus();
+                }
+            }, 1L, thingConfig.getPollingInterval(), TimeUnit.SECONDS);
         }
     }
 
