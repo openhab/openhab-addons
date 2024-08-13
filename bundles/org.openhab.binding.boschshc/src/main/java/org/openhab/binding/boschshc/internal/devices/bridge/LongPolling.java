c2613c77598f9a955a649f47e44ad0007ec40ea9
David Pace <dev@davidpace.de>
2024-08-13 20:20:51 +0200

    [boschshc] Fix NPE during deserialization, make long polling more robust (#17190)
    * Fix NPE while deserializing service data JSON objects
    * Catch exceptions while handling long poll results
    * Correct @type name for user-defined states
    * Add unit tests and enhance existing unit tests
    
    Signed-off-by: David Pace <dev@davidpace.de>

--- bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/devices/bridge/LongPolling.java
+++ bundles/org.openhab.binding.boschshc/src/main/java/org/openhab/binding/boschshc/internal/devices/bridge/LongPolling.java
@@ -211,7 +211,7 @@ public class LongPolling {
      * @param subscriptionId Id of subscription the response is for
      * @param content Content of the response
      */
-    private void handleLongPollResponse(BoschHttpClient httpClient, String subscriptionId, @Nullable String content) {
+    void handleLongPollResponse(BoschHttpClient httpClient, String subscriptionId, @Nullable String content) {
         logger.debug("Long poll response: {}", content);
 
         try {
@@ -238,6 +238,10 @@ public class LongPolling {
         } catch (JsonSyntaxException e) {
             this.handleFailure.accept(
                     new LongPollingFailedException("Could not deserialize long poll response: '" + content + "'", e));
+            return;
+        } catch (Exception e) {
+            this.handleFailure.accept(
+                    new LongPollingFailedException("Error while handling long poll response: '" + content + "'", e));
             return;
         }
 
