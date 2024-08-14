e4c166726253b7942b4ca9f31535805df91de31b
Laurent ARNAL <laurent@clae.net>
2024-08-14 12:49:52 +0200

    fix maxpower web api url
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.binding.linky/src/main/java/org/openhab/binding/linky/internal/handler/EnedisWebBridgeHandler.java
+++ bundles/org.openhab.binding.linky/src/main/java/org/openhab/binding/linky/internal/handler/EnedisWebBridgeHandler.java
@@ -69,7 +69,7 @@ public class EnedisWebBridgeHandler extends ApiBridgeHandler {
     private static final String MEASURE_DAILY_CONSUMPTION_URL = PRM_INFO_BASE_URL
             + "undefined/prms/%s/donnees-energie?dateDebut=%s&dateFin=%s&mesuretypecode=CONS";
 
-    private static final String MEASURE_MAX_POWER_URL = BASE_URL
+    private static final String MEASURE_MAX_POWER_URL = PRM_INFO_BASE_URL
             + "undefined/prms/%s/donnees-pmax?dateDebut=%s&dateFin=%s&mesuretypecode=CONS";
 
     private static final DateTimeFormatter API_DATE_FORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy");
