099d8a37545febfdfc7937fca1e9372f2be5ca86
Martin Grześlowski <martin.grzeslowski@gmail.com>
2024-08-25 22:43:11 +0200

    revert
    Signed-off-by: Martin Grześlowski <martin.grzeslowski@gmail.com>

--- bundles/org.openhab.binding.visualcrossing/src/main/java/org/openhab/binding/visualcrossing/internal/api/rest/ApiClient.java
+++ bundles/org.openhab.binding.visualcrossing/src/main/java/org/openhab/binding/visualcrossing/internal/api/rest/ApiClient.java
@@ -1,13 +1,13 @@
 /**
  * Copyright (c) 2010-2024 Contributors to the openHAB project
- * <p>
+ *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
- * <p>
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
- * <p>
+ *
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.openhab.binding.visualcrossing.internal.api.rest;
@@ -63,53 +63,42 @@ public class ApiClient implements RestClient {
     @SuppressWarnings("ConstantValue")
     private @Nullable String execute(Request request, @Nullable Header[] headers, String url)
             throws VisualCrossingApiException, VisualCrossingAuthException, VisualCrossingRateException {
-        for (int i = 0; i < 10; i++) {
-            try {
-                try {
-                    if (headers != null) {
-                        for (var header : headers) {
-                            if (header == null) {
-                                continue;
-                            }
-                            for (var value : header.values()) {
-                                request.header(header.name(), value);
-                            }
-                        }
+        try {
+            if (headers != null) {
+                for (var header : headers) {
+                    if (header == null) {
+                        continue;
                     }
-                    request.timeout(TIMEOUT, SECONDS);
-                    request.idleTimeout(IDLE_TIMEOUT, SECONDS);
-                    var response = request.send();
-                    var status = response.getStatus();
-                    if (status < 200 || status >= 399) {
-                        throw new HttpVisualCrossingApiException(status, response.getReason());
+                    for (var value : header.values()) {
+                        request.header(header.name(), value);
                     }
-                    return response.getContentAsString();
-                } catch (RuntimeException | TimeoutException | ExecutionException | InterruptedException ex) {
-                    Throwable cause = ex;
-                    while (cause != null) {
-                        if (cause instanceof HttpResponseException hte) {
-                            var response = hte.getResponse();
-                            int status = response.getStatus();
-                            if (status == 401) {
-                                throw new VisualCrossingAuthException();
-                            }
-                            if (status == 429) {
-                                throw new VisualCrossingRateException();
-                            }
-                            throw new HttpVisualCrossingApiException(response.getStatus(), response.getReason(), hte);
-                        }
-                        cause = cause.getCause();
-                    }
-                    throw new VisualCrossingApiException("Error while executing request to " + url, ex);
-                }
-            } catch (VisualCrossingApiException ex) {
-                if (i < 10 - 1) {
-                    logger.debug("Error while calling GET {}. Retrying {}/{}...", url, i + 1, 10, ex);
-                } else {
-                    throw ex;
                 }
             }
+            request.timeout(TIMEOUT, SECONDS);
+            request.idleTimeout(IDLE_TIMEOUT, SECONDS);
+            var response = request.send();
+            var status = response.getStatus();
+            if (status < 200 || status >= 399) {
+                throw new HttpVisualCrossingApiException(status, response.getReason());
+            }
+            return response.getContentAsString();
+        } catch (RuntimeException | TimeoutException | ExecutionException | InterruptedException ex) {
+            Throwable cause = ex;
+            while (cause != null) {
+                if (cause instanceof HttpResponseException hte) {
+                    var response = hte.getResponse();
+                    int status = response.getStatus();
+                    if (status == 401) {
+                        throw new VisualCrossingAuthException();
+                    }
+                    if (status == 429) {
+                        throw new VisualCrossingRateException();
+                    }
+                    throw new HttpVisualCrossingApiException(response.getStatus(), response.getReason(), hte);
+                }
+                cause = cause.getCause();
+            }
+            throw new VisualCrossingApiException("Error while executing request to " + url, ex);
         }
-        throw new IllegalStateException();
     }
 }
