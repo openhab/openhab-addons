b0eff3093db78cf2bdc6b3591336e373a4a4db64
Laurent ARNAL <laurent@clae.net>
2024-08-27 16:03:05 +0200

    - remote updateState on updateConsumptionTimeSeries because we don't have current value, but only previous one - review calculation of week index
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.binding.linky/src/main/java/org/openhab/binding/linky/internal/handler/LinkyHandler.java
+++ bundles/org.openhab.binding.linky/src/main/java/org/openhab/binding/linky/internal/handler/LinkyHandler.java
@@ -461,7 +461,7 @@ public class LinkyHandler extends BaseThingHandler {
         }
 
         sendTimeSeries(groupId, channelId, timeSeries);
-        updateState(groupId, channelId, new DecimalType(iv[iv.length - 1].value));
+        // updateState(groupId, channelId, new DecimalType(iv[iv.length - 1].value));
     }
 
     private void updateKwhChannel(String groupId, String channelId, double consumption) {
@@ -701,7 +701,7 @@ public class LinkyHandler extends BaseThingHandler {
                 int baseYear = meterReading.dayValue[0].date.getYear();
                 int baseMonth = meterReading.dayValue[0].date.getMonthValue();
                 int baseDayOfYear = meterReading.dayValue[0].date.getDayOfYear();
-                int baseWeek = ((baseDayOfYear - 1) / 7) + 1;
+                int baseWeek = meterReading.dayValue[0].date.get(WeekFields.of(Locale.FRANCE).weekOfYear());
 
                 for (int idx = 0; idx < size; idx++) {
                     IntervalReading ir = meterReading.dayValue[idx];
