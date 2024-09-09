e93e0a360c2c4e133defba32c0b4b44042785275
lsiepel <leosiepel@gmail.com>
2024-09-09 21:21:37 +0200

    [melcloud] Improve null handling (#17295)
    * Add null annotations
    
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>
    Signed-off-by: Ciprian Pascu <contact@ciprianpascu.ro>

--- bundles/org.openhab.binding.melcloud/src/main/java/org/openhab/binding/melcloud/internal/api/dto/HeatpumpDeviceStatus.java
+++ bundles/org.openhab.binding.melcloud/src/main/java/org/openhab/binding/melcloud/internal/api/dto/HeatpumpDeviceStatus.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.melcloud.internal.api.json;
+package org.openhab.binding.melcloud.internal.api.dto;
 
 import java.util.List;
 
