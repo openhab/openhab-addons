6a55ff344416f8c5e0a1a330db3bfa0e6368d1be
lsiepel <leosiepel@gmail.com>
2024-09-09 19:12:23 +0200

    [senechome] Fix `ArrayIndexOutOfBoundsException` when less than 4 packs (#17299)
    * Fix issue and warnings
    * Fix operator
    
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.senechome/src/main/java/org/openhab/binding/senechome/internal/dto/SenecHomeResponse.java
+++ bundles/org.openhab.binding.senechome/src/main/java/org/openhab/binding/senechome/internal/dto/SenecHomeResponse.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.senechome.internal.json;
+package org.openhab.binding.senechome.internal.dto;
 
 import java.io.Serializable;
 
