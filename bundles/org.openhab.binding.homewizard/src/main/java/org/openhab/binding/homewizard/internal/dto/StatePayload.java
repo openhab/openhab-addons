34c5e144991f590384bd03050076b89e40821bb9
lsiepel <leosiepel@gmail.com>
2024-10-07 08:11:04 +0200

    [homewizard] Add current, voltage and failure channels (#16995)
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>
    Signed-off-by: Ciprian Pascu <contact@ciprianpascu.ro>

--- bundles/org.openhab.binding.homewizard/src/main/java/org/openhab/binding/homewizard/internal/dto/StatePayload.java
+++ bundles/org.openhab.binding.homewizard/src/main/java/org/openhab/binding/homewizard/internal/dto/StatePayload.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.homewizard.internal;
+package org.openhab.binding.homewizard.internal.dto;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
