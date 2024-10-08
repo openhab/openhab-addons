7f425a3ebf925c5eadab5e1110b1d8a3010bd08e
DrRSatzteil <thomas_lauterbach@arcor.de>
2024-10-09 00:13:43 +0200

    Revert "removed awtrixlight binding from branch"
    This reverts commit aeb6391d351ff943e81060b5fe5c8a16a00e5ee7.
    
    Signed-off-by: DrRSatzteil <thomas_lauterbach@arcor.de>

--- bundles/org.openhab.binding.unifi/src/main/java/org/openhab/binding/unifi/internal/api/UniFiController.java
+++ bundles/org.openhab.binding.unifi/src/main/java/org/openhab/binding/unifi/internal/api/UniFiController.java
@@ -281,7 +281,6 @@ public class UniFiController {
             } else {
                 login();
                 result = (T) executeRequest(request);
-                logout();
             }
         } catch (final UniFiNotAuthorizedException e) {
             logger.warn("Not Authorized! Please make sure your controller credentials have administrator rights");
