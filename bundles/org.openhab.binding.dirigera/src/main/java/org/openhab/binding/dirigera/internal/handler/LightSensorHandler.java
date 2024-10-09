339db5ff7e02fbfdcb63354971653d0016a5eb54
Bernd Weymann <bernd.weymann@gmail.com>
2024-10-09 23:19:01 +0200

    add light sensor
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

---
+++ bundles/org.openhab.binding.dirigera/src/main/java/org/openhab/binding/dirigera/internal/handler/LightSensorHandler.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.dirigera.internal.handler;
+
+import static org.openhab.binding.dirigera.internal.Constants.CHANNEL_ILLUMINANCE;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.json.JSONObject;
+import org.openhab.binding.dirigera.internal.model.Model;
+import org.openhab.core.library.types.QuantityType;
+import org.openhab.core.library.unit.Units;
+import org.openhab.core.thing.ChannelUID;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.types.Command;
+import org.openhab.core.types.RefreshType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link LightSensorHandler} basic DeviceHandler for all devices
+ *
+ * @author Bernd Weymann - Initial contribution
+ */
+@NonNullByDefault
+public class LightSensorHandler extends BaseDeviceHandler {
+    private final Logger logger = LoggerFactory.getLogger(LightSensorHandler.class);
+
+    public LightSensorHandler(Thing thing, Map<String, String> mapping) {
+        super(thing, mapping);
+        super.setChildHandler(this);
+    }
+
+    @Override
+    public void initialize() {
+        // handle general initialize like setting bridge
+        super.initialize();
+        gateway().registerDevice(this);
+        // finally get attributes from model in order to get initial values
+        JSONObject values = gateway().model().getAllFor(config.id);
+        logger.error("DIRIGERA MOTION_DEVICE values for initial update {}", values);
+        handleUpdate(values);
+    }
+
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        // only handle RefreshType
+        String channel = channelUID.getIdWithoutGroup();
+        logger.trace("DIRIGERA MOTION_DEVICE handle command {} for {}", command, channel);
+        if (command instanceof RefreshType) {
+            JSONObject values = gateway().model().getAllFor(config.id);
+            handleUpdate(values);
+        }
+    }
+
+    @Override
+    public void handleUpdate(JSONObject update) {
+        // handle reachable flag
+        super.handleUpdate(update);
+        // now device specific
+        if (update.has(Model.ATTRIBUTES)) {
+            JSONObject attributes = update.getJSONObject(Model.ATTRIBUTES);
+            Iterator<String> attributesIterator = attributes.keys();
+            logger.trace("DIRIGERA MOTION_DEVICE update delivered {} attributes", attributes.length());
+            while (attributesIterator.hasNext()) {
+                String key = attributesIterator.next();
+                String targetChannel = property2ChannelMap.get(key);
+                if (targetChannel != null) {
+                    if (CHANNEL_ILLUMINANCE.equals(targetChannel)) {
+                        updateState(new ChannelUID(thing.getUID(), targetChannel),
+                                QuantityType.valueOf(attributes.getInt(key), Units.LUX));
+                    } else {
+                        logger.trace("DIRIGERA MOTION_DEVICE no channel for {} available", key);
+                    }
+                } else {
+                    logger.trace("DIRIGERA MOTION_DEVICE no targetChannel for {}", key);
+                }
+            }
+        }
+    }
+}
