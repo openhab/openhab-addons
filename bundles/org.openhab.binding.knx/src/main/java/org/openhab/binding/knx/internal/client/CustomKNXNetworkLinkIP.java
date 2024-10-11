340e1a903c62776bae3e5d7a150ef35bbec1d60b
Holger Friedrich <mail@holger-friedrich.de>
2024-10-11 23:45:38 +0200

    [knx] Perpare for Calimero 3.0
    Signed-off-by: Holger Friedrich <mail@holger-friedrich.de>

--- bundles/org.openhab.binding.knx/src/main/java/org/openhab/binding/knx/internal/client/CustomKNXNetworkLinkIP.java
+++ bundles/org.openhab.binding.knx/src/main/java/org/openhab/binding/knx/internal/client/CustomKNXNetworkLinkIP.java
@@ -14,10 +14,10 @@ package org.openhab.binding.knx.internal.client;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
-import tuwien.auto.calimero.KNXException;
-import tuwien.auto.calimero.knxnetip.KNXnetIPConnection;
-import tuwien.auto.calimero.link.KNXNetworkLinkIP;
-import tuwien.auto.calimero.link.medium.KNXMediumSettings;
+import io.calimero.KNXException;
+import io.calimero.knxnetip.KNXnetIPConnection;
+import io.calimero.link.KNXNetworkLinkIP;
+import io.calimero.link.medium.KNXMediumSettings;
 
 /**
  * Subclass of {@link KNXNetworkLinkIP} which exposes the protected constructor in order to work-around
@@ -29,8 +29,8 @@ import tuwien.auto.calimero.link.medium.KNXMediumSettings;
 @NonNullByDefault
 public class CustomKNXNetworkLinkIP extends KNXNetworkLinkIP {
 
-    public static final int TUNNELING = KNXNetworkLinkIP.TUNNELING;
-    public static final int TUNNELINGV2 = KNXNetworkLinkIP.TunnelingV2;
+    public static final int TunnelingV1 = KNXNetworkLinkIP.TunnelingV1;
+    public static final int TunnelingV2 = KNXNetworkLinkIP.TunnelingV2;
     public static final int ROUTING = KNXNetworkLinkIP.ROUTING;
 
     CustomKNXNetworkLinkIP(final int serviceMode, KNXnetIPConnection conn, KNXMediumSettings settings)
