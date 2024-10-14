c7af89f276cc68ce1996245eb75f609eba6a41e3
Bernd Weymann <bernd.weymann@gmail.com>
2024-10-14 09:52:49 +0200

    prepare unit tests
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

--- bundles/org.openhab.binding.dirigera/src/test/java/org/openhab/binding/dirigera/TestModel.java
+++ bundles/org.openhab.binding.dirigera/src/test/java/org/openhab/binding/dirigera/TestModel.java
@@ -12,11 +12,11 @@
  */
 package org.openhab.binding.dirigera;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
 import java.util.concurrent.ExecutorService;
 
-import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.openhab.binding.dirigera.internal.interfaces.Gateway;
 import org.openhab.binding.dirigera.internal.model.Model;
@@ -31,15 +31,21 @@ class TestModel {
 
     @Test
     void testCustomName() {
-        String modelString = FileReader.readFileInString("src/test/resources/NewHome.json");
-        JSONObject modelJson = new JSONObject(modelString);
-        Model model = new Model(mock(Gateway.class), modelJson);
-        System.out.println(
-                "Custom name for light sensor " + model.getCustonNameFor("5ac5e131-44a4-4d75-be78-759a095d31fb_3"));
+        Model model = new Model(mock(Gateway.class));
+        String modelString = FileReader.readFileInString("src/test/resources/CustomNameHome.json");
+        model.update(modelString);
+        // test device with given custom name
+        assertEquals("Floor Lamp", model.getCustonNameFor("891790db-8c17-483a-a1a6-c85bffd3a373_1"), "Floor Lamp name");
+        // test device without custom name - take model name
+        assertEquals("VALLHORN Wireless Motion Sensor",
+                model.getCustonNameFor("5ac5e131-44a4-4d75-be78-759a095d31fb_3"), "Motion Sensor name");
+        // test device without custom name and no model name
+        assertEquals("light", model.getCustonNameFor("c27faa27-4c18-464f-81a0-a31ce57d83d5_1"), "Lamp");
     }
 
     @Test
     void testModelStress() {
+        String mdoelString = FileReader.readFileInString("src/test/resources/NewHome.json");
         ExecutorService scheduler = ThreadPoolManager.getPool("ModelStressTest");
     }
 }
