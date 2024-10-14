60a59363118eecb7f072ea9c6ae61e764d097c9e
Thomas Lauterbach <2452988+DrRSatzteil@users.noreply.github.com>
2024-10-14 22:41:00 +0200

    [unifi] Add some standard channels to access points (#17530)
    Signed-off-by: Thomas Lauterbach <thomas_lauterbach@arcor.de>
    Signed-off-by: Ciprian Pascu <contact@ciprianpascu.ro>

--- bundles/org.openhab.binding.unifi/src/main/java/org/openhab/binding/unifi/internal/UniFiBindingConstants.java
+++ bundles/org.openhab.binding.unifi/src/main/java/org/openhab/binding/unifi/internal/UniFiBindingConstants.java
@@ -61,21 +61,23 @@ public final class UniFiBindingConstants {
     public static final String CHANNEL_PASSPHRASE = "passphrase";
     public static final String CHANNEL_QRCODE_ENCODING = "qrcodeEncoding";
 
-    // List of common wired + wireless client channels
+    // List of common wired + wireless client + device channels
     public static final String CHANNEL_ONLINE = "online";
     public static final String CHANNEL_NAME = "name";
-    public static final String CHANNEL_HOSTNAME = "hostname";
     public static final String CHANNEL_SITE = "site";
-    public static final String CHANNEL_MAC_ADDRESS = "macAddress";
     public static final String CHANNEL_IP_ADDRESS = "ipAddress";
     public static final String CHANNEL_UPTIME = "uptime";
     public static final String CHANNEL_LAST_SEEN = "lastSeen";
+    public static final String CHANNEL_EXPERIENCE = "experience";
+
+    // List of common wired + wireless client channels
+    public static final String CHANNEL_HOSTNAME = "hostname";
+    public static final String CHANNEL_MAC_ADDRESS = "macAddress";
     public static final String CHANNEL_GUEST = "guest";
     public static final String CHANNEL_BLOCKED = "blocked";
     public static final String CHANNEL_RECONNECT = "reconnect";
     public static final String CHANNEL_CMD = "cmd";
     public static final String CHANNEL_CMD_RECONNECT = "reconnect";
-    public static final String CHANNEL_EXPERIENCE = "experience";
 
     // List of additional wireless client channels
     public static final String CHANNEL_AP = "ap";
@@ -95,8 +97,9 @@ public final class UniFiBindingConstants {
     public static final String CHANNEL_PORT_POE_VOLTAGE = "voltage";
     public static final String CHANNEL_PORT_POE_CURRENT = "current";
 
-    // List of access point channels
+    // List of access point device channels
     public static final String CHANNEL_AP_ENABLE = "enable";
+    public static final String CHANNEL_AP_STATE = "state";
 
     // List of all Parameters
     public static final String PARAMETER_HOST = "host";
