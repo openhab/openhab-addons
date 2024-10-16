e1855c37e5b2dafe9e5f333ed77afab24e16e536
Bernd Weymann <bernd.weymann@gmail.com>
2024-10-16 02:12:49 +0200

    add lightcontroller device
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

---
+++ bundles/org.openhab.binding.dirigera/src/main/java/org/openhab/binding/dirigera/internal/handler/LightControllerHandler.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.dirigera.internal.handler;
+
+import static org.openhab.binding.dirigera.internal.Constants.PROPERTY_DEVICES;
+
+import java.util.Map;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.json.JSONObject;
+import org.openhab.core.thing.ChannelUID;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.types.Command;
+import org.openhab.core.types.RefreshType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link LightControllerHandler} basic DeviceHandler for all devices
+ *
+ * @author Bernd Weymann - Initial contribution
+ */
+@NonNullByDefault
+public class LightControllerHandler extends BaseDeviceHandler {
+    private final Logger logger = LoggerFactory.getLogger(LightControllerHandler.class);
+
+    public LightControllerHandler(Thing thing, Map<String, String> mapping) {
+        super(thing, mapping);
+        super.setChildHandler(this);
+    }
+
+    @Override
+    public void initialize() {
+        // handle general initialize like setting bridge
+        super.initialize();
+        gateway().registerDevice(this);
+        // finally get attributes from model in order to get initial values
+        JSONObject values = gateway().model().getAllFor(config.id, PROPERTY_DEVICES);
+        handleUpdate(values);
+    }
+
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        // only handle RefreshType
+        String channel = channelUID.getIdWithoutGroup();
+        logger.trace("DIRIGERA MOTION_DEVICE handle command {} for {}", command, channel);
+        if (command instanceof RefreshType) {
+            JSONObject values = gateway().model().getAllFor(config.id, PROPERTY_DEVICES);
+            handleUpdate(values);
+        }
+    }
+
+    @Override
+    public void handleUpdate(JSONObject update) {
+        // handle reachable flag, no more special handling
+        super.handleUpdate(update);
+    }
+}
