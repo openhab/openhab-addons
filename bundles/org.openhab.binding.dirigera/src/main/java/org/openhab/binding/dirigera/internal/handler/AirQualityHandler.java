aeff770f4190d3cf170df555ac466ff96950f04c
Bernd Weymann <bernd.weymann@gmail.com>
2024-10-17 11:35:40 +0200

    add envrionment sensor
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

---
+++ bundles/org.openhab.binding.dirigera/src/main/java/org/openhab/binding/dirigera/internal/handler/AirQualityHandler.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.dirigera.internal.handler;
+
+import static org.openhab.binding.dirigera.internal.Constants.*;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.json.JSONObject;
+import org.openhab.binding.dirigera.internal.model.Model;
+import org.openhab.core.library.types.QuantityType;
+import org.openhab.core.library.unit.SIUnits;
+import org.openhab.core.library.unit.Units;
+import org.openhab.core.thing.ChannelUID;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.types.Command;
+import org.openhab.core.types.RefreshType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link AirQualityHandler} basic DeviceHandler for all devices
+ *
+ * @author Bernd Weymann - Initial contribution
+ */
+@NonNullByDefault
+public class AirQualityHandler extends BaseDeviceHandler {
+    private final Logger logger = LoggerFactory.getLogger(AirQualityHandler.class);
+
+    public AirQualityHandler(Thing thing, Map<String, String> mapping) {
+        super(thing, mapping);
+        super.setChildHandler(this);
+    }
+
+    @Override
+    public void initialize() {
+        // handle general initialize like setting bridge
+        super.initialize();
+        // finally get attributes from model in order to get initial values
+        JSONObject values = gateway().model().getAllFor(config.id, PROPERTY_DEVICES);
+        logger.trace("DIRIGERA AIR_QUALITY values for initial update {}", values);
+        handleUpdate(values);
+    }
+
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        // only handle RefreshType
+        String channel = channelUID.getIdWithoutGroup();
+        logger.trace("DIRIGERA AIR_QUALITY handle command {} for {}", command, channel);
+        if (command instanceof RefreshType) {
+            JSONObject values = gateway().model().getAllFor(config.id, PROPERTY_DEVICES);
+            handleUpdate(values);
+        }
+    }
+
+    @Override
+    public void handleUpdate(JSONObject update) {
+        // handle reachable flag
+        super.handleUpdate(update);
+        // now device specific
+        if (update.has(Model.ATTRIBUTES)) {
+            JSONObject attributes = update.getJSONObject(Model.ATTRIBUTES);
+            Iterator<String> attributesIterator = attributes.keys();
+            logger.trace("DIRIGERA AIR_QUALITY update delivered {} attributes", attributes.length());
+            while (attributesIterator.hasNext()) {
+                String key = attributesIterator.next();
+                String targetChannel = property2ChannelMap.get(key);
+                if (targetChannel != null) {
+                    switch (targetChannel) {
+                        case CHANNEL_TEMPERATURE:
+                            updateState(new ChannelUID(thing.getUID(), CHANNEL_TEMPERATURE),
+                                    QuantityType.valueOf(attributes.getDouble(key), SIUnits.CELSIUS));
+                            break;
+                        case CHANNEL_HUMIDITY:
+                            updateState(new ChannelUID(thing.getUID(), CHANNEL_HUMIDITY),
+                                    QuantityType.valueOf(attributes.getDouble(key), Units.PERCENT));
+                            break;
+                        case CHANNEL_PARTICULATE_MATTER:
+                            updateState(new ChannelUID(thing.getUID(), CHANNEL_PARTICULATE_MATTER),
+                                    QuantityType.valueOf(attributes.getDouble(key), Units.MICROGRAM_PER_CUBICMETRE));
+                            break;
+                        case CHANNEL_VOC_INDEX:
+                            updateState(new ChannelUID(thing.getUID(), CHANNEL_VOC_INDEX),
+                                    QuantityType.valueOf(attributes.getDouble(key) + "mg/m³"));
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
