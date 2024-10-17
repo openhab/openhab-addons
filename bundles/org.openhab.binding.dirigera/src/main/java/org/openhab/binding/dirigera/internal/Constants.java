5e40b2357167c6fbc9e1e57ff11d31fb5245ab0c
Bernd Weymann <bernd.weymann@gmail.com>
2024-10-17 11:35:40 +0200

    add envrionment sensor
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

--- bundles/org.openhab.binding.dirigera/src/main/java/org/openhab/binding/dirigera/internal/Constants.java
+++ bundles/org.openhab.binding.dirigera/src/main/java/org/openhab/binding/dirigera/internal/Constants.java
@@ -43,12 +43,13 @@ public class Constants {
     public static final ThingTypeUID THING_TYPE_SCENE = new ThingTypeUID(BINDING_ID, "scene");
     public static final ThingTypeUID THING_TYPE_REPEATER = new ThingTypeUID(BINDING_ID, "repeater");
     public static final ThingTypeUID THING_TYPE_LIGHT_CONTROLLER = new ThingTypeUID(BINDING_ID, "light-controller");
+    public static final ThingTypeUID THING_TYPE_AIR_QUALITY = new ThingTypeUID(BINDING_ID, "air-quality");
     public static final ThingTypeUID THING_TYPE_UNKNNOWN = new ThingTypeUID(BINDING_ID, "unkown");
 
     public static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Set.of(THING_TYPE_GATEWAY,
             THING_TYPE_COLOR_LIGHT, THING_TYPE_TEMPERATURE_LIGHT, THING_TYPE_MOTION_SENSOR, THING_TYPE_CONTACT_SENSOR,
             THING_TYPE_SMART_PLUG, THING_TYPE_SPEAKER, THING_TYPE_SCENE, THING_TYPE_REPEATER,
-            THING_TYPE_LIGHT_CONTROLLER, THING_TYPE_MOTION_LIGHT_SENSOR, THING_TYPE_PLUG);
+            THING_TYPE_LIGHT_CONTROLLER, THING_TYPE_MOTION_LIGHT_SENSOR, THING_TYPE_PLUG, THING_TYPE_AIR_QUALITY);
 
     public static final Set<ThingTypeUID> IGNORE_THING_TYPES_UIDS = Set.of(THING_TYPE_LIGHT_SENSOR);
 
@@ -87,6 +88,7 @@ public class Constants {
     public static final String DEVICE_TYPE_SPEAKER = "speaker";
     public static final String DEVICE_TYPE_REPEATER = "repeater";
     public static final String DEVICE_TYPE_LIGHT_CONTROLLER = "lightController";
+    public static final String DEVICE_TYPE_ENVIRONMENT_SENSOR = "environmentSensor";
 
     // Gateway channels
     public static final String CHANNEL_STATISTICS = "statistics";
@@ -135,9 +137,19 @@ public class Constants {
     public static final String CHANNEL_TRIGGER = "trigger";
     public static final String CHANNEL_LAST_TRIGGER = "last-trigger";
 
+    // Air quality channels
+    public static final String CHANNEL_TEMPERATURE = "temperature";
+    public static final String CHANNEL_HUMIDITY = "humidity";
+    public static final String CHANNEL_PARTICULATE_MATTER = "particulate-matter";
+    public static final String CHANNEL_VOC_INDEX = "voc-index";
+
     // Websocket update types
     public static final String EVENT_TYPE_STATE_CHANGE = "deviceStateChanged";
     public static final String EVENT_TYPE_SCENE_UPDATE = "sceneUpdated";
+
+    /**
+     * Maps connecting device attributes to channel ids
+     */
 
     // Mappings for ota
     public static final Map<String, Integer> OTA_STATUS_MAP = Map.of("upToDate", 0, "updateAvailable", 1);
@@ -179,6 +191,10 @@ public class Constants {
             PROPERTY_OTA_STATE, CHANNEL_OTA_STATE, PROPERTY_OTA_PROGRESS, CHANNEL_OTA_PROGRESS);
 
     public static final Map<String, String> LIGHT_CONTROLLER_MAP = Map.of("batteryPercentage", CHANNEL_BATTERY_LEVEL,
+            PROPERTY_OTA_STATUS, CHANNEL_OTA_STATUS, PROPERTY_OTA_STATE, CHANNEL_OTA_STATE, PROPERTY_OTA_PROGRESS,
+            CHANNEL_OTA_PROGRESS);
+    public static final Map<String, String> AIR_QUALITY_MAP = Map.of("currentTemperature", CHANNEL_TEMPERATURE,
+            "currentRH", CHANNEL_HUMIDITY, "currentPM25", CHANNEL_PARTICULATE_MATTER, "vocIndex", CHANNEL_VOC_INDEX,
             PROPERTY_OTA_STATUS, CHANNEL_OTA_STATUS, PROPERTY_OTA_STATE, CHANNEL_OTA_STATE, PROPERTY_OTA_PROGRESS,
             CHANNEL_OTA_PROGRESS);
 }
