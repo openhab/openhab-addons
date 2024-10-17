372e1ca76ac30164d68837b8ac0ebdc33c66f564
Wouter Born <github@maindrain.net>
2024-10-17 07:28:00 +0200

    Remove JUnit 4 dependencies (#17580)
    Using JUnit 5 should suffice.
    
    Signed-off-by: Wouter Born <github@maindrain.net>

--- bundles/org.openhab.binding.broadlink/src/test/java/org/openhab/binding/broadlink/internal/handler/BroadlinkRemoteModelProHandlerTest.java
+++ bundles/org.openhab.binding.broadlink/src/test/java/org/openhab/binding/broadlink/internal/handler/BroadlinkRemoteModelProHandlerTest.java
@@ -12,8 +12,8 @@
  */
 package org.openhab.binding.broadlink.internal.handler;
 
-import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
 import java.io.IOException;
