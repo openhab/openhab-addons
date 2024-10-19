455330e741d3e7d7abd16e4231a25dfa24e6f4ba
lsiepel <leosiepel@gmail.com>
2024-10-19 16:59:52 +0200

    [multiple] Reduce SAT warnings (#17564)
    * NoEmptyLineSeparatorCheck
    * ModifierOrderCheck
    * TypeNameCheck
    * ConstantNameCheck loggers
    * UnusedPrivateField
    * Fix imports
    * New line
    * dynamodb static logger
    
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.bluetooth.radoneye/src/main/java/org/openhab/binding/bluetooth/radoneye/internal/RadoneyeDataParser.java
+++ bundles/org.openhab.binding.bluetooth.radoneye/src/main/java/org/openhab/binding/bluetooth/radoneye/internal/RadoneyeDataParser.java
@@ -34,14 +34,14 @@ public class RadoneyeDataParser {
     private static final int EXPECTED_DATA_LEN_V2 = 12;
     private static final int EXPECTED_VER_PLUS = 1;
 
-    private static final Logger logger = LoggerFactory.getLogger(RadoneyeDataParser.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RadoneyeDataParser.class);
 
     private RadoneyeDataParser() {
     }
 
     public static Map<String, Number> parseRd200Data(int fwVersion, int[] data) throws RadoneyeParserException {
-        logger.debug("Parsed data length: {}", data.length);
-        logger.debug("Parsed data: {}", data);
+        LOGGER.debug("Parsed data length: {}", data.length);
+        LOGGER.debug("Parsed data: {}", data);
 
         final Map<String, Number> result = new HashMap<>();
 
