b2255ccbc88bffbf725980e44562611df56664be
Jørgen Melhus <5099412+jmelhus@users.noreply.github.com>
2024-10-21 21:32:23 +0200

    [entsoe] Initial contribution (#17416)
    Signed-off-by: Jørgen Melhus <jmelhus@outlook.com>

---
+++ bundles/org.openhab.binding.entsoe/src/main/java/org/openhab/binding/entsoe/internal/exception/EntsoeConfigurationException.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.entsoe.internal.exception;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * @author Miika Jukka - Initial contribution
+ *
+ */
+@NonNullByDefault
+public class EntsoeConfigurationException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public EntsoeConfigurationException(String message) {
+        super(message);
+    }
+
+    public EntsoeConfigurationException(Throwable cause) {
+        super(cause);
+    }
+}
