2e7e47b3dfc653b9293d391239c0e65bbd3041fd
Artur-Fedjukevits <87441995+Artur-Fedjukevits@users.noreply.github.com>
2024-10-21 14:08:39 +0200

    Update bundles/org.openhab.binding.chatgpt/src/main/java/org/openhab/binding/chatgpt/internal/ChatGPTHandler.java
    Co-authored-by: Kai Kreuzer <kai@openhab.org>
    Signed-off-by: Artur-Fedjukevits <87441995+Artur-Fedjukevits@users.noreply.github.com>

--- bundles/org.openhab.binding.chatgpt/src/main/java/org/openhab/binding/chatgpt/internal/ChatGPTHandler.java
+++ bundles/org.openhab.binding.chatgpt/src/main/java/org/openhab/binding/chatgpt/internal/ChatGPTHandler.java
@@ -230,7 +230,6 @@ public class ChatGPTHandler extends BaseThingHandler {
 
                         if (data != null) {
                             logger.debug("Models: {}", data.toString());
-
                             List<String> modelList = new ArrayList<>();
 
                             data.forEach(model -> {
