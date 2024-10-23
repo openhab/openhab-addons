b560c0d3dcf2ef66be83ab7e74b516781c78e044
Bernd Weymann <bernd.weymann@gmail.com>
2024-10-23 23:53:11 +0200

    add shortcut controller
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

--- bundles/org.openhab.binding.dirigera/src/main/java/org/openhab/binding/dirigera/internal/Constants.java
+++ bundles/org.openhab.binding.dirigera/src/main/java/org/openhab/binding/dirigera/internal/Constants.java
@@ -48,6 +48,8 @@ public class Constants {
     public static final ThingTypeUID THING_TYPE_LIGHT_CONTROLLER = new ThingTypeUID(BINDING_ID, "light-controller");
     public static final ThingTypeUID THING_TYPE_BLIND_CONTROLLER = new ThingTypeUID(BINDING_ID, "blind-controller");
     public static final ThingTypeUID THING_TYPE_SOUND_CONTROLLER = new ThingTypeUID(BINDING_ID, "sound-controller");
+    public static final ThingTypeUID THING_TYPE_SHORTCUT_CONTROLLER = new ThingTypeUID(BINDING_ID,
+            "shortcut-controller");
     public static final ThingTypeUID THING_TYPE_AIR_PURIFIER = new ThingTypeUID(BINDING_ID, "air-purifier");
     public static final ThingTypeUID THING_TYPE_AIR_QUALITY = new ThingTypeUID(BINDING_ID, "air-quality");
     public static final ThingTypeUID THING_TYPE_WATER_SENSOR = new ThingTypeUID(BINDING_ID, "water-sensor");
@@ -59,8 +61,8 @@ public class Constants {
             THING_TYPE_COLOR_LIGHT, THING_TYPE_TEMPERATURE_LIGHT, THING_TYPE_MOTION_SENSOR, THING_TYPE_CONTACT_SENSOR,
             THING_TYPE_SIMPLE_PLUG, THING_TYPE_POWER_PLUG, THING_TYPE_SMART_PLUG, THING_TYPE_SPEAKER, THING_TYPE_SCENE,
             THING_TYPE_REPEATER, THING_TYPE_LIGHT_CONTROLLER, THING_TYPE_BLIND_CONTROLLER, THING_TYPE_SOUND_CONTROLLER,
-            THING_TYPE_MOTION_LIGHT_SENSOR, THING_TYPE_AIR_QUALITY, THING_TYPE_AIR_PURIFIER, THING_TYPE_WATER_SENSOR,
-            THING_TYPE_BLIND);
+            THING_TYPE_SHORTCUT_CONTROLLER, THING_TYPE_MOTION_LIGHT_SENSOR, THING_TYPE_AIR_QUALITY,
+            THING_TYPE_AIR_PURIFIER, THING_TYPE_WATER_SENSOR, THING_TYPE_BLIND);
 
     public static final Set<ThingTypeUID> IGNORE_THING_TYPES_UIDS = Set.of(THING_TYPE_LIGHT_SENSOR);
 
@@ -114,6 +116,7 @@ public class Constants {
     public static final String DEVICE_TYPE_LIGHT_CONTROLLER = "lightController";
     public static final String DEVICE_TYPE_BLIND_CONTROLLER = "blindsController";
     public static final String DEVICE_TYPE_SOUND_CONTROLLER = "soundController";
+    public static final String DEVICE_TYPE_SHORTCUT_CONTROLLER = "shortcutController";
 
     // Gateway channels
     public static final String CHANNEL_STATISTICS = "statistics";
@@ -241,6 +244,9 @@ public class Constants {
             PROPERTY_OTA_STATUS, CHANNEL_OTA_STATUS, PROPERTY_OTA_STATE, CHANNEL_OTA_STATE, PROPERTY_OTA_PROGRESS,
             CHANNEL_OTA_PROGRESS);
     public static final Map<String, String> SOUND_CONTROLLER_MAP = Map.of("batteryPercentage", CHANNEL_BATTERY_LEVEL,
+            PROPERTY_OTA_STATUS, CHANNEL_OTA_STATUS, PROPERTY_OTA_STATE, CHANNEL_OTA_STATE, PROPERTY_OTA_PROGRESS,
+            CHANNEL_OTA_PROGRESS);
+    public static final Map<String, String> SHORTCUT_CONTROLLER_MAP = Map.of("batteryPercentage", CHANNEL_BATTERY_LEVEL,
             PROPERTY_OTA_STATUS, CHANNEL_OTA_STATUS, PROPERTY_OTA_STATE, CHANNEL_OTA_STATE, PROPERTY_OTA_PROGRESS,
             CHANNEL_OTA_PROGRESS);
     public static final Map<String, String> AIR_QUALITY_MAP = Map.of("currentTemperature", CHANNEL_TEMPERATURE,
