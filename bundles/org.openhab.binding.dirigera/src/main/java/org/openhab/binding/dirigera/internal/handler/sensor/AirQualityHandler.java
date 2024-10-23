2cf211e126752bf228e50fa1012aa40c77f6edda
Bernd Weymann <bernd.weymann@gmail.com>
2024-10-23 22:30:32 +0200

    handler reorg
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

--- bundles/org.openhab.binding.dirigera/src/main/java/org/openhab/binding/dirigera/internal/handler/sensor/AirQualityHandler.java
+++ bundles/org.openhab.binding.dirigera/src/main/java/org/openhab/binding/dirigera/internal/handler/sensor/AirQualityHandler.java
@@ -10,30 +10,33 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.dirigera.internal.handler;
+package org.openhab.binding.dirigera.internal.handler.sensor;
 
-import static org.openhab.binding.dirigera.internal.Constants.CHANNEL_STATE;
+import static org.openhab.binding.dirigera.internal.Constants.*;
 
 import java.util.Iterator;
 import java.util.Map;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.json.JSONObject;
+import org.openhab.binding.dirigera.internal.handler.BaseHandler;
 import org.openhab.binding.dirigera.internal.model.Model;
-import org.openhab.core.library.types.OpenClosedType;
+import org.openhab.core.library.types.QuantityType;
+import org.openhab.core.library.unit.SIUnits;
+import org.openhab.core.library.unit.Units;
 import org.openhab.core.thing.ChannelUID;
 import org.openhab.core.thing.Thing;
 import org.openhab.core.types.Command;
 
 /**
- * The {@link ContactSensorHandler} basic DeviceHandler for all devices
+ * The {@link AirQualityHandler} basic DeviceHandler for all devices
  *
  * @author Bernd Weymann - Initial contribution
  */
 @NonNullByDefault
-public class ContactSensorHandler extends BaseHandler {
+public class AirQualityHandler extends BaseHandler {
 
-    public ContactSensorHandler(Thing thing, Map<String, String> mapping) {
+    public AirQualityHandler(Thing thing, Map<String, String> mapping) {
         super(thing, mapping);
         super.setChildHandler(this);
     }
@@ -64,12 +67,23 @@ public class ContactSensorHandler extends BaseHandler {
                 String key = attributesIterator.next();
                 String targetChannel = property2ChannelMap.get(key);
                 if (targetChannel != null) {
-                    if (CHANNEL_STATE.equals(targetChannel)) {
-                        OpenClosedType state = OpenClosedType.CLOSED;
-                        if (attributes.getBoolean(key)) {
-                            state = OpenClosedType.OPEN;
-                        }
-                        updateState(new ChannelUID(thing.getUID(), targetChannel), state);
+                    switch (targetChannel) {
+                        case CHANNEL_TEMPERATURE:
+                            updateState(new ChannelUID(thing.getUID(), CHANNEL_TEMPERATURE),
+                                    QuantityType.valueOf(attributes.getDouble(key), SIUnits.CELSIUS));
+                            break;
+                        case CHANNEL_HUMIDITY:
+                            updateState(new ChannelUID(thing.getUID(), CHANNEL_HUMIDITY),
+                                    QuantityType.valueOf(attributes.getDouble(key), Units.PERCENT));
+                            break;
+                        case CHANNEL_PARTICULATE_MATTER:
+                            updateState(new ChannelUID(thing.getUID(), CHANNEL_PARTICULATE_MATTER),
+                                    QuantityType.valueOf(attributes.getDouble(key), Units.MICROGRAM_PER_CUBICMETRE));
+                            break;
+                        case CHANNEL_VOC_INDEX:
+                            updateState(new ChannelUID(thing.getUID(), CHANNEL_VOC_INDEX),
+                                    QuantityType.valueOf(attributes.getDouble(key) + "mg/m³"));
+                            break;
                     }
                 }
             }
