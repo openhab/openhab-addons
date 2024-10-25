48de73e266112f55d11dd9f97d5a1965ea2f7dc8
Bernd Weymann <bernd.weymann@gmail.com>
2024-10-25 07:23:28 +0200

    add dimmable light
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

--- bundles/org.openhab.binding.dirigera/src/main/java/org/openhab/binding/dirigera/internal/handler/light/DimmableLightHandler.java
+++ bundles/org.openhab.binding.dirigera/src/main/java/org/openhab/binding/dirigera/internal/handler/light/DimmableLightHandler.java
@@ -12,13 +12,14 @@
  */
 package org.openhab.binding.dirigera.internal.handler.light;
 
-import static org.openhab.binding.dirigera.internal.Constants.CHANNEL_LIGHT_TEMPERATURE;
+import static org.openhab.binding.dirigera.internal.Constants.CHANNEL_LIGHT_BRIGHTNESS;
 
 import java.util.Iterator;
 import java.util.Map;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.json.JSONObject;
+import org.openhab.binding.dirigera.internal.handler.BaseHandler;
 import org.openhab.binding.dirigera.internal.model.Model;
 import org.openhab.core.library.types.PercentType;
 import org.openhab.core.thing.ChannelUID;
@@ -28,22 +29,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The {@link TemperatureLightHandler} basic DeviceHandler for all devices
+ * The {@link DimmableLightHandler} basic DeviceHandler for all devices
  *
  * @author Bernd Weymann - Initial contribution
  */
 @NonNullByDefault
-public class TemperatureLightHandler extends DimmableLightHandler {
-    private final Logger logger = LoggerFactory.getLogger(TemperatureLightHandler.class);
+public class DimmableLightHandler extends BaseHandler {
+    private final Logger logger = LoggerFactory.getLogger(DimmableLightHandler.class);
 
-    // default values of "standard Ikea lamps" from json
-    // "colorTemperatureMin": 4000,
-    // "colorTemperatureMax": 2202,
-    private int colorTemperatureMax = 2202;
-    private int colorTemperatureMin = 4000;
-    private int range = colorTemperatureMin - colorTemperatureMax;
-
-    public TemperatureLightHandler(Thing thing, Map<String, String> mapping) {
+    public DimmableLightHandler(Thing thing, Map<String, String> mapping) {
         super(thing, mapping);
         super.setChildHandler(this);
     }
@@ -53,21 +47,6 @@ public class TemperatureLightHandler extends DimmableLightHandler {
         super.initialize();
         if (super.checkHandler()) {
             JSONObject values = gateway().api().readDevice(config.id);
-
-            // check for settings of color temperature in attributes
-            JSONObject attributes = values.getJSONObject(Model.ATTRIBUTES);
-            Iterator<String> attributesIterator = attributes.keys();
-            while (attributesIterator.hasNext()) {
-                String key = attributesIterator.next();
-                if ("colorTemperatureMin".equals(key)) {
-                    colorTemperatureMin = attributes.getInt(key);
-                } else if ("colorTemperatureMax".equals(key)) {
-                    colorTemperatureMax = attributes.getInt(key);
-                }
-            }
-            range = colorTemperatureMin - colorTemperatureMax;
-            logger.trace("DIRIGERA TEMPERATURE_LIGHT_DEVICE new temperatures from {} to {}", colorTemperatureMin,
-                    colorTemperatureMax);
             handleUpdate(values);
         }
     }
@@ -79,11 +58,10 @@ public class TemperatureLightHandler extends DimmableLightHandler {
         String targetProperty = channel2PropertyMap.get(channel);
         if (targetProperty != null) {
             switch (channel) {
-                case CHANNEL_LIGHT_TEMPERATURE:
+                case CHANNEL_LIGHT_BRIGHTNESS:
                     if (command instanceof PercentType percent) {
-                        int kelvin = Math.round(colorTemperatureMin - (range * percent.intValue() / 100));
                         JSONObject attributes = new JSONObject();
-                        attributes.put(targetProperty, kelvin);
+                        attributes.put(targetProperty, percent.intValue());
                         logger.trace("DIRIGERA TEMPERATURE_LIGHT_DEVICE send to API {}", attributes);
                         gateway().api().sendPatch(config.id, attributes);
                     } else {
@@ -108,14 +86,9 @@ public class TemperatureLightHandler extends DimmableLightHandler {
                 String targetChannel = property2ChannelMap.get(key);
                 if (targetChannel != null) {
                     switch (targetChannel) {
-                        case CHANNEL_LIGHT_TEMPERATURE:
-                            int kelvin = attributes.getInt(key);
-                            // seems some lamps are delivering temperature values out of range
-                            // keep it in range with min/max
-                            kelvin = Math.min(kelvin, colorTemperatureMin);
-                            kelvin = Math.max(kelvin, colorTemperatureMax);
-                            int percent = Math.round(100 - ((kelvin - colorTemperatureMax) * 100 / range));
-                            updateState(new ChannelUID(thing.getUID(), targetChannel), new PercentType(percent));
+                        case CHANNEL_LIGHT_BRIGHTNESS:
+                            updateState(new ChannelUID(thing.getUID(), targetChannel),
+                                    new PercentType(attributes.getInt(key)));
                             break;
                     }
                 }
