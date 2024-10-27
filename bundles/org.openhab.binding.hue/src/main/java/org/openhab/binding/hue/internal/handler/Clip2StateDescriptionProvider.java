db34aa3a987fdd362d3538fe4df9f0ec301f695d
AndrewFG <software@whitebear.ch>
2024-10-27 10:36:26 +0000

    [hue] provide color temperature min/max values dynamically in state description
    Signed-off-by: AndrewFG <software@whitebear.ch>

--- bundles/org.openhab.binding.hue/src/main/java/org/openhab/binding/hue/internal/handler/Clip2StateDescriptionProvider.java
+++ bundles/org.openhab.binding.hue/src/main/java/org/openhab/binding/hue/internal/handler/Clip2StateDescriptionProvider.java
@@ -12,19 +12,30 @@
  */
 package org.openhab.binding.hue.internal.handler;
 
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.core.events.EventPublisher;
+import org.openhab.core.thing.Channel;
+import org.openhab.core.thing.ChannelUID;
 import org.openhab.core.thing.binding.BaseDynamicStateDescriptionProvider;
+import org.openhab.core.thing.events.ThingEventFactory;
 import org.openhab.core.thing.i18n.ChannelTypeI18nLocalizationService;
 import org.openhab.core.thing.link.ItemChannelLinkRegistry;
 import org.openhab.core.thing.type.DynamicStateDescriptionProvider;
+import org.openhab.core.types.StateDescription;
+import org.openhab.core.types.StateDescriptionFragment;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * The {@link Clip2StateDescriptionProvider} provides dynamic state descriptions of scene channels whose list of options
- * is determined at runtime.
+ * The {@link Clip2StateDescriptionProvider} provides dynamic state descriptions of alert, effect, scene, and colour
+ * temperature channels whose capabilities are dynamically determined at runtime.
  *
  * @author Andrew Fiddian-Green - Initial contribution
  *
@@ -33,6 +44,8 @@ import org.osgi.service.component.annotations.Reference;
 @Component(service = { DynamicStateDescriptionProvider.class, Clip2StateDescriptionProvider.class })
 public class Clip2StateDescriptionProvider extends BaseDynamicStateDescriptionProvider {
 
+    private final Map<ChannelUID, StateDescriptionFragment> stateDescriptionFragments = new ConcurrentHashMap<>();
+
     @Activate
     public Clip2StateDescriptionProvider(final @Reference EventPublisher eventPublisher,
             final @Reference ItemChannelLinkRegistry itemChannelLinkRegistry,
@@ -40,5 +53,24 @@ public class Clip2StateDescriptionProvider extends BaseDynamicStateDescriptionPr
         this.eventPublisher = eventPublisher;
         this.itemChannelLinkRegistry = itemChannelLinkRegistry;
         this.channelTypeI18nLocalizationService = channelTypeI18nLocalizationService;
+    }
+
+    @Override
+    public @Nullable StateDescription getStateDescription(Channel channel,
+            @Nullable StateDescription originalStateDescription, @Nullable Locale locale) {
+        StateDescriptionFragment stateDescriptionFragment = stateDescriptionFragments.get(channel.getUID());
+        return stateDescriptionFragment != null ? stateDescriptionFragment.toStateDescription()
+                : super.getStateDescription(channel, originalStateDescription, locale);
+    }
+
+    public void setStateDescriptionFragment(ChannelUID channelUID, StateDescriptionFragment stateDescriptionFragment) {
+        StateDescriptionFragment oldStateDescriptionFragment = stateDescriptionFragments.get(channelUID);
+        if (!stateDescriptionFragment.equals(oldStateDescriptionFragment)) {
+            stateDescriptionFragments.put(channelUID, stateDescriptionFragment);
+            ItemChannelLinkRegistry itemChannelLinkRegistry = this.itemChannelLinkRegistry;
+            postEvent(ThingEventFactory.createChannelDescriptionChangedEvent(channelUID,
+                    itemChannelLinkRegistry != null ? itemChannelLinkRegistry.getLinkedItemNames(channelUID) : Set.of(),
+                    stateDescriptionFragment, oldStateDescriptionFragment));
+        }
     }
 }
--- bundles/org.openhab.binding.hue/src/main/java/org/openhab/binding/hue/internal/handler/Clip2ThingHandler.java
+++ bundles/org.openhab.binding.hue/src/main/java/org/openhab/binding/hue/internal/handler/Clip2ThingHandler.java
@@ -38,6 +38,7 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.binding.hue.internal.action.DynamicsActions;
 import org.openhab.binding.hue.internal.api.dto.clip2.Alerts;
+import org.openhab.binding.hue.internal.api.dto.clip2.ColorTemperature;
 import org.openhab.binding.hue.internal.api.dto.clip2.ColorXy;
 import org.openhab.binding.hue.internal.api.dto.clip2.Dimming;
 import org.openhab.binding.hue.internal.api.dto.clip2.Effects;
@@ -91,6 +92,8 @@ import org.openhab.core.thing.type.ChannelTypeUID;
 import org.openhab.core.types.Command;
 import org.openhab.core.types.RefreshType;
 import org.openhab.core.types.State;
+import org.openhab.core.types.StateDescriptionFragment;
+import org.openhab.core.types.StateDescriptionFragmentBuilder;
 import org.openhab.core.types.StateOption;
 import org.openhab.core.types.UnDefType;
 import org.slf4j.Logger;
@@ -945,6 +948,7 @@ public class Clip2ThingHandler extends BaseThingHandler {
             case LIGHT:
                 if (fullUpdate) {
                     updateEffectChannel(resource);
+                    updateColorTempAbsChannel(resource);
                 }
                 updateState(CHANNEL_2_COLOR_TEMP_PERCENT, resource.getColorTemperaturePercentState(), fullUpdate);
                 updateState(CHANNEL_2_COLOR_TEMP_ABSOLUTE, resource.getColorTemperatureAbsoluteState(), fullUpdate);
@@ -1112,6 +1116,32 @@ public class Clip2ThingHandler extends BaseThingHandler {
             stateDescriptionProvider.setStateOptions(new ChannelUID(thing.getUID(), CHANNEL_2_EFFECT), stateOptions);
             logger.debug("{} -> updateEffects() found {} effects", resourceId, stateOptions.size());
         }
+    }
+
+    /**
+     * Process the incoming Resource to initialize the colour temperature absolute channel's state description based on
+     * the minimum and maximum values supported by the lamp's Mirek schema.
+     *
+     * @param resource a Resource possibly containing a color temperature element and respective Mirek schema element.
+     */
+    private void updateColorTempAbsChannel(Resource resource) {
+        ColorTemperature colorTemperature = resource.getColorTemperature();
+        if (colorTemperature == null) {
+            return;
+        }
+        MirekSchema mirekSchema = colorTemperature.getMirekSchema();
+        if (mirekSchema == null) {
+            return;
+        }
+        StateDescriptionFragment stateDescriptionFragment = StateDescriptionFragmentBuilder.create()
+                .withMinimum(BigDecimal.valueOf(1000000 / mirekSchema.getMirekMaximum())) //
+                .withMaximum(BigDecimal.valueOf(1000000 / mirekSchema.getMirekMinimum())) //
+                .withPattern("%.0f K") //
+                .withReadOnly(false) //
+                .build();
+        stateDescriptionProvider.setStateDescriptionFragment(
+                new ChannelUID(thing.getUID(), CHANNEL_2_COLOR_TEMP_ABSOLUTE), stateDescriptionFragment);
+        logger.debug("{} -> updateColorTempAbsChannel() {} ", resource.getId(), stateDescriptionFragment);
     }
 
     /**
