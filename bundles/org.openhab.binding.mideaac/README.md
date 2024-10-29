8ff9cd3e7668964a611722453d411ffe30880c97
Bob Eckhoff <katmandodo@yahoo.com>
2024-10-29 17:02:02 -0400

    Midea AC after partial PR review
    Mideaac binding after partial PR review.  Main remaining issue is the connection manager which currently needs to be embedded in the MideaACHandler to leverage the OH base thing handler.
    Signed-off-by: Bob Eckhoff <katmandodo@yahoo.com>

---
+++ bundles/org.openhab.binding.mideaac/README.md
@@ -0,0 +1,121 @@
+# Midea AC Binding
+
+This binding integrates Air Conditioners that use the Midea protocol. Midea is an OEM for many brands.
+
+An AC device is likely supported if it uses one of the following Android apps or it's iOS equivalent.
+
+| Application                                  | Comment                               | Options      |
+|--:-------------------------------------------|--:------------------------------------|--------------|
+| Midea Air (com.midea.aircondition.obm)       | Full Support of key and token updates | Midea Air    |
+| NetHome Plus (com.midea.aircondition)        | Full Support of key and token updates | NetHome Plus |
+| SmartHome/MSmartHome (com.midea.ai.overseas) | Full Support of key and token updates | MSmartHome   |
+
+Note: The Air Conditioner must already be set-up on the WiFi network and have a fixed IP Address with one of the three apps listed above for full discovery and key and token updates.
+
+## Supported Things
+
+This binding supports one Thing type `ac`.
+
+## Discovery
+
+Once the Air Conditioner is on the network (WiFi active) the other required parameters can be discovered automatically.
+An IP broadcast message is sent and every responding unit gets added to the Inbox.
+As an alternative use the python application msmart-ng from <https://github.com/mill1000/midea-msmart> with the msmart-ng discover ipAddress option.
+
+## Binding Configuration
+
+No binding configuration is required.
+
+## Thing Configuration
+
+| Parameter   | Required ?  | Comment                                                           | Default |
+|--:----------|--:----------|--:----------------------------------------------------------------|---------|
+| ipAddress   | Yes         | IP Address of the device.                                         |         |
+| ipPort      | Yes         | IP port of the device                                             | 6444    |
+| deviceId    | Yes         | ID of the device. Leave 0 to do ID discovery (length 6 bytes).    | 0       |
+| cloud       | Yes for V.3 | Cloud Provider name for email and password                        |         |
+| email       | No          | Email for cloud account chosen in Cloud Provider.                 |         |
+| password    | No          | Password for cloud account chosen in Cloud Provider.              |         |
+| token       | Yes for V.3 | Secret Token (length 128 HEX)                                     |         |
+| key         | Yes for V.3 | Secret Key (length 64 HEX)                                        |         |
+| pollingTime | Yes         | Polling time in seconds. Minimum time is 30 seconds.              | 60      |
+| timeout     | Yes         | Connecting timeout. Minimum time is 2 second, maximum 10 seconds. | 4       |
+| promptTone  | Yes         | "Ding" tone when command is received and executed.                | False   |
+| version     | Yes         | Version 3 has token, key and cloud requirements.                  | 3       |
+
+## Channels
+
+Following channels are available:
+
+| Channel                      | Type               | Description                                                                                            | Read only | Advanced |
+|--:---------------------------|--:-----------------|--:-----------------------------------------------------------------------------------------------------|--:--------|--:-------|
+| power                        | Switch             | Turn the AC on and off.                                                                                |           |          |
+| target-temperature           | Number:Temperature | Target temperature.                                                                                    |           |          |
+| operational-mode             | String             | Operational mode: OFF (turns off), AUTO, COOL, DRY, HEAT, FAN ONLY                                     |           |          |
+| fan-speed                    | String             | Fan speed: OFF (turns off), SILENT, LOW, MEDIUM, HIGH, AUTO. Not all modes supported by all units.     |           |          |
+| swing-mode                   | String             | Swing mode: OFF, VERTICAL, HORIZONTAL, BOTH. Not all modes supported by all units.                     |           |          |
+| eco-mode                     | Switch             | Eco mode - Cool only (Temperature is set to 24 C (75 F) and fan on AUTO)                               |           |          |
+| turbo-mode                   | Switch             | Turbo mode, "Boost" in Midea Air app, long press "+" on IR Remote Controller. COOL and HEAT mode only. |           |          |
+| sleep-function               | Switch             | Sleep function ("Moon with a star" icon on IR Remote Controller).                                      |           |          |
+| indoor-temperature           | Number:Temperature | Indoor temperature measured in the room, where internal unit is installed.                             | Yes       |          |
+| outdoor-temperature          | Number:Temperature | Outdoor temperature by external unit. Some units do not report reading when off.                       | Yes       |          |
+| temperature-unit             | Switch             | Sets the LED display on the evaporator to Fahrenheit (true) or Celsius (false).                        |           | Yes      |
+| on-timer                     | String             | Sets the future time to turn on the AC.                                                                |           | Yes      |
+| off-timer                    | String             | Sets the future time to turn off the AC.                                                               |           | Yes      |
+| screen-display               | Switch             | If device supports across LAN, turns off the LED display.                                              |           | Yes      |
+| humidity                     | Number             | If device supports, the indoor humidity.                                                               | Yes       | Yes      |
+| dropped-commands             | Number             | Quality of WiFi connections - For debugging only.                                                      | Yes       | Yes      |
+| appliance-error              | Switch             | If device supports, appliance error                                                                    | Yes       | Yes      |
+| auxiliary-heat               | Switch             | If device supports, auxiliary heat                                                                     | Yes       | Yes      |
+| alternate-target-temperature | Number:Temperature | Alternate Target Temperature - not currently used                                                      | Yes       | Yes      |
+
+## Examples
+
+### `demo.things` Example
+
+```java
+Thing mideaac:ac:mideaac "myAC" @ "Room" [ ipAddress="192.168.1.200", ipPort="6444", deviceId="deviceId", cloud="your cloud (e.g NetHome Plus)", email="yourclouduser@email.com", password="yourcloudpassword", token="token", key ="key", pollingTime = 60, timeout=4, promptTone="false", version="3"] 
+```
+
+Option to use the built-in binding discovery of ipPort, deviceId, token and key.
+
+```java
+Thing mideaac:ac:mideaac "myAC" @ "Room" [ ipAddress="192.168.1.200", ipPort="", deviceId="", cloud="your cloud (e.g NetHome Plus)", email="yourclouduser@email.com", password="yourcloudpassword", token="", key ="", pollingTime = 60, timeout=4, promptTone="false", version="3"] 
+```
+
+### `demo.items` Example
+
+```java
+Switch power "Power"                                                        { channel="mideaac:ac:mideaac:power" }
+Number:Temperature target_temperature "Target Temperature [%.1f °F]"        { channel="mideaac:ac:mideaac:target-temperature" }
+String operational_mode "Operational Mode"                                  { channel="mideaac:ac:mideaac:operational-mode" }
+String fan_speed "Fan Speed"                                                { channel="mideaac:ac:mideaac:fan-speed" }
+String swing_mode "Swing Mode"                                              { channel="mideaac:ac:mideaac:swing-mode" }
+Number:Temperature indoor_temperature "Indoor Temperature [%.1f °F]"        { channel="mideaac:ac:mideaac:indoor-temperature" }
+Number:Temperature outdoor_temperature "Current Temperature [%.1f °F]"      { channel="mideaac:ac:mideaac:outdoor-temperature" }
+Switch eco_mode "Eco Mode"                                                  { channel="mideaac:ac:mideaac:eco-mode" }
+Switch turbo_mode "Turbo Mode"                                              { channel="mideaac:ac:mideaac:turbo-mode" }
+Switch sleep_function "Sleep function"                                      { channel="mideaac:ac:mideaac:sleep-function" }
+Switch temperature_unit "Fahrenheit or Celsius"                             { channel="mideaac:ac:mideaac:temperature-unit" }
+```
+
+### `demo.sitemap` Example
+
+```java
+sitemap midea label="Split AC MBR"{
+    Frame label="AC Unit" {
+    Text item=outdoor_temperature label="Outdoor Temperature [%.1f °F]"
+    Text item=indoor_temperature label="Indoor Temperature [%.1f °F]"
+    Setpoint item=target_temperature label="Target Temperature [%.1f °F]" minValue=63.0 maxValue=78 step=1.0
+    Switch item=power label="Midea AC Power"
+    Switch item=temperature_unit label= "Temp Unit" mappings=[ON="Fahrenheit", OFF="Celsius"]
+    Selection item=fan_speed label="Midea AC Fan Speed"
+    Selection item=operational_mode label="Midea AC Mode"
+    Selection item=swing_mode label="Midea AC Louver Swing Mode"
+}
+}
+```
+
+## Debugging and Tracing
+
+Switch the log level to TRACE or DEBUG on the UI Settings Page (Add-on Settings)
---
+++ bundles/org.openhab.binding.mideaac/src/main/java/org/openhab/binding/mideaac/internal/handler/Response.java
@@ -0,0 +1,389 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.mideaac.internal.handler;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.binding.mideaac.internal.handler.CommandBase.FanSpeed;
+import org.openhab.binding.mideaac.internal.handler.CommandBase.OperationalMode;
+import org.openhab.binding.mideaac.internal.handler.CommandBase.SwingMode;
+import org.openhab.binding.mideaac.internal.handler.Timer.TimerData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link Response} performs the byte data stream decoding
+ * The original reference is
+ * https://github.com/georgezhao2010/midea_ac_lan/blob/06fc4b582a012bbbfd6bd5942c92034270eca0eb/custom_components/midea_ac_lan/midea/devices/ac/message.py#L418
+ *
+ * @author Jacek Dobrowolski - Initial contribution
+ * @author Bob Eckhoff - Add Java Docs, minor fixes
+ */
+@NonNullByDefault
+public class Response {
+    byte[] data;
+
+    // set empty to match the return from an empty byte avoid null
+    float empty = (float) -19.0;
+    private Logger logger = LoggerFactory.getLogger(Response.class);
+
+    private final int version;
+    String responseType;
+    byte bodyType;
+
+    private int getVersion() {
+        return version;
+    }
+
+    /**
+     * Response class Parameters
+     * 
+     * @param data byte array from device
+     * @param version version of the device
+     * @param responseType response type
+     * @param bodyType Body type
+     */
+    public Response(byte[] data, int version, String responseType, byte bodyType) {
+        this.data = data;
+        this.version = version;
+        this.bodyType = bodyType;
+        this.responseType = responseType;
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Power State: {}", getPowerState());
+            logger.debug("Target Temperature: {}", getTargetTemperature());
+            logger.debug("Operational Mode: {}", getOperationalMode());
+            logger.debug("Fan Speed: {}", getFanSpeed());
+            logger.debug("On Timer: {}", getOnTimer());
+            logger.debug("Off Timer: {}", getOffTimer());
+            logger.debug("Swing Mode: {}", getSwingMode());
+            logger.debug("Sleep Function: {}", getSleepFunction());
+            logger.debug("Turbo Mode: {}", getTurboMode());
+            logger.debug("Indoor Temperature: {}", getIndoorTemperature());
+            logger.debug("Outdoor Temperature: {}", getOutdoorTemperature());
+            logger.debug("LED Display: {}", getDisplayOn());
+        }
+
+        if (logger.isTraceEnabled()) {
+            logger.trace("Prompt Tone: {}", getPromptTone());
+            logger.trace("Appliance Error: {}", getApplianceError());
+            logger.trace("Auxiliary Heat: {}", getAuxHeat());
+            logger.trace("Eco Mode: {}", getEcoMode());
+            logger.trace("Fahrenheit: {}", getFahrenheit());
+            logger.trace("Humidity: {}", getHumidity());
+            logger.trace("Alternate Target Temperature {}", getAlternateTargetTemperature());
+        }
+
+        /**
+         * Trace Log Response and Body Type for V3. V2 set at "" and 0x00
+         * This was for future development since only 0xC0 is currently used
+         */
+        if (version == 3) {
+            logger.trace("Response and Body Type: {}, {}", responseType, bodyType);
+            if ("notify2".equals(responseType) && bodyType == -95) { // 0xA0 = -95
+                logger.trace("Response Handler: XA0Message");
+            } else if ("notify1".equals(responseType) && bodyType == -91) { // 0xA1 = -91
+                logger.trace("Response Handler: XA1Message");
+            } else if (("notify2".equals(responseType) || "set".equals(responseType) || "query".equals(responseType))
+                    && (bodyType == 0xB0 || bodyType == 0xB1 || bodyType == 0xB5)) {
+                logger.trace("Response Handler: XBXMessage");
+            } else if (("set".equals(responseType) || "query".equals(responseType)) && bodyType == -64) { // 0xC0 = -64
+                logger.trace("Response Handler: XCOMessage");
+            } else if ("query".equals(responseType) && bodyType == 0xC1) {
+                logger.trace("Response Handler: XC1Message");
+            } else {
+                logger.trace("Response Handler: _general_");
+            }
+        }
+    }
+
+    /**
+     * Device On or Off
+     * 
+     * @return power state true or false
+     */
+    public boolean getPowerState() {
+        return (data[0x01] & 0x1) > 0;
+    }
+
+    /**
+     * Read only
+     * 
+     * @return prompt tone true or false
+     */
+    public boolean getPromptTone() {
+        return (data[0x01] & 0x40) > 0;
+    }
+
+    /**
+     * Read only
+     * 
+     * @return appliance error true or false
+     */
+    public boolean getApplianceError() {
+        return (data[0x01] & 0x80) > 0;
+    }
+
+    /**
+     * Setpoint for Heat Pump
+     * 
+     * @return current setpoint in degrees C
+     */
+    public float getTargetTemperature() {
+        return (data[0x02] & 0xf) + 16.0f + (((data[0x02] & 0x10) > 0) ? 0.5f : 0.0f);
+    }
+
+    /**
+     * Cool, Heat, Fan Only, etc. See Command Base class
+     * 
+     * @return Cool, Heat, Fan Only, etc.
+     */
+    public OperationalMode getOperationalMode() {
+        return OperationalMode.fromId((data[0x02] & 0xe0) >> 5);
+    }
+
+    /**
+     * Low, Medium, High, Auto etc. See Command Base class
+     * 
+     * @return Low, Medium, High, Auto etc.
+     */
+    public FanSpeed getFanSpeed() {
+        return FanSpeed.fromId(data[0x03] & 0x7f, getVersion());
+    }
+
+    /**
+     * Creates String representation of the On timer to the channel
+     * 
+     * @return String of HH:MM
+     */
+    public Timer getOnTimer() {
+        return new Timer((data[0x04] & 0x80) > 0, ((data[0x04] & (byte) 0x7c) >> 2),
+                ((data[0x04] & 0x3) * 15 + 15 - (((data[0x06] & (byte) 0xf0) >> 4) & 0x0f)));
+    }
+
+    /**
+     * This is used to carry the current On Timer (last response) through
+     * subsequent Set commands, so it is not overwritten.
+     * 
+     * @return status plus String of HH:MM
+     */
+    public TimerData getOnTimerData() {
+        int hours = 0;
+        int minutes = 0;
+        Timer timer = new Timer(true, hours, minutes);
+        boolean status = (data[0x04] & 0x80) > 0;
+        hours = ((data[0x04] & (byte) 0x7c) >> 2);
+        minutes = ((data[0x04] & 0x3) * 15 + 15 - (((data[0x06] & (byte) 0xf0) >> 4) & 0x0f));
+        return timer.new TimerData(status, hours, minutes);
+    }
+
+    /**
+     * Creates String representation of the Off timer to the channel
+     * 
+     * @return String of HH:MM
+     */
+    public Timer getOffTimer() {
+        return new Timer((data[0x05] & 0x80) > 0, ((data[0x05] & (byte) 0x7c) >> 2),
+                ((data[0x05] & 0x3) * 15 + 15 - (data[0x06] & (byte) 0xf)));
+    }
+
+    /**
+     * This is used to carry the Off timer (last response) through
+     * subsequent Set commands, so it is not overwritten.
+     * 
+     * @return status plus String of HH:MM
+     */
+    public TimerData getOffTimerData() {
+        int hours = 0;
+        int minutes = 0;
+        Timer timer = new Timer(true, hours, minutes);
+        boolean status = (data[0x05] & 0x80) > 0;
+        hours = ((data[0x05] & (byte) 0x7c) >> 2);
+        minutes = (data[0x05] & 0x3) * 15 + 15 - (((data[0x06] & (byte) 0xf) & 0x0f));
+        return timer.new TimerData(status, hours, minutes);
+    }
+
+    /**
+     * Status of the vertical and/or horzontal louver
+     * 
+     * @return Vertical, Horizontal, Off, Both
+     */
+    public SwingMode getSwingMode() {
+        return SwingMode.fromId(data[0x07] & 0x3f, getVersion());
+    }
+
+    /**
+     * Read only - heat mode only
+     * 
+     * @return auxiliary heat active
+     */
+    public boolean getAuxHeat() {
+        return (data[0x09] & (byte) 0x08) != 0;
+    }
+
+    /**
+     * Ecomode status - Fan to Auto and temp to 24 C
+     * 
+     * @return Eco mode on (true) or (false)
+     */
+    public boolean getEcoMode() {
+        return (data[0x09] & (byte) 0x10) != 0;
+    }
+
+    /**
+     * Sleep function status. Setpoint Temp increases in first
+     * two hours of sleep by 1 degree in Cool mode
+     * 
+     * @return Sleep mode on (true) or (false)
+     */
+    public boolean getSleepFunction() {
+        return (data[0x0a] & (byte) 0x01) != 0;
+    }
+
+    /**
+     * Turbo mode status for maximum cooling or heat
+     * 
+     * @return Turbo mode on (true) or (false)
+     */
+    public boolean getTurboMode() {
+        return (data[0x0a] & (byte) 0x02) != 0;
+    }
+
+    /**
+     * If true display on indoor unit is degrees F, else C
+     * 
+     * @return Fahrenheit on (true) or Celsius
+     */
+    public boolean getFahrenheit() {
+        return (data[0x0a] & (byte) 0x04) != 0;
+    }
+
+    /**
+     * There is some variation in how this is handled by different
+     * AC models. This covers at least 2 versions found.
+     * 
+     * @return Indoor temperature
+     */
+    public Float getIndoorTemperature() {
+        double indoorTempInteger;
+        double indoorTempDecimal;
+
+        if (data[0] == (byte) 0xc0) {
+            if (((Byte.toUnsignedInt(data[11]) - 50) / 2.0) < -19) {
+                return (float) -19;
+            }
+            if (((Byte.toUnsignedInt(data[11]) - 50) / 2.0) > 50) {
+                return (float) 50;
+            } else {
+                indoorTempInteger = (float) ((Byte.toUnsignedInt(data[11]) - 50f) / 2.0f);
+            }
+
+            indoorTempDecimal = (float) ((data[15] & 0x0F) * 0.1f);
+
+            if (Byte.toUnsignedInt(data[11]) > 49) {
+                return (float) (indoorTempInteger + indoorTempDecimal);
+            } else {
+                return (float) (indoorTempInteger - indoorTempDecimal);
+            }
+        }
+
+        /**
+         * Not observed or tested, but left in from original author
+         * This was for future development since only 0xC0 is currently used
+         */
+        if (data[0] == (byte) 0xa0 || data[0] == (byte) 0xa1) {
+            if (data[0] == (byte) 0xa0) {
+                if ((data[1] >> 2) - 4 == 0) {
+                    indoorTempInteger = -1;
+                } else {
+                    indoorTempInteger = (data[1] >> 2) + 12;
+                }
+
+                if (((data[1] >> 1) & 0x01) == 1) {
+                    indoorTempDecimal = 0.5f;
+                } else {
+                    indoorTempDecimal = 0;
+                }
+            }
+            if (data[0] == (byte) 0xa1) {
+                if (((Byte.toUnsignedInt(data[13]) - 50) / 2.0f) < -19) {
+                    return (float) -19;
+                }
+                if (((Byte.toUnsignedInt(data[13]) - 50) / 2.0f) > 50) {
+                    return (float) 50;
+                } else {
+                    indoorTempInteger = (float) (Byte.toUnsignedInt(data[13]) - 50f) / 2.0f;
+                }
+                indoorTempDecimal = (data[18] & 0x0f) * 0.1f;
+
+                if (Byte.toUnsignedInt(data[13]) > 49) {
+                    return (float) (indoorTempInteger + indoorTempDecimal);
+                } else {
+                    return (float) (indoorTempInteger - indoorTempDecimal);
+                }
+            }
+        }
+        return empty;
+    }
+
+    /**
+     * There is some variation in how this is handled by different
+     * AC models. This covers at least 2 versions. Some models
+     * do not report outside temp when the AC is off. Returns 0.0 in that case.
+     * 
+     * @return Outdoor temperature
+     */
+    public Float getOutdoorTemperature() {
+        if (data[12] != (byte) 0xff) {
+            double tempInteger = (float) (Byte.toUnsignedInt(data[12]) - 50f) / 2.0f;
+            double tempDecimal = ((data[15] & 0xf0) >> 4) * 0.1f;
+            if (Byte.toUnsignedInt(data[12]) > 49) {
+                return (float) (tempInteger + tempDecimal);
+            } else {
+                return (float) (tempInteger - tempDecimal);
+            }
+        }
+        return 0.0f;
+    }
+
+    /**
+     * Returns the Alternative Target Temperature (not used)
+     * 
+     * @return Alternate target Temperature
+     */
+    public Float getAlternateTargetTemperature() {
+        if ((data[13] & 0x1f) != 0) {
+            return (data[13] & 0x1f) + 12.0f + (((data[0x02] & 0x10) > 0) ? 0.5f : 0.0f);
+        } else {
+            return 0.0f;
+        }
+    }
+
+    /**
+     * Returns status of Device LEDs
+     * 
+     * @return LEDs on (true) or (false)
+     */
+    public boolean getDisplayOn() {
+        return (data[14] & (byte) 0x70) != (byte) 0x70;
+    }
+
+    /**
+     * Not observed with units being tested
+     * From reference Document
+     * 
+     * @return humidity
+     */
+    public int getHumidity() {
+        return (data[19] & (byte) 0x7f);
+    }
+}
