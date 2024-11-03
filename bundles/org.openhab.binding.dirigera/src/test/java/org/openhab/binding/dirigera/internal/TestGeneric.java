3d4b1719b3d888d193e2682a639ecc484defe886
Bernd Weymann <bernd.weymann@gmail.com>
2024-11-03 21:15:38 +0100

    add tests
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

---
+++ bundles/org.openhab.binding.dirigera/src/test/java/org/openhab/binding/dirigera/internal/TestGeneric.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.dirigera.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.json.JSONObject;
+
+/**
+ * {@link TestGeneric} some basic tests
+ *
+ * @author Bernd Weymann - Initial Contribution
+ */
+@NonNullByDefault
+class TestGeneric {
+
+    void testJsonChannel() {
+        String deviceInspelning = FileReader.readFileInString("src/test/resources/devices/inspelning.json");
+        JSONObject inspelningObject = new JSONObject(deviceInspelning);
+        final JSONObject newObject = new JSONObject();
+        inspelningObject.keySet().forEach(key -> {
+            newObject.put(key, inspelningObject.getString(key));
+        });
+    }
+}
---
+++ bundles/org.openhab.binding.dirigera/src/test/java/org/openhab/binding/dirigera/internal/handler/lights/TestTemperatureLight.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.dirigera.internal.handler.lights;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.openhab.binding.dirigera.internal.Constants.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.junit.jupiter.api.Test;
+import org.openhab.binding.dirigera.internal.handler.DirigeraBridgeProvider;
+import org.openhab.binding.dirigera.internal.handler.light.TemperatureLightHandler;
+import org.openhab.binding.dirigera.internal.mock.CallbackMock;
+import org.openhab.core.library.types.DecimalType;
+import org.openhab.core.library.types.OnOffType;
+import org.openhab.core.library.types.PercentType;
+import org.openhab.core.library.types.QuantityType;
+import org.openhab.core.library.unit.Units;
+import org.openhab.core.thing.Bridge;
+import org.openhab.core.thing.ChannelUID;
+import org.openhab.core.thing.internal.ThingImpl;
+import org.openhab.core.types.RefreshType;
+import org.openhab.core.types.State;
+
+/**
+ * {@link TestTemperatureLight} Tests device handler creation, initializing and refresh of channels
+ *
+ * @author Bernd Weymann - Initial Contribution
+ */
+@NonNullByDefault
+class TestTemperatureLight {
+    @Test
+    void testTemperatureLightDevice() {
+        Bridge hubBridge = DirigeraBridgeProvider.prepareSimuBridge();
+        ThingImpl thing = new ThingImpl(THING_TYPE_TEMPERATURE_LIGHT, "test-device");
+        thing.setBridgeUID(hubBridge.getBridgeUID());
+        TemperatureLightHandler handler = new TemperatureLightHandler(thing, TEMPERATURE_LIGHT_MAP);
+        CallbackMock callback = new CallbackMock();
+        callback.setBridge(hubBridge);
+        handler.setCallback(callback);
+
+        // set the right id
+        Map<String, Object> config = new HashMap<>();
+        config.put("id", "a1e1eacc-2dcf-45bd-9f93-62a436b6a7ed_1");
+        handler.handleConfigurationUpdate(config);
+
+        handler.initialize();
+        callback.waitForOnline();
+        checkLightStates(callback);
+
+        callback.clear();
+        handler.handleCommand(new ChannelUID(thing.getUID(), CHANNEL_OTA_STATUS), RefreshType.REFRESH);
+        handler.handleCommand(new ChannelUID(thing.getUID(), CHANNEL_OTA_STATE), RefreshType.REFRESH);
+        handler.handleCommand(new ChannelUID(thing.getUID(), CHANNEL_OTA_PROGRESS), RefreshType.REFRESH);
+        handler.handleCommand(new ChannelUID(thing.getUID(), CHANNEL_POWER_STATE), RefreshType.REFRESH);
+        handler.handleCommand(new ChannelUID(thing.getUID(), CHANNEL_LIGHT_BRIGHTNESS), RefreshType.REFRESH);
+        handler.handleCommand(new ChannelUID(thing.getUID(), CHANNEL_LIGHT_TEMPERATURE), RefreshType.REFRESH);
+        checkLightStates(callback);
+    }
+
+    void checkLightStates(CallbackMock callback) {
+        State onOffState = callback.getState("dirigera:temperature-light:test-device:power-state");
+        assertNotNull(onOffState);
+        assertTrue(onOffState instanceof OnOffType);
+        assertTrue(OnOffType.ON.equals((onOffState)), "On");
+
+        State temperatureState = callback.getState("dirigera:temperature-light:test-device:temperature");
+        assertNotNull(temperatureState);
+        assertTrue(temperatureState instanceof PercentType);
+        assertEquals(0, ((PercentType) temperatureState).intValue(), "Temperature");
+
+        State brightnessState = callback.getState("dirigera:temperature-light:test-device:brightness");
+        assertNotNull(brightnessState);
+        assertTrue(brightnessState instanceof PercentType);
+        assertEquals(49, ((PercentType) brightnessState).intValue(), "Brightness");
+
+        // test ota
+        State otaStatus = callback.getState("dirigera:temperature-light:test-device:ota-status");
+        assertNotNull(otaStatus);
+        assertTrue(otaStatus instanceof DecimalType);
+        assertEquals(0, ((DecimalType) otaStatus).intValue(), "OTA Status");
+        State otaState = callback.getState("dirigera:temperature-light:test-device:ota-state");
+        assertNotNull(otaState);
+        assertTrue(otaState instanceof DecimalType);
+        assertEquals(0, ((DecimalType) otaState).intValue(), "OTA State");
+        State otaProgess = callback.getState("dirigera:temperature-light:test-device:ota-progress");
+        assertNotNull(otaProgess);
+        assertTrue(otaProgess instanceof QuantityType);
+        assertTrue(((QuantityType<?>) otaProgess).getUnit().equals(Units.PERCENT));
+        assertEquals(0, ((QuantityType<?>) otaProgess).intValue(), "OTA Progress");
+    }
+}
