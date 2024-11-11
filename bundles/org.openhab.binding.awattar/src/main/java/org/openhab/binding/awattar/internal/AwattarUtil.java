1ae354eb70ae117388fbcfe0de8510a182647e6b
Jacob Laursen <jacob-github@vindvejr.dk>
2024-11-11 20:28:16 +0100

    Simplify DateTimeType handling for aWATTar
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>

--- bundles/org.openhab.binding.awattar/src/main/java/org/openhab/binding/awattar/internal/AwattarUtil.java
+++ bundles/org.openhab.binding.awattar/src/main/java/org/openhab/binding/awattar/internal/AwattarUtil.java
@@ -21,7 +21,6 @@ import javax.measure.quantity.Time;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.openhab.core.i18n.TimeZoneProvider;
-import org.openhab.core.library.types.DateTimeType;
 import org.openhab.core.library.types.QuantityType;
 import org.openhab.core.library.unit.Units;
 
@@ -45,10 +44,6 @@ public class AwattarUtil {
 
     public static ZonedDateTime getCalendarForHour(int hour, ZoneId zone) {
         return ZonedDateTime.now(zone).truncatedTo(ChronoUnit.DAYS).plusHours(hour);
-    }
-
-    public static DateTimeType getDateTimeType(long time, TimeZoneProvider tz) {
-        return new DateTimeType(ZonedDateTime.ofInstant(Instant.ofEpochMilli(time), tz.getTimeZone()));
     }
 
     public static QuantityType<Time> getDuration(long millis) {
