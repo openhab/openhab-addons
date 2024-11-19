4107800dcef2e30940861341f4772eb8407b38ad
Jacob Laursen <jacob-github@vindvejr.dk>
2024-11-19 20:27:10 +0100

    Simplify DateTimeType handling for MongoDB
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>

--- bundles/org.openhab.persistence.mongodb/src/test/java/org/openhab/persistence/mongodb/internal/MongoDBPersistenceServiceTest.java
+++ bundles/org.openhab.persistence.mongodb/src/test/java/org/openhab/persistence/mongodb/internal/MongoDBPersistenceServiceTest.java
@@ -59,7 +59,7 @@ import ch.qos.logback.core.read.ListAppender;
 import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 
 /**
- * This is the implementation of the test for MongoDB {@link PersistenceService}.
+ * This is the implementation of the test for MongoDB {@link org.openhab.core.persistence.PersistenceService}.
  *
  * @author René Ulbricht - Initial contribution
  */
@@ -699,8 +699,9 @@ public class MongoDBPersistenceServiceTest {
 
             if (item instanceof DateTimeItem) {
                 // verify just the date part
-                assertEquals(((DateTimeType) item.getState()).getZonedDateTime().toLocalDate(),
-                        ((DateTimeType) result.iterator().next().getState()).getZonedDateTime().toLocalDate());
+                assertEquals(((DateTimeType) item.getState()).getZonedDateTime(ZoneId.systemDefault()).toLocalDate(),
+                        ((DateTimeType) result.iterator().next().getState()).getZonedDateTime(ZoneId.systemDefault())
+                                .toLocalDate());
             } else {
                 VerificationHelper.verifyQueryResult(result, item.getState());
             }
