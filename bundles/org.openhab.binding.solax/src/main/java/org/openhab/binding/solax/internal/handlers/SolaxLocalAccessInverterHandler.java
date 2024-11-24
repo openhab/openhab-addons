7f05db03908536143d3f2ccde854425bad02e67e
Jacob Laursen <jacob-github@vindvejr.dk>
2024-11-24 23:25:27 +0100

    Simplify DateTimeType handling for Solax
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>

--- bundles/org.openhab.binding.solax/src/main/java/org/openhab/binding/solax/internal/handlers/SolaxLocalAccessInverterHandler.java
+++ bundles/org.openhab.binding.solax/src/main/java/org/openhab/binding/solax/internal/handlers/SolaxLocalAccessInverterHandler.java
@@ -12,7 +12,6 @@
  */
 package org.openhab.binding.solax.internal.handlers;
 
-import java.time.ZonedDateTime;
 import java.util.Set;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -194,6 +193,6 @@ public class SolaxLocalAccessInverterHandler extends SolaxLocalAccessAbstractHan
                 Units.HERTZ, supportedChannels);
 
         // Binding provided data
-        updateState(SolaxBindingConstants.CHANNEL_TIMESTAMP, new DateTimeType(ZonedDateTime.now()));
+        updateState(SolaxBindingConstants.CHANNEL_TIMESTAMP, new DateTimeType());
     }
 }
