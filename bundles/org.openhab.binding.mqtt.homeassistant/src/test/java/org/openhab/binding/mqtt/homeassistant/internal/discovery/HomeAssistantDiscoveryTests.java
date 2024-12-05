8563b8a85896da413c12f06cca7cc7b0c4ba7131
Cody Cutrer <cody@cutrer.us>
2024-12-05 14:31:58 -0700

    [mqtt.homeassistant] Fix thing consistency for existing things when a device adds or removes components (#17851)
    * [mqtt.homeassistant] gracefully handle a component's discovery info being deleted
    
    Signed-off-by: Cody Cutrer <cody@cutrer.us>

--- bundles/org.openhab.binding.mqtt.homeassistant/src/test/java/org/openhab/binding/mqtt/homeassistant/internal/discovery/HomeAssistantDiscoveryTests.java
+++ bundles/org.openhab.binding.mqtt.homeassistant/src/test/java/org/openhab/binding/mqtt/homeassistant/internal/discovery/HomeAssistantDiscoveryTests.java
@@ -15,13 +15,13 @@ package org.openhab.binding.mqtt.homeassistant.internal.discovery;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
@@ -56,14 +56,6 @@ public class HomeAssistantDiscoveryTests extends AbstractHomeAssistantTests {
     }
 
     @Test
-    public void testComponentNameSummary() {
-        assertThat(
-                HomeAssistantDiscovery.getComponentNamesSummary(
-                        Stream.of("Sensor", "Switch", "Sensor", "Foobar", "Foobar", "Foobar")), //
-                is("3x Foobar, 2x Sensor, Switch"));
-    }
-
-    @Test
     public void testOneThingDiscovery() throws Exception {
         var discoveryListener = new LatchDiscoveryListener();
         var latch = discoveryListener.createWaitForThingsDiscoveredLatch(1);
@@ -88,9 +80,105 @@ public class HomeAssistantDiscoveryTests extends AbstractHomeAssistantTests {
         assertThat(result.getProperties().get(Thing.PROPERTY_VENDOR), is("TuYa"));
         assertThat(result.getProperties().get(Thing.PROPERTY_FIRMWARE_VERSION), is("Zigbee2MQTT 1.18.2"));
         assertThat(result.getProperties().get(HandlerConfiguration.PROPERTY_BASETOPIC), is("homeassistant"));
-        assertThat(result.getLabel(), is("th1 (Climate Control, Switch)"));
+        assertThat(result.getLabel(), is("th1"));
         assertThat((List<String>) result.getProperties().get(HandlerConfiguration.PROPERTY_TOPICS), hasItems(
                 "climate/0x847127fffe11dd6a_climate_zigbee2mqtt", "switch/0x847127fffe11dd6a_auto_lock_zigbee2mqtt"));
+    }
+
+    @Test
+    public void testComponentAddedToExistingThing() throws Exception {
+        var discoveryListener = new LatchDiscoveryListener();
+        var latch = discoveryListener.createWaitForThingsDiscoveredLatch(1);
+
+        // When discover one thing with two channels
+        discovery.addDiscoveryListener(discoveryListener);
+        discovery.receivedMessage(HA_UID, bridgeConnection,
+                "homeassistant/climate/0x847127fffe11dd6a_climate_zigbee2mqtt/config",
+                getResourceAsByteArray("component/configTS0601ClimateThermostat.json"));
+
+        // Then one thing found
+        assert latch.await(3, TimeUnit.SECONDS);
+        var discoveryResults = discoveryListener.getDiscoveryResults();
+        assertThat(discoveryResults.size(), is(1));
+        var result = discoveryResults.get(0);
+        assertThat(result.getBridgeUID(), is(HA_UID));
+        assertThat(result.getProperties().get(Thing.PROPERTY_MODEL_ID),
+                is("Radiator valve with thermostat (TS0601_thermostat)"));
+        assertThat(result.getProperties().get(Thing.PROPERTY_VENDOR), is("TuYa"));
+        assertThat(result.getProperties().get(Thing.PROPERTY_FIRMWARE_VERSION), is("Zigbee2MQTT 1.18.2"));
+        assertThat(result.getProperties().get(HandlerConfiguration.PROPERTY_BASETOPIC), is("homeassistant"));
+        assertThat(result.getLabel(), is("th1"));
+        assertThat((List<String>) result.getProperties().get(HandlerConfiguration.PROPERTY_TOPICS),
+                hasItems("climate/0x847127fffe11dd6a_climate_zigbee2mqtt"));
+
+        // Now another component added to the same thing
+        latch = discoveryListener.createWaitForThingsDiscoveredLatch(1);
+        discovery.receivedMessage(HA_UID, bridgeConnection,
+                "homeassistant/switch/0x847127fffe11dd6a_auto_lock_zigbee2mqtt/config",
+                getResourceAsByteArray("component/configTS0601AutoLock.json"));
+
+        assert latch.await(3, TimeUnit.SECONDS);
+        discoveryResults = discoveryListener.getDiscoveryResults();
+        assertThat(discoveryResults.size(), is(1));
+        result = discoveryResults.get(0);
+        assertThat(result.getBridgeUID(), is(HA_UID));
+        assertThat(result.getProperties().get(Thing.PROPERTY_MODEL_ID),
+                is("Radiator valve with thermostat (TS0601_thermostat)"));
+        assertThat(result.getProperties().get(Thing.PROPERTY_VENDOR), is("TuYa"));
+        assertThat(result.getProperties().get(Thing.PROPERTY_FIRMWARE_VERSION), is("Zigbee2MQTT 1.18.2"));
+        assertThat(result.getProperties().get(HandlerConfiguration.PROPERTY_BASETOPIC), is("homeassistant"));
+        assertThat(result.getLabel(), is("th1"));
+        assertThat((List<String>) result.getProperties().get(HandlerConfiguration.PROPERTY_TOPICS), hasItems(
+                "climate/0x847127fffe11dd6a_climate_zigbee2mqtt", "switch/0x847127fffe11dd6a_auto_lock_zigbee2mqtt"));
+    }
+
+    @Test
+    public void testComponentRemovedFromExistingThing() throws Exception {
+        var discoveryListener = new LatchDiscoveryListener();
+        var latch = discoveryListener.createWaitForThingsDiscoveredLatch(1);
+
+        // When discover one thing with two channels
+        discovery.addDiscoveryListener(discoveryListener);
+        discovery.receivedMessage(HA_UID, bridgeConnection,
+                "homeassistant/climate/0x847127fffe11dd6a_climate_zigbee2mqtt/config",
+                getResourceAsByteArray("component/configTS0601ClimateThermostat.json"));
+        discovery.receivedMessage(HA_UID, bridgeConnection,
+                "homeassistant/switch/0x847127fffe11dd6a_auto_lock_zigbee2mqtt/config",
+                getResourceAsByteArray("component/configTS0601AutoLock.json"));
+
+        // Then one thing found
+        assert latch.await(3, TimeUnit.SECONDS);
+        var discoveryResults = discoveryListener.getDiscoveryResults();
+        assertThat(discoveryResults.size(), is(1));
+        var result = discoveryResults.get(0);
+        assertThat(result.getBridgeUID(), is(HA_UID));
+        assertThat(result.getProperties().get(Thing.PROPERTY_MODEL_ID),
+                is("Radiator valve with thermostat (TS0601_thermostat)"));
+        assertThat(result.getProperties().get(Thing.PROPERTY_VENDOR), is("TuYa"));
+        assertThat(result.getProperties().get(Thing.PROPERTY_FIRMWARE_VERSION), is("Zigbee2MQTT 1.18.2"));
+        assertThat(result.getProperties().get(HandlerConfiguration.PROPERTY_BASETOPIC), is("homeassistant"));
+        assertThat(result.getLabel(), is("th1"));
+        assertThat((List<String>) result.getProperties().get(HandlerConfiguration.PROPERTY_TOPICS), hasItems(
+                "climate/0x847127fffe11dd6a_climate_zigbee2mqtt", "switch/0x847127fffe11dd6a_auto_lock_zigbee2mqtt"));
+
+        // Now remove the second component
+        latch = discoveryListener.createWaitForThingsDiscoveredLatch(1);
+        discovery.topicVanished(HA_UID, bridgeConnection,
+                "homeassistant/switch/0x847127fffe11dd6a_auto_lock_zigbee2mqtt/config");
+
+        assert latch.await(3, TimeUnit.SECONDS);
+        discoveryResults = discoveryListener.getDiscoveryResults();
+        assertThat(discoveryResults.size(), is(1));
+        result = discoveryResults.get(0);
+        assertThat(result.getBridgeUID(), is(HA_UID));
+        assertThat(result.getProperties().get(Thing.PROPERTY_MODEL_ID),
+                is("Radiator valve with thermostat (TS0601_thermostat)"));
+        assertThat(result.getProperties().get(Thing.PROPERTY_VENDOR), is("TuYa"));
+        assertThat(result.getProperties().get(Thing.PROPERTY_FIRMWARE_VERSION), is("Zigbee2MQTT 1.18.2"));
+        assertThat(result.getProperties().get(HandlerConfiguration.PROPERTY_BASETOPIC), is("homeassistant"));
+        assertThat(result.getLabel(), is("th1"));
+        assertThat((List<String>) result.getProperties().get(HandlerConfiguration.PROPERTY_TOPICS),
+                hasItems("climate/0x847127fffe11dd6a_climate_zigbee2mqtt"));
     }
 
     private static class TestHomeAssistantDiscovery extends HomeAssistantDiscovery {
@@ -122,8 +210,10 @@ public class HomeAssistantDiscoveryTests extends AbstractHomeAssistantTests {
             return Collections.emptyList();
         }
 
-        public CopyOnWriteArrayList<DiscoveryResult> getDiscoveryResults() {
-            return discoveryResults;
+        public List<DiscoveryResult> getDiscoveryResults() {
+            ArrayList<DiscoveryResult> localResults = new ArrayList<>(discoveryResults);
+            discoveryResults.clear();
+            return localResults;
         }
 
         public CountDownLatch createWaitForThingsDiscoveredLatch(int count) {
