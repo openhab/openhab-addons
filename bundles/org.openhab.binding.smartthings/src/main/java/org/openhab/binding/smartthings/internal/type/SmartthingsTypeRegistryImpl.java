6ba1fe89d78ce7c3309df67b43567fd952358811
Laurent ARNAL <laurent@clae.net>
2024-12-07 17:04:11 +0100

    fix sat errors
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/type/SmartthingsTypeRegistryImpl.java
+++ bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/type/SmartthingsTypeRegistryImpl.java
@@ -240,14 +240,14 @@ public class SmartthingsTypeRegistryImpl implements SmartthingsTypeRegistry {
 
     public void loadCapabilityDef(String path) {
         try {
-            logger.info(String.format("loading capa: %s", path));
+            logger.info("loading capa: {}", path);
             String template = readTemplate(path);
 
             SmartthingsJSonCapabilities resultObj = getGson().fromJson(template, SmartthingsJSonCapabilities.class);
             capabilitiesDict.put(resultObj.id, resultObj);
 
         } catch (Exception ex) {
-            logger.info(String.format("error loading capa: %s <> %s", path, ex));
+            logger.info("error loading capa: {} <> {}", path, ex);
             logger.info("");
         }
     }
