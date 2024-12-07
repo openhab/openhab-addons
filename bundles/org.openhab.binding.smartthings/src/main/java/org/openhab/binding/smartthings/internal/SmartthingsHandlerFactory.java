87eb87c7521cf5cccdddabc14d3ba627b13fb19c
Laurent ARNAL <laurent@clae.net>
2024-12-07 16:43:50 +0100

    try to fix compile error
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/SmartthingsHandlerFactory.java
+++ bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/SmartthingsHandlerFactory.java
@@ -21,6 +21,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import javax.validation.constraints.NotNull;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.jetty.client.HttpClient;
@@ -121,7 +123,9 @@ public class SmartthingsHandlerFactory extends BaseThingHandlerFactory
             bridgeHandler = new SmartthingsHubBridgeHandler((Bridge) thing, this, bundleContext, httpService,
                     oAuthFactory, httpClientFactory);
 
+            @NotNull
             SmartthingsAccountHandler accountHandler = bridgeHandler;
+
             authService.setSmartthingsAccountHandler(accountHandler);
 
             bridgeUID = thing.getUID();
@@ -140,7 +144,9 @@ public class SmartthingsHandlerFactory extends BaseThingHandlerFactory
             bridgeHandler = new SmartthingsCloudBridgeHandler((Bridge) thing, this, bundleContext, httpService,
                     oAuthFactory, httpClientFactory);
 
+            @NotNull
             SmartthingsAccountHandler accountHandler = bridgeHandler;
+
             authService.setSmartthingsAccountHandler(accountHandler);
 
             bridgeUID = thing.getUID();
