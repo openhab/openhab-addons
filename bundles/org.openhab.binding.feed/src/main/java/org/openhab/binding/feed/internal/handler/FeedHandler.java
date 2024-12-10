61a7ac0215dafcd83fe0bd33c5a7c9cbf3425b4e
Jacob Laursen <jacob-github@vindvejr.dk>
2024-12-10 21:53:11 +0100

    Simplify DateTimeType handling for Feed
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>

--- bundles/org.openhab.binding.feed/src/main/java/org/openhab/binding/feed/internal/handler/FeedHandler.java
+++ bundles/org.openhab.binding.feed/src/main/java/org/openhab/binding/feed/internal/handler/FeedHandler.java
@@ -21,8 +21,6 @@ import java.math.BigDecimal;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ScheduledFuture;
@@ -184,8 +182,7 @@ public class FeedHandler extends BaseThingHandler {
                     return;
                 } else {
                     Date date = latestEntry.getPublishedDate();
-                    ZonedDateTime zdt = ZonedDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
-                    state = new DateTimeType(zdt);
+                    state = new DateTimeType(date.toInstant());
                 }
                 break;
             case CHANNEL_AUTHOR:
