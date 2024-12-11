8c16be3ed59aed4e448176b6a3d7508ac38860fa
Laurent ARNAL <laurent@clae.net>
2024-12-11 12:07:47 +0100

    add location and room support to API
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

---
+++ bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/dto/SmartthingsRoom.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package org.openhab.binding.smartthings.internal.dto;
+
+/**
+ * Data object for smartthings app description
+ *
+ * @author Laurent ARNAL - Initial contribution
+ */
+
+public class SmartthingsRoom {
+    public String roomId;
+    public String locationId;
+    public String name;
+    public String backgroundImage;
+
+    public String[] allowed;
+    public String created;
+    public String lastModified;
+
+    public record indoorMap(String mapRoomId, String color) {
+    }
+
+    public indoorMap indoorMap;
+}
