610bbb2f9415cb1516f4d01fd54a880d4d70ebca
Bernd Weymann <bernd.weymann@gmail.com>
2024-12-12 00:18:08 +0100

    gateway exception
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

---
+++ bundles/org.openhab.binding.dirigera/src/main/java/org/openhab/binding/dirigera/internal/exception/NoGatewayException.java
@@ -0,0 +1,30 @@
+package org.openhab.binding.dirigera.internal.exception;
+
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * The {@link NoGatewayException} Exception if no gateway is available
+ *
+ * @author Bernd Weymann - Initial contribution
+ */
+@NonNullByDefault
+public class NoGatewayException extends RuntimeException {
+
+    private static final long serialVersionUID = 5184571486237890053L;
+
+    public NoGatewayException(String message) {
+        super(message);
+    }
+}
