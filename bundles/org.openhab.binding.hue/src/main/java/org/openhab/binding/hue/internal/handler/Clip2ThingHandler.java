44e5d2f9eb4ef7c3b0722631cfb455a8ae85dc34
Andrew Fiddian-Green <software@whitebear.ch>
2024-12-16 19:19:47 +0000

    [hue] Recognise `grouped_motion` and `smart_scene` resources (#17905)
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>
    Signed-off-by: Ciprian Pascu <contact@ciprianpascu.ro>

--- bundles/org.openhab.binding.hue/src/main/java/org/openhab/binding/hue/internal/handler/Clip2ThingHandler.java
+++ bundles/org.openhab.binding.hue/src/main/java/org/openhab/binding/hue/internal/handler/Clip2ThingHandler.java
@@ -765,7 +765,7 @@ public class Clip2ThingHandler extends BaseThingHandler {
      * @param fullResources the full list of resources of the given type.
      */
     public void onResourcesList(ResourceType resourceType, List<Resource> fullResources) {
-        if (resourceType == ResourceType.SCENE) {
+        if (SUPPORTED_SCENE_TYPES.contains(resourceType)) {
             updateSceneContributors(fullResources);
         } else {
             fullResources.stream().filter(r -> resourceId.equals(r.getId())).findAny()
