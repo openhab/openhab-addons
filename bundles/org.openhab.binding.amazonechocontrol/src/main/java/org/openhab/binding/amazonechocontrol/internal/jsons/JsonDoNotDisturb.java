2e344cb78b08878f9f655bdafb1891d1502d39b9
Cody Cutrer <cody@cutrer.us>
2024-12-20 08:31:38 -0700

    [amazonechocontrol] Add do not disturb channel
    Signed-off-by: Cody Cutrer <cody@cutrer.us>

---
+++ bundles/org.openhab.binding.amazonechocontrol/src/main/java/org/openhab/binding/amazonechocontrol/internal/jsons/JsonDoNotDisturb.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.amazonechocontrol.internal.jsons;
+
+import java.util.List;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * The {@link JsonDoNotDisturb} encapsulate the GSON data of the /api/dnd/device-status-list response
+ *
+ * @author Cody Cutrer - Initial contribution
+ */
+@NonNullByDefault
+public class JsonDoNotDisturb {
+
+    public @Nullable List<DoNotDisturbDeviceStatus> doNotDisturbDeviceStatusList;
+
+    public static class DoNotDisturbDeviceStatus {
+        public @Nullable Boolean enabled;
+        public @Nullable String deviceSerialNumber;
+        public @Nullable String deviceType;
+    }
+}
