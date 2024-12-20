746c4560387f8a575689f298694f032d83dc8c23
Cody Cutrer <cody@cutrer.us>
2024-12-20 10:25:48 -0700

    [tuya] Initial contribution
    This is a port of the SmartHome/J binding by @J-N-K
    
    Changes from that binding:
     * Use BaseDynamicDescriptionProvider from core, rather than
       SmartHome/J's commons.SimpleDynamicDescriptionProvider
     * Generated i18n properties file
     * Minor wording tweak in the README
    
    Co-authored-by: Jan N. Klug <github@klug.nrw>
    Co-authored-by: jsetton <jeremy.setton@gmail.com>
    Co-authored-by: Jimmy Tanagra <jcode@tanagra.id.au>
    Signed-off-by: Cody Cutrer <cody@cutrer.us>

---
+++ bundles/org.openhab.binding.tuya/src/main/java/org/openhab/binding/tuya/internal/TuyaDynamicCommandDescriptionProvider.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.tuya.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.thing.binding.BaseDynamicCommandDescriptionProvider;
+
+/**
+ * This class provides the list of valid commands for dynamic channels.
+ *
+ * @author Cody Cutrer - Initial contribution
+ *
+ */
+@NonNullByDefault
+class TuyaDynamicCommandDescriptionProvider extends BaseDynamicCommandDescriptionProvider {
+}
