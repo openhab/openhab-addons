82d68168775c975822522ec1a9286b9b1bb020d2
Jeremy <jsetton@users.noreply.github.com>
2025-01-06 07:08:17 -0500

    [insteon] Fix legacy all link broadcast message not processed (#18049)
    Signed-off-by: Jeremy Setton <jeremy.setton@gmail.com>
    Signed-off-by: Christian Koch <78686276+chilobo@users.noreply.github.com>

--- bundles/org.openhab.binding.insteon/src/main/java/org/openhab/binding/insteon/internal/device/LegacyDevice.java
+++ bundles/org.openhab.binding.insteon/src/main/java/org/openhab/binding/insteon/internal/device/LegacyDevice.java
@@ -334,7 +334,7 @@ public class LegacyDevice {
             if (qe == null) {
                 return 0L;
             }
-            if (!qe.getMsg().isBroadcast()) {
+            if (!qe.getMsg().isAllLinkBroadcast()) {
                 logger.debug("qe taken off direct: {} {}", qe.getFeature(), qe.getMsg());
                 lastQueryTime = timeNow;
                 // mark feature as pending
@@ -382,7 +382,7 @@ public class LegacyDevice {
         synchronized (mrequestQueue) {
             mrequestQueue.add(new QEntry(feature, msg, now + delay));
         }
-        if (!msg.isBroadcast()) {
+        if (!msg.isAllLinkBroadcast()) {
             msg.setQuietTime(QUIET_TIME_DIRECT_MESSAGE);
         }
         logger.trace("enqueing direct message with delay {}", delay);
