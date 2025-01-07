379f00fb956193f27694acb2a1511cc51e866c1c
Michael Lobstein <michael.lobstein@gmail.com>
2025-01-06 21:22:37 -0600

    Add End Time and Progress channels
    Signed-off-by: Michael Lobstein <michael.lobstein@gmail.com>

--- bundles/org.openhab.binding.roku/src/main/java/org/openhab/binding/roku/internal/RokuBindingConstants.java
+++ bundles/org.openhab.binding.roku/src/main/java/org/openhab/binding/roku/internal/RokuBindingConstants.java
@@ -55,6 +55,8 @@ public class RokuBindingConstants {
     public static final String PLAY_MODE = "playMode";
     public static final String TIME_ELAPSED = "timeElapsed";
     public static final String TIME_TOTAL = "timeTotal";
+    public static final String END_TIME = "endTime";
+    public static final String PROGRESS = "progress";
     public static final String ACTIVE_CHANNEL = "activeChannel";
     public static final String SIGNAL_MODE = "signalMode";
     public static final String SIGNAL_QUALITY = "signalQuality";
