5f9300863829871d1c96d27752a9ff6a0a64e7be
Andrew Fiddian-Green <software@whitebear.ch>
2025-02-11 10:12:24 +0000

    [hdpowerview] Fix discovery to use serial number as representation property (#18200)
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/internal/discovery/GatewayDiscoveryParticipant.java
+++ bundles/org.openhab.binding.hdpowerview/src/main/java/org/openhab/binding/hdpowerview/internal/discovery/GatewayDiscoveryParticipant.java
@@ -14,6 +14,7 @@ package org.openhab.binding.hdpowerview.internal.discovery;
 
 import static org.openhab.binding.hdpowerview.internal.HDPowerViewBindingConstants.*;
 
+import java.util.Arrays;
 import java.util.Set;
 
 import javax.jmdns.ServiceInfo;
@@ -24,6 +25,7 @@ import org.openhab.binding.hdpowerview.internal.config.HDPowerViewHubConfigurati
 import org.openhab.core.config.discovery.DiscoveryResult;
 import org.openhab.core.config.discovery.DiscoveryResultBuilder;
 import org.openhab.core.config.discovery.mdns.MDNSDiscoveryParticipant;
+import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.ThingTypeUID;
 import org.openhab.core.thing.ThingUID;
 import org.osgi.service.component.annotations.Component;
@@ -39,20 +41,21 @@ import org.slf4j.LoggerFactory;
 @Component
 public class GatewayDiscoveryParticipant implements MDNSDiscoveryParticipant {
 
-    private static final String LABEL_KEY = "discovery.gateway.label";
-
     private final Logger logger = LoggerFactory.getLogger(GatewayDiscoveryParticipant.class);
 
     @Override
     public @Nullable DiscoveryResult createResult(ServiceInfo service) {
-        for (String host : service.getHostAddresses()) {
-            if (VALID_IP_V4_ADDRESS.matcher(host).matches()) {
-                ThingUID thingUID = new ThingUID(THING_TYPE_GATEWAY, host.replace('.', '_'));
+        ThingUID thingUID = getThingUID(service);
+        if (thingUID != null) {
+            String serial = thingUID.getId();
+            String host = getIpV4Address(service);
+            if (host != null) {
+                String label = String.format("@text/%s [\"%s\"]", LABEL_KEY_GATEWAY, host);
                 DiscoveryResult hub = DiscoveryResultBuilder.create(thingUID)
                         .withProperty(HDPowerViewHubConfiguration.HOST, host)
-                        .withRepresentationProperty(HDPowerViewHubConfiguration.HOST)
-                        .withLabel(String.format("@text/%s [\"%s\"]", LABEL_KEY, host)).build();
-                logger.debug("mDNS discovered Gen 3 gateway on host '{}'", host);
+                        .withProperty(Thing.PROPERTY_SERIAL_NUMBER, serial)
+                        .withRepresentationProperty(Thing.PROPERTY_SERIAL_NUMBER).withLabel(label).build();
+                logger.debug("mDNS discovered Gen 3 gateway '{}' on host '{}'", thingUID, host);
                 return hub;
             }
         }
@@ -71,11 +74,20 @@ public class GatewayDiscoveryParticipant implements MDNSDiscoveryParticipant {
 
     @Override
     public @Nullable ThingUID getThingUID(ServiceInfo service) {
-        for (String host : service.getHostAddresses()) {
-            if (VALID_IP_V4_ADDRESS.matcher(host).matches()) {
-                return new ThingUID(THING_TYPE_GATEWAY, host.replace('.', '_'));
+        String host = getIpV4Address(service);
+        if (host != null) {
+            String serial = service.getPropertyString("sn");
+            if (serial != null) {
+                return new ThingUID(THING_TYPE_GATEWAY, serial);
+            } else {
+                logger.debug("Error discovering gateway 'missing serial number'");
             }
         }
         return null;
+    }
+
+    private static @Nullable String getIpV4Address(ServiceInfo service) {
+        return Arrays.stream(service.getHostAddresses()).filter(host -> VALID_IP_V4_ADDRESS.matcher(host).matches())
+                .findFirst().orElse(null);
     }
 }
