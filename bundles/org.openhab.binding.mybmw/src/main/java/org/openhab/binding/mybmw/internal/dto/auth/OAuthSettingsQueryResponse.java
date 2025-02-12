3b820edcb237e95f26d6651eedbdf761b4a85eb9
Mark Herwege <mherwege@users.noreply.github.com>
2025-02-12 21:48:24 +0100

    [mybmw] improve authentication (#18235)
    * authentication
    
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.binding.mybmw/src/main/java/org/openhab/binding/mybmw/internal/dto/auth/OAuthSettingsQueryResponse.java
+++ bundles/org.openhab.binding.mybmw/src/main/java/org/openhab/binding/mybmw/internal/dto/auth/OAuthSettingsQueryResponse.java
@@ -14,13 +14,15 @@ package org.openhab.binding.mybmw.internal.dto.auth;
 
 import java.util.List;
 
+import org.openhab.binding.mybmw.internal.utils.Constants;
+
 /**
- * The {@link AuthQueryResponse} Data Transfer Object
+ * The {@link OAuthSettingsQueryResponse} Data Transfer Object
  *
  * @author Bernd Weymann - Initial contribution
  * @author Martin Grassl - add toString for debugging
  */
-public class AuthQueryResponse {
+public class OAuthSettingsQueryResponse {
     public String clientName;// ": "mybmwapp",
     public String clientSecret;// ": "c0e3393d-70a2-4f6f-9d3c-8530af64d552",
     public String clientId;// ": "31c357a0-7a1d-4590-aa99-33b97244d048",
@@ -31,7 +33,7 @@ public class AuthQueryResponse {
     public String country;// ": "US",
     public String authorizationEndpoint;// ": "https://customer.bmwgroup.com/oneid/login",
     public String tokenEndpoint;// ": "https://customer.bmwgroup.com/gcdm/oauth/token",
-    public List<String> scopes;// ;": [
+    private List<String> scopes;// ;": [
     // "openid",
     // "profile",
     // "email",
@@ -50,9 +52,13 @@ public class AuthQueryResponse {
     public List<String> promptValues; // ": ["login"]
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see java.lang.Object#toString()
      */
+
+    public String scopes() {
+        return String.join(Constants.SPACE, scopes);
+    }
 
     @Override
     public String toString() {
