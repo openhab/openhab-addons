65e04da798e628547f56ac11613523ae878d4131
Bernd Weymann <bernd.weymann@gmail.com>
2025-02-17 22:48:54 +0100

    fix header
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

--- bundles/org.openhab.binding.solarforecast/src/test/java/org/openhab/binding/solarforecast/internal/solcast/handler/SolcastBridgeMock.java
+++ bundles/org.openhab.binding.solarforecast/src/test/java/org/openhab/binding/solarforecast/internal/solcast/handler/SolcastBridgeMock.java
@@ -1,5 +1,5 @@
-/**
- * Copyright (c) 2010-2024 Contributors to the openHAB project
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -19,7 +19,7 @@ import org.openhab.core.thing.Bridge;
 
 /**
  * The {@link SolcastBridgeMock} is mocking Solcast Bridge Handler
- * 
+ *
  * @author Bernd Weymann - Initial contribution
  */
 @NonNullByDefault
