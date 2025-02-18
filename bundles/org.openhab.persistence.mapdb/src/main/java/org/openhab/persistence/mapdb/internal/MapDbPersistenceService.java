e3fd0c0df9fe8e514838687b6439dddfee12f1fc
Mark Herwege <mark.herwege@telenet.be>
2025-02-18 12:19:25 +0100

    mapdb
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.persistence.mapdb/src/main/java/org/openhab/persistence/mapdb/internal/MapDbPersistenceService.java
+++ bundles/org.openhab.persistence.mapdb/src/main/java/org/openhab/persistence/mapdb/internal/MapDbPersistenceService.java
@@ -165,7 +165,7 @@ public class MapDbPersistenceService implements QueryablePersistenceService {
     @Override
     public Set<PersistenceItemInfo> getItemInfo() {
         return map.values().stream().map(this::deserialize).flatMap(MapDbPersistenceService::streamOptional)
-                .collect(Collectors.<PersistenceItemInfo> toUnmodifiableSet());
+                .collect(Collectors.<PersistenceItemInfo>toUnmodifiableSet());
     }
 
     @Override
@@ -211,13 +211,17 @@ public class MapDbPersistenceService implements QueryablePersistenceService {
     }
 
     @Override
-    public @Nullable PersistedItem persistedItem(String itemName) {
-        String json = map.get(itemName);
+    public @Nullable PersistedItem persistedItem(String itemName, @Nullable String alias) {
+        String json = map.get(alias != null ? alias : itemName);
         if (json == null) {
             return null;
         }
         Optional<MapDbItem> item = deserialize(json);
-        return item.orElse(null);
+        MapDbItem dbItem = item.orElse(null);
+        if (dbItem != null) {
+            dbItem.setName(itemName);
+        }
+        return dbItem;
     }
 
     private String serialize(MapDbItem item) {
