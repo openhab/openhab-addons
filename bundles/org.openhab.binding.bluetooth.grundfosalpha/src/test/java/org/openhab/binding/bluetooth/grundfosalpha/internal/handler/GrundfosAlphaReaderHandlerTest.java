64eeeae32c54988e5c9d1d3c8c65f526868d15c8
Jacob Laursen <jacob-github@vindvejr.dk>
2025-02-19 09:32:11 +0100

    [grundfosalpha] Add support for Alpha3 pump (#18187)
    * Add support for Alpha3
    * Add I18N support for discovery labels
    
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>

--- bundles/org.openhab.binding.bluetooth.grundfosalpha/src/test/java/org/openhab/binding/bluetooth/grundfosalpha/internal/handler/GrundfosAlphaReaderHandlerTest.java
+++ bundles/org.openhab.binding.bluetooth.grundfosalpha/src/test/java/org/openhab/binding/bluetooth/grundfosalpha/internal/handler/GrundfosAlphaReaderHandlerTest.java
@@ -10,16 +10,18 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.bluetooth.grundfosalpha.internal;
+package org.openhab.binding.bluetooth.grundfosalpha.internal.handler;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.openhab.binding.bluetooth.grundfosalpha.internal.GrundfosAlphaBindingConstants;
 import org.openhab.binding.bluetooth.notification.BluetoothScanNotification;
 import org.openhab.core.library.types.QuantityType;
 import org.openhab.core.library.unit.SIUnits;
@@ -34,23 +36,23 @@ import org.openhab.core.thing.binding.ThingHandlerCallback;
 import org.openhab.core.util.HexUtils;
 
 /**
- * Test the {@link GrundfosAlphaHandler}.
+ * Test the {@link GrundfosAlphaReaderHandler}.
  *
  * @author Markus Heberling - Initial contribution
  */
+@NonNullByDefault
 @ExtendWith(MockitoExtension.class)
-class GrundfosAlphaHandlerTest {
+class GrundfosAlphaReaderHandlerTest {
 
-    private @Mock Thing thingMock;
-
-    private @Mock ThingHandlerCallback callback;
+    private @NonNullByDefault({}) @Mock Thing thingMock;
+    private @NonNullByDefault({}) @Mock ThingHandlerCallback callback;
 
     @Test
     public void testMessageType0xf1() {
         byte[] data = HexUtils.hexToBytes("15f130017a5113030300994109589916613003004005");
         final BluetoothScanNotification scanNotification = new BluetoothScanNotification();
         scanNotification.setManufacturerData(data);
-        final GrundfosAlphaHandler handler = new GrundfosAlphaHandler(thingMock);
+        final GrundfosAlphaReaderHandler handler = new GrundfosAlphaReaderHandler(thingMock);
         handler.setCallback(callback);
         handler.onScanRecordReceived(scanNotification);
 
@@ -67,23 +69,23 @@ class GrundfosAlphaHandlerTest {
         byte[] data = HexUtils.hexToBytes("14f23001650305065419b9180f011f007c1878170d");
         final BluetoothScanNotification scanNotification = new BluetoothScanNotification();
         scanNotification.setManufacturerData(data);
-        final GrundfosAlphaHandler handler = new GrundfosAlphaHandler(thingMock);
+        final GrundfosAlphaReaderHandler handler = new GrundfosAlphaReaderHandler(thingMock);
         handler.setCallback(callback);
         handler.onScanRecordReceived(scanNotification);
 
         verify(callback).statusUpdated(thingMock,
                 new ThingStatusInfo(ThingStatus.ONLINE, ThingStatusDetail.NONE, null));
         verify(callback).stateUpdated(
-                new ChannelUID(thingMock.getUID(), GrundfosAlphaBindingConstants.CHANNEL_TYPE_BATTERY_LEVEL),
+                new ChannelUID(thingMock.getUID(), GrundfosAlphaBindingConstants.CHANNEL_BATTERY_LEVEL),
                 new QuantityType<>(75, Units.PERCENT));
         verify(callback).stateUpdated(
-                new ChannelUID(thingMock.getUID(), GrundfosAlphaBindingConstants.CHANNEL_TYPE_FLOW_RATE),
+                new ChannelUID(thingMock.getUID(), GrundfosAlphaBindingConstants.CHANNEL_FLOW_RATE),
                 new QuantityType<>(0.98939496, Units.CUBICMETRE_PER_HOUR));
         verify(callback).stateUpdated(
-                new ChannelUID(thingMock.getUID(), GrundfosAlphaBindingConstants.CHANNEL_TYPE_PUMP_HEAD),
+                new ChannelUID(thingMock.getUID(), GrundfosAlphaBindingConstants.CHANNEL_PUMP_HEAD),
                 new QuantityType<>(1.9315165, SIUnits.METRE));
         verify(callback).stateUpdated(
-                new ChannelUID(thingMock.getUID(), GrundfosAlphaBindingConstants.CHANNEL_TYPE_PUMP_TEMPERATUR),
+                new ChannelUID(thingMock.getUID(), GrundfosAlphaBindingConstants.CHANNEL_PUMP_TEMPERATURE),
                 new QuantityType<>(31, SIUnits.CELSIUS));
 
         verifyNoMoreInteractions(callback);
