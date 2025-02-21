7c4cd0803ea30d2dbfcc870e8fec82d34032ad24
Holger Hees <holger.hees@desy.de>
2025-02-21 12:46:57 +0100

    seperate python code into a new repository

--- bundles/org.openhab.automation.pythonscripting/src/main/resources/lib/openhab/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "0.1.16"
-
-from openhab.helper import rule, logger, Registry, Timer
--- bundles/org.openhab.automation.pythonscripting/src/main/resources/lib/openhab/actions.py
+++
@@ -1,6 +0,0 @@
-from org.openhab.core.model.script.actions import Audio, BusEvent, Ephemeris, Exec, HTTP, Log, Ping, ScriptExecution, Semantics, Things, Transformation, Voice
-
-try:
-    from org.openhab.io.openhabcloud import NotificationAction
-except:
-    NotificationAction = None
