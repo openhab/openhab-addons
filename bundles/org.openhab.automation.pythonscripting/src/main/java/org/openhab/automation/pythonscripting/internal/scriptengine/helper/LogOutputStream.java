661e46870ffbc61b3f2e13e8cf71faa6451929c6
Holger Hees <holger.hees@desy.de>
2025-02-24 18:33:55 +0100

    code reorganisation

--- bundles/org.openhab.automation.pythonscripting/src/main/java/org/openhab/automation/pythonscripting/internal/scriptengine/helper/LogOutputStream.java
+++ bundles/org.openhab.automation.pythonscripting/src/main/java/org/openhab/automation/pythonscripting/internal/scriptengine/helper/LogOutputStream.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.automation.pythonscripting.internal.scriptengine;
+package org.openhab.automation.pythonscripting.internal.scriptengine.helper;
 
 import java.io.OutputStream;
 
