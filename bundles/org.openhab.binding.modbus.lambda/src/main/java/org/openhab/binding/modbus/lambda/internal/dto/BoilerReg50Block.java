b08963f98bd44183d88e2e57a6026f8dced407dc
Christian Koch <78686276+chilobo@users.noreply.github.com>
2025-02-26 21:42:54 +0100

    This is my commit message
    Signed-off-by: Christian Koch <christian@koch-bensheim.de>
    Signed-off-by: Christian Koch <78686276+chilobo@users.noreply.github.com>

---
+++ bundles/org.openhab.binding.modbus.lambda/src/main/java/org/openhab/binding/modbus/lambda/internal/dto/BoilerReg50Block.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.modbus.lambda.internal.dto;
+
+/**
+ * Dto class for the BoilerReg50 Block
+ *
+ * @author Paul Frank - Initial contribution
+ * @author Christian Koch - modified for lambda heat pump based on stiebeleltron binding for modbus
+ *
+ */
+public class BoilerReg50Block {
+
+    public int boilerMaximumBoilerTemperature;
+}
