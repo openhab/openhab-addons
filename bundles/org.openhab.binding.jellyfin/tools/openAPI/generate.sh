d7b8b43c52cbb452b1ac4e4cc5ae84310ba73f8d
Patrik Gfeller <patrik.gfeller@proton.me>
2025-02-28 14:13:10 +0100

    🔧 work on openAPI poc
    Signed-off-by: Patrik Gfeller <patrik.gfeller@proton.me>

--- bundles/org.openhab.binding.jellyfin/tools/openAPI/generate.sh
+++ bundles/org.openhab.binding.jellyfin/tools/openAPI/generate.sh
@@ -3,7 +3,11 @@ clear
 
 VERSION=$(curl -sL https://repo.jellyfin.org/releases/openapi/jellyfin-openapi-stable.json | jq -r .info.version)
 
-echo "ℹ️  - Using latest Jellyfin API: ${VERSION}"
+echo "ℹ️  - Latest Jellyfin API: ${VERSION}"
+
+VERSION=10.8.13
+
+echo "ℹ️  - Generate API for: ${VERSION}"
 
 FILENAME="./jellyfin-openapi-$VERSION.json"
 
@@ -19,16 +23,39 @@ yq -p json -o yaml $FILENAME > jellyfin-openapi-$VERSION.yaml
 
 DOCKER_IMAGE=openapitools/openapi-generator-cli:v7.12.0
 DOCKER_VOLUME_CONFIG="/config"
-DOCKER_VOLUME_WORK="/config"
+DOCKER_VOLUME_WORK="/work"
+
 # --rm: Automatically remove the container and its associated anonymous 
 #      volumes when it exits
-docker run --rm --interactive \
+# docker run --rm --interactive \
+#     --user $(id -u):$(id -g)  \
+#     --workdir ${DOCKER_VOLUME_WORK} \
+#     --volume "${PWD}:${DOCKER_VOLUME_CONFIG}" \
+#     --volume "${PWD}/poc:${DOCKER_VOLUME_WORK}" \
+#     $DOCKER_IMAGE validate \
+#         --input-spec ${DOCKER_VOLUME_CONFIG}/jellyfin-openapi-$VERSION.yaml \
+#         > console-validate.log
+
+docker run --rm --interactive  \
+    --user $(id -u):$(id -g)  \
     --workdir ${DOCKER_VOLUME_WORK} \
     --volume "${PWD}:${DOCKER_VOLUME_CONFIG}" \
     --volume "${PWD}/poc:${DOCKER_VOLUME_WORK}" \
-    $DOCKER_IMAGE validate --input-spec ${DOCKER_VOLUME_CONFIG}/jellyfin-openapi-$VERSION.yaml
-docker run --rm --interactive  \
-    --workdir ${DOCKER_VOLUME} \
-    -volume "${PWD}:${DOCKER_VOLUME_CONFIG}" \
-    --volume "${PWD}/poc:${DOCKER_VOLUME_WORK}" \
-    $DOCKER_IMAGE generate --config openapitools.json  --generator-name java --input-spec ${DOCKER_VOLUME_CONFIG}/jellyfin-openapi-$VERSION.yaml
+    --env-file image.ini \
+    $DOCKER_IMAGE generate \
+        --generator-name java \
+        --output ${DOCKER_VOLUME_WORK} \
+        --config ${DOCKER_VOLUME_CONFIG}/codegen.config.json \
+        --input-spec ${DOCKER_VOLUME_CONFIG}/jellyfin-openapi-$VERSION.yaml \
+        > console-generate.log
+
+# docker run --rm --interactive  \
+#     --user $(id -u):$(id -g)  \
+#     --workdir ${DOCKER_VOLUME_WORK} \
+#     --volume "${PWD}:${DOCKER_VOLUME_CONFIG}" \
+#     --volume "${PWD}/poc:${DOCKER_VOLUME_WORK}" \
+#     --env-file image.ini \
+#     $DOCKER_IMAGE generate \
+#         --generator-name java \
+#         --config ${DOCKER_VOLUME_CONFIG}/codegen.config.json \
+#         > console-generate.log
