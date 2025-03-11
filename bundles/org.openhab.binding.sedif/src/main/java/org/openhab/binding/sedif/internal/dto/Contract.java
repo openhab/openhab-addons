6d45a7135acf25c142e69736fa9ad5f29ae50655
Laurent ARNAL <laurent@clae.net>
2025-03-11 19:01:38 +0100

    Add decoding response refactor login setp
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

---
+++ bundles/org.openhab.binding.sedif/src/main/java/org/openhab/binding/sedif/internal/dto/Contract.java
@@ -0,0 +1,17 @@
+package org.openhab.binding.sedif.internal.dto;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+public class Contract {
+    public String AutoriteOrganisatrice;
+    public String DateSortieEPT;
+    public boolean eFacture;
+    public boolean iclActive;
+    public @Nullable String Id;
+    public @Nullable String Name;
+    public boolean prelevAuto;
+    public @Nullable String SITE_Commune;
+    public @Nullable String SITE_CP;
+    public @Nullable String SITE_Rue;
+    public @Nullable String Statut;
+}
