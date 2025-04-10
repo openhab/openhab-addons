f7581fa6a8685cd8f5435790c8cb176744362c1a
Andrew Fiddian-Green <software@whitebear.ch>
2025-04-10 22:44:38 +0100

    second try
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.binding.siemensrds/src/main/java/org/openhab/binding/siemensrds/internal/RdsAccessToken.java
+++ bundles/org.openhab.binding.siemensrds/src/main/java/org/openhab/binding/siemensrds/internal/RdsAccessToken.java
@@ -22,6 +22,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -86,7 +87,7 @@ public class RdsAccessToken {
 
         try (OutputStream outputStream = https.getOutputStream();
                 DataOutputStream dataOutputStream = new DataOutputStream(outputStream)) {
-            dataOutputStream.write(payload.getBytes(StandardCharsets.UTF_8));
+            dataOutputStream.writeBytes(URLEncoder.encode(payload, StandardCharsets.UTF_8));
             dataOutputStream.flush();
         }
 
