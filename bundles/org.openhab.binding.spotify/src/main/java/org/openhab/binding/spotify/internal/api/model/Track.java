2fa09c8ebc8aec8f611a4488902b11739defa690
Laurent ARNAL <laurent@clae.net>
2025-04-12 16:15:33 +0200

    add forget files
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

---
+++ bundles/org.openhab.binding.spotify/src/main/java/org/openhab/binding/spotify/internal/api/model/Track.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.spotify.internal.api.model;
+
+import java.util.List;
+
+/**
+ * Spotify Web Api Playlist data class.
+ *
+ * @author Hilbrand Bouwkamp - Initial contribution
+ */
+public class Track {
+
+    private String name;
+    private String uri;
+
+    private Album album;
+    private List<Artist> artists;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public List<Artist> getArtists() {
+        return artists;
+    }
+
+    public List<Image> getImages() {
+        return album.getImages();
+    }
+}
