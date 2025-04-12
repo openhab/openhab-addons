51ffec25d802bbf3e32fd38fc4d5b969fd49d970
Laurent ARNAL <laurent@clae.net>
2025-04-12 16:15:33 +0200

    add forget files
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

---
+++ bundles/org.openhab.binding.spotify/src/main/java/org/openhab/binding/spotify/internal/api/model/Show.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.spotify.internal.api.model;
+
+import java.util.List;
+
+/**
+ * Spotify Api Album data class.
+ *
+ * @author Andreas Stenlund - Initial contribution
+ * @author Hilbrand Bouwkamp - Moved to it's own class
+ */
+public class Show {
+
+    private List<String> availableMarkets;
+    private ExternalUrl externalUrls;
+    private String href;
+    private String id;
+    private List<Image> images;
+    private String name;
+    private String type;
+    private String uri;
+
+    public List<String> getAvailableMarkets() {
+        return availableMarkets;
+    }
+
+    public ExternalUrl getExternalUrls() {
+        return externalUrls;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<Image> getImages() {
+        return images;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+}
