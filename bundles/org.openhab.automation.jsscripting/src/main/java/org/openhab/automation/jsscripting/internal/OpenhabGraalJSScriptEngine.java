openhab/openhab-addons

commit a4222250fb29cb399d2b2f170f497a820a801d33

[jsscripting] Code improvements (#18500)
Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.automation.jsscripting/src/main/java/org/openhab/automation/jsscripting/internal/OpenhabGraalJSScriptEngine.java
@@ -14,6 +14,7 @@ package org.openhab.automation.jsscripting.internal;
 
 import static org.openhab.core.automation.module.script.ScriptEngineFactory.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -25,7 +26,6 @@ import java.nio.file.LinkOption;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.OpenOption;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.attribute.FileAttribute;
 import java.time.Duration;
 import java.time.Instant;
@@ -77,7 +77,8 @@ public class OpenhabGraalJSScriptEngine
     private static final Source GLOBAL_SOURCE;
     static {
         try {
-            GLOBAL_SOURCE = Source.newBuilder("js", getFileAsReader("node_modules/@jsscripting-globals.js"),
+            GLOBAL_SOURCE = Source.newBuilder("js",
+                    getFileAsReader(GraalJSScriptEngineFactory.NODE_DIR + File.separator + "@jsscripting-globals.js"),
                     "@jsscripting-globals.js").cached(true).build();
         } catch (IOException e) {
             throw new IllegalStateException("Failed to load @jsscripting-globals.js", e);
@@ -87,9 +88,9 @@ public class OpenhabGraalJSScriptEngine
     private static final Source OPENHAB_JS_SOURCE;
     static {
         try {
-            OPENHAB_JS_SOURCE = Source
-                    .newBuilder("js", getFileAsReader("node_modules/@openhab-globals.js"), "@openhab-globals.js")
-                    .cached(true).build();
+            OPENHAB_JS_SOURCE = Source.newBuilder("js",
+                    getFileAsReader(GraalJSScriptEngineFactory.NODE_DIR + File.separator + "@openhab-globals.js"),
+                    "@openhab-globals.js").cached(true).build();
         } catch (IOException e) {
             throw new IllegalStateException("Failed to load @openhab-globals.js", e);
         }
@@ -97,8 +98,6 @@ public class OpenhabGraalJSScriptEngine
     private static final String OPENHAB_JS_INJECTION_CODE = "Object.assign(this, require('openhab'));";
 
     private static final String REQUIRE_WRAPPER_NAME = "__wraprequire__";
-    /** Final CommonJS search path for our library */
-    private static final Path NODE_DIR = Paths.get("node_modules");
     /** Shared Polyglot {@link Engine} across all instances of {@link OpenhabGraalJSScriptEngine} */
     private static final Engine ENGINE = Engine.newBuilder().allowExperimentalOptions(true)
             .option("engine.WarnInterpreterOnly", "false").build();
@@ -325,7 +324,7 @@ public class OpenhabGraalJSScriptEngine
      * @return whether the given path is a node root directory
      */
     private static boolean isRootNodePath(Path path) {
-        return path.startsWith(path.getRoot().resolve(NODE_DIR));
+        return path.startsWith(path.getRoot().resolve(GraalJSScriptEngineFactory.NODE_DIR));
     }
 
     /**

bundles/org.openhab.automation.jsscripting/pom.xml
@@ -22,7 +22,6 @@
       !jdk.vm.ci.services</bnd.importpackage>
     <!-- Remember to check if the fix https://github.com/openhab/openhab-core/pull/4437 still works when upgrading GraalJS -->
     <graaljs.version>24.2.0</graaljs.version>
-    <oh.version>${project.version}</oh.version>
     <ohjs.version>openhab@5.10.0</ohjs.version>
   </properties>
 

bundles/org.openhab.automation.jsscripting/src/main/java/org/openhab/automation/jsscripting/internal/GraalJSScriptEngineFactory.java
@@ -12,6 +12,8 @@
  */
 package org.openhab.automation.jsscripting.internal;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -21,6 +23,7 @@ import javax.script.ScriptEngine;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.automation.jsscripting.internal.fs.watch.JSDependencyTracker;
+import org.openhab.core.OpenHAB;
 import org.openhab.core.automation.module.script.ScriptDependencyTracker;
 import org.openhab.core.automation.module.script.ScriptEngineFactory;
 import org.openhab.core.config.core.ConfigParser;
@@ -44,6 +47,12 @@ import com.oracle.truffle.js.scriptengine.GraalJSEngineFactory;
 @ConfigurableService(category = "automation", label = "JS Scripting", description_uri = "automation:jsscripting")
 @NonNullByDefault
 public final class GraalJSScriptEngineFactory implements ScriptEngineFactory {
+    public static final Path JS_DEFAULT_PATH = Paths.get(OpenHAB.getConfigFolder(), "automation", "js");
+    public static final String NODE_DIR = "node_modules";
+    public static final Path JS_LIB_PATH = JS_DEFAULT_PATH.resolve(NODE_DIR);
+
+    public static final String SCRIPT_TYPE = "application/javascript";
+
     private static final String CFG_INJECTION_ENABLED = "injectionEnabled";
     private static final String CFG_INJECTION_CACHING_ENABLED = "injectionCachingEnabled";
 
@@ -54,8 +63,8 @@ public final class GraalJSScriptEngineFactory implements ScriptEngineFactory {
     private static List<String> createScriptTypes() {
         // Add those for backward compatibility (existing scripts may rely on those MIME types)
         List<String> backwardCompat = List.of("application/javascript;version=ECMAScript-2021", "graaljs");
-        return Stream.of(factory.getMimeTypes(), factory.getExtensions(), backwardCompat).flatMap(List::stream)
-                .toList();
+        return Stream.of(List.of(SCRIPT_TYPE), factory.getMimeTypes(), factory.getExtensions(), backwardCompat)
+                .flatMap(List::stream).distinct().toList();
     }
 
     private boolean injectionEnabled = true;

bundles/org.openhab.automation.jsscripting/src/main/java/org/openhab/automation/jsscripting/internal/fs/watch/JSDependencyTracker.java
@@ -12,9 +12,8 @@
  */
 package org.openhab.automation.jsscripting.internal.fs.watch;
 
-import java.io.File;
-
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.automation.jsscripting.internal.GraalJSScriptEngineFactory;
 import org.openhab.core.automation.module.script.ScriptDependencyTracker;
 import org.openhab.core.automation.module.script.rulesupport.loader.AbstractScriptDependencyTracker;
 import org.openhab.core.service.WatchService;
@@ -35,11 +34,9 @@ import org.osgi.service.component.annotations.ReferencePolicy;
 @NonNullByDefault
 public class JSDependencyTracker extends AbstractScriptDependencyTracker {
 
-    private static final String LIB_PATH = String.join(File.separator, "automation", "js", "node_modules");
-
     @Activate
     public JSDependencyTracker(@Reference(target = WatchService.CONFIG_WATCHER_FILTER) WatchService watchService) {
-        super(watchService, LIB_PATH);
+        super(watchService, GraalJSScriptEngineFactory.JS_LIB_PATH.toString());
     }
 
     @Deactivate

bundles/org.openhab.automation.jsscripting/src/main/java/org/openhab/automation/jsscripting/internal/fs/watch/JSScriptFileWatcher.java
@@ -12,11 +12,11 @@
  */
 package org.openhab.automation.jsscripting.internal.fs.watch;
 
-import java.io.File;
 import java.nio.file.Path;
 import java.util.Optional;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.automation.jsscripting.internal.GraalJSScriptEngineFactory;
 import org.openhab.core.automation.module.script.ScriptDependencyTracker;
 import org.openhab.core.automation.module.script.ScriptEngineManager;
 import org.openhab.core.automation.module.script.rulesupport.loader.AbstractScriptFileWatcher;
@@ -37,21 +37,21 @@ import org.osgi.service.component.annotations.Reference;
 @Component(immediate = true, service = { ScriptFileWatcher.class, ScriptDependencyTracker.Listener.class })
 @NonNullByDefault
 public class JSScriptFileWatcher extends AbstractScriptFileWatcher {
-    private static final String FILE_DIRECTORY = "automation" + File.separator + "js";
-
     @Activate
     public JSScriptFileWatcher(final @Reference(target = WatchService.CONFIG_WATCHER_FILTER) WatchService watchService,
             final @Reference ScriptEngineManager manager, final @Reference ReadyService readyService,
             final @Reference StartLevelService startLevelService) {
-        super(watchService, manager, readyService, startLevelService, FILE_DIRECTORY, true);
+        super(watchService, manager, readyService, startLevelService,
+                GraalJSScriptEngineFactory.JS_DEFAULT_PATH.toString(), true);
     }
 
     @Override
     protected Optional<String> getScriptType(Path scriptFilePath) {
-        Optional<String> scriptType = super.getScriptType(scriptFilePath);
-        if (scriptType.isPresent() && !scriptFilePath.startsWith(getWatchPath().resolve("node_modules"))
-                && ("js".equals(scriptType.get()))) {
-            return scriptType;
+        if (!scriptFilePath.startsWith(GraalJSScriptEngineFactory.JS_LIB_PATH)) {
+            Optional<String> scriptType = super.getScriptType(scriptFilePath);
+            if (scriptType.isPresent() && ("js".equals(scriptType.get()))) {
+                return Optional.of(GraalJSScriptEngineFactory.SCRIPT_TYPE);
+            }
         }
         return Optional.empty();
     }

bundles/org.openhab.automation.jsscripting/src/main/java/org/openhab/automation/jsscripting/internal/scope/AbstractScriptExtensionProvider.java
@@ -32,8 +32,8 @@ import org.osgi.service.component.annotations.Activate;
  * @author Jonathan Gilbert - Initial contribution
  */
 public abstract class AbstractScriptExtensionProvider implements ScriptExtensionProvider {
-    private Map<String, Function<String, Object>> types = new HashMap<>();
-    private Map<String, Map<String, Object>> idToTypes = new ConcurrentHashMap<>();
+    protected final Map<String, Function<String, Object>> types = new HashMap<>();
+    protected final Map<String, Map<String, Object>> idToTypes = new ConcurrentHashMap<>();
 
     protected abstract String getPresetName();
 

bundles/org.openhab.automation.jsscripting/src/main/java/org/openhab/automation/jsscripting/internal/scope/ScriptDisposalAwareScriptExtensionProvider.java
@@ -12,78 +12,16 @@
  */
 package org.openhab.automation.jsscripting.internal.scope;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
-
-import org.eclipse.jdt.annotation.Nullable;
-import org.openhab.core.automation.module.script.ScriptExtensionProvider;
-import org.osgi.framework.BundleContext;
-import org.osgi.service.component.annotations.Activate;
 
 /**
  * Base class to offer support for script extension providers
  *
  * @author Jonathan Gilbert - Initial contribution
+ * @author Florian Hotze - Refactor to inherit from {@link AbstractScriptExtensionProvider}
  */
-public abstract class ScriptDisposalAwareScriptExtensionProvider
-        implements ScriptExtensionProvider, ScriptDisposalAware {
-    private Map<String, Function<String, Object>> types;
-    private Map<String, Map<String, Object>> idToTypes = new ConcurrentHashMap<>();
-
-    protected abstract String getPresetName();
-
-    protected abstract void initializeTypes(final BundleContext context);
-
-    protected void addType(String name, Function<String, Object> value) {
-        types.put(name, value);
-    }
-
-    @Activate
-    public void activate(final BundleContext context) {
-        types = new HashMap<>();
-        initializeTypes(context);
-    }
-
-    @Override
-    public Collection<String> getDefaultPresets() {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public Collection<String> getPresets() {
-        return Set.of(getPresetName());
-    }
-
-    @Override
-    public Collection<String> getTypes() {
-        return types.keySet();
-    }
-
-    @Override
-    public @Nullable Object get(String scriptIdentifier, String type) throws IllegalArgumentException {
-        Map<String, Object> forScript = idToTypes.computeIfAbsent(scriptIdentifier, k -> new HashMap<>());
-        return forScript.computeIfAbsent(type,
-                k -> Objects.nonNull(types.get(k)) ? types.get(k).apply(scriptIdentifier) : null);
-    }
-
-    @Override
-    public Map<String, Object> importPreset(String scriptIdentifier, String preset) {
-        if (getPresetName().equals(preset)) {
-            Map<String, Object> results = new HashMap<>(types.size());
-            for (String type : types.keySet()) {
-                results.put(type, get(scriptIdentifier, type));
-            }
-            return results;
-        }
-
-        return Collections.emptyMap();
-    }
+public abstract class ScriptDisposalAwareScriptExtensionProvider extends AbstractScriptExtensionProvider
+        implements ScriptDisposalAware {
 
     @Override
     public void unload(String scriptIdentifier) {

bundles/org.openhab.automation.jsscripting/src/main/resources/OH-INF/addon/addon.xml
@@ -5,7 +5,7 @@
 
 	<type>automation</type>
 	<name>JavaScript Scripting</name>
-	<description>This adds a JS (ECMAScript-2021) script engine.</description>
+	<description>This adds a JS (ECMAScript-2024) script engine.</description>
 	<connection>none</connection>
 
 	<service-id>org.openhab.jsscripting</service-id>

commit c92e7c620ad7abf4686ca45f23697055589886f3

[jsscripting] Fix bundled resources not found on windows
Regression from #18500.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.automation.jsscripting/src/main/java/org/openhab/automation/jsscripting/internal/OpenhabGraalJSScriptEngine.java
@@ -14,7 +14,6 @@ package org.openhab.automation.jsscripting.internal;
 
 import static org.openhab.core.automation.module.script.ScriptEngineFactory.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -77,9 +76,10 @@ public class OpenhabGraalJSScriptEngine
     private static final Source GLOBAL_SOURCE;
     static {
         try {
-            GLOBAL_SOURCE = Source.newBuilder("js",
-                    getFileAsReader(GraalJSScriptEngineFactory.NODE_DIR + File.separator + "@jsscripting-globals.js"),
-                    "@jsscripting-globals.js").cached(true).build();
+            GLOBAL_SOURCE = Source
+                    .newBuilder("js", getFileAsReader(GraalJSScriptEngineFactory.NODE_DIR + "/@jsscripting-globals.js"),
+                            "@jsscripting-globals.js")
+                    .cached(true).build();
         } catch (IOException e) {
             throw new IllegalStateException("Failed to load @jsscripting-globals.js", e);
         }
@@ -88,9 +88,10 @@ public class OpenhabGraalJSScriptEngine
     private static final Source OPENHAB_JS_SOURCE;
     static {
         try {
-            OPENHAB_JS_SOURCE = Source.newBuilder("js",
-                    getFileAsReader(GraalJSScriptEngineFactory.NODE_DIR + File.separator + "@openhab-globals.js"),
-                    "@openhab-globals.js").cached(true).build();
+            OPENHAB_JS_SOURCE = Source
+                    .newBuilder("js", getFileAsReader(GraalJSScriptEngineFactory.NODE_DIR + "/@openhab-globals.js"),
+                            "@openhab-globals.js")
+                    .cached(true).build();
         } catch (IOException e) {
             throw new IllegalStateException("Failed to load @openhab-globals.js", e);
         }