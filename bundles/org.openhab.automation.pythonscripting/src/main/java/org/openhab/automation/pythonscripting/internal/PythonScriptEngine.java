4ea61d38dd27428040788a7047c8610b4ea86679
Holger Hees <holger.hees@desy.de>
2025-05-10 19:25:01 +0200

    [pythonscripting] Fix python timedelta to java duration mapping (#18671)
    * fix python timedelta to java duration mapping
    
    * fix duration accuracy

--- bundles/org.openhab.automation.pythonscripting/src/main/java/org/openhab/automation/pythonscripting/internal/PythonScriptEngine.java
+++ bundles/org.openhab.automation.pythonscripting/src/main/java/org/openhab/automation/pythonscripting/internal/PythonScriptEngine.java
@@ -121,7 +121,7 @@ public class PythonScriptEngine
             .targetTypeMapping(Value.class, Duration.class,
                     // picking two members to check as Duration has many common function names
                     v -> v.hasMember("total_seconds") && v.hasMember("total_seconds"),
-                    v -> Duration.ofNanos(v.invokeMember("total_seconds").asLong() * 10000000),
+                    v -> Duration.ofNanos(Math.round(v.invokeMember("total_seconds").asDouble() * 1000000000)),
                     HostAccess.TargetMappingPrecedence.LOW)
 
             // Translate python item to org.openhab.core.items.Item
