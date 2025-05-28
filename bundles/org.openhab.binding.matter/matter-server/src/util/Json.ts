649552572e6c6158b84288ea18eab812a11dc961
Dan Cunningham <dan@digitaldan.com>
2025-05-28 16:07:23 -0700

    remove apache headers
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

--- bundles/org.openhab.binding.matter/matter-server/src/util/Json.ts
+++ bundles/org.openhab.binding.matter/matter-server/src/util/Json.ts
@@ -1,9 +1,3 @@
-/**
- * @license
- * Copyright 2022-2024 Matter.js Authors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import { Bytes } from "@matter/general";
 import { ValueModel } from "@matter/model";
 import { ValidationDatatypeMismatchError } from "@matter/types";
--- bundles/org.openhab.binding.matter/matter-server/src/util/String.ts
+++ bundles/org.openhab.binding.matter/matter-server/src/util/String.ts
@@ -1,9 +1,3 @@
-/**
- * @license
- * Copyright 2022-2024 Matter.js Authors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 export function camelize(str: string) {
     return str.charAt(0).toLowerCase() + str.slice(1);
 }
