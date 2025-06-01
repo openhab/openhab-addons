d5f88f3afcbededa090dae26d36a09730557f3d7
Laurent ARNAL <laurent@clae.net>
2025-06-01 13:21:54 +0200

    add meter type decoding
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

---
+++ bundles/org.openhab.binding.teleinfo/src/main/java/org/openhab/binding/teleinfo/internal/types/MeterType.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.teleinfo.internal.types;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Define the evolution option values
+ *
+ * @author Laurent Arnal - Initial contribution
+ *
+ */
+@NonNullByDefault
+public enum MeterType {
+    NOATTRIB(0, "NoAttrib"),
+    BLEUE(1, "Blue"),
+    JAUNE(2, "Yellow"),
+    LINKY(3, "Linky"),
+    PRISME(4, "Prisme"),
+    OTHER(5, "Other");
+
+    private final int id;
+    private final String label;
+
+    MeterType(int id, String label) {
+        this.id = id;
+        this.label = label;
+    }
+
+    public final int getId() {
+        return id;
+    }
+
+    public final String getLabel() {
+        return label;
+    }
+}
