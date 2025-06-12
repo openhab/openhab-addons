969ddecff3ed25d8a7072b6bb90e6e06e12f527f
Paul Smedley <paul@smedley.id.au>
2025-06-12 17:22:51 +0930

    Initial framework
    Signed-off-by: Paul Smedley <paul@smedley.id.au>

---
+++ bundles/org.openhab.binding.roborock/src/main/java/org/openhab/binding/roborock/internal/roborockHandlerFactory.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.roborock.internal;
+
+import static org.openhab.binding.roborock.internal.roborockBindingConstants.*;
+
+import java.util.Set;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.thing.ThingTypeUID;
+import org.openhab.core.thing.binding.BaseThingHandlerFactory;
+import org.openhab.core.thing.binding.ThingHandler;
+import org.openhab.core.thing.binding.ThingHandlerFactory;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * The {@link roborockHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author paul@smedley.id.au - Initial contribution
+ */
+@NonNullByDefault
+@Component(configurationPid = "binding.roborock", service = ThingHandlerFactory.class)
+public class roborockHandlerFactory extends BaseThingHandlerFactory {
+
+    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Set.of(THING_TYPE_SAMPLE);
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
+
+    @Override
+    protected @Nullable ThingHandler createHandler(Thing thing) {
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (THING_TYPE_SAMPLE.equals(thingTypeUID)) {
+            return new roborockHandler(thing);
+        }
+
+        return null;
+    }
+}
