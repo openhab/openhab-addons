d9d8354d7acfea2dc0b68edd74ee2034a37f0a3f
Paul Smedley <paul@smedley.id.au>
2025-06-20 15:47:43 +0930

    Add new files
    Signed-off-by: Paul Smedley <paul@smedley.id.au>

---
+++ bundles/org.openhab.binding.roborock/src/main/java/org/openhab/binding/roborock/internal/util/HashUtil.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.roborock.internal.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Johannes Ptaszyk - Initial contribution
+ */
+@NonNullByDefault
+public class HashUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HashUtil.class);
+
+    private HashUtil() {
+        // Prevent instantiation of util class
+    }
+
+    public static String md5Hex(String data) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            // should never occur
+            throw new RuntimeException(e);
+        }
+        byte[] array = md.digest(data.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (byte b : array) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
