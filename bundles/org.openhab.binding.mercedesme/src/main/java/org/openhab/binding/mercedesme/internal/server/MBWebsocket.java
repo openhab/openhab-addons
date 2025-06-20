15f3d04a48fddc744aa1a9bd0d39124bacc23138
Bernd Weymann <bernd.weymann@gmail.com>
2025-06-20 14:30:58 +0200

    remove waiting loop from websocket
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

--- bundles/org.openhab.binding.mercedesme/src/main/java/org/openhab/binding/mercedesme/internal/server/MBWebsocket.java
+++ bundles/org.openhab.binding.mercedesme/src/main/java/org/openhab/binding/mercedesme/internal/server/MBWebsocket.java
@@ -25,8 +25,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
@@ -39,6 +43,8 @@ import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
 import org.openhab.binding.mercedesme.internal.Constants;
 import org.openhab.binding.mercedesme.internal.handler.AccountHandler;
+import org.openhab.binding.mercedesme.internal.server.MBWebsocket.WebsocketState;
+import org.openhab.core.common.ThreadPoolManager;
 import org.openhab.core.thing.ThingStatus;
 import org.openhab.core.thing.ThingStatusDetail;
 import org.slf4j.LoggerFactory;
@@ -62,100 +68,119 @@ public class MBWebsocket {
     // addon time of 1 minute for a new send command
     private static final int ADDON_MESSAGE_TIME_MS = 60 * 1000;
     // check Socket time elapsed each second
-    private static final int CHECK_INTERVAL_MS = 1000;
+    private static final int CHECK_INTERVAL_MS = 60 * 1000;
     // additional 5 minutes after keep alive
     private static final int KEEP_ALIVE_ADDON = 5 * 60 * 1000;
 
     private final Logger logger = LoggerFactory.getLogger(MBWebsocket.class);
     private final Map<String, Instant> pingPongMap = new HashMap<>();
+    private final AccountHandler accountHandler;
+    private final HttpClient httpClient;
+    private final ScheduledExecutorService scheduler = ThreadPoolManager
+            .getPoolBasedSequentialScheduledExecutorService("mercedesme-websocket", null);
 
-    private List<ClientMessage> commandQueue = new ArrayList<>();
+    private Optional<ScheduledFuture<?>> refresher = Optional.empty();
+    private Optional<WebSocketClient> webSocketClient = Optional.empty();
     private Optional<Session> session = Optional.empty();
+    private List<ClientMessage> commandQueue = new ArrayList<>();
     private List<File> fileDumps = new ArrayList<>();
-    private AccountHandler accountHandler;
-    private HttpClient httpClient;
-    private boolean running = false;
     private Instant runTill = Instant.now();
-
+    private WebsocketState state = WebsocketState.STOPPED;
     private boolean keepAlive = false;
+    private boolean disposed = true;
 
-    public MBWebsocket(AccountHandler ah, HttpClient hc) {
-        accountHandler = ah;
-        httpClient = hc;
+    public enum WebsocketState {
+        STOPPED,
+        DISCONNECTED,
+        CONNECTED,
+        STARTED
+    };
+
+    public MBWebsocket(AccountHandler accountHandler, HttpClient httpClient) {
+        this.accountHandler = accountHandler;
+        this.httpClient = httpClient;
     }
 
     /**
-     * Is called by
-     * - scheduler every 15 minutes
-     * - handler sending a command
-     * - handler requesting refresh
+     * Regular update call from AccountHandler to refresh data according to refreshInterval
      */
-    public void run() {
-        synchronized (this) {
-            if (running) {
-                return;
-            } else {
-                running = true;
-                runTill = Instant.now().plusMillis(WS_RUNTIME_MS);
+    public void update() {
+        scheduler.execute(this::doRefresh);
+    }
+
+    /**
+     * If proto update is received an acknowledge message needs to be sent to the server to avoid repeating this message
+     * again and again.
+     */
+    public void sendAcknowledgeMessage(ClientMessage message) {
+        session.ifPresent(s -> {
+            try {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                message.writeTo(baos);
+                s.getRemote().sendBytes(ByteBuffer.wrap(baos.toByteArray()));
+            } catch (IOException e) {
+                logger.warn("Error sending acknowledge {} : {}", message.getAllFields(), e.getMessage());
             }
-        }
-        WebSocketClient client = new WebSocketClient(httpClient);
-        try {
-            client.setMaxIdleTimeout(CONNECT_TIMEOUT_MS);
-            ClientUpgradeRequest request = accountHandler.getClientUpgradeRequest();
-            String websocketURL = accountHandler.getWSUri();
-            if (Constants.JUNIT_TOKEN.equals(request.getHeader("Authorization"))) {
-                // avoid unit test requesting real web socket - simply return
-                return;
-            }
-            logger.trace("Websocket start {} max message size {}", websocketURL, client.getMaxBinaryMessageSize());
-            client.start();
-            client.connect(this, new URI(websocketURL), request);
-            Instant nextPing = Instant.now().plusMillis(60 * 1000);
-            while (keepAlive || Instant.now().isBefore(runTill)) {
-                try {
-                    Thread.sleep(CHECK_INTERVAL_MS);
-                } catch (InterruptedException ie) {
-                    logger.trace("Websocket interrupted during sleeping - stop executing");
-                    Thread.currentThread().interrupt();
-                    runTill = Instant.MIN;
-                }
-                // sends one message per second
-                if (sendMessage()) {
-                    // add additional runtime to execute and finish command
-                    runTill = runTill.plusMillis(ADDON_MESSAGE_TIME_MS);
-                }
-                // send
-                if (Instant.now().isAfter(nextPing)) {
-                    // ping every minute
-                    ping();
-                    nextPing = Instant.now().plusMillis(60 * 1000);
-                }
-            }
-            logger.trace("Websocket stop");
-            if (session.isPresent()) {
-                // close session normally and wait till next update cycle
-                session.get().close(1000, "Websocket closed by binding");
-            } else {
-                client.stop();
-                client.destroy();
-            }
-        } catch (Throwable t) {
-            // catch Exceptions of start stop and declare communication error
-            accountHandler.updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
-                    "@text/mercedesme.account.status.websocket-failure");
-            logger.warn("Websocket handling exception: {}", t.getMessage());
-        } finally {
-            synchronized (this) {
-                running = false;
-            }
+        });
+    }
+
+    /**
+     * Add command to the command queue to be sent to the server. Immediately request refresh to send command as soon as
+     * possible.
+     *
+     * @param command to be sent
+     */
+    public void addCommand(ClientMessage command) {
+        commandQueue.add(command);
+        scheduler.execute(this::doRefresh);
+    }
+
+    /**
+     * Dispose websocket in case of disposed AccountHandler. Cleanup stored files and stop web socket client.
+     */
+    public void dispose(boolean disposed) {
+        this.disposed = disposed;
+        if (disposed) {
+            runTill = Instant.MIN;
+            keepAlive = false;
+            fileDumps.forEach(file -> {
+                file.delete();
+            });
+            fileDumps.clear();
+            refresher.ifPresent(job -> {
+                job.cancel(false);
+                refresher = Optional.empty();
+            });
+            scheduler.execute(this::stop);
         }
     }
 
-    public void setCommand(ClientMessage cm) {
-        commandQueue.add(cm);
+    /**
+     * Set keep alive mode for web socket connection. If keep alive is set to true the web socket will not be closed
+     *
+     * @param alive
+     */
+    public void keepAlive(boolean alive) {
+        if (!keepAlive) {
+            if (alive) {
+                logger.trace("WebSocket - keep alive start");
+            }
+        } else {
+            if (!alive) {
+                // after keep alive is finished add 5 minutes to cover e.g. door events after
+                // trip is finished
+                runTill = Instant.now().plusMillis(KEEP_ALIVE_ADDON);
+                logger.trace("Websocket - keep alive stop - run till {}", runTill.toString());
+            }
+        }
+        keepAlive = alive;
     }
 
+    /**
+     * Send message to the server if there is a command available in the command queue.
+     *
+     * @return true if command is successfully submitted, false otherwise
+     */
     private boolean sendMessage() {
         if (!commandQueue.isEmpty()) {
             ClientMessage message = commandQueue.remove(0);
@@ -178,49 +203,110 @@ public class MBWebsocket {
         return false;
     }
 
-    public void sendAcknowledgeMessage(ClientMessage message) {
-        session.ifPresent(s -> {
+    /**
+     * Start the web socket client and connect to the server. If the web socket client is already running it will not
+     */
+    private void start() {
+        if (!disposed && webSocketClient.isEmpty()) {
+            WebSocketClient client = new WebSocketClient(httpClient);
             try {
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                message.writeTo(baos);
-                s.getRemote().sendBytes(ByteBuffer.wrap(baos.toByteArray()));
-            } catch (IOException e) {
-                logger.warn("Error sending acknowledge {} : {}", message.getAllFields(), e.getMessage());
+                client.setMaxIdleTimeout(CONNECT_TIMEOUT_MS);
+                ClientUpgradeRequest request = accountHandler.getClientUpgradeRequest();
+                String websocketURL = accountHandler.getWSUri();
+                if (Constants.JUNIT_TOKEN.equals(request.getHeader("Authorization"))) {
+                    // avoid unit test requesting real web socket - simply return
+                    return;
+                }
+                logger.trace("Websocket start {} max message size {}", websocketURL, client.getMaxBinaryMessageSize());
+                client.start();
+                client.connect(this, new URI(websocketURL), request);
+                webSocketClient = Optional.of(client);
+                runTill = Instant.now().plusMillis(WS_RUNTIME_MS);
+                state = WebsocketState.STARTED;
+            } catch (Exception e) {
+                // catch Exceptions of start stop and declare communication error
+                accountHandler.updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
+                        "@text/mercedesme.account.status.websocket-failure");
+                logger.warn("Websocket handling exception: {}", e.getMessage());
             }
-        });
-    }
-
-    public void interrupt() {
-        synchronized (this) {
-            runTill = Instant.MIN;
-            keepAlive = false;
         }
     }
 
     /**
-     * If disposed temp debug files are deleted
+     * Performs an update of the web socket connection. If websocket is disposed refresh will not be executed. In case
+     * of CONNECTED it will check
+     * - if there are commands to be sent
+     * - send ping to the server
+     * - check if keep alive is set or run time is not over
+     * In case of other state it will start the web socket connection.
      */
-    public void dispose() {
-        interrupt();
-    }
-
-    public void keepAlive(boolean b) {
-        if (!keepAlive) {
-            if (b) {
-                logger.trace("WebSocket - keep alive start");
+    private void doRefresh() {
+        if (!disposed) {
+            if (state == WebsocketState.CONNECTED) {
+                logger.trace("Refresh: Websocket fine - state {}", state);
+                if (sendMessage()) {
+                    // add additional runtime to execute and finish command
+                    runTill = runTill.plusMillis(ADDON_MESSAGE_TIME_MS);
+                }
+                ping();
+                if (keepAlive || Instant.now().isBefore(runTill)) {
+                    // doRefresh is called by AccountHandler, websocket endpoint onConnect and addCommand. To avoid
+                    // multiple future calls cancel the current running or future schedule calls.
+                    refresher.ifPresent(job -> {
+                        job.cancel(false);
+                    });
+                    refresher = Optional
+                            .of(scheduler.schedule(this::doRefresh, CHECK_INTERVAL_MS, TimeUnit.MILLISECONDS));
+                } else {
+                    // run time is over - disconnect
+                    logger.debug("Websocket run time is over - disconnect");
+                    scheduler.execute(this::stop);
+                }
+            } else {
+                logger.trace("Refresh: Websocket needs to be started - state {}", state);
+                scheduler.execute(this::start);
             }
         } else {
-            if (!b) {
-                // after keep alive is finished add 5 minutes to cover e.g. door events after
-                // trip is finished
-                runTill = Instant.now().plusMillis(KEEP_ALIVE_ADDON);
-                logger.trace("Websocket - keep alive stop - run till {}", runTill.toString());
-            }
+            logger.trace("Refresh: Websocket disposed - state {}", state);
         }
-        keepAlive = b;
     }
 
-    public void ping() {
+    /**
+     * Request to disconnect the web socket session. This will close the session normally with a status code of 1000
+     */
+    private void disconnect() {
+        session.ifPresent(session -> {
+            // close session normally
+            session.close(1000, "Websocket closed by binding");
+        });
+    }
+
+    /**
+     * Stop the web socket client and disconnect the session if it is still connected.
+     */
+    private void stop() {
+        session.ifPresentOrElse(session -> {
+            logger.trace("Websocket stop - disconnect session first - state {}", state);
+            scheduler.execute(this::disconnect);
+        }, () -> {
+            logger.trace("Websocket stop - state {}", state);
+            webSocketClient.ifPresent(client -> {
+                try {
+                    client.stop();
+                } catch (Exception e) {
+                    logger.warn("Websocket stop exception: {}", e.getMessage());
+                }
+                client.destroy();
+                webSocketClient = Optional.empty();
+            });
+            state = WebsocketState.STOPPED;
+        });
+    }
+
+    /**
+     * Ping the server to keep the connection alive and to check if the connection is still valid.
+     */
+    private void ping() {
         logger.trace("Websocket ping {}", Instant.now().toString());
         session.ifPresent(session -> {
             try {
@@ -234,7 +320,7 @@ public class MBWebsocket {
     }
 
     /**
-     * endpoints
+     * web socket endpoints
      */
 
     @OnWebSocketMessage
@@ -295,27 +381,37 @@ public class MBWebsocket {
         }
     }
 
-    @OnWebSocketClose
-    public void onDisconnect(Session session, int statusCode, String reason) {
-        pingPongMap.clear();
-        this.session = Optional.empty();
-        logger.debug("Disconnected from server. Status {} Reason {}", statusCode, reason);
-        // ensure execution stop if disconnect was triggered from server side
-        interrupt();
-    }
-
     @OnWebSocketConnect
     public void onConnect(Session session) {
-        pingPongMap.clear();
         this.session = Optional.of(session);
+        state = WebsocketState.CONNECTED;
+        pingPongMap.clear();
         accountHandler.updateStatus(ThingStatus.ONLINE);
+        logger.trace("Websocket connected - state {}", state);
+        // websocket client is started and connected - time to refresh
+        scheduler.execute(this::doRefresh);
+    }
+
+    @OnWebSocketClose
+    public void onDisconnect(Session session, int statusCode, String reason) {
+        onClosedSession(null);
+        logger.trace("Disconnected from server. Status {} Reason {}", statusCode, reason);
     }
 
     @OnWebSocketError
     public void onError(Throwable t) {
-        logger.debug("Error during web socket connection - {}", t.getMessage());
+        onClosedSession(t);
+    }
+
+    private void onClosedSession(@Nullable Throwable throwable) {
+        this.session = Optional.empty();
+        state = WebsocketState.DISCONNECTED;
         pingPongMap.clear();
-        accountHandler.updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
-                "@text/mercedesme.account.status.websocket-failure [\"" + t.getMessage() + "\"]");
+        if (throwable != null) {
+            accountHandler.updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
+                    "@text/mercedesme.account.status.websocket-failure [\"" + throwable.getMessage() + "\"]");
+        }
+        // stop web socket client for closed session
+        scheduler.execute(this::stop);
     }
 }
