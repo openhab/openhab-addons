6d2e9a064cb5808a238c29fa7e355fcb565e9a38
Laurent ARNAL <laurent@clae.net>
2025-06-26 15:59:01 +0200

    implements paging and lazy loading on MediaCollection (for now only test / implements on upnp Binding)
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.binding.upnpcontrol/src/main/java/org/openhab/binding/upnpcontrol/internal/handler/UpnpRendererHandler.java
+++ bundles/org.openhab.binding.upnpcontrol/src/main/java/org/openhab/binding/upnpcontrol/internal/handler/UpnpRendererHandler.java
@@ -804,7 +804,7 @@ public class UpnpRendererHandler extends UpnpHandler {
                     }
 
                     serverHandler.serveMedia();
-                    // pause();
+                    pause();
                     play();
                     udpateControlState(PlayPauseType.PLAY);
                 }
