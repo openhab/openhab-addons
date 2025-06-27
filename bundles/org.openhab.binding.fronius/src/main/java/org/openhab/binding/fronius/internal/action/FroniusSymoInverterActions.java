b3e5c977693ddcdd25643e552ef4a0352e578d70
Florian Hotze <dev@florianhotze.com>
2025-06-27 12:01:14 +0200

    [fronius] Add Thing actions to prevent battery from charging & Refactorings (#18846)
    * [fronius] Refactor battery control actions to reduce code redundancy
    
    Signed-off-by: Florian Hotze <dev@florianhotze.com>
    Signed-off-by: Paul Smedley <paul@smedley.id.au>

--- bundles/org.openhab.binding.fronius/src/main/java/org/openhab/binding/fronius/internal/action/FroniusSymoInverterActions.java
+++ bundles/org.openhab.binding.fronius/src/main/java/org/openhab/binding/fronius/internal/action/FroniusSymoInverterActions.java
@@ -19,6 +19,9 @@ import javax.measure.quantity.Power;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.fronius.internal.api.FroniusBatteryControl;
+import org.openhab.binding.fronius.internal.api.FroniusCommunicationException;
+import org.openhab.binding.fronius.internal.api.FroniusUnauthorizedException;
 import org.openhab.binding.fronius.internal.handler.FroniusSymoInverterHandler;
 import org.openhab.core.automation.annotation.ActionInput;
 import org.openhab.core.automation.annotation.ActionOutput;
@@ -30,6 +33,8 @@ import org.openhab.core.thing.binding.ThingActionsScope;
 import org.openhab.core.thing.binding.ThingHandler;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Implementation of the {@link ThingActions} interface used for controlling battery charging and discharging for
@@ -41,6 +46,7 @@ import org.osgi.service.component.annotations.ServiceScope;
 @ThingActionsScope(name = "fronius")
 @NonNullByDefault
 public class FroniusSymoInverterActions implements ThingActions {
+    private final Logger logger = LoggerFactory.getLogger(FroniusSymoInverterActions.class);
     private @Nullable FroniusSymoInverterHandler handler;
 
     public static boolean resetBatteryControl(ThingActions actions) {
@@ -98,6 +104,34 @@ public class FroniusSymoInverterActions implements ThingActions {
         return addForcedBatteryChargingSchedule(actions, from.toLocalTime(), until.toLocalTime(), power);
     }
 
+    public static boolean preventBatteryCharging(ThingActions actions) {
+        if (actions instanceof FroniusSymoInverterActions froniusSymoInverterActions) {
+            return froniusSymoInverterActions.preventBatteryCharging();
+        } else {
+            throw new IllegalArgumentException(
+                    "The 'actions' argument is not an instance of FroniusSymoInverterActions");
+        }
+    }
+
+    public static boolean addPreventBatteryChargingSchedule(ThingActions actions, LocalTime from, LocalTime until) {
+        if (actions instanceof FroniusSymoInverterActions froniusSymoInverterActions) {
+            return froniusSymoInverterActions.addPreventBatteryChargingSchedule(from, until);
+        } else {
+            throw new IllegalArgumentException(
+                    "The 'actions' argument is not an instance of FroniusSymoInverterActions");
+        }
+    }
+
+    public static boolean addPreventBatteryChargingSchedule(ThingActions actions, ZonedDateTime from,
+            ZonedDateTime until) {
+        if (actions instanceof FroniusSymoInverterActions froniusSymoInverterActions) {
+            return froniusSymoInverterActions.addPreventBatteryChargingSchedule(from, until);
+        } else {
+            throw new IllegalArgumentException(
+                    "The 'actions' argument is not an instance of FroniusSymoInverterActions");
+        }
+    }
+
     public static boolean setBackupReservedBatteryCapacity(ThingActions actions, int percent) {
         if (actions instanceof FroniusSymoInverterActions froniusSymoInverterActions) {
             return froniusSymoInverterActions.setBackupReservedBatteryCapacity(percent);
@@ -128,18 +162,24 @@ public class FroniusSymoInverterActions implements ThingActions {
 
     @RuleAction(label = "@text/actions.reset-battery-control.label", description = "@text/actions.reset-battery-control.description")
     public @ActionOutput(type = "boolean", label = "Success") boolean resetBatteryControl() {
-        FroniusSymoInverterHandler handler = this.handler;
-        if (handler != null) {
-            return handler.resetBatteryControl();
+        FroniusBatteryControl batteryControl = getBatteryControl();
+        if (batteryControl != null) {
+            return executeBatteryControlAction(() -> {
+                batteryControl.reset();
+                return true;
+            });
         }
         return false;
     }
 
     @RuleAction(label = "@text/actions.hold-battery-charge.label", description = "@text/actions.hold-battery-charge.description")
     public @ActionOutput(type = "boolean", label = "Success") boolean holdBatteryCharge() {
-        FroniusSymoInverterHandler handler = this.handler;
-        if (handler != null) {
-            return handler.holdBatteryCharge();
+        FroniusBatteryControl batteryControl = getBatteryControl();
+        if (batteryControl != null) {
+            return executeBatteryControlAction(() -> {
+                batteryControl.holdBatteryCharge();
+                return true;
+            });
         }
         return false;
     }
@@ -148,9 +188,12 @@ public class FroniusSymoInverterActions implements ThingActions {
     public @ActionOutput(type = "boolean", label = "Success") boolean addHoldBatteryChargeSchedule(
             @ActionInput(name = "from", label = "@text/actions.from.label", description = "@text/actions.from.description", required = true) LocalTime from,
             @ActionInput(name = "until", label = "@text/actions.until.label", description = "@text/actions.until.description", required = true) LocalTime until) {
-        FroniusSymoInverterHandler handler = this.handler;
-        if (handler != null) {
-            return handler.addHoldBatteryChargeSchedule(from, until);
+        FroniusBatteryControl batteryControl = getBatteryControl();
+        if (batteryControl != null) {
+            return executeBatteryControlAction(() -> {
+                batteryControl.addHoldBatteryChargeSchedule(from, until);
+                return true;
+            });
         }
         return false;
     }
@@ -162,9 +205,12 @@ public class FroniusSymoInverterActions implements ThingActions {
     @RuleAction(label = "@text/actions.force-battery-charging.label", description = "@text/actions.force-battery-charging.description")
     public @ActionOutput(type = "boolean", label = "Success") boolean forceBatteryCharging(
             @ActionInput(name = "power", label = "@text/actions.power.label", description = "@text/actions.power.label", type = "QuantityType<Power>", required = true) QuantityType<Power> power) {
-        FroniusSymoInverterHandler handler = this.handler;
-        if (handler != null) {
-            return handler.forceBatteryCharging(power);
+        FroniusBatteryControl batteryControl = getBatteryControl();
+        if (batteryControl != null) {
+            return executeBatteryControlAction(() -> {
+                batteryControl.forceBatteryCharging(power);
+                return true;
+            });
         }
         return false;
     }
@@ -174,9 +220,12 @@ public class FroniusSymoInverterActions implements ThingActions {
             @ActionInput(name = "from", label = "@text/actions.from.label", description = "@text/actions.from.description", required = true) LocalTime from,
             @ActionInput(name = "until", label = "@text/actions.until.label", description = "@text/actions.until.description", required = true) LocalTime until,
             @ActionInput(name = "power", label = "@text/actions.power.label", description = "@text/actions.power.label", type = "QuantityType<Power>", required = true) QuantityType<Power> power) {
-        FroniusSymoInverterHandler handler = this.handler;
-        if (handler != null) {
-            return handler.addForcedBatteryChargingSchedule(from, until, power);
+        FroniusBatteryControl batteryControl = getBatteryControl();
+        if (batteryControl != null) {
+            return executeBatteryControlAction(() -> {
+                batteryControl.addForcedBatteryChargingSchedule(from, until, power);
+                return true;
+            });
         }
         return false;
     }
@@ -186,17 +235,85 @@ public class FroniusSymoInverterActions implements ThingActions {
         return addForcedBatteryChargingSchedule(from.toLocalTime(), until.toLocalTime(), power);
     }
 
+    @RuleAction(label = "@text/actions.prevent-battery-charging.label", description = "@text/actions.prevent-battery-charging.description")
+    public @ActionOutput(type = "boolean", label = "Success") boolean preventBatteryCharging() {
+        FroniusBatteryControl batteryControl = getBatteryControl();
+        if (batteryControl != null) {
+            return executeBatteryControlAction(() -> {
+                batteryControl.preventBatteryCharging();
+                return true;
+            });
+        }
+        return false;
+    }
+
+    @RuleAction(label = "@text/actions.add-prevent-battery-charging-schedule.label", description = "@text/actions.add-prevent-battery-charging-schedule.description")
+    public @ActionOutput(type = "boolean", label = "Success") boolean addPreventBatteryChargingSchedule(
+            @ActionInput(name = "from", label = "@text/actions.from.label", description = "@text/actions.from.description", required = true) LocalTime from,
+            @ActionInput(name = "until", label = "@text/actions.until.label", description = "@text/actions.until.description", required = true) LocalTime until) {
+        FroniusBatteryControl batteryControl = getBatteryControl();
+        if (batteryControl != null) {
+            return executeBatteryControlAction(() -> {
+                batteryControl.addPreventBatteryChargingSchedule(from, until);
+                return true;
+            });
+        }
+        return false;
+    }
+
+    public boolean addPreventBatteryChargingSchedule(ZonedDateTime from, ZonedDateTime until) {
+        return addPreventBatteryChargingSchedule(from.toLocalTime(), until.toLocalTime());
+    }
+
     @RuleAction(label = "@text/actions.backup-reserved-battery-capacity.label", description = "@text/actions.backup-reserved-battery-capacity.description")
     public @ActionOutput(type = "boolean", label = "Success") boolean setBackupReservedBatteryCapacity(
             @ActionInput(name = "percent", label = "@text/actions.soc.label", description = "@text/actions.soc.description", required = true) int percent) {
-        FroniusSymoInverterHandler handler = this.handler;
-        if (handler != null) {
-            return handler.setBackupReservedBatteryCapacity(percent);
+        FroniusBatteryControl batteryControl = getBatteryControl();
+        if (batteryControl != null) {
+            return executeBatteryControlAction(() -> {
+                try {
+                    batteryControl.setBackupReservedCapacity(percent);
+                } catch (IllegalArgumentException e) {
+                    logger.warn("Failed to set backup reserved battery capacity: {}", e.getMessage());
+                    return false;
+                }
+                return true;
+            });
         }
         return false;
     }
 
     public boolean setBackupReservedBatteryCapacity(PercentType percent) {
         return setBackupReservedBatteryCapacity(percent.intValue());
+    }
+
+    private @Nullable FroniusBatteryControl getBatteryControl() {
+        FroniusSymoInverterHandler handler = this.handler;
+        if (handler != null) {
+            return handler.getBatteryControl();
+        }
+        return null;
+    }
+
+    @FunctionalInterface
+    public interface ExceptionalSupplier<T> {
+        T get() throws FroniusCommunicationException, FroniusUnauthorizedException;
+    }
+
+    /**
+     * Executes a battery control action provided through a {@link ExceptionalSupplier} and handles common exceptions.
+     * 
+     * @param supplier the action to execute
+     * @return the result of the action
+     */
+    private boolean executeBatteryControlAction(ExceptionalSupplier<Boolean> supplier) {
+        try {
+            return supplier.get();
+        } catch (FroniusCommunicationException e) {
+            logger.warn("Failed to execute battery control action", e);
+        } catch (FroniusUnauthorizedException e) {
+            logger.warn("Failed to execute battery control action: Invalid username or password");
+        }
+        return false;
     }
 }
