7de646ee40ad666942820ec8c2a4c261042b2190
Jacob Laursen <jacob-github@vindvejr.dk>
2025-07-08 15:48:02 +0200

    [hue] Fix `HueBridgeNupnpDiscoveryOSGITest` (#18911)
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>
    Signed-off-by: Scott Hanson <scooter_seh@yahoo.com>

--- itests/org.openhab.binding.hue.tests/src/main/java/org/openhab/binding/hue/internal/discovery/HueBridgeNupnpDiscoveryOSGITest.java
+++ itests/org.openhab.binding.hue.tests/src/main/java/org/openhab/binding/hue/internal/discovery/HueBridgeNupnpDiscoveryOSGITest.java
@@ -217,12 +217,6 @@ public class HueBridgeNupnpDiscoveryOSGITest extends JavaOSGiTest {
                     @Nullable Collection<ThingTypeUID> thingTypeUIDs, @Nullable ThingUID bridgeUID) {
                 return null;
             }
-
-            @Override
-            public @Nullable Collection<ThingUID> removeOlderResults(DiscoveryService source, long timestamp,
-                    @Nullable Collection<ThingTypeUID> thingTypeUIDs, @Nullable ThingUID bridgeUID) {
-                return null;
-            }
         });
 
         // missing ip
