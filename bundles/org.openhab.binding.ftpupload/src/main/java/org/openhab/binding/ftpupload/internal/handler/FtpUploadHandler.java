7e5c28f6ec6717f34f3fd427027da449ca1cc8eb
Leo Siepel <leosiepel@gmail.com>
2025-07-20 00:43:06 +0200

    ftpupload
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.ftpupload/src/main/java/org/openhab/binding/ftpupload/internal/handler/FtpUploadHandler.java
+++ bundles/org.openhab.binding.ftpupload/src/main/java/org/openhab/binding/ftpupload/internal/handler/FtpUploadHandler.java
@@ -67,6 +67,11 @@ public class FtpUploadHandler extends BaseThingHandler implements FtpServerEvent
         configuration = getConfigAs(FtpUploadConfig.class);
         logger.debug("Using configuration: {}", configuration.toString());
 
+        if (configuration.userName.isBlank() || configuration.password.isBlank()) {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
+                    "Username and password must be configured");
+            return;
+        }
         ftpServer.addEventListener(this);
         try {
             ftpServer.addAuthenticationCredentials(configuration.userName, configuration.password);
