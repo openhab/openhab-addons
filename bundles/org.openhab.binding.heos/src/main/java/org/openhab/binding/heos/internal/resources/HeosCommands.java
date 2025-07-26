2ed930a6d01596a343d82d50f667b284fd2a878e
lsiepel <leosiepel@gmail.com>
2025-07-26 16:14:58 +0200

    [various] Fix ConstantNameCheck (#18992)
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.heos/src/main/java/org/openhab/binding/heos/internal/resources/HeosCommands.java
+++ bundles/org.openhab.binding.heos/src/main/java/org/openhab/binding/heos/internal/resources/HeosCommands.java
@@ -30,58 +30,58 @@ public class HeosCommands {
     private static final String REGISTER_CHANGE_EVENT_ON = "heos://system/register_for_change_events?enable=on";
     private static final String REGISTER_CHANGE_EVENT_OFF = "heos://system/register_for_change_events?enable=off";
     private static final String HEOS_ACCOUNT_CHECK = "heos://system/check_account";
-    private static final String prettifyJSONon = "heos://system/prettify_json_response?enable=on";
-    private static final String prettifyJSONoff = "heos://system/prettify_json_response?enable=off";
-    private static final String rebootSystem = "heos://system/reboot";
-    private static final String signOut = "heos://system/sign_out";
-    private static final String heartbeat = "heos://system/heart_beat";
+    private static final String PRETTIFY_JSON_ON = "heos://system/prettify_json_response?enable=on";
+    private static final String PRETTIFY_JSON_OFF = "heos://system/prettify_json_response?enable=off";
+    private static final String REBOOT_SYSTEM = "heos://system/reboot";
+    private static final String SIGN_OUT = "heos://system/sign_out";
+    private static final String HEARTBEAT = "heos://system/heart_beat";
 
     // Player Commands Control
-    private static final String setPlayStatePlay = "heos://player/set_play_state?pid=";
-    private static final String setPlayStatePause = "heos://player/set_play_state?pid=";
-    private static final String setPlayStateStop = "heos://player/set_play_state?pid=";
-    private static final String setVolume = "heos://player/set_volume?pid=";
-    private static final String volumeUp = "heos://player/volume_up?pid=";
-    private static final String volumeDown = "heos://player/volume_down?pid=";
-    private static final String setMute = "heos://player/set_mute?pid=";
-    private static final String setMuteToggle = "heos://player/toggle_mute?pid=";
-    private static final String playNext = "heos://player/play_next?pid=";
-    private static final String playPrevious = "heos://player/play_previous?pid=";
-    private static final String playQueueItem = "heos://player/play_queue?pid=";
-    private static final String clearQueue = "heos://player/clear_queue?pid=";
-    private static final String deleteQueueItem = "heos://player/remove_from_queue?pid=";
-    private static final String setPlayMode = "heos://player/set_play_mode?pid=";
+    private static final String SET_PLAY_STATE_PLAY = "heos://player/set_play_state?pid=";
+    private static final String SET_PLAY_STATE_PAUSE = "heos://player/set_play_state?pid=";
+    private static final String SET_PLAY_STATE_STOP = "heos://player/set_play_state?pid=";
+    private static final String SET_VOLUME = "heos://player/set_volume?pid=";
+    private static final String VOLUME_UP = "heos://player/volume_up?pid=";
+    private static final String VOLUME_DOWN = "heos://player/volume_down?pid=";
+    private static final String SET_MUTE = "heos://player/set_mute?pid=";
+    private static final String SET_MUTE_TOGGLE = "heos://player/toggle_mute?pid=";
+    private static final String PLAY_NEXT = "heos://player/play_next?pid=";
+    private static final String PLAY_PREVIOUS = "heos://player/play_previous?pid=";
+    private static final String PLAY_QUEUE_ITEM = "heos://player/play_queue?pid=";
+    private static final String CLEAR_QUEUE = "heos://player/clear_queue?pid=";
+    private static final String DELETE_QUEUE_ITEM = "heos://player/remove_from_queue?pid=";
+    private static final String SET_PLAY_MODE = "heos://player/set_play_mode?pid=";
 
     // Group Commands Control
-    private static final String getGroups = "heos://group/get_groups";
-    private static final String getGroupsInfo = "heos://group/get_group_info?gid=";
-    private static final String setGroup = "heos://group/set_group?pid=";
-    private static final String getGroupVolume = "heos://group/get_volume?gid=";
-    private static final String setGroupVolume = "heos://group/set_volume?gid=";
-    private static final String getGroupMute = "heos://group/get_mute?gid=";
-    private static final String setGroupMute = "heos://group/set_mute?gid=";
-    private static final String toggleGroupMute = "heos://group/toggle_mute?gid=";
-    private static final String groupVolumeUp = "heos://group/volume_up?gid=";
-    private static final String groupVolumeDown = "heos://group/volume_down?gid=";
+    private static final String GET_GROUPS = "heos://group/get_groups";
+    private static final String GET_GROUPS_INFO = "heos://group/get_group_info?gid=";
+    private static final String SET_GROUP = "heos://group/set_group?pid=";
+    private static final String GET_GROUP_VOLUME = "heos://group/get_volume?gid=";
+    private static final String SET_GROUP_VOLUME = "heos://group/set_volume?gid=";
+    private static final String GET_GROUP_MUTE = "heos://group/get_mute?gid=";
+    private static final String SET_GROUP_MUTE = "heos://group/set_mute?gid=";
+    private static final String TOGGLE_GROUP_MUTE = "heos://group/toggle_mute?gid=";
+    private static final String GROUP_VOLUME_UP = "heos://group/volume_up?gid=";
+    private static final String GROUP_VOLUME_DOWN = "heos://group/volume_down?gid=";
 
     // Player Commands get Information
 
-    private static final String getPlayers = "heos://player/get_players";
-    private static final String getPlayerInfo = "heos://player/get_player_info?pid=";
-    private static final String getPlayState = "heos://player/get_play_state?pid=";
-    private static final String getNowPlayingMedia = "heos://player/get_now_playing_media?pid=";
-    private static final String playerGetVolume = "heos://player/get_volume?pid=";
-    private static final String playerGetMute = "heos://player/get_mute?pid=";
-    private static final String getQueue = "heos://player/get_queue?pid=";
-    private static final String getPlayMode = "heos://player/get_play_mode?pid=";
+    private static final String GET_PLAYERS = "heos://player/get_players";
+    private static final String GET_PLAYER_INFO = "heos://player/get_player_info?pid=";
+    private static final String GET_PLAY_STATE = "heos://player/get_play_state?pid=";
+    private static final String GET_NOW_PLAYING_MEDIA = "heos://player/get_now_playing_media?pid=";
+    private static final String PLAYER_GET_VOLUME = "heos://player/get_volume?pid=";
+    private static final String PLAYER_GET_MUTE = "heos://player/get_mute?pid=";
+    private static final String GET_QUEUE = "heos://player/get_queue?pid=";
+    private static final String GET_PLAY_MODE = "heos://player/get_play_mode?pid=";
 
     // Browse Commands
-    private static final String getMusicSources = "heos://browse/get_music_sources";
-    private static final String browseSource = "heos://browse/browse?sid=";
-    private static final String playStream = "heos://browse/play_stream?pid=";
-    private static final String addToQueue = "heos://browse/add_to_queue?pid=";
-    private static final String playInputSource = "heos://browse/play_input?pid=";
-    private static final String playURL = "heos://browse/play_stream?pid=";
+    private static final String GET_MUSIC_SOURCES = "heos://browse/get_music_sources";
+    private static final String BROWSE_SOURCE = "heos://browse/browse?sid=";
+    private static final String PLAY_STREAM = "heos://browse/play_stream?pid=";
+    private static final String ADD_TO_QUEUE = "heos://browse/add_to_queue?pid=";
+    private static final String PLAY_INPUT_SOURCE = "heos://browse/play_input?pid=";
+    private static final String PLAY_URL = "heos://browse/play_stream?pid=";
 
     public static String registerChangeEventOn() {
         return REGISTER_CHANGE_EVENT_ON;
@@ -96,99 +96,99 @@ public class HeosCommands {
     }
 
     public static String setPlayStatePlay(String pid) {
-        return setPlayStatePlay + pid + "&state=play";
+        return SET_PLAY_STATE_PLAY + pid + "&state=play";
     }
 
     public static String setPlayStatePause(String pid) {
-        return setPlayStatePause + pid + "&state=pause";
+        return SET_PLAY_STATE_PAUSE + pid + "&state=pause";
     }
 
     public static String setPlayStateStop(String pid) {
-        return setPlayStateStop + pid + "&state=stop";
+        return SET_PLAY_STATE_STOP + pid + "&state=stop";
     }
 
     public static String volumeUp(String pid) {
-        return volumeUp + pid + "&step=1";
+        return VOLUME_UP + pid + "&step=1";
     }
 
     public static String volumeDown(String pid) {
-        return volumeDown + pid + "&step=1";
+        return VOLUME_DOWN + pid + "&step=1";
     }
 
     public static String setMuteOn(String pid) {
-        return setMute + pid + "&state=on";
+        return SET_MUTE + pid + "&state=on";
     }
 
     public static String setMuteOff(String pid) {
-        return setMute + pid + "&state=off";
+        return SET_MUTE + pid + "&state=off";
     }
 
     public static String setMuteToggle(String pid) {
-        return setMuteToggle + pid + "&state=off";
+        return SET_MUTE_TOGGLE + pid + "&state=off";
     }
 
     public static String setShuffleMode(String pid, String shuffle) {
-        return setPlayMode + pid + "&shuffle=" + shuffle;
+        return SET_PLAY_MODE + pid + "&shuffle=" + shuffle;
     }
 
     public static String setRepeatMode(String pid, String repeat) {
-        return setPlayMode + pid + "&repeat=" + repeat;
+        return SET_PLAY_MODE + pid + "&repeat=" + repeat;
     }
 
     public static String getPlayMode(String pid) {
-        return getPlayMode + pid;
+        return GET_PLAY_MODE + pid;
     }
 
     public static String playNext(String pid) {
-        return playNext + pid;
+        return PLAY_NEXT + pid;
     }
 
     public static String playPrevious(String pid) {
-        return playPrevious + pid;
+        return PLAY_PREVIOUS + pid;
     }
 
     public static String setVolume(String vol, String pid) {
-        return setVolume + pid + "&level=" + vol;
+        return SET_VOLUME + pid + "&level=" + vol;
     }
 
     public static String getPlayers() {
-        return getPlayers;
+        return GET_PLAYERS;
     }
 
     public static String getPlayerInfo(String pid) {
-        return getPlayerInfo + pid;
+        return GET_PLAYER_INFO + pid;
     }
 
     public static String getPlayState(String pid) {
-        return getPlayState + pid;
+        return GET_PLAY_STATE + pid;
     }
 
     public static String getNowPlayingMedia(String pid) {
-        return getNowPlayingMedia + pid;
+        return GET_NOW_PLAYING_MEDIA + pid;
     }
 
     public static String getVolume(String pid) {
-        return playerGetVolume + pid;
+        return PLAYER_GET_VOLUME + pid;
     }
 
     public static String getMusicSources() {
-        return getMusicSources;
+        return GET_MUSIC_SOURCES;
     }
 
     public static String prettifyJSONon() {
-        return prettifyJSONon;
+        return PRETTIFY_JSON_ON;
     }
 
     public static String prettifyJSONoff() {
-        return prettifyJSONoff;
+        return PRETTIFY_JSON_OFF;
     }
 
     public static String getMute(String pid) {
-        return playerGetMute + pid;
+        return PLAYER_GET_MUTE + pid;
     }
 
     public static String getQueue(String pid) {
-        return getQueue + pid;
+        return GET_QUEUE + pid;
     }
 
     public static String getQueue(String pid, int start, int end) {
@@ -196,40 +196,40 @@ public class HeosCommands {
     }
 
     public static String playQueueItem(String pid, String qid) {
-        return playQueueItem + pid + "&qid=" + qid;
+        return PLAY_QUEUE_ITEM + pid + "&qid=" + qid;
     }
 
     public static String deleteQueueItem(String pid, String qid) {
-        return deleteQueueItem + pid + "&qid=" + qid;
+        return DELETE_QUEUE_ITEM + pid + "&qid=" + qid;
     }
 
     public static String browseSource(String sid) {
-        return browseSource + sid;
+        return BROWSE_SOURCE + sid;
     }
 
     public static String playStream(String pid) {
-        return playStream + pid;
+        return PLAY_STREAM + pid;
     }
 
     public static String addToQueue(String pid) {
-        return addToQueue + pid;
+        return ADD_TO_QUEUE + pid;
     }
 
     public static String addContainerToQueuePlayNow(String pid, String sid, String cid) {
-        return addToQueue + pid + "&sid=" + sid + "&cid=" + cid + "&aid=1";
+        return ADD_TO_QUEUE + pid + "&sid=" + sid + "&cid=" + cid + "&aid=1";
     }
 
     public static String clearQueue(String pid) {
-        return clearQueue + pid;
+        return CLEAR_QUEUE + pid;
     }
 
     public static String rebootSystem() {
-        return rebootSystem;
+        return REBOOT_SYSTEM;
     }
 
     public static String playStream(@Nullable String pid, @Nullable String sid, @Nullable String cid,
             @Nullable String mid, @Nullable String name) {
-        String newCommand = playStream;
+        String newCommand = PLAY_STREAM;
         if (pid != null) {
             newCommand = newCommand + pid;
         }
@@ -249,15 +249,15 @@ public class HeosCommands {
     }
 
     public static String playStream(String pid, String sid, String mid) {
-        return playStream + pid + "&sid=" + sid + "&mid=" + mid;
+        return PLAY_STREAM + pid + "&sid=" + sid + "&mid=" + mid;
     }
 
     public static String playInputSource(String des_pid, String source_pid, String input) {
-        return playInputSource + des_pid + "&spid=" + source_pid + "&input=inputs/" + input;
+        return PLAY_INPUT_SOURCE + des_pid + "&spid=" + source_pid + "&input=inputs/" + input;
     }
 
     public static String playURL(String pid, String url) {
-        return playURL + pid + "&url=" + url;
+        return PLAY_URL + pid + "&url=" + url;
     }
 
     public static String signIn(String username, String password) {
@@ -267,57 +267,57 @@ public class HeosCommands {
     }
 
     public static String signOut() {
-        return signOut;
+        return SIGN_OUT;
     }
 
     public static String heartbeat() {
-        return heartbeat;
+        return HEARTBEAT;
     }
 
     public static String getGroups() {
-        return getGroups;
+        return GET_GROUPS;
     }
 
     public static String getGroupInfo(String gid) {
-        return getGroupsInfo + gid;
+        return GET_GROUPS_INFO + gid;
     }
 
     public static String setGroup(String[] gid) {
         String players = String.join(",", gid);
 
-        return setGroup + players;
+        return SET_GROUP + players;
     }
 
     public static String getGroupVolume(String gid) {
-        return getGroupVolume + gid;
+        return GET_GROUP_VOLUME + gid;
     }
 
     public static String setGroupVolume(String volume, String gid) {
-        return setGroupVolume + gid + "&level=" + volume;
+        return SET_GROUP_VOLUME + gid + "&level=" + volume;
     }
 
     public static String setGroupVolumeUp(String gid) {
-        return groupVolumeUp + gid + "&step=1";
+        return GROUP_VOLUME_UP + gid + "&step=1";
     }
 
     public static String setGroupVolumeDown(String gid) {
-        return groupVolumeDown + gid + "&step=1";
+        return GROUP_VOLUME_DOWN + gid + "&step=1";
     }
 
     public static String getGroupMute(String gid) {
-        return getGroupMute + gid;
+        return GET_GROUP_MUTE + gid;
     }
 
     public static String setGroupMuteOn(String gid) {
-        return setGroupMute + gid + "&state=on";
+        return SET_GROUP_MUTE + gid + "&state=on";
     }
 
     public static String setGroupMuteOff(String gid) {
-        return setGroupMute + gid + "&state=off";
+        return SET_GROUP_MUTE + gid + "&state=off";
     }
 
     public static String getToggleGroupMute(String gid) {
-        return toggleGroupMute + gid;
+        return TOGGLE_GROUP_MUTE + gid;
     }
 
     private static String urlEncode(String username) {
