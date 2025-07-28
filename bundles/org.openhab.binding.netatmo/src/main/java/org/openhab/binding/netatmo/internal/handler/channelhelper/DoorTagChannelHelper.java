ba59d1417093263240caed1f001b0795fe47a1d9
clinique <gael@lhopital.org>
2025-07-29 01:18:13 +0200

    Restore Netatmo file
    Signed-off-by: clinique <gael@lhopital.org>

--- bundles/org.openhab.binding.netatmo/src/main/java/org/openhab/binding/netatmo/internal/handler/channelhelper/DoorTagChannelHelper.java
+++ bundles/org.openhab.binding.netatmo/src/main/java/org/openhab/binding/netatmo/internal/handler/channelhelper/DoorTagChannelHelper.java
@@ -1,4 +1,5 @@
 <<<<<<< Upstream, based on main
+<<<<<<< Upstream, based on main
 /*
  * Copyright (c) 2010-2026 Contributors to the openHAB project
  *
@@ -105,6 +106,61 @@ public class DoorTagChannelHelper extends ChannelHelper {
                 }).orElse(UnDefType.UNDEF);
             }
 >>>>>>> ec4a730 Restoring lost Netatmo file
+=======
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.netatmo.internal.handler.channelhelper;
+
+import static org.openhab.binding.netatmo.internal.NetatmoBindingConstants.CHANNEL_STATUS;
+
+import java.util.Set;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.netatmo.internal.api.dto.HomeStatusModule;
+import org.openhab.binding.netatmo.internal.api.dto.NAThing;
+import org.openhab.core.config.core.Configuration;
+import org.openhab.core.library.types.OpenClosedType;
+import org.openhab.core.types.State;
+import org.openhab.core.types.UnDefType;
+
+/**
+ * The {@link DoorTagChannelHelper} handles channels of door sensor things.
+ *
+ * @author Gaël L'hopital - Initial contribution
+ *
+ */
+@NonNullByDefault
+public class DoorTagChannelHelper extends ChannelHelper {
+
+    public DoorTagChannelHelper(Set<String> providedGroups) {
+        super(providedGroups);
+    }
+
+    @Override
+    protected @Nullable State internalGetProperty(String channelId, NAThing naThing, Configuration config) {
+        if (naThing instanceof HomeStatusModule doorTag) {
+            if (CHANNEL_STATUS.equalsIgnoreCase(channelId)) {
+                return doorTag.getStatus().map(status -> {
+                    try {
+                        return (State) OpenClosedType.valueOf(status.toUpperCase());
+                    } catch (IllegalArgumentException e) {
+                        // Issue #16629 tag can also return 'no_news'
+                        return UnDefType.UNDEF;
+                    }
+                }).orElse(UnDefType.UNDEF);
+            }
+>>>>>>> d3bbd17 Restore Netatmo file
         }
         return null;
     }
