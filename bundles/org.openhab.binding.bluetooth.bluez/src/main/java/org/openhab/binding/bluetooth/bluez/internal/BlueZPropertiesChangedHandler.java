4a81d903d5a10a5f67cca7ca5cf4d99afd32fdb1
Jacob Laursen <jacob-github@vindvejr.dk>
2025-07-31 08:25:30 +0200

    [bluetooth.bluez] Fix data not being updated (#19086)
    * Fix ServiceDataEvent
    
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>
    Signed-off-by: Scott Hanson <scooter_seh@yahoo.com>

--- bundles/org.openhab.binding.bluetooth.bluez/src/main/java/org/openhab/binding/bluetooth/bluez/internal/BlueZPropertiesChangedHandler.java
+++ bundles/org.openhab.binding.bluetooth.bluez/src/main/java/org/openhab/binding/bluetooth/bluez/internal/BlueZPropertiesChangedHandler.java
@@ -185,8 +185,9 @@ public class BlueZPropertiesChangedHandler extends AbstractPropertiesChangedHand
 
             map.forEach((key, value) -> {
                 if (key instanceof UInt16 iKey && value instanceof Variant<?> vValue
-                        && vValue.getValue() instanceof byte[] bValue) {
-                    eventData.put(iKey.shortValue(), bValue);
+                        && vValue.getValue() instanceof List<?> byteList && !byteList.isEmpty()
+                        && byteList.get(0) instanceof Byte) {
+                    eventData.put(iKey.shortValue(), toByteArray(byteList));
                 }
             });
 
@@ -202,8 +203,9 @@ public class BlueZPropertiesChangedHandler extends AbstractPropertiesChangedHand
 
             map.forEach((key, value) -> {
                 if (key instanceof String sKey && value instanceof Variant<?> vValue
-                        && vValue.getValue() instanceof byte[] bValue) {
-                    serviceData.put(sKey, bValue);
+                        && vValue.getValue() instanceof List<?> byteList && !byteList.isEmpty()
+                        && byteList.get(0) instanceof Byte) {
+                    serviceData.put(sKey, toByteArray(byteList));
                 }
             });
 
