fcaa7567b00dd90261d03fe039205f99b1fdf274
Markus Michels <markus7017@gmail.com>
2025-08-04 22:26:17 +0200

    review change
    Signed-off-by: Markus Michels <markus7017@gmail.com>

--- bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/ShellyDevices.java
+++ bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/ShellyDevices.java
@@ -156,8 +156,6 @@ public class ShellyDevices {
     public static final String SERVICE_NAME_SHELLYBLU_PREFIX = "shellyblu";
     public static final String SERVICE_NAME_SHELLYPLUSRANGE_PREFIX = "shellyplusrange";
 
-    public static final String SERVICE_NAME_SHELLY2_PREFIX = "shellyswitch";
-    public static final String SERVICE_NAME_SHELLY25_PREFIX = "shellyswitch25";
     public static final String SERVICE_NAME_SHELLYPLUG_PREFIX = "shellyplug";
     public static final String SERVICE_NAME_SHELLYPLUGS_PREFIX = "shellyplugs";
     public static final String SERVICE_NAME_SHELLYPLUGU1_PREFIX = "shellyplugu1"; // Shelly Plug US
