13f96d34f03378b85bac6601c456f5a32c3bf952
Christian Kemper <dev@bestof5.de>
2025-08-06 21:42:42 +0200

    Add support for power consumption and production in one channel
    Signed-off-by: christian <dev@bestof5.de>

--- bundles/org.openhab.binding.tibber/src/main/java/org/openhab/binding/tibber/internal/handler/TibberHandler.java
+++ bundles/org.openhab.binding.tibber/src/main/java/org/openhab/binding/tibber/internal/handler/TibberHandler.java
@@ -484,6 +484,7 @@ public class TibberHandler extends BaseThingHandler {
             value = Utils.getJsonValue(jsonData, "accumulatedProductionLastHour");
             updateChannel(CHANNEL_GROUP_STATISTICS, CHANNEL_LAST_HOUR_PRODUCTION, value, "kWh");
             value = Utils.getJsonValue(jsonData, "power");
+            String consumption = Utils.getJsonValue(jsonData, "power");
             updateChannel(CHANNEL_GROUP_LIVE, CHANNEL_CONSUMPTION, value, "W");
             value = Utils.getJsonValue(jsonData, "minPower");
             updateChannel(CHANNEL_GROUP_LIVE, CHANNEL_MIN_COSNUMPTION, value, "W");
@@ -492,7 +493,9 @@ public class TibberHandler extends BaseThingHandler {
             value = Utils.getJsonValue(jsonData, "averagePower");
             updateChannel(CHANNEL_GROUP_LIVE, CHANNEL_AVERAGE_CONSUMPTION, value, "W");
             value = Utils.getJsonValue(jsonData, "powerProduction");
+            String production = Utils.getJsonValue(jsonData, "powerProduction");
             updateChannel(CHANNEL_GROUP_LIVE, CHANNEL_PRODUCTION, value, "W");
+            updateConsumptionAndProductionChannel(consumption, production);
             value = Utils.getJsonValue(jsonData, "minPowerProduction");
             updateChannel(CHANNEL_GROUP_LIVE, CHANNEL_MIN_PRODUCTION, value, "W");
             value = Utils.getJsonValue(jsonData, "maxPowerProduction");
@@ -509,6 +512,18 @@ public class TibberHandler extends BaseThingHandler {
             updateChannel(CHANNEL_GROUP_LIVE, CHANNEL_CURRENT_2, value, "A");
             value = Utils.getJsonValue(jsonData, "currentL3");
             updateChannel(CHANNEL_GROUP_LIVE, CHANNEL_CURRENT_3, value, "A");
+        }
+    }
+
+    private void updateConsumptionAndProductionChannel(String consumption, String production) {
+        if ("0".equals(consumption)) {
+            // Consumption is 0 - assume production
+            updateChannel(CHANNEL_GROUP_LIVE, CHANNEL_CONSUMPTION_AND_PRODUCTION, "-" + production, "W");
+        } else if ("0".equals(production)) {
+            // Production is 0 - assume consumption
+            updateChannel(CHANNEL_GROUP_LIVE, CHANNEL_CONSUMPTION_AND_PRODUCTION, consumption, "W");
+        } else {
+            updateChannel(CHANNEL_GROUP_LIVE, CHANNEL_CONSUMPTION_AND_PRODUCTION, "0", "W");
         }
     }
 
