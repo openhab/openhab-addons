c1efa93b42c8c3374399e9ee5423e1558aa4e4dc
Patrik Gfeller <patrik.gfeller@proton.me>
2025-08-15 10:20:19 +0200

    ...
    Signed-off-by: Patrik Gfeller <patrik.gfeller@proton.me>

--- bundles/org.openhab.binding.jellyfin/src/main/java/org/openhab/binding/jellyfin/internal/handler/ServerHandler.java
+++ bundles/org.openhab.binding.jellyfin/src/main/java/org/openhab/binding/jellyfin/internal/handler/ServerHandler.java
@@ -19,11 +19,15 @@ import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.jellyfin.internal.Configuration;
 import org.openhab.binding.jellyfin.internal.api.ApiClient;
 import org.openhab.binding.jellyfin.internal.exceptions.ExceptionHandler;
 import org.openhab.binding.jellyfin.internal.handler.tasks.ConnectionTask;
 import org.openhab.core.thing.Bridge;
 import org.openhab.core.thing.ChannelUID;
+import org.openhab.core.thing.ThingStatus;
+import org.openhab.core.thing.ThingStatusDetail;
+import org.openhab.core.thing.ThingStatusInfo;
 import org.openhab.core.thing.binding.BaseBridgeHandler;
 import org.openhab.core.types.Command;
 import org.slf4j.Logger;
@@ -84,14 +88,28 @@ public class ServerHandler extends BaseBridgeHandler {
 
     private synchronized Runnable initializeHandler() {
         return () -> {
-            this.stopTasks();
-            this.startTasks();
+            try {
+                if (this.configurationExists()) {
+                    this.stopTasks();
+                    this.startTasks();
+                } else {
+                    logger.warn("Jellyfin configuration is missing or incomplete. Please check your settings.");
+
+                    var description = "";
+                    ThingStatusInfo statusInfo = new ThingStatusInfo(ThingStatus.OFFLINE,
+                            ThingStatusDetail.CONFIGURATION_PENDING, description);
+                    this.getThing().setStatusInfo(statusInfo);
+                }
+            } catch (Exception e) {
+                this.logger.warn("Error during initialization: {}", e.getMessage(), e);
+
+                this.exceptionHandler.handle(e);
+            }
         };
     }
 
     private synchronized void startTasks() {
-        String taskId = getTask();
-
+        String taskId = TASKS.CONNECT;
         Runnable task = null;
 
         long delay = TASKS.delays.get(taskId);
@@ -113,8 +131,10 @@ public class ServerHandler extends BaseBridgeHandler {
         }
     }
 
-    private String getTask() {
-        return TASKS.CONNECT;
+    private boolean configurationExists() {
+        var configuration = this.getConfigAs(Configuration.class);
+
+        return (configuration.token.trim() != "");
     }
 
     private Object handleConnection(ApiClient instance) {
@@ -159,9 +179,5 @@ public class ServerHandler extends BaseBridgeHandler {
 
     private @Nullable ScheduledFuture<?> executeTask(Runnable task, long initialDelay, long interval) {
         return scheduler.scheduleWithFixedDelay(task, initialDelay, interval, TimeUnit.SECONDS);
-    }
-
-    String getAccessToken() {
-        return "123";
     }
 }
