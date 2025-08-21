f8fed2b93875bbfae2fb9a87094b718a0ad21fb6
Marcel Goerentz <57457529+marcelGoerentz@users.noreply.github.com>
2025-08-21 15:36:51 +0200

    [evcc] Fix bug #19206 where heating devices showing % instead of °C (#19211)
    * Fix bug #19206 and add datapoints for currents for grid and chargers
    
    Signed-off-by: Marcel Goerentz <57457529+marcelGoerentz@users.noreply.github.com>

--- bundles/org.openhab.binding.evcc/src/main/java/org/openhab/binding/evcc/internal/handler/EvccHeatingHandler.java
+++ bundles/org.openhab.binding.evcc/src/main/java/org/openhab/binding/evcc/internal/handler/EvccHeatingHandler.java
@@ -12,9 +12,15 @@
  */
 package org.openhab.binding.evcc.internal.handler;
 
+import static org.openhab.binding.evcc.internal.EvccBindingConstants.JSON_MEMBER_LOADPOINTS;
+
+import java.util.Arrays;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.thing.ChannelUID;
 import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.type.ChannelTypeRegistry;
+import org.openhab.core.types.Command;
 
 import com.google.gson.JsonObject;
 
@@ -26,6 +32,9 @@ import com.google.gson.JsonObject;
 @NonNullByDefault
 public class EvccHeatingHandler extends EvccLoadpointHandler {
 
+    private static final String[] TEMPERATURE_CHANNEL_IDS = { "loadpoint-effective-limit-temperature",
+            "loadpoint-limit-temperature", "loadpoint-vehicle-temperature" };
+
     public EvccHeatingHandler(Thing thing, ChannelTypeRegistry channelTypeRegistry) {
         super(thing, channelTypeRegistry);
     }
@@ -36,7 +45,36 @@ public class EvccHeatingHandler extends EvccLoadpointHandler {
     }
 
     @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        String id = channelUID.getId();
+        if (Arrays.asList(TEMPERATURE_CHANNEL_IDS).contains(id)) {
+            // Replace the generated key with the original one to get the correct
+            String thingKey = getThingKey(id.replace("Temperature", "Soc"));
+            channelUID = new ChannelUID(getThing().getUID(), thingKey);
+        }
+        super.handleCommand(channelUID, command);
+    }
+
+    @Override
     public void updateFromEvccState(JsonObject state) {
+        updateJSON(state);
         super.updateFromEvccState(state);
+    }
+
+    protected void updateJSON(JsonObject state) {
+        JsonObject heatingState = state.getAsJsonArray(JSON_MEMBER_LOADPOINTS).get(index).getAsJsonObject();
+        if (heatingState.has("vehicleLimitSoc")) {
+            heatingState.addProperty("vehicleLimitTemperature", heatingState.get("vehicleLimitSoc").getAsDouble());
+            heatingState.remove("vehicleLimitSoc");
+        }
+        if (heatingState.has("effectiveLimitSoc")) {
+            heatingState.add("effectiveLimitTemperature", heatingState.get("effectiveLimitSoc"));
+            heatingState.remove("effectiveLimitSoc");
+        }
+        if (heatingState.has("vehicleSoc")) {
+            heatingState.add("vehicleTemperature", heatingState.get("vehicleSoc"));
+            heatingState.remove("vehicleSoc");
+        }
+        state.getAsJsonArray(JSON_MEMBER_LOADPOINTS).set(index, heatingState); // Update the IDs in the original JSON
     }
 }
