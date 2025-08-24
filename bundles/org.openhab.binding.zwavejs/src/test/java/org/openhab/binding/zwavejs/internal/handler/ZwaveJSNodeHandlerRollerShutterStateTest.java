80e5378cbdd89d5c3e6c0f9124159966d2f921f0
Leo Siepel <leosiepel@gmail.com>
2025-08-24 22:28:42 +0200

    Fix header
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.zwavejs/src/test/java/org/openhab/binding/zwavejs/internal/handler/ZwaveJSNodeHandlerRollerShutterStateTest.java
+++ bundles/org.openhab.binding.zwavejs/src/test/java/org/openhab/binding/zwavejs/internal/handler/ZwaveJSNodeHandlerRollerShutterStateTest.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.zwavejs.internal.handler;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,6 +24,9 @@ import org.openhab.binding.zwavejs.internal.type.capabilities.RollerShutterCapab
 import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.binding.ThingHandlerCallback;
 
+/**
+ * @author Leo Siepel - Initial contribution
+ */
 public class ZwaveJSNodeHandlerRollerShutterStateTest {
     private ZwaveJSNodeHandlerMock handler;
     private RollerShutterCapability capability;
