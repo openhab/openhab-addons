3d1e00035f55b157f596cd6cbffdb81fafbeab91
Joshua Freeman <github@frejos.net>
2025-09-12 11:23:27 -0500

    Make bug fixes for Wiz binding for stability and consistency.
    1) Replaced Thread.sleep in setPilotCommand with scheduled execution and reduced delay. The
    Thread.sleep statement was added for testing and was not intended to be included in release
    and causes unintended issues from thread blocking.
    
    2) Correct divide by zero error in colorTempToPercent method when min and max are the same,
    also added min and max color temperature to thing properties.
    
    3) Correct direction of percentage calculation in colorTempToPercent, 0% is cold (highest K)
    and 100% is warm (lowest K). This matches pre-dated percentToColorTemp direction and Openhab
    most common direction for consistency.
    
    4) Corrected WizColorConverter to remove warm white LEDs which cause over saturation and
    to ensure hsbToRgbw and rgbwDimmingToHSB are consistent to prevent setting jumping, the user
    selected setting is now maintained in both directions. Replacing this logic, also corrected an
    issue in rgbwDimmingToHSB with integer division that caused saturation to almost always be set
    to 100%. I also tested the Openhab standard ColorUtils methods since I needed to make changes
    here, however they produce extremely white saturated colors on these devices for all values.
    
    Signed-off-by: Joshua Freeman <github@frejos.net>

--- bundles/org.openhab.binding.wiz/src/main/java/org/openhab/binding/wiz/internal/utils/WizColorConverter.java
+++ bundles/org.openhab.binding.wiz/src/main/java/org/openhab/binding/wiz/internal/utils/WizColorConverter.java
@@ -38,53 +38,27 @@ public class WizColorConverter {
      * Converts an {@link DecimalType} hue and a {@link PercentType} saturation to
      * red, green, blue, and white (RGBW) components. Because the WiZ bulbs keep
      * dimming in a separate channel, we only take account hue and saturation for
-     * the color channels. When creating colors, the WiZ bulbs only use the warm
-     * white channel, the cool white channel is ignored.
+     * the color channels. In RGB mode the WiZ bulbs only use the warm
+     * white channel, the cool white channel is ignored. I have also found that adding
+     * the warm white channel washes out the color too much to be useful. Leaving
+     * warm white set to 0 seems to give the best color representation across the full
+     * saturation range.
      *
-     * Taken from Tasmota HsToRGB
+     * NOTE: This method must be kept in sync with rgbwDimmingToHSB in order for the
+     * color representation to remain accurate between conversions in both directions.
      *
      * @param hsbColor the {@link HSBType}.
      *
      * @return an interger array of the color components
      */
     public int[] hsbToRgbw(HSBType hsb) {
-        // Since we're going to use the white lights to control saturation, recalculate what
-        // the HSBvalue would be if the color was at full brightness and saturation
-        HSBType hsbFullBrightness = new HSBType(hsb.getHue(), hsb.getSaturation(), PercentType.HUNDRED);
-        PercentType[] rgbPercent = ColorUtil.hsbToRgbPercent(hsbFullBrightness);
-        double redD = rgbPercent[0].doubleValue();
-        double greenD = rgbPercent[1].doubleValue();
-        double blueD = rgbPercent[2].doubleValue();
-
-        double saturationPercent = hsb.getSaturation().doubleValue() / 100;
-
-        int red;
-        int green;
-        int blue;
-        int white;
-
-        // Calculate the white intensity from saturation and adjust down the other colors
-        // This is approximately what the WiZ app does. Personally, I think it undersaturates everything
-        if (saturationPercent < 0.5) {
-            // At less than 50% saturation, maximize white and lower the other intensities by 2x of the saturation
-            // percent. (2x to give us full range between 0-50%)
-            // white = 255;
-            // ^^ WiZ does this.. I think it's very undersaturated that way
-            white = 255 / 2; // Divide by two to not undersaturate
-            red = (int) (redD * (2 * saturationPercent));
-            green = (int) (greenD * (2 * saturationPercent));
-            blue = (int) (blueD * (2 * saturationPercent));
-        } else {
-            // At >50% saturation, colors are at full and increase saturation by decreasing the white intensity.
-            // white = (int) (255 * 2 * (1 - saturationPercent));
-            // ^^ WiZ does this.. I think it's very undersaturated that way
-            white = (int) ((255 / 2) * 2 * (1 - saturationPercent));
-            red = (int) redD;
-            green = (int) greenD;
-            blue = (int) blueD;
-        }
-
-        // Note: We're keeping the brightness in a totally separate channel
+        // Only use RGB, ignore white
+        HSBType hsbFull = new HSBType(hsb.getHue(), hsb.getSaturation(), PercentType.HUNDRED);
+        PercentType[] rgbPercent = ColorUtil.hsbToRgbPercent(hsbFull);
+        int red = (int) (rgbPercent[0].doubleValue());
+        int green = (int) (rgbPercent[1].doubleValue());
+        int blue = (int) (rgbPercent[2].doubleValue());
+        int white = 0;
         return new int[] { red, green, blue, white };
     }
 
@@ -97,22 +71,22 @@ public class WizColorConverter {
      * @param int white - the value of the white component (0-255)
      * @param int dimming - the brightness of the bulb, independent of the RGB color (0-100)
      *
-     *            Totally made this up.
+     * NOTE: This method must be kept in sync with hsbToRgbw in order for the
+     * color representation to remain accurate between conversions in both directions.
      *
      * @return a {@link HSBType} with the color components
      */
     public HSBType rgbwDimmingToHSB(int red, int green, int blue, int white, int dimming) {
-        // Can get hue from the ratios of the colors.
-        // The calculated *hue* component of the HSB should be correct regardless of the
-        // state of the white lights because it's strictly based on the ratio of the colors
+        // Only use RGB for saturation
+        double r = red / 255.0;
+        double g = green / 255.0;
+        double b = blue / 255.0;
+        double max = Math.max(r, Math.max(g, b));
+        double min = Math.min(r, Math.min(g, b));
+        double saturation = (max == 0) ? 0 : (max - min) / max;
+        int saturationPercent = (int) (saturation * 100.0);
         DecimalType hue = HSBType.fromRGB(red, green, blue).getHue();
-        double saturationPercent;
-        if (white < 255) {
-            saturationPercent = (int) (1 - (white / (255 * 2)));
-        } else {
-            saturationPercent = Math.max(red, Math.max(green, blue)) / (255 * 2);
-        }
-        HSBType out = new HSBType(hue, new PercentType((int) saturationPercent * 100), new PercentType(dimming));
+        HSBType out = new HSBType(hue, new PercentType(saturationPercent), new PercentType(dimming));
         return out;
     }
 }
