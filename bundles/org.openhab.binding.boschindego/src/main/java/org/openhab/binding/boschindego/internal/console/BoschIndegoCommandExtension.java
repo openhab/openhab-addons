f189e9dc4ec1c29157c5dc024226f87015957e43
Andreas Lanz <andreas.lanz@gmx.de>
2025-10-24 22:53:55 +0200

    [boschindego] Add support for multiple accounts / bridges
    Signed-off-by: Andreas Lanz <andreas.lanz@gmx.de>

--- bundles/org.openhab.binding.boschindego/src/main/java/org/openhab/binding/boschindego/internal/console/BoschIndegoCommandExtension.java
+++ bundles/org.openhab.binding.boschindego/src/main/java/org/openhab/binding/boschindego/internal/console/BoschIndegoCommandExtension.java
@@ -12,8 +12,9 @@
  */
 package org.openhab.binding.boschindego.internal.console;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
@@ -27,7 +28,7 @@ import org.openhab.core.io.console.extensions.AbstractConsoleCommandExtension;
 import org.openhab.core.io.console.extensions.ConsoleCommandExtension;
 import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.ThingRegistry;
-import org.openhab.core.thing.binding.ThingHandler;
+import org.openhab.core.thing.ThingUID;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -54,26 +55,53 @@ public class BoschIndegoCommandExtension extends AbstractConsoleCommandExtension
 
     @Override
     public void execute(String[] args, Console console) {
-        if (args.length != 2 || !AUTHORIZE.equals(args[0])) {
+        String authCode;
+        Thing bridge;
+
+        if (args.length == 2 && AUTHORIZE.equals(args[0])) {
+            try {
+                bridge = getSingleBridge();
+            } catch (IllegalStateException e) {
+                console.println("Error finding indego bridge: " + e.getMessage());
+                printUsage(console);
+                return;
+            }
+
+            if (bridge == null) {
+                console.println("No BoschIndego Bridge defined.");
+                return;
+            }
+
+            authCode = args[1];
+        } else if (args.length == 3 && AUTHORIZE.equals(args[1])) {
+            bridge = getBridgeById(args[0]);
+            if (bridge == null) {
+                console.println("Unknown thing id '" + args[0] + "'");
+                return;
+            }
+
+            authCode = args[2];
+        } else {
             printUsage(console);
             return;
         }
 
-        for (Thing thing : thingRegistry.getAll()) {
-            ThingHandler thingHandler = thing.getHandler();
-            if (thingHandler instanceof BoschAccountHandler accountHandler) {
-                try {
-                    accountHandler.authorize(args[1]);
-                } catch (IndegoAuthenticationException e) {
-                    console.println("Authorization error: " + e.getMessage());
-                }
+        if (bridge.getHandler() instanceof BoschAccountHandler accountHandler) {
+            try {
+                accountHandler.authorize(authCode);
+            } catch (IndegoAuthenticationException e) {
+                console.println("Authorization error: " + e.getMessage());
             }
+        } else {
+            console.println("Bridge is not a valid BoschIndego bridge");
+            printUsage(console);
         }
     }
 
     @Override
     public List<String> getUsages() {
-        return Arrays.asList(buildCommandUsage(AUTHORIZE, "authorize by authorization code"));
+        return List
+                .of(buildCommandUsage("[<bridgeId>] " + AUTHORIZE + " <AuthToken>", "authorize by authorization code"));
     }
 
     @Override
@@ -84,8 +112,46 @@ public class BoschIndegoCommandExtension extends AbstractConsoleCommandExtension
     @Override
     public boolean complete(String[] args, int cursorArgumentIndex, int cursorPosition, List<String> candidates) {
         if (cursorArgumentIndex <= 0) {
+            return new StringsCompleter(
+                    Stream.concat(Stream.of(AUTHORIZE), getBridgeIds().stream().map(ThingUID::getAsString)).toList(),
+                    false).complete(args, cursorArgumentIndex, cursorPosition, candidates);
+        } else if (cursorArgumentIndex == 1 && !AUTHORIZE.equals(args[0])) {
             return SUBCMD_COMPLETER.complete(args, cursorArgumentIndex, cursorPosition, candidates);
         }
         return false;
+    }
+
+    private List<ThingUID> getBridgeIds() {
+        List<ThingUID> bridgeIds = new ArrayList<>();
+        for (Thing thing : thingRegistry.getAll()) {
+            if (thing.getHandler() instanceof BoschAccountHandler) {
+                bridgeIds.add(thing.getUID());
+            }
+        }
+        return bridgeIds;
+    }
+
+    private @Nullable Thing getBridgeById(String uid) {
+        Thing thing;
+        try {
+            ThingUID thingUID = new ThingUID(uid);
+            thing = thingRegistry.get(thingUID);
+        } catch (IllegalArgumentException e) {
+            thing = null;
+        }
+        return thing;
+    }
+
+    private @Nullable Thing getSingleBridge() {
+        Thing bridge = null;
+        for (Thing thing : thingRegistry.getAll()) {
+            if (thing.getHandler() instanceof BoschAccountHandler) {
+                if (bridge != null) {
+                    throw new IllegalStateException("More than one BoschIndego Bridge found.");
+                }
+                bridge = thing;
+            }
+        }
+        return bridge;
     }
 }
