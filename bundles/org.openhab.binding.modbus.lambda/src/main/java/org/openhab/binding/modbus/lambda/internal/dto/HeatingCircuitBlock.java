e393064c19923c0528c5729ac284861d2b6d0ae8
Christian Koch <78686276+chilobo@users.noreply.github.com>
2025-11-23 11:07:57 +0100

    [modbus.lambda] Initial contribution (#19378)
    * New Crowdin updates (#19375)
    
    Signed-off-by: Christian Koch <78686276+chilobo@users.noreply.github.com>
    Signed-off-by: Johannes Koch <github@lockhead.net>

---
+++ bundles/org.openhab.binding.modbus.lambda/src/main/java/org/openhab/binding/modbus/lambda/internal/dto/HeatingCircuitBlock.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.modbus.lambda.internal.dto;
+
+/**
+ * Dto class for the HeatingCircuit Block
+ *
+ * @author Paul Frank - Initial contribution
+ * @author Christian Koch - modified for lambda heat pump based on stiebeleltron binding for modbus
+ *
+ */
+public class HeatingCircuitBlock {
+    public int heatingcircuitErrorNumber;
+    public int heatingcircuitOperatingState;
+    public int heatingcircuitFlowLineTemperature;
+    public int heatingcircuitReturnLineTemperature;
+    public int heatingcircuitRoomDeviceTemperature;
+    public int heatingcircuitSetpointFlowLineTemperature;
+    public int heatingcircuitOperatingMode;
+    public int heatingcircuitTargetTemperatureFlowLine;
+}
---
+++ bundles/org.openhab.binding.modbus.lambda/src/main/java/org/openhab/binding/modbus/lambda/internal/dto/SolarBlock.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.modbus.lambda.internal.dto;
+
+/**
+ * Data transfer object for solar thermic component data
+ *
+ * @author Paul Frank - Initial contribution
+ * @author Christian Koch - modified for lambda heat pump based on stiebeleltron binding for modbus to add handler
+ */
+public class SolarBlock {
+    public int solarErrorNumber;
+    public int solarOperatingState;
+    public int solarCollectorTemperature;
+    public int solarBuffer1Temperature;
+    public int solarBuffer2Temperature;
+}
