6c019cb75c449f8c3a2fb864cf20f9d0ac9aca1e
Bernd Weymann <bernd.weymann@gmail.com>
2025-12-12 09:55:52 +0100

    [e3dc] Add performance ratio channels (#19763)
    * add performance ratio
    
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

--- bundles/org.openhab.binding.modbus.e3dc/src/main/java/org/openhab/binding/modbus/e3dc/internal/config/E3DCWallboxConfiguration.java
+++ bundles/org.openhab.binding.modbus.e3dc/src/main/java/org/openhab/binding/modbus/e3dc/internal/config/E3DCWallboxConfiguration.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.modbus.e3dc.internal;
+package org.openhab.binding.modbus.e3dc.internal.config;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
