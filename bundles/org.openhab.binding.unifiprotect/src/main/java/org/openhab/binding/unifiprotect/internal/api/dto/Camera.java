76e316f978b0c0c780c85fe69146e1aafe922c29
Dan Cunningham <dan@digitaldan.com>
2025-12-12 08:09:41 -0800

    [unifiprotect] Initial Contribution  (#19411)
    * Unifi Protect Binding
    
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

---
+++ bundles/org.openhab.binding.unifiprotect/src/main/java/org/openhab/binding/unifiprotect/internal/api/dto/Camera.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.unifiprotect.internal.api.dto;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * Camera device DTO.
+ *
+ * Matches the camera schema.
+ *
+ * @author Dan Cunningham - Initial contribution
+ */
+public class Camera extends Device {
+    public Boolean isMicEnabled;
+    public OsdSettings osdSettings;
+    public LedSettings ledSettings;
+    public LcdMessage lcdMessage;
+    public Integer micVolume; // 0-100
+    public @Nullable Integer activePatrolSlot;
+    public VideoMode videoMode;
+    public HdrType hdrType;
+    public CameraFeatureFlags featureFlags;
+    public SmartDetectSettings smartDetectSettings;
+}
