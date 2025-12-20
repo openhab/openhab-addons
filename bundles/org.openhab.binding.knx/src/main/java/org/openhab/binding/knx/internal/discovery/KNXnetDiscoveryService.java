6e4ee70b9016a6329d225b5a7af533f12227bce5
Holger Friedrich <mail@holger-friedrich.de>
2025-12-20 20:48:25 +0100

    [knx] Prevent reconfigutation of Things during device discovery (#19839)
    Prevents reconfiguration of connection type during discovery runs.
    Previously, existing Things were rediscovered and reconfigured
    to default connection type.
    
    This comes with the drawback that all discovered devices need
    to be manually configured (i.e. set a proper connection type).
    
    Signed-off-by: Holger Friedrich <mail@holger-friedrich.de>

--- bundles/org.openhab.binding.knx/src/main/java/org/openhab/binding/knx/internal/discovery/KNXnetDiscoveryService.java
+++ bundles/org.openhab.binding.knx/src/main/java/org/openhab/binding/knx/internal/discovery/KNXnetDiscoveryService.java
@@ -109,19 +109,19 @@ public class KNXnetDiscoveryService extends AbstractDiscoveryService {
                     }
 
                     if (services.containsKey(ServiceFamiliesDIB.ServiceFamily.Tunneling)) {
+                        // .withProperty("type", "TUNNEL") was omitted to avoid reconfiguration
                         thingDiscovered(DiscoveryResultBuilder.create(new ThingUID(THING_TYPE_IP_BRIDGE, serial))
                                 .withLabel(response.getDevice().getName()).withProperty("serialNumber", serial)
-                                .withProperty("type", "TUNNEL")
                                 .withProperty("ipAddress",
                                         "" + response.getControlEndpoint().endpoint().getAddress().getHostAddress())
                                 .withProperty("port", "" + response.getControlEndpoint().endpoint().getPort())
                                 .withRepresentationProperty("serialNumber").build());
                     }
                     if (services.containsKey(ServiceFamiliesDIB.ServiceFamily.Routing)) {
+                        // .withProperty("type", "ROUTER") was omitted to avoid reconfiguration
                         thingDiscovered(DiscoveryResultBuilder.create(new ThingUID(THING_TYPE_IP_BRIDGE, serial))
                                 .withLabel(response.getDevice().getName() + " (router mode)")
-                                .withProperty("serialNumber", serial + "-r").withProperty("type", "ROUTER")
-                                .withProperty("ipAddress", "224.0.23.12")
+                                .withProperty("serialNumber", serial + "-r").withProperty("ipAddress", "224.0.23.12")
                                 .withProperty("port", "" + response.getControlEndpoint().endpoint().getPort())
                                 .withRepresentationProperty("serialNumber").build());
                     }
