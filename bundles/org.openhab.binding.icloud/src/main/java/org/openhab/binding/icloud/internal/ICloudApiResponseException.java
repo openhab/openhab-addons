a727e121e2a72fb7f3539557a5480c98eb970f67
Simon Spielmann <simon.spielmann@gmx.de>
2025-12-22 15:12:05 +0100

    [icloud] Implement SRP authentication algorithm (#19832)
    Signed-off-by: Simon Spielmann <simon.spielmann@gmx.de>

--- bundles/org.openhab.binding.icloud/src/main/java/org/openhab/binding/icloud/internal/ICloudApiResponseException.java
+++ bundles/org.openhab.binding.icloud/src/main/java/org/openhab/binding/icloud/internal/ICloudApiResponseException.java
@@ -24,7 +24,8 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
 public class ICloudApiResponseException extends Exception {
 
     private static final long serialVersionUID = 1L;
-    private int statusCode;
+    protected int statusCode;
+    protected String body;
 
     /**
      * The constructor.
@@ -32,9 +33,15 @@ public class ICloudApiResponseException extends Exception {
      * @param url URL for which the exception occurred
      * @param statusCode HTTP status code which was reported
      */
-    public ICloudApiResponseException(String url, int statusCode) {
+    public ICloudApiResponseException(String url, int statusCode, String body) {
         super(String.format("Request %s failed with %s.", url, statusCode));
         this.statusCode = statusCode;
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "ICloudApiResponseException [statusCode=" + statusCode + ", body=" + body + "]";
     }
 
     /**
