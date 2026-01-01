9109e34ef51a7efc3aced4c428a0b788eebbefbf
Simmon Yau <simmonyau@gmail.com>
2026-01-01 04:37:06 -0500

    [tapocontrol] Add TP-Link HS200 Kasa Smart Wi-Fi Light Switch (#19924)
    * Support TP-Link Smart Switches HS200
    
    Signed-off-by: Simmon Yau <simmonyau@gmail.com>

---
+++ bundles/org.openhab.binding.tapocontrol/src/main/java/org/openhab/binding/tapocontrol/internal/devices/wifi/lightswitch/TapoLightSwitchData.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.tapocontrol.internal.devices.wifi.lightswitch;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.binding.tapocontrol.internal.devices.dto.TapoBaseDeviceData;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@NonNullByDefault
+public class TapoLightSwitchData extends TapoBaseDeviceData {
+    @SerializedName("device_on")
+    @Expose(serialize = true, deserialize = true)
+    private boolean deviceOn = false;
+
+    public void switchOnOff(boolean on) {
+        deviceOn = on;
+    }
+
+    public boolean isOn() {
+        return deviceOn;
+    }
+
+    public boolean isOff() {
+        return !deviceOn;
+    }
+}
