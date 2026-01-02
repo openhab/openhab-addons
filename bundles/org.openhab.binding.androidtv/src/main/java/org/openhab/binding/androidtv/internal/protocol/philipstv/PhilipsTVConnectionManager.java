6ce658be99a6e97ca10706f505bf37b47cecaf71
Marcel <marcel@verpaalen.com>
2026-01-02 23:11:38 +0100

    [androidtv] Fix loop for PhilipsTV (#19953)
    Signed-off-by: Marcel Verpaalen <marcel@verpaalen.com>

--- bundles/org.openhab.binding.androidtv/src/main/java/org/openhab/binding/androidtv/internal/protocol/philipstv/PhilipsTVConnectionManager.java
+++ bundles/org.openhab.binding.androidtv/src/main/java/org/openhab/binding/androidtv/internal/protocol/philipstv/PhilipsTVConnectionManager.java
@@ -532,10 +532,6 @@ public class PhilipsTVConnectionManager implements DiscoveryListener {
     public synchronized void postUpdateThing(ThingStatus status, ThingStatusDetail statusDetail, String msg) {
         logger.trace("postUpdateThing {} {} {}", status, statusDetail, msg);
         if (status == ThingStatus.ONLINE) {
-            PhilipsTVService powerService = channelServices.get(CHANNEL_POWER);
-            if (powerService != null) {
-                powerService.handleCommand(CHANNEL_POWER, RefreshType.REFRESH);
-            }
             if (!msg.equalsIgnoreCase(STANDBY_MSG)) {
                 startDeviceHealthJob(5, TimeUnit.SECONDS);
                 pendingPowerOn = false;
