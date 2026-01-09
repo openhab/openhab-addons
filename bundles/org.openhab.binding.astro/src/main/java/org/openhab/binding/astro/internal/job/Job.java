da69c5bdf2c9079e3d90e1e13bf2dbc1c90a6c57
clinique <gael@lhopital.org>
2026-01-09 10:22:18 +0100

    Remove leftover
    Signed-off-by: clinique <gael@lhopital.org>

--- bundles/org.openhab.binding.astro/src/main/java/org/openhab/binding/astro/internal/job/Job.java
+++ bundles/org.openhab.binding.astro/src/main/java/org/openhab/binding/astro/internal/job/Job.java
@@ -157,11 +157,7 @@ public interface Job extends SchedulerRunnable, Runnable {
      * Schedules an {@link EventJob} instance
      *
      * @param astroHandler the {@link AstroThingHandler} instance
-     *            <<<<<<< Upstream, based on main
      * @param eventAt the {@link Instant} instance denoting scheduled instant
-     *            =======
-     * @param eventAt the {@link Calendar} instance denoting scheduled instant
-     *            >>>>>>> f56c745 Review Moon Distance and factorization of MoonCalc
      * @param events the event IDs to schedule
      * @param channelId the channel ID
      */
