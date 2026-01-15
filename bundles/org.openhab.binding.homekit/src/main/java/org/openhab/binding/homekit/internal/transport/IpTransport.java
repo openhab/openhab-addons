bc718e04482b4652e746872f023d32bfde13cdb1
Andrew Fiddian-Green <software@whitebear.ch>
2026-01-15 11:02:48 +0000

    adopt reviewer suggestions
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.binding.homekit/src/main/java/org/openhab/binding/homekit/internal/transport/IpTransport.java
+++ bundles/org.openhab.binding.homekit/src/main/java/org/openhab/binding/homekit/internal/transport/IpTransport.java
@@ -266,7 +266,7 @@ public class IpTransport implements AutoCloseable {
     private byte[] buildRequest(String method, String endpoint, String contentType, byte[] content) throws IOException {
         StringBuilder sb = new StringBuilder();
         sb.append(method).append(" ").append(endpoint).append(" HTTP/1.1\r\n");
-        if (!hostName.isEmpty()) {
+        if (!hostName.isBlank()) {
             sb.append("Host: ").append(hostName).append("\r\n");
         }
         if (!contentIsEmpty(method)) {
