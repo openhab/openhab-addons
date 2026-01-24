27fa9d3616f0b597df5c4614f5f0a3ce169a5422
Bernd Weymann <bernd.weymann@gmail.com>
2026-01-24 16:07:29 +0100

    [solarforecast] Internal rework (#19815)
    * sync and optional rework
    
    Signed-off-by: Bernd Weymann <bernd.weymann@gmail.com>

--- bundles/org.openhab.binding.solarforecast/src/main/java/org/openhab/binding/solarforecast/internal/SolarForecastHandlerFactory.java
+++ bundles/org.openhab.binding.solarforecast/src/main/java/org/openhab/binding/solarforecast/internal/SolarForecastHandlerFactory.java
@@ -14,8 +14,6 @@ package org.openhab.binding.solarforecast.internal;
 
 import static org.openhab.binding.solarforecast.internal.SolarForecastBindingConstants.*;
 
-import java.util.Optional;
-
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.binding.solarforecast.internal.forecastsolar.handler.AdjustableForecastSolarPlaneHandler;
@@ -59,7 +57,7 @@ public class SolarForecastHandlerFactory extends BaseThingHandlerFactory {
     private final TimeZoneProvider timeZoneProvider;
     private final HttpClientFactory httpClientFactory;
     private final PersistenceServiceRegistry persistenceRegistry;
-    private Optional<PointType> location = Optional.empty();
+    private @Nullable PointType location;
     private Storage<String> storage;
 
     @Activate
@@ -70,10 +68,7 @@ public class SolarForecastHandlerFactory extends BaseThingHandlerFactory {
         timeZoneProvider = tzp;
         httpClientFactory = hcf;
         Utils.setTimeZoneProvider(tzp);
-        PointType pt = lp.getLocation();
-        if (pt != null) {
-            location = Optional.of(pt);
-        }
+        location = lp.getLocation();
         storage = storageService.getStorage(SolarForecastBindingConstants.BINDING_ID);
     }
 
