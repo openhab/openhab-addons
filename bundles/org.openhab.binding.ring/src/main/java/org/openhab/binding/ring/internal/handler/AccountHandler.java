ef5a8da7b5a4e6395112928c24580e9b3bb25e03
Paul Smedley <paul@smedley.id.au>
2026-01-31 10:52:48 +1030

    Fix download of videos
    Signed-off-by: Paul Smedley <paul@smedley.id.au>

--- bundles/org.openhab.binding.ring/src/main/java/org/openhab/binding/ring/internal/handler/AccountHandler.java
+++ bundles/org.openhab.binding.ring/src/main/java/org/openhab/binding/ring/internal/handler/AccountHandler.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
@@ -214,6 +215,7 @@ public class AccountHandler extends BaseBridgeHandler implements RingAccount {
 
     protected void startAutomaticRefresh(final int refreshInterval) {
         refreshJob = scheduler.scheduleWithFixedDelay(this::refresh, 0, refreshInterval, TimeUnit.SECONDS);
+        videoExecutorService = Executors.newSingleThreadExecutor();
     }
 
     protected void stopAutomaticRefresh() {
