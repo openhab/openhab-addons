67bc6fe19b8a8fc2a32b0e42a61b24269b4c37be
Andrew Fiddian-Green <software@whitebear.ch>
2026-02-05 16:02:38 +0000

    [binding.homekit] Improve I/O streaming (#20106)
    * parser supplies at most one future; blocks otherwise
    * improve error case handling
    * fix input streams with help of new unit tests
    * remove intermediate handover threads
    * fix plain to encrypted socket transition sync
    * add fix for #20040
    * handle other interrupted exceptions
    * tighter constraints on response window and logging
    * suppress logging on shutdown
    - add EncrypingOutputStream for socket output
    - harden the set session keys handover
    - add round trip crypto test
    * reduce log clutter
    - use single communication error handler
    - expand thing status detail description
    
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.binding.homekit/src/main/java/org/openhab/binding/homekit/internal/handler/HomekitAccessoryHandler.java
+++ bundles/org.openhab.binding.homekit/src/main/java/org/openhab/binding/homekit/internal/handler/HomekitAccessoryHandler.java
@@ -511,18 +511,7 @@ public class HomekitAccessoryHandler extends HomekitBaseAccessoryHandler {
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt(); // shutting down; restore interrupt flag but otherwise do nothing
         } catch (Exception e) {
-            if (isCommunicationException(e)) {
-                // communication exception; log at debug and try to reconnect
-                logger.debug("{} communication error '{}' sending command '{}' to '{}', reconnecting..", thing.getUID(),
-                        e.getMessage(), command, channelUID, e);
-                scheduleConnectionAttempt();
-            } else {
-                // other exception; log at warn and don't try to reconnect
-                logger.warn("{} unexpected error '{}' sending command '{}' to '{}'", thing.getUID(), e.getMessage(),
-                        command, channelUID, e);
-            }
-            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
-                    THING_STATUS_FMT.formatted("error.error-sending-command", e.getMessage()));
+            onCommunicationError(e, I18N_SUFFIX_ERROR_SENDING_COMMAND);
         }
     }
 
