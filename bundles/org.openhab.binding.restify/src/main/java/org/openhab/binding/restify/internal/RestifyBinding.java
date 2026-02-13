d4d6ff9677a2f3d38e386487da2abf5dffb9d6ce
Martin Grześlowski <martin.grzeslowski@gmail.com>
2026-02-13 10:21:12 +0100

    Add logging to RestifyBinding for configuration loading
    Signed-off-by: Martin Grześlowski <martin.grzeslowski@gmail.com>

--- bundles/org.openhab.binding.restify/src/main/java/org/openhab/binding/restify/internal/RestifyBinding.java
+++ bundles/org.openhab.binding.restify/src/main/java/org/openhab/binding/restify/internal/RestifyBinding.java
@@ -5,6 +5,8 @@ import java.util.Dictionary;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.osgi.service.cm.ManagedService;
 import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The {@link RestifyBindingConstants} class defines common constants, which are
@@ -15,7 +17,7 @@ import org.osgi.service.component.annotations.Component;
 @NonNullByDefault
 @Component(configurationPid = "binding.restify", service = { RestifyBinding.class, ManagedService.class })
 public class RestifyBinding implements ManagedService {
-
+    private final Logger logger = LoggerFactory.getLogger(RestifyBinding.class);
     private volatile RestifyBindingConfig config = RestifyBindingConfig.DEFAULT;
 
     @Override
@@ -27,6 +29,7 @@ public class RestifyBinding implements ManagedService {
 
         var enforceAuthentication = (Boolean) properties.get("enforceAuthentication");
         this.config = new RestifyBindingConfig(enforceAuthentication != null && enforceAuthentication);
+        logger.debug("Loaded configuration: {}", config);
     }
 
     public RestifyBindingConfig getConfig() {
