b9f404619515978bad1e0463b9e049f29f808faf
Dan Cunningham <dan@digitaldan.com>
2026-02-13 15:01:24 -0800

    [matter] Adds missing Diagnostic Clusters
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

---
+++ bundles/org.openhab.binding.matter/src/main/java/org/openhab/binding/matter/internal/controller/devices/converter/NetworkCommissioningConverter.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2010-2026 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.matter.internal.controller.devices.converter;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.binding.matter.internal.client.dto.cluster.gen.NetworkCommissioningCluster;
+import org.openhab.binding.matter.internal.client.dto.ws.AttributeChangedMessage;
+import org.openhab.binding.matter.internal.handler.MatterBaseThingHandler;
+import org.openhab.core.thing.Channel;
+import org.openhab.core.thing.ChannelGroupUID;
+import org.openhab.core.types.StateDescription;
+
+import com.google.gson.Gson;
+
+/**
+ * A converter for translating {@link NetworkCommissioningCluster} events and attributes to openHAB channels and
+ * back again.
+ *
+ * @author Dan Cunningham - Initial contribution
+ */
+@NonNullByDefault
+public class NetworkCommissioningConverter extends GenericConverter<NetworkCommissioningCluster> {
+    private Gson gson = new Gson();
+
+    public NetworkCommissioningConverter(NetworkCommissioningCluster cluster, MatterBaseThingHandler handler,
+            int endpointNumber, String labelPrefix) {
+        super(cluster, handler, endpointNumber, labelPrefix);
+    }
+
+    @Override
+    public void pollCluster() {
+        handler.readCluster(NetworkCommissioningCluster.class, endpointNumber, initializingCluster.id)
+                .thenAccept(cluster -> {
+                    updateThingProperties(cluster);
+                }).exceptionally(e -> {
+                    logger.debug("Error polling network commissioning", e);
+                    return null;
+                });
+    }
+
+    @Override
+    public Map<Channel, @Nullable StateDescription> createChannels(ChannelGroupUID channelGroupUID) {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public void onEvent(AttributeChangedMessage message) {
+        updateThingAttributeProperty(message.path.attributeName, message.value);
+        super.onEvent(message);
+    }
+
+    @Override
+    public void initState() {
+        logger.debug("initState");
+        updateThingProperties(initializingCluster);
+    }
+
+    private void updateThingProperties(NetworkCommissioningCluster cluster) {
+        updateThingAttributeProperty(NetworkCommissioningCluster.ATTRIBUTE_MAX_NETWORKS, cluster.maxNetworks);
+        updateThingAttributeProperty(NetworkCommissioningCluster.ATTRIBUTE_NETWORKS, gson.toJson(cluster.networks));
+        updateThingAttributeProperty(NetworkCommissioningCluster.ATTRIBUTE_SCAN_MAX_TIME_SECONDS,
+                cluster.scanMaxTimeSeconds);
+        updateThingAttributeProperty(NetworkCommissioningCluster.ATTRIBUTE_CONNECT_MAX_TIME_SECONDS,
+                cluster.connectMaxTimeSeconds);
+        updateThingAttributeProperty(NetworkCommissioningCluster.ATTRIBUTE_INTERFACE_ENABLED, cluster.interfaceEnabled);
+        updateThingAttributeProperty(NetworkCommissioningCluster.ATTRIBUTE_LAST_NETWORKING_STATUS,
+                cluster.lastNetworkingStatus);
+        updateThingAttributeProperty(NetworkCommissioningCluster.ATTRIBUTE_LAST_NETWORK_ID,
+                gson.toJson(cluster.lastNetworkId));
+        updateThingAttributeProperty(NetworkCommissioningCluster.ATTRIBUTE_LAST_CONNECT_ERROR_VALUE,
+                cluster.lastConnectErrorValue);
+        updateThingAttributeProperty(NetworkCommissioningCluster.ATTRIBUTE_SUPPORTED_WI_FI_BANDS,
+                cluster.supportedWiFiBands != null ? gson.toJson(cluster.supportedWiFiBands) : null);
+        updateThingAttributeProperty(NetworkCommissioningCluster.ATTRIBUTE_SUPPORTED_THREAD_FEATURES,
+                cluster.supportedThreadFeatures != null ? gson.toJson(cluster.supportedThreadFeatures) : null);
+        updateThingAttributeProperty(NetworkCommissioningCluster.ATTRIBUTE_THREAD_VERSION,
+                cluster.threadVersion != null ? cluster.threadVersion : null);
+    }
+}
