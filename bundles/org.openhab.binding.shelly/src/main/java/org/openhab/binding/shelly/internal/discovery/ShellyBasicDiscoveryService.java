b6b9ddc4475c0781fa8fae1ec63142358bc250f3
Ravi Nadahar <nadahar@rediffmail.com>
2026-02-13 22:15:55 +0100

    Avoid initializing WebSocket during discovery
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>

--- bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/discovery/ShellyBasicDiscoveryService.java
+++ bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/discovery/ShellyBasicDiscoveryService.java
@@ -24,7 +24,6 @@ import java.util.TreeMap;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.jetty.client.HttpClient;
-import org.eclipse.jetty.websocket.client.WebSocketClient;
 import org.openhab.binding.shelly.internal.api.ShellyApiException;
 import org.openhab.binding.shelly.internal.api.ShellyApiResult;
 import org.openhab.binding.shelly.internal.api.ShellyDeviceProfile;
@@ -126,7 +125,6 @@ public class ShellyBasicDiscoveryService extends AbstractDiscoveryService {
         String deviceName = "";
         String thingType = "";
         Map<String, Object> properties = new TreeMap<>();
-        WebSocketClient webSocketClient = null;
 
         try {
             ShellyThingConfiguration config = fillConfig(bindingConfig, ipAddress, name);
@@ -169,14 +167,6 @@ public class ShellyBasicDiscoveryService extends AbstractDiscoveryService {
         } finally {
             if (api != null) {
                 api.close();
-            }
-            if (webSocketClient != null) {
-                // TODO: Temporary code to be refactored away
-                try {
-                    webSocketClient.stop();
-                } catch (Exception e) {
-                    logger.warn("Discovery: Unable to stop websocket client: {}", e.getMessage(), e);
-                }
             }
         }
 
