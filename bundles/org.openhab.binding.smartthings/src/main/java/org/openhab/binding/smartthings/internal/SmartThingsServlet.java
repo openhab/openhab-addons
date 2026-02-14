b378b31bb7ca1dec788dd2b012a3b4e1c935d32f
Laurent ARNAL <laurent@clae.net>
2026-02-14 16:36:41 +0100

    spotless:apply
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/SmartThingsServlet.java
+++ bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/SmartThingsServlet.java
@@ -409,5 +409,4 @@ public class SmartThingsServlet extends HttpServlet {
             }
         }
     }
-
 }
--- bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/handler/SmartThingsCloudBridgeHandler.java
+++ bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/handler/SmartThingsCloudBridgeHandler.java
@@ -65,7 +65,6 @@ public class SmartThingsCloudBridgeHandler extends SmartThingsBridgeHandler {
     public void initialize() {
         updateStatus(ThingStatus.UNKNOWN);
         super.initialize();
-
     }
 
     @Override
