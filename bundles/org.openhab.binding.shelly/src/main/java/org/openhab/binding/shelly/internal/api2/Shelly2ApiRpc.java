a6b7ea6e3a60d732532438e2a42770db945c335e
Markus Michels <markus7017@gmail.com>
2026-02-20 19:24:21 +0100

    remove WS poll, do async GetStatus only once and only for alwaysOn devices
    Signed-off-by: Markus Michels <markus7017@gmail.com>

--- bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/api2/Shelly2ApiRpc.java
+++ bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/api2/Shelly2ApiRpc.java
@@ -175,7 +175,6 @@ public class Shelly2ApiRpc extends Shelly2ApiClient implements ShellyApiInterfac
         if (profile.device.type == null) {
             profile.device = getDeviceInfo();
         }
-        boolean wasInitialized = profile.initialized;
 
         Shelly2GetConfigResult dc = apiRequest(SHELLYRPC_METHOD_GETCONFIG, null, Shelly2GetConfigResult.class);
         profile.settingsJson = gson.toJson(dc);
@@ -336,7 +335,7 @@ public class Shelly2ApiRpc extends Shelly2ApiClient implements ShellyApiInterfac
         }
 
         if (!profile.initialized && profile.alwaysOn) {
-            getStatus(false); // make sure profile.status is initialized (e.g,. relay/meter status)
+            getStatus(); // make sure profile.status is initialized (e.g,. relay/meter status)
             asyncApiRequest(SHELLYRPC_METHOD_GETSTATUS); // request periodic status updates from device
         }
         profile.initialized = true;
@@ -795,7 +794,7 @@ public class Shelly2ApiRpc extends Shelly2ApiClient implements ShellyApiInterfac
     }
 
     @Override
-    public ShellySettingsStatus getStatus(boolean ping) throws ShellyApiException {
+    public ShellySettingsStatus getStatus() throws ShellyApiException {
         ShellyDeviceProfile profile = getProfile();
         ShellySettingsStatus status = profile.status;
         Shelly2DeviceStatusResult ds = apiRequest(SHELLYRPC_METHOD_GETSTATUS, null, Shelly2DeviceStatusResult.class);
@@ -858,9 +857,6 @@ public class Shelly2ApiRpc extends Shelly2ApiClient implements ShellyApiInterfac
             }
         }
 
-        if (ping) {
-            asyncApiRequest(SHELLYRPC_METHOD_GETSTATUS); // request periodic status updates from device
-        }
         return status;
     }
 
@@ -872,7 +868,7 @@ public class Shelly2ApiRpc extends Shelly2ApiClient implements ShellyApiInterfac
     public ShellyStatusRelay getRelayStatus(int relayIndex) throws ShellyApiException {
         if (getProfile().status.wifiSta.ssid == null) {
             // Update status when not yet initialized
-            getStatus(false);
+            getStatus();
         }
         return relayStatus;
     }
