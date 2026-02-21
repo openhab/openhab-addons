091e48f7bebd07662a17ad7d61e041c70345e57c
Markus Michels <markus7017@gmail.com>
2026-02-21 14:38:00 +0100

    removed "old" Shely2ApiRc constructor for discovery service (no longer used)
    Signed-off-by: Markus Michels <markus7017@gmail.com>

--- bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/api2/Shelly2ApiRpc.java
+++ bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/api2/Shelly2ApiRpc.java
@@ -32,7 +32,6 @@ import java.util.stream.Collectors;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.io.EofException;
 import org.eclipse.jetty.websocket.api.StatusCode;
@@ -128,21 +127,6 @@ public class Shelly2ApiRpc extends Shelly2ApiClient implements ShellyApiInterfac
     public Shelly2ApiRpc(String thingName, ShellyThingTable thingTable, ShellyThingInterface thing,
             WebSocketClient webSocketClient) {
         super(thingName, thing);
-        this.thingTable = thingTable;
-        this.client = webSocketClient;
-    }
-
-    /**
-     * Simple initialization - called by discovery handler
-     *
-     * @param thingName Symbolic thing name
-     * @param config Thing Configuration
-     * @param httpClient HTTP Client to be passed to ShellyHttpClient
-     */
-    public Shelly2ApiRpc(String thingName, ShellyThingTable thingTable, ShellyThingConfiguration config,
-            HttpClient httpClient, WebSocketClient webSocketClient) {
-        super(thingName, config, httpClient);
-        this.thingName = thingName;
         this.thingTable = thingTable;
         this.client = webSocketClient;
     }
