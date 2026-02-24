a4d119204357d88bed8d1fb6d652e102f6668e6f
gael@lhopital.org <gael@lhopital.org>
2026-02-24 10:53:02 +0100

    lolodomo's code review
    Signed-off-by: gael@lhopital.org <gael@lhopital.org>

--- bundles/org.openhab.binding.ecowatt/src/main/java/org/openhab/binding/ecowatt/internal/restapi/EcowattRestApi.java
+++ bundles/org.openhab.binding.ecowatt/src/main/java/org/openhab/binding/ecowatt/internal/restapi/EcowattRestApi.java
@@ -104,10 +104,12 @@ public class EcowattRestApi {
             if (response.getHeaders().contains(HttpHeader.RETRY_AFTER)) {
                 try {
                     retryAfter = Integer.parseInt(response.getHeaders().get(HttpHeader.RETRY_AFTER));
+                    throw new EcowattApiLimitException(retryAfter, "@text/exception.api-limit-reached-params",
+                            retryAfter);
                 } catch (NumberFormatException e) {
+                    throw new EcowattApiLimitException(retryAfter, "@text/exception.api-limit-reached");
                 }
             }
-            throw new EcowattApiLimitException(retryAfter, "@text/exception.api-limit-reached", retryAfter);
         } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR_500) {
             int retryAfter = 60;
             throw new EcowattApiLimitException(retryAfter, "@text/exception.server-error", retryAfter);
@@ -115,7 +117,6 @@ public class EcowattRestApi {
             throw new CommunicationException("@text/exception.api-request-failed-params", statusCode,
                     response.getContentAsString());
         }
-
         try {
             EcowattApiResponse deserializedResp = gson.fromJson(response.getContentAsString(),
                     EcowattApiResponse.class);
