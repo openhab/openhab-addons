f3aa92009515923afdfc5ace596f839bbe9d914e
Laurent ARNAL <laurent@clae.net>
2026-03-24 17:15:36 +0100

    fix missing headers
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/handler/SmartThingsActions.java
+++ bundles/org.openhab.binding.smartthings/src/main/java/org/openhab/binding/smartthings/internal/handler/SmartThingsActions.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2010-2026 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.smartthings.internal.handler;
 
 import java.util.Map;
@@ -15,6 +27,12 @@ import org.openhab.core.thing.binding.ThingHandler;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
 
+/**
+ * The {@link SmartThingsActions} is responsible for handling commands, which are
+ * sent to one of the channels.
+ *
+ * @author Laurent Arnal - review code for new API
+ */
 @Component(scope = ServiceScope.PROTOTYPE, service = SmartThingsActions.class)
 @ThingActionsScope(name = "smartthings") // Your bindings id is usually the scope
 @NonNullByDefault
