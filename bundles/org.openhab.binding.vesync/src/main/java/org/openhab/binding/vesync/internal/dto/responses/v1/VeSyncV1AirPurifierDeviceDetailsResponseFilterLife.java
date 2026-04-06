8d22d83e25a833ab9f2ab0bd769c6f58e181258d
David Goodyear <david.goodyear@gmail.com>
2026-04-06 02:10:05 +0100

    [veSync] V1 Air Purifier DTO
    [veSync] V1 Air Purifier DTO
    
    Signed-off-by: David Goodyear <david.goodyear@gmail.com>

---
+++ bundles/org.openhab.binding.vesync/src/main/java/org/openhab/binding/vesync/internal/dto/responses/v1/VeSyncV1AirPurifierDeviceDetailsResponseFilterLife.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2010-2026 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.vesync.internal.dto.responses.v1;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * The {@link VeSyncV1AirPurifierDeviceDetailsResponseFilterLife} is a Java class used as a DTO to hold the Vesync's V1
+ * API's
+ * common response data, in regard's to an Air Purifier's Filter Life.
+ *
+ * @author David Goodyear - Initial contribution
+ */
+public class VeSyncV1AirPurifierDeviceDetailsResponseFilterLife {
+    @SerializedName("percent")
+    public int percent;
+
+    public int getPercent() {
+        return percent;
+    }
+}
