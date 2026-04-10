19faa409869a1d50aef7f60d003e039bed8b67bb
Uğur Tafralı <tafraliugur@gmail.com>
2026-04-10 10:48:59 +0300

    [systeminfo] Prevent noisy hostname-resolution logging (#20544)
    * Respect configured log level in systeminfo discovery service
    
    Signed-off-by: lsiepel <leosiepel@gmail.com>
    Signed-off-by: Christoph <36006493+fd0cwp@users.noreply.github.com>

--- bundles/org.openhab.binding.systeminfo/src/main/java/org/openhab/binding/systeminfo/internal/discovery/SystemInfoDiscoveryService.java
+++ bundles/org.openhab.binding.systeminfo/src/main/java/org/openhab/binding/systeminfo/internal/discovery/SystemInfoDiscoveryService.java
@@ -66,7 +66,6 @@ public class SystemInfoDiscoveryService extends AbstractDiscoveryService {
             }
         } catch (UnknownHostException ex) {
             hostname = DEFAULT_THING_ID;
-            logger.info("Hostname can not be resolved. Computer name will be set to the default one: {}",
                     DEFAULT_THING_ID);
         }
 
