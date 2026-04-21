62405b62b56ce3e29e86113845fab975e634ddc5
Martin <martin@grzeslowski.pl>
2026-04-21 11:27:25 +0200

    [bambulab] Add support for tray type: ASA-CF (#20607)
    * [BambuLab / AMS] Unsupported tray type: ASA-CF #20606
    
    Signed-off-by: Martin Grześlowski <martin.grzeslowski@gmail.com>
    Signed-off-by: Christoph <36006493+fd0cwp@users.noreply.github.com>

--- bundles/org.openhab.binding.bambulab/src/test/java/org/openhab/binding/bambulab/internal/TrayTypeTest.java
+++ bundles/org.openhab.binding.bambulab/src/test/java/org/openhab/binding/bambulab/internal/TrayTypeTest.java
@@ -115,6 +115,7 @@ class TrayTypeTest {
     @MethodSource
     void testPerformsCaseInsensitiveComparisonForAllEnumValues(String given, TrayType trayType) {
         assertThat(TrayType.findTrayType(given))//
+                .withFailMessage("For %s findTrayType should return %s", given, trayType)//
                 .isPresent()//
                 .hasValueSatisfying(type -> assertThat(type).isEqualTo(trayType));
     }
@@ -122,9 +123,10 @@ class TrayTypeTest {
     static Stream<Arguments> testPerformsCaseInsensitiveComparisonForAllEnumValues() {
         return Arrays.stream(TrayType.values())//
                 .flatMap(trayType -> Stream.of(//
-                        Arguments.of(trayType.name().toUpperCase(), trayType), //
-                        Arguments.of(trayType.name().toLowerCase(), trayType), //
-                        Arguments.of(trayType.name().charAt(0) + trayType.name().substring(1).toLowerCase(),
+                        Arguments.of(trayType.getTypeName().toUpperCase(), trayType), //
+                        Arguments.of(trayType.getTypeName().toLowerCase(), trayType), //
+                        Arguments.of(
+                                trayType.getTypeName().charAt(0) + trayType.getTypeName().substring(1).toLowerCase(),
                                 trayType)));
     }
 }
