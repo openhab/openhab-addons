8432e9ffd60ab62f66f4f0c44b3e78610f496c13
Mike Jagdis <mjagdis@eris-associates.co.uk>
2026-05-07 15:31:11 +0100

    [tuya] Protocol 3.1 responses are not encrypted
    Signed-off-by: Mike Jagdis <mjagdis@eris-associates.co.uk>

--- bundles/org.openhab.binding.tuya/src/main/java/org/openhab/binding/tuya/internal/local/handlers/TuyaDecoder.java
+++ bundles/org.openhab.binding.tuya/src/main/java/org/openhab/binding/tuya/internal/local/handlers/TuyaDecoder.java
@@ -205,6 +205,7 @@ public class TuyaDecoder extends ByteToMessageDecoder {
             byte[] decodedMessage = switch (protocol) {
                 case V3_5 -> CryptoUtil.decryptAesGcm(payload, sessionKey, header, null);
                 case V3_4 -> CryptoUtil.decryptAesEcb(payload, sessionKey, true);
+                case V3_1 -> payload;
                 default -> CryptoUtil.decryptAesEcb(payload, sessionKey, false);
             };
             if (decodedMessage == null) {
