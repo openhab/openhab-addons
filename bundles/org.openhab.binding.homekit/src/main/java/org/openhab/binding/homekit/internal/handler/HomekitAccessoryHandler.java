784a8db4e9b4784d1385ff0581c7a19a53f18c36
Andrew Fiddian-Green <software@whitebear.ch>
2026-05-19 19:20:15 +0100

    add status description if battery is low
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.binding.homekit/src/main/java/org/openhab/binding/homekit/internal/handler/HomekitAccessoryHandler.java
+++ bundles/org.openhab.binding.homekit/src/main/java/org/openhab/binding/homekit/internal/handler/HomekitAccessoryHandler.java
@@ -68,6 +68,7 @@ import org.openhab.core.thing.ManagedThingProvider;
 import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.ThingStatus;
 import org.openhab.core.thing.ThingStatusDetail;
+import org.openhab.core.thing.ThingStatusInfo;
 import org.openhab.core.thing.ThingUID;
 import org.openhab.core.thing.binding.builder.BridgeBuilder;
 import org.openhab.core.thing.binding.builder.ChannelBuilder;
@@ -927,7 +928,10 @@ public class HomekitAccessoryHandler extends HomekitBaseAccessoryHandler {
                                 && cxx.value instanceof JsonElement element) {
                             State state = convertJsonToState(element, channel);
                             switch (channel.getKind()) {
-                                case STATE -> updateState(channelUID, state);
+                                case STATE -> {
+                                    updateState(channelUID, state);
+                                    updateOnlineStatusDescription(cxx, state);
+                                }
                                 case TRIGGER -> triggerChannel(channelUID, state.toFullString());
                             }
                             // check for snapshot refresh triggers
@@ -1313,5 +1317,33 @@ public class HomekitAccessoryHandler extends HomekitBaseAccessoryHandler {
         // prevent un-pairing (which would erase the pairing keys) while migration is in progress
         return migrating.get() ? ACTION_RESULT_ERROR_FORMAT.formatted("auto-migration in progress")
                 : super.unpairInner();
+    }
+
+    /**
+     * Update thing status description based on the given battery low state. If the battery is low and the description
+     * is null, then the description is set to the respective battery low translatable text. If the battery is OK, and
+     * the description is the low battery translatable text, then the description message is cleared. If the thing is
+     * not ONLINE, or if it does not have a battery low channel then the description is not updated. NOTE: this method
+     * only updates the status description if the thing does not already have a status description for a different
+     * issue.
+     */
+    private void updateOnlineStatusDescription(Characteristic characteristic, State batteryLowState) {
+        ThingStatusInfo statusInfo = getThing().getStatusInfo();
+        if (CharacteristicType.STATUS_LO_BATT.equals(characteristic.getCharacteristicType())
+                && statusInfo.getStatus() == ThingStatus.ONLINE) {
+            String description = statusInfo.getDescription();
+
+            // if battery is OK and description is the low battery text then clear the description
+            if (OpenClosedType.CLOSED.equals(batteryLowState) && TEXT_ONLINE_BATTERY_LOW.equals(description)) {
+                updateStatus(statusInfo.getStatus(), statusInfo.getStatusDetail());
+                return;
+            }
+
+            // if battery is low and description is null then apply the low battery text
+            if (OpenClosedType.OPEN.equals(batteryLowState) && description == null) {
+                updateStatus(statusInfo.getStatus(), statusInfo.getStatusDetail(), TEXT_ONLINE_BATTERY_LOW);
+                return;
+            }
+        }
     }
 }
