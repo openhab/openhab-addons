93041c2b4bd4b0696da476e4420250b36b672ec8
Andrew Fiddian-Green <software@whitebear.ch>
2026-05-19 23:21:11 +0100

    handle blank description
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.binding.tado/src/main/java/org/openhab/binding/tado/internal/handler/TadoZoneHandler.java
+++ bundles/org.openhab.binding.tado/src/main/java/org/openhab/binding/tado/internal/handler/TadoZoneHandler.java
@@ -570,7 +570,7 @@ public class TadoZoneHandler extends BaseHomeThingHandler {
             }
 
             // if battery is low and description is null then apply the low battery text
-            if (OnOffType.ON.equals(batteryLowState) && description == null) {
+            if (OnOffType.ON.equals(batteryLowState) && (description == null || description.isBlank())) {
                 updateStatus(statusInfo.getStatus(), statusInfo.getStatusDetail(), TEXT_ONLINE_BATTERY_LOW);
                 return;
             }
