e75cb57cc00bcd81f658b3d02b19e9f4506fe404
Andrew Fiddian-Green <software@whitebear.ch>
2026-05-19 23:22:51 +0100

    handle blank descfription
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.binding.neohub/src/main/java/org/openhab/binding/neohub/internal/NeoBaseHandler.java
+++ bundles/org.openhab.binding.neohub/src/main/java/org/openhab/binding/neohub/internal/NeoBaseHandler.java
@@ -301,7 +301,7 @@ public class NeoBaseHandler extends BaseThingHandler {
             }
 
             // if battery is low and description is null then apply the low battery text
-            if (OnOffType.ON.equals(batteryLowState) && description == null) {
+            if (OnOffType.ON.equals(batteryLowState) && (description == null || description.isBlank())) {
                 updateStatus(statusInfo.getStatus(), statusInfo.getStatusDetail(), TEXT_ONLINE_BATTERY_LOW);
                 return;
             }
