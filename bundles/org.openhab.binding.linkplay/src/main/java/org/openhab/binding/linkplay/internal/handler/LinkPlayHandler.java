95b20f3a41b8ae904a8d9f89ea502bd956788a58
Dan Cunningham <dan@digitaldan.com>
2026-06-01 09:49:14 -0700

    [linkplay] Set correct thing status on UPNP sub changes (#20838)
    * [linkplay] Set correct thing status on UPNP sub changes
    
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>
    Co-authored-by: lsiepel <leosiepel@gmail.com>
    Signed-off-by: Ciprian Pascu <contact@ciprianpascu.ro>

--- bundles/org.openhab.binding.linkplay/src/main/java/org/openhab/binding/linkplay/internal/handler/LinkPlayHandler.java
+++ bundles/org.openhab.binding.linkplay/src/main/java/org/openhab/binding/linkplay/internal/handler/LinkPlayHandler.java
@@ -581,7 +581,7 @@ public class LinkPlayHandler extends BaseThingHandler
             return;
         }
         if (!allSubscriptionsSuccessful) {
-            updateStatus(ThingStatus.INITIALIZING, ThingStatusDetail.NOT_YET_READY, "Waiting for UPnP subscriptions");
+            updateStatus(ThingStatus.UNKNOWN, ThingStatusDetail.NONE, "Waiting for UPnP subscriptions");
             return;
         }
         if (getThing().getStatus() != ThingStatus.ONLINE) {
