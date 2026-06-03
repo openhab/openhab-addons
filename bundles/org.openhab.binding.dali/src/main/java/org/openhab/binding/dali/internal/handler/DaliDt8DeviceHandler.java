6b47c7fc6a78dd8bbb152d6279f82ed75ce7680f
Holger Friedrich <mail@holger-friedrich.de>
2026-06-03 21:34:44 +0200

    [dali] Fix deprecations in color conversion code (#20854)
    * [dali] Fix deprecations in color conversion code
    
    Signed-off-by: Holger Friedrich <mail@holger-friedrich.de>
    Signed-off-by: Ciprian Pascu <contact@ciprianpascu.ro>

--- bundles/org.openhab.binding.dali/src/main/java/org/openhab/binding/dali/internal/handler/DaliDt8DeviceHandler.java
+++ bundles/org.openhab.binding.dali/src/main/java/org/openhab/binding/dali/internal/handler/DaliDt8DeviceHandler.java
@@ -35,6 +35,7 @@ import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.ThingStatus;
 import org.openhab.core.thing.ThingStatusDetail;
 import org.openhab.core.types.Command;
+import org.openhab.core.util.ColorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -134,7 +135,7 @@ public class DaliDt8DeviceHandler extends DaliDeviceHandler {
                     throw new DaliException("unknown device type");
                 }
                 if (command instanceof HSBType hsbCommand) {
-                    PercentType[] rgb = hsbCommand.toRGB();
+                    PercentType[] rgb = ColorUtil.hsbToRgbPercent(hsbCommand);
                     final int r = (int) (254 * (rgb[0].floatValue() / 100));
                     final int g = (int) (254 * (rgb[1].floatValue() / 100));
                     final int b = (int) (254 * (rgb[2].floatValue() / 100));
