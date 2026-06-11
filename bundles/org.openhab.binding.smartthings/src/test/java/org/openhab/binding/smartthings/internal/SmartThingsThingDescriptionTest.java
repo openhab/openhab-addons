76b0f274e658632e1e7d5b242f3083661f6bbaa4
Kai Kreuzer <kai@openhab.org>
2026-06-11 09:40:12 +0200

    Mark SmartThings dynamic discovery advanced
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

---
+++ bundles/org.openhab.binding.smartthings/src/test/java/org/openhab/binding/smartthings/internal/SmartThingsThingDescriptionTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2010-2026 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.smartthings.internal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * Tests for SmartThings thing description metadata.
+ */
+@NonNullByDefault
+class SmartThingsThingDescriptionTest {
+
+    @Test
+    void dynamicThingDiscoveryIsAdvancedBridgeConfiguration() throws Exception {
+        Document document = parseThingDescription("OH-INF/thing/account.xml");
+
+        Element parameter = findParameter(document, "useDynamicThings");
+
+        assertNotNull(parameter);
+        Node advanced = parameter.getElementsByTagName("advanced").item(0);
+        assertNotNull(advanced);
+        assertEquals("true", advanced.getTextContent());
+    }
+
+    private Document parseThingDescription(String resourceName) throws Exception {
+        InputStream stream = SmartThingsThingDescriptionTest.class.getResourceAsStream("/" + resourceName);
+        assertNotNull(stream);
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setExpandEntityReferences(false);
+        factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        return factory.newDocumentBuilder().parse(stream);
+    }
+
+    private Element findParameter(Document document, String parameterName) {
+        NodeList parameters = document.getElementsByTagName("parameter");
+        for (int i = 0; i < parameters.getLength(); i++) {
+            Element parameter = (Element) parameters.item(i);
+            if (parameterName.equals(parameter.getAttribute("name"))) {
+                return parameter;
+            }
+        }
+        throw new AssertionError("Missing parameter: " + parameterName);
+    }
+}
