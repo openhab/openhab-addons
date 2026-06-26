9d77b7b8b5f8e393bbd8b9ac0cd74d3ec37d8ece
Lee Ballard <ballle98@gmail.com>
2026-06-26 03:58:42 -0500

    [ddwrt] Initial contribution of OH5 DD-WRT/OpenWrt/Tomato SSH based binding (#20505)
    * [ddwrt] New OH4/5 dd-wrt binding
    
    Signed-off-by: Lee Ballard <ballle98@gmail.com>
    Signed-off-by: Ciprian Pascu <contact@ciprianpascu.ro>

---
+++ bundles/org.openhab.binding.ddwrt/src/main/java/org/openhab/binding/ddwrt/internal/api/SyslogListener.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2010-2026 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.ddwrt.internal.api;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.binding.ddwrt.internal.api.SyslogParser.SyslogEvent;
+
+/**
+ * Listener interface for classified syslog events from the SSH log follower.
+ * Modeled after the logreader binding's FileReaderListener pattern.
+ *
+ * @author Lee Ballard - Initial contribution
+ */
+@NonNullByDefault
+public interface SyslogListener {
+
+    /**
+     * Called when a DHCP event is detected (lease, renewal, release).
+     * Processes: dnsmasq-dhcp, dhcpd, udhcpc
+     */
+    void onDhcpEvent(SyslogEvent event);
+
+    /**
+     * Called when a wireless association/deassociation event is detected.
+     * Processes: hostapd, wpa_supplicant, nas
+     */
+    void onWirelessEvent(SyslogEvent event);
+
+    /**
+     * Called when a warning-level event is detected.
+     * Includes firewall IPTABLES log entries and syslog warning priority.
+     */
+    void onWarningEvent(SyslogEvent event);
+
+    /**
+     * Called when an error-level event is detected.
+     * Includes syslog error/critical/emergency priority.
+     */
+    void onErrorEvent(SyslogEvent event);
+}
