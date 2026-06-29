db2efe91a58599d7925f1555952ac50d460a120f
Holger Friedrich <mail@holger-friedrich.de>
2026-06-29 18:14:44 +0200

    rebase
    Signed-off-by: Holger Friedrich <mail@holger-friedrich.de>

--- bundles/org.openhab.binding.unifi/src/main/java/org/openhab/binding/unifi/internal/access/api/UnifiAccessApiClient.java
+++ bundles/org.openhab.binding.unifi/src/main/java/org/openhab/binding/unifi/internal/access/api/UnifiAccessApiClient.java
@@ -44,7 +44,6 @@ import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
-import org.eclipse.jetty.websocket.client.io.UpgradeListener;
 import org.openhab.binding.unifi.internal.access.dto.Device;
 import org.openhab.binding.unifi.internal.access.dto.DeviceAccessMethodSettings;
 import org.openhab.binding.unifi.internal.access.dto.Door;
@@ -56,6 +55,7 @@ import org.openhab.binding.unifi.internal.access.dto.Notification;
 import org.openhab.binding.unifi.internal.access.dto.UnifiAccessApiException;
 import org.openhab.binding.unifi.internal.api.UniFiException.AuthState;
 import org.openhab.binding.unifi.internal.api.UniFiSession;
+import org.openhab.binding.unifi.internal.api.UniFiWebSocketUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -965,24 +965,16 @@ public final class UnifiAccessApiClient implements Closeable {
                 }
             };
 
-            // The upgrade HTTP status is delivered here directly; a 401 means an expired session cookie.
-            UpgradeListener upgradeListener = new UpgradeListener() {
-                @Override
-                @NonNullByDefault({})
-                public void onHandshakeRequest(UpgradeRequest request) {
-                }
-
-                @Override
-                @NonNullByDefault({})
-                public void onHandshakeResponse(UpgradeResponse response) {
-                    int status = response.getStatusCode();
-                    logger.debug("Notifications WebSocket upgrade response: {}", status);
-                    if (allowReauth && status == HttpStatus.UNAUTHORIZED_401) {
+            // In Jetty 12 UpgradeListener was removed; a failed upgrade (e.g. HTTP 401) surfaces as an
+            // exceptional completion of the Future returned by connect().
+            wsClient.connect(socket, wsUri, req).whenComplete((session, throwable) -> {
+                if (throwable != null) {
+                    logger.debug("Notifications WebSocket upgrade failed: {}", throwable.getMessage());
+                    if (allowReauth && UniFiWebSocketUtil.isUnauthorizedUpgrade(throwable)) {
                         executorService.execute(() -> reauthenticateAndReconnect(onOpen, onMessage, onError));
                     }
                 }
-            };
-            wsClient.connect(socket, wsUri, req, upgradeListener);
+            });
         } catch (IOException e) {
             throw new UnifiAccessApiException("WebSocket connect failed: " + e.getMessage(), e);
         }
