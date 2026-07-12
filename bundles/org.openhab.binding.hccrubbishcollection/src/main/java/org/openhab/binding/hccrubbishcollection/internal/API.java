6c44d15c33b9469640b4c1615a17394b29fcefca
Stewart Cossey <stewart.cossey@gmail.com>
2026-07-12 19:44:15 +1200

    Update API URL
    The API url has changed. Also added in staging URL, but unused at the moment.
    
    Signed-off-by: Stewart Cossey <stewart.cossey@gmail.com>

--- bundles/org.openhab.binding.hccrubbishcollection/src/main/java/org/openhab/binding/hccrubbishcollection/internal/API.java
+++ bundles/org.openhab.binding.hccrubbishcollection/src/main/java/org/openhab/binding/hccrubbishcollection/internal/API.java
@@ -41,7 +41,8 @@ import com.google.gson.JsonParser;
 @NonNullByDefault
 public class API {
     private static final int REQUEST_TIMEOUT = 10;
-    private static final String REQUEST_URL = "https://api.hcc.govt.nz/FightTheLandFill/get_Collection_Dates?address_string=";
+    private static final String REQUEST_URL = "https://api2.hcc.govt.nz/FightTheLandFill/get_Collection_Dates?address_string=";
+    private static final String REQUEST_URL_STAGING = "https://apim-hcc-it-ops-nonprod.azure-api.net/FightTheLandFill/get_Collection_Dates?address_string=";
     private static final int HTTP_OK = 200;
 
     private final Logger logger = LoggerFactory.getLogger(API.class);
