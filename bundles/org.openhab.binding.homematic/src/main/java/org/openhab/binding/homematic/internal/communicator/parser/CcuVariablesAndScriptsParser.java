695d61deb12fe31c558f3dd0c0de0503a079a117
Leo Siepel <leosiepel@gmail.com>
2026-07-17 21:35:54 +0200

    review comment, consolidate convertToType
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/communicator/parser/CcuVariablesAndScriptsParser.java
+++ bundles/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/communicator/parser/CcuVariablesAndScriptsParser.java
@@ -44,11 +44,11 @@ public class CcuVariablesAndScriptsParser extends CommonRpcParser<TclScriptDataL
             for (TclScriptDataEntry entry : entries) {
                 HmDatapoint dp = channel.getDatapoint(HmParamsetType.VALUES, entry.name);
                 if (dp != null) {
-                    dp.setValue(convertToType(dp, entry.value));
+                    dp.setValue(convertToType(dp.getType(), entry.value));
                 } else {
-                    dp = new HmDatapoint(entry.name, entry.description, HmValueType.parse(entry.valueType), null,
-                            entry.readOnly, HmParamsetType.VALUES);
-                    dp.setValue(convertToType(dp, entry.value));
+                    HmValueType valueType = HmValueType.parse(entry.valueType);
+                    dp = new HmDatapoint(entry.name, entry.description, valueType,
+                            convertToType(valueType, entry.value), entry.readOnly, HmParamsetType.VALUES);
                     dp.setInfo(entry.name);
                     if (dp.isIntegerType()) {
                         dp.setMinValue(toInteger(entry.minValue));
