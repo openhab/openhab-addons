1ebf92e98958b3d6d5dd4444f207f0b5acf3aeb4
Andrew Fiddian-Green <software@whitebear.ch>
2026-07-24 19:53:14 +0100

    improve color temperature command loop back
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.binding.hue/src/main/java/org/openhab/binding/hue/internal/handler/Clip2ThingHandler.java
+++ bundles/org.openhab.binding.hue/src/main/java/org/openhab/binding/hue/internal/handler/Clip2ThingHandler.java
@@ -673,13 +673,26 @@ public class Clip2ThingHandler extends BaseThingHandler {
     /*
      * Many channels have 'autoUpdatePolicy == veto' since we normally expect the bridge to immediately send a
      * state update event after a PUT command. However if the bridge returned a "succeeded with error" message
-     * then, depending on the manufacturer, it may not send a state update, so we send ourself a mock event via
-     * loop-back in order to update the channel faster.
+     * (HTTP status code 207) then, depending on the manufacturer, it may not send a state update, so we send
+     * ourself a mock event via loop-back in order to update the channel faster.
      */
     private void loopBackNotify(Resource resource) {
         scheduler.submit(() -> {
             logger.debug("{} -> loopBackNotify() with resource {}", resourceId, resource);
-            onResource(resource.setContentType(ContentType.UPDATE));
+            Resource clone = resource.clone().setContentType(ContentType.UPDATE);
+
+            /*
+             * If looping back a color temperature command then add the approximate Planckian locus XY value to
+             * the resource to keep the color channel state XY channels in synch with the color temperature. The
+             * XY value will be more precisely updated by the bridge on the next real SSE event.
+             */
+            if (ResourceType.LIGHT == clone.getType()
+                    && clone.getColorTemperature() instanceof ColorTemperature colorTemp
+                    && colorTemp.getMirek() instanceof Long mirek && clone.getColorXy() == null) {
+                clone.setColorXy(new ColorXy().setXY(ColorUtil.kelvinToXY(1000000.0 / mirek)));
+            }
+
+            onResource(clone);
         });
     }
 
