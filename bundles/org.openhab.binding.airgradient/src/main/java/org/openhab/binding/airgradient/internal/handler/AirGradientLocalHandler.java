2df702fb667f71f2505c456d1d73237d4e417e6c
Jørgen Austvik <jaustvik@acm.org>
2026-08-26 07:49:11 +0200

    [airgradient] Support measures added for firmwares up to v3.1.21 (#18430)
    Also-by: Jørgen Austvik <jaustvik@acm.org>
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.airgradient/src/main/java/org/openhab/binding/airgradient/internal/handler/AirGradientLocalHandler.java
+++ bundles/org.openhab.binding.airgradient/src/main/java/org/openhab/binding/airgradient/internal/handler/AirGradientLocalHandler.java
@@ -36,6 +36,7 @@ import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.ThingStatus;
 import org.openhab.core.thing.ThingStatusDetail;
 import org.openhab.core.thing.binding.BaseThingHandler;
+import org.openhab.core.thing.binding.ThingHandlerCallback;
 import org.openhab.core.thing.binding.builder.ThingBuilder;
 import org.openhab.core.types.Command;
 import org.openhab.core.types.RefreshType;
@@ -62,6 +63,7 @@ public class AirGradientLocalHandler extends BaseThingHandler {
 
     private @NonNullByDefault({}) RemoteAPIController apiController = null;
     private @NonNullByDefault({}) AirGradientAPIConfiguration apiConfig = null;
+    private @Nullable String cachedDeviceSignature;
 
     public AirGradientLocalHandler(Thing thing, HttpClient httpClient) {
         super(thing);
@@ -152,7 +154,7 @@ public class AirGradientLocalHandler extends BaseThingHandler {
                     "Need to set hostname to a valid URL. Refresh interval needs to be a positive integer.");
             return;
         }
-
+        cachedDeviceSignature = null;
         apiController = new RemoteAPIController(httpClient, gson, apiConfig);
 
         // set the thing status to UNKNOWN temporarily and let the background task decide for the real status.
@@ -175,6 +177,15 @@ public class AirGradientLocalHandler extends BaseThingHandler {
             }
 
             Measure measure = measures.get(0);
+            ThingHandlerCallback callback = getCallback();
+            if (callback != null) {
+                ThingBuilder measurementBuilder = DynamicChannelHelper.updateThingWithMeasurementChannels(thing,
+                        callback, null, this::editThing, measure);
+                if (measurementBuilder != null) {
+                    updateThing(measurementBuilder.build());
+                }
+            }
+
             updateProperties(MeasureHelper.createProperties(measure));
             Map<String, State> states = MeasureHelper.createStates(measure);
             for (Map.Entry<String, State> entry : states.entrySet()) {
@@ -183,12 +194,18 @@ public class AirGradientLocalHandler extends BaseThingHandler {
                 }
             }
 
+            String deviceSignature = DynamicChannelHelper
+                    .getDynamicChannelCapabilitySignature(measure.getFirmwareVersion(), measure.getModel());
+            boolean deviceSignatureChanged = deviceSignature == null || !deviceSignature.equals(cachedDeviceSignature);
             LocalConfiguration localConfig = apiController.getConfig();
             if (localConfig != null) {
-                // If we are able to read config, we add config channels
-                ThingBuilder builder = DynamicChannelHelper.updateThingWithConfigurationChannels(thing, editThing());
-                updateThing(builder.build());
-
+                if (deviceSignatureChanged && callback != null) {
+                    ThingBuilder builder = DynamicChannelHelper.updateThingWithConfigurationChannels(thing, callback,
+                            null, this::editThing, localConfig);
+                    if (builder != null) {
+                        updateThing(builder.build());
+                    }
+                }
                 updateProperties(ConfigurationHelper.createProperties(localConfig));
                 Map<String, State> configStates = ConfigurationHelper.createStates(localConfig);
                 for (Map.Entry<String, State> entry : configStates.entrySet()) {
@@ -197,6 +214,7 @@ public class AirGradientLocalHandler extends BaseThingHandler {
                     }
                 }
             }
+            cachedDeviceSignature = deviceSignature;
 
         } catch (AirGradientCommunicationException agce) {
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR, agce.getMessage());
@@ -225,5 +243,9 @@ public class AirGradientLocalHandler extends BaseThingHandler {
 
     protected void setConfiguration(AirGradientAPIConfiguration config) {
         this.apiConfig = config;
+    }
+
+    protected void setApiController(RemoteAPIController apiController) {
+        this.apiController = apiController;
     }
 }
--- bundles/org.openhab.binding.airgradient/src/main/java/org/openhab/binding/airgradient/internal/model/LocalConfiguration.java
+++ bundles/org.openhab.binding.airgradient/src/main/java/org/openhab/binding/airgradient/internal/model/LocalConfiguration.java
@@ -23,53 +23,39 @@ import org.eclipse.jdt.annotation.Nullable;
 @NonNullByDefault
 public class LocalConfiguration {
 
-    @Nullable
-    public String country; // ALPHA-2 Country code
+    public @Nullable String country; // ALPHA-2 Country code
 
-    @Nullable
-    public String pmStandard; // usaqi/ugm3
+    public @Nullable String pmStandard; // usaqi/ugm3
 
-    @Nullable
-    public String ledBarMode; // off, pm, co2
+    public @Nullable String ledBarMode; // off, pm, co2
 
-    @Nullable
-    public Long abcDays; // Co2 calibration automatic baseline calibration days ( 0-200)
+    public @Nullable Long abcDays; // Co2 calibration automatic baseline calibration days ( 0-200)
 
-    @Nullable
-    public Long tvocLearningOffset; // Time constant of long-term estimator for offset. Past events will be forgotten
-                                    // after about twice the learning time. Range 1..1000 [hours]
+    public @Nullable Long tvocLearningOffset; // Time constant of long-term estimator for offset. Past events will be
+                                              // forgotten
+    // after about twice the learning time. Range 1..1000 [hours]
 
-    @Nullable
-    public Long noxLearningOffset; // Time constant of long-term estimator for offset. Past events will be forgotten
-                                   // after about twice the learning time. Range 1..1000 [hours]
+    public @Nullable Long noxLearningOffset; // Time constant of long-term estimator for offset. Past events will be
+                                             // forgotten
+    // after about twice the learning time. Range 1..1000 [hours]
 
-    @Nullable
-    public String mqttBrokerUrl;
+    public @Nullable String mqttBrokerUrl;
 
-    @Nullable
-    public String temperatureUnit; // c/f
+    public @Nullable String temperatureUnit; // c/f
 
-    @Nullable
-    public String configurationControl; // local, cloud, both
+    public @Nullable String configurationControl; // local, cloud, both
 
-    @Nullable
-    public Boolean postDataToAirGradient;
+    public @Nullable Boolean postDataToAirGradient;
 
-    @Nullable
-    public Long ledBarBrightness; // 0 - 100
+    public @Nullable Long ledBarBrightness; // 0 - 100
 
-    @Nullable
-    public Long displayBrightness; // 0 - 100
+    public @Nullable Long displayBrightness; // 0 - 100
 
-    @Nullable
-    public Boolean offlineMode; // Don't connect to wifi
+    public @Nullable Boolean offlineMode; // Don't connect to wifi
 
-    @Nullable
-    public String model;
+    public @Nullable String model;
 
-    @Nullable
-    public Boolean co2CalibrationRequested; // TRIGGER: Calibration of Co2 sensor
+    public @Nullable Boolean co2CalibrationRequested; // TRIGGER: Calibration of Co2 sensor
 
-    @Nullable
-    public Boolean ledBarTestRequested; // TRIGGER: LEDs will run test sequence
+    public @Nullable Boolean ledBarTestRequested; // TRIGGER: LEDs will run test sequence
 }
