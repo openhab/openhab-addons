ed22071fabfbdf81e255d955f026df39f22f287d
Markus Michels <markus7017@gmail.com>
2026-09-12 12:16:52 +0200

    [shelly] Skip a status refresh which is still running while the Thing is disposed
    Cancelling the status job only interrupts it, a refresh cycle which is already inside the API call runs to
    completion and updated the Thing status afterwards. It now bails out when the handler is shutting down.
    
    Signed-off-by: Markus Michels <markus7017@gmail.com>

--- bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/handler/ShellyBaseHandler.java
+++ bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/handler/ShellyBaseHandler.java
@@ -634,6 +634,10 @@ public abstract class ShellyBaseHandler extends BaseThingHandler
      * Update device status and channels
      */
     protected void refreshStatus() {
+        if (stopping) {
+            // cancel(true) only interrupts the job, a cycle which is already running has to bail out itself
+            return;
+        }
         try {
             if (vibrationFilter > 0) {
                 vibrationFilter--;
