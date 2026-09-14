52719d146a3454e781272c6a02db983dc0de3884
Richard Koshak <rlkoshak@gmail.com>
2026-09-14 11:04:24 -0600

    Extrtact DTOs into their own files.
    Signed-off-by: Richard Koshak <rlkoshak@gmail.com>

---
+++ bundles/org.openhab.binding.eyeonwater/src/main/java/org/openhab/binding/eyeonwater/internal/api/LeakDTO.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2010-2026 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 WHICH is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.eyeonwater.internal.api;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * GSON DTO representing leak details.
+ *
+ * @author Richard Koshak - Initial contribution
+ */
+@NonNullByDefault
+class LeakDTO {
+    @Nullable Double rate;
+}
