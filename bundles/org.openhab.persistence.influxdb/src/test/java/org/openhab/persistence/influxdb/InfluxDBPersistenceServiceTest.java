7c941316a49403476629240c11b7be0acaccf4b7
cedricboon <cedric.boon@hotmail.com>
2026-09-19 08:38:49 +0200

    [influxdb] Add support for InfluxDB 3.0 (#21644)
    * Added Influxdb V3 support
    
    Signed-off-by: Cedric Boon <cedric.boon@hotmail.com>

--- bundles/org.openhab.persistence.influxdb/src/test/java/org/openhab/persistence/influxdb/InfluxDBPersistenceServiceTest.java
+++ bundles/org.openhab.persistence.influxdb/src/test/java/org/openhab/persistence/influxdb/InfluxDBPersistenceServiceTest.java
@@ -74,6 +74,26 @@ public class InfluxDBPersistenceServiceTest {
             DATABASE_PARAM, "openhab", //
             RETENTION_POLICY_PARAM, "default");
 
+    private static final Map<String, Object> VALID_V3_CONFIGURATION = Map.of( //
+            URL_PARAM, "http://localhost:8181", //
+            VERSION_PARAM, InfluxDBVersion.V3.name(), //
+            TOKEN_PARAM, "sampletoken", //
+            DATABASE_PARAM, "openhab", //
+            RETENTION_POLICY_PARAM, "default");
+
+    private static final Map<String, Object> INVALID_V3_CONFIGURATION = Map.of( //
+            URL_PARAM, "http://localhost:8181", //
+            VERSION_PARAM, InfluxDBVersion.V3.name(), //
+            DATABASE_PARAM, "openhab", //
+            RETENTION_POLICY_PARAM, "default");
+
+    private static final Map<String, Object> VALID_V3_CONFIGURATION_WITHOUT_RETENTION_POLICY = Map.of( //
+            URL_PARAM, "http://localhost:8181", //
+            VERSION_PARAM, InfluxDBVersion.V3.name(), //
+            TOKEN_PARAM, "sampletoken", //
+            DATABASE_PARAM, "openhab", //
+            RETENTION_POLICY_PARAM, "");
+
     private @Mock @NonNullByDefault({}) InfluxDBRepository influxDBRepositoryMock;
 
     private final InfluxDBMetadataService influxDBMetadataService = new InfluxDBMetadataService(
@@ -92,6 +112,19 @@ public class InfluxDBPersistenceServiceTest {
     }
 
     @Test
+    public void activateWithValidV3ConfigShouldConnectRepository() {
+        getService(VALID_V3_CONFIGURATION);
+        verify(influxDBRepositoryMock).connect();
+    }
+
+    @Test
+    public void activateWithValidV3ConfigWithoutRetentionPolicyShouldConnectRepository() {
+        // InfluxDB 3 databases have no retention-policy concept, so an explicitly empty value must still be valid.
+        getService(VALID_V3_CONFIGURATION_WITHOUT_RETENTION_POLICY);
+        verify(influxDBRepositoryMock).connect();
+    }
+
+    @Test
     public void activateWithInvalidV1ConfigShouldFail() {
         assertThrows(IllegalArgumentException.class, () -> getService(INVALID_V1_CONFIGURATION));
     }
@@ -99,6 +132,11 @@ public class InfluxDBPersistenceServiceTest {
     @Test
     public void activateWithInvalidV2ShouldFail() {
         assertThrows(IllegalArgumentException.class, () -> getService(INVALID_V2_CONFIGURATION));
+    }
+
+    @Test
+    public void activateWithInvalidV3ShouldFail() {
+        assertThrows(IllegalArgumentException.class, () -> getService(INVALID_V3_CONFIGURATION));
     }
 
     @Test
