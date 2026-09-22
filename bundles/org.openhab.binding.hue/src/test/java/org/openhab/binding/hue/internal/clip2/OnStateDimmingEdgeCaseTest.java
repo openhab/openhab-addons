29d9741916156e028fde99e894a3786d8f15399b
Jacob Laursen <jacob-github@vindvejr.dk>
2026-09-22 22:36:47 +0200

    Fix switch and brightness state handling for dimmable lights
    Resolves #21742
    
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>

--- bundles/org.openhab.binding.hue/src/test/java/org/openhab/binding/hue/internal/clip2/OnStateDimmingEdgeCaseTest.java
+++ bundles/org.openhab.binding.hue/src/test/java/org/openhab/binding/hue/internal/clip2/OnStateDimmingEdgeCaseTest.java
@@ -18,14 +18,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.junit.jupiter.api.Test;
+import org.openhab.binding.hue.internal.api.dto.clip2.ColorXy;
 import org.openhab.binding.hue.internal.api.dto.clip2.Dimming;
 import org.openhab.binding.hue.internal.api.dto.clip2.OnState;
 import org.openhab.binding.hue.internal.api.dto.clip2.Resource;
 import org.openhab.binding.hue.internal.api.dto.clip2.enums.ResourceType;
 import org.openhab.binding.hue.internal.exceptions.CriticalFieldMissingException;
+import org.openhab.core.library.types.HSBType;
 import org.openhab.core.library.types.OnOffType;
 import org.openhab.core.library.types.PercentType;
 import org.openhab.core.types.UnDefType;
+import org.openhab.core.util.ColorUtil.Gamut;
 
 /**
  * JUnit test for edge cases of OnState and Dimming event and cache resources.
@@ -71,18 +74,29 @@ class OnStateDimmingEdgeCaseTest {
     }
 
     @Test
-    void getBrightnessStateWhenOnAndDimmingMinus1Return0() throws CriticalFieldMissingException {
-        assertThat(createLightResource(true, -1.0).getBrightnessState(), is(equalTo(PercentType.ZERO)));
+    void getBrightnessStateWhenOnAndDimmingMinus1ReturnMinBrightnessWhenOn() throws CriticalFieldMissingException {
+        assertThat(createLightResource(true, -1.0).getBrightnessState(), is(equalTo(new PercentType("0.01"))));
     }
 
     @Test
-    void getBrightnessStateWhenOnAndDimming0Return0() throws CriticalFieldMissingException {
-        assertThat(createLightResource(true, 0.0).getBrightnessState(), is(equalTo(PercentType.ZERO)));
+    void getBrightnessStateWhenOnAndDimming0ReturnMinBrightnessWhenOn() throws CriticalFieldMissingException {
+        assertThat(createLightResource(true, 0.0).getBrightnessState(), is(equalTo(new PercentType("0.01"))));
     }
 
     @Test
-    void getBrightnessStateWhenOnAndDimming0AndCustomMinimumBrightnessReturn0() throws CriticalFieldMissingException {
-        assertThat(createLightResource(true, 0.0).getBrightnessState(), is(equalTo(PercentType.ZERO)));
+    void getBrightnessStateWhenOnAndDimming0AndCustomMinimumBrightnessReturnMinBrightnessWhenOn()
+            throws CriticalFieldMissingException {
+        assertThat(createLightResource(true, 0.0).getBrightnessState(), is(equalTo(new PercentType("0.01"))));
+    }
+
+    @Test
+    void getColorStateWhenOnAndDimming0ReturnMinBrightnessWhenOn() throws CriticalFieldMissingException {
+        assertThat(createLightResource(true, 0.0).getColorState(), is(equalTo(new HSBType("31.146,87.39200,0.01"))));
+    }
+
+    @Test
+    void getColorStateWhenOffAndDimming100ReturnBrightness0() throws CriticalFieldMissingException {
+        assertThat(createLightResource(false, 100.0).getColorState(), is(equalTo(new HSBType("31.146,87.39200,0"))));
     }
 
     @Test
@@ -102,28 +116,29 @@ class OnStateDimmingEdgeCaseTest {
     }
 
     @Test
-    void getTwoStatesWhenOnAndDimming0ReturnOff() throws CriticalFieldMissingException {
-        // test "soft off": evaluation yields OnOffType.OFF state and brightness verbatim
+    void getTwoStatesWhenOnAndDimming0ReturnOn() throws CriticalFieldMissingException {
+        // test "soft off": evaluation yields OnOffType.ON state and brightness verbatim
         Resource res = createLightResource(true, 0.0);
-        assertThat(res.getBrightnessState(), is(equalTo(PercentType.ZERO)));
-        assertThat(res.getSwitchState(), is(equalTo(OnOffType.OFF)));
+        assertThat(res.getBrightnessState(), is(equalTo(new PercentType("0.01"))));
+        assertThat(res.getSwitchState(), is(equalTo(OnOffType.ON)));
     }
 
     private Resource createLightResource(@Nullable Boolean on, @Nullable Double brightness) {
         Resource resource = new Resource(ResourceType.LIGHT);
 
         if (on != null) {
-            OnState onState = new OnState();
-            onState.setOn(on);
+            OnState onState = new OnState().setOn(on);
             resource.setOnState(onState);
         }
 
         if (brightness != null) {
-            Dimming dimming = new Dimming();
-            dimming.setBrightness(brightness);
-
+            Dimming dimming = new Dimming().setBrightness(brightness);
             resource.setDimming(dimming);
         }
+
+        var color = new ColorXy().setXY(new double[] { 0.5791, 0.3918 }).setGamut(new Gamut(
+                new double[] { 0.6915, 0.3083 }, new double[] { 0.17, 0.7 }, new double[] { 0.1532, 0.0475 }));
+        resource.setColorXy(color);
 
         return resource;
     }
