576d5d373fc7b624ced86c26c84f422ed1f08b4c
Mark Herwege <mark.herwege@telenet.be>
2026-09-26 12:55:50 +0200

    spotless
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.binding.lghorizon/src/main/java/org/openhab/binding/lghorizon/internal/handler/LGHorizonAccountHandler.java
+++ bundles/org.openhab.binding.lghorizon/src/main/java/org/openhab/binding/lghorizon/internal/handler/LGHorizonAccountHandler.java
@@ -501,7 +501,7 @@ public class LGHorizonAccountHandler extends BaseBridgeHandler implements LGHori
         }
 
         this.languageByProfileId = profiles.stream().filter(p -> p.profileId != null)
-                .collect(Collectors.<CustomerDto.ProfileDto, String, String>toMap(p -> p.profileId,
+                .collect(Collectors.<CustomerDto.ProfileDto, String, String> toMap(p -> p.profileId,
                         p -> p.options != null && p.options.lang != null ? p.options.lang : DEFAULT_LANGUAGE));
 
         EntitlementsDto entitlementsDto = auth.get(auth.getServiceConfig().getServiceUrl(PURCHASE_SERVICE_URL_FIELD),
